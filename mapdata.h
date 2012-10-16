a24a743911a4e6ce3014b0ac5739f1a4e6169af9
TheDarklingWolf <TheDarklingWolf@gmail.com>
2012-10-16 17:51:32 +0200

    Makeshift beds

--- mapdata.h
+++ mapdata.h
@@ -104,7 +104,7 @@ t_marloss, t_fungus, t_tree_fungal,
 t_water_sh, t_water_dp, t_sewage,
 t_lava,
 // Embellishments
-t_bed, t_toilet, t_bed_crap,
+t_bed, t_toilet, t_makeshift_bed,
 t_sandbox, t_slide, t_monkey_bars, t_backboard,
 t_bench, t_table, t_pool_table,
 t_gas_pump, t_gas_pump_smashed,
