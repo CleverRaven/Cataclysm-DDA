8b134d84c6bb9a6f2ce7b513ed7759c2997488e8
Kevin Granade <kevin.granade@gmail.com>
2012-12-21 19:08:39 -0600

    Make active_gunmod() public.

--- item.h
+++ item.h
@@ -64,6 +64,7 @@ public:
  bool has_flag(item_flag f);
  bool has_technique(technique_id t, player *p = NULL);
  int has_gunmod(int type);
+ int active_gunmod();
  std::vector<technique_id> techniques();
  bool goes_bad();
  bool count_by_charges();
@@ -130,7 +131,6 @@ public:
 
 private:
  static itype * nullitem_m;
- int active_gunmod();
 };
 
 std::ostream & operator<<(std::ostream &, const item &);
