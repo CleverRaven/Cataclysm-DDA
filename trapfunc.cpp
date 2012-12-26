9767fd08658bdee6dc4e0ea0a10c65aa107d7e17
TheDarklingWolf <TheDarklingWolf@gmail.com>
2012-12-26 16:41:09 +0000

    Merged revision f8b645d341 of DinoCataclysm

--- trapfunc.cpp
+++ trapfunc.cpp
@@ -713,12 +713,9 @@ void trapfuncm::snake(game *g, monster *z, int x, int y)
 
 bool trap::is_benign()
 {
-<<<<<<< HEAD
-  return (components[0] == itm_rollmat || components[0] == itm_cot);
-=======
-  if (id == tr_rollmat || id == itm_cot || id == itm_firepit)
+  if (id == tr_rollmat || id == itm_cot)
     return true;
   else      
   return false;
->>>>>>> f8b645d... bugfix for benign trap checks
+
 }
