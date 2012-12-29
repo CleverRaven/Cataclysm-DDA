1523f637500f600c023f87d6db0071d73afcffd4
TheDarklingWolf <TheDarklingWolf@gmail.com>
2012-12-29 14:19:36 +0000

    Fixed sewing kit reloading

--- itypedef.cpp
+++ itypedef.cpp
@@ -4313,6 +4313,7 @@ std::string ammo_name(ammotype t)
   case AT_308:	  return ".308";
   case AT_40MM:   return "40mm grenade";
   case AT_GAS:	  return "gasoline";
+  case AT_THREAD: return "thread";
   case AT_BATT:   return "batteries";
   case AT_PLUT:   return "plutonium";
   case AT_FUSION: return "fusion cell";
@@ -4349,6 +4350,7 @@ itype_id default_ammo(ammotype guntype)
  case AT_PLASMA:return itm_plasma;
  case AT_PLUT:	return itm_plut_cell;
  case AT_GAS:	return itm_gasoline;
+ case AT_THREAD:return itm_thread;
  }
  return itm_null;
 }
