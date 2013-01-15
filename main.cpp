14edde3a64f8744849f4ca41b7a1e51a3deba5dd
Williham Totland <williham.totland@gmail.com>
2013-01-15 11:14:21 +0100

    Improved RNG seeding.
    Also, whitespace fixes.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #if (defined _WIN32 || defined WINDOWS)
 	#include "catacurse.h"
 #elif (defined __CYGWIN__)
-       #include "ncurses/curses.h"
+  #include "ncurses/curses.h"
 #else
 	#include <curses.h>
 #endif
@@ -20,14 +20,11 @@
 #include "debug.h"
 //#include <sys/stat.h>
 
-
 int main(int argc, char *argv[])
 {
 #ifdef ENABLE_LOGGING
-    setupDebug();
+  setupDebug();
 #endif
- srand(time(NULL));
-
 // ncurses stuff
  initscr(); // Initialize ncurses
  noecho();  // Don't echo keypresses
@@ -36,8 +33,8 @@ int main(int argc, char *argv[])
  init_colors(); // See color.cpp
  curs_set(0); // Invisible cursor
 
- rand();  // For some reason a call to rand() seems to be necessary to avoid
-          // repetion.
+ std::srand(time(NULL));
+
  bool quit_game = false;
  bool delete_world = false;
  game *g = new game;
@@ -46,12 +43,12 @@ int main(int argc, char *argv[])
  load_options();
  do {
   g->setup();
-  while (!g->do_turn());
+  while (!g->do_turn()) ;
   if (g->uquit == QUIT_DELETE_WORLD)
     delete_world = true;
   if (g->game_quit())
    quit_game = true;
- } while (!quit_game); 
+ } while (!quit_game);
  MAPBUFFER.save();
 
   if (delete_world && (remove("save/") != 0))
@@ -60,9 +57,9 @@ int main(int argc, char *argv[])
       system("rmdir /s /q save");
     #else
       system("rm -rf save/*");
-    #endif 
+    #endif
   }
- 
+
  erase(); // Clear screen
  endwin(); // End ncurses
 #if (defined _WIN32 || defined WINDOWS)
@@ -73,4 +70,3 @@ int main(int argc, char *argv[])
 #endif
  return 0;
 }
-
