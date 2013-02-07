324a5e676b4dfcbac807eabb25a40e1e5f045685
Owen Diamond <oddzball@gmail.com>
2013-02-06 19:27:56 -0700

    size test 2

--- overmap.cpp
+++ overmap.cpp
@@ -1360,14 +1360,14 @@ spawns happen at... <cue Clue music>
 20:56	<kevingranade>: game:pawn_mon() in game.cpp:7380*/
 void overmap::place_cities(std::vector<city> &cities, int min) //Oddzball-Population control? 
 {
- int NUM_CITIES = dice(2, 7) + rng(min, min + 4);
+ int NUM_CITIES = (1);
  int cx, cy, cs;
  int start_dir;
 
  for (int i = 0; i < NUM_CITIES; i++) {
   cx = rng(20, OMAPX - 41);
   cy = rng(20, OMAPY - 41);
-  cs = rng(4, 10); //Oddzball-Smaller cities test
+  cs = rng(1, 5); //Oddzball-Smaller cities test
   if (ter(cx, cy) == ot_field) {
    ter(cx, cy) = ot_road_nesw;
    city tmp; tmp.x = cx; tmp.y = cy; tmp.s = cs;
