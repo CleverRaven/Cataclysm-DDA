0f0da70f137dbc876595eb50118521cf89d4bcab
Owen Diamond <oddzball@gmail.com>
2013-02-23 19:52:25 -0700

    Windows scalable viewport attempt 4

--- catacurse.cpp
+++ catacurse.cpp
@@ -285,8 +285,8 @@ fin.open("data\\FONTDATA");
  }
     halfwidth=fontwidth / 2;
     halfheight=fontheight / 2;
-    WindowWidth= (69 + OPTIONS[OPT_VIEWPORT_X]) * fontwidth;
-    WindowHeight=(14 + OPTIONS[OPT_VIEWPORT_Y]) *fontheight;
+    WindowWidth= (55 + (OPTIONS[OPT_VIEWPORT_X] * 2 + 1)) * fontwidth;
+    WindowHeight= (OPTIONS[OPT_VIEWPORT_Y] * 2 + 1) *fontheight;
     WindowX=(GetSystemMetrics(SM_CXSCREEN) / 2)-WindowWidth/2;    //center this
     WindowY=(GetSystemMetrics(SM_CYSCREEN) / 2)-WindowHeight/2;   //sucker
     WinCreate();    //Create the actual window, register it, etc
@@ -325,7 +325,7 @@ fin.open("data\\FONTDATA");
 //    WindowCount=0;
 
     delete typeface_c;
-    mainwin = newwin((14 + (OPTIONS[OPT_VIEWPORT_Y] * 2 + 1)),(69 + (OPTIONS[OPT_VIEWPORT_Y] * 2 + 1)),0,0);
+    mainwin = newwin((OPTIONS[OPT_VIEWPORT_Y] * 2 + 1),(55 + (OPTIONS[OPT_VIEWPORT_Y] * 2 + 1)),0,0);
     return mainwin;   //create the 'stdscr' window and return its ref
 };
 
