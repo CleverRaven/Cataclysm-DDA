23f75a3248bed914bcf73a994ce69e5c81baf5ca
TheDarklingWolf <TheDarklingWolf@gmail.com>
2013-03-03 17:10:32 +0000

    Fixed unburied landmines being placed in mx_minefield

--- mapgen.cpp
+++ mapgen.cpp
@@ -8941,7 +8941,7 @@ void map::add_extra(map_extra type, game *g)
      }
      if (placed == tr_beartrap && has_flag(diggable, i, j)) {
       if (one_in(8))
-       placed = tr_landmine;
+       placed = tr_landmine_buried;
       else
        placed = tr_beartrap_buried;
      }
@@ -9101,9 +9101,9 @@ void map::add_extra(map_extra type, game *g)
   }
   for (int i = 0; i < num_mines; i++) {
    int x = rng(0, SEEX * 2 - 1), y = rng(0, SEEY * 2 - 1);
-   if (!has_flag(diggable, x, y) || one_in(4))
+   if (!has_flag(diggable, x, y) || one_in(8))
     ter(x, y) = t_dirtmound;
-   add_trap(x, y, tr_landmine);
+   add_trap(x, y, tr_landmine_buried);
   }
  }
  break;
