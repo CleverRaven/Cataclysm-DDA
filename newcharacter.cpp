63ab9ddd9ffb737fe3c0897c17d7be337cfb2143
tasetase <pp.89@live.com>
2013-03-05 22:47:23 -0500

    Fixed missed confict (\!?)

--- newcharacter.cpp
+++ newcharacter.cpp
@@ -249,19 +249,11 @@ End of cheatery */
  if (has_trait(PF_ASTHMA)) {
   tmp = item(g->itypes[itm_inhaler], 0, 'a' + worn.size());
   inv.push_back(tmp);
-<<<<<<< HEAD
  }
  // Add starter gear, possibly tie this to profession setting later.
 /* tmp = item(g->itypes[itm_pockknife], 0,'a' + worn.size());
   inv.push_back(tmp);
  tmp = item(g->itypes[itm_lighter], 0,'a' + worn.size());
-=======
- }
- // Add starter gear, possibly tie this to profession setting later.
-/* tmp = item(g->itypes[itm_pockknife], 0,'a' + worn.size());
-  inv.push_back(tmp);
- tmp = item(g->itypes[itm_lighter], 0,'a' + worn.size());
->>>>>>> upstream/master
   inv.push_back(tmp); */
 // make sure we have no mutations
  for (int i = 0; i < PF_MAX2; i++)
