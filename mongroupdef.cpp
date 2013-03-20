302bfd83a55a9d44afe544e15498181ec386c16f
tasetase <pp.89@live.com>
2013-03-19 21:55:34 -0400

    Fixed compile issue

--- mongroupdef.cpp
+++ mongroupdef.cpp
@@ -83,7 +83,7 @@ MonsterGroup MonsterGroupManager::GetMonsterGroup(std::string group)
     std::map<std::string, MonsterGroup>::iterator it = monsterGroupMap.find(group);
     if(it == monsterGroupMap.end())
     {
-        debugmsg("Unable to get the group '%s'", group);
+        debugmsg("Unable to get the group '%s'", group.c_str());
         return MonsterGroup();
     }
     else
