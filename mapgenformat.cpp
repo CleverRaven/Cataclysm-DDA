c955683776229206f5774bab8fe074e09a749496
Quietust <quietust@gmail.com>
2013-04-21 18:30:15 -0500

    Minor cleanup to determine_terrain_with_simple_method -some other compilers choke on this

--- mapgenformat.cpp
+++ mapgenformat.cpp
@@ -86,7 +86,7 @@ internal::format_effect* simple_method_bind(std::string characters, ...)
  va_list vl;
  va_start(vl,characters);
  for(int i = 0; i < characters.size(); i++)
-  determiners.push_back( new internal::determine_terrain_with_simple_method( va_arg(vl, ter_id (*)() ) ));
+  determiners.push_back( new internal::determine_terrain_with_simple_method( va_arg(vl, internal::determine_terrain_with_simple_method::ter_id_func ) ));
  va_end(vl);
  return new internal::format_effect(characters, determiners);
 }
