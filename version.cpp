c6ae17fc13e8ee624a72f1b5fb5079d86d8950b0
fnord <fnord@fnord.mobi>
2013-04-29 11:16:11 -0500

    win32: Define hard coded version instead of including version.h Temporary workaround.

--- version.cpp
+++ version.cpp
@@ -1,2 +1,11 @@
+#if (defined _WIN32 || defined WINDOWS)
+
+#define VERSION "0.3" // FIXME: automatically generate VERSION based on git
+
+#else
+
 #include "version.h"
+
+#endif
+
 const char* getVersionString() { return VERSION; }
