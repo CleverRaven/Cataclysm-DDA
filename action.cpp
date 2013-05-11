84244301c0d6c87fe39057c6330b85a2a3b461be
fnord <fnord@fnord.mobi>
2013-05-11 08:31:47 -0500

    game::load_keyboard_settings correctly handles it whn space and tab are bound.

--- action.cpp
+++ action.cpp
@@ -30,8 +30,9 @@ Warning!  data/keymap.txt contains an unknown action, \"%s\"\n\
 Fix data/keymap.txt at your next chance!", id.c_str());
    else {
     while (fin.peek() != '\n' && !fin.eof()) {
-     char ch;
-     fin >> ch;
+     char space; char ch;
+     fin >> std::noskipws >> space >> ch;
+     fin >> std::skipws;
      if (keymap.find(ch) != keymap.end())
       debugmsg("\
 Warning!  '%c' assigned twice in the keymap!\n\
