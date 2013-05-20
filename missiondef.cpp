9074961c55ec8556fdeadfd22aed26df35940421
acidia <stm.vol@gmail.com>
2013-05-19 19:38:03 -0500

    Added Kill Jabberwock

--- missiondef.cpp
+++ missiondef.cpp
@@ -87,6 +87,11 @@ mission_type(id, name, goal, diff, val, urgent, place, start, end, fail) )
 	&mission_end::standard, &mission_fail::standard);
   ORIGINS(ORIGIN_OPENER_NPC, ORIGIN_ANY_NPC);
   ITEM("record_accounting");
+  
+ MISSION("Kill Jabberwock", MGOAL_KILL_MONSTER, 5, 2500, true,
+	&mission_place::always, &mission_start::place_jabberwock,
+	&mission_end::standard, &mission_fail::standard);
+  ORIGINS(ORIGIN_OPENER_NPC, ORIGIN_ANY_NPC);
 
  MISSION("Find a Book", MGOAL_FIND_ANY_ITEM, 2, 800, false,
 	&mission_place::always, &mission_start::place_book,
