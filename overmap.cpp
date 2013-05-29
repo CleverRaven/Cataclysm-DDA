7642811bbf469959cd913978f4b4460f2d5dd199
Fergus Dall <fergus@beware.dropbear.id.au>
2013-05-29 20:58:09 +0930

    Generate extra downstairs in labs as needed to ensure connectivity

--- overmap.cpp
+++ overmap.cpp
@@ -1794,6 +1794,7 @@ void overmap::make_road(int cx, int cy, int cs, int dir, city town)
 
 bool overmap::build_lab(int x, int y, int z, int s)
 {
+ std::vector<point> generated_lab;
  ter(x, y, z) = ot_lab;
  for (int n = 0; n <= 1; n++) {	// Do it in two passes to allow diagonals
   for (int i = 1; i <= s; i++) {
@@ -1802,14 +1803,31 @@ bool overmap::build_lab(int x, int y, int z, int s)
      if ((ter(lx - 1, ly, z) == ot_lab || ter(lx + 1, ly, z) == ot_lab ||
          ter(lx, ly - 1, z) == ot_lab || ter(lx, ly + 1, z) == ot_lab) &&
          one_in(i))
-      ter(lx, ly, z) = ot_lab;
+     {
+         ter(lx, ly, z) = ot_lab;
+         generated_lab.push_back(point(lx,ly));
+     }
     }
    }
   }
  }
+ bool generate_stairs = true;
+ for (std::vector<point>::iterator it=generated_lab.begin();
+      it != generated_lab.end(); it++)
+ {
+     if (ter(it->x, it->y, z+1) == ot_lab_stairs)
+         generate_stairs = false;
+ }
+ if (generate_stairs && generated_lab.size() > 0)
+ {
+     int v = rng(0,generated_lab.size());
+     point p = generated_lab[v];
+     ter(p.x, p.y, z+1) = ot_lab_stairs;
+ }
+
  ter(x, y, z) = ot_lab_core;
  int numstairs = 0;
- if (s > 1) {	// Build stairs going down
+ if (s > 0) {	// Build stairs going down
   while (!one_in(6)) {
    int stairx, stairy;
    int tries = 0;
