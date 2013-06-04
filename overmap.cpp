7202b9da7a8f2460af9f19b8bcc0c0f003de1d01
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2013-06-04 18:20:49 +1200

    Make indents match surrounding code

--- overmap.cpp
+++ overmap.cpp
@@ -1292,37 +1292,39 @@ void overmap::draw(WINDOW *w, game *g, int z, int &cursx, int &cursy,
     {
         print_npcs(g, w, cursx, cursy, z);
     }
-   cur_ter = ter(cursx, cursy, z);
+
+
+  cur_ter = ter(cursx, cursy, z);
 // Draw the vertical line
-   for (int j = 0; j < om_map_height; j++)
-    mvwputch(w, j, om_map_width, c_white, LINE_XOXO);
+  for (int j = 0; j < om_map_height; j++)
+   mvwputch(w, j, om_map_width, c_white, LINE_XOXO);
 // Clear the legend
-   for (int i = om_map_width + 1; i < om_map_width + 55; i++) {
-    for (int j = 0; j < om_map_height; j++)
-     mvwputch(w, j, i, c_black, ' ');
-   }
+  for (int i = om_map_width + 1; i < om_map_width + 55; i++) {
+   for (int j = 0; j < om_map_height; j++)
+   mvwputch(w, j, i, c_black, ' ');
+  }
 
-   if (csee) {
-    mvwputch(w, 1, om_map_width + 1, oterlist[ccur_ter].color, oterlist[ccur_ter].sym);
-    mvwprintz(w, 1, om_map_width + 3, oterlist[ccur_ter].color, "%s",
-              oterlist[ccur_ter].name.c_str());
-   } else
-    mvwprintz(w, 1, om_map_width + 1, c_dkgray, "# Unexplored");
+  if (csee) {
+   mvwputch(w, 1, om_map_width + 1, oterlist[ccur_ter].color, oterlist[ccur_ter].sym);
+   mvwprintz(w, 1, om_map_width + 3, oterlist[ccur_ter].color, "%s",
+             oterlist[ccur_ter].name.c_str());
+  } else
+   mvwprintz(w, 1, om_map_width + 1, c_dkgray, "# Unexplored");
 
-   if (target.x != -1 && target.y != -1) {
-    int distance = rl_dist(origx, origy, target.x, target.y);
-    mvwprintz(w, 3, om_map_width + 1, c_white, "Distance to target: %d", distance);
-   }
-   mvwprintz(w, 15, om_map_width + 1, c_magenta, "Use movement keys to pan.  ");
-   mvwprintz(w, 16, om_map_width + 1, c_magenta, "0 - Center map on character");
-   mvwprintz(w, 17, om_map_width + 1, c_magenta, "/ - Search                 ");
-   mvwprintz(w, 18, om_map_width + 1, c_magenta, "N - Add/Edit a note        ");
-   mvwprintz(w, 19, om_map_width + 1, c_magenta, "D - Delete a note          ");
-   mvwprintz(w, 20, om_map_width + 1, c_magenta, "L - List notes             ");
-   mvwprintz(w, 21, om_map_width + 1, c_magenta, "Esc or q - Return to game  ");
-   char level_string[10];
-   sprintf(level_string, "LEVEL %i",z);
-   mvwprintz(w, getmaxy(w)-1, om_map_width + 1, c_red, level_string);
+  if (target.x != -1 && target.y != -1) {
+   int distance = rl_dist(origx, origy, target.x, target.y);
+   mvwprintz(w, 3, om_map_width + 1, c_white, "Distance to target: %d", distance);
+  }
+  mvwprintz(w, 15, om_map_width + 1, c_magenta, "Use movement keys to pan.  ");
+  mvwprintz(w, 16, om_map_width + 1, c_magenta, "0 - Center map on character");
+  mvwprintz(w, 17, om_map_width + 1, c_magenta, "/ - Search                 ");
+  mvwprintz(w, 18, om_map_width + 1, c_magenta, "N - Add/Edit a note        ");
+  mvwprintz(w, 19, om_map_width + 1, c_magenta, "D - Delete a note          ");
+  mvwprintz(w, 20, om_map_width + 1, c_magenta, "L - List notes             ");
+  mvwprintz(w, 21, om_map_width + 1, c_magenta, "Esc or q - Return to game  ");
+  char level_string[10];
+  sprintf(level_string, "LEVEL %i",z);
+  mvwprintz(w, getmaxy(w)-1, om_map_width + 1, c_red, level_string);
 // Done with all drawing!
   wrefresh(w);
 }
