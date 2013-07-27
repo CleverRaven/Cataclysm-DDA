a4a9b15c2ea6f7850a7046426a85391209dd388c
Peter Lacey-Bordeaux <placeybordeaux@gmail.com>
2013-07-27 19:07:09 -0400

    added \ns

--- options.cpp
+++ options.cpp
@@ -422,7 +422,7 @@ std::string option_desc(option_key key)
     case OPT_AUTO_PICKUP:         return _("Enable item auto pickup. Change\npickup rules with the Auto Pickup\nManager in the Help Menu ?3");
     case OPT_AUTO_PICKUP_ZERO:    return _("Auto pickup items with\n0 Volume or Weight");
     case OPT_AUTO_PICKUP_SAFEMODE:return _("Auto pickup is disabled\nas long as you can see\nmonsters nearby.\n\nThis is affected by\nSafemode proximity distance.");
-    case OPT_SORT_CRAFTING:       return _("Will sort the crafting menus so things that you are able to craft have priority");
+    case OPT_SORT_CRAFTING:       return _("Will sort the crafting menus\n so things that you are\n able to craft come up first");
     default:                      return " ";
     }
     return "Big ol Bug (options.cpp:option_desc)";
