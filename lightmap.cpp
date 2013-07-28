f2ae1635368c08ffa8a25239f624d33b72477d9c
fnord <fnord@fnord.mobi>
2013-07-28 01:27:39 -0500

    Refactor the field system due to the incredible amounts of overhead and slowdown the previous refactor introduced in large fires.
    + process_fields_in_submap: Eliminate hundreds of thousands of field iterations via pointer variables
      in process_fields_in_submap (instead of if(ptr->findField(..) && ptr->findField(..)->something() ) {
      ptr->findField(same_freaking_field)->stuff.
    
    + field::* Completely eliminate the need for iteration by changing field storage from std::vector to
      std::map.
    
    In a burning hospital test case, this reduced the runtime of process_fields_in_submap from ~1000 ms to
      ~300 ms; switching to variables instead of performing repeated lookups for the same thing took it to
      ~650ms, switching to std::map accounts for the rest.
    
    Further optimization is possible; caching fire and smoke in a struct array made the
      same scenario process in 110ms. Lightmap is also incredibly wasteful as light sources cast rays into
      light sources in the next tile.

--- lightmap.cpp
+++ lightmap.cpp
@@ -89,8 +89,9 @@ void map::generate_lightmap(game* g)
     apply_light_source(sx, sy, 3, false);
 
    field_entry *cur = NULL;
-	for(std::vector<field_entry*>::iterator field_list_it = current_field.getFieldStart(); field_list_it != current_field.getFieldEnd(); ++field_list_it){
-		cur = (*field_list_it);
+   for(std::map<field_id, field_entry*>::iterator field_list_it = current_field.getFieldStart(); field_list_it != current_field.getFieldEnd(); ++field_list_it){
+       cur = field_list_it->second;
+
 		if(cur == NULL) continue;
    // TODO: [lightmap] Attach light brightness to fields
 		switch(cur->getFieldType()) {
