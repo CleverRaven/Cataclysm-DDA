01e1e418f360705a3b0a96d2031c0f309767457b
gmcnew <gmcnew@gmail.com>
2013-08-23 21:49:55 +0000

    Fix an indexing bug when disarming engine traps.

--- map.cpp
+++ map.cpp
@@ -2853,7 +2853,7 @@ void map::disarm_trap(game *g, const int x, const int y)
       for (int i = -1; i <= 1; i++) {
           for (int j = -1; j <= 1; j++) {
               if (i != 0 || j != 0) {
-                  remove_trap(x + i, y + i);
+                  remove_trap(x + i, y + j);
               }
           }
       }
