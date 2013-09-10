d0aeefa0619ff7d207c1b4943584c12c8f91a3e4
fnord <fnord@fnord.mobi>
2013-09-10 16:05:08 -0500

    extra save/load helper functions

--- picofunc.h
+++ picofunc.h
@@ -1,21 +1,7 @@
 #ifndef _PICOFUNC_H_
 #define _PICOFUNC_H_
 #include "picojson.h"
-/* these defines will go away when all of them are complete, which means
-   #define jsonsave_full
-   is a reality.
-*/
-#define jsonsave_item 1 // complete, optimized to skip values if default. Affects: game.sav and maps.txt
-#define jsonsave_monster 1 // complete, unoptimized (not needed): game.sav
-// #define jsonsave_player 1 // pending: .sav
-// #define jsonsave_misc 1 // misc stuff in game.sav.
-// #define jsonsave_invlet_cache 1 // pending: game.sav, overmap
-// #define jsonsave_inventory 1 // requires jsonsave_invlet_cache: game.sav, overmap
-/* 
-   npcs can be jsonized chunks in overmap, instead of stringsteam chunks
-*/
-// #define jsonsave_npc 1 // pending
-// #define 
+#include "enums.h"
 
 
 #define pv(x) picojson::value(x)
@@ -24,5 +10,14 @@ bool picoverify();
 bool picobool(const picojson::object & obj, std::string key, bool & var);
 bool picoint(const picojson::object & obj, std::string key, int & var);
 bool picostring(const picojson::object & obj, std::string key, std::string & var);
+bool picouint(const picojson::object & obj, std::string key, unsigned int & var);
+bool picopoint(const picojson::object & obj, std::string key, point & var);
+bool picovector(picojson::object & obj, std::string key, std::vector<int> & var);
+
+picojson::array * pgetarray(picojson::object & obj, std::string key);
+picojson::object * pgetmap(picojson::object & obj, std::string key);
+
+picojson::value json_wrapped_vector ( std::vector<int> val );
+picojson::value json_wrapped_vector ( std::vector<std::string> val );
 
 #endif
