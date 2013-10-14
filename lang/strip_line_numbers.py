23c3aafe7dc5d354129ecb9dd95808f63328ca4b
Tommy <yobbobanana@gmx.com>
2013-10-15 07:45:33 +1300

    Refactor lang/strip_line_numbers.py slightly.

--- lang/strip_line_numbers.py
+++ lang/strip_line_numbers.py
@@ -8,43 +8,48 @@ import os
 
 usage = "USAGE: strip_line_numbers.py <filename>"
 
-if len(sys.argv) < 2:
-    # ideally this would read from stdin and input to stdout,
-    # but not need to do that yet.
-    print(usage)
-    exit(1)
-elif len(sys.argv) > 2:
-    # should take a list of files, but again, no need yet.
-    print(usage)
-    exit(1)
+def strip_line_numbers(filename):
+    "Strip line numbers from the specified .pot or .po file."
 
-filename = sys.argv[1]
+    if not os.path.isfile(filename):
+        print("invalid filename: %s" % filename)
+        exit(1)
 
-if not os.path.isfile(filename):
-    print("invalid filename: %s" % filename)
-    exit(1)
+    # modifying the file in-place, so just read the whole thing
+    lines = open(filename, 'r').readlines()
 
-# modifying the file in-place, so just read the whole thing
-lines = open(filename, 'r').readlines()
+    # strip line-numbers
+    for i in range(len(lines)):
+        line = lines[i]
+        # only act on line-comments
+        if not line.startswith("#:"): continue
+        # line comments are of the format: "#: <file>:<line> <file>:<line>"
+        items = line.split()
+        newitems = ["#:"]
+        # replace line elements while yanking the line number,
+        # and checking for duplicates
+        for item in items[1:]:
+            newname = item.split(':')[0]
+            if not newname in newitems:
+                newitems.append(newname)
+        # replace the old line with the new
+        newline = ' '.join(newitems) + '\n'
+        lines[i] = newline
 
-# strip line-numbers
-for i in range(len(lines)):
-    line = lines[i]
-    # only act on line-comments
-    if not line.startswith("#:"): continue
-    # line comments are of the format: "#: <file>:<line> <file>:<line>"
-    items = line.split()
-    newitems = ["#:"]
-    # replace line elements while yanking the line number,
-    # and checking for duplicates
-    for item in items[1:]:
-        newname = item.split(':')[0]
-        if not newname in newitems:
-            newitems.append(newname)
-    # replace the old line with the new
-    newline = ' '.join(newitems) + '\n'
-    lines[i] = newline
+    # write the file back out with the line numbers stripped
+    open(filename, 'w').writelines(lines)
 
-# write the file back out with the line numbers stripped
-open(filename, 'w').writelines(lines)
+if __name__ == "__main__":
+    # get filename from sys.argv and strip
+    if len(sys.argv) < 2:
+        # ideally this would read from stdin and input to stdout,
+        # but not need to do that yet.
+        print(usage)
+        exit(1)
+    elif len(sys.argv) > 2:
+        # should take a list of files, but again, no need yet.
+        print(usage)
+        exit(1)
+
+    strip_line_numbers(sys.argv[1])
 
