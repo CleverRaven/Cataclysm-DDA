d2ed735694fb6574fd5799f34308a26ed68a6727
Tommy <yobbobanana@gmx.com>
2013-10-17 04:43:12 +1300

    Style iexamine::rubble.

--- iexamine.cpp
+++ iexamine.cpp
@@ -188,32 +188,32 @@ void iexamine::cardreader(game *g, player *p, map *m, int examx, int examy) {
 }
 
 void iexamine::rubble(game *g, player *p, map *m, int examx, int examy) {
- if (!(p->has_amount("shovel", 1) || p->has_amount("primitive_shovel", 1)|| p->has_amount("e_tool", 1))) {
-  g->add_msg(_("If only you had a shovel..."));
-  return;
- }
- const char *xname = m->tername(examx, examy).c_str();
- if (query_yn(_("Clear up that %s?"),xname)) {
-   // "Remove"
-  p->moves -= 200;
+    if (!(p->has_amount("shovel", 1) || p->has_amount("primitive_shovel", 1)|| p->has_amount("e_tool", 1))) {
+        g->add_msg(_("If only you had a shovel..."));
+        return;
+    }
+    const char *xname = m->tername(examx, examy).c_str();
+    if (query_yn(_("Clear up that %s?"),xname)) {
+        // "Remove"
+        p->moves -= 200;
 
-   // "Replace"
-  if(m->ter(examx,examy) == t_rubble) {
-   item rock(g->itypes["rock"], g->turn);
-   m->add_item(p->posx, p->posy, rock);
-   m->add_item(p->posx, p->posy, rock);
-  }
+        // "Replace"
+        if(m->ter(examx,examy) == t_rubble) {
+            item rock(g->itypes["rock"], g->turn);
+            m->add_item(p->posx, p->posy, rock);
+            m->add_item(p->posx, p->posy, rock);
+        }
 
-   // "Refloor"
-  if (g->levz < 0) {
-   m->ter_set(examx, examy, t_rock_floor);
-  } else {
-   m->ter_set(examx, examy, t_dirt);
-  }
+        // "Refloor"
+        if (g->levz < 0) {
+            m->ter_set(examx, examy, t_rock_floor);
+        } else {
+            m->ter_set(examx, examy, t_dirt);
+        }
 
-   // "Remind"
-  g->add_msg(_("You clear up that %s."), xname);
- }
+        // "Remind"
+        g->add_msg(_("You clear up that %s."), xname);
+    }
 }
 
 void iexamine::chainfence(game *g, player *p, map *m, int examx, int examy) {
