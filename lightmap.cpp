e0dc41e1d26f14ac7fad470bd273cc693382431f
Ian Strachan <ian_e_strachan@hotmail.com>
2013-10-19 16:52:06 -0400

    That's not how modulus works.

--- lightmap.cpp
+++ lightmap.cpp
@@ -182,8 +182,8 @@ void map::generate_lightmap(game* g)
        std::vector<int> light_indices = vehs[v].v->all_parts_with_feature("CIRCLE_LIGHT");
        for (std::vector<int>::iterator part = light_indices.begin();
                part != light_indices.end(); ++part) {
-           if((g->turn % 2 && vehs[v].v->part_info(*part).has_flag("EVENTURN")) ||
-                   (!(g->turn % 2) && vehs[v].v->part_info(*part).has_flag("ODDTURN"))) {
+           if((g->turn % 2 && vehs[v].v->part_info(*part).has_flag("ODDTURN")) ||
+                   (!(g->turn % 2) && vehs[v].v->part_info(*part).has_flag("EVENTURN"))) {
                int px = vehs[v].x + vehs[v].v->parts[*part].precalc_dx[0];
                int py = vehs[v].y + vehs[v].v->parts[*part].precalc_dy[0];
                if(INBOUNDS(px, py)) {
