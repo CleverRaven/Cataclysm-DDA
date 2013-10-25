76e45071bd824a4d1b0ac87ec4f78838060bb91e
illi-kun <D.E.Serov@gmail.com>
2013-10-25 16:09:09 +0400

    modification of advanced inventory

--- advanced_inv.cpp
+++ advanced_inv.cpp
@@ -177,13 +177,13 @@ void advanced_inventory::print_items(advanced_inventory_pane &pane, bool active)
         mvwprintz(window, 6 + x, ( compact ? 1 : 4 ), thiscolor, "%s", items[i].it->tname(g).c_str() );
 
         if(items[i].it->charges > 0) {
-            wprintz(window, thiscolor, " (%d)",items[i].it->charges);
+            wprintz(window, thiscolor, " [%d]",items[i].it->charges);
         } else if(items[i].it->contents.size() == 1 && items[i].it->contents[0].charges > 0) {
-            wprintz(window, thiscolor, " (%d)",items[i].it->contents[0].charges);
+            wprintz(window, thiscolor, " [%d]",items[i].it->contents[0].charges);
         }
 
         if( isinventory && items[i].stacks > 1 ) {
-            mvwprintz(window, 6 + x, amount_column, thiscolor, "[%d]", items[i].stacks);
+            mvwprintz(window, 6 + x, amount_column, thiscolor, "x %d", items[i].stacks);
         } else if ( isall ) {
             mvwprintz(window, 6 + x, amount_column, thiscolor, "%s", squares[items[i].area].shortname.c_str());
         }
