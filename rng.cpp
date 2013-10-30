3b90760f738c0df9b66457c892112b7bb67a4b23
phaethonfire <phaethonfire@gmail.com>
2013-10-30 14:34:17 -0500

    fix merge conflicts

--- rng.cpp
+++ rng.cpp
@@ -4,12 +4,8 @@
 
 
 // http://software.intel.com/en-us/articles/fast-random-number-generator-on-the-intel-pentiumr-4-processor
-<<<<<<< HEAD
 // fastrand() is significantly faster than rand().  Not crypto secure, but good for CataDDA.
 // These functions should be revisited if the code moves to C++11
-=======
-// fastrand() is 5x faster than rand().  Not crypto secure, but better than rand.
->>>>>>> d56c8c5c09ac7376f9c944206f386f2ed62b82b5
 static unsigned int g_seed;
 
 //Used to seed the generator.
@@ -17,28 +13,17 @@ void fast_srand( int seed ) {
     g_seed = seed;
 }
 
-<<<<<<< HEAD
 //fastrand routine returns one integer between 0 and 2^15-1
 inline int fastrand() {
     g_seed = (214013*g_seed+2531011);
     return (g_seed>>16)&0x7FFFF; 
-=======
-//fastrand routine returns one integer, modified from the intel version to give a max of 1048576.
-inline int fastrand() {
-    g_seed = (214013*g_seed+2531011);
-    return (g_seed>>12); 
->>>>>>> d56c8c5c09ac7376f9c944206f386f2ed62b82b5
 }
 
 long rng(long val1, long val2)
 {
     long minVal = (val1 < val2) ? val1 : val2;
     long maxVal = (val1 < val2) ? val2 : val1;
-<<<<<<< HEAD
     return minVal + long((maxVal - minVal + 1) * double(fastrand() / 32768.0));
-=======
-    return minVal + long((maxVal - minVal + 1) * double(fastrand() / double(1048577)));
->>>>>>> d56c8c5c09ac7376f9c944206f386f2ed62b82b5
 }
 
 bool one_in(int chance)
@@ -48,11 +33,7 @@ bool one_in(int chance)
 
 bool x_in_y(double x, double y)
 {
-<<<<<<< HEAD
     return ((double)fastrand() / 32768.0) <= ((double)x/y);
-=======
-    return ((double)fastrand() / 1048577) <= ((double)x/y);
->>>>>>> d56c8c5c09ac7376f9c944206f386f2ed62b82b5
 }
 
 int dice(int number, int sides)
