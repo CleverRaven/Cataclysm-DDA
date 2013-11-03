366522edded634126aa2f2402c85496add0919c8
KA101 <cstrayer@rocketmail.com>
2013-11-02 20:22:36 -0400

    Alpha mutagen
    Item, mutation categories, spawn locations, recipe item, mention in the
    Lab Notes, actual recipe, iuse flag, and dreams.
    
    I *think* that should be everything?

--- iuse.cpp
+++ iuse.cpp
@@ -899,6 +899,9 @@ int iuse::mutagen(game *g, player *p, item *it, bool t) {
     } else if( it->has_flag("MUTAGEN_TROGLOBITE") ) {
         g->add_msg_if_player(p, _("You yearn for a cool, dark place to hide."));
         p->mutate_category(g, "MUTCAT_TROGLO");
+    } else if( it->has_flag("MUTAGEN_ALPHA") ) {
+        g->add_msg_if_player(p, _("You feel...better. Somehow."));
+        p->mutate_category(g, "MUTCAT_ALPHA");
     } else {
         if (!one_in(3)) {
             p->mutate(g);
