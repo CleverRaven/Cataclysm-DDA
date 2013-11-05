4fe0d7fd5c4206db1328709935ef81f933b7de73
KA101 <cstrayer@rocketmail.com>
2013-11-05 18:43:06 -0500

    spacing again

--- melee.cpp
+++ melee.cpp
@@ -946,7 +946,7 @@ bool player::block_hit(game *g, monster *z, player *p, body_part &bp_hit, int &s
     if (blocks_left <= 0) return false;
 
     // if weapon, then extra reduction
-    if (!unarmed_attack()  && can_weapon_block()) {
+    if (!unarmed_attack() && can_weapon_block()) {
         float mult = 1.0f;
         if (weapon.has_technique("WBLOCK_1",this)) {
             mult = 0.4;
