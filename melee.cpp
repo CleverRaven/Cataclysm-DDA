7c442c2b2636301a73c97bef84586d02cdf04735
KA101 <cstrayer@rocketmail.com>
2013-11-04 23:41:19 -0500

    WBLOCK support
    As Kevin noted.

--- melee.cpp
+++ melee.cpp
@@ -939,7 +939,7 @@ bool player::block_hit(game *g, monster *z, player *p, body_part &bp_hit, int &s
     if (blocks_left <= 0) return false;
 
     // if weapon, then extra reduction
-    if (!unarmed_attack()) {
+    if (!unarmed_attack()  && can_weapon_block()) {
         float mult = 1.0f;
         if (weapon.has_technique("WBLOCK_1",this)) {
             mult = 0.4;
