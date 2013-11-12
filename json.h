b8d5c3a2c80e426b1273d43f2a0231481566a7ea
Tommy <yobbobanana@gmx.com>
2013-11-13 09:34:11 +1300

    Add empty virtual destructor to Json(De/S)erializer.

--- json.h
+++ json.h
@@ -480,6 +480,7 @@ public:
 
 class JsonSerializer {
 public:
+    virtual ~JsonSerializer() {}
     virtual void serialize(JsonOut &jsout) const = 0;
     std::string serialize() const {
         std::ostringstream s;
@@ -494,6 +495,7 @@ public:
 
 class JsonDeserializer {
 public:
+    virtual ~JsonDeserializer() {}
     virtual void deserialize(JsonIn &jsin) = 0;
     void deserialize(const std::string &json_string) {
         std::istringstream s(json_string);
