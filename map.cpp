38be7964df75d335f06a0382cfd89b15c261f990
freezerbunny <freezerbunny92@gmail.com>
2013-11-13 06:21:16 +0000

    Revert "Change last operator."
    This reverts commit 10ce9583fd7799119b56fea4befe4a5a7c93bcf8.

--- map.cpp
+++ map.cpp
@@ -3482,6 +3482,7 @@ void map::shift(game *g, const int wx, const int wy, const int wz, const int sx,
                     } else if (!loadn(g, wx + sx, wy + sy, wz, gridx, gridy))
                         loadn(g, wx + sx, wy + sy, wz, gridx, gridy);
                 }
+//                debugmsg("Sy>=0");
             } else { // sy < 0; work through it backwards
                 for (int gridy = my_MAPSIZE - 1; gridy >= 0; gridy--) {
                     if (gridx + sx < my_MAPSIZE && gridy + sy > 0) {
@@ -3491,30 +3492,35 @@ void map::shift(game *g, const int wx, const int wy, const int wz, const int sx,
                     } else if (!loadn(g, wx + sx, wy + sy, wz, gridx, gridy))
                         loadn(g, wx + sx, wy + sy, wz, gridx, gridy);
                 }
+//                debugmsg("Sy<0");
             }
+//            debugmsg("Sx>=0");
         }
     } else { // sx < 0; work through it backwards
         for (int gridx = my_MAPSIZE - 1; gridx > 0; gridx--) {
             if (sy >= 0) {
                 for (int gridy = 0; gridy < my_MAPSIZE; gridy++) {
-                    if (gridx + sx > 0 && gridy + sy < my_MAPSIZE) {
+                    if (gridx + sx > 1 && gridy + sy < my_MAPSIZE) {
                         copy_grid(gridx + gridy * my_MAPSIZE,
                                   gridx + sx + (gridy + sy) * my_MAPSIZE);
                         update_vehicle_list(gridx + gridy * my_MAPSIZE);
                     } else if (!loadn(g, wx + sx, wy + sy, wz, gridx, gridy))
                         loadn(g, wx + sx, wy + sy, wz, gridx, gridy);
                 }
+//                debugmsg("Sy>=0");
             } else { // sy < 0; work through it backwards
                 for (int gridy = my_MAPSIZE - 1; gridy > 0; gridy--) {
-                    if (gridx + sx > 0 && gridy + sy > 0) {
+                    if (gridx + sx > 1 && gridy + sy > 0) {
                         copy_grid(gridx + gridy * my_MAPSIZE,
                                   gridx + sx + (gridy + sy) * my_MAPSIZE);
                         update_vehicle_list(gridx + gridy * my_MAPSIZE);
                     } else if (!loadn(g, wx + sx, wy + sy, wz, gridx, gridy))
                         loadn(g, wx + sx, wy + sy, wz, gridx, gridy);
                 }
+//                debugmsg("Sy<0");
             }
         }
+//        debugmsg("Sx<0");
     }
     reset_vehicle_cache();
 }
