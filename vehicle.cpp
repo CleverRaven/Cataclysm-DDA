fabba1b633926de15198aece44c1cab63cf03aa1
Axujen <axujen@autistici.org>
2013-11-14 18:13:02 +0000

    Add vehicle part curtains.
    Curtains can be installed on vehicles using up a sheet, they can only be
    opened or closed from inside the vehicle.

--- vehicle.cpp
+++ vehicle.cpp
@@ -2991,6 +2991,12 @@ void vehicle::close(int part_index)
 
 void vehicle::open_or_close(int part_index, bool opening)
 {
+  if (part_info(part_index).has_flag("OPENCLOSE_INSIDE")){
+      // check if player is inside
+      vehicle *veh = g->m.veh_at(g->u.posx, g->u.posx);
+      if (!veh || veh != this)
+        return;
+  }
   parts[part_index].open = opening ? 1 : 0;
   insides_dirty = true;
 
