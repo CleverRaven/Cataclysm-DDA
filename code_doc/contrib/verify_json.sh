7c1a5c689457f6150165699f9ca60e0ba1e65216
fnord <fnord@fnord.mobi>
2013-11-14 06:38:15 -0600

    code_doc/contrib/verify_json.sh: set TERM to xterm as that requires less output mangling

--- code_doc/contrib/verify_json.sh
+++ code_doc/contrib/verify_json.sh
@@ -2,11 +2,11 @@
 # run ./cataclysm with --jsonverify, spamming spacebar for debugmsg and stripping ncurses control sequences for
 # TERM = screen, xterm, vt100, linux, xterm-color. Exit code > 0 means error
 e=$'\033'
+export TERM=xterm
 out=$(yes ' ' |./cataclysm --jsonverify 2>&1)
 rc=$?
 echo -n "$out" |
   tr -d '\r' |
   sed -r "s/$e(\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K|d|H|J|B|@|r]|\[\??[0-9]+[hl]|[\(\)=<>][B0]?|[78])//g" |
-  sed -r "s/^\s{40}\s+//" | 
   grep -v '^  Press spacebar\.\.\.$'
 exit $rc
