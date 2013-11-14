7ae19eaca949078d50f3e692d2baf6135aadad5c
fnord <fnord@fnord.mobi>
2013-11-14 13:12:51 -0600

    Turn silly pipe chain into silly sed expression chain which ensures no silly !printables

--- code_doc/contrib/verify_json.sh
+++ code_doc/contrib/verify_json.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # run ./cataclysm with --jsonverify, spamming spacebar for debugmsg and stripping ncurses control sequences for
 # TERM = screen, xterm, vt100, linux, xterm-color. Exit code > 0 means error
-e=$'\033'
 export TERM=xterm
+unset LC_CMAP
+unset LANG
+unset TERMCAP
 out=$(yes ' ' |./cataclysm --jsonverify 2>&1)
 rc=$?
 echo -n "$out" |
-  tr -d '\b\r' |
-  sed -r "s/$e(\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K|d|H|J|B|@|r]|\[\??[0-9]+[hl]|[\(\)=<>][B0]?|[78])//g" |
-  grep -v '^  Press spacebar\.\.\.$'
+  sed -r -e "s/.\x08//g" \
+    -e "s/\x1B(\[\??([0-9]{1,4}(;[0-9]{1,2})?)?[a-zA-Z]|[\(\)><=][A-Z]?)//g" \
+    -e "s/[\x00-\x09\x0B-\x1F]//g" \
+    -e "/^  Press spacebar\.\.\.$/ d"
 exit $rc
