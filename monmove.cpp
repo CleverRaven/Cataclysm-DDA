1d7adc4dcafa5f5a94fd55831efe6c9502232d10
freezerbunny <freezerbunny92@gmail.com>
2013-11-15 01:47:54 +0000

    Look for the player in the vehicle better.

--- monmove.cpp
+++ monmove.cpp
@@ -305,7 +305,7 @@ void monster::move(game *g)
         // CONCRETE PLANS - Most likely based on sight
         // Check for a vehicle. If the player isn't in it, avoid the vehicle.
         if (g->m.veh_at(plans[0].x, plans[0].y))  {
-          if (!g->u.in_vehicle)  { // If the player is in the vehicle, we don't care. Smash it.
+          if (!g->m.veh_at(g->u.posx, g->u.posy))  { // If the player is in the vehicle, we don't care. Smash it.
             // Try to go around.
             int newx [8] = {-1, 0, 1, 1, 1, 0, -1, -1};  // x positions from top left.
             int newy [8] = {-1, -1, -1, 0, 1, 1, 1, 0};  // y positions from top left.
