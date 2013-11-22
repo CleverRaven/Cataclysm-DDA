bc8e8d024b801923c5cd5cfc86727d48af3ed00e
freezerbunny <freezerbunny92@gmail.com>
2013-11-22 21:30:38 +0000

    Remove current contact lenses first before adding the new ones.

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -6164,6 +6164,7 @@ int iuse::contacts(player *p, item *it, bool t)
   if(p->has_disease("contacts") && query_yn(_("Replace your current lenses?")) ) {
     p->moves -= 200;
     g->add_msg_if_player(p, _("You replace your current %s."), it->name.c_str());
+    p->rem_disease("contacts");
     p->add_disease("contacts", duration);
     return it->type->charges_to_use();
   }
