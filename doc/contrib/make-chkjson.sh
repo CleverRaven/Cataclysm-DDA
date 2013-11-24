4a3b62d1d12896b019716a95122479c058070f4e
fnord <fnord@fnord.mobi>
2013-11-24 13:26:53 -0600

    doc/contrib/catacharset.h: don't need it sitting there

--- doc/contrib/make-chkjson.sh
+++ doc/contrib/make-chkjson.sh
@@ -3,6 +3,20 @@
     echo -n "wat, no chkjson.cpp in" `pwd`
     exit 1
   fi
+  cat > catacharset.h << END
+/*
+    Stub out additional requirements and butt in before real catacharset is included
+*/
+#ifndef CATACHARSET_H
+#define CATACHARSET_H 1
+#include <stdint.h>
+#include <string>
+std::string utf32_to_utf8(unsigned ch) {
+    return std::string("-stub-");
+}
+#endif
+END
   if g++ -I. chkjson.cpp -I../../src ../../src/json.cpp -o chkjson; then
      ls -ld chkjson && echo run this from $(readlink -f ../..)
   fi
+  rm catacharset.h
