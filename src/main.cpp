356bbb44d9b5af489f78202b9e1716168ae6472d
Anuradha Dissanayake <falconne@gmail.com>
2013-11-28 23:04:33 +1300

    Use input_context (in "legacy mode") to handle input in inventory screen.
    Should hopefully workaround some weird SDL input bugs in the cross compiled Windows build.
    
    This also allows the ncurses version to register for mouse events once and keep them registered, as using the mouse won't return an invalid input in the inventory screen.

--- src/main.cpp
+++ src/main.cpp
@@ -75,9 +75,7 @@ int main(int argc, char *argv[])
     initOptions();
     load_options(); // For getting size options
     initscr(); // Initialize ncurses
-#ifdef SDLTILES
-    init_tiles();
-#endif
+    init_interface();
     noecho();  // Don't echo keypresses
     cbreak();  // C-style breaks (e.g. ^C to SIGINT)
     keypad(stdscr, true); // Numpad is numbers
