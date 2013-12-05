34ee11e2e3cf616b7d35493215ffc5c90ad0761f
fnord <fnord@fnord.mobi>
2013-12-05 12:59:36 -0600

    line/dist functions: constify, reference vector arguments

--- src/line.h
+++ src/line.h
@@ -20,16 +20,16 @@ NORTHWEST
 };
 
 // The "t" value decides WHICH Bresenham line is used.
-std::vector <point> line_to(int x1, int y1, int x2, int y2, int t);
+std::vector <point> line_to(const int x1, const int y1, const int x2, const int y2, int t);
 // sqrt(dX^2 + dY^2)
-int trig_dist(int x1, int y1, int x2, int y2);
+int trig_dist(const int x1, const int y1, const int x2, const int y2);
 // Roguelike distance; minimum of dX and dY
-int square_dist(int x1, int y1, int x2, int y2);
-int rl_dist(int x1, int y1, int x2, int y2);
+int square_dist(const int x1, const int y1, const int x2, const int y2);
+int rl_dist(const int x1, const int y1, const int x2, const int y2);
 int rl_dist(point a, point b);
-double slope_of(std::vector<point> line);
-std::vector<point> continue_line(std::vector<point> line, int distance);
-direction direction_from(int x1, int y1, int x2, int y2);
+double slope_of(const std::vector<point> & line);
+std::vector<point> continue_line(const std::vector<point> & line, const int distance);
+direction direction_from(const int x1, const int y1, const int x2, const int y2);
 std::string direction_name(direction dir);
 std::string direction_name_short(direction dir);
 
