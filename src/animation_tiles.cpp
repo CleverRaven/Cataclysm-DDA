36584fc45a932332fa62c857b5ec419f5a64d3e5
Kevin Granade <kevin.granade@gmail.com>
2013-12-13 18:50:51 -0600

    Quick fix for SDL build breakage.

--- src/animation_tiles.cpp
+++ src/animation_tiles.cpp
@@ -39,7 +39,8 @@ void game::draw_explosion(int x, int y, int radius, nc_color col)
 }
 /* Bullet Animation -- Maybe change this to animate the ammo itself flying through the air?*/
 // need to have a version where there is no player defined, possibly. That way shrapnel works as intended
-void game::draw_bullet(player &p, int tx, int ty, int i, std::vector<point> trajectory, char bullet_char, timespec &ts)
+void game::draw_bullet(Creature &p, int tx, int ty, int i,
+                       std::vector<point> trajectory, char bullet_char, timespec &ts)
 {
     (void)i; //unused
     (void)trajectory; //unused
@@ -57,8 +58,9 @@ void game::draw_bullet(player &p, int tx, int ty, int i, std::vector<point> traj
         // pass to tilecontext
         tilecontext->init_draw_bullet(tx, ty, bullet);
         wrefresh(w_terrain);
-        if (&p == &u)
-         nanosleep(&ts, NULL);
+        if (p.is_player()) {
+            nanosleep(&ts, NULL);
+        }
         tilecontext->void_bullet();
    }
 }
