8aa72349faa493c26605133202efacf05739844e
fnord <fnord@fnord.mobi>
2013-12-16 08:12:10 -0600

    struct id_or_id: specification for custom grass_or_dirt / dirt_or_pile / tomato_or_tomato. set via id_or_id(str primary, int chance, str secondary), or (int,int,int) int .get() returns one_in(chance) ? secondary : primary bool .match(int) compares both

--- src/enums.h
+++ src/enums.h
@@ -1,6 +1,7 @@
 #ifndef _ENUMS_H_
 #define _ENUMS_H_
 
+#include "rng.h"
 #include "json.h" // (de)serialization for points
 
 #ifndef sgn
@@ -63,6 +64,31 @@ struct tripoint {
  tripoint(int X = 0, int Y = 0, int Z = 0) : x (X), y (Y), z (Z) {}
  tripoint(const tripoint &p) : x (p.x), y (p.y), z (p.z) {}
  ~tripoint(){}
+};
+
+/*
+ * Container for custom 'grass_or_dirt' functionality. Returns int but can store str values for delayed lookup and conversion
+ */
+struct id_or_id {
+   std::string primary_str;   // 32
+   std::string secondary_str; // 64
+   int chance;                // 68
+   int primary;               // 72
+   int secondary;             // 76
+   id_or_id(const std::string & s1, const int i, const::std::string s2) : primary_str(s1), secondary_str(s2), chance(i), primary(-1), secondary(-1) { }
+   id_or_id(const int id1, const int i, const int id2) : primary_str(""), secondary_str(""), chance(i), primary(id1), secondary(id2) { }
+   std::string get_str() const {
+       return ( one_in(chance) ? secondary_str : primary_str );
+   };
+   bool match( const int iid ) const {
+       if ( iid == primary || iid == secondary ) {
+           return true;
+       }
+       return false;
+   }
+   int get() const {
+       return ( one_in(chance) ? secondary : primary );
+   }
 };
 
 #endif
