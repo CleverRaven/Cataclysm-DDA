003efee12dcc0d89b20e25470f7dd3e435a4b44c
Williham Totland <williham.totland@gmail.com>
2014-01-03 15:23:31 +0100

    Mass libs

---
+++ src/physquant.h
@@ -0,0 +1,12 @@
+#ifndef _PHYSQUANT_H_
+#define _PHYSQUANT_H_
+
+#include "temperature.h"
+#include "mass.h"
+
+typedef Temperature::magnitude temperature_t;
+typedef Temperature::delta temperature_dt;
+
+typedef Mass::magnitude mass_t;
+
+#endif // _PHYSQUANT_H_
