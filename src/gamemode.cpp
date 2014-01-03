3cc76f6d5671445fad9f07666bb6a462caba7ff0
illi-kun <D.E.Serov@gmail.com>
2014-01-03 19:20:45 +0400

    astyle

--- src/gamemode.cpp
+++ src/gamemode.cpp
@@ -3,33 +3,37 @@
 
 std::string special_game_name(special_game_id id)
 {
- switch (id) {
-  case SGAME_NULL:
-  case NUM_SPECIAL_GAMES:   return "Bug (gamemode.cpp:special_game_name)";
-  case SGAME_TUTORIAL:      return _("Tutorial");
-  case SGAME_DEFENSE:       return _("Defense");
-  default:                  return "Uncoded (gamemode.cpp:special_game_name)";
- }
+    switch (id) {
+    case SGAME_NULL:
+    case NUM_SPECIAL_GAMES:
+        return "Bug (gamemode.cpp:special_game_name)";
+    case SGAME_TUTORIAL:
+        return _("Tutorial");
+    case SGAME_DEFENSE:
+        return _("Defense");
+    default:
+        return "Uncoded (gamemode.cpp:special_game_name)";
+    }
 }
 
-special_game* get_special_game(special_game_id id)
+special_game *get_special_game(special_game_id id)
 {
- special_game* ret;
- switch (id) {
-  case SGAME_NULL:
-   ret = new special_game;
-   break;
-  case SGAME_TUTORIAL:
-   ret = new tutorial_game;
-   break;
-  case SGAME_DEFENSE:
-   ret = new defense_game;
-   break;
-  default:
-   debugmsg("Missing something in gamemode.cpp:get_special_game()?");
-   ret = new special_game;
-   break;
- }
+    special_game *ret;
+    switch (id) {
+    case SGAME_NULL:
+        ret = new special_game;
+        break;
+    case SGAME_TUTORIAL:
+        ret = new tutorial_game;
+        break;
+    case SGAME_DEFENSE:
+        ret = new defense_game;
+        break;
+    default:
+        debugmsg("Missing something in gamemode.cpp:get_special_game()?");
+        ret = new special_game;
+        break;
+    }
 
- return ret;
+    return ret;
 }
