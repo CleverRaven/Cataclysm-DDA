84863d36112d34aef421399219353f943bbbc099
Kevin Granade <kevin.granade@gmail.com>
2014-01-03 23:09:02 -0600

    Use map::accessable_items() to prevent accessing items the player can't reach.

--- src/inventory.cpp
+++ src/inventory.cpp
@@ -546,10 +546,7 @@ void inventory::form_from_map(point origin, int range, bool assign_invlet)
  items.clear();
  for (int x = origin.x - range; x <= origin.x + range; x++) {
   for (int y = origin.y - range; y <= origin.y + range; y++) {
-   int junk;
-   if (g->m.has_flag("SEALED", x, y) ||
-       ((origin.x != x || origin.y != y) &&
-        !g->m.clear_path( origin.x, origin.y, x, y, range, 1, 100, junk ) ) ) {
+   if(g->m.accessable_items(origin.x, origin.y, x, y, range)) {
      continue;
    }
    for (int i = 0; i < g->m.i_at(x, y).size(); i++)
