d59cf8ae5fb70a77cc009724656ffe064971fa6e
Kevin Granade <kevin.granade@gmail.com>
2014-01-24 22:47:43 -0600

    Restyle mapbuffer.cpp and mapbuffer.h

--- src/mapbuffer.h
+++ src/mapbuffer.h
@@ -5,18 +5,29 @@
 
 class game;
 
-struct pointcomp
-{
- bool operator() (const tripoint &lhs, const tripoint &rhs) const
- {
-  if (lhs.x < rhs.x) return true;
-  if (lhs.x > rhs.x) return false;
-  if (lhs.y < rhs.y) return true;
-  if (lhs.y > rhs.y) return false;
-  if (lhs.z < rhs.z) return true;
-  if (lhs.z > rhs.z) return false;
-  return false;
- };
+struct pointcomp {
+    bool operator() (const tripoint &lhs, const tripoint &rhs) const
+    {
+        if (lhs.x < rhs.x) {
+            return true;
+        }
+        if (lhs.x > rhs.x) {
+            return false;
+        }
+        if (lhs.y < rhs.y) {
+            return true;
+        }
+        if (lhs.y > rhs.y) {
+            return false;
+        }
+        if (lhs.z < rhs.z) {
+            return true;
+        }
+        if (lhs.z > rhs.z) {
+            return false;
+        }
+        return false;
+    };
 };
 
 /**
@@ -24,51 +35,51 @@ struct pointcomp
  */
 class mapbuffer
 {
- public:
-  mapbuffer();
-  ~mapbuffer();
+    public:
+        mapbuffer();
+        ~mapbuffer();
 
-  /** Tells the mapbuffer that there is some unsaved change.
-   *
-   *  The next time save_if_dirty is called, the mapbuffer will be saved.
-   */
-  void set_dirty();
+        /** Tells the mapbuffer that there is some unsaved change.
+         *
+         *  The next time save_if_dirty is called, the mapbuffer will be saved.
+         */
+        void set_dirty();
 
-  /** Clears the dirty flag. **/
-  void make_volatile();
+        /** Clears the dirty flag. **/
+        void make_volatile();
 
-  /** Load the entire world from savefiles into submaps in this instance. **/
-  void load(std::string worldname);
-  void unserialize(std::ifstream & fin);
-  bool unserialize_legacy(std::ifstream & fin);
-  /** Store all submaps in this instance into savefiles. **/
-  void save();
+        /** Load the entire world from savefiles into submaps in this instance. **/
+        void load(std::string worldname);
+        void unserialize(std::ifstream &fin);
+        bool unserialize_legacy(std::ifstream &fin);
+        /** Store all submaps in this instance into savefiles. **/
+        void save();
 
-  /** Save only if the dirty flag is set. **/
-  void save_if_dirty();
+        /** Save only if the dirty flag is set. **/
+        void save_if_dirty();
 
-  /** Delete all buffered submaps. **/
-  void reset();
+        /** Delete all buffered submaps. **/
+        void reset();
 
-  /** Add a new submap to the buffer.
-   *
-   * @param x, y, z The absolute world position in submap coordinates.
-   */
-  bool add_submap(int x, int y, int z, submap *sm);
+        /** Add a new submap to the buffer.
+         *
+         * @param x, y, z The absolute world position in submap coordinates.
+         */
+        bool add_submap(int x, int y, int z, submap *sm);
 
-  /** Get a submap stored in this buffer.
-   *
-   * @param x, y, z The absolute world position in submap coordinates.
-   */
-  submap* lookup_submap(int x, int y, int z);
+        /** Get a submap stored in this buffer.
+         *
+         * @param x, y, z The absolute world position in submap coordinates.
+         */
+        submap *lookup_submap(int x, int y, int z);
 
-  /** Returns the amount of buffered submaps. **/
-  int size();
+        /** Returns the amount of buffered submaps. **/
+        int size();
 
- private:
-  std::map<tripoint, submap*, pointcomp> submaps;
-  std::list<submap*> submap_list;
-  bool dirty;
+    private:
+        std::map<tripoint, submap *, pointcomp> submaps;
+        std::list<submap *> submap_list;
+        bool dirty;
 };
 
 extern mapbuffer MAPBUFFER;
