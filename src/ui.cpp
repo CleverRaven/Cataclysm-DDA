eefba20af72de7dcdb462f91ab9779fe0bd4e178
Vlasov Vitaly <vnigtha@gmail.com>
2014-01-25 15:35:40 +0400

    changed left and right border chars

--- src/ui.cpp
+++ src/ui.cpp
@@ -456,15 +456,18 @@ void uimenu::show() {
         setup();
     }
     std::string padspaces = std::string(w_width - 2 - pad_left - pad_right, ' ');
-    for ( int i = 0; i < textformatted.size(); i++ ) {
+    const int text_lines = textformatted.size();
+    for ( int i = 0; i < text_lines; i++ ) {
         mvwprintz(window, 1+i, 2, text_color, "%s", textformatted[i].c_str());
     }
     
+    mvwputch(window, text_lines + 1, 0, border_color, LINE_XXXO);
     for ( int i = 1; i < w_width - 1; ++i) {
-        mvwputch(window, textformatted.size() + 1, i, border_color, LINE_OXOX);
+        mvwputch(window, text_lines + 1, i, border_color, LINE_OXOX);
     }
+    mvwputch(window, text_lines + 1, w_width - 1, border_color, LINE_XOXX);
 
-    int estart = textformatted.size() + 2;
+    int estart = text_lines + 2;
 
     if( OPTIONS["MENU_SCROLL"] ) {
         if (fentries.size() > vmax) {
