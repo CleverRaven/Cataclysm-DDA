979a8f21dc212dab8d7ec65bc613f60dff021dd1
KA101 <cstrayer@rocketmail.com>
2014-01-29 21:24:34 -0500

    OVERSIZE implementation
    Fixes #5783.

--- src/mutation.cpp
+++ src/mutation.cpp
@@ -678,9 +678,11 @@ void mutation_effect(player &p, std::string mut)
         }
     }
 
+    std::string mutation_safe = "OVERSIZE";
     for (int i = 0; i < p.worn.size(); i++) {
         for (int j = 0; j < bps.size(); j++) {
-            if ((dynamic_cast<it_armor*>(p.worn[i].type))->covers & mfb(bps[j])) {
+            if ( ((dynamic_cast<it_armor*>(p.worn[i].type))->covers & mfb(bps[j])) &&
+            (!(p.worn[i].has_flag(mutation_safe))) ) {
                 if (destroy) {
                     if (is_u) {
                         g->add_msg(_("Your %s is destroyed!"), p.worn[i].tname().c_str());
