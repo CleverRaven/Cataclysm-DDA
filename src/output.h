410c45f214663bf0e1b7809e16c74ab7fa671dfc
Thomas Kirsten <tho_ki@gmx.de>
2014-01-31 17:51:47 +0100

    add function display_table and use it to display kill count
    adds string_from_color to reverse color_from_string, to get color-tags to use in fold_and_print_from

--- src/output.h
+++ src/output.h
@@ -53,6 +53,7 @@ std::vector<std::string> foldstring ( std::string str, int width );
 int fold_and_print(WINDOW* w, int begin_y, int begin_x, int width, nc_color color, const char *mes, ...);
 int fold_and_print_from(WINDOW* w, int begin_y, int begin_x, int width, int begin_line, nc_color color, const char *mes, ...);
 void center_print(WINDOW *w, int y, nc_color FG, const char *mes, ...);
+void display_table(WINDOW *w, const std::string &title, int columns, const std::vector<std::string> &data);
 
 void mvputch(int y, int x, nc_color FG, long ch);
 void wputch(WINDOW* w, nc_color FG, long ch);
