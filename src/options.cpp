274fcfb8de6e5306283a31ff506a6c203cef8893
Chase <chasesan@gmail.com>
2014-02-07 01:57:06 -0500

    Initial points update.

--- src/options.cpp
+++ src/options.cpp
@@ -500,12 +500,12 @@ void initOptions() {
 
     OPTIONS["INITIAL_POINTS"] =         cOpt("debug", _("Initial points"),
                                              _("Initial points available on character generation."),
-                                             0, 96, 6
+                                             0, 1000, 6
                                             );
 
     OPTIONS["MAX_TRAIT_POINTS"] =       cOpt("debug", _("Maximum trait points"),
                                              _("Maximum trait points available for character generation."),
-                                             0, 96, 12
+                                             0, 1000, 12
                                             );
 
     OPTIONS["SPAWN_DENSITY"] =          cOpt("world_default", _("Spawn rate scaling factor"),
