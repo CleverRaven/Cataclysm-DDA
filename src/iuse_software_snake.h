00131961e4176456e0436de828e6475dfbf535a7
illi-kun <D.E.Serov@gmail.com>
2014-02-22 22:37:01 +0400

    Make SNAKE translatable

--- src/iuse_software_snake.h
+++ src/iuse_software_snake.h
@@ -1,10 +1,13 @@
 #ifndef _SOFTWARE_SNAKE_H_
 #define _SOFTWARE_SNAKE_H_
 
-class snake_game {
-public:
-    snake_game();
-    void print_score(WINDOW *w_snake, int iScore);
-    int start_game();
+class snake_game
+{
+    public:
+        snake_game();
+        void print_score(WINDOW *w_snake, int iScore);
+        void print_header(WINDOW *w_snake);
+        void snake_over(WINDOW *w_snake, int iScore);
+        int start_game();
 };
 #endif
