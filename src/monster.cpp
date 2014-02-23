116393f092e9c9d0a09655a6f831d93b2f23ca48
Soyweiser <Soyweiser@gmail.com>
2014-02-23 18:29:44 +0100

    removed outdated comment.

--- src/monster.cpp
+++ src/monster.cpp
@@ -1085,7 +1085,7 @@ void monster::die()
    for (int i = 0; i < deathfunctions.size(); i++) {
      func = deathfunctions.at(i);
      (md.*func)(this);
-   }//(md.*type->dies)(this);
+   }
  }
 // If our species fears seeing one of our own die, process that
  int anger_adjust = 0, morale_adjust = 0;
