541d9a302fed09d3601f4a31bd2b15b4deafd6b4
Thomas Kirsten <tho_ki@gmx.de>
2014-03-04 13:04:30 +0100

    remove double formatting, center_print already does formatting.

--- src/iuse_software_snake.cpp
+++ src/iuse_software_snake.cpp
@@ -78,7 +78,7 @@ void snake_game::snake_over(WINDOW *w_snake, int iScore)
         mvwprintz(w_snake, i + 3, 17, c_ltred, game_over_text[i].c_str());
     }
 
-    center_print(w_snake, 17, c_yellow, string_format( _("TOTAL SCORE: %d"), iScore).c_str());
+    center_print(w_snake, 17, c_yellow, _("TOTAL SCORE: %d"), iScore);
     center_print(w_snake, 21, c_white, _("Press 'q' or ESC to exit."));
     wrefresh(w_snake);
     do {
