3dc2ce83956ac3b5dec43c64a10457d84172a5cf
Rivet-the-Zombie <rivet.the.zombie@gmail.com>
2014-03-08 19:50:33 -0600

    Damage mod tweak.

--- src/item.cpp
+++ src/item.cpp
@@ -1354,7 +1354,7 @@ int item::damage_cut() const
     }
     if( is_null() )
         return 0;
-    total -= total * (damage * 0.2) ;
+    total -= total * (damage * 0.15) ;
       if (total > 0) {
       return total;
       } else {
