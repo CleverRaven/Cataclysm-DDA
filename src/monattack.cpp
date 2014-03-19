3ce9526fb3ae2c948eb2d6a216e6d4e88653b073
Lain- <ryou@outlook.com>
2014-03-19 19:46:43 +0700

    tweak

--- src/monattack.cpp
+++ src/monattack.cpp
@@ -95,7 +95,7 @@ void mattack::howl(monster *z)
   return; // Out of range
  z->moves -= 200;   // It takes a while
  z->sp_timeout = z->type->sp_freq; // Reset timer
- g->sound(z->posx(), z->posy(), 30, _("an ear-piercing howl!"));
+ g->sound(z->posx(), z->posy(), 35, _("an ear-piercing howl!"));
 }
 
 void mattack::rattle(monster *z)
