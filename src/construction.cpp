6312b836d095656de50e53684a08a33e2940fb96
Thomas Kirsten <tho_ki@gmx.de>
2014-03-25 13:30:19 +0100

    Move deconstruct items for terrain from cpp to json

--- src/construction.cpp
+++ src/construction.cpp
@@ -722,58 +722,6 @@ void construct::done_deconstruct(point p)
         g->add_msg(_("You have to push away %s first."), g->m.furnname(p.x, p.y).c_str());
     } else {
         g->add_msg(_("You disassemble the %s."), g->m.tername(p.x, p.y).c_str());
-        std::string ter_here = g->m.get_ter(p.x, p.y);
-        if(ter_here == "t_door_c" || ter_here == "t_door_o") {
-            g->m.spawn_item(p.x, p.y, "2x4", 4);
-            g->m.spawn_item(p.x, p.y, "nail", 0, rng(6, 12));
-            g->m.ter_set(p.x, p.y, t_door_frame);
-        } else if(ter_here == "t_rdoor_c" || ter_here == "t_rdoor_o") {
-            g->m.spawn_item(p.x, p.y, "2x4", 24);
-            g->m.spawn_item(p.x, p.y, "nail", 0, rng(36, 48));
-            g->m.ter_set(p.x, p.y, t_door_c);
-        } else if(ter_here == "t_curtains" || ter_here == "t_window_domestic") {
-            g->m.spawn_item(g->u.posx, g->u.posy, "stick");
-            g->m.spawn_item(g->u.posx, g->u.posy, "sheet", 2);
-            g->m.spawn_item(g->u.posx, g->u.posy, "glass_sheet");
-            g->m.spawn_item(g->u.posx, g->u.posy, "nail", 0, rng(3, 4));
-            g->m.spawn_item(g->u.posx, g->u.posy, "string_36", 0, 1);
-            g->m.ter_set(p.x, p.y, t_window_empty);
-        } else if(ter_here == "t_window") {
-            g->m.spawn_item(p.x, p.y, "glass_sheet");
-            g->m.ter_set(p.x, p.y, t_window_empty);
-        } else if(ter_here == "t_backboard") {
-            g->m.spawn_item(p.x, p.y, "2x4", 4);
-            g->m.spawn_item(p.x, p.y, "nail", 0, rng(6, 10));
-            g->m.ter_set(p.x, p.y, t_pavement);
-        } else if(ter_here == "t_sandbox") {
-            g->m.spawn_item(p.x, p.y, "2x4", 4);
-            g->m.spawn_item(p.x, p.y, "nail", 0, rng(6, 10));
-            g->m.ter_set(p.x, p.y, t_floor);
-        } else if(ter_here == "t_slide") {
-            g->m.spawn_item(p.x, p.y, "sheet_metal");
-            g->m.spawn_item(p.x, p.y, "pipe", rng(4, 8));
-            g->m.ter_set(p.x, p.y, t_grass);
-        } else if(ter_here == "t_monkey_bars") {
-            g->m.spawn_item(p.x, p.y, "pipe", rng(6, 12));
-            g->m.ter_set(p.x, p.y, t_grass);
-        } else if(ter_here == "t_radio_controls" || ter_here == "t_console" ||
-                  ter_here == "t_console_broken") {
-            g->m.spawn_item(p.x, p.y, "processor", rng(1, 2));
-            g->m.spawn_item(p.x, p.y, "RAM", rng(4, 8));
-            g->m.spawn_item(p.x, p.y, "cable", rng(4, 6));
-            g->m.spawn_item(p.x, p.y, "small_lcd_screen", rng(1, 2));
-            g->m.spawn_item(p.x, p.y, "e_scrap", rng(10, 16));
-            g->m.spawn_item(p.x, p.y, "circuit", rng(6, 10));
-            g->m.spawn_item(p.x, p.y, "power_supply", rng(2, 4));
-            g->m.spawn_item(p.x, p.y, "amplifier", rng(2, 4));
-            g->m.spawn_item(p.x, p.y, "plastic_chunk", rng(10, 12));
-            g->m.spawn_item(p.x, p.y, "scrap", rng(6, 8));
-            g->m.ter_set(p.x, p.y, t_floor);
-        } else if(ter_here == "t_water_pump") {
-            g->m.spawn_item(p.x, p.y, "well_pump", 1);
-            g->m.spawn_item(p.x, p.y, "pipe", rng(1, 6));
-            g->m.ter_set(p.x, p.y, t_covered_well);
-        }
     }
 }
 
