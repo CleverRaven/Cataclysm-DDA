c66d92aacaded1815e034ead8604b4169d409fa5
C0DEHERO <codehero@nerdpol.ch>
2014-03-24 21:52:57 -0400

    improved dgamelaunch setup script

--- tools/setupdgamelaunch.sh
+++ tools/setupdgamelaunch.sh
@@ -4,7 +4,11 @@ if (pwd | grep "Cataclysm-DDA/tools")
 then
 cd ..
 else
+if (ls Cataclysm-DDA)
+then;
+else
 git clone https://github.com/CleverRaven/Cataclysm-DDA
+fi
 cd Cataclysm-DDA
 fi
 
@@ -12,7 +16,11 @@ make
 
 cd ..
 
+if (ls dgamelaunch)
+then;
+else
 git clone https://github.com/C0DEHERO/dgamelaunch
+fi
 cd dgamelaunch
 
 ./autogen.sh --enable-sqlite --enable-shmem --with-config-file=/opt/dgamelaunch/cdda/etc/dgamelaunch.conf
