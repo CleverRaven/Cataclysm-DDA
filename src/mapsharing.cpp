6c82e275692fbf6ed606c2bf9428f6c20f1dc11a
C0DEHERO <codehero@nerdpol.ch>
2014-03-25 22:00:46 -0400

    add file locks for map.key and map chunk saving. still have to do other saving stuff (npc, missions, artifacts). missions should be per user though

--- src/mapsharing.cpp
+++ src/mapsharing.cpp
@@ -6,3 +6,26 @@ void MAP_SHARING::setUsername(std::string name) {MAP_SHARING::username = name;}
 bool MAP_SHARING::isSharing() {return MAP_SHARING::sharing;}
 std::string MAP_SHARING::getUsername() {return MAP_SHARING::username;}
 
+int MAP_SHARING::getLock( char const *lockName ) {
+    #ifdef __linux__ // make non-linux operating systems happy
+    mode_t m = umask( 0 );
+    int fd = open( lockName, O_RDWR|O_CREAT, 0666 );
+    umask( m );
+    if( fd >= 0 && flock( fd, LOCK_EX | LOCK_NB ) < 0 )
+    {
+        close( fd );
+        fd = -1;
+    }
+    return fd;
+    #endif // __linux__
+    return -1;
+}
+
+void MAP_SHARING::releaseLock( int fd, char const *lockName ) {
+    #ifdef __linux__ // same as above
+    if( fd < 0 )
+        return;
+    remove( lockName );
+    close( fd );
+    #endif // __linux__
+}
--- src/mapsharing.h
+++ src/mapsharing.h
@@ -1,6 +1,15 @@
 #ifndef MAPSHARING_H_INCLUDED
 #define MAPSHARING_H_INCLUDED
 
+#ifdef __linux__
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#endif // __linux__
+
 #include <string>
 
 namespace MAP_SHARING {
@@ -11,6 +20,9 @@ void setSharing(bool mode);
 void setUsername(std::string name);
 bool isSharing();
 std::string getUsername();
+
+int getLock( char const *lockName );
+void releaseLock( int fd, char const *lockName );
 }
 
 #endif // MAPSHARING_H_INCLUDED
