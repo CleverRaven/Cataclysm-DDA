482db421e7db418d30413692a508e4a342795a07
TranquilMarmot <nate272@gmail.com>
2014-04-12 15:09:57 -0700

    Remove extra comma

--- src/tile_id_data.h
+++ src/tile_id_data.h
@@ -47,7 +47,7 @@ const std::string field_names[num_fields] =
     toString(fd_cigsmoke),
     toString(fd_weedsmoke),
     toString(fd_cracksmoke),
-    toString(fd_methsmoke),
+    toString(fd_methsmoke)
 };
 const std::string monster_names[num_monsters] =
 {
