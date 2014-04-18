77671bd704948dfd11a83277414e9e4d17eca526
Kevin Granade <kevin.granade@gmail.com>
2014-04-18 10:05:03 -0500

    Wild stab at fixing builds across GCC and various mingw flavors.

--- src/game.cpp
+++ src/game.cpp
@@ -62,10 +62,8 @@
 #include <tchar.h>
 #endif
 
-#ifndef _MSC_VER
-	#if !defined(__MINGW32__) || defined(__MINGW64_VERSION_MAJOR)
+#if !defined(_MSC_VER) || !defined(__MINGW32__) || defined(__MINGW64_VERSION_MAJOR)
 namespace std { float abs(float a) { return a < 0 ? -a : a; } }
-	#endif
 #endif
 
 #define dbg(x) dout((DebugLevel)(x),D_GAME) << __FILE__ << ":" << __LINE__ << ": "
