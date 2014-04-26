990fd6badb3022009be75c9952dcfc954479cfac
John-Candlebury <albertobenvenuto1@hotmail.es>
2014-04-26 13:39:01 +0200

    Made docile monsters display differently

--- src/monster.cpp
+++ src/monster.cpp
@@ -214,6 +214,10 @@ void monster::get_Attitude(nc_color &color, std::string &text)
             color = h_white;
             text = _("Friendly ");
             break;
+        case MATT_FPASSIVE:
+            color = h_white;
+            text = _("Passive ");
+            break;
         case MATT_FLEE:
             color = c_green;
             text = _("Fleeing! ");
@@ -471,8 +475,10 @@ bool monster::is_fleeing(player &u)
 
 monster_attitude monster::attitude(player *u)
 {
- if (friendly != 0)
+ if (friendly != 0 && !(has_effect("docile")))
   return MATT_FRIEND;
+ if (friendly != 0 )
+  return MATT_FPASSIVE;
  if (has_effect("run"))
   return MATT_FLEE;
 
