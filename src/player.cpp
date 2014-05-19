365aeffda3ed6c85353a5b640fe012016ee5682d
Lucas A. Rosevear <lucasargh@gmail.com>
2014-05-18 19:51:23 -0700

    Testing again.

--- src/player.cpp
+++ src/player.cpp
@@ -8811,7 +8811,7 @@ void player::use(int pos)
                                 used->charges),
                                 used->tname().c_str(),
                                 used->charges, long(charges_used));
-            return 0;
+            return;
         }
     } else if (used->type->use == &iuse::boots          ||
                used->type->use == &iuse::sheath_sword   ||
