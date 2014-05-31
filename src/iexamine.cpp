dd2dffa9f1197b39b5f9f352dddaf62468bbe599
Rick Luddy <rick@redfoe.com>
2014-05-31 15:24:11 -0400

    Changed keg examine to state current charges and percentage filled rather than number of 250-ml volume units.

--- src/iexamine.cpp
+++ src/iexamine.cpp
@@ -1572,14 +1572,9 @@ void iexamine::keg(player *p, map *m, int examx, int examy) {
 
         if(menu_items[choice]==_("Examine")){
             add_msg(m_info, _("That is a %s."), m->name(examx, examy).c_str());
-            int d_vol = drink->volume(false, true)/1000;
-            if (d_vol < 1)
-                add_msg(m_info, ngettext("It has %d portion of %s left.",
-                                    "It has %d portions of %s left.",
-                                    drink->charges),
-                           drink->charges, drink->name.c_str());
-            else
-                add_msg(m_info, _("%s contained: %d/%d"), drink->name.c_str(), d_vol, keg_cap);
+            int full_pct = drink->volume(false, true) / (keg_cap*10);
+            add_msg(m_info, _("It contains %s (%d), %d%% full."),
+                    drink->name.c_str(), drink->charges, full_pct);
             return;
         }
     }
