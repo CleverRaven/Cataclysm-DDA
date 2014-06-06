18d6282987757c9f790495f91fc860e6fbbb6212
Kevin Granade <kevin.granade@gmail.com>
2014-06-05 23:13:34 -0500

    Only color-code books if the player has perused them.

--- src/item.cpp
+++ src/item.cpp
@@ -1067,7 +1067,7 @@ nc_color item::color(player *u) const
                 ret = c_green;
             }
         }
-    } else if (is_book()) {
+    } else if (is_book() && u.has_identified( type->id ) ) {
         it_book* tmp = dynamic_cast<it_book*>(type);
         if (tmp->type && tmp->intel <= u->int_cur + u->skillLevel(tmp->type) &&
                 (tmp->intel == 0 || !u->has_trait("ILLITERATE")) &&
