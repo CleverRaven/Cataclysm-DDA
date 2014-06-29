0a79e9c3882a89a5dde87c0ab2a295c650da59e9
Fira <loyauflorian@gmail.com>
2014-06-29 19:56:12 +0200

    Fix disassembly bogus check, resulting in bogus results, memory sweeping and segfaults

--- src/crafting.cpp
+++ src/crafting.cpp
@@ -1840,8 +1840,13 @@ bool game::can_disassemble(item *dis_item, recipe *cur_recipe, inventory &crafti
              cur_recipe->tools.begin();
          it != cur_recipe->tools.end(); ++it) {
         bool have_this_tool = false;
-        for (std::vector<component>::iterator tool = it->begin();
-             tool != it->end(); ++it) {
+        
+        std::vector<component> tools = *it;
+	std::vector<component>::iterator tool;
+
+        for (tool = tools.begin();
+             tool != tools.end(); ++tool) {
+
             itype_id type = tool->type;
             int req = tool->count; // -1 => 1
 
@@ -1854,7 +1859,8 @@ bool game::can_disassemble(item *dis_item, recipe *cur_recipe, inventory &crafti
                 (req <= 0 && type == "mold_plastic") ||
                 (req >  0 && crafting_inv.has_charges(type, req))) {
                 have_this_tool = true;
-                tool = it->end();
+                
+		break;
             }
             // If crafting recipe required a welder,
             // disassembly requires a hacksaw or super toolkit.
