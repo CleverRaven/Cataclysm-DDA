9586f516785bafe93c44a1f3ac7854d3fcb77a1a
huxtus <huxtuz@gmail.com>
2014-07-06 19:52:43 +0600

    cargo to boat

--- src/iexamine.h
+++ src/iexamine.h
@@ -75,7 +75,7 @@ public:
  void keg (player *p, map *m, const int examx, const int examy);
  void reload_furniture (player *p, map *m, const int examx, const int examy);
  void curtains (player *p, map *m, const int examx, const int examy);
- void sign (player *p, map *m, const int examx, const int examy); 
+ void sign (player *p, map *m, const int examx, const int examy);
 };
 
 typedef void (iexamine::*iexamine_function)(player*, map*, int, int);
