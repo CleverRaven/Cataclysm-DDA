93cb2676db1a3ee7c28034b48cfb46a5d09af39f
huxtus <huxtuz@gmail.com>
2014-07-08 21:14:29 +0600

    fix: json variables "unfold_msg" and "moves" hardcoded and does not applicable to actions.

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -7719,10 +7719,25 @@ int iuse::unfold_generic(player *p, item *it, bool)
         return 0;
     }
     g->m.update_vehicle_cache(veh, true);
-    p->add_msg_if_player(_("You painstakingly unfold the %s and make it ready to ride."),
-                         veh->name.c_str());
-    p->moves -= 500;
-    return 1;
+
+	std::string unfold_msg = it->item_vars["unfold_msg"];
+	if (unfold_msg.size() == 0)
+	{
+	    unfold_msg = _("You painstakingly unfold the %s and make it ready to ride.");
+	} else
+	{
+	    unfold_msg = _(unfold_msg.c_str());
+	}
+	p->add_msg_if_player(unfold_msg.c_str(), veh->name.c_str());
+
+	std::string smoves = it->item_vars["moves"];
+	int moves = 500;
+	if (smoves.size() > 0)
+	{
+	    moves = std::stoi(smoves);
+	}
+	p->moves -= moves;
+	return 1;
 }
 
 int iuse::adrenaline_injector(player *p, item *it, bool)
