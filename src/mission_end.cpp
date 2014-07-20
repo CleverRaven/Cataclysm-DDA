cbeea054b9ebbeec83cdf7d8bc2c7cb7f0b80312
acidia <stm.vol@gmail.com>
2014-07-20 16:59:27 -0700

    Added mission_end, thankful

--- src/mission_end.cpp
+++ src/mission_end.cpp
@@ -23,6 +23,21 @@ void mission_end::leave(mission *miss)
     p->attitude = NPCATT_NULL;
 }
 
+void mission_end::thankful(mission *miss)
+{
+    npc *p = g->find_npc(miss->npc_id);
+    if (p == NULL) {
+        debugmsg("could not find mission NPC %d", miss->npc_id);
+        return;
+    }
+    if ( p->attitude == NPCATT_MUG || p->attitude == NPCATT_WAIT_FOR_LEAVE ||
+        p->attitude == NPCATT_FLEE || p->attitude == NPCATT_KILL )
+        p->attitude = NPCATT_NULL;
+    if (p->chatbin.first_topic != TALK_FRIEND)
+        p->chatbin.first_topic = TALK_STRANGER_FRIENDLY;
+    p->personality.aggression -= 1;
+}
+
 void mission_end::deposit_box(mission *miss)
 {
     npc *p = g->find_npc(miss->npc_id);
