16026c9d3b2c41b0d9645015b5926816ff1eefe9
acidia <stm.vol@gmail.com>
2014-07-23 20:14:45 -0700

    Adding support for faction jsons

--- src/init.cpp
+++ src/init.cpp
@@ -34,6 +34,7 @@
 #include "omdata.h"
 #include "options.h"
 #include "game.h"
+#include "faction.h"
 
 #include <string>
 #include <vector>
@@ -194,6 +195,9 @@ void DynamicDataLoader::initialize()
     // mod information, ignored, handled by the mod manager
     type_function_map["MOD_INFO"] = new StaticFunctionAccessor(&load_ingored_type);
     type_function_map["BULLET_PULLING"] = new StaticFunctionAccessor(&iuse::load_bullet_pulling);
+    
+    type_function_map["faction"] = new StaticFunctionAccessor(
+        &faction::load_faction);
 }
 
 void DynamicDataLoader::reset()
