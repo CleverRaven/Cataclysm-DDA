8988510a25b4b50477c804a1024ef958b37f2339
wormingdead <wormingdead@gmail.com>
2014-07-26 16:22:00 -0700

    Reformating and whitespace.

--- utilities/building-utility/deconstruct.py
+++ utilities/building-utility/deconstruct.py
@@ -1,25 +1,26 @@
 # This program runs well with PYTHON 2.5 but may not
 # run without an error with PYTHON 2.2
 #
-# I (acidia) just made this as a quick utility to change a character map in drawing.txt
-#into a json that the game can process.  You'll have to modify the paths in this
-#folder unless you decide to put all the files in 'c:\building-utility' and create
-#a folder called 'output' for it to drop the finished result in.  This is in no
-#way polished, it just saved me a huge amount of time creating 9x9 buildings so
-#I figured someone else might be able to use it.
-#
+# I (acidia) just made this as a quick utility to change a character map in
+# drawing.txt into a json that the game can process.  You'll have to modify the
+# paths in this folder unless you decide to put all the files in
+# 'c:\building-utility' and create a folder called 'output' for it to drop the
+# finished result in.  This is in no way polished, it just saved me a huge
+# amount of time creating 9x9 buildings so I figured someone else might be able
+# to use it.
+
 import os
 
 base_path = 'C:\\building-utility\\'
 
 def main():
     global base_path
-    
+
     #This should auto-detect the correct directory.
     full_path = os.path.realpath(__file__)
     path, file = os.path.split(full_path)
     base_path = path + "\\"
-    
+
     mapCount = 1
     file_path = base_path + 'drawing.txt'
     file = open(file_path,"r")
@@ -29,9 +30,11 @@ def main():
         lineCT = 0
         while len(line) > 23:
             if len(mapList[lineCT]) >= 725:
-                mapList[lineCT] = mapList[lineCT] + '				"' + line[:24] + '"\n'
+                mapList[lineCT] = (mapList[lineCT]
+                                  + '\t\t\t\t"' + line[:24] + '"\n')
             else:
-                mapList[lineCT] = mapList[lineCT] + '				"' + line[:24] + '",\n'
+                mapList[lineCT] = (mapList[lineCT]
+                                  + '\t\t\t\t"' + line[:24] + '",\n')
             line = line[24:]
             lineCT += 1
         if len(mapList[0]) >= 760:
