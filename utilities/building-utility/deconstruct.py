988a7e049d5bb9b13149f9487c0257df71c6a812
wormingdead <wormingdead@gmail.com>
2014-08-02 13:17:17 -0700

    [Building Utility] Remove old functionality. Add parser (with help section) and use new functionality.

--- utilities/building-utility/deconstruct.py
+++ utilities/building-utility/deconstruct.py
@@ -15,92 +15,6 @@ _TEMPLATE_FUNC_OBJ_REPLACE = "object_replace"
 _TEMPLATE_FUNC_STR_FORMAT = "string_format"
 
 
-def main():
-    # initally clear overmap_terrain_entry.json
-    # this is a hack, file operations in general need to be refactored a lot
-    with open(os.path.join(os.getcwd(), 'output',
-                           'overmap_terrain_entry.json'),
-              "w") as outfile:
-        pass
-
-    with open(os.path.join(os.getcwd(), 'drawing.txt'),"r") as infile:
-        data = '[\n'
-        mapCount = 1
-        mapList = [""] * 9
-
-        for line in infile:
-            lineCT = 0
-
-            while len(line) >= _MAP_CELL_SIZE:
-                mapList[lineCT] += '\t\t\t\t"' + line[:_MAP_CELL_SIZE] + '"'
-
-                # magic # of chars to know when to stop adding commas
-                if len(mapList[lineCT]) < 735:
-                    mapList[lineCT] += ','
-                mapList[lineCT] += '\n'
-
-                line = line[_MAP_CELL_SIZE:]
-                lineCT += 1
-
-            # magic # of chars that shows no errors occured
-            if len(mapList[0]) >= 760:
-                for line in mapList:
-                    if len(line) > 20:
-                        data = writeJSON(mapCount, line, data)
-                        writeTerrain(mapCount)
-                        mapCount += 1
-                mapList = [""] * 9
-
-    # remove last comma
-    data = data[:-1] + "\n]\n"
-    finalizeEntry(data)
-
-#Takes the mapNum and 'text' is the 24x24 map
-def writeJSON(mapNum, text, data):
-    entry = ''
-    with open(os.path.join(os.getcwd(), 'json_header.txt'),
-              "r") as json_header_file:
-        entry += json_header_file.read().rstrip()
-
-    entry += str(mapNum)
-
-    with open(os.path.join(os.getcwd(), 'json_middle.txt'),
-              "r") as json_middle_file:
-        entry += json_middle_file.read()
-
-    entry += text
-
-    with open(os.path.join(os.getcwd(), 'json_footer.txt'),
-              "r") as json_footer_file:
-        entry += json_footer_file.read().rstrip()
-
-    return data + entry
-
-def finalizeEntry(data):
-    with open(os.path.join(os.getcwd(), 'output', 'office_tower.json'),
-              "w") as outfile:
-        outfile.write(data)
-    with open(os.path.join(os.getcwd(), 'output',
-                           'overmap_terrain_entry.json'),
-              "a") as out_terrain_file:
-        out_terrain_file.write("\n")
-
-def writeTerrain(mapNum):
-    entry = ''
-    with open(os.path.join(os.getcwd(), 'terrain_header.txt'),
-              "r") as terrain_header_file:
-        entry += terrain_header_file.read().rstrip()
-
-    entry += str(mapNum)
-
-    with open(os.path.join(os.getcwd(), 'terrain_footer.txt'),
-              "r") as terrain_footer_file:
-        entry += terrain_footer_file.read().rstrip()
-
-    with open(os.path.join(os.getcwd(), 'output',
-                           'overmap_terrain_entry.json'),
-              "a") as outfile:
-        outfile.write(entry)
 def division_split(div, single_list):
     "Divides a list into a list of lists each containing div amount of "
     "elements.  The last list will contain less than the div amount if the "
@@ -234,5 +148,51 @@ def complete_json_file(template_file, all_cells, remove_template=True):
 
 
 if __name__ == "__main__":
-    main()
+    # TODO: epilog needs actual formatting. see argparse formatterclass
+    parser = argparse.ArgumentParser(description=
+        "A script for combining multi-cell maps with their json data.",
+        epilog="To format the json template, add the key '{section}' to the "
+        "root dictionary.  In that section, create a dictionary with one or "
+        "more template type keys.  Each type key should have a dictionary of "
+        "function keys.  Each function key has its own value format, however "
+        "they will all have a destination or list of destinations of where "
+        "in the json template to perform the function.\n\nList of types:\t"
+        "'{cell_map}':\tThe list of ascii lines representing the map cell.\n\t"
+        "'{cell_num}':\tThe number of the current cell.\n\nList of functions:"
+        "\t'{obj_repl}':\tValue is a keypath or list of keypaths where the "
+        "value at the end of the keypath will be replaced with the chosen "
+        "type.\n\t'{str_form}'\tValue is a dictionary with printf formatted "
+        "strings as keys which have keypath or list of keypaths as values.  "
+        "The value at the end of the keypath(s) will be replaced with the "
+        "string formatted with the type as input.".format(
+            section=_TEMPLATE_JSON_SECTION, cell_map=_TEMPLATE_TYPE_CELL_MAP,
+            cell_num=_TEMPLATE_TYPE_CELL_NUM,
+            obj_repl=_TEMPLATE_FUNC_OBJ_REPLACE,
+            str_form=_TEMPLATE_FUNC_STR_FORMAT))
+
+    parser.add_argument("map_file", metavar="map-file",
+                        type=argparse.FileType("r"),
+                        help="A file that contains an ascii representation "
+                        "of one or more map cells.")
+
+    parser.add_argument("json_templates", metavar="json-file",
+                        nargs="+",
+                        type=argparse.FileType("r"),
+                        help="A json template that will be combined with the "
+                        "map cells to a properly formatted json map file.  "
+                        "See below for templating format.")
+
+    args = parser.parse_args()
+
+    with args.map_file:
+        all_cells = get_map_cells(args.map_file, _MAP_CELL_SIZE)
+
+    for json_template in args.json_templates:
+        with json_template:
+            try:
+                complete_json_file(json_template, all_cells)
+            except ValueError as e:
+                parser.exit(1, "Could not parse json in file {file_name}\n"
+                "{json_err}\n".format(file_name=json_template.name,
+                                      json_err=str(e)))
 
