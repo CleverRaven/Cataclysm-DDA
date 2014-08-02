7275dedb4581e4f0e69c6cdb31e43a5a9b2f2678
wormingdead <wormingdead@gmail.com>
2014-08-02 11:56:29 -0700

    [Building Utility] Add global constants for template format.

--- utilities/building-utility/deconstruct.py
+++ utilities/building-utility/deconstruct.py
@@ -12,6 +12,11 @@
 import os
 
 _MAP_CELL_SIZE = 24
+_TEMPLATE_JSON_SECTION = "_Building_Utility"
+_TEMPLATE_TYPE_CELL_MAP = "CELL_MAP"
+_TEMPLATE_TYPE_CELL_NUM = "CELL_NUM"
+_TEMPLATE_FUNC_OBJ_REPLACE = "object_replace"
+_TEMPLATE_FUNC_STR_FORMAT = "string_format"
 
 
 def main():
