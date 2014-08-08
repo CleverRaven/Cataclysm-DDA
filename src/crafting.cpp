f9064fe64c131ebe589fc646749e7d0654e59b8c
Shoes01 <jason.gilbert@nbed.nb.ca>
2014-08-08 10:00:00 -0300

    Recipe skill search sorting improved, can also search for skill used only

--- src/crafting.cpp
+++ src/crafting.cpp
@@ -678,7 +678,7 @@ recipe *game::select_crafting_recipe()
             keepline = true;
         } else if (action == "FILTER") {
             filterstring = string_input_popup(_("Search:"), 85, filterstring,
-                                              _("Search tools or component or skills using prefix t, c and s. \n(i.e. \"t:hammer\" or \"c:two by four\" or \"s:cooking\".)"));
+                                              _("Search tools or component using prefix t. \nSearch skills using prefix s, or S for skill used only. \n (i.e. \"t:hammer\" or \"c:two by four\" or \"s:cooking\".)"));
             redraw = true;
         } else if (action == "QUIT") {
             done = true;
@@ -954,6 +954,7 @@ void game::pick_recipes(const inventory &crafting_inv, std::vector<recipe *> &cu
     bool search_tool = false;
     bool search_component = false;
     bool search_skill = false;
+    bool search_skill_primary_only = false;
     size_t pos = filter.find(":");
     if(pos != std::string::npos) {
         search_name = false;
@@ -967,6 +968,8 @@ void game::pick_recipes(const inventory &crafting_inv, std::vector<recipe *> &cu
                 search_component = true;
             } else if(*it == 's') {
                 search_skill = true;
+            } else if(*it == 'S') {
+                search_skill_primary_only = true;
             }
         }
         filter = filter.substr(pos + 1);
@@ -987,7 +990,7 @@ void game::pick_recipes(const inventory &crafting_inv, std::vector<recipe *> &cu
 
     current.clear();
     available.clear();
-    std::map<int, recipe*> ordered_list;
+    std::vector<recipe *> filtered_list;
     int max_difficulty = 0;
 
     for (recipe_list::iterator iter = available_recipes.begin();
@@ -1023,12 +1026,23 @@ void game::pick_recipes(const inventory &crafting_inv, std::vector<recipe *> &cu
                     if( !rec->skill_used) {
                         continue;
                     }
-                    else if( !lcmatch(rec->skill_used->name() ,      filter) &&
-                             !lcmatch(rec->required_skills_string(), filter)) {
+                    else if( !lcmatch( rec->skill_used->name(), filter ) &&
+                             !lcmatch( rec->required_skills_string(), filter )) {
+                        continue;
+                    }
+                }
+                if(search_skill_primary_only) {
+                    if( !rec->skill_used ) {
+                        continue;
+                    }
+                    else if( !lcmatch( rec->skill_used->name(), filter )) {
                         continue;
                     }
                 }
             }
+
+            filtered_list.push_back(rec);
+
             if (rec->can_make_with_inventory(crafting_inv)) {
                 current.insert(current.begin(), rec);
                 available.insert(available.begin(), true);
@@ -1040,28 +1054,32 @@ void game::pick_recipes(const inventory &crafting_inv, std::vector<recipe *> &cu
         max_difficulty = std::max(max_difficulty, rec->difficulty);
     }
 
-    std::vector<recipe *> ordered_current;
-    std::vector<bool> ordered_available;
+    if (!search_skill && !search_skill_primary_only) return;
 
+    current.clear();
+    available.clear();
+
+    int truecount = 0;
     for (int i = max_difficulty; i != -1; --i)
     {
-        for (std::vector<recipe*>::iterator iter = current.begin(); iter != current.end(); ++iter)
+        for (std::vector<recipe*>::iterator iter = filtered_list.begin(); iter != filtered_list.end(); ++iter)
         {
-            if ((*iter)->difficulty == i)
+            recipe *rec = *iter;
+            if (rec->difficulty == i)
             {
-                ordered_current.push_back((*iter));
-                if ((*iter)->can_make_with_inventory(crafting_inv)) {
-                    ordered_available.push_back(true);
+                if (rec->can_make_with_inventory(crafting_inv)) {
+                    current.insert(current.begin(), rec);
+                    available.insert(available.begin(), true);
+                    truecount++;
                 } else {
-                    ordered_available.push_back(false);
+                    current.push_back(rec);
+                    available.push_back(false);
                 }
             }
         }
     }
-
-    current = ordered_current;
-    available = ordered_available;
-
+    // This is so the list of available recipes is also is order of difficulty.
+    std::reverse(current.begin(), current.begin() + truecount);
 }
 
 void game::make_craft(recipe *making)
