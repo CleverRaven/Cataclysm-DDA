4000529b9db5e3f171b7dc2b68f733d7ccae352a
Wuzzy <almikes@aol.com>
2014-08-11 06:34:54 +0200

    Reserve world names “TUTORIAL” and “DEFENSE”

--- src/worldfactory.cpp
+++ src/worldfactory.cpp
@@ -1262,7 +1262,7 @@ bool worldfactory::valid_worldname(std::string name, bool automated)
 {
     std::string msg;
 
-    if (name == "save") {
+    if (name == "save" || name == "TUTORIAL" || name == "DEFENSE") {
         msg = string_format(_("%s is not a valid world name, it is a reserved name"), name.c_str());
     } else if (std::find(all_worldnames.begin(), all_worldnames.end(), name) == all_worldnames.end()) {
         return true;
