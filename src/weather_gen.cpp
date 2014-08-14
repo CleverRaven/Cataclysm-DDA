85e037ef50aaba5f41b2755c31fd5f4758593ee4
KA101 <cstrayer@rocketmail.com>
2014-08-14 02:32:38 -0400

    Removes the bugcheck line.

--- src/weather_gen.cpp
+++ src/weather_gen.cpp
@@ -10,7 +10,7 @@
 const double tau = 2 * std::acos(-1);
 
 weather_generator::weather_generator() { }
-weather_generator::weather_generator(unsigned seed) : SEED(seed) { this->test_weather(); }
+weather_generator::weather_generator(unsigned seed) : SEED(seed) { }
 
 w_point weather_generator::get_weather(const point &location, const calendar &t)
 {
