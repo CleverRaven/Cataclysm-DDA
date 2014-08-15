f2a1b2165f7dbe2aca3177938af50cb737e2a88b
Waladil <ian.barnhart@stonybrook.edu>
2014-08-15 00:47:25 -0600

    Formatting & error-fixes for book colors
    Coding /is/ a perishable skill, after all.

--- src/item.cpp
+++ src/item.cpp
@@ -1124,20 +1124,23 @@ nc_color item::color(player *u) const
                 ret = c_green;
             }
         }
-    } else if (is_book() && u->has_identified( type->id ) ) {
-        it_book* tmp = dynamic_cast<it_book*>(type);
-        if (tmp->type && tmp->intel <= u->int_cur + u->skillLevel(tmp->type) &&
-                tmp->intel == 0) &&
-                (u->skillLevel(tmp->type) >= (int)tmp->req) &&
-                (u->skillLevel(tmp->type) < (int)tmp->level)) {
-            ret = c_ltblue;
-        } else if (!u->studied_all_recipes(tmp)) {
-          ret = c_yellow;
-        }
-    }
-if ((is_book() && !u->has_identified( type->id )) || (is_book() && u->has_trait("ILLITERATE"))) {
-	ret = c_red
+    } else if (is_book()) {
+    	if(u->has_identified( type->id )) {
+	    it_book* tmp = dynamic_cast<it_book*>(type);
+	    if (tmp->type && tmp->intel <= u->int_cur + u->skillLevel(tmp->type) &&
+		 tmp->intel == 0) &&
+		 (u->skillLevel(tmp->type) >= (int)tmp->req) &&
+		 (u->skillLevel(tmp->type) < (int)tmp->level) {
+	        ret = c_ltblue;
+	    } else if (!u->studied_all_recipes(tmp)) {
+	        ret = c_yellow;
+	    }
+	} else {
+		ret = c_red;
 	}
+    }
+    return ret;
+}
 
 nc_color item::color_in_inventory() const
 {
