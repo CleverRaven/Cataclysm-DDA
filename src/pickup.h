3d2b0a4a8136e97a7a1a773fe7299e2316ca7176
Kevin Granade <kevin.granade@gmail.com>
2014-09-01 21:24:12 -0500

    Remove unecessary from_veh member of Pickup class.

--- src/pickup.h
+++ src/pickup.h
@@ -7,26 +7,21 @@
 class Pickup
 {
     public:
-        Pickup()
-        {
-            from_veh = false;
-        }
+        Pickup() {}
 
         static void do_pickup( point pickup_target, bool from_vehicle,
                                std::list<int> &indices, std::list<int> &quantities );
         static void pick_up(int posx, int posy, int min); // Pick up items; ',' or via examine()
 
     private:
-        bool from_veh;
-
         // Pickup helper functions
         static int interact_with_vehicle( vehicle *veh, int posx, int posy, int veh_root_part );
 
-        static int handle_quiver_insertion(item &here, bool inv_on_fail, int &moves_to_decrement,
-                                           bool &picked_up);
-        void remove_from_map_or_vehicle(int posx, int posy, vehicle *veh, int cargo_part,
-                                        int &moves_taken, int curmit);
-        static void show_pickup_message(std::map<std::string, int> &mapPickup);
+        static int handle_quiver_insertion( item &here, bool inv_on_fail, int &moves_to_decrement,
+                                            bool &picked_up );
+        static void remove_from_map_or_vehicle( int posx, int posy, vehicle *veh, int cargo_part,
+                                                int &moves_taken, int curmit );
+        static void show_pickup_message( std::map<std::string, int> &mapPickup );
 };
 
 
