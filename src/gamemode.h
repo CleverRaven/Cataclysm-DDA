edf04b594811a141100694b19ae73f3177510b9d
Thomas Kirsten <tho_ki@gmx.de>
2014-09-12 21:29:11 +0200

    Remove storing and restoring old data values in defense games.
    That data is reloaded whenever the user starts a new game anyway (as it's part of the world data, probably altered by mods).
    
    Also remove `(void)g` - old artifacts of a simpler time when g was parameter to those functions.

--- src/gamemode.h
+++ src/gamemode.h
@@ -127,11 +127,6 @@ enum caravan_category {
     NUM_CARAVAN_CATEGORIES
 };
 
-struct defense_game_monchanges {
-    int original_difficulty;
-    std::set<m_flag> added_flags;
-};
-
 struct defense_game : public special_game {
         defense_game();
 
@@ -152,13 +147,9 @@ struct defense_game : public special_game {
         void setup();
         void refresh_setup(WINDOW *w, int selection);
         void init_itypes();
-        void reset_itypes();
         void init_mtypes();
-        void reset_mtypes();
         void init_constructions();
-        void reset_constructions();
         void init_recipes();
-        void reset_recipes();
         void init_map();
         std::vector<itype_id> carvan_items(caravan_category cat);
 
