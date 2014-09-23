3003f54167fb9b09ae4a4531b789ed38b347efa9
Aenye <3e5d45c2@opayq.com>
2014-09-23 20:39:36 +0200

    Increases effect of mass on vehicle speed.
    Was doing testing for a road roller (uses ten roller drums for wheels) - experimented on a roller I've found (51' rollers, so wheel bigness was a constant, done on v12 engine), I was changing width (w) and mass (m) of the roller drum, while jotting down the safe/maximum speed (sp) that was attainable:
    w=200, m=750 kg -> sp 33/1239
    w=200, m=1500 kg -> sp 33/1239
    w=200, m=15000 kg -> sp 33/1239
    
    w=60, m=750 kg -> sp 95/1239
    w=60, m=1500 kg -> sp 95/1239
    w=60, m=15000 kg -> sp 94/1239
    w=60, m=1 g -> sp 95/1239
    
    w=20, m=750 kg -> sp 197/1239
    w=20, m=1500 kg -> sp 197/1239
    w=20, m=15000 kg -> sp 189/1239
    
    While a v12 engine is powerful, being able to blast a roller weighing 150 000 000 grams (10*15 000kg in the most extreme case) at 189 km/h safely is a bit too much. That and the fact, that increasing total vehicle mass by a factor of ten lowered the safe speed by 8 hm/h (4%).

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -2457,7 +2457,7 @@ float vehicle::k_mass ()
     float ma0 = 50.0;
 
     // calculate safe speed reduction due to mass
-    float km = ma0 / (ma0 + (total_mass() / 8) / (8 * (float) wa));
+    float km = ma0 / (ma0 + (total_mass()) / (8 * (float) wa));
 
     return km;
 }
