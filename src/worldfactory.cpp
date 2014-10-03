82f936043575210d4569bcedc3245a7e78790515
mafagafogigante <mafagafogigante@gmail.com>
2014-10-03 17:14:50 -0300

    Removed a never used variable.

--- src/worldfactory.cpp
+++ src/worldfactory.cpp
@@ -308,7 +308,6 @@ std::map<std::string, WORLDPTR> worldfactory::get_all_worlds()
             bool no_options = true;
             std::vector<std::string> detected_world_op = file_finder::get_files_from_path(WORLD_OPTION_FILE,
                     world_dirs[i], false);
-            std::string world_op_file = WORLD_OPTION_FILE;
             if ( ! detected_world_op.empty() ) {
                 no_options = false;
             }
