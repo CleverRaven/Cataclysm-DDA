5e93b331e2cd4aa8db268243948eac3f2a3c1c47
SeanMirrsen <controllersean@mail.ru>
2014-10-06 10:43:48 +0400

    Define reinitialize_framebuffer() for pure curses
    Er, whops. How'd that slip past me. >_>

--- src/game.cpp
+++ src/game.cpp
@@ -255,13 +255,16 @@ game::~game()
 void to_map_font_dimension(int &w, int &h);
 void from_map_font_dimension(int &w, int &h);
 void to_overmap_font_dimension(int &w, int &h);
+void reinitialize_framebuffer();
 #else
 // unchanged, nothing to be translated without tiles
 void to_map_font_dimension(int &, int &) { }
 void from_map_font_dimension(int &, int &) { }
 void to_overmap_font_dimension(int &, int &) { }
+//in pure curses, the framebuffer won't need reinitializing
+void reinitialize_framebuffer() { }
 #endif
-void reinitialize_framebuffer();
+
 
 void game::init_ui()
 {
