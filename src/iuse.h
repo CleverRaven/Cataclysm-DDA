1db892c5adc8f0da87ab24df37db86d6669f8981
jcd000 <ikontochris@gmail.com>
2014-10-17 12:53:27 +0300

    Revert "Revert "Merge branch 'fire-from-mag-glass' of https://github.com/jcd000/Cataclysm-DDA into merge""
    This reverts commit 5c46c3cd3bb607d3e57b8ce501c98d16789cc8fe.
    
    revert the revert!

--- src/iuse.h
+++ src/iuse.h
@@ -73,9 +73,9 @@ public:
     int catfood             (player*, item*, bool);
 
 // TOOLS
-    int lighter             (player *, item *, bool);
-    int primitive_fire      (player *, item *, bool);
-    int ref_lit             (player *, item *, bool);
+    int firestarter         (player *, item *, bool);
+    int resolve_firestarter_use(player *p, item *, int posx, int posy);
+    int calculate_time_for_lens_fire (player *p, float light_level);
     int sew                 (player *, item *, bool);
     int extra_battery       (player *, item *, bool);
     int rechargeable_battery(player *, item *, bool);
--- src/player_activity.cpp
+++ src/player_activity.cpp
@@ -25,7 +25,8 @@ const std::string &player_activity::get_stop_phrase() const
         _(" Stop cutting tissues?"), _(" Stop dropping?"),
         _(" Stop stashing?"), _(" Stop picking up?"),
         _(" Stop moving items?"),
-        _(" Stop interacting with inventory?")
+        _(" Stop interacting with inventory?"),
+        _(" Stop lighting the fire?")
     };
     return stop_phrase[type];
 }
@@ -49,6 +50,7 @@ bool player_activity::is_abortable() const
     case ACT_PICKUP:
     case ACT_MOVE_ITEMS:
     case ACT_ADV_INVENTORY:
+    case ACT_START_FIRE:
         return true;
     default:
         return false;
