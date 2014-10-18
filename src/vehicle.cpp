b0f194888a4fb135a21f061db9dcb9c483731eea
desrik <koderkore@gmail.com>
2014-10-18 06:45:04 -0400

    Add diesel to can_mount function of vehicle.cpp

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -1033,7 +1033,7 @@ bool vehicle::can_mount (int dx, int dy, std::string id)
         for(std::vector<int>::const_iterator it = parts_in_square.begin();
             it != parts_in_square.end(); ++it ) {
             if(part_info(*it).has_flag(VPFLAG_ENGINE) &&
-               part_info(*it).fuel_type == fuel_type_gasoline) {
+               (part_info(*it).fuel_type == fuel_type_gasoline || part_info(*it).fuel_type == fuel_type_diesel)) {
                 anchor_found = true;
             }
         }
