5c5a4ac36752b9a57c4c96c031f82a6328979007
wormingdead <wormingdead@gmail.com>
2014-10-19 16:54:49 -0700

    No need to compare bools to bools. Also remove repeated comment.

--- src/game.cpp
+++ src/game.cpp
@@ -3822,11 +3822,11 @@ void game::update_scent()
             sum_3_scent_y[y][x] = 0;
             squares_used_y[y][x] = 0;
             for (int i = y - 1; i <= y + 1; ++i) {
-                if (wall[x][i] == false) {
-                    if (reduce_scent[x][i] == true) {
+                if (not wall[x][i]) {
+                    if (reduce_scent[x][i]) {
                         // only 20% of scent can diffuse on REDUCE_SCENT squares
                         sum_3_scent_y[y][x] += 2 * grscent[x][i];
-                        squares_used_y[y][x] += 2; // only 20% diffuses into REDUCE_SCENT squares
+                        squares_used_y[y][x] += 2;
                     } else {
                         sum_3_scent_y[y][x] += 10 * grscent[x][i];
                         squares_used_y[y][x] += 10;
@@ -3838,7 +3838,7 @@ void game::update_scent()
 
     for (int x = scentmap_minx; x <= scentmap_maxx; ++x) {
         for (int y = scentmap_miny; y <= scentmap_maxy; ++y) {
-            if (wall[x][y] == false) {
+            if (not wall[x][y]) {
                 // to how many neighboring squares do we diffuse out? (include our own square
                 // since we also include our own square when diffusing in)
                 int squares_used = squares_used_y[y][x - 1]
@@ -3846,7 +3846,7 @@ void game::update_scent()
                                    + squares_used_y[y][x + 1];
 
                 int this_diffusivity;
-                if (reduce_scent[x][y] == false) {
+                if (not reduce_scent[x][y]) {
                     this_diffusivity = diffusivity;
                 } else {
                     this_diffusivity = diffusivity / 5; //less air movement for REDUCE_SCENT square
