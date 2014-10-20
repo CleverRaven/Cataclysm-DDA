b76fdcce1fe4486aab0313622f8f5cbbd7db7791
Kevin Granade <kevin.granade@gmail.com>
2014-10-19 22:04:01 -0500

    Update radio item triggering to use new lambda-based iteration over map items. Fixes #8238

--- src/map.cpp
+++ src/map.cpp
@@ -3478,49 +3478,39 @@ std::list<std::pair<tripoint, item *> > map::get_rc_items( int x, int y, int z )
     return rc_pairs;
 }
 
+static bool trigger_radio_item( std::string signal, std::vector<item> &items, int n, point pos )
+{
+    bool trigger_item = false;
+    if( items[n].has_flag("RADIO_ACTIVATION") && items[n].has_flag(signal) ) {
+        g->sound(pos.x, pos.y, 6, "beep.");
+        if( items[n].has_flag("BOMB") ) {
+            // Set charges to 0 to ensure it detonates.
+            items[n].charges = 0;
+        }
+        trigger_item = true;
+    } else if( items[n].has_flag("RADIO_CONTAINER") && !items[n].contents.empty() &&
+               items[n].contents[0].has_flag( signal ) ) {
+        // A bomb is the only thing meaningfully placed in a container,
+        // If that changes, this needs logic to handle the alternative.
+        itype_id bomb_type = items[n].contents[0].type->id;
+
+        items[n].make(bomb_type);
+        items[n].charges = 0;
+        trigger_item = true;
+    }
+    if( trigger_item ) {
+        return process_item( items, n, pos, true );
+    }
+    return false;
+}
+
 void map::trigger_rc_items( std::string signal )
 {
-    point pos;
-    for( pos.x = 0; pos.x < SEEX * MAPSIZE; pos.x++ ) {
-        for( pos.y = 0; pos.y < SEEY * MAPSIZE; pos.y++ ) {
-            std::vector<item> &items = i_at( pos.x, pos.y );
-            for( size_t n = 0; n < items.size(); n++ ) {
-                int adjust_active_item_count = 0;
-                bool trigger_item = false;
-                // Grab this value here so it isn't overwritten by item::make().
-                bool previously_active = items[n].needs_processing();
-                if( items[n].has_flag("RADIO_ACTIVATION") && items[n].has_flag(signal) ) {
-                    g->sound(pos.x, pos.y, 6, "beep.");
-                    if( items[n].has_flag("BOMB") ) {
-                        // Set charges to 0 to ensure it detonates.
-                        items[n].charges = 0;
-                    }
-                    trigger_item = true;
-                } else if( items[n].has_flag("RADIO_CONTAINER") && !items[n].contents.empty() &&
-                           items[n].contents[0].has_flag( signal ) ) {
-                    // A bomb is the only thing meaningfully placed in a container,
-                    // If that changes, this needs logic to handle the alternative.
-                    itype_id bomb_type = items[n].contents[0].type->id;
-
-                    items[n].make(bomb_type);
-                    items[n].charges = 0;
-                    trigger_item = true;
-                }
-                if( trigger_item ) {
-                    bool item_destroyed = process_item( items, n, pos, true );
-                    if( previously_active && (item_destroyed || !items[n].needs_processing()) ) {
-                        adjust_active_item_count = -1;
-                    } else if( !previously_active &&
-                               (item_destroyed || items[n].needs_processing()) ) {
-                        adjust_active_item_count = 1;
-                    }
-                    if( adjust_active_item_count != 0 ) {
-                        get_submap_at( pos.x, pos.y )->active_item_count += adjust_active_item_count;
-                    }
-                }
-            }
-        }
-    }
+    process_items(
+        false,
+        [&] ( std::vector<item> &items, size_t n, point pos, vehicle *, int ) {
+            return trigger_radio_item( signal, items, n, pos );
+        } );
 }
 
 std::string map::trap_get(const int x, const int y) const {
