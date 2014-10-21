c6eba83bf432cee9aeac4f2e0d3ecd051f66518e
Coolthulhu <Coolthulhu@gmail.com>
2014-10-21 02:23:15 +0200

    Fixes claw damage only applying when wearing gloves

--- src/melee.cpp
+++ src/melee.cpp
@@ -704,7 +704,7 @@ int player::roll_cut_damage(bool crit)
     }
 
     if (unarmed_attack()) {
-        if (wearing_something_on(bp_hand_l)) {
+        if (!wearing_something_on(bp_hand_l)) {
             if (has_trait("CLAWS") || (has_active_mutation("CLAWS_RETRACT")) ) {
                 ret += 3;
             }
@@ -719,7 +719,7 @@ int player::roll_cut_damage(bool crit)
                 ret += rng(2, 3);
             }
         }
-        if (wearing_something_on(bp_hand_r)) {
+        if (!wearing_something_on(bp_hand_r)) {
             if (has_trait("CLAWS") || (has_active_mutation("CLAWS_RETRACT")) ) {
                 ret += 3;
             }
