de1fefa679233e5c8935e907cb94a40c91806011
Pouar <thepouar@gmail.com>
2014-10-25 14:36:54 -0500

    add djb2sum

---
+++ tools/djb2sum.c
@@ -0,0 +1,28 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+
+
+int djb2_hash(unsigned char *str)
+{
+    unsigned long hash = 5381;
+    unsigned char c = *str++;
+    while (c != '\0') {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+        c = *str++;
+    }
+    return hash;
+}
+
+int main()
+{
+		char djb2_char[100];
+		fgets(djb2_char,100,stdin);
+		printf("%u",djb2_hash((unsigned char *)djb2_char));
+
+
+}
+
+ 
