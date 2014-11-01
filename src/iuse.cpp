7cd756459d66f61a67e3a6e9170d4013dfbf995c
malhawee <malhawee@gmail.com>
2014-11-01 23:30:49 +1100

    Added removal flag, as per BevapDin.

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -59,6 +59,7 @@ void remove_recharge_mod( item &it, player &p )
     p.i_add_or_drop( mod, 1 );
     it.item_tags.erase( "RECHARGE" );
     it.item_tags.erase( "NO_UNLOAD" );
+    it.item_tags.erase( "NO_RELOAD" );
 }
 
 void remove_atomic_mod( item &it, player &p )
