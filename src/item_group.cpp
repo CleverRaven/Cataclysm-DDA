97f764af17c1534ff6aa2ef752b4ef20c88ff1f4
Thomas Kirsten <tho_ki@gmx.de>
2014-11-01 14:44:42 +0100

    Fix item spawn charges not being set for liquids.

--- src/item_group.cpp
+++ src/item_group.cpp
@@ -164,7 +164,7 @@ void Item_modifier::modify(item &new_item) const
     long ch = (charges.first == charges.second) ? charges.first : rng(charges.first, charges.second);
     if(ch != -1) {
         it_tool *t = dynamic_cast<it_tool *>(new_item.type);
-        if(new_item.count_by_charges()) {
+        if( new_item.count_by_charges() || new_item.made_of( LIQUID ) ) {
             // food, ammo
             new_item.charges = ch;
         } else if(t != NULL) {
