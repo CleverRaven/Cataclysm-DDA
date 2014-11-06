82f7d741e9562bc4bd7aa73cd3a239c95d4d80d9
Kevin Granade <kevin.granade@gmail.com>
2014-11-05 23:38:30 -0600

    Don't apply jolt damage to protrusions.

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -4176,7 +4176,8 @@ void vehicle::damage_all (int dmg1, int dmg2, int type, const point &impact)
     if (dmg1 < 1) { return; }
     for (size_t p = 0; p < parts.size(); p++) {
         int distance = 1 + square_dist( parts[p].mount_dx, parts[p].mount_dy, impact.x, impact.y );
-        if( distance > 1 && part_info(p).location == part_location_structure ) {
+        if( distance > 1 && part_info(p).location == part_location_structure &&
+            !part_info(p).has_flag("PROTRUSION") ) {
             damage_direct (p, rng( dmg1, dmg2 ) / (distance * distance), type);
         }
     }
