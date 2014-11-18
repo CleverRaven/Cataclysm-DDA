5669d52f425ed7c98549b78da3b6cfdeb998be6a
Mshock777 <jnpahan@gmail.com>
2014-11-18 20:17:41 +0400

    Revert "Furniture (de)construction, warmth changes"
    This reverts commit b55f5f3659ae53e7c2a47f8aefcaa1208f836489.

--- src/player.cpp
+++ src/player.cpp
@@ -839,16 +839,15 @@ void player::update_bodytemp()
         if( furn_at_pos == f_bed ) {
             floor_bedding_warmth += 1000;
         } else if( furn_at_pos == f_makeshift_bed || furn_at_pos == f_armchair ||
-                   furn_at_pos == f_sofa ) {
+                   furn_at_pos == f_sofa || furn_at_pos == f_hay ) {
             floor_bedding_warmth += 500;
         } else if( furn_at_pos == f_straw_bed ) {
             floor_bedding_warmth += 200;
-        } else if( trap_at_pos == tr_cot || ter_at_pos == t_improvised_shelter ||
-                   furn_at_pos == f_tatami ) {
+        } else if( trap_at_pos == tr_cot || ter_at_pos == t_improvised_shelter ) {
             floor_bedding_warmth -= 500;
         } else if( trap_at_pos == tr_rollmat ) {
             floor_bedding_warmth -= 1000;
-        } else if( trap_at_pos == tr_fur_rollmat || furn_at_pos == f_hay ) {
+        } else if( trap_at_pos == tr_fur_rollmat ) {
             floor_bedding_warmth += 0;
         } else if( veh && veh->part_with_feature (vpart, "SEAT") >= 0 ) {
             floor_bedding_warmth += 200;
@@ -10085,7 +10084,7 @@ bool player::can_sleep()
  const furn_id furn_at_pos = g->m.furn(posx, posy);
  if ( ((veh && veh->part_with_feature (vpart, "BED") >= 0) ||
      furn_at_pos == f_makeshift_bed || trap_at_pos == tr_cot ||
-     furn_at_pos == f_sofa || (in_shell)) &&
+     furn_at_pos == f_sofa || furn_at_pos == f_hay || (in_shell)) &&
      (!(plantsleep)) ) {
     sleepy += 4;
  }
@@ -10094,16 +10093,13 @@ bool player::can_sleep()
       furn_at_pos == f_armchair || ter_at_pos == t_improvised_shelter) && (!(plantsleep)) ) {
     sleepy += 3;
  }
- else if ( (furn_at_pos == f_straw_bed || furn_at_pos == f_hay || furn_at_pos == f_tatami) &&
-          (!(plantsleep)) ) {
+ else if ( (furn_at_pos == f_straw_bed) && (!(plantsleep)) ) {
     sleepy += 2;
  }
  else if ( (furn_at_pos == f_bed) && (!(plantsleep)) ) {
     sleepy += 5;
  }
- else if ( (ter_at_pos == t_floor || ter_at_pos == t_floor_waxed || ter_at_pos == t_carpet_red ||
-          ter_at_pos == t_carpet_yellow || ter_at_pos == t_carpet_green ||
-          ter_at_pos == t_carpet_purple) && (!(plantsleep)) ) {
+ else if ( (ter_at_pos == t_floor) && (!(plantsleep)) ) {
     sleepy += 1;
  }
  else if (plantsleep) {
