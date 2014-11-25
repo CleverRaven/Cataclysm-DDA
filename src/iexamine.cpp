ece3e1f6a2da36f30945d676cedf789ab67b5b19
jokermatt999 <jokermatt999@gmail.com>
2014-11-25 18:17:37 -0500

    Use parameter, add message
    Elegant solution?

--- src/iexamine.cpp
+++ src/iexamine.cpp
@@ -1805,8 +1805,9 @@ void iexamine::tree_marloss(player *p, map *m, int examx, int examy)
     }
 }
 
-void iexamine::shrub_wildveggies(player *p, int examx, int examy)
+void iexamine::shrub_wildveggies(player *p, map *m, int examx, int examy)
 {
+    add_msg("You forage through the %s.", m->tername(examx, examy).c_str());
     p->assign_activity(ACT_FORAGE, 500 / (p->skillLevel("survival") + 1), 0);
     p->activity.placement = point(examx, examy);
     return;
