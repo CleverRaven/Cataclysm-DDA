8252c54da50907362f635d5b99be10a8ecce04f5
Angela Graves <rivet.the.zombie@gmail.com>
2014-11-26 22:21:19 -0600

    Revert "Reload time lose effect when it less than 200"

--- src/player.cpp
+++ src/player.cpp
@@ -12884,6 +12884,10 @@ void player::assign_activity(activity_type type, int moves, int index, int pos, 
         }
         activity = player_activity(type, moves, index, pos, name);
     }
+    if (this->moves <= activity.moves_left) {
+        activity.moves_left -= this->moves;
+    }
+    this->moves = 0;
     activity.warned_of_proximity = false;
 }
 
