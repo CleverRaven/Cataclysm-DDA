6dcf0686c8186a925e23531e5ace8136539c69a9
Thomas Kirsten <tho_ki@gmx.de>
2014-12-09 18:51:01 +0100

    Prevent removal of shoulder strap from worn gun.
    Also remove an outdated debug message. It does not matter whether the item is armor, the item functions used there will work fine for non-armor.

--- src/player.cpp
+++ src/player.cpp
@@ -11182,6 +11182,11 @@ activate your weapon."), gun->tname().c_str(), _(mod->location.c_str()));
             add_msg(m_info, _("Your %s doesn't appear to be modded."), used->tname().c_str());
             return;
         }
+        if( inventory_position < -1 ) {
+            // Prevent removal of shoulder straps and thereby making the gun un-wearable again.
+            add_msg( _( "You can not modify your %s while it's worn." ), used->tname().c_str() );
+            return;
+        }
         // Create menu.
         int choice = -1;
 
@@ -12097,11 +12102,6 @@ int player::encumb(body_part bp, double &layers, int &armorenc) const
     }
 
     for (size_t i = 0; i < worn.size(); ++i) {
-        if( !worn[i].is_armor() ) {
-            debugmsg("%s::encumb hit a non-armor item at worn[%d] (%s)", name.c_str(),
-                     i, worn[i].tname().c_str());
-        }
-
         if( worn[i].covers(bp) ) {
             if( worn[i].has_flag( "SKINTIGHT" ) ) {
                 level = UNDERWEAR;
