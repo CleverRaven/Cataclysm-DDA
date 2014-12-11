6f3f8e77d15025f54af21875d977530b4037b795
Thomas Kirsten <tho_ki@gmx.de>
2014-12-11 15:25:37 +0100

    Apply astyle to Item_factory::set_use_methods_from_json

--- src/item_factory.cpp
+++ src/item_factory.cpp
@@ -1237,34 +1237,34 @@ void Item_factory::load_item_group(JsonObject &jsobj, const Group_tag &group_id,
     }
 }
 
-void Item_factory::set_use_methods_from_json(JsonObject &jo, std::string member,
-        itype *new_item_template)
+void Item_factory::set_use_methods_from_json( JsonObject &jo, std::string member,
+        itype *new_item_template )
 {
-    if (jo.has_array(member)) {
-        JsonArray jarr = jo.get_array(member);
-        while (jarr.has_more()) {
+    if( jo.has_array( member ) ) {
+        JsonArray jarr = jo.get_array( member );
+        while( jarr.has_more() ) {
             use_function new_function;
-            if (jarr.test_string()) {
-                new_function = use_from_string(jarr.next_string());
-            } else if (jarr.test_object()) {
-                new_function = use_from_object(jarr.next_object());
+            if( jarr.test_string() ) {
+                new_function = use_from_string( jarr.next_string() );
+            } else if( jarr.test_object() ) {
+                new_function = use_from_object( jarr.next_object() );
             } else {
-                debugmsg("use_action array element for item %s is neither string nor object.",
-                         new_item_template->id.c_str());
+                debugmsg( "use_action array element for item %s is neither string nor object.",
+                          new_item_template->id.c_str() );
             }
-            new_item_template->use_methods.push_back(new_function);
+            new_item_template->use_methods.push_back( new_function );
         }
     } else {
         use_function new_function;
-        if (jo.has_string("use_action")) {
-            new_function = use_from_string(jo.get_string("use_action"));
-        } else if (jo.has_object("use_action")) {
-            new_function = use_from_object(jo.get_object("use_action"));
+        if( jo.has_string( "use_action" ) ) {
+            new_function = use_from_string( jo.get_string( "use_action" ) );
+        } else if( jo.has_object( "use_action" ) ) {
+            new_function = use_from_object( jo.get_object( "use_action" ) );
         } else {
-            debugmsg("use_action member for item %s is neither string nor object.",
-                     new_item_template->id.c_str());
+            debugmsg( "use_action member for item %s is neither string nor object.",
+                      new_item_template->id.c_str() );
         }
-        new_item_template->use_methods.push_back(new_function);
+        new_item_template->use_methods.push_back( new_function );
     }
 }
 
