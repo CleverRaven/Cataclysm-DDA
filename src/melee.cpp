2e0dffb12c9bd9f91c13be72d82bd9a0baaf1124
jokermatt999 <jokermatt999@gmail.com>
2014-12-13 16:42:05 -0500

    Make code work

--- src/melee.cpp
+++ src/melee.cpp
@@ -880,7 +880,10 @@ int player::roll_stuck_penalty(bool stabbing, ma_technique &tec)
     // The cost of the weapon getting stuck, in units of move points.
     const int weapon_speed = attack_speed(*this);
     int stuck_cost = weapon_speed;
-    int attack_skill = stabbing ? (get_skill_level("stabbing")*2) : get_skill_level("cutting");
+    int attack_skill = stabbing ? get_skill_level("stabbing") : get_skill_level("cutting");
+    if (stabbing) {
+        attack_skill*2;
+    }
 
     if (has_active_bionic("bio_cqb")) {
         attack_skill = 5;
