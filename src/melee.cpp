ba9373c47d455e1e786c1bd62f9feb20216e90cd
jokermatt999 <jokermatt999@gmail.com>
2014-12-14 21:58:13 -0500

    Remove accidentally left skill buff

--- src/melee.cpp
+++ src/melee.cpp
@@ -881,9 +881,6 @@ int player::roll_stuck_penalty(bool stabbing, ma_technique &tec)
     const int weapon_speed = attack_speed(*this);
     int stuck_cost = weapon_speed;
     int attack_skill = stabbing ? get_skill_level("stabbing") : get_skill_level("cutting");
-    if (stabbing) {
-        attack_skill*2;
-    }
 
     if (has_active_bionic("bio_cqb")) {
         attack_skill = 5;
