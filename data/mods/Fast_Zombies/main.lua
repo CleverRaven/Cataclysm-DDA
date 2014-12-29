c0800c8d5ff1f142725689f17230a205f18d2fc9
Coolthulhu <Coolthulhu@gmail.com>
2014-12-29 21:15:21 +0100

    Changes id string search into proper species check

--- data/mods/Fast_Zombies/main.lua
+++ data/mods/Fast_Zombies/main.lua
@@ -1,8 +1,8 @@
 monster_types = game.get_monster_types()
 
--- double the speed of all monsters that start with mon_zombie
+-- double the speed of all monsters whose species is "ZOMBIE"
 for _, monster_type in ipairs(monster_types) do
-    if monster_type:find("mon_zombie") then
+    if game.monster_type(monster_type):in_species("ZOMBIE") then
         game.monster_type(monster_type).speed = game.monster_type(monster_type).speed * 2
     end
 end
