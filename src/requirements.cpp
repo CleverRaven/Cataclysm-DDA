d19fe84d1d51d9ca19a94ef2ccbe6ee6d7e0181f
macrosblackd <MacrosBlackD@gmail.com>
2014-12-31 21:05:35 -0600

    Change recoverability flag into string
    Changes the flag for recoverability into a string "NO_RECOVER".

--- src/requirements.cpp
+++ src/requirements.cpp
@@ -94,8 +94,10 @@ void item_comp::load( JsonArray &ja )
     JsonArray comp = ja.next_array();
     type = comp.get_string( 0 );
     count = comp.get_int( 1 );
-    //Default true for backward compatibility.
-    recoverable = comp.size() > 2 ? comp.get_bool(2) : true;
+    // Recoverable is true by default.
+    if(comp.size() > 2) {
+        recoverable = comp.get_string(2) == "NO_RECOVER" ? false : true;
+    }
 }
 
 template<typename T>
