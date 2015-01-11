1a8619ebb9922ba587e07f2b018a0b659709c10a
Brandon Kentel <bkentel@users.noreply.github.com>
2015-01-11 23:19:38 +0900

    comments

--- src/filesystem.h
+++ src/filesystem.h
@@ -14,15 +14,16 @@ bool rename_file(const std::string &old_path, const std::string &new_path);
 
 //--------------------------------------------------------------------------------------------------
 /**
- * Returns a vector of files or directories matching pattern at root_path.
+ * Returns a vector of files or directories matching pattern at @p root_path.
  *
- * Seaches through the directory tree breadth-first. Directories are searched in lexigraphical
- * order. Matching files within in each directory are also ordered lexigraphically.
+ * Searches through the directory tree breadth-first. Directories are searched in lexical
+ * order. Matching files within in each directory are also ordered lexically.
  *
- * @param pattern The substring to match.
+ * @param pattern The sub-string to match.
  * @param root_path The path relative to the current working directory to search; empty means ".".
  * @param recursive_search Whether to recursively search sub directories.
- * @param match_extension If true, match pattern at the end of file names.
+ * @param match_extension If true, match pattern at the end of file names. Otherwise, match anywhere
+ *                        in the file name.
  */
 std::vector<std::string> get_files_from_path(std::string const &pattern,
         std::string const &root_path = "", bool recursive_search = false,
@@ -30,7 +31,7 @@ std::vector<std::string> get_files_from_path(std::string const &pattern,
 
 //--------------------------------------------------------------------------------------------------
 /**
- * Returns a vector of directories containing files matching any of patterns.
+ * Returns a vector of directories which contain files matching any of @p patterns.
  *
  * @param patterns A vector or patterns to match.
  * @see get_files_from_path
