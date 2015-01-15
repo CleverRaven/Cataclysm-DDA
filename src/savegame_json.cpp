0d9af6ccb58e3dc4beb10c73c44164c01177923e
DavidKeaton <keaton.david@gmail.com>
2015-01-14 20:44:08 -0500

    [de|]serialize data properly for cameras, dome lights, aisle lights on save/load

--- src/savegame_json.cpp
+++ src/savegame_json.cpp
@@ -1300,6 +1300,9 @@ void vehicle::deserialize(JsonIn &jsin)
     data.read("of_turn_carry", of_turn_carry);
     data.read("is_locked", is_locked);
     data.read("is_alarm_on", is_alarm_on);
+    data.read("camera_on", camera_on);
+    data.read("dome_lights_on", dome_lights_on);
+    data.read("aisle_lights_on", aisle_lights_on);
 
     face.init (fdir);
     move.init (mdir);
@@ -1373,6 +1376,9 @@ void vehicle::serialize(JsonOut &json) const
     json.member( "labels", labels );
     json.member( "is_locked", is_locked );
     json.member( "is_alarm_on", is_alarm_on );
+    json.member( "camera_on", camera_on );
+    json.member( "dome_lights_on", dome_lights_on );
+    json.member( "aisle_lights_on", aisle_lights_on );
     json.end_object();
 }
 
