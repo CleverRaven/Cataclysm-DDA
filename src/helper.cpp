bca1feb01e6ff7d54054fdc522fdd515f9e64e1e
DavidKeaton <keaton.david@gmail.com>
2015-01-19 13:46:40 -0500

    added clamp function, fixed is_between

--- src/helper.cpp
+++ src/helper.cpp
@@ -1,8 +1,15 @@
 #include "helper.h"
 
-bool is_between(int low, int in, int high)
+bool is_between(int low, int in, int high, bool equal_to)
 {
-    return ((in >= low) ? ((in <= high) ? true : false) : false);
+    bool is_above_low  = ((equal_to == true) ? (in >= low)  : (in > low));
+    bool is_below_high = ((equal_to == true) ? (in <= high) : (in < high));
+    return ((is_above_low && is_below_high) ? true : false);
+}
+
+int clamp_value(int low, int in, int high)
+{
+    return ((in > low) ? ((in < high) ? in : high) : low);
 }
 
 
