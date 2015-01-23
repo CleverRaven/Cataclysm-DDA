727f3cbf732313715f6e8cabdda652a8ccbb36db
Thomas Kirsten <tho_ki@gmx.de>
2015-01-23 14:36:19 +0100

    Remove a copy constructor, use the compiler generated one instead.

--- src/dialogue.h
+++ src/dialogue.h
@@ -119,24 +119,6 @@ struct talk_response {
         success = TALK_NONE;
         failure = TALK_NONE;
     }
-
-    talk_response(const talk_response &rhs)
-    {
-        text = rhs.text;
-        trial = rhs.trial;
-        difficulty = rhs.difficulty;
-        mission_index = rhs.mission_index;
-        miss = rhs.miss;
-        tempvalue = rhs.tempvalue;
-        skill = rhs.skill;
-        style = rhs.style;
-        effect_success = rhs.effect_success;
-        effect_failure = rhs.effect_failure;
-        opinion_success = rhs.opinion_success;
-        opinion_failure = rhs.opinion_failure;
-        success = rhs.success;
-        failure = rhs.failure;
-    }
 };
 
 struct talk_response_list {
