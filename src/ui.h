3f11abb120e64db8c27deb49659134ff22e88beb
Brandon Kentel <bkentel@users.noreply.github.com>
2015-01-23 13:19:23 +0900

    revert MSVC fix in this branch

--- src/ui.h
+++ src/ui.h
@@ -38,7 +38,7 @@ struct uimenu_entry {
     std::string txt;      // what it says on the tin
     nc_color hotkey_color;
     nc_color text_color;
-    mvwzstr extratxt = mvwzstr { 0, c_unset, "", 0};
+    mvwzstr extratxt = { 0, c_unset, "", 0};
     //std::string filtertxt; // possibly useful
     uimenu_entry(std::string T) : retval(-1), enabled(true), hotkey(-1), txt(T)
     {
