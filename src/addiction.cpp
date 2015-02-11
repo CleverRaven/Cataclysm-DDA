309c5f2af8318dff924c4aa5bbf098de76c89777
Brandon Kentel <bkentel@users.noreply.github.com>
2015-02-11 17:32:23 +0900

    add_msg -> add_message_if_player

--- src/addiction.cpp
+++ src/addiction.cpp
@@ -4,7 +4,6 @@
 #include "player.h"
 #include "morale.h"
 #include "rng.h"
-#include "messages.h"
 
 void addict_effect(player &u, addiction &add,
                    std::function<void (char const*)> const &cancel_activity)
@@ -14,7 +13,7 @@ void addict_effect(player &u, addiction &add,
     switch (add.type) {
     case ADD_CIG:
         if (in > 20 || one_in((500 - 20 * in))) {
-            add_msg(rng(0, 6) < in ? _("You need some nicotine.") :
+            u.add_msg_if_player(rng(0, 6) < in ? _("You need some nicotine.") :
                     _("You could use some nicotine."));
             u.add_morale(MORALE_CRAVING_NICOTINE, -15, -50);
             if (one_in(800 - 50 * in)) {
@@ -29,13 +28,13 @@ void addict_effect(player &u, addiction &add,
     case ADD_CAFFEINE:
         u.moves -= 2;
         if (in > 20 || one_in((500 - 20 * in))) {
-            add_msg(m_warning, _("You want some caffeine."));
+            u.add_msg_if_player(m_warning, _("You want some caffeine."));
             u.add_morale(MORALE_CRAVING_CAFFEINE, -5, -30);
             if (u.stim > -150 && rng(0, 10) < in) {
                 u.stim--;
             }
             if (rng(8, 400) < in) {
-                add_msg(m_bad, _("Your hands start shaking... you need it bad!"));
+                u.add_msg_if_player(m_bad, _("Your hands start shaking... you need it bad!"));
                 u.add_effect("shakes", 20);
             }
         }
@@ -48,10 +47,10 @@ void addict_effect(player &u, addiction &add,
             u.mod_healthy_mod(-1);
         }
         if (one_in(20) && rng(0, 20) < in) {
-            add_msg(m_warning, _("You could use a drink."));
+            u.add_msg_if_player(m_warning, _("You could use a drink."));
             u.add_morale(MORALE_CRAVING_ALCOHOL, -35, -120);
         } else if (rng(8, 300) < in) {
-            add_msg(m_bad, _("Your hands start shaking... you need a drink bad!"));
+            u.add_msg_if_player(m_bad, _("Your hands start shaking... you need a drink bad!"));
             u.add_morale(MORALE_CRAVING_ALCOHOL, -35, -120);
             u.add_effect("shakes", 50);
         } else if (!u.has_effect("hallu") && rng(10, 1600) < in) {
@@ -84,14 +83,14 @@ void addict_effect(player &u, addiction &add,
                 u.mod_healthy_mod(-1);
             }
             if (one_in(20) && dice(2, 20) < in) {
-                add_msg(m_bad, _("Your hands start shaking... you need some painkillers."));
+                u.add_msg_if_player(m_bad, _("Your hands start shaking... you need some painkillers."));
                 u.add_morale(MORALE_CRAVING_OPIATE, -40, -200);
                 u.add_effect("shakes", 20 + in * 5);
             } else if (one_in(20) && dice(2, 30) < in) {
-                add_msg(m_bad, _("You feel anxious.  You need your painkillers!"));
+                u.add_msg_if_player(m_bad, _("You feel anxious.  You need your painkillers!"));
                 u.add_morale(MORALE_CRAVING_OPIATE, -30, -200);
             } else if (one_in(50) && dice(3, 50) < in) {
-                add_msg(m_bad, _("You throw up heavily!"));
+                u.add_msg_if_player(m_bad, _("You throw up heavily!"));
                 cancel_activity(_("Throwing up."));
                 u.vomit();
             }
@@ -113,14 +112,14 @@ void addict_effect(player &u, addiction &add,
             u.mod_healthy_mod(-1);
         }
         if (dice(2, 100) < in) {
-            add_msg(m_warning, _("You feel depressed.  Speed would help."));
+            u.add_msg_if_player(m_warning, _("You feel depressed.  Speed would help."));
             u.add_morale(MORALE_CRAVING_SPEED, -25, -200);
         } else if (one_in(10) && dice(2, 80) < in) {
-            add_msg(m_bad, _("Your hands start shaking... you need a pick-me-up."));
+            u.add_msg_if_player(m_bad, _("Your hands start shaking... you need a pick-me-up."));
             u.add_morale(MORALE_CRAVING_SPEED, -25, -200);
             u.add_effect("shakes", in * 20);
         } else if (one_in(50) && dice(2, 100) < in) {
-            add_msg(m_bad, _("You stop suddenly, feeling bewildered."));
+            u.add_msg_if_player(m_bad, _("You stop suddenly, feeling bewildered."));
             cancel_activity(nullptr);
             u.moves -= 300;
         } else if (!u.has_effect("hallu") && one_in(20) && 8 + dice(2, 80) < in) {
@@ -133,11 +132,11 @@ void addict_effect(player &u, addiction &add,
         u.mod_int_bonus(-1);
         u.mod_per_bonus(-1);
         if (in >= 30 || one_in((900 - 30 * in))) {
-            add_msg(m_warning, _("You feel like you need a bump."));
+            u.add_msg_if_player(m_warning, _("You feel like you need a bump."));
             u.add_morale(MORALE_CRAVING_COCAINE, -20, -250);
         }
         if (dice(2, 80) <= in) {
-            add_msg(m_warning, _("You feel like you need a bump."));
+            u.add_msg_if_player(m_warning, _("You feel like you need a bump."));
             u.add_morale(MORALE_CRAVING_COCAINE, -20, -250);
             if (u.stim > -150) {
                 u.stim -= 3;
@@ -149,11 +148,11 @@ void addict_effect(player &u, addiction &add,
         u.mod_int_bonus(-1);
         u.mod_per_bonus(-1);
         if (in >= 30 || one_in((900 - 30 * in))) {
-            add_msg(m_bad, _("You're shivering, you need some crack."));
+            u.add_msg_if_player(m_bad, _("You're shivering, you need some crack."));
             u.add_morale(MORALE_CRAVING_CRACK, -80, -250);
         }
         if (dice(2, 80) <= in) {
-            add_msg(m_bad, _("You're shivering, you need some crack."));
+            u.add_msg_if_player(m_bad, _("You're shivering, you need some crack."));
             u.add_morale(MORALE_CRAVING_CRACK, -80, -250);
             if (u.stim > -150) {
                 u.stim -= 3;
@@ -164,17 +163,17 @@ void addict_effect(player &u, addiction &add,
     case ADD_MUTAGEN:
         if (u.has_trait("MUT_JUNKIE")) {
             if (one_in(600 - 50 * in)) {
-                add_msg(m_warning, rng(0, 6) < in ? _("You so miss the exquisite rainbow of post-humanity.") :
+                u.add_msg_if_player(m_warning, rng(0, 6) < in ? _("You so miss the exquisite rainbow of post-humanity.") :
                         _("Your body is SOO booorrrring.  Just a little sip to liven things up?"));
                 u.add_morale(MORALE_CRAVING_MUTAGEN, -20, -200);
             }
             if (u.focus_pool > 40 && one_in(800 - 20 * in)) {
                 u.focus_pool -= (in);
-                add_msg(m_warning,
+                u.add_msg_if_player(m_warning,
                         _("You daydream what it'd be like if you were *different*.  Different is good."));
             }
         } else if (in > 5 || one_in((500 - 15 * in))) {
-            add_msg(m_warning, rng(0, 6) < in ? _("You haven't had any mutagen lately.") :
+            u.add_msg_if_player(m_warning, rng(0, 6) < in ? _("You haven't had any mutagen lately.") :
                     _("You could use some new parts..."));
             u.add_morale(MORALE_CRAVING_MUTAGEN, -5, -50);
         }
@@ -187,16 +186,16 @@ void addict_effect(player &u, addiction &add,
             u.mod_healthy_mod(-1);
         }
         if (one_in(20) && rng(0, 20) < in) {
-            add_msg(m_warning, _("You could use some diazepam."));
+            u.add_msg_if_player(m_warning, _("You could use some diazepam."));
             u.add_morale(MORALE_CRAVING_DIAZEPAM, -35, -120);
         } else if (rng(8, 200) < in) {
-            add_msg(m_bad, _("You're shaking... you need some diazepam!"));
+            u.add_msg_if_player(m_bad, _("You're shaking... you need some diazepam!"));
             u.add_morale(MORALE_CRAVING_DIAZEPAM, -35, -120);
             u.add_effect("shakes", 50);
         } else if (!u.has_effect("hallu") && rng(10, 3200) < in) {
             u.add_effect("hallu", 3600);
         } else if (one_in(50) && dice(3, 50) < in) {
-            add_msg(m_bad, _("You throw up heavily!"));
+            u.add_msg_if_player(m_bad, _("You throw up heavily!"));
             cancel_activity(_("Throwing up."));
             u.vomit();
         }
@@ -204,7 +203,7 @@ void addict_effect(player &u, addiction &add,
     case ADD_MARLOSS_R:
         if (one_in(800 - 20 * in)) {
             u.add_morale(MORALE_CRAVING_MARLOSS, -5, -25);
-            add_msg(m_info, _("You daydream about luscious pink berries as big as your fist."));
+            u.add_msg_if_player(m_info, _("You daydream about luscious pink berries as big as your fist."));
             if (u.focus_pool > 40) {
                 u.focus_pool -= (in);
             }
@@ -213,7 +212,7 @@ void addict_effect(player &u, addiction &add,
     case ADD_MARLOSS_B:
         if (one_in(800 - 20 * in)) {
             u.add_morale(MORALE_CRAVING_MARLOSS, -5, -25);
-            add_msg(m_info, _("You daydream about nutty cyan seeds as big as your hand."));
+            u.add_msg_if_player(m_info, _("You daydream about nutty cyan seeds as big as your hand."));
             if (u.focus_pool > 40) {
                 u.focus_pool -= (in);
             }
@@ -222,7 +221,7 @@ void addict_effect(player &u, addiction &add,
     case ADD_MARLOSS_Y:
         if (one_in(800 - 20 * in)) {
             u.add_morale(MORALE_CRAVING_MARLOSS, -5, -25);
-            add_msg(m_info, _("You daydream about succulent, pale golden gel, sweet but light."));
+            u.add_msg_if_player(m_info, _("You daydream about succulent, pale golden gel, sweet but light."));
             if (u.focus_pool > 40) {
                 u.focus_pool -= (in);
             }
