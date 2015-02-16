aa047ac40702e5a78638ae2880a1293c239b61fe
Brandon Kentel <bkentel@users.noreply.github.com>
2015-02-16 11:46:36 +0900

    tweaks
    + use `std::default_random_engine`.
    + modfiy the distributions' paramater set instead of creating new
    instances.

--- src/rng.cpp
+++ src/rng.cpp
@@ -2,7 +2,7 @@
 #include <random>
 
 namespace {
-std::mt19937 random_generator {std::random_device{}()};
+std::default_random_engine random_generator {std::random_device{}()};
 } //namespace
 
 void seed_random_number_generator(char const* seed)
@@ -13,18 +13,24 @@ void seed_random_number_generator(char const* seed)
 
 long rng(long const val1, long const val2)
 {
-    return std::uniform_int_distribution<long> {
+    static std::uniform_int_distribution<long> dist;
+    dist.param(std::uniform_int_distribution<long>::param_type {
         (val1 < val2) ? val1 : val2, // lo
         (val1 < val2) ? val2 : val1  // hi
-    }(random_generator);
+    });
+
+    return dist(random_generator);
 }
 
 double rng_float(double const val1, double const val2)
 {
-    return std::uniform_real_distribution<double> {
+    static std::uniform_real_distribution<double> dist;
+    dist.param(std::uniform_real_distribution<double>::param_type {
         (val1 < val2) ? val1 : val2, // lo
         (val1 < val2) ? val2 : val1  // hi
-    }(random_generator);
+    });
+
+    return dist(random_generator);
 }
 
 bool one_in(int chance)
@@ -39,14 +45,16 @@ bool one_in_improved(double chance)
     return (chance <= 1 || rng_float(0, chance) < 1);
 }
 
-bool x_in_y(double x, double y)
+bool x_in_y(double const x, double const y)
 {
+    static std::uniform_real_distribution<double> dist {0, 1};
+
     double const ratio = x / y;
     if (ratio >= 1.0) {
         return true;
     }
 
-    return std::uniform_real_distribution<double> {0, 1}(random_generator) < ratio;
+    return dist(random_generator) < ratio;
 }
 
 int dice(int number, int sides)
