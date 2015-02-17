b51ce237b23d3716cb48af4927abc0a022b18c49
Brandon Kentel <bkentel@users.noreply.github.com>
2015-02-17 19:43:51 +0900

    switched the wrong one in haste

--- src/item.cpp
+++ src/item.cpp
@@ -1078,10 +1078,10 @@ std::string item::info(bool showtext, std::vector<iteminfo> *dump, bool debug) c
                 dump->push_back(iteminfo("TOOL", "",
                                          ((tool->ammo_id == "NULL") ? ngettext("Maximum <num> charge.", "Maximum <num> charges.", tool->max_charges * 100) :
                                           string_format(ngettext("Maximum <num> charge of %s.", "Maximum <num> charges of %s.", tool->max_charges * 100),
-                                          ammo_id("plutonium").c_str())), tool->max_charges * 100));
+                                          ammo_name("plutonium").c_str())), tool->max_charges * 100));
             } else {
                 dump->push_back(iteminfo("TOOL", "",
-                    ((tool->ammo_name == "NULL") ? ngettext("Maximum <num> charge.", "Maximum <num> charges.", tool->max_charges) :
+                    ((tool->ammo_id == "NULL") ? ngettext("Maximum <num> charge.", "Maximum <num> charges.", tool->max_charges) :
                      string_format(ngettext("Maximum <num> charge of %s.", "Maximum <num> charges of %s.", tool->max_charges),
                                    ammo_name(tool->ammo_id).c_str())), tool->max_charges));
             }
