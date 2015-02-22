e7cf609fef1ad68f38cfc9d1307e02aa717eb4cf
DanmakuDan <danmakudan@gmail.com>
2015-02-21 20:55:01 -0600

    Added scrolling buttons N and P to construction menu to scroll between stages of a project. Also improved stage printout.

--- src/requirements.h
+++ src/requirements.h
@@ -164,9 +164,10 @@ struct requirement_data {
         int print_components(WINDOW *w, int ypos, int xpos, int width, nc_color col,
                              const inventory &crafting_inv, int batch = 1) const;
         std::vector<std::string> get_folded_components_list( int width, nc_color col,
-                const inventory &crafting_inv, int batch) const;
+                const inventory &crafting_inv, int batch = 1) const;
         int print_tools(WINDOW *w, int ypos, int xpos, int width, nc_color col,
                         const inventory &crafting_inv, int batch = 1) const;
+        std::vector<std::string> get_folded_tools_list(int width, nc_color col, const inventory &crafting_inv, int batch = 1) const;
 
     private:
         bool check_enough_materials(const inventory &crafting_inv, int batch = 1) const;
@@ -183,6 +184,9 @@ struct requirement_data {
         template<typename T>
         static int print_list(WINDOW *w, int ypos, int xpos, int width, nc_color col,
                               const inventory &crafting_inv, const std::vector< std::vector<T> > &objs, int batch = 1);
+        template<typename T>
+        static std::vector<std::string> get_folded_list(int width, nc_color col, const inventory &crafting_inv,
+                                   const std::vector< std::vector<T> > &objs, int batch = 1);
         template<typename T>
         static bool remove_item(const std::string &type, std::vector< std::vector<T> > &vec);
         template<typename T>
