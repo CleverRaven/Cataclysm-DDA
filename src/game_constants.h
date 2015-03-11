e6e05161cd6551f2e6738e2b8a0eb1e55d410214
Dustin Lacewell <dlacewell@gmail.com>
2015-03-11 10:51:04 -0700

    Change defines to use const instead

--- src/game_constants.h
+++ src/game_constants.h
@@ -28,41 +28,50 @@
 #define MAX_ITEM_IN_VEHICLE_STORAGE MAX_ITEM_IN_SQUARE // no reason to differ
 
 // vital thresholds and messages
-#define HUNGER_MINOR 3000
-#define HUNGER_MINOR_MSG "Your stomach feels so empty..."
-#define HUNGER_MAJOR 4000
-#define HUNGER_MAJOR_MSG "You are STARVING!"
-#define HUNGER_CRITICAL 5000
-#define HUNGER_CRITICAL_MSG "Food..."
-#define HUNGER_DEATH 6000
-#define HUNGER_DEATH_MSG "You have starved to death."
-#define HUNGER_MALE_MEMORIAL_MSG "Died of starvation"
-#define HUNGER_FEMALE_MEMORIAL_MSG "Died of starvation"
+const HUNGER_FREQUENCY 20;
+const HUNGER_MINOR 3000;
+const HUNGER_MAJOR 4000;
+const HUNGER_CRITICAL 5000;
+const HUNGER_DEATH 6000;
 
-#define THIRST_MINOR 600
-#define THIRST_MINOR_MSG "Your mouth feels so dry..."
-#define THIRST_MAJOR 800
-#define THIRST_MAJOR_MSG "You are THIRSTY!"
-#define THIRST_CRITICAL 1000
-#define THIRST_CRITICAL_MSG "Even your eyes feel dry..."
-#define THIRST_DEATH 1200
-#define THIRST_DEATH_MSG "You have died of dehydration."
-#define THIRST_MALE_MEMORIAL_MSG "Died of thirst."
-#define THIRST_FEMALE_MEMORIAL_MSG "Died of thirst."
+const HUNGER_MINOR_MSG "Your stomach feels so empty...";
+const HUNGER_MAJOR_MSG "You are STARVING!";
+const HUNGER_CRITICAL_MSG "Food...";
+const HUNGER_DEATH_MSG "You have starved to death.";
 
-#define FATIGUE_TRIVIAL 383
-#define FATIGUE_TRIVIAL_MSG "*yawn* You should really get some sleep."
-#define FATIGUE_MINOR 575
-#define FATIGUE_MINOR_MSG "How much longer until bedtime?"
-#define FATIGUE_MAJOR 600
-#define FATIGUE_MAJOR_MSG "You're too tired to stop yawning."
-#define FATIGUE_CRITICAL 700
-#define FATIGUE_CRITICAL_MSG "You feel like you haven't slept in days."
-#define FATIGUE_DIRE 800
-#define FATIGUE_DIRE_MSG "Anywhere would be a good place to sleep..."
-#define FATIGUE_SLEEP 1000
-#define FATIGUE_SLEEP_MSG "Survivor sleep now."
-#define FATIGUE_MALE_MEMORIAL_MSG "Succumbed to lack of sleep"
-#define FATIGUE_FEMALE_MEMORIAL_MSG "Succumbed to lack of sleep"
+const HUNGER_MALE_MEMORIAL_MSG "Died of starvation";
+const HUNGER_FEMALE_MEMORIAL_MSG "Died of starvation";
+
+const THIRST_FREQUENCY 20;
+const THIRST_MINOR 600;
+const THIRST_MAJOR 800;
+const THIRST_CRITICAL 1000;
+const THIRST_DEATH 1200;
+
+const THIRST_MINOR_MSG "Your mouth feels so dry...";
+const THIRST_MAJOR_MSG "You are THIRSTY!";
+const THIRST_CRITICAL_MSG "Even your eyes feel dry...";
+const THIRST_DEATH_MSG "You have died of dehydration.";
+
+const THIRST_MALE_MEMORIAL_MSG "Died of thirst.";
+const THIRST_FEMALE_MEMORIAL_MSG "Died of thirst.";
+
+const FATIGUE_FREQUENCY 50;
+const FATIGUE_TRIVIAL 383;
+const FATIGUE_MINOR 575;
+const FATIGUE_MAJOR 600;
+const FATIGUE_CRITICAL 700;
+const FATIGUE_DIRE 800;
+const FATIGUE_SLEEP 1000;
+
+const FATIGUE_TRIVIAL_MSG "*yawn* You should really get some sleep.";
+const FATIGUE_MINOR_MSG "How much longer until bedtime?";
+const FATIGUE_MAJOR_MSG "You're too tired to stop yawning.";
+const FATIGUE_CRITICAL_MSG "You feel like you haven't slept in days.";
+const FATIGUE_DIRE_MSG "Anywhere would be a good place to sleep...";
+const FATIGUE_SLEEP_MSG "Survivor sleep now.";
+
+const FATIGUE_MALE_MEMORIAL_MSG "Succumbed to lack of sleep";
+const FATIGUE_FEMALE_MEMORIAL_MSG "Succumbed to lack of sleep";
 
 #endif
