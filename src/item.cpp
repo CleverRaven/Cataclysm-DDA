e512cc662b62e5fb66efa579560b5c9700182664
Night-Pryanik <Night_Pryanik@mail.ru>
2015-03-13 23:50:59 +0300

    Update item.cpp
    This adds description for every footwear with "BOOTS" flag.

--- src/item.cpp
+++ src/item.cpp
@@ -1304,6 +1304,11 @@ std::string item::info(bool showtext, std::vector<iteminfo> *dump, bool debug) c
             dump->push_back(iteminfo("DESCRIPTION",
                 _("This gear has an alarm clock feature.")));
         }
+        if (is_armor() && has_flag("BOOTS")) {
+            dump->push_back(iteminfo("DESCRIPTION", "--"));
+            dump->push_back(iteminfo("DESCRIPTION",
+                _("You can store knives in this gear.")));
+        }
         if (is_armor() && has_flag("FANCY")) {
             dump->push_back(iteminfo("DESCRIPTION", "--"));
             dump->push_back(iteminfo("DESCRIPTION",
