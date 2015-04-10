8035b2c11db4f32171394a205f4032741cce9f79
Thomas Kirsten <tho_ki@gmx.de>
2015-04-10 15:56:21 +0200

    Export definition of nearby fire item to json.

--- lang/extract_json_strings.py
+++ lang/extract_json_strings.py
@@ -493,7 +493,6 @@ def add_fake_types():
     outfile = os.path.join(to_dir, "faketypes.py")
 
     # fake item types
-    writestr(outfile, "nearby fire")
     writestr(outfile, "cvd machine")
     writestr(outfile, "integrated toolset")
     writestr(outfile, "a smoking device and a source of flame")
--- src/itypedef.cpp
+++ src/itypedef.cpp
@@ -103,12 +103,6 @@ void Item_factory::init_old()
         new itype("null", 0, "none", "none", "", '#', c_white, no_materials, PNULL,
                   0, 0, 0, 0, 0) );
     itypes["null"]->item_tags.insert( "PSEUDO" );
-    // Fire - only appears in crafting recipes
-    add_item_type(
-        new itype("fire", 0, "nearby fire", "none",
-                  "Some fire - if you are reading this it's a bug! (itypdef:fire)",
-                  '$', c_red, no_materials, PNULL, 0, 0, 0, 0, 0) );
-    itypes["fire"]->item_tags.insert( "PSEUDO" );
     // Integrated toolset - ditto
     add_item_type(
         new itype("toolset", 0, "integrated toolset", "none",
