cb95c6274d3d1a15ce876c5e0421d7bdbb2e6e09
brezerk <brezerk@gmail.com>
2015-04-24 11:32:48 +0300

    Add GIT_BINARY option; Fix options layout a bit;

--- src/version.cmake
+++ src/version.cmake
@@ -1,8 +1,12 @@
-FIND_PACKAGE(Git)
+IF(GIT_EXECUTABLE)
+	EXECUTE_PROCESS(
+		COMMAND ${GIT_EXECUTABLE} describe --tags --always --dirty --match "[0-9]*.[0-9]*"
+		OUTPUT_VARIABLE VERSION
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+ELSE(GIT_EXECUTABLE)
+	MESSAGE(WARNING "Git binary not found. Build version will be set to NULL. Install Git package or use -DGIT_BINARY to set path to git binary.")
+	SET (VERSION "NULL")
+ENDIF(GIT_EXECUTABLE)
 
-EXECUTE_PROCESS(
-	COMMAND ${GIT_EXECUTABLE} describe --tags --always --dirty --match "[0-9]*.[0-9]*"
-	OUTPUT_VARIABLE VERSION
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
 CONFIGURE_FILE(${SRC} ${DST} @ONLY)
