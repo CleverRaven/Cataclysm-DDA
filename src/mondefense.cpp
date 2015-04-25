77238d91c2710445cf7e2306a4df0220cd91f219
PropaneSoup <misterrocket550@gmail.com>
2015-04-25 16:50:16 -0400

    Glowing zombie, and tweaks

--- src/mondefense.cpp
+++ src/mondefense.cpp
@@ -46,25 +46,26 @@ void mdefense::zapback(monster *const m, Creature *const source, projectile cons
 void mdefense::acidsplash(monster *const m, Creature *const source, projectile const* const proj)
 {
     // Not a melee attack, attacker lucked out or out of range
-    if( (proj) || (rng(0, 100) > m->def_chance) || (rl_dist(m->pos(), source->pos()) > 1) ) {
+    if( (proj) || (rng(0, 100) > m->def_chance) || (rl_dist(m->pos(), source->pos()) > 1) || one_in(2) ) {
         return;
     }
 
     body_part hit1 = random_body_part();
     body_part hit2 = random_body_part();
-    damage_instance const acid {DT_ACID, static_cast<float>(rng(1, 5))};
+    damage_instance const acid {DT_ACID, static_cast<float>(rng(1, 3))};
     source->deal_damage(m, hit1, acid);
     source->deal_damage(m, hit2, acid);
 
 if (one_in(2)){
         for (int i = 0; i < rng(2,4); i++) {
         g->m.add_field(m->posx() + rng(-1,1), m->posy() + rng(-1, 1), fd_acid, rng(2,3));
-         if( g->u.sees(source->pos()) ) {
-        auto const msg_type = (source == &g->u) ? m_bad : m_info;
-        add_msg(msg_type, _("Acid flies out of the %s as %s hit it!"),
-            m->name().c_str(), source->disp_name().c_str());
         }
+    if( g->u.sees(source->pos()) ) {
+    auto const msg_type = (source == &g->u) ? m_bad : m_info;
+    add_msg(msg_type, _("Acid flies out of the %s as %s hit it!"),
+        m->name().c_str(), source->disp_name().c_str());
     }
+
 }
 
     if( g->u.sees(source->pos()) ) {
