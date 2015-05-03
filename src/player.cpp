9ff99788b88b9d4f029ee5a21f9a520675fd771f
illi-kun <D.E.Serov@gmail.com>
2015-05-03 19:53:39 +0300

    abs -> std::abs

--- src/player.cpp
+++ src/player.cpp
@@ -4320,7 +4320,7 @@ void player::pause()
         veh = v.v;
         if (veh && veh->velocity != 0 && veh->player_in_control(*this)) {
             if (one_in(8)) {
-                double exp_temp = 1 + veh->total_mass() / 400.0 + abs (veh->velocity / 3200.0);
+                double exp_temp = 1 + veh->total_mass() / 400.0 + std::abs (veh->velocity / 3200.0);
                 int experience = static_cast<int>( floorf( exp_temp ) );
                 if( exp_temp - experience > 0 && x_in_y( exp_temp - experience, 1.0 ) ) {
                     experience++;
