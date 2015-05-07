de40e52b9c868f702432080e53533c4ecf574d98
BevapDin <tho_ki@gmx.de>
2015-05-07 03:51:03 +0200

    Remove pointless usage of c_str

--- src/game.cpp
+++ src/game.cpp
@@ -12440,7 +12440,7 @@ void game::fling_creature(Creature *c, const int &dir, float flvel, bool control
             slam = true;
             int vpart;
             vehicle *veh = m.veh_at( pt, vpart );
-            dname = veh ? veh->part_info(vpart).name : m.tername( pt ).c_str();
+            dname = veh ? veh->part_info(vpart).name : m.tername( pt );
             if (m.is_bashable( pt )) {
                 // Only go through if we successfully destroy what we hit
                 thru = m.bash( pt, flvel ).second;
