67ef2ae21308855ff780fda0eea6cf777115cbf6
PropaneSoup <misterrocket550@gmail.com>
2015-05-09 00:29:42 -0400

    use target->

--- src/monattack.cpp
+++ src/monattack.cpp
@@ -593,11 +593,11 @@ void mattack::boomer_glow(monster *z, int index)
     }
     if( !target->uncanny_dodge() ) {
         if (rng(0, 10) > target->get_dodge() || one_in( target->get_dodge() ) ) {
-            g->u.add_env_effect("boomered", bp_eyes, 5, 25);
+            target->add_env_effect("boomered", bp_eyes, 5, 25);
             for (int i = 0; i < rng(2,4); i++){
                 body_part bp = random_body_part();
-                g->u.add_env_effect("glowing", bp, 4, 40);
-                if (foe->has_effect("glowing")){
+                target->add_env_effect("glowing", bp, 4, 40);
+                if (target->has_effect("glowing")){
                     break;
                 }
             }
