577f9e0280d1463dbd81a8c45d8971c67b40bd00
Coolthulhu <Coolthulhu@gmail.com>
2015-05-22 17:38:11 +0200

    Remove nulter

--- src/map.cpp
+++ src/map.cpp
@@ -41,7 +41,6 @@ extern bool is_valid_in_w_terrain(int,int);
 #define dbg(x) DebugLog((DebugLevel)(x),D_MAP) << __FILE__ << ":" << __LINE__ << ": "
 
 static std::list<item>  nulitems;          // Returned when &i_at() is asked for an OOB value
-static ter_id           nulter;            // Returned when &ter() is asked for an OOB value
 static field            nulfield;          // Returned when &field_at() is asked for an OOB value
 static int              null_temperature;  // Because radiation does it too
 static level_cache      nullcache;         // Dummy cache for z-levels outside bounds
@@ -127,7 +126,6 @@ item &map_stack::operator[]( size_t index )
 
 map::map( int mapsize, bool zlev )
 {
-    nulter = t_null;
     my_MAPSIZE = mapsize;
     zlevels = zlev;
     if( zlevels ) {
