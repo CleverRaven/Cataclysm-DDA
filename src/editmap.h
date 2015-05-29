2b24c031e83c94aaafa3a39dc2bca72ccaee31e2
BevapDin <tho_ki@gmx.de>
2015-05-29 16:58:31 +0200

    Move the editmap constructor and destructor into the cpp file.

--- src/editmap.h
+++ src/editmap.h
@@ -113,65 +113,9 @@ class editmap
         int tmaxy;
         bool uberdraw;
         std::map<oter_id, int> oter_special;
-        editmap()
-        {
-            width = TERMX - TERRAIN_WINDOW_TERM_WIDTH;
-            height = TERMY;
-            infoHeight = 0;
-            sel_ter = -1;
-            target_ter = -1;
-            sel_frn = -1;
-            target_frn = -1;
-            ter_frn_mode = 0;
-            cur_field = 0;
-            cur_trap = tr_null;
-            sel_field = -1;
-            sel_fdensity = -1;
-            sel_trap = -1;
 
-            fsel = -1;
-            fset = -1;
-            trsel = -1;
-            trset = -1;
-            w_info = 0;
-            w_help = 0;
-            padding = std::string( width - 2, ' ' );
-            blink = false;
-            altblink = false;
-            moveall = false;
-            editshape = editmap_rect;
-            refresh_mplans = true;
-
-            tmaxx = getmaxx( g->w_terrain );
-            tmaxy = getmaxy( g->w_terrain );
-            fids[0] = "-clear-";
-            fids[fd_fire_vent] = "fire_vent";
-            fids[fd_push_items] = "push_items";
-            fids[fd_shock_vent] = "shock_vent";
-            fids[fd_acid_vent] = "acid_vent";
-            target_list.clear();
-            hilights.clear();
-            hilights["mplan"].blink_interval.push_back( true );
-            hilights["mplan"].blink_interval.push_back( false );
-            hilights["mplan"].cur_blink = 0;
-            hilights["mplan"].color = c_red;
-            hilights["mplan"].setup();
-
-            hilights["mapgentgt"].blink_interval.push_back( true );
-            hilights["mapgentgt"].blink_interval.push_back( false );
-            hilights["mapgentgt"].blink_interval.push_back( false );
-            hilights["mapgentgt"].cur_blink = 0;
-            hilights["mapgentgt"].color = c_cyan;
-            hilights["mapgentgt"].setup();
-
-            oter_special.clear();
-            uberdraw = false;
-        };
-        ~editmap()
-        {
-            delwin( w_info );
-            delwin( w_help );
-        }
+        editmap();
+        ~editmap();
 };
 
 #endif
