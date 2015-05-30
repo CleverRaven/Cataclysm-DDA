4429cebbcc47dfe358f38ca87d44e0f3936816be
Tomas Volka <tomas.volka@gmail.com>
2015-05-30 02:29:15 +0200

    Comment fixes.

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -5633,9 +5633,10 @@ void iuse::play_music( player * const p, const tripoint &source, int const volum
             sound = music.sound;
             // music source is on player's square
             if( p->pos() == source && volume != 0 ) {
+                // generic stereo players
                 sound = string_format( _("You listen to %s"), music.sound.c_str() );
             } else if ( p->pos() == source && volume == 0 && !p->is_deaf() ) {
-                // able to listen, if not deaf
+                // mp3 player, that we are able to hear it, if not deaf
                 p->add_msg_if_player( _( "You listen to %s"), music.sound.c_str() );
             }
         }
