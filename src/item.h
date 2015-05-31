afeaa06a39480152e01583af67c091f4570c47f0
BevapDin <tho_ki@gmx.de>
2015-05-31 17:40:18 +0200

    Document the undocumented item flags.

--- src/item.h
+++ src/item.h
@@ -656,6 +656,9 @@ public:
         /**
          * @name Item flags
          *
+         * If you use any new flags, add a comment to doc/JSON_FLAGS.md and make sure your new
+         * flag does not conflict with any existing flag.
+         *
          * Item flags are taken from the item type (@ref itype::item_tags), but also from the
          * item itself (@ref item_tags). The item has the flag if it appears in either set.
          *
