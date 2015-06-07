d215bf041cbaac9849054110cfbe108140de03ae
Kevin Granade <kevin.granade@gmail.com>
2015-06-07 13:48:07 -0500

    Allow seeing light sources when your vision is impaired.

--- src/map.cpp
+++ src/map.cpp
@@ -5062,12 +5062,17 @@ lit_level map::apparent_light_at( const tripoint &p, const visibility_variables 
     if( dist <= cache.u_clairvoyance ) {
         return LL_BRIGHT;
     }
-    // Several vision impairments cut vision range to a hard limit.
-    if( dist > g->u.unimpaired_range() ) {
-        return LL_DARK;
-    }
     const auto &map_cache = get_cache_ref(p.z);
     const float apparent_light = map_cache.seen_cache[p.x][p.y] * map_cache.lm[p.x][p.y];
+    // Unimpaired range is an override to strictly limit vision range based on various conditions,
+    // but the player can still see light sources.
+    if( dist > g->u.unimpaired_range() ) {
+        if( map_cache.sm[p.x][p.y] > 0.0 ) {
+            return LL_BRIGHT_ONLY;
+        } else {
+            return LL_DARK;
+        }
+    }
     if( map_cache.seen_cache[p.x][p.y] <= LIGHT_TRANSPARENCY_SOLID + 0.1 ) {
         if( apparent_light > LIGHT_AMBIENT_LIT ) {
             if( apparent_light > cache.g_light_level ) {
