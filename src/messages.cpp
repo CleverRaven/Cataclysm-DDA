fcb3cc9a28775121be9056964e9103c082238ed6
Piotr S. Staszewski <p.staszewski@gmail.com>
2015-07-10 16:19:43 +0200

    Sidebar log with newest messages at the top

--- src/messages.cpp
+++ src/messages.cpp
@@ -293,21 +293,21 @@ void Messages::display_messages(WINDOW *const ipk_target, int const left, int co
     }
     
     int const maxlength = right - left;
-    int line = bottom;
+    int line = top;
 
     for (int i = size() - 1; i >= 0; --i) {
-        if (line < top) {
+        if (line > bottom) {
             break;
         }
 
         const game_message &m = player_messages.impl_->messages[i];
         const nc_color col = m.get_color(player_messages.impl_->curmes);
 
-        const auto folded_strings = foldstring(m.get_with_count(), maxlength);
-        const auto folded_rend = folded_strings.rend();
-        for( auto string_iter = folded_strings.rbegin();
-             string_iter != folded_rend && line >= top; ++string_iter, line-- ) {
-            mvwprintz(ipk_target, line, left, col, "%s", string_iter->c_str());
+        for( const std::string &folded : foldstring(m.get_with_count(), maxlength) ) {
+            if (line > bottom) {
+                break;
+            }
+            mvwprintz(ipk_target, line++, left, col, "%s", folded.c_str());
         }
     }
 
