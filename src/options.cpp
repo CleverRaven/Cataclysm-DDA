b925613c1f8332cd15450e5e043c95c3b70ac628
Kevin Granade <kevin.granade@gmail.com>
2015-07-11 02:17:59 +0000

    Leave scrolling messages upwards as the default.

--- src/options.cpp
+++ src/options.cpp
@@ -712,7 +712,7 @@ void initOptions()
     optionNames["new_bottom"] = _("Bottom");
     OPTIONS["SIDEBAR_LOG_FLOW"] = cOpt("interface", _("Sidebar log flow"),
                                        _("Where new sidebar log messages should show. Requires restart."),
-                                       "new_top,new_bottom", "new_top"
+                                       "new_top,new_bottom", "new_bottom"
                                       );
 
     //~ style of vehicle interaction menu; vertical is old one.
