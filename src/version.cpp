6ef3034a47a782674e3e61c4d90a144a673b389d
Karl Parakenings <karl.parakenings@gmail.com>
2015-07-14 23:47:42 -0700

    Added CMake injection into version fallback to play nice with other platforms.

--- src/version.cpp
+++ src/version.cpp
@@ -1,4 +1,14 @@
+#if (defined _WIN32 || defined WINDOWS || defined MINGW) && ! defined GIT_VERSION && ! defined CROSS_LINUX && !defined _MSC_VER
+
+#ifndef VERSION
+#define VERSION "0.C"
+#endif
+
+#else
+
 #include "version.h"
+
+#endif
 
 #include "get_version.h"
 
