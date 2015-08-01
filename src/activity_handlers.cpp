c388386d069409f85bf21a2f17cc004b540e78c4
BevapDin <tho_ki@gmx.de>
2015-08-01 11:41:58 +0200

    Only make a reload-sound when the reloading causes noise,
    which is already handled, the removed code would add a sound even if the reloading is quiet.

--- src/activity_handlers.cpp
+++ src/activity_handlers.cpp
@@ -922,7 +922,6 @@ void activity_handlers::reload_finish( player_activity *act, player *p )
               sfx::play_variant_sound( "reload", reloadable->typeId(), sfx::get_heard_volume(p->pos()));
               sounds::ambient_sound( p->pos(), gun->reload_noise_volume, gun->reload_noise );
             }
-            sfx::play_variant_sound( "reload", reloadable->typeId(), sfx::get_heard_volume(p->pos()));
         }
     } else {
         add_msg(m_info, _("Can't reload your %s."), reloadable->tname().c_str());
