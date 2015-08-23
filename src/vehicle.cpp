4e746911d2585a7d55b2bccc70a4c1b06463872b
jaked122 <jaked122@gmail.com>
2015-08-23 15:03:54 -0400

    changed total power calculation for vehicle to include plows

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -3100,6 +3100,9 @@ int vehicle::total_power(bool const fueled) const
             pwr += part_power(p); // alternators have negative power
         }
     }
+    for( size_t a : all_parts_with_feature("PLOW") ){
+        pwr += part_power(a);
+    }
     if (cnt > 1) {
         pwr = pwr * 4 / (4 + cnt -1);
     }
@@ -3523,7 +3526,6 @@ void vehicle::power_parts()
     if( dome_lights_on ) epower += dome_lights_epower;
     if( aisle_lights_on ) epower += aisle_lights_epower;
     if( scoop_on ) epower += scoop_epower;
-    if( plow_on ) epower += plow_epower;
     // Engines: can both produce (plasma) or consume (gas, diesel)
     // Gas engines require epower to run for ignition system, ECU, etc.
     int engine_epower = 0;
