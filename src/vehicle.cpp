c322b63fdb4821bfd4eb37247faa0cc81afdee29
jaked122 <jaked122@gmail.com>
2015-08-23 15:13:46 -0400

    fixed plow_epower still being used

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -4881,7 +4881,6 @@ void vehicle::refresh()
     aisle_lights_epower = 0;
     alternator_load = 0;
     camera_epower = 0;
-    plow_epower = 0;
     plow_friction = 0;
     has_atomic_lights = false;
     // Used to sort part list so it displays properly when examining
@@ -4955,10 +4954,10 @@ void vehicle::refresh()
         if( vpi.has_flag( "ATOMIC_LIGHT" ) ) {
             has_atomic_lights = true;
         }
-        if( vpi.has_flag( "PLOW" ) ) {
+        if(vpi.has_flag( "PLOW" )){
             plow_friction += vpi.bonus;
-            plow_epower += vpi.epower;
         }
+
         // Build map of point -> all parts in that point
         const point pt = parts[p].mount;
         // This will keep the parts at point pt sorted
