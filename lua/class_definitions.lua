49c5f42e494aaffae96ecbaf0d3a63e4fcc0c8f9
Christian Buskirk <i2amroy@gmail.com>
2015-08-25 20:05:56 -0700

    Potential Lua fix

--- lua/class_definitions.lua
+++ lua/class_definitions.lua
@@ -82,9 +82,9 @@ classes = {
             male = { type = "bool", writable = true },
             name = { type = "string", writable = true },
             nv_cached = { type = "bool", writable = true },
-            hunger = { type = "int", writable = true },
-            stomach_food = { type = "int", writable = true },
-            stomach_water = { type = "int", writable = true },
+            hunger = { type = "int", writable = false },
+            stomach_food = { type = "int", writable = false },
+            stomach_water = { type = "int", writable = false },
         },
         functions = {
             { name = "add_effect", rval = nil, args = { "string", "int" } },
