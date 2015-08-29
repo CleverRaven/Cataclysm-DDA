828884b24088eaa7cc9908ea7ca328f573f58870
BevapDin <tho_ki@gmx.de>
2015-08-29 12:12:54 +0200

    Fix z-component being used twice:
    `get_levz` returns the same as abs_sub.z, which is the (absolute) z-level of the player/the reality bubble. If the player is one level below ground, this would add up to -2, which is *not* the z-level that should be marked as seen.

--- src/map.cpp
+++ src/map.cpp
@@ -5360,7 +5360,7 @@ void map::update_visibility_cache( visibility_variables &cache, const int zlev )
     for( int gridx = 0; gridx < my_MAPSIZE; gridx++ ) {
         for( int gridy = 0; gridy < my_MAPSIZE; gridy++ ) {
             if( sm_squares_seen[gridx][gridy] > 36 ) { // 25% of the submap is visible
-                const tripoint sm( gridx, gridy, g->get_levz() );
+                const tripoint sm( gridx, gridy, 0 );
                 const auto abs_sm = map::abs_sub + sm;
                 const auto abs_omt = overmapbuffer::sm_to_omt_copy( abs_sm );
                 overmap_buffer.set_seen( abs_omt.x, abs_omt.y, abs_omt.z, true);
