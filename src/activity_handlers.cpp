fa1a24211f70b770085175e747b0a64c54a41676
jcd000 <ikontochris@gmail.com>
2015-08-31 22:45:33 +0300

    This makes the opening of a gate an activity, so that it will be interruptable when danger looms. The side-effect is that any monster *beyond* the gate will only see the player after the gate is opened, not during. This is because only when the activity ends, the gate is opened. There may be a way to change this to the old behaviour if: 1. The gate is opened immediately (or after, say, half the time needed to really open it) 2. A cancellation of the activity results in the gate automatically closing
    I am against this route however.

--- src/activity_handlers.cpp
+++ src/activity_handlers.cpp
@@ -1124,3 +1124,109 @@ void activity_handlers::cracking_finish( player_activity *act, player *p )
     p->add_msg_if_player( m_good, _("The safe opens!"));
     g->m.furn_set( act->placement, f_safe_o);
 }
+
+void activity_handlers::open_gate_finish( player_activity *act, player *p )
+{
+    const tripoint &pos = act->placement;
+    const ter_id handle_type = g->m.ter( pos );
+    int examx = pos.x;
+    int examy = pos.y;
+    ter_id wall_type;
+    ter_id door_type;
+    ter_id floor_type;
+    const char *open_message;
+    const char *close_message;
+    int bash_dmg;
+
+    if (handle_type == t_gates_mech_control) {
+        wall_type = t_wall;
+        door_type = t_door_metal_locked;
+        floor_type = t_floor;
+        open_message = _("The gate is opened!");
+        close_message = _("The gate is closed!");
+        bash_dmg = 40;
+    } else if (handle_type == t_gates_control_concrete) {
+        wall_type = t_concrete_wall;
+        door_type = t_door_metal_locked;
+        floor_type = t_floor;
+        open_message = _("The gate is opened!");
+        close_message = _("The gate is closed!");
+        bash_dmg = 40;
+    } else if (handle_type == t_barndoor) {
+        wall_type = t_wall_wood;
+        door_type = t_door_metal_locked;
+        floor_type = t_dirtfloor;
+        open_message = _("The barn doors opened!");
+        close_message = _("The barn doors closed!");
+        bash_dmg = 40;
+    } else if (handle_type == t_palisade_pulley) {
+        wall_type = t_palisade;
+        door_type = t_palisade_gate;
+        floor_type = t_palisade_gate_o;
+        open_message = _("The palisade gate swings open!");
+        close_message = _("The palisade gate swings closed with a crash!");
+        bash_dmg = 30;
+    } else if (handle_type == t_gates_control_metal) {
+        wall_type = t_wall_metal;
+        door_type = t_door_metal_locked;
+        floor_type = t_metal_floor;
+        open_message = _("The door rises!");
+        close_message = _("The door slams shut!");
+        bash_dmg = 60;
+    } else {
+        return;
+    }
+    
+    bool open = false;
+    bool close = false;
+
+    for (int wall_x = -1; wall_x <= 1; wall_x++) {
+        for (int wall_y = -1; wall_y <= 1; wall_y++) {
+            for (int gate_x = -1; gate_x <= 1; gate_x++) {
+                for (int gate_y = -1; gate_y <= 1; gate_y++) {
+                    if ((wall_x + wall_y == 1 || wall_x + wall_y == -1) &&
+                        // make sure wall not diagonally opposite to handle
+                        (gate_x + gate_y == 1 || gate_x + gate_y == -1) &&  // same for gate direction
+                        ((wall_y != 0 && (g->m.ter(examx + wall_x, examy + wall_y) == wall_type)) ||
+                         //horizontal orientation of the gate
+                         (wall_x != 0 &&
+                          (g->m.ter(examx + wall_x, examy + wall_y) == wall_type)))) { //vertical orientation of the gate
+
+                        int cur_x = examx + wall_x + gate_x;
+                        int cur_y = examy + wall_y + gate_y;
+
+                        if (!close &&
+                            (g->m.ter(examx + wall_x + gate_x, examy + wall_y + gate_y) == door_type)) {  //opening the gate...
+                            open = true;
+                            while (g->m.ter(cur_x, cur_y) == door_type) {
+                                g->m.ter_set(cur_x, cur_y, floor_type);
+                                cur_x = cur_x + gate_x;
+                                cur_y = cur_y + gate_y;
+                            }
+                        }
+
+                        if (!open &&
+                            (g->m.ter(examx + wall_x + gate_x, examy + wall_y + gate_y) == floor_type)) {  //closing the gate...
+                            close = true;
+                            while (g->m.ter(cur_x, cur_y) == floor_type) {
+                                g->forced_gate_closing( tripoint( cur_x, cur_y, pos.z ), door_type, bash_dmg );
+                                cur_x = cur_x + gate_x;
+                                cur_y = cur_y + gate_y;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if (open) {
+        p->add_msg_if_player(open_message);
+    } else if (close) {
+        p->add_msg_if_player(close_message);
+    } else {
+        p->add_msg_if_player(_("Nothing happens."));
+    }
+}
+
+
