96443fb90c19af49ecd90a0fdfbfa2624026824b
Brandon Kime <rbrandonkime@gmail.com>
2015-09-15 10:29:24 -0400

    Nerf marloss tree chance from fungal haze
    Like with the blossoms, the numbers will probably need tweaking to get
    just right. This is still a very large amount of marloss in the right
    conditions.

--- src/game.cpp
+++ src/game.cpp
@@ -13900,7 +13900,7 @@ bool game::spread_fungus( const tripoint &p )
                                     if (u.sees(p)) {
                                         add_msg(m_warning, _("The young tree blooms forth into a fungal blossom!"));
                                     }
-                                } else if (one_in(2)) {
+                                } else if (one_in(4)) {
                                     m.ter_set(dest, t_marloss_tree);
                                 }
                             } else {
@@ -13917,7 +13917,7 @@ bool game::spread_fungus( const tripoint &p )
                                     if (u.sees(p)) {
                                         add_msg(m_warning, _("The tree blooms forth into a fungal blossom!"));
                                     }
-                                } else if (one_in(3)) {
+                                } else if (one_in(6)) {
                                     m.ter_set(dest, t_marloss_tree);
                                 }
                             } else {
