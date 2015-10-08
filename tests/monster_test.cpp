93046ea589ac5b2c3a7885154705ae95e4fe39c0
Kevin Granade <kevin.granade@gmail.com>
2015-10-08 20:54:20 +0000

    Experimental testing changes.

--- tests/monster_test.cpp
+++ tests/monster_test.cpp
@@ -9,6 +9,8 @@
 #include "mtype.h"
 #include "options.h"
 
+#include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -39,6 +41,7 @@ monster &spawn_test_monster( const std::string &monster_type, const tripoint &st
 int moves_to_destination( const std::string &monster_type,
                           const tripoint &start, const tripoint &end )
 {
+    REQUIRE( g->num_zombies() == 0 );
     monster &test_monster = spawn_test_monster( monster_type, start );
     // Get it riled up and give it a goal.
     test_monster.anger = 100;
@@ -66,8 +69,9 @@ int moves_to_destination( const std::string &monster_type,
 /**
  * Simulate a player running from the monster, checking if it can catch up.
  **/
-bool can_catch_player( const std::string &monster_type, const tripoint &direction_of_flight )
+int can_catch_player( const std::string &monster_type, const tripoint &direction_of_flight )
 {
+    REQUIRE( g->num_zombies() == 0 );
     player &test_player = g->u;
     // Strip off any potentially encumbering clothing.
     std::vector<item> taken_off_items;
@@ -88,6 +92,13 @@ bool can_catch_player( const std::string &monster_type, const tripoint &directio
     const int target_speed = 100;
 
     int moves_spent = 0;
+    struct track {
+        char participant;
+        int distance;
+        int moves;
+        tripoint location;
+    };
+    std::vector<track> tracker;
     for( int turn = 0; turn < 1000; ++turn ) {
         test_player.mod_moves( target_speed );
         while( test_player.moves >= 0 ) {
@@ -106,47 +117,71 @@ bool can_catch_player( const std::string &monster_type, const tripoint &directio
                     }
                 }
             }
-            test_player.mod_moves( -100 );
+            const int move_cost = g->m.combined_movecost(
+                test_player.pos(), test_player.pos() + direction_of_flight, nullptr, 0 );
+            tracker.push_back({'p', move_cost, rl_dist(test_monster.pos(), test_player.pos()),
+                        test_player.pos()} );
+            test_player.mod_moves( -move_cost );
         }
         test_monster.set_dest( test_player.pos() );
         test_monster.mod_moves( monster_speed );
         while( test_monster.moves >= 0 ) {
             int moves_before = test_monster.moves;
             test_monster.move();
+            tracker.push_back({'m', moves_before - test_monster.moves,
+                        rl_dist(test_monster.pos(), test_player.pos()),
+                        test_monster.pos()} );
             moves_spent += moves_before - test_monster.moves;
             if( rl_dist( test_monster.pos(), test_player.pos() ) == 1 ) {
                 g->remove_zombie( 0 );
-                return true;
-            } else if( rl_dist( test_monster.pos(), test_player.pos() ) > 50 ) {
+                std::stringstream path_string;
+                for( const auto track_inst : tracker ) {
+                    path_string << track_inst.participant << "," << track_inst.moves << "," <<
+                        track_inst.distance << "," << track_inst.location << " ";
+                }
+                WARN(path_string.str());
+                return turn;
+            } else if( rl_dist( test_monster.pos(), test_player.pos() ) > 20 ) {
                 g->remove_zombie( 0 );
-                return false;
+                std::stringstream path_string;
+                for( const auto track_inst : tracker ) {
+                    path_string << track_inst.participant << "," << track_inst.moves << "," <<
+                        track_inst.distance << "," << track_inst.location << " ";
+                }
+                WARN(path_string.str());
+                return -turn;
             }
         }
     }
     g->remove_zombie( 0 );
-    return false;
+    return -1000;
 }
 
 class statistics {
 private:
+    int _types;
     int _n;
     int _sum;
     int _max;
     int _min;
 
 public:
-    statistics() : _n(0), _sum(0), _max(INT_MIN), _min(INT_MAX) {}
-    
+    statistics() : _types(0), _n(0), _sum(0), _max(INT_MIN), _min(INT_MAX) {}
+
+    void new_type() {
+        _types++;
+    }
     void add( int new_val ) {
         _n++;
         _sum += new_val;
         _max = std::max(_max, new_val);
         _min = std::min(_min, new_val);
     }
-    int sum() { return _sum; }
-    int max() { return _max; }
-    int min() { return _min; }
-    int avg() { return _sum / _n; }
+    int types() const { return _types; }
+    int sum() const { return _sum; }
+    int max() const { return _max; }
+    int min() const { return _min; }
+    float avg() const { return (float)_sum / (float)_n; }
 };
 
 // Verify that the named monster has the expected effective speed, not reduced
@@ -166,6 +201,9 @@ void check_shamble_speed( const std::string monster_type, const tripoint &destin
             break;
         }
     }
+    CAPTURE(slope);
+    CAPTURE(move_stats.avg());
+    INFO(diagonal_multiplier);
     CHECK( (move_stats.avg() / (10000.0 * diagonal_multiplier)) ==
            Approx(1.0).epsilon(0.02) );
 }
@@ -203,11 +241,61 @@ void monster_check() {
         check_shamble_speed( "mon_zombie", {x, 100, 0} );
     }
 
+    std::map<int, statistics> turns_at_distance;
+    std::map<float, statistics> turns_at_angle;
+    for( int x = 0; x <= 100; x += 2 ) {
+        for( int y = 0; y <= 100; y += 2 ) {
+            const int distance = rl_dist({50,50,0}, {x,y,0});
+                // Scale the scaling factor based on the ratio of diagonal to cardinal steps.
+            const float norm_slope = get_normalized_angle( {50, 50}, {x, y} );
+            const float diagonal_multiplier = 1.0 + (OPTIONS["CIRCLEDIST"] ? (norm_slope * 0.41) : 0.0);
+            if( distance < 5 ) {
+                // Very short ranged tests are squirrely.
+                continue;
+            }
+            const int rise = 50 - y;
+            const int run = 50 - x;
+            const float slope = atan2( run, rise );
+            turns_at_angle[slope].new_type();
+            for( int i = 0; i < 500; ++i ) {
+                int moves = moves_to_destination( "mon_zombie_dog", {50, 50, 0}, {x, y, 0} );
+                turns_at_distance[distance].add( (moves * diagonal_multiplier) / distance );
+                turns_at_angle[slope].add( (moves * diagonal_multiplier) / distance);
+                if( turns_at_angle[slope].avg() == Approx(100.0).epsilon(0.04) ) {
+                    break;
+                }
+            }
+        }
+    }
+    std::stringstream slope_map;
+    for( const auto &stat_pair : turns_at_distance ) {
+        INFO( "Dist: " << stat_pair.first << " moves: " << stat_pair.second.avg() );
+        CHECK( stat_pair.second.avg() == Approx(100.0).epsilon(0.02) );
+    }
+    for( const auto &stat_pair : turns_at_angle ) {
+        WARN( "Slope: " << stat_pair.first << " moves: " << stat_pair.second.avg() <<
+              " types: " << stat_pair.second.types() );
+        slope_map << stat_pair.first << " " << stat_pair.second.avg() << "\n" ;
+        CHECK( stat_pair.second.avg() == Approx(100.0).epsilon(0.04) );
+    }
+    std::ofstream data;
+    data.open("slope_test_data");
+    data << slope_map.str();
+    data.close();
+    
     INFO( "Trigdist is " << ( OPTIONS["CIRCLEDIST"] ? "on" : "off" ) );
-    CHECK_FALSE( can_catch_player( "mon_zombie", {1,0,0} ) );
-    CHECK_FALSE( can_catch_player( "mon_zombie", {1,1,0} ) );
-    CHECK( can_catch_player( "mon_zombie_dog", {1,0,0} ) );
-    CHECK( can_catch_player( "mon_zombie_dog", {1,1,0} ) );
+    int zombie_horizontal_capture_speed = can_catch_player( "mon_zombie", {1,0,0} );
+    WARN( zombie_horizontal_capture_speed );
+    CHECK( zombie_horizontal_capture_speed < 0 );
+    int zombie_vertical_capture_speed = can_catch_player( "mon_zombie", {1,1,0} );
+    WARN( zombie_vertical_capture_speed );
+    CHECK( zombie_vertical_capture_speed < 0 );
+    int zombie_dog_horizontal_capture_speed = can_catch_player( "mon_zombie_dog", {1,0,0} );
+    WARN( zombie_dog_horizontal_capture_speed );
+    CHECK( zombie_dog_horizontal_capture_speed > 0 );
+    int zombie_dog_vertical_capture_speed = can_catch_player( "mon_zombie_dog", {1,1,0} );
+    WARN( zombie_dog_vertical_capture_speed );
+    CHECK( zombie_dog_vertical_capture_speed > 0 );
 }
 
 // Characterization test for monster movement speed.
