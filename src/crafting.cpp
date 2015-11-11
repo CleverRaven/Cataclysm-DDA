d4de282ec86bde3be26f9735e4afed896fa3b90a
JornVernee <jbvernee@xs4all.nl>
2015-11-11 20:05:27 +0100

    Revert "Changed recipe_dictionary::clear to avoid concurrent modifications while deleting recipes."
    This reverts commit 63bc0823c2e9f7e8e30db2cca914d129d00d70b9.

--- src/crafting.cpp
+++ src/crafting.cpp
@@ -99,9 +99,14 @@ void recipe_dictionary::remove_from_component_lookup( recipe* r )
 
 void recipe_dictionary::clear()
 {
-    while( !recipes.empty() ) {
-        delete recipes.front();
+    by_component.clear();
+    by_name.clear();
+    by_index.clear();
+    by_category.clear();
+    for( auto &recipe : recipes ) {
+        delete recipe;
     }
+    recipes.clear();
 }
 
 void load_recipe_category(JsonObject &jsobj)
