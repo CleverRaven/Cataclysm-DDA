3d8c88ab48b51c7d098a4399494e9bd8e2dfa34f
JornVernee <jbvernee@xs4all.nl>
2015-11-18 22:15:28 +0100

    Adding files

---
+++ src/lua_console.cpp
@@ -0,0 +1,93 @@
+#include "lua_console.h"
+
+#include "catalua.h"
+#include "catacurse.h"
+
+#include "debug.h"
+#include "catacharset.h"
+
+lua_console::lua_console()
+{
+    cWin = newwin(lines, width, 0, 0);
+    iWin = newwin(1, width,lines, 0);
+}
+
+void lua_console::dispose()
+{
+    werase(cWin);
+    werase(iWin);
+    delwin(cWin);
+    delwin(iWin);
+}
+
+std::string lua_console::get_input()
+{
+    long key = 0;
+    int pos = -1;
+    return string_input_win(iWin, "", width, 0, 0, width, true, key, pos, "LUA", iWin->x, iWin->y, true, false,
+        (std::map<long, Invokable *> {
+            {KEY_ESCAPE, &quit_callback},
+            {KEY_NPAGE, &scroll_up_callback},
+            {KEY_PPAGE, &scroll_down_callback}
+        }));
+}
+
+void lua_console::draw()
+{
+    werase(cWin);
+
+    // Some juggling to make sure text is aligned with the bottom of the console.
+    int stack_size = text_stack.size() - scroll;
+    for( int i = lines; i > lines - stack_size && i >= 0; i--) {
+        auto line = text_stack[stack_size - 1 - (lines - i)];
+        mvwprintz(cWin, i - 1, 0, line.second, line.first.c_str());
+    }
+
+    wrefresh(cWin);
+}
+
+void lua_console::quit()
+{
+    done = true;
+}
+
+void lua_console::scroll_down()
+{
+    scroll = std::min(std::max((int) text_stack.size() - lines, 0), scroll + 1);
+    draw();
+}
+
+void lua_console::scroll_up()
+{
+    scroll = std::max(0, scroll - 1);
+    draw();
+}
+
+void lua_console::run()
+{
+    while( !done ) {
+        draw();
+
+        std::string input = get_input();
+
+#ifdef LUA
+        call_lua( input );
+        std::string line;
+        if( line == "" ) {
+            continue; // no need to parse if we have no input.
+        }
+
+        while( std::getline(lua_output_stream, line) ) {
+            text_stack.push_back( {line, c_white} );
+        }
+        lua_output_stream = std::stringstream(); // empty the buffer
+
+        while( std::getline(lua_error_stream, line) ) {
+            text_stack.push_back( {line, c_red} );
+        }
+        lua_error_stream = std::stringstream(); // empty the buffer
+#else
+        text_stack.push_back( "This build does not support lua." );
+#endif // LUA
+    }
+}
---
+++ src/lua_console.h
@@ -0,0 +1,44 @@
+#ifndef LUA_CONSOLE_H
+#define LUA_CONSOLE_H
+
+#include "output.h"
+#include "input.h"
+
+#include <map>
+#include <string>
+#include <vector>
+#include <utility>
+
+struct WINDOW;
+
+class lua_console {
+    public:
+        lua_console();
+        void dispose();
+        void run();
+    private:
+        const int width = TERMX;
+        const int lines = 10; // 5 lines
+
+        WINDOW *cWin;
+        WINDOW *iWin;
+
+        std::vector<std::pair<std::string, nc_color>> text_stack;
+        std::string get_input();
+        void print( std::string text );
+        void draw();
+
+        bool done = false;
+
+        int scroll = 0;
+
+        void quit();
+        void scroll_up();
+        void scroll_down();
+
+        action_callback<lua_console> quit_callback = action_callback<lua_console>(this, quit);
+        action_callback<lua_console> scroll_up_callback = action_callback<lua_console>(this, scroll_up);
+        action_callback<lua_console> scroll_down_callback = action_callback<lua_console>(this, scroll_down);
+};
+
+#endif // LUA_CONSOLE_H
