a20252f3e234b8dac7608fe51f821329253ae1d5
DanmakuDan <danmakudan@gmail.com>
2015-11-20 21:26:39 -0600

    Fix some issues with how pixel minimap is drawn ingame.

--- src/cata_tiles.h
+++ src/cata_tiles.h
@@ -203,7 +203,7 @@ class cata_tiles
 
         /** Minimap functionality */
         void draw_minimap( int destx, int desty, const tripoint &center, int width, int height);
-        void draw_rhombus( int destx, int desty, int size, SDL_Color color);
+        void draw_rhombus( int destx, int desty, int size, SDL_Color color, int widthLimit, int heightLimit);
     protected:
         /** How many rows and columns of tiles fit into given dimensions **/
         void get_window_tile_counts(const int width, const int height, int &columns, int &rows) const;
