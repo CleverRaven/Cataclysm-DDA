5235cd7d415cc586a23e36ec26a010471ffdc9c9
JornVernee <jbvernee@xs4all.nl>
2015-11-22 18:26:44 +0100

    Added files. - Various code changes. - Added tabbed panel.

--- src/cata_ui.cpp
+++ src/cata_ui.cpp
@@ -5,15 +5,54 @@
 #include <cmath>
 #include <array>
 
-rect::rect( size_t size_x, size_t size_y, unsigned int x, unsigned int y ) : size_x( size_x ), size_y( size_y ), x( x ), y( y )
+rect::rect( size_t size_x, size_t size_y, unsigned int x, unsigned int y ) : x( x ), y( y ), global_x(x), global_y(y), size_x( size_x ), size_y( size_y )
 {
 }
 
-ui_window::ui_window( size_t size_x, size_t size_y ) : ui_window( size_x, size_y, 0, 0 )
+void rect::set_parent(const rect *p_rect)
 {
+    parent = p_rect;
+    if(parent) {
+        global_x = x + parent->x;
+        global_y = y + parent->y;
+    } else {
+        global_x = x;
+        global_y = y;
+    }
 }
 
-ui_window::ui_window( size_t size_x, size_t size_y, unsigned int x, unsigned int y ) : el_rect( rect( size_x, size_y, x, y ) ), win( newwin(size_x, size_y, x, y) )
+unsigned int rect::get_x() const
+{
+    return x;
+}
+
+void rect::set_x(unsigned int new_x)
+{
+    x = new_x;
+    global_x = x + parent->x;
+}
+
+unsigned int rect::get_y() const
+{
+    return y;
+}
+
+void rect::set_y(unsigned int new_y)
+{
+    y = new_y;
+    global_y = y + parent->y;
+}
+
+unsigned int rect::get_global_x() const
+{
+    return global_x;
+}
+unsigned int rect::get_global_y() const
+{
+    return global_y;
+}
+
+ui_window::ui_window( size_t size_x, size_t size_y, unsigned int x, unsigned int y ) : win( newwin(size_x, size_y, x, y) )
 {
 }
 
@@ -36,22 +75,12 @@ void ui_window::draw()
     wrefresh( win );
 }
 
-void ui_window::add_child( ui_element *child )
+template<typename T>
+T *ui_window::add_child( const T &child )
 {
-    if( !child ) {
-        return;
-    }
-
-    children.push_back( child );
-}
-
-void ui_window::add_child( const ui_element &child )
-{
-    add_child( child.clone() );
-}
-
-ui_element::ui_element(size_t size_x, size_t size_y) : ui_element(size_x, size_y, 0, 0)
-{
+    auto child_clone = child.clone();
+    children.push_back( child_clone );
+    return (T *) child_clone;
 }
 
 ui_element::ui_element(size_t size_x, size_t size_y, unsigned int x, unsigned int y) : el_rect(rect(size_x, size_y, x, y))
@@ -68,8 +97,18 @@ bool ui_element::is_visible() const
     return show;
 }
 
-ui_panel::ui_panel(size_t size_x, size_t size_y) : ui_panel(size_x, size_y, 0, 0)
+void ui_element::set_parent(ui_element *parent)
 {
+    if(parent) {
+        el_rect.set_parent(&parent->get_rect());
+    } else {
+        el_rect.set_parent( nullptr );
+    }
+}
+
+const rect &ui_element::get_rect() const
+{
+    return el_rect;
 }
 
 ui_panel::ui_panel(size_t size_x, size_t size_y, unsigned int x, unsigned int y) : ui_element(size_x, size_y, x, y)
@@ -95,24 +134,15 @@ ui_element *ui_panel::clone() const
     return panel_clone;
 }
 
-void ui_panel::add_child( ui_element *child )
+template<typename T>
+T *ui_panel::add_child( const T &child )
 {
-    if( !child ) {
-        return;
-    }
+    auto child_clone = child.clone();
 
-    // offset the child's location by parent location.
-    child->el_rect.x += el_rect.x;
-    child->el_rect.y += el_rect.y;
+    child_clone->set_parent(this);
 
-    // TODO: move/resize child to fit in window.
-
-    children.push_back( child );
-}
-
-void ui_panel::add_child( const ui_element &child )
-{
-    add_child( child.clone() );
+    children.push_back( child_clone );
+    return (T *) child_clone;
 }
 
 void ui_panel::draw( WINDOW *win )
@@ -122,10 +152,6 @@ void ui_panel::draw( WINDOW *win )
             child->draw( win );
         }
     }
-}
-
-ui_label::ui_label( std::string text ) : ui_label( text, 0, 0 )
-{
 }
 
 ui_label::ui_label( std::string text ,unsigned int x, unsigned int y ) : ui_element( utf8_width( text.c_str() ), 1, x, y ), text( text )
@@ -139,17 +165,13 @@ ui_element *ui_label::clone() const
 
 void ui_label::draw( WINDOW *win )
 {
-    mvwprintz( win, el_rect.y, el_rect.x, text_color, text.c_str() );
+    mvwprintz( win, el_rect.get_global_y(), el_rect.get_global_x(), text_color, text.c_str() );
 }
 
 void ui_label::set_text( std::string new_text )
 {
     text = new_text;
     el_rect.size_x = new_text.size();
-}
-
-bordered_panel::bordered_panel(size_t size_x, size_t size_y) : bordered_panel(size_x, size_y, 0, 0)
-{
 }
 
 bordered_panel::bordered_panel(size_t size_x, size_t size_y, unsigned int x, unsigned int y ) : ui_panel(size_x, size_y, x, y)
@@ -171,10 +193,6 @@ void bordered_panel::draw( WINDOW *win )
     ui_panel::draw( win );
 }
 
-health_bar::health_bar(size_t size_x) : health_bar(size_x, 0, 0)
-{
-}
-
 health_bar::health_bar(size_t size_x, unsigned int x, unsigned int y) : ui_element(size_x, 1, x, y),
                        max_health(size_x * points_per_char), current_health(max_health)
 {
@@ -190,7 +208,7 @@ ui_element *health_bar::clone() const
 
 void health_bar::draw( WINDOW *win )
 {
-    mvwprintz( win, el_rect.y, el_rect.x, bar_color, bar_str.c_str() );
+    mvwprintz( win, el_rect.get_global_y(), el_rect.get_global_x(), bar_color, bar_str.c_str() );
 }
 
 void health_bar::refresh_bar( bool overloaded, float percentage )
@@ -241,10 +259,6 @@ void health_bar::set_health_percentage( float percentage )
     refresh_bar( overloaded, percentage );
 }
 
-smiley_indicator::smiley_indicator() : smiley_indicator(0, 0)
-{
-}
-
 smiley_indicator::smiley_indicator(unsigned int x, unsigned int y) : ui_element(2, 1, x, y)
 {
 }
@@ -256,7 +270,7 @@ ui_element *smiley_indicator::clone() const
 
 void smiley_indicator::draw( WINDOW *win )
 {
-    mvwprintz( win, el_rect.y, el_rect.x, smiley_color, smiley_str.c_str() );
+    mvwprintz( win, el_rect.get_global_y(), el_rect.get_global_x(), smiley_color, smiley_str.c_str() );
 }
 
 void smiley_indicator::set_state( smiley_state new_state )
@@ -287,12 +301,8 @@ void smiley_indicator::set_state( smiley_state new_state )
 }
 
 tile_panel::tile_panel(tripoint center, std::function<const char_tile(int, int, int)> tile_at,
-           size_t size_x, size_t size_y) : tile_panel(center, tile_at, size_x, size_y, 0, 0)
-{
-}
-
-tile_panel::tile_panel(tripoint center, std::function<const char_tile(int, int, int)> tile_at,
-           size_t size_x, size_t size_y, unsigned int x, unsigned int y) : ui_element(size_x, size_y, x, y), tile_at(tile_at), center(center)
+                       size_t size_x, size_t size_y, unsigned int x, unsigned int y)
+                       : ui_element(size_x, size_y, x, y), tile_at(tile_at), center(center)
 {
     if(size_x % 2 == 0) {
         el_rect.size_x += 1;
@@ -321,7 +331,7 @@ void tile_panel::draw( WINDOW *win )
 
     for(unsigned int x = start_x; x <= end_x; x++) {
         for(unsigned int y = start_y; y <= end_y; y++) {
-            tile_at(x, y, center.z).draw( win, el_rect.x + x, el_rect.y + y );
+            tile_at(x, y, center.z).draw( win, el_rect.get_global_x() + x, el_rect.get_global_y() + y );
         }
     }
 }
@@ -343,4 +353,88 @@ void char_tile::draw( WINDOW *win, int x, int y ) const
 
 char_tile::char_tile(long sym, nc_color color) : sym(sym), color(color)
 {
+}
+
+tabbed_panel::tabbed_panel(size_t size_x, size_t size_y, unsigned int x, unsigned int y) : bordered_panel(size_x, size_y, x, y)
+{
+}
+
+ui_element *tabbed_panel::clone() const
+{
+    return new tabbed_panel(*this);
+}
+
+int tabbed_panel::draw_tab(const std::string &tab, bool selected, int x_offset, WINDOW *win) const
+{
+    int gy = el_rect.get_global_y();
+    int gx = el_rect.get_global_x() + x_offset;
+
+    int width = utf8_width(tab.c_str()) + 2;
+    //print top border
+    for(int x = gx; x < gx + width; x++) {
+        mvwputch(win, gy, x, border_color, LINE_OXOX);
+    }
+
+    //print text and 2 borders
+    mvwputch(win, gy + 1, gx, border_color, LINE_XOXO);
+    mvwprintz(win, gy + 1, gx + 1, border_color, tab.c_str());
+    mvwputch(win, gy + 1, gx + width, border_color, LINE_XOXO);
+
+    if(selected) {
+        // print selection braces
+        mvwputch(win, gy + 1, gx - 1, border_color, '<');
+        mvwputch(win, gy + 1, gx + width + 1, border_color, '>');
+
+        // print bottom tab corners
+        mvwputch(win, gy + 2, gx, border_color, LINE_XOOX);
+        mvwputch(win, gy + 2, gx + width, border_color, LINE_XXOO);
+    } else {
+        // print T
+        mvwputch(win, gy + 2, gx, border_color, LINE_XXOX);
+        // print bottom border
+        for(int x = gx + 1; x < gx + width - 1; x++) {
+            mvwputch(win, gy + 2, x, border_color, LINE_OXOX);
+        }
+        // print T
+        mvwputch(win, gy + 2, gx + width, border_color, LINE_XXOX);
+    }
+    return width;
+}
+
+void tabbed_panel::draw( WINDOW *win )
+{
+    bordered_panel::draw( win );
+    int x_offset = 1;
+    for(unsigned int i = 0; i < tabs.size(); i++) {
+        x_offset += draw_tab(tabs[i], tab_index == i, x_offset, win) + 1;
+    }
+}
+
+void tabbed_panel::add_tab(std::string tab)
+{
+    tabs.push_back(tab);
+}
+
+void tabbed_panel::next_tab()
+{
+    if( tab_index == tabs.size() - 1 ){
+        tab_index = 0;
+    } else {
+        tab_index++;
+    }
+}
+
+void tabbed_panel::previous_tab()
+{
+    if( tab_index == 0 ){
+        tab_index = tabs.size() - 1;
+    } else {
+        tab_index--;
+    }
+}
+
+
+const std::string &tabbed_panel::current_tab() const
+{
+    return tabs[tab_index];
 }
--- src/cata_ui.h
+++ src/cata_ui.h
@@ -10,29 +10,47 @@
 typedef int nc_color;
 
 // so we can pass around dimensions.
-struct rect {
-    // Size of the rect.
-    size_t size_x, size_y;
+class rect {
+    private:
+        const rect *parent;
 
-    // Position of the rect.
-    unsigned int x, y;
+        // Position of the rect relative to the parent.
+        // These are private because we need the setters to be used.
+        unsigned int x, y;
+        unsigned int global_x, global_y;
+    public:
+        // Size of the rect.
+        size_t size_x, size_y;
 
-    rect( size_t size_x, size_t size_y, unsigned int x, unsigned int y );
+        rect( size_t size_x, size_t size_y, unsigned int x, unsigned int y );
+
+        void set_parent(const rect *p_rect);
+
+        unsigned int get_x() const;
+        void set_x(unsigned int new_x);
+
+        unsigned int get_y() const;
+        void set_y(unsigned int new_y);
+
+        unsigned int get_global_x() const;
+        unsigned int get_global_y() const;
 };
 
 class ui_element {
-    private:
+    protected:
         bool show = true;
-    public:
-        // protected seems to work different from C#, so this has to be public.
-        rect el_rect;
 
-        ui_element(size_t size_x, size_t size_y);
-        ui_element(size_t size_x, size_t size_y, unsigned int x, unsigned int y);
+        rect el_rect;
+    public:
+        ui_element(size_t size_x, size_t size_y, unsigned int x = 0, unsigned int y = 0);
         virtual ~ui_element() = default;
 
         virtual void draw( WINDOW * ) = 0;
         virtual ui_element *clone() const = 0;
+
+        virtual void set_parent(ui_element *parent);
+
+        const rect &get_rect() const;
 
         virtual void set_visible(bool visible);
         virtual bool is_visible() const;
@@ -41,19 +59,17 @@ class ui_element {
 // composite pattern
 class ui_window {
     protected:
-        rect el_rect;
         WINDOW *win;
 
         std::list<ui_element *> children;
     public:
-        ui_window(size_t size_x, size_t size_y);
-        ui_window(size_t size_x, size_t size_y, unsigned int x, unsigned int y);
+        ui_window(size_t size_x, size_t size_y, unsigned int x = 0, unsigned int y = 0);
         ~ui_window();
 
         virtual void draw();
 
-        void add_child( ui_element *child );
-        void add_child( const ui_element &child );
+        template<typename T = ui_element>
+        T *add_child( const T &child );
 };
 
 // similar to window but different
@@ -61,13 +77,12 @@ class ui_panel : public ui_element {
     public:
         std::list<ui_element *> children;
     public:
-        ui_panel(size_t size_x, size_t size_y);
-        ui_panel(size_t size_x, size_t size_y, unsigned int x, unsigned int y);
+        ui_panel(size_t size_x, size_t size_y, unsigned int x = 0, unsigned int y = 0);
         virtual ui_element *clone() const override;
         ~ui_panel();
 
-        void add_child( ui_element *child );
-        void add_child( const ui_element &child );
+        template<typename T = ui_element>
+        T *add_child( const T &child );
 
         virtual void draw( WINDOW * ) override;
 };
@@ -89,8 +104,7 @@ class ui_label : public ui_element {
 
 class bordered_panel : public ui_panel {
     public:
-        bordered_panel(size_t size_x, size_t size_y);
-        bordered_panel(size_t size_x, size_t size_y, unsigned int x, unsigned int y);
+        bordered_panel(size_t size_x, size_t size_y, unsigned int x = 0, unsigned int y = 0);
         virtual ui_element *clone() const override;
 
         virtual void draw( WINDOW * ) override;
@@ -110,8 +124,7 @@ class health_bar : public ui_element {
 
         void refresh_bar( bool overloaded, float percentage );
     public:
-        health_bar(size_t size_x);
-        health_bar(size_t size_x, unsigned int x, unsigned int y);
+        health_bar(size_t size_x, unsigned int x = 0, unsigned int y = 0);
         virtual ui_element *clone() const override;
 
         virtual void draw( WINDOW * ) override;
@@ -134,8 +147,7 @@ class smiley_indicator : public ui_element {
         nc_color smiley_color = c_white;
         std::string smiley_str = "";
     public:
-        smiley_indicator();
-        smiley_indicator(unsigned int x, unsigned int y);
+        smiley_indicator(unsigned int x = 0, unsigned int y = 0);
         virtual ui_element *clone() const override;
 
         virtual void draw( WINDOW * ) override;
@@ -162,15 +174,32 @@ class tile_panel : public ui_element {
         unsigned int y_radius;
     public:
         tile_panel(tripoint center, std::function<const char_tile(int, int, int)> tile_at,
-                   size_t size_x, size_t size_y);
-        tile_panel(tripoint center, std::function<const char_tile(int, int, int)> tile_at,
-                   size_t size_x, size_t size_y, unsigned int x, unsigned int y);
+                   size_t size_x, size_t size_y, unsigned int x = 0, unsigned int y = 0);
         virtual ui_element *clone() const override;
 
         virtual void draw( WINDOW * ) override;
 
         void set_center(tripoint new_center);
         const tripoint &get_center() const;
+};
+
+class tabbed_panel : public bordered_panel {
+    private:
+        std::vector<std::string> tabs;
+        unsigned int tab_index;
+
+        int draw_tab(const std::string &, bool, int, WINDOW *) const;
+    public:
+        tabbed_panel(size_t size_x, size_t size_y, unsigned int x = 0, unsigned int y = 0);
+        virtual ui_element *clone() const override;
+
+        virtual void draw( WINDOW * ) override;
+
+        void add_tab(std::string tab);
+
+        void next_tab();
+        void previous_tab();
+        const std::string &current_tab() const;
 };
 
 #endif // CATA_UI_H
