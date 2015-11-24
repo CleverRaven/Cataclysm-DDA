767632b0f76e31da8f82552967de233a6f87d577
OzoneH3 <satori.jj@gmail.com>
2015-11-24 21:17:51 +0100

    Don't quit if a wrong button gets pressed.

--- src/inventory_ui.cpp
+++ src/inventory_ui.cpp
@@ -968,10 +968,8 @@ item_location game::inv_map_splice(
 
         } else if( ch >= min_invlet && ch <= max_invlet ) {
             // Indexed item on ground or in vehicle
-            if ( invlets.size() > ch - min_invlet ) {
+            if ( (long)invlets.size() > ch - min_invlet ) {
                 return std::move( invlets[ch - min_invlet] );
-            } else {
-                return item_location::nowhere();
             }
 
         } else if( inv_s.handle_movement( action ) ) {
