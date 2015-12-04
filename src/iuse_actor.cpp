b9abc21e8ec296fdd776b444cc961277f0b022e8
Mugling <mugling@users.noreply.github.com>
2015-12-04 22:14:25 +0000

    Reimplement Iaijutsu attacks when drawing weapon
    Fixes #14297

--- src/iuse_actor.cpp
+++ src/iuse_actor.cpp
@@ -1799,6 +1799,28 @@ long holster_actor::use( player *p, item *it, bool, const tripoint & ) const
 
     if( pos >= 0 ) {
         p->wield_contents( it, pos, draw_speed );
+
+        // hanlde iaijutsu attacks when drawing swords and other such blades
+        if( p->weapon.has_flag( "IAIJUTSU" ) &&
+            p->skillLevel( skill_id("cutting") ) >= 7 &&
+            one_in(12 - p->skillLevel( skill_id("cutting") ) ) ) {
+
+            for( const auto &pos : closest_tripoints_first( 1, p->pos() ) ) {
+                if( g->mon_at( pos, true ) != -1 ) {
+                    // at least one adjacent monster so choose direction defaulting to first monster
+                    monster *mon = g->monster_at( pos, true );
+                    tripoint sel;
+                    if (choose_adjacent( _( "Slash where?" ), sel ) && g->mon_at( sel, true ) != -1 ) {
+                        mon = g->monster_at( sel, true );
+                    }
+                    p->melee_attack( *mon, true );
+                    p->add_msg_if_player( m_good, _( "You slash at the %1$s as you draw your %2$s." ),
+                                          mon->name().c_str(), p->weapon.tname().c_str() );
+                    break;
+                }
+            }
+        }
+
     } else {
         item &obj = p->i_at( g->inv_for_filter( prompt, [&](const item& e) { return can_holster(e); } ) );
 
