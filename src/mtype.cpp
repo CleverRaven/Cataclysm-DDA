8d6c373c731371a4bf4debaf85e8859315e70140
Kevin Granade <kevin.granade@gmail.com>
2015-12-09 20:56:22 +0000

    Astyling.

--- src/mtype.cpp
+++ src/mtype.cpp
@@ -53,7 +53,7 @@ std::string mtype::nname(unsigned int quantity) const
 
 bool mtype::has_special_attack( const std::string &attack_name ) const
 {
-    return special_attacks.find(attack_name) != special_attacks.end();
+    return special_attacks.find( attack_name ) != special_attacks.end();
 }
 
 bool mtype::has_flag(m_flag flag) const
