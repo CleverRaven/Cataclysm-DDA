82df05e1f65874c388626c190fcc1ab8a26150a0
Mugling <mugling@users.noreply.github.com>
2015-12-12 15:07:45 +0000

    Use item::ammo_remaining() and item::ammo_capacity()

--- src/item.cpp
+++ src/item.cpp
@@ -3921,60 +3921,39 @@ item *item::get_usable_item( const std::string &use_name )
 
 int item::pick_reload_ammo( const player &u, bool interactive )
 {
-    if( is_null() ) {
-        return INT_MIN;
-    }
+    std::set<std::string> ammo_types; // for unloaded items any ammo will do
+    std::set<std::string> item_types; // for partially loaded require matching type
 
-    if( !is_gun() && !is_tool() ) {
-        debugmsg( "RELOADING NON-GUN NON-TOOL" );
-        return INT_MIN;
-    }
-    int has_spare_mag = has_gunmod( "spare_mag" );
-    // Ammo types that we could use.
-    std::set<std::string> ammo_types;
-    // Specific item types that we could use (in case of partially loaded guns).
-    std::set<std::string> item_types;
-    if( is_gun() ) {
-        if( charges <= 0 && has_spare_mag != -1 && contents[has_spare_mag].charges > 0 ) {
-            // Special return to use magazine for reloading.
-            return INT_MIN + 1;
+    // @todo deprecate handling of spare magazine as special case
+    auto spare = has_gunmod( "spare_mag" );
+    if( spare >= 0 ) {
+        if( ammo_remaining() <= 0 && contents[spare].charges > 0 ) {
+            return INT_MIN + 1; // special return to use spare magazine for reloading.
         }
-
-        // If there's room to load more ammo into the gun or a spare mag, stash the ammo.
-        // If the gun is partially loaded make sure the ammo matches.
-        // If the gun is empty, either the spare mag is empty too and anything goes,
-        // or the spare mag is loaded and we're doing a tactical reload.
-        if( charges < clip_size() ||
-            ( has_spare_mag != -1 && contents[has_spare_mag].charges < spare_mag_size() ) ) {
-            if( charges > 0 && has_curammo() ) {
-                item_types.insert( get_curammo_id() );
+        if( contents[spare].charges < ammo_remaining() ) {
+            if( contents[spare].charges > 0 || ammo_remaining() > 0 ) {
+                item_types.insert( get_curammo_id() ); // gun or spare mag partially loaded
             } else {
-                ammo_types.insert( ammo_type() );
+                ammo_types.insert( ammo_type() ); // neither loaded
             }
         }
-
-        // ammo for gun attachments (shotgun attachments, grenade attachments, etc.)
-        // for each attachment, find its associated ammo & append it to the ammo vector
-        for( auto & cont : contents ) {
-            if( !cont.is_auxiliary_gunmod() ) {
-                // not a gunmod, or has no separate firing mode and can not be load
-                continue;
-            }
-            const auto mod = cont.type->gun.get();
-            if( cont.charges >= mod->clip ) {
-                // already fully loaded
-                continue;
-            }
-            if( cont.charges > 0 && cont.has_curammo() ) {
-                item_types.insert( cont.get_curammo_id() );
-            } else {
-                ammo_types.insert( mod->ammo );
-            }
-        }
-    } else { //non-gun.
-        // this is probably a tool.
-        ammo_types.insert( ammo_type() );
     }
+
+    // checks if item reloadable and if so appends to either ammo_type or item_type
+    auto wants_ammo = [&ammo_types,&item_types](const item& it) {
+        if( it.ammo_remaining() < it.ammo_capacity() && !it.has_flag( "NO_RELOAD" ) ) {
+            if( it.ammo_remaining() > 0 ) {
+                item_types.insert( it.get_curammo_id() ); // partially loaded
+            } else {
+                ammo_types.insert( it.ammo_type() ); // not loaded
+            }
+        }
+    };
+
+    // consider both the item and any attached gunmods
+    wants_ammo( *this );
+    std::for_each( contents.begin(), contents.end(), wants_ammo );
+
     // Actual ammo items that can be used as ammo.
     std::vector<const item*> am = u.items_with( [&ammo_types, &item_types]( const item & it ) {
         if( !it.is_ammo() ) {
