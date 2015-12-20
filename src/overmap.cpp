71215ddb474323949b775a0a8c5e3b8732b52e6b
illi-kun <D.E.Serov@gmail.com>
2015-12-20 12:40:14 +0300

    remove overmap boolean flag "unique"

--- src/overmap.cpp
+++ src/overmap.cpp
@@ -160,7 +160,6 @@ void load_overmap_specials(JsonObject &jo)
     }
 
     spec.rotatable = jo.get_bool("rotate", false);
-    spec.unique = jo.get_bool("unique", false);
     spec.required = jo.get_bool("required", false);
 
     if(jo.has_object("spawns")) {
