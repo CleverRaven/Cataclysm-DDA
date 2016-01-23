66800720e723746adc0b8ff2a5b8639c00c46472
Kevin Granade <kevin.granade@gmail.com>
2016-01-23 01:05:23 +0000

    Astyle campaign 6.

--- src/live_view.cpp
+++ src/live_view.cpp
@@ -12,32 +12,33 @@
 
 const efftype_id effect_blind( "blind" );
 
-namespace {
+namespace
+{
 constexpr int START_LINE = 1;
 constexpr int START_COLUMN = 1;
 
-void print_items(WINDOW *const w, const map_stack &items, int &line)
+void print_items( WINDOW *const w, const map_stack &items, int &line )
 {
     std::map<std::string, int> item_names;
-    for (auto &item : items) {
+    for( auto &item : items ) {
         ++item_names[item.tname()];
     }
 
-    int const last_line = getmaxy(w) - START_LINE - 1;
-    int const max_w = getmaxx(w) - START_COLUMN - 1; // border
+    int const last_line = getmaxy( w ) - START_LINE - 1;
+    int const max_w = getmaxx( w ) - START_COLUMN - 1; // border
 
-    for (auto const &it : item_names) {
-        if (line == last_line) {
-            mvwprintz(w, line++, START_COLUMN, c_yellow, _("More items here..."));
+    for( auto const &it : item_names ) {
+        if( line == last_line ) {
+            mvwprintz( w, line++, START_COLUMN, c_yellow, _( "More items here..." ) );
             break;
         }
 
-        if (it.second > 1) {
+        if( it.second > 1 ) {
             //~ item name [quantity]
-            trim_and_print(w, line++, START_COLUMN, max_w, c_white, _("%s [%d]"),
-                it.first.c_str(), it.second);
+            trim_and_print( w, line++, START_COLUMN, max_w, c_white, _( "%s [%d]" ),
+                            it.first.c_str(), it.second );
         } else {
-            trim_and_print(w, line++, START_COLUMN, max_w, c_white, "%s", it.first.c_str());
+            trim_and_print( w, line++, START_COLUMN, max_w, c_white, "%s", it.first.c_str() );
         }
     }
 }
@@ -48,54 +49,54 @@ bool live_view::is_compact() const
     return compact_view;
 }
 
-void live_view::set_compact(bool const value)
+void live_view::set_compact( bool const value )
 {
     compact_view = value;
 }
 
-void live_view::init(int const start_x, int const start_y, int const w, int const h)
+void live_view::init( int const start_x, int const start_y, int const w, int const h )
 {
     enabled = true;
     width   = w;
     height  = h;
 
-    w_live_view.reset(newwin(height, width, start_y, start_x));
+    w_live_view.reset( newwin( height, width, start_y, start_x ) );
 
     hide();
 }
 
-void live_view::show(const int x, const int y)
+void live_view::show( const int x, const int y )
 {
-    if (!enabled || !w_live_view) {
+    if( !enabled || !w_live_view ) {
         return;
     }
 
-    bool did_hide = hide(false); // Clear window if it's visible
+    bool did_hide = hide( false ); // Clear window if it's visible
 
-    if (!g->u.sees(x, y)) {
-        if (did_hide) {
-            wrefresh(*this);
+    if( !g->u.sees( x, y ) ) {
+        if( did_hide ) {
+            wrefresh( *this );
         }
         return;
     }
 
     map &m = g->m;
-    mvwprintz(*this, 0, START_COLUMN, c_white, "< ");
-    wprintz(*this, c_green, _("Mouse View"));
-    wprintz(*this, c_white, " >");
+    mvwprintz( *this, 0, START_COLUMN, c_white, "< " );
+    wprintz( *this, c_green, _( "Mouse View" ) );
+    wprintz( *this, c_white, " >" );
     int line = START_LINE;
 
     // TODO: Z
     tripoint p( x, y, g->get_levz() );
 
-    g->print_all_tile_info( p, *this, START_COLUMN, line, true);
+    g->print_all_tile_info( p, *this, START_COLUMN, line, true );
 
-    if (m.can_put_items( p ) && m.sees_some_items( p, g->u)) {
-        if(g->u.has_effect( effect_blind) || g->u.worn_with_flag("BLIND")) {
-            mvwprintz(*this, line++, START_COLUMN, c_yellow,
-                      _("There's something here, but you can't see what it is."));
+    if( m.can_put_items( p ) && m.sees_some_items( p, g->u ) ) {
+        if( g->u.has_effect( effect_blind ) || g->u.worn_with_flag( "BLIND" ) ) {
+            mvwprintz( *this, line++, START_COLUMN, c_yellow,
+                       _( "There's something here, but you can't see what it is." ) );
         } else {
-            print_items(*this, m.i_at(p), line);
+            print_items( *this, m.i_at( p ), line );
         }
     }
 
@@ -108,38 +109,38 @@ void live_view::show(const int x, const int y)
     // currently support the mouse. If and when it does, there'll need to
     // be a different code path here that works for ncurses.
     int full_height = w_live_view->height;
-    if (line < w_live_view->height - 1) {
-        w_live_view->height = (line > 11) ? line : 11;
+    if( line < w_live_view->height - 1 ) {
+        w_live_view->height = ( line > 11 ) ? line : 11;
     }
     last_height = w_live_view->height;
 #endif
 
-    draw_border(*this);
+    draw_border( *this );
 
 #if (defined TILES || defined _WIN32 || defined WINDOWS)
     w_live_view->height = full_height;
 #endif
 
     inuse = true;
-    wrefresh(*this);
+    wrefresh( *this );
 }
 
-bool live_view::hide(bool refresh /*= true*/, bool force /*= false*/)
+bool live_view::hide( bool refresh /*= true*/, bool force /*= false*/ )
 {
-    if (!enabled || (!inuse && !force)) {
+    if( !enabled || ( !inuse && !force ) ) {
         return false;
     }
 
 #if (defined TILES || defined _WIN32 || defined WINDOWS)
     int full_height = w_live_view->height;
-    if (use_narrow_sidebar() && last_height > 0) {
+    if( use_narrow_sidebar() && last_height > 0 ) {
         // When using the narrow sidebar mode, the lower part of the screen
         // is used for the message queue. Best not to obscure too much of it.
         w_live_view->height = last_height;
     }
 #endif
 
-    werase(*this);
+    werase( *this );
 
 #if (defined TILES || defined _WIN32 || defined WINDOWS)
     w_live_view->height = full_height;
@@ -147,8 +148,8 @@ bool live_view::hide(bool refresh /*= true*/, bool force /*= false*/)
 
     inuse = false;
     last_height = -1;
-    if (refresh) {
-        wrefresh(*this);
+    if( refresh ) {
+        wrefresh( *this );
     }
 
     return true;
--- src/ui.h
+++ src/ui.h
@@ -41,28 +41,27 @@ struct uimenu_entry {
     mvwzstr extratxt;
 
     //std::string filtertxt; // possibly useful
-    uimenu_entry(std::string T) : retval(-1), enabled(true), hotkey(-1), txt(T)
-    {
+    uimenu_entry( std::string T ) : retval( -1 ), enabled( true ), hotkey( -1 ), txt( T ) {
         text_color = c_red_red;
     };
-    uimenu_entry(std::string T, std::string D) : retval(-1), enabled(true), hotkey(-1), txt(T), desc(D)
-    {
+    uimenu_entry( std::string T, std::string D ) : retval( -1 ), enabled( true ), hotkey( -1 ),
+        txt( T ), desc( D ) {
         text_color = c_red_red;
     };
-    uimenu_entry(std::string T, int K) : retval(-1), enabled(true), hotkey(K), txt(T)
-    {
+    uimenu_entry( std::string T, int K ) : retval( -1 ), enabled( true ), hotkey( K ), txt( T ) {
         text_color = c_red_red;
     };
-    uimenu_entry(int R, bool E, int K, std::string T) : retval(R), enabled(E), hotkey(K), txt(T)
-    {
+    uimenu_entry( int R, bool E, int K, std::string T ) : retval( R ), enabled( E ), hotkey( K ),
+        txt( T ) {
         text_color = c_red_red;
     };
-    uimenu_entry(int R, bool E, int K, std::string T, std::string D) : retval(R), enabled(E), hotkey(K), txt(T), desc(D)
-    {
+    uimenu_entry( int R, bool E, int K, std::string T, std::string D ) : retval( R ), enabled( E ),
+        hotkey( K ), txt( T ), desc( D ) {
         text_color = c_red_red;
     };
-    uimenu_entry(int R, bool E, int K, std::string T, nc_color H, nc_color C) : retval(R), enabled(E), hotkey(K), txt(T),
-        hotkey_color(H), text_color(C) {};
+    uimenu_entry( int R, bool E, int K, std::string T, nc_color H, nc_color C ) : retval( R ),
+        enabled( E ), hotkey( K ), txt( T ),
+        hotkey_color( H ), text_color( C ) {};
 };
 
 /*
@@ -109,17 +108,15 @@ class uimenu_callback
 {
     public:
         void *myptr;
-        void setptr(void *ptr)
-        {
+        void setptr( void *ptr ) {
             myptr = ptr;
         }
-        virtual void select(int /*entnum*/, uimenu *) {};
-        virtual bool key(int /*key*/, int /*entnum*/, uimenu *)
-        {
+        virtual void select( int /*entnum*/, uimenu * ) {};
+        virtual bool key( int /*key*/, int /*entnum*/, uimenu * ) {
             return false;
         };
-        virtual void refresh(uimenu *) {};
-        virtual void redraw(uimenu *) {};
+        virtual void refresh( uimenu * ) {};
+        virtual void redraw( uimenu * ) {};
         virtual ~uimenu_callback() {};
 };
 /*
@@ -175,34 +172,36 @@ class uimenu: public ui_container
 
         uimenu( const std::string &hotkeys = "" ); // bare init
 
-        uimenu(bool cancancel, const char *message, ...);  // legacy menu()
-        uimenu(bool cancelable, const char *mes, const std::vector<std::string> options); // legacy menu_vec
-        uimenu(bool cancelable, const char *mes, const std::vector<std::string> &options, const std::string &hotkeys);
-        uimenu(bool cancelable, int startx, int width, int starty, std::string title,
-               std::vector<uimenu_entry> ents);
-        uimenu(int startx, int width, int starty, std::string title, std::vector<uimenu_entry> ents);
+        uimenu( bool cancancel, const char *message, ... ); // legacy menu()
+        uimenu( bool cancelable, const char *mes,
+                const std::vector<std::string> options ); // legacy menu_vec
+        uimenu( bool cancelable, const char *mes, const std::vector<std::string> &options,
+                const std::string &hotkeys );
+        uimenu( bool cancelable, int startx, int width, int starty, std::string title,
+                std::vector<uimenu_entry> ents );
+        uimenu( int startx, int width, int starty, std::string title, std::vector<uimenu_entry> ents );
 
         void init();
         void setup();
         void show();
-        bool scrollby(int scrollby = 0, const int key = 0 );
-        void query(bool loop = true);
+        bool scrollby( int scrollby = 0, const int key = 0 );
+        void query( bool loop = true );
         void filterlist();
         void apply_scrollbar();
         std::string inputfilter();
-        void refresh(bool refresh_callback = true) override;
-        void redraw(bool redraw_callback = true);
-        void addentry(std::string str);
-        void addentry(const char *format, ...);
-        void addentry(int r, bool e, int k, std::string str);
-        void addentry(int r, bool e, int k, const char *format, ...);
-        void addentry_desc(std::string str, std::string desc);
-        void addentry_desc(int r, bool e, int k, std::string str, std::string desc);
-        void settext(std::string str);
-        void settext(const char *format, ...);
+        void refresh( bool refresh_callback = true ) override;
+        void redraw( bool redraw_callback = true );
+        void addentry( std::string str );
+        void addentry( const char *format, ... );
+        void addentry( int r, bool e, int k, std::string str );
+        void addentry( int r, bool e, int k, const char *format, ... );
+        void addentry_desc( std::string str, std::string desc );
+        void addentry_desc( int r, bool e, int k, std::string str, std::string desc );
+        void settext( std::string str );
+        void settext( const char *format, ... );
 
         void reset();
-        ~uimenu ();
+        ~uimenu();
 
         operator int() const;
 
@@ -217,7 +216,8 @@ class uimenu: public ui_container
 
 // Callback for uimenu that pairs menu entries with points
 // When an entry is selected, view will be centered on the paired point
-class pointmenu_cb : public uimenu_callback {
+class pointmenu_cb : public uimenu_callback
+{
     private:
         const std::vector< tripoint > &points;
         int last; // to suppress redrawing
