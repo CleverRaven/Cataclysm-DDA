8988292a7f7c1d211e1203405b7521cbe2f8f03c
Kevin Granade <kevin.granade@gmail.com>
2016-01-28 20:16:43 +0000

    Astyle campaign pt F.

--- src/overmap_types.h
+++ src/overmap_types.h
@@ -3,13 +3,13 @@
 
 class scent_trace
 {
-public:
-    // Default constructor makes an invalid trace.
-    scent_trace() : creation_turn( -1 ), initial_strength( 0 ) {};
-    scent_trace( int turn, int strength ) : creation_turn( turn ), initial_strength( strength ) {}
-    // TODO: give these accessors so they can become private and immutable.
-    int creation_turn; // Timestamp when scent trace was created.
-    int initial_strength; // Original strength, doesn't weaken, it's just adjusted by age.
+    public:
+        // Default constructor makes an invalid trace.
+        scent_trace() : creation_turn( -1 ), initial_strength( 0 ) {};
+        scent_trace( int turn, int strength ) : creation_turn( turn ), initial_strength( strength ) {}
+        // TODO: give these accessors so they can become private and immutable.
+        int creation_turn; // Timestamp when scent trace was created.
+        int initial_strength; // Original strength, doesn't weaken, it's just adjusted by age.
 };
 
 #endif
