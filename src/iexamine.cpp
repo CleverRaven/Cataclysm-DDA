28fe1eb552fa33b1597efaa6ee641f669bdb1085
Chaosvolt <chaosvolt@users.noreply.github.com>
2016-01-30 16:26:50 -0600

    Fixes copy-paste error

--- src/iexamine.cpp
+++ src/iexamine.cpp
@@ -35,7 +35,7 @@ const mtype_id mon_dark_wyrm( "mon_dark_wyrm" );
 const mtype_id mon_fungal_blossom( "mon_fungal_blossom" );
 const mtype_id mon_spider_web_s( "mon_spider_web_s" );
 const mtype_id mon_spider_widow_giant_s( "mon_spider_widow_giant_s" );
-const mtype_id mon_spider_cellar_giant_s( "mon_spider_widow_giant_s" );
+const mtype_id mon_spider_cellar_giant_s( "mon_spider_cellar_giant_s" );
 const mtype_id mon_turret( "mon_turret" );
 const mtype_id mon_turret_rifle( "mon_turret_rifle" );
 
