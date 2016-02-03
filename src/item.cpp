6f39378970cf1efb2db802415d8f69aa9b9f250a
Mugling <mugling@users.noreply.github.com>
2016-02-03 02:34:40 +0000

    Fix reloading using liquids in containers
    Closes #15200

--- src/item.cpp
+++ src/item.cpp
@@ -4303,9 +4303,16 @@ item_location item::pick_reload_ammo( player &u, bool interactive ) const
 
     std::vector<item_location> ammo_list;
 
-    auto filter = [&item_types,&ammo_types,&compat_mag]( const item *e ) {
-        return ( e->is_ammo() && ( item_types.count( e->ammo_current() ) || ammo_types.count( e->ammo_type() ) ) ) ||
-               ( e->is_magazine() && compat_mag.count( e->typeId() ) );
+    auto filter = [&item_types,&ammo_types,&compat_mag]( const item *e ) -> bool {
+        if( e->is_ammo() ) {
+            return item_types.count( e->ammo_current() ) || ammo_types.count( e->ammo_type() );
+        } else if( e->is_ammo_container() ) {
+            return item_types.count( e->contents[0].ammo_current() ) || ammo_types.count( e->contents[0].ammo_type() );
+        } else if ( e->is_magazine() ) {
+            return compat_mag.count( e->typeId() );
+        } else {
+            return false;
+        }
     };
 
     // first check the inventory for suitable ammo
@@ -4313,7 +4320,7 @@ item_location item::pick_reload_ammo( player &u, bool interactive ) const
         if( filter( node ) ) {
             ammo_list.emplace_back( item_location::on_character( u, node ) );
         }
-        return ( node->is_magazine() || node->is_gun() || node->is_tool() ) ? VisitResponse::SKIP : VisitResponse::NEXT;
+        return ( node->is_magazine() || node->is_container() || node->is_gun() || node->is_tool() ) ? VisitResponse::SKIP : VisitResponse::NEXT;
     });
 
     for( const auto &pos : closest_tripoints_first( 1, u.pos() ) ) {
@@ -4324,7 +4331,7 @@ item_location item::pick_reload_ammo( player &u, bool interactive ) const
                     if( filter( node ) ) {
                         ammo_list.emplace_back( item_location::on_map( pos, node ) );
                     }
-                    return ( node->is_magazine() || node->is_gun() || node->is_tool() ) ? VisitResponse::SKIP : VisitResponse::NEXT;
+                    return ( node->is_magazine() || node->is_container() || node->is_gun() || node->is_tool() ) ? VisitResponse::SKIP : VisitResponse::NEXT;
                 });
             }
         }
