cbf85d840a14c1a6b6d1f47f6509a11d4424b7e8
Mugling <mugling@users.noreply.github.com>
2016-02-10 01:00:50 +0000

    Use loop not function argument

--- src/map_selector.cpp
+++ src/map_selector.cpp
@@ -6,7 +6,7 @@
 map_selector::map_selector( const tripoint& pos, int radius, bool accessible ) {
     for( const auto& e : closest_tripoints_first( radius, pos ) ) {
         if( !accessible || g->m.clear_path( pos, e, radius, 1, 100 ) ) {
-	    data.emplace_back( pos );
+	    data.emplace_back( e );
         }
     }
 }
