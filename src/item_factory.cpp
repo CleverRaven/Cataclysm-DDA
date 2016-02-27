171f7d45179c6d97223a214e1be54ac3ca73f0cf
Mugling <mugling@users.noreply.github.com>
2016-02-27 18:50:23 +0000

    Add loudness property to ammo

--- src/item_factory.cpp
+++ src/item_factory.cpp
@@ -703,6 +703,12 @@ void Item_factory::load( islot_ammo &slot, JsonObject &jo )
     slot.recoil = jo.get_int( "recoil", 0 );
     slot.def_charges = jo.get_long( "count" );
     slot.ammo_effects = jo.get_tags( "effects" );
+
+    if( jo.has_int( "loudness" ) ) {
+        slot.loudness = jo.get_int( "loudness" );
+    } else {
+        slot.loudness = std::max( std::max( { slot.damage, slot.pierce, slot.range } ) * 3, slot.recoil / 3 );
+    }
 }
 
 void Item_factory::load_ammo(JsonObject &jo)
