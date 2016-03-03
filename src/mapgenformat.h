182fe99bfafb6eaa636ae446dfba196bf30fd5fe
BevapDin <tho_ki@gmx.de>
2016-03-03 14:52:10 +0100

    Document the ter_bind and furn_bind functions

--- src/mapgenformat.h
+++ src/mapgenformat.h
@@ -17,17 +17,19 @@ namespace mapf
 template<typename ID>
 class format_effect;
 
-/** The return statement for this method is not finalized.
- * The following things will be acces from this return statements.
- * - the region of points set by given character
- * - the region of points set to a given terrian id
- * - possibly some other stuff
- * You will have specify the values you want to track with a parameter.
+/**
+ * Set terrain and furniture on the supplied map.
+ * @param ter_b,furn_b The lookup table for placing terrain / furniture
+ *   (result of @ref ter_bind / @ref furn_bind).
+ * @param cstr Contains the ASCII representation of the map. Each character in it represents
+ *   one tile on the map. It will be looked up in \p ter_b and \p furn_b to get the terrain/
+ *   furniture to place there (if that lookup returns a null id, nothing is set on the map).
+ *   A newline character continues on the next line (resets `x` to \p startx and increments `y`).
+ * @param startx,starty Coordinates in the map where to start drawing \p cstr.
  */
 void formatted_set_simple( map *m, const int startx, const int starty, const char *cstr,
                            format_effect<ter_id> ter_b, format_effect<furn_id> furn_b );
 
-// This class will become an interface in the future.
 template<typename ID>
 class format_effect
 {
@@ -42,6 +44,23 @@ class format_effect
         ID translate( char c ) const;
 };
 
+/**
+ * The functions create a mapping of characters to ids, usable with @ref formatted_set_simple.
+ * The first parameter must a string literal, containing the mapped characters.
+ * Only every second character of it is mapped:
+ * `"a b c"` maps `a` to the first id, `b` to the second and `c` to the third id.
+ * The further parameters form an array of suitable size with ids to map to.
+ *
+ * \code
+ * ter_bind( "a", t_dirt );
+ * ter_bind( "a b", t_dirt, t_wall );
+ * // This does not work (t_wall is not mapped to any character):
+ * ter_bind( "a", t_dirt, t_wall );
+ * // This does not work (character b is not mapped to any id):
+ * ter_bind( "a b", t_dirt );
+ * \endcode
+ */
+/**@{*/
 template<size_t N, typename ...Args>
 inline format_effect<ter_id> ter_bind( const char ( &characters )[N], Args... ids )
 {
@@ -61,6 +80,7 @@ inline format_effect<furn_id> furn_bind( const char ( &characters )[N], Args... 
                    "list of characters to bind to must match the size of the remaining arguments" );
     return format_effect<furn_id>( characters, { std::forward<Args>( ids )... } );
 }
+/**@}*/
 
 } //END NAMESPACE mapf
 
