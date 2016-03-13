cf185e6442abaaf6238de15561159ea896805cf1
illi-kun <d.e.serov@gmail.com>
2016-03-14 01:03:40 +0300

    another attempt to calm down clang

--- src/bionics.cpp
+++ src/bionics.cpp
@@ -80,7 +80,7 @@ std::string bodyparts[] = { bp_asText[0], bp_asText[1], bp_asText[2], bp_asText[
                           };
 
 void draw_background( WINDOW *win, const bool empty_list );
-void draw_header( WINDOW *win, const std::pair<int, int> power_units, const std::string help_key );
+void draw_header( WINDOW *win, std::string power_string, const std::string help_key );
 const char *power_description( std::string const &id );
 
 void draw_background( WINDOW *win, const bool empty_list )
