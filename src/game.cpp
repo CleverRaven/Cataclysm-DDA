75d24c8f943c9d243d47e4c63261649518d7b0d0
Lucian Poston <lucian.poston@gmail.com>
2016-03-22 14:08:33 -0700

    Fix vehicle part desc wrapping around info box
    Fixes #15951

--- src/game.cpp
+++ src/game.cpp
@@ -8397,7 +8397,7 @@ void game::print_object_info( const tripoint &lp, WINDOW *w_look, const int colu
         line = critter->print_info( w_look, line, 6, column );
     } else if (veh) {
         mvwprintw(w_look, line++, column, _("There is a %s there. Parts:"), veh->name.c_str());
-        line = veh->print_part_desc(w_look, line, (mouse_hover) ? getmaxx(w_look) : 48, veh_part);
+        line = veh->print_part_desc(w_look, line, getmaxx(w_look), veh_part);
         if (!mouse_hover) {
             m.drawsq( w_terrain, u, lp, true, true, lp );
         }
