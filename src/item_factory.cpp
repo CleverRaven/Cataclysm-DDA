56fe0ac0ab3e2d27d4df53f784451c97b3cb4bad
Mugling <mugling@users.noreply.github.com>
2016-03-26 15:14:03 +0000

    Support both extending and deleting tags

--- src/item_factory.cpp
+++ src/item_factory.cpp
@@ -833,11 +833,19 @@ typename std::enable_if<std::is_same<T, std::string>::value, bool>::type assign(
 template <typename T>
 typename std::enable_if<std::is_same<T, std::set<std::string>>::value, bool>::type assign(
     JsonObject &jo, const std::string& name, T& val ) {
+
+    if( jo.has_string( name ) || jo.has_array( name ) ) {
+        val = jo.get_tags( name );
+        return true;
+    }
+
+    bool res = false;
+
     auto add = jo.get_object( "extend" );
     if( add.has_string( name ) || add.has_array( name ) ) {
         auto tags = add.get_tags( name );
         val.insert( tags.begin(), tags.end() );
-        return true;
+        res = true;
     }
 
     auto del = jo.get_object( "delete" );
@@ -845,14 +853,10 @@ typename std::enable_if<std::is_same<T, std::set<std::string>>::value, bool>::ty
         for( const auto& e : del.get_tags( name ) ) {
             val.erase( e );
         }
-        return true;
+        res = true;
     }
 
-    if( jo.has_string( name ) || jo.has_array( name ) ) {
-        val = jo.get_tags( name );
-        return true;
-    }
-    return false;
+    return res;
 }
 
 void Item_factory::load( islot_ammo &slot, JsonObject &jo )
