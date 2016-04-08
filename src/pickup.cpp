95e7114cbbc45c456fe51aec3f40855060382d71
Lucian Poston <lucian.poston@gmail.com>
2016-04-08 00:38:30 -0700

    Add local directional keybindings for pickup menu
    Fixes #16124

--- src/pickup.cpp
+++ src/pickup.cpp
@@ -656,10 +656,10 @@ void Pickup::pick_up( const tripoint &pos, int min )
         std::string action;
         long raw_input_char = ' ';
         input_context ctxt("PICKUP");
-        ctxt.register_action("UP", _("Previous item"));
-        ctxt.register_action("DOWN", _("Next item"));
-        ctxt.register_action("RIGHT", _("Mark selected item"));
-        ctxt.register_action("LEFT", _("Unmark selected item"));
+        ctxt.register_action("UP");
+        ctxt.register_action("DOWN");
+        ctxt.register_action("RIGHT");
+        ctxt.register_action("LEFT");
         ctxt.register_action("NEXT_TAB", _("Next page"));
         ctxt.register_action("PREV_TAB", _("Previous page"));
         ctxt.register_action("SCROLL_UP");
