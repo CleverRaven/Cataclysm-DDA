bfbf6007639f52e2724aa851b68b6198b0bac051
Mugling <mugling@users.noreply.github.com>
2016-04-19 14:46:00 +0100

    Deprecate item::has_quality

--- src/item.h
+++ src/item.h
@@ -467,8 +467,6 @@ class item : public JsonSerializer, public JsonDeserializer, public visitable<it
     /*@}*/
 
     int get_quality( const std::string &quality_id ) const;
-    bool has_quality( std::string quality_id ) const;
-    bool has_quality( std::string quality_id, int quality_value ) const;
     bool count_by_charges() const;
     bool craft_has_charges();
 
