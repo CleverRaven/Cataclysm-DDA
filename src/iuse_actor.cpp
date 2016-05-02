fd0bd12362f8a4329ab301fe81fd2c337eb8f298
Coolthulhu <Coolthulhu@gmail.com>
2016-05-02 04:36:54 +0200

    Split scaling bonus, document healing

--- src/iuse_actor.cpp
+++ src/iuse_actor.cpp
@@ -2534,14 +2534,17 @@ const std::string &repair_item_actor::action_description( repair_item_actor::rep
 void heal_actor::load( JsonObject &obj )
 {
     // Mandatory
-    limb_power = obj.get_int( "limb_power" );
     move_cost = obj.get_int( "move_cost" );
+    limb_power = obj.get_float( "limb_power" );
 
     // Optional
-    head_power = obj.get_int( "head_power", 0.8f * limb_power );
-    torso_power = obj.get_int( "torso_power", 1.5f * limb_power );
+    head_power = obj.get_float( "head_power", 0.8f * limb_power );
+    torso_power = obj.get_float( "torso_power", 1.5f * limb_power );
 
-    bonus_scaling = obj.get_float( "bonus_scaling", 0.25f * limb_power );
+    limb_scaling = obj.get_float( "limb_scaling", 0.25f * limb_power );
+    float scaling_ratio = limb_scaling / limb_power;
+    head_scaling = obj.get_float( "head_scaling", scaling_ratio * head_power );
+    torso_scaling = obj.get_float( "torso_scaling", scaling_ratio * torso_power );
 
     bleed = obj.get_float( "bleed", 0.0f );
     bite = obj.get_float( "bite", 0.0f );
@@ -2597,7 +2600,7 @@ long heal_actor::use( player *p, item *it, bool, const tripoint &pos ) const
     if( long_action ) {
         // A hack: long action healing on NPCs isn't done yet.
         // So just heal at start and paralyze the player for 5 minutes.
-        cost /= (p->get_skill_level( skill_firstaid ) + 1);
+        cost /= std::min( 10, p->get_skill_level( skill_firstaid ) + 1 );
     }
 
     // NPCs can use first aid now, but they can't perform long actions
@@ -2626,19 +2629,18 @@ int heal_actor::get_heal_value( const player &healer, hp_part healed ) const
     float bonus_mult;
     if( healed == hp_head ) {
         heal_base = head_power;
-        bonus_mult = 0.8f;
+        bonus_mult = head_scaling;
     } else if( healed == hp_torso ) {
         heal_base = torso_power;
-        bonus_mult = 1.5f;
+        bonus_mult = torso_scaling;
     } else {
         heal_base = limb_power;
-        bonus_mult = 1.0f;
+        bonus_mult = limb_scaling;
     }
 
     if( heal_base > 0 ) {
         ///\EFFECT_FIRSTAID increases healing item effects
-        float bonus = healer.get_skill_level( skill_firstaid ) * bonus_scaling;
-        return heal_base + bonus_mult * bonus;
+        return heal_base + bonus_mult * healer.get_skill_level( skill_firstaid );
     }
 
     return heal_base;
