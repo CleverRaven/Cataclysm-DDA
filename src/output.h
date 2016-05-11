53134a89cb357d11071697e39386fd5d8f5de4dc
illi-kun <d.e.serov@gmail.com>
2016-05-11 08:49:41 +0300

    Remove unused printz() and mvprintz()

--- src/output.h
+++ src/output.h
@@ -249,9 +249,7 @@ void mvputch_hi(int y, int x, nc_color FG, const std::string &ch);
 // Using long ch is deprecated, use an UTF-8 encoded string instead
 void mvwputch_hi(WINDOW *w, int y, int x, nc_color FG, long ch);
 void mvwputch_hi(WINDOW *w, int y, int x, nc_color FG, const std::string &ch);
-void mvprintz(int y, int x, nc_color FG, const char *mes, ...);
 void mvwprintz(WINDOW *w, int y, int x, nc_color FG, const char *mes, ...);
-void printz(nc_color FG, const char *mes, ...);
 void wprintz(WINDOW *w, nc_color FG, const char *mes, ...);
 
 void draw_custom_border(WINDOW *w, chtype ls = 1, chtype rs = 1, chtype ts = 1, chtype bs = 1, chtype tl = 1, chtype tr = 1,
