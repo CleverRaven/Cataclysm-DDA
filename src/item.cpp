3da3fd86fc9965bb96de8a4a609075741dd16482
Mugling <mugling@users.noreply.github.com>
2016-05-21 14:44:28 +0100

    Add markup for value

--- src/item.cpp
+++ src/item.cpp
@@ -1329,7 +1329,7 @@ std::string item::info( bool showtext, std::vector<iteminfo> &info ) const
     auto name_quality = [&info]( const std::pair<quality_id,int>& q ) {
         std::string str;
         if( q.first == quality_jack || q.first == quality_lift ) {
-            str = string_format( _( "Has level <info>%1$d %2$s</info> quality and is rated at %3$dkg" ),
+            str = string_format( _( "Has level <info>%1$d %2$s</info> quality and is rated at <info>%3$dkg</info>" ),
                                  q.second, q.first.obj().name.c_str(), q.second * TOOL_LIFT_FACTOR / 1000 );
         } else {
             str = string_format( _( "Has level <info>%1$d %2$s</info> quality." ),
