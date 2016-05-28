7925aaead77ac44a0a095a6369531f3d9d434aea
pisskop <jfusco1989@gmail.com>
2016-05-28 15:55:08 -0400

    Update iuse.h

--- src/iuse.h
+++ src/iuse.h
@@ -113,8 +113,8 @@ public:
     int granade             ( player*, item*, bool, const tripoint& );
     int granade_act         ( player*, item*, bool, const tripoint& );
     int c4                  ( player*, item*, bool, const tripoint& );
-    int acidbomb_act        ( player*, item*, bool, const tripoint& );
     int arrow_flamable      ( player*, item*, bool, const tripoint& );
+    int acidbomb_act        ( player*, item*, bool, const tripoint& );
     int grenade_inc_act     ( player*, item*, bool, const tripoint& );
     int molotov_lit         ( player*, item*, bool, const tripoint& );
     int firecracker_pack    ( player*, item*, bool, const tripoint& );
