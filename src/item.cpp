d84c5a6324e0e58875379df2d864cb9f40213383
Mugling <mugling@users.noreply.github.com>
2016-05-28 19:41:44 +0100

    Fix display for auxiliary gunmods

--- src/item.cpp
+++ src/item.cpp
@@ -817,10 +817,10 @@ std::string item::info( bool showtext, std::vector<iteminfo> &info ) const
 
     } else {
         const item *mod = this;
-        auto& aux = *gun_current_mode();
+        const auto aux = gun_current_mode();
         // if we have an active auxiliary gunmod display stats for this instead
-        if( aux.is_gunmod() && aux.is_gun() ) {
-            mod = &aux;
+        if( aux && aux->is_gunmod() && aux->is_gun() ) {
+            mod = &*aux;
             info.emplace_back( "DESCRIPTION", string_format( _( "Stats of the active <info>gunmod (%s)</info> are shown." ),
                                                              mod->tname().c_str() ) );
         }
