6e5e4feedf6d6d8e48da6dc7f891c3b4d8783880
codemime <codemime@gmail.com>
2016-06-07 18:56:15 +0300

    Support aliases for monsters

--- src/monstergenerator.cpp
+++ src/monstergenerator.cpp
@@ -74,7 +74,7 @@ bool string_id<species_type>::is_valid() const
 }
 
 MonsterGenerator::MonsterGenerator()
-: mon_templates( new generic_factory<mtype>( "monster type" ) )
+: mon_templates( new generic_factory<mtype>( "monster type", "id", "alias" ) )
 , mon_species( new generic_factory<species_type>( "species" ) )
 {
     mon_templates->insert( mtype() );
