13ffbbf6bec9734dde21799332987c7f90db699f
Anton Burmistrov <Night_Pryanik@mail.ru>
2016-06-16 00:07:20 +0400

    Rename function

--- src/player.cpp
+++ src/player.cpp
@@ -9557,7 +9557,7 @@ bool player::can_wear( const item& it, bool alert ) const
         return false;
     }
 
-    if( it.is_disgusting_for() && has_trait( "SQUEAMISH" ) ) {
+    if( it.is_filthy() && has_trait( "SQUEAMISH" ) ) {
         if( alert ) {
             add_msg_if_player( m_info, _( "You can't wear that, it's filthy!" ) );
         }
