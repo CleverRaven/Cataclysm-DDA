0b5fd4f3c5369772d08c95a23d5b94ac5abef656
sethsimon <sethsimon@tuta.io>
2016-06-17 18:33:32 -0400

    Change butchery's tainted bone logic, fix offal
    In activity_handlers::butcher_finish, some zombies generate bones depending on the variable `bones`; others depend on the variable `pieces`. This changes it so they all use `bones`
    
    - get_meat_itype doesn't return "bone_tainted" if there's no POISON flag
    - In butcher_finish, assume "bone_tainted" means no meat/offal (fixes offal for skeletons)
    - Don't generate offal when the meat itype is "veggy" (fixes offal for triffids)

--- src/activity_handlers.cpp
+++ src/activity_handlers.cpp
@@ -190,6 +190,7 @@ void activity_handlers::butcher_finish( player_activity *act, player *p )
     const mtype *corpse = corpse_item.get_mtype();
     auto contents = corpse_item.contents;
     const int age = corpse_item.bday;
+    const itype_id meat = corpse->get_meat_itype();
     g->m.i_rem( p->pos(), act->index );
 
     const int factor = p->max_quality( quality_id( "BUTCHER" ) );
@@ -292,6 +293,8 @@ void activity_handlers::butcher_finish( player_activity *act, player *p )
             g->m.spawn_item( p->pos(), "bone", bones, 0, age );
             p->add_msg_if_player( m_good, _( "You harvest some usable bones!" ) );
         }
+    } else if( meat == "bone_tainted" ) {
+        p->add_msg_if_player( m_bad, _( "Your clumsy butchering destroys the bones!" ) );
     }
 
     if( sinews > 0 ) {
@@ -460,20 +463,19 @@ void activity_handlers::butcher_finish( player_activity *act, player *p )
         }
     }
 
-    if( pieces <= 0 ) {
-        p->add_msg_if_player(m_bad, _("Your clumsy butchering destroys the flesh!"));
-    } else {
-        p->add_msg_if_player(m_good, _("You harvest some flesh."));
-        const itype_id meat = corpse->get_meat_itype();
-        if( meat == "null" ) {
-            return;
-        }
+    //now handle the meat, if there is any
+    //bone_tainted is handled above (treated as a byproduct, not a meat)
+    if( pieces <= 0 && meat != "null" && meat != "bone_tainted" ) {
+        p->add_msg_if_player( m_bad, _( "Your clumsy butchering destroys the flesh!" ) );
+    } else if( meat!= "null" && meat != "bone_tainted" ) {
+        p->add_msg_if_player( m_good, _( "You harvest some flesh." ) );
 
         item chunk( meat, age );
         chunk.set_mtype( corpse );
 
-        // for now don't drop non-tainted parts overhaul of taint system to not require excessive item duplication
-        item parts( chunk.is_tainted() || chunk.has_flag( "CANNIBALISM" ) ? meat : "offal", age );
+        // for now don't drop tainted or cannibal. parts overhaul of taint system to not require excessive item duplication
+        bool make_offal = !chunk.is_tainted() && !chunk.has_flag( "CANNIBALISM" ) && meat != "veggy";
+        item parts( make_offal ? "offal" : meat, age );
         parts.set_mtype( corpse );
 
         g->m.add_item_or_charges( p->pos(), chunk );
