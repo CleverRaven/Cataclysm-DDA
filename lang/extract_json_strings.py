5a574349e76f752ce11a879a317785218e686628
BevapDin <tho_ki@gmx.de>
2016-06-25 11:12:32 +0200

    Extract reload_noise text from JSON for translation
    It's attempted to be translated in item_factory.cpp, so it needs extraction.

--- lang/extract_json_strings.py
+++ lang/extract_json_strings.py
@@ -624,6 +624,9 @@ def extract(item, infilename):
     if "text" in item:
         writestr(outfile, item["text"], **kwargs)
         wrote = True
+    if "reload_noise" in item:
+        writestr(outfile, item["reload_noise"], **kwargs)
+        wrote = True
     if "messages" in item:
         for message in item["messages"]:
             writestr(outfile, message, **kwargs)
