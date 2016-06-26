f934e87e9860206f0c1000fd428d7e3fb769c7f2
DLaboratory <dlaboratory@126.com>
2016-06-26 21:41:25 +0800

    astyle rng.cpp

--- src/rng.cpp
+++ src/rng.cpp
@@ -6,46 +6,49 @@ unsigned long long xorshift_state[2];
 
 unsigned long long xorshift_rng_u64()
 {
-	unsigned long long s1 = xorshift_state[0];
-	const unsigned long long s0 = xorshift_state[1];
-	const unsigned long long result = s0 + s1;
-	xorshift_state[0] = s0;
-	s1 ^= s1 << 23;
-	xorshift_state[1] = s1 ^ s0 ^ (s1 >> 18) ^ (s0 >> 5);
-	return result;
+    unsigned long long s1 = xorshift_state[0];
+    const unsigned long long s0 = xorshift_state[1];
+    const unsigned long long result = s0 + s1;
+    xorshift_state[0] = s0;
+    s1 ^= s1 << 23;
+    xorshift_state[1] = s1 ^ s0 ^ ( s1 >> 18 ) ^ ( s0 >> 5 );
+    return result;
 }
 
 long long xorshift_rng_s64()
 {
-	return xorshift_rng_u64() & 0x7FFFFFFFFFFFFFFFLL;
+    return xorshift_rng_u64() & 0x7FFFFFFFFFFFFFFFLL;
 }
 
 int xorshift_rng_s32()
 {
-	return xorshift_rng_u64() & 0x7FFFFFFF;
+    return xorshift_rng_u64() & 0x7FFFFFFF;
 }
 
 void init_xorshift_rng( int seed )
 {
-	// use linear congruential algorithm to feed the 128-bit state.
-	unsigned int x = seed, y = ( seed * 1103515245 + 12345 );
-	xorshift_state[0] = ( (unsigned long long)(x) << 32 ) + y;
-	x = ( y * 1103515245 + 12345 );
-	y = ( x * 1103515245 + 12345 );
-	xorshift_state[1] = ( (unsigned long long)(x) << 32 ) + y;
+    // use linear congruential algorithm to feed the 128-bit state.
+    unsigned int x = seed, y = ( seed * 1103515245 + 12345 );
+    xorshift_state[0] = ( ( unsigned long long )( x ) << 32 ) + y;
+    x = ( y * 1103515245 + 12345 );
+    y = ( x * 1103515245 + 12345 );
+    xorshift_state[1] = ( ( unsigned long long )( x ) << 32 ) + y;
 }
 
 long rng( long val1, long val2 )
 {
-	if ( val1 > val2 )
-		std::swap( val1, val2 );
-    return val1 + long( ( val2 - val1 + 1 ) * double( xorshift_rng_u64() / double( UINT64_MAX + 1.0 ) ) );
+    if( val1 > val2 ) {
+        std::swap( val1, val2 );
+    }
+    return val1 + long( ( val2 - val1 + 1 ) * double( xorshift_rng_u64() / double(
+                            UINT64_MAX + 1.0 ) ) );
 }
 
 double rng_float( double val1, double val2 )
 {
-	if ( val1 > val2 )
-		std::swap( val1, val2 );
+    if( val1 > val2 ) {
+        std::swap( val1, val2 );
+    }
     return val1 + ( val2 - val1 ) * double( xorshift_rng_u64() ) / double( UINT64_MAX + 1.0 );
 }
 
@@ -96,12 +99,12 @@ int divide_roll_remainder( double dividend, double divisor )
 // for better world seeding.
 unsigned long long fnv1a_hash( const unsigned char *str )
 {
-	const unsigned long long offset_basis = 14695981039346656037ULL, fnv_prime = 1099511628211ULL;
-	unsigned long long hash = offset_basis;
-	unsigned char c = *str++;
-	while ( c != '\0' ) {
-		hash = ( hash ^ c ) * fnv_prime;
-		c = *str++;
-	}
-	return hash;
+    const unsigned long long offset_basis = 14695981039346656037ULL, fnv_prime = 1099511628211ULL;
+    unsigned long long hash = offset_basis;
+    unsigned char c = *str++;
+    while( c != '\0' ) {
+        hash = ( hash ^ c ) * fnv_prime;
+        c = *str++;
+    }
+    return hash;
 }
