c946443eeb2243adc17369e14dc0a919ed4f72f4
DLaboratory <dlaboratory@126.com>
2016-06-26 22:35:05 +0800

    expose xorshift RNG functions in rng.h

--- src/rng.h
+++ src/rng.h
@@ -5,8 +5,12 @@
 
 #include <functional>
 
+// xorshift128+ RNG
 void init_xorshift_rng( int seed );
+unsigned long long xorshift_rng_u64();
+long long xorshift_rng_s64();
 int xorshift_rng_s32();
+
 long rng( long val1, long val2 );
 double rng_float( double val1, double val2 );
 bool one_in( int chance );
