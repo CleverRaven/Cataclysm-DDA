43320444c031e2daa7c52fdb9de089f81bd4eba2
codemime <codemime@gmail.com>
2016-07-03 22:31:03 +0300

    Dont add dropped items to the inventory

--- src/player.cpp
+++ src/player.cpp
@@ -10503,9 +10503,8 @@ bool player::takeoff( const item &it, std::list<item> *res )
         if( volume_carried() + it.volume() > volume_capacity_reduced_by( it.get_storage() ) ) {
             if( is_npc() || query_yn( _( "No room in inventory for your %s.  Drop it?" ), it.tname().c_str() ) ) {
                 drop( get_item_position( &it ) );
-            } else {
-                return false;
             }
+            return false;
         }
         inv.add_item_keep_invlet( it );
     } else {
