1c063a81f54b0788bef47e16a9c19f195056c783
mugling <mugling@users.noreply.github.com>
2016-08-18 11:00:43 +0100

    Dont display misleading training message

--- src/ranged.cpp
+++ src/ranged.cpp
@@ -564,9 +564,6 @@ int player::fire_gun( const tripoint &target, int shots, item& gun )
     moves -= time_to_fire( *this, *gun.type );
 
     practice( skill_gun, xp * get_skill_level( skill_gun ) );
-    if( shots > 0 && xp == 0 && one_in( 20 ) ) {
-        add_msg_if_player( m_info, _( "You'll need a more accurate gun to keep improving your aim." ) );
-    }
 
     return curshot;
 }
