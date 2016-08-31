00f92c4f77503a157f981f74a873fbacfe45f4a3
James Dobson <jim.i.dobson@gmail.com>
2016-09-01 00:41:33 +0100

    corrected spacing

--- src/overmap.cpp
+++ src/overmap.cpp
@@ -4320,7 +4320,7 @@ void overmap::open()
     std::string const terfilename = overmapbuffer::terrain_filename(loc.x, loc.y);
 
     using namespace std::placeholders;
-	const auto unserialise_reader = ( const std::function<void( std::istream & )> & ) std::bind( &overmap::unserialize, this, _1 );
+    const auto unserialise_reader = ( const std::function<void( std::istream & )> & ) std::bind( &overmap::unserialize, this, _1 );
     const auto unserialise_view_reader = ( const std::function<void( std::istream & )> & ) std::bind( &overmap::unserialize_view, this, _1 );
     if( read_from_file_optional( terfilename, unserialise_reader ) ) {
         read_from_file_optional( plrfilename, unserialise_view_reader );
