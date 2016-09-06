e6f04d7f4974e9a8f9da427772bba54a57d0e7f3
James Dobson <jim.i.dobson@gmail.com>
2016-09-06 22:09:33 +0100

    astyle regression

--- src/name.cpp
+++ src/name.cpp
@@ -164,7 +164,7 @@ void NameGenerator::load( JsonIn &jsin )
 void load_names_from_file( const std::string &filename )
 {
     using namespace std::placeholders;
-    read_from_file_jsonin( filename, 
-		std::bind( &NameGenerator::load, &NameGenerator::generator(), _1 ) );
+    read_from_file_jsonin( filename,
+                       std::bind( &NameGenerator::load, &NameGenerator::generator(), _1 ) );
 }
 
