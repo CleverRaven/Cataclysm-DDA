036eca26e1bb7d2acf2e0455fe05226f4bb51f21
Pierre-David Bélanger <pierredavidbelanger@gmail.com>
2016-09-19 17:27:27 -0400

    make the to_liter returns double

--- src/units.h
+++ src/units.h
@@ -257,9 +257,9 @@ inline constexpr value_type to_milliliter( const quantity<value_type, volume_in_
 }
 
 template<typename value_type>
-inline constexpr value_type to_liter( const quantity<value_type, volume_in_milliliter_tag> &v )
+inline constexpr double to_liter( const quantity<value_type, volume_in_milliliter_tag> &v )
 {
-    return v / from_milliliter<value_type>( 1000 );
+    return v / from_milliliter<double>( 1000 );
 }
 
 namespace literals
