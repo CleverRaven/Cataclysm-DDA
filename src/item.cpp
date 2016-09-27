bae8bf3ad73a3d92d75e1507591000489662606d
Izicata <ercr@mac.com>
2016-09-27 04:11:46 -0400

    Add unarmed_weapon flag description
    Knowing which weapons can be used with "unarmed" martial arts shouldn't require guessing or code-diving.

--- src/item.cpp
+++ src/item.cpp
@@ -1486,6 +1486,10 @@ std::string item::info( bool showtext, std::vector<iteminfo> &info ) const
         }
 
         //lets display which martial arts styles character can use with this weapon
+        if( has_flag( "UNARMED_WEAPON" ) ) {
+            info.push_back( iteminfo( "DESCRIPTION",
+                                      _( "This weapon <good>can be used with</good> <info>unarmed martial arts styles</info>." ) ) );
+
         const auto &styles = g->u.ma_styles;
         const std::string valid_styles = enumerate_as_string( styles.begin(), styles.end(),
         [ this ]( const matype_id &mid ) {
