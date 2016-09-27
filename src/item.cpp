9a1d51aa8c0119990e6e00e94faf0a1d59dcb94a
Izicata <ercr@mac.com>
2016-09-27 05:07:03 -0400

    tweak description, martial arts -> fighting
    Brawling isn't exactly a martial art, but it's still an unarmed style.

--- src/item.cpp
+++ src/item.cpp
@@ -1488,7 +1488,7 @@ std::string item::info( bool showtext, std::vector<iteminfo> &info ) const
         //lets display which martial arts styles character can use with this weapon
         if( has_flag( "UNARMED_WEAPON" ) ) {
             info.push_back( iteminfo( "DESCRIPTION",
-                                      _( "This weapon <good>can be used with</good> <info>unarmed martial arts styles</info>." ) ) );
+                                      _( "This weapon <good>can be used with</good> <info>unarmed fighting styles</info>." ) ) );
 
         const auto &styles = g->u.ma_styles;
         const std::string valid_styles = enumerate_as_string( styles.begin(), styles.end(),
