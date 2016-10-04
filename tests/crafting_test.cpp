b353cc3d337e9413848c230a8fbb66b973fe4fec
codemime <codemime@gmail.com>
2016-10-05 01:34:50 +0300

    Add unit tests for 'recipe_subset'

---
+++ tests/crafting_test.cpp
@@ -0,0 +1,98 @@
+#include "catch/catch.hpp"
+
+#include "crafting.h"
+#include "recipe_dictionary.h"
+
+TEST_CASE( "recipe_subset" ) {
+    recipe_subset subset;
+
+    REQUIRE( subset.size() == 0 );
+
+    GIVEN( "a recipe of rum" ) {
+        const recipe *r = &recipe_dict[ "brew_rum" ];
+
+        WHEN( "the recipe is included" ) {
+            subset.include( r );
+
+            THEN( "it's in the subset" ) {
+                CHECK( subset.size() == 1 );
+                CHECK( subset.contains( r ) );
+            }
+
+            THEN( "it has its default difficulty" ) {
+                CHECK( subset.get_custom_difficulty( r ) == r->difficulty );
+            }
+
+            THEN( "it's in the right category" ) {
+                const auto cat_recipes( subset.in_category( "CC_FOOD" ) );
+
+                CHECK( cat_recipes.size() == 1 );
+                CHECK( std::find( cat_recipes.begin(), cat_recipes.end(), r ) != cat_recipes.end() );
+            }
+
+            THEN( "it uses water" ) {
+                const auto comp_recipes( subset.of_component( "water" ) );
+
+                CHECK( comp_recipes.size() == 1 );
+                CHECK( std::find( comp_recipes.begin(), comp_recipes.end(), r ) != comp_recipes.end() );
+            }
+
+            AND_WHEN( "the subset is cleared" ) {
+                subset.clear();
+
+                THEN( "it's no longer in the subset" ) {
+                    CHECK( subset.size() == 0 );
+                    CHECK_FALSE( subset.contains( r ) );
+                }
+            }
+        }
+
+        WHEN( "the recipe is included with higher difficulty" ) {
+            subset.include( r, r->difficulty + 1 );
+
+            THEN( "it's harder to perform" ) {
+                CHECK( subset.get_custom_difficulty( r ) == r->difficulty + 1 );
+            }
+
+            AND_WHEN( "it's included again with default difficulty" ) {
+                subset.include( r );
+
+                THEN( "it recovers its normal difficulty" ) {
+                    CHECK( subset.get_custom_difficulty( r ) == r->difficulty );
+                }
+            }
+
+            AND_WHEN( "it's included again with lower difficulty" ) {
+                subset.include( r, r->difficulty - 1 );
+
+                THEN( "it becomes easier to perform" ) {
+                    CHECK( subset.get_custom_difficulty( r ) == r->difficulty - 1 );
+                }
+            }
+        }
+
+        WHEN( "the recipe is included with lower difficulty" ) {
+            subset.include( r, r->difficulty - 1 );
+
+            THEN( "it's easier to perform" ) {
+                CHECK( subset.get_custom_difficulty( r ) == r->difficulty - 1 );
+            }
+
+            AND_WHEN( "it's included again with default difficulty" ) {
+                subset.include( r );
+
+                THEN( "it's still easier to perform" ) {
+                    CHECK( subset.get_custom_difficulty( r ) == r->difficulty - 1 );
+                }
+            }
+
+            AND_WHEN( "it's included again with higher difficulty" ) {
+                subset.include( r, r->difficulty + 1 );
+
+                THEN( "it's still easier to perform" ) {
+                    CHECK( subset.get_custom_difficulty( r ) == r->difficulty - 1 );
+                }
+            }
+        }
+    }
+}
