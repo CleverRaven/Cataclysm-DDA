8fbc54ab2554b28c921d91e36a1d5fb28507cd72
mugling <mugling@users.noreply.github.com>
2016-10-07 23:11:06 +0100

    Scale repair times with damage (#18663)
    * Scale repair times with damage
    
    * Add assertion for repairing vehicle parts

--- src/veh_interact.cpp
+++ src/veh_interact.cpp
@@ -26,6 +26,7 @@
 #include <iterator>
 #include <algorithm>
 #include <numeric>
+#include <cassert>
 
 #ifdef _MSC_VER
 #include <math.h>
@@ -71,7 +72,12 @@ player_activity veh_interact::run( vehicle &veh, int x, int y )
             time = vp->install_time( g->u );
             break;
         case 'r':
-            time = pt->is_broken() ? vp->install_time( g->u ) : ( vp->repair_time( g->u ) * pt->base.damage() );
+            if( pt->is_broken() ) {
+                time = vp->install_time( g->u );
+            } else {
+                assert( pt->base.max_damage() > 0 ); // why repairing part that cannot be damaged?
+                time = vp->repair_time( g->u ) * double( pt->base.damage() ) / pt->base.max_damage();
+            }
             break;
         case 'o':
             time = vp->removal_time( g->u );
