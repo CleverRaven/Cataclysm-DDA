3fa13d4a7cbb7f539d821bee55c11b4d4fd53d6a
mugling <mugling@users.noreply.github.com>
2016-10-09 19:21:48 +0100

    Check for true unarmed combat

--- src/melee.cpp
+++ src/melee.cpp
@@ -151,7 +151,7 @@ int player::get_hit_weapon( const item &weap ) const
     ///\EFFECT_BASHING improves hit chance for bashing weapons
     ///\EFFECT_CUTTING improves hit chance for cutting weapons
     ///\EFFECT_STABBING improves hit chance for piercing weapons
-    auto bonus = get_skill_level( weap.melee_skill() );
+    auto bonus = get_skill_level( is_armed() ? weap.melee_skill() : skill_unarmed );
 
     // CQB bionic acts as a lower bound providing item uses a weapon skill
     if( bonus < BIO_CQB_LEVEL && has_active_bionic( "bio_cqb" ) ) {
@@ -522,7 +522,7 @@ double player::crit_chance( int roll_hit, int target_dodge, const item &weap ) c
     ///\EFFECT_CUTTING increases crit chance with cutting weapons
     ///\EFFECT_STABBING increases crit chance with piercing weapons
     ///\EFFECT_UNARMED increases crit chance with unarmed weapons
-    int sk = get_skill_level( weap.melee_skill() );
+    int sk = get_skill_level( is_armed() ? weap.melee_skill() : skill_unarmed );
     if( has_active_bionic( "bio_cqb" ) ) {
         sk = std::max( sk, BIO_CQB_LEVEL );
     }
