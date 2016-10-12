CleverRaven/Cataclysm-DDA

commit 686a60d4185b9865e063a8a3931abb4e92f7fce3

Implement gunmod_attach function

src/player.cpp
@@ -10921,21 +10921,6 @@ void player::use(int inventory_position)
         }
 
         invoke_item( used );
-    } else if (used->is_gunmod()) {
-        int gunpos = g->inv_for_filter( _("Select gun to modify:" ), [&used]( const item& e ) {
-            return e.gunmod_compatible( *used, false, false );
-        }, _( "You don't have compatible guns." ) );
-
-        if( gunpos == INT_MIN ) {
-            add_msg_if_player( m_info, _( "Never mind." ) );
-            return;
-        }
-
-        item& gun = i_at( gunpos );
-        if( gun.gunmod_compatible( *used ) ) {
-            gunmod_add( gun, *used );
-        }
-        return;
 
     } else if (used->is_bionic()) {
         if( install_bionics( *used->type ) ) {

src/item_factory.cpp
@@ -130,6 +130,10 @@ void Item_factory::finalize() {
             }
         }
 
+        if( obj.gunmod ) {
+            obj.use_methods.emplace( "GUNMOD_ATTACH", usage_from_string( "GUNMOD_ATTACH" ) );
+        }
+
         if( obj.engine && get_world_option<bool>( "NO_FAULTS" ) ) {
             obj.engine->faults.clear();
         }
@@ -449,6 +453,7 @@ void Item_factory::init()
     add_iuse( "GRANADE_ACT", &iuse::granade_act );
     add_iuse( "GRENADE_INC_ACT", &iuse::grenade_inc_act );
     add_iuse( "GUN_REPAIR", &iuse::gun_repair );
+    add_iuse( "GUNMOD_ATTACH", &iuse::gunmod_attach );
     add_iuse( "HACKSAW", &iuse::hacksaw );
     add_iuse( "HAIRKIT", &iuse::hairkit );
     add_iuse( "HAMMER", &iuse::hammer );

src/iuse.cpp
@@ -5642,6 +5642,33 @@ int iuse::gun_repair(player *p, item *it, bool, const tripoint& )
     return it->type->charges_to_use();
 }
 
+int iuse::gunmod_attach( player *p, item *it, bool, const tripoint& ) {
+    if( !it || !it->is_gunmod() ) {
+        debugmsg( "tried to attach non-gunmod" );
+        return 0;
+    }
+
+    if( !p ) {
+        return 0;
+    }
+
+    int gunpos = g->inv_for_filter( _( "Select gun to modify:" ), [&it]( const item &e ) {
+        return e.gunmod_compatible( *it, false, false );
+    }, _( "You don't have compatible guns." ) );
+
+    if( gunpos == INT_MIN ) {
+        add_msg( m_info, _( "Never mind." ) );
+        return 0;
+    }
+
+    item& gun = p->i_at( gunpos );
+    if( gun.gunmod_compatible( *it ) ) {
+        p->gunmod_add( gun, *it );
+    }
+
+    return 0;
+}
+
 int iuse::misc_repair(player *p, item *it, bool, const tripoint& )
 {
     if( !it->ammo_sufficient() ) {

src/iuse.h
@@ -170,6 +170,7 @@ public:
     int fishing_rod         ( player*, item*, bool, const tripoint& );
     int fish_trap           ( player*, item*, bool, const tripoint& );
     int gun_repair          ( player*, item*, bool, const tripoint& );
+    int gunmod_attach       ( player*, item*, bool, const tripoint& );
     int misc_repair         ( player*, item*, bool, const tripoint& );
     int rm13armor_off       ( player*, item*, bool, const tripoint& );
     int rm13armor_on        ( player*, item*, bool, const tripoint& );

commit 2a84f8f67f1236e782e95087496f00b4bdd679f1

Fix a bug with gunmods considered as guns (in #18627)
This fixes a bug introduced in 686a60d4185b9865e063a8a3931abb4e92f7fce3.

src/player.cpp
@@ -10884,7 +10884,7 @@ void player::use(int inventory_position)
         read(inventory_position);
         return;
 
-    } else if (used->is_gun()) {
+    } else if( used->is_gun() && !used->is_gunmod() ) {
         auto mods = used->gunmods();
 
         if( mods.empty() ) {