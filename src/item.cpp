b49c974c0206b61559a0973dd2fa5bbe4941af5f
mugling <mugling@users.noreply.github.com>
2016-10-13 01:23:15 +0100

    Fix regression in flag inheritance

--- src/item.cpp
+++ src/item.cpp
@@ -2591,10 +2591,10 @@ bool item::has_flag( const std::string &f ) const
 
     if( json_flag::get( f ).inherit() ) {
         for( const auto e : is_gun() ? gunmods() : toolmods() ) {
-            if( e->is_gun() ) {
-                return false; // gunmods fired separately do not contribute to base gun flags
+            // gunmods fired separately do not contribute to base gun flags
+            if( !e->is_gun() && e->has_flag( f ) ) {
+                return true;
             }
-            return e->has_flag( f );
         }
     }
 
