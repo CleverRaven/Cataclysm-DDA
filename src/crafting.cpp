4dd5150cbec0efe080ff8b9c82ba6d0effbe2d15
Oliver Jowett <oliver@mutability.co.uk>
2016-10-15 14:36:09 +0100

    Warn on long disassembly, include disassembly time in the confim message.
    There is a lot of unpredictable variation in disassembly times.
    It is surprising to do something that sounds "simple", like disassembling
    a rag, and only discover once it's in progress that it takes an hour.

--- src/crafting.cpp
+++ src/crafting.cpp
@@ -1132,8 +1132,10 @@ bool player::disassemble( item &obj, int pos, bool ground, bool interactive )
     }
 
     // last chance to back out
-    if( interactive && get_option<bool>( "QUERY_DISASSEMBLE" ) &&
-        !query_yn( _( "Really disassemble the %s?" ), obj.tname().c_str() ) ) {
+    if( interactive &&
+        ( get_option<bool>( "QUERY_DISASSEMBLE" ) || r.time > MOVES( MINUTES( 15 ) ) ) &&
+        !query_yn( _( "Disassembling the %s will take about %s. Continue?" ),
+                   obj.tname().c_str(), calendar::print_duration( r.time / 100 ).c_str() ) ) {
         return false;
     }
 
