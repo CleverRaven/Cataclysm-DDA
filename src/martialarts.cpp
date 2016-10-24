b5360ff085f96d2e3ea5322c573d78ec16f46a95
Coolthulhu <Coolthulhu@gmail.com>
2016-10-24 16:12:39 +0200

    Fix martial art buff stacking and decay

--- src/martialarts.cpp
+++ src/martialarts.cpp
@@ -264,13 +264,13 @@ public:
         // add_effect add the duration to an existing effect, but it must never be
         // above buff_duration, this keeps the old ma_buff behavior
         max_duration = buff.buff_duration;
-        dur_add_perc = 1;
+        dur_add_perc = 100;
         // each add_effect call increases the intensity by 1
         int_add_val = 1;
         // effect intensity increases by -1 each turn.
         int_decay_step = -1;
         int_decay_tick = 1;
-//        int int_dur_factor;
+        int_dur_factor = 0;
 //        bool main_parts_only;
 //        std::string resist_trait;
 //        std::string resist_effect;
