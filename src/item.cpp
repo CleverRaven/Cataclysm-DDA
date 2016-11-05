7bd10dd637d9ef88c03391aac495f8298f7cdc6f
Brett Dong <brett.browning.dong@gmail.com>
2016-11-05 22:19:21 +0800

    Display localized string of armor flags

--- src/item.cpp
+++ src/item.cpp
@@ -1545,7 +1545,7 @@ std::string item::info( bool showtext, std::vector<iteminfo> &info ) const
         for( const auto &e : flags ) {
             auto &f = json_flag::get( e );
             if( !f.info().empty() ) {
-                info.emplace_back( "DESCRIPTION", string_format( "* %s", f.info().c_str() ) );
+                info.emplace_back( "DESCRIPTION", string_format( "* %s", _( f.info().c_str() ) ) );
             }
         }
 
