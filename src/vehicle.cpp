1b78a16e88d9decf0966343ee51f1fc8951394b4
mugling <mugling@users.noreply.github.com>
2016-11-19 13:50:20 +0000

    Fix overspeed conditional

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -2927,7 +2927,7 @@ int vehicle::rpm( const vehicle_part &pt ) const
 
 bool vehicle::overspeed( const vehicle_part &pt ) const
 {
-    return pt.is_engine() ? rpm( pt ) <= pt.base.type->engine->redline : false;
+    return pt.is_engine() ? rpm( pt ) > pt.base.type->engine->redline : false;
 }
 
 int vehicle::load( const vehicle_part &pt ) const
