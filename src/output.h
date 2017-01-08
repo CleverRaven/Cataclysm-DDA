d990f8bce17cbf7a895d0b29c601d269bae34dee
BevapDin <Tho_Ki@gmx.de>
2017-01-08 12:19:45 +0100

    Move declaration of WINDOW_PTR into cursesdef.h
    The type needs to know about the WINDOW type.
    And it's tightly coupled with the curses system anyway.

--- src/output.h
+++ src/output.h
@@ -65,25 +65,6 @@ extern int FULL_SCREEN_HEIGHT; // height of "full screen" popups
 extern int OVERMAP_WINDOW_WIDTH; // width of overmap window
 extern int OVERMAP_WINDOW_HEIGHT; // height of overmap window
 
-struct delwin_functor {
-    void operator()( WINDOW *w ) const;
-};
-/**
- * A Wrapper around the WINDOW pointer, it automatically deletes the
- * window (see delwin_functor) when the variable gets out of scope.
- * This includes calling werase, wrefresh and delwin.
- * Usage:
- * 1. Acquire a WINDOW pointer via @ref newwin like normal, store it in a pointer variable.
- * 2. Create a variable of type WINDOW_PTR *on the stack*, initialize it with the pointer from 1.
- * 3. Do the usual stuff with window, print, update, etc. but do *not* call delwin on it.
- * 4. When the function is left, the WINDOW_PTR variable is destroyed, and its destructor is called,
- *    it calls werase, wrefresh and most importantly delwin to free the memory associated wit the pointer.
- * To trigger the delwin call earlier call some_window_ptr.reset().
- * To prevent the delwin call when the function is left (because the window is already deleted or, it should
- * not be deleted), call some_window_ptr.release().
- */
-typedef std::unique_ptr<WINDOW, delwin_functor> WINDOW_PTR;
-
 enum game_message_type : int {
     m_good,    /* something good happened to the player character, eg. health boost, increasing in skill */
     m_bad,      /* something bad happened to the player character, eg. damage, decreasing in skill */
