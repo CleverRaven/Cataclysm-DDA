c4f407b3a140caba027b1383feab9e082c85251e
Kevin Granade <kevin.granade@gmail.com>
2017-01-10 05:09:16 +0000

    Revert "RPM always zero if engines switched off"
    This reverts commit bd96aa76a9535adf65bc34dab011a9206ab0bead.

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -2925,7 +2925,7 @@ int vehicle::gear( const vehicle_part &pt ) const {
 
 int vehicle::rpm( const vehicle_part &pt ) const
 {
-    if( !( engine_on && pt.is_engine() ) ) {
+    if( !pt.is_engine() ) {
         return 0;
     }
     // @todo once stalling is implemented remove lower bound ensuring rpm above idle
