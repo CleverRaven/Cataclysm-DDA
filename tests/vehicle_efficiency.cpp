fd0c6c90b37400e27f859e16c0fa424ef8de13b1
Kevin Granade <kevin.granade@gmail.com>
2017-01-16 07:31:42 +0000

    Loosen test a bit and set more test values.

--- tests/vehicle_efficiency.cpp
+++ tests/vehicle_efficiency.cpp
@@ -37,17 +37,37 @@ void clear_game( const ter_id &terrain )
     g->m.build_map_cache( 0, true );
 }
 
+void set_vehicle_fuel( vehicle *v, float veh_fuel_mult ) {
+    for( size_t p = 0; p < v->parts.size(); p++ ) {
+        auto &pt = v->parts[ p ];
+
+       if( pt.is_battery() ) {
+            pt.ammo_set( "battery", pt.ammo_capacity() * veh_fuel_mult );
+        }
+
+        if( pt.is_tank() && v->type->parts[p].fuel != "null" ) {
+            float qty = pt.ammo_capacity() * veh_fuel_mult;
+            qty *= std::max( item::find_type( v->type->parts[p].fuel )->stack_size, 1 );
+            qty /= to_milliliter( units::legacy_volume_factor );
+            pt.ammo_set( v->type->parts[ p ].fuel, qty );
+        }
+    }
+}
+
+const float fuel_level = 0.01;
+
 long test_efficiency( const vproto_id &veh_id, const ter_id &terrain, int reset_velocity_turn, long min_dist, long max_dist )
 {
     clear_game( terrain );
 
     const tripoint starting_point( 60, 60, 0 );
-    vehicle *veh_ptr = g->m.add_vehicle( veh_id, starting_point, -90, 5, 0 );
+    vehicle *veh_ptr = g->m.add_vehicle( veh_id, starting_point, -90, 0, 0 );
     REQUIRE( veh_ptr != nullptr );
     if( veh_ptr == nullptr ) {
         return 0;
     }
 
+    set_vehicle_fuel( veh_ptr, fuel_level );
     vehicle &veh = *veh_ptr;
     veh.tags.insert( "IN_CONTROL_OVERRIDE" );
     veh.engine_on = true;
@@ -78,9 +98,10 @@ long test_efficiency( const vproto_id &veh_id, const ter_id &terrain, int reset_
         }
     }
 
-    CHECK( tiles_travelled >= min_dist );
-    CHECK( tiles_travelled <= max_dist );
-    return tiles_travelled;
+    long adjusted_tiles_travelled = tiles_travelled / fuel_level;
+    CHECK( adjusted_tiles_travelled >= min_dist );
+    CHECK( adjusted_tiles_travelled <= max_dist );
+    return adjusted_tiles_travelled;
 }
 
 void find_inner( std::string type, std::string terrain, int delay ) {
@@ -104,42 +125,36 @@ void find_efficiency( std::string type ) {
 
 void test_vehicle( std::string type, long pavement_target, long dirt_target, long pavement_target_w_stops, long dirt_target_w_stops ) {
     SECTION( type + " on pavement" ) {
-      for( int i = 0; i < 10; i++)
         test_efficiency( vproto_id( type ), ter_id( "t_pavement" ), -1, pavement_target * 0.8, pavement_target * 1.2 );
     }
     SECTION( type + " on dirt" ) {
-      for( int i = 0; i < 10; i++)
-        test_efficiency( vproto_id( type ), ter_id( "t_dirt" ), -1, dirt_target * 0.9, dirt_target * 1.1 );
+        test_efficiency( vproto_id( type ), ter_id( "t_dirt" ), -1, dirt_target * 0.8, dirt_target * 1.2 );
     }
     SECTION( type + " on pavement, full stop every 5 turns" ) {
-      for( int i = 0; i < 10; i++)
-        test_efficiency( vproto_id( type ), ter_id( "t_pavement" ), 5, pavement_target_w_stops * 0.9, pavement_target_w_stops * 1.1 );
+        test_efficiency( vproto_id( type ), ter_id( "t_pavement" ), 5, pavement_target_w_stops * 0.8, pavement_target_w_stops * 1.2 );
     }
     SECTION( type + " on dirt, full stop every 5 turns" ) {
-      for( int i = 0; i < 10; i++)
-        test_efficiency( vproto_id( type ), ter_id( "t_dirt" ), 5, dirt_target_w_stops * 0.9, dirt_target_w_stops * 1.1 );
+        test_efficiency( vproto_id( type ), ter_id( "t_dirt" ), 5, dirt_target_w_stops * 0.8, dirt_target_w_stops * 1.2 );
     }
 }
 
 TEST_CASE( "vehicle_efficiency", "[vehicle] [engine]" ) {
-  //test_vehicle( "beetle", 48518, 44531, 2372, 2175 );
-  //test_vehicle( "car", 48000, 31000, 2375, 1575 );
-  //test_vehicle( "car_sports", 51400, 31000, 2465, 1478 );
+  test_vehicle( "beetle", 937800, 886750, 47400, 43300 );
+  test_vehicle( "car", 907050, 595600, 47650, 31500 );
+  test_vehicle( "car_sports", 858200, 429150, 48500, 28850 );
   // Electric car seems to spawn with no charge.
   //test_vehicle( "electric_car", 300 );
-  //test_vehicle( "suv", 99000, 56500, 4950, 2725 );
-  //test_vehicle( "motorcycle", 7400, 4100, 575, 330 );
-  //test_vehicle( "quad_bike", 6600, 4500, 500, 330 );
-  //test_vehicle( "scooter", 6550, 6900, 550, 550 );
-  //test_vehicle( "superbike", 8200, 4167, 600, 300 );
-  //test_vehicle( "ambulance", 95000, 86000, 3650, 3200 );
-  //test_vehicle( "fire_engine", 97000, 92000, 4120, 3820 );
-  find_efficiency( "apc" );
-  find_efficiency( "humvee" );
-  test_vehicle( "fire_truck", 77077, 16972, 3201, 763 );
-  test_vehicle( "truck_swat", 80451, 11170, 3470, 745 );
-  test_vehicle( "tractor_plow", 55923, 56126, 2351, 2349 );
-  test_vehicle( "aapc-mg", 320207, 292725, 11843, 10152 );
-  //test_vehicle( "apc", 82000, 16000, 3200, 800 );
-  //test_vehicle( "humvee", 82000, 16000, 3200, 800 );
+  test_vehicle( "suv", 1953850, 1078100, 98800, 54300 );
+  test_vehicle( "motorcycle", 133850, 68450, 11350, 6450 );
+  test_vehicle( "quad_bike", 113800, 78600, 9700, 6450 );
+  test_vehicle( "scooter", 130550, 121900, 11150, 10400 );
+  test_vehicle( "superbike", 121950, 40700, 11650, 5700 );
+  test_vehicle( "ambulance", 1825150, 1647700, 72850, 63950 );
+  test_vehicle( "fire_engine", 1859050, 1752900, 82250, 77350 );
+  test_vehicle( "fire_truck", 1445700, 277700, 64350, 15550 );
+  test_vehicle( "truck_swat", 1553350, 206350, 69250, 15050 );
+  test_vehicle( "tractor_plow", 1045350, 1091050, 47000, 47000 );
+  test_vehicle( "aapc-mg", 6372750, 5765650, 236900, 203050 );
+  test_vehicle( "apc", 6379250, 5894700, 236550, 219450 );
+  test_vehicle( "humvee", 1873600, 962600, 79300, 42400 );
 }
