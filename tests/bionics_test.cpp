f9a8bba7d371ad06809ddfa0a9034a0521b63513
keyspace <keyspace@users.noreply.github.com>
2017-01-20 16:04:51 +0200

    tests: work up to car battery consumption (test fails!).
    Squashed:
    
    * check if given bionic id is valid - there's no check in low-level
      add_bionic(), it's done in install_bionic() during game;
    * try using appropriate functions where available;
    * `activated` denotes a an active/passive bionic, not if it's been
      activated - that's `toggled`;
    * clear bionics so one section failure doesn't cascade to the rest
      (only got one section now, but will be helpful in the future);
    * some REQUIREs got demoted to CHECKs while debugging the latter;
    * car battery test fails as expected - it's now a magazine, so
      not sure if should fix test or consumption code.

--- tests/bionics_test.cpp
+++ tests/bionics_test.cpp
@@ -1,52 +1,86 @@
 #include "catch/catch.hpp"
 
 #include "bionics.h"
+#include "item.h"
+#include "itype.h"
 #include "game.h"
 #include "player.h"
 
-// TODO: test: bio_battery bio_cable bio_furnace bio_reactor bio_advreactor ... anything with power_source
+// TODO: test: bio_batteries bio_cable bio_furnace bio_reactor bio_advreactor ... anything with power_source
 
 void give_and_activate( player &p, std::string const &bioid ) {
+    INFO( "bionic " + bioid + " is valid" );
+    REQUIRE( is_valid_bionic( bioid ) );
+    
     p.add_bionic( bioid );
-    INFO( "dummy has " + bioid + " bionic " );
+    INFO( "dummy has gotten " + bioid + " bionic " );
     REQUIRE( p.has_bionic( bioid ) );
 
     // shortcut: "last added bionic"
+    // @todo: rework to allow "linked" bionics
     const int bioindex = p.num_bionics() - 1;
-    const bionic &bio = p.my_bionics[bioindex];
+    const bionic &bio = p.bionic_at_index( bioindex );
     REQUIRE( bio.id == bioid );
 
-    // activate if not on by default
-    if( !( bionic_info( bioid ).activated ) ) {
+    // turn on if possible
+    if( bionic_info( bio.id ).toggled && !bio.powered ) {
         p.activate_bionic( bioindex );
-        INFO( "bionic " + bioid + " with index " + std::to_string( bioindex ) + " is active " );
-        // @todo: CHECK() when there's an interface
+        INFO( "bionic " + bio.id + " with index " + std::to_string( bioindex ) + " is active " );
+        REQUIRE( p.has_active_bionic( bioid ) );
     }
+
+    return;
+}
+
+void clear_bionics( player &p ) {
+    p.my_bionics.clear();
+    p.power_level = 0;
+    p.max_power_level = 0;
 
     return;
 }
 
 TEST_CASE( "bionics", "[bionics] [item]" ) {
     player &dummy = g->u;
-    //reset_bionics();
-    // TODO: remove all bionics for multi-SECTION?
+
+    // one section failing shouldn't affect the rest
+    clear_bionics( dummy );
 
     // Could be a SECTION, but prerequisite for many tests.
-    REQUIRE( dummy.max_power_level == 0 );
+    INFO( "no power capacity at first" );
+    CHECK( dummy.max_power_level == 0 );
+
     dummy.add_bionic( "bio_power_storage" );
-    REQUIRE( dummy.power_level == 0 );
+
+    INFO( "adding Power Storage CBM only increases capacity" );
+    CHECK( dummy.power_level == 0 );
     REQUIRE( dummy.max_power_level > 0 );
 
-    SECTION( "bio_battery" ) {
-        give_and_activate( dummy, "bio_battery" );
-        // SECTION( "Can consume regular batteries" ) {
-        //     INFO( "TODO" );
-        // }
-        // SECTION( "Can't consume UPS with charges" ) {
-        //     INFO( "TODO" );
-        // }
-        // SECTION( "Can't consume car battery with charges" ) {
-        //     INFO( "TODO" );
-        // }
+    SECTION( "bio_batteries" ) {
+        give_and_activate( dummy, "bio_batteries" );
+
+        constexpr int turn0 = 0;
+        constexpr long charges0 = -1;
+
+        INFO( "CAN consume old-school batteries" );
+        REQUIRE( dummy.can_consume( item( "battery", turn0, 1 ) ) );
+
+        item it;
+
+        it = item( "UPS_off", turn0, charges0 );
+        INFO( "CAN'T consume UPS without charges" );
+        REQUIRE( !dummy.can_consume( it ) );
+
+        it.charges = 1;
+        INFO( "CAN'T consume UPS with charges" );
+        REQUIRE( !dummy.can_consume( it ) );
+
+        it = item( "battery_car", turn0, charges0 );
+        INFO( "CAN'T consume car battery without charges" );
+        REQUIRE( !dummy.can_consume( it ) );
+
+        it.emplace_back( it.type->magazine->default_ammo, turn0, 1 );
+        INFO( "CAN'T consume car battery with charges" );
+        REQUIRE( !dummy.can_consume( it ) );
     }
 }
