9c13cb8ff03e9e763d059d7605e9f25956e700ba
keyspace <keyspace@users.noreply.github.com>
2017-01-20 14:14:34 +0200

    tests: minimal bionics test.
    Adding power + wrapper to add bionic by id.

--- tests/bionics_test.cpp
+++ tests/bionics_test.cpp
@@ -4,19 +4,49 @@
 #include "game.h"
 #include "player.h"
 
-// TODO: use BDD-style instead? https://github.com/philsquared/Catch/blob/master/docs/tutorial.md#bdd-style
-TEST_CASE( "bio_battery", "[bionic] [item]" ) {
-    player &dummy = g->u;
-    // TODO: give power storage bionic and tested bionic, turn it on - perhaps in a function
-    INFO( "Dummy has bio_battery bionic: %s" + std::to_string( dummy.has_bionic( "bio_battery" ) ) );
+// TODO: test: bio_battery bio_cable bio_furnace bio_reactor bio_advreactor ... anything with power_source
 
-        SECTION( "Can consume regular batteries" ) {
-            INFO( "TODO" );
-        }
-        SECTION( "Can't consume UPS with charges" ) {
-            INFO( "TODO" );
-        }
-        SECTION( "Can't consume car battery with charges" ) {
-            INFO( "TODO" );
-        }
+void give_and_activate( player &p, std::string const &bioid ) {
+    p.add_bionic( bioid );
+    INFO( "dummy has " + bioid + " bionic " );
+    REQUIRE( p.has_bionic( bioid ) );
+
+    // shortcut: "last added bionic"
+    const int bioindex = p.num_bionics() - 1;
+    const bionic &bio = p.my_bionics[bioindex];
+    REQUIRE( bio.id == bioid );
+
+    // activate if not on by default
+    if( !( bionic_info( bioid ).activated ) ) {
+        p.activate_bionic( bioindex );
+        INFO( "bionic " + bioid + " with index " + std::to_string( bioindex ) + " is active " );
+        // @todo: CHECK() when there's an interface
+    }
+
+    return;
+}
+
+TEST_CASE( "bionics", "[bionics] [item]" ) {
+    player &dummy = g->u;
+    //reset_bionics();
+    // TODO: remove all bionics for multi-SECTION?
+
+    // Could be a SECTION, but prerequisite for many tests.
+    REQUIRE( dummy.max_power_level == 0 );
+    dummy.add_bionic( "bio_power_storage" );
+    REQUIRE( dummy.power_level == 0 );
+    REQUIRE( dummy.max_power_level > 0 );
+
+    SECTION( "bio_battery" ) {
+        give_and_activate( dummy, "bio_battery" );
+        // SECTION( "Can consume regular batteries" ) {
+        //     INFO( "TODO" );
+        // }
+        // SECTION( "Can't consume UPS with charges" ) {
+        //     INFO( "TODO" );
+        // }
+        // SECTION( "Can't consume car battery with charges" ) {
+        //     INFO( "TODO" );
+        // }
+    }
 }
