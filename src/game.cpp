4dcec31fc2c6d79827b6bcede87ec0a8120d847a
Coolthulhu <Coolthulhu@gmail.com>
2017-03-07 11:35:05 +0100

    Remove needless migration

--- src/game.cpp
+++ src/game.cpp
@@ -121,8 +121,7 @@
 
 #define dbg(x) DebugLog((DebugLevel)(x),D_GAME) << __FILE__ << ":" << __LINE__ << ": "
 
-// core_version 6->7 is removal of construction skill
-const int core_version = 7;
+const int core_version = 6;
 
 /** Will be set to true when running unit tests */
 bool test_mode = false;
