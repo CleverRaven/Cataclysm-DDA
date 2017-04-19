44dc559e03aa1a0bcdfc712d03a1e468b0ebba10
BevapDin <Tho_Ki@gmx.de>
2017-04-19 13:54:41 +0200

    generate_bindings.lua: prefix enum values with the enum name
    to properly handle class enums, like `side`.

--- src/lua/generate_bindings.lua
+++ src/lua/generate_bindings.lua
@@ -694,7 +694,7 @@ for enum_name, values in pairs(enums) do
     cpp_output = cpp_output .. "template<>" .. br
     cpp_output = cpp_output .. "const "..cpp_name.."::EMap "..cpp_name.."::BINDINGS = {"..br
     for _, name in ipairs(values) do
-        cpp_output = cpp_output .. tab.."{\""..name.."\", "..name.."},"..br
+        cpp_output = cpp_output .. tab.."{\""..name.."\", "..enum_name.."::"..name.."},"..br
     end
     cpp_output = cpp_output .. "};" .. br
 end
