933397c25cbed37e4d8f89f8cc1a40e77beebe12
AMurkin <amurkin1988@yandex.ru>
2017-05-06 00:45:16 +0300

    Correct marker pos

--- src/overmap.cpp
+++ src/overmap.cpp
@@ -2328,8 +2328,8 @@ void overmap::draw(WINDOW *w, WINDOW *wbar, const tripoint &center,
          target.x >= offset_x + om_map_width ||
          target.y < offset_y ||
          target.y >= offset_y + om_map_height)) {
-        int marker_x = ( target.x >= cursx - om_half_width  ) ? ( ( target.x < cursx + om_half_width  ) ? target.x : om_map_width  - 1) : 0;
-        int marker_y = ( target.y >= cursy - om_half_height ) ? ( ( target.y < cursy + om_half_height ) ? target.y : om_map_height - 1) : 0;
+        int marker_x = ( target.x >= offset_x ) ? ( ( target.x < offset_x + om_map_width  ) ? target.x - offset_x : om_map_width  - 1) : 0;
+        int marker_y = ( target.y >= offset_y ) ? ( ( target.y < offset_y + om_map_height ) ? target.y - offset_y : om_map_height - 1) : 0;
         long marker_sym = ' ';
         switch (direction_from(cursx, cursy, target.x, target.y)) {
         case NORTH:
