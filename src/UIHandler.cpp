6fbf05ec2ce84ce9c54af29e25e713a227890e85
Hal Gentz <zegentzy@gmail.com>
2017-05-21 02:31:23 -0600

    Styling
    Signed-off-by: Hal Gentz <zegentzy@gmail.com>

--- src/UIHandler.cpp
+++ src/UIHandler.cpp
@@ -1,6 +1,7 @@
 #include "UIHandler.h"
 
-UIWindow::UIWindow(int minSizeX, int minSizeY, Location location, bool drawBorder) : m_panel(new UIPaddingPanel(drawBorder))
+UIWindow::UIWindow( int minSizeX, int minSizeY, Location location,
+                    bool drawBorder ) : m_panel( new UIPaddingPanel( drawBorder ) )
 {
     m_minSize.x = minSizeX;
     m_minSize.y = minSizeY;
@@ -14,40 +15,55 @@ int UIWindow::UpdateWindow()
 {
     // TODO: Clear prev contents? Is that our job?
 
-    m_thisSize = m_panel->RequestedSize(Sizes::Prefered);    
-    
-    point minSize = m_panel->RequestedSize(Sizes::Minimum);
+    m_thisSize = m_panel->RequestedSize( Sizes::Prefered );
 
-    if (m_minSize.x > m_thisSize.x) m_thisSize.x = m_minSize.x;
-    if (m_minSize.y > m_thisSize.y) m_thisSize.y = m_minSize.y;
+    point minSize = m_panel->RequestedSize( Sizes::Minimum );
 
-    if (m_thisSize.x > TERMX) m_thisSize.x = TERMX;
-    if (m_thisSize.y > TERMY) m_thisSize.y = TERMY;
+    if( m_minSize.x > m_thisSize.x ) {
+        m_thisSize.x = m_minSize.x;
+    }
+    if( m_minSize.y > m_thisSize.y ) {
+        m_thisSize.y = m_minSize.y;
+    }
 
-    if (m_minSize.x > TERMX) DebugLog(D_ERROR, DC_ALL) << "Not enought screen space...";
-    if (m_minSize.y > TERMY) DebugLog(D_ERROR, DC_ALL) << "Not enought screen space...";
+    if( m_thisSize.x > TERMX ) {
+        m_thisSize.x = TERMX;
+    }
+    if( m_thisSize.y > TERMY ) {
+        m_thisSize.y = TERMY;
+    }
 
-    if (minSize.x > TERMX) DebugLog(D_ERROR, DC_ALL) << "Not enought screen space...";
-    if (minSize.y > TERMY) DebugLog(D_ERROR, DC_ALL) << "Not enought screen space...";
+    if( m_minSize.x > TERMX ) {
+        DebugLog( D_ERROR, DC_ALL ) << "Not enought screen space...";
+    }
+    if( m_minSize.y > TERMY ) {
+        DebugLog( D_ERROR, DC_ALL ) << "Not enought screen space...";
+    }
 
-    m_panel->SetSize(m_thisSize);
+    if( minSize.x > TERMX ) {
+        DebugLog( D_ERROR, DC_ALL ) << "Not enought screen space...";
+    }
+    if( minSize.y > TERMY ) {
+        DebugLog( D_ERROR, DC_ALL ) << "Not enought screen space...";
+    }
 
-    switch (m_thisLocation)
-    {
-    case Location::Centered:
-        m_offset.x = (TERMX > m_thisSize.x) ? (TERMX - m_thisSize.x) / 2 : 0;
-        m_offset.y = (TERMY > m_thisSize.y) ? (TERMY - m_thisSize.y) / 2 : 0;
-        break;
+    m_panel->SetSize( m_thisSize );
 
-    default:
-        DebugLog(D_ERROR, DC_ALL) << "Please state the location...";
-        break;
+    switch( m_thisLocation ) {
+        case Location::Centered:
+            m_offset.x = ( TERMX > m_thisSize.x ) ? ( TERMX - m_thisSize.x ) / 2 : 0;
+            m_offset.y = ( TERMY > m_thisSize.y ) ? ( TERMY - m_thisSize.y ) / 2 : 0;
+            break;
+
+        default:
+            DebugLog( D_ERROR, DC_ALL ) << "Please state the location...";
+            break;
     }
 
     // set up window
-    m_wf_win = newwin(m_thisSize.y, m_thisSize.x, m_offset.y, m_offset.x);
+    m_wf_win = newwin( m_thisSize.y, m_thisSize.x, m_offset.y, m_offset.x );
     m_wf_winptr = WINDOW_PTR( m_wf_win );
-        
+
     m_lastSize = m_thisSize;
 
     m_lastLocation = m_thisLocation;
@@ -56,272 +72,274 @@ int UIWindow::UpdateWindow()
 
 void UIWindow::DrawEverything()
 {
-    werase(m_wf_win);
-    m_panel->DrawEverything(m_wf_win, { 0, 0 });
+    werase( m_wf_win );
+    m_panel->DrawEverything( m_wf_win, { 0, 0 } );
 }
 
 std::vector<std::shared_ptr<UIPanel>> UIPaddingPanel::GetChild() const
 {
-    return m_childPanels; 
+    return m_childPanels;
 }
 
-void UIPaddingPanel::AddChild(std::shared_ptr<UIPanel> panel)
-{   
-    if (!m_childPanels.empty())
-    {
-        DebugLog(D_ERROR, DC_ALL) << "Only supports one panel";
+void UIPaddingPanel::AddChild( std::shared_ptr<UIPanel> panel )
+{
+    if( !m_childPanels.empty() ) {
+        DebugLog( D_ERROR, DC_ALL ) << "Only supports one panel";
         return;
     }
-    m_childPanels.push_back(panel);
+    m_childPanels.push_back( panel );
 }
 
-void UIPaddingPanel::RemoveChild(size_t index)
+void UIPaddingPanel::RemoveChild( size_t index )
 {
-    if (index != m_childPanels.size() -1)
-        m_childPanels[index] = std::move(m_childPanels.back());
+    if( index != m_childPanels.size() - 1 ) {
+        m_childPanels[index] = std::move( m_childPanels.back() );
+    }
     m_childPanels.pop_back();
 }
 
-point UIPaddingPanel::RequestedSize(Sizes sizes)
+point UIPaddingPanel::RequestedSize( Sizes sizes )
 {
     point size;
-    
-    if (m_drawBorder) 
+
+    if( m_drawBorder )
         size += { 2, 2 };
 
-    if (m_childPanels.empty())
+    if( m_childPanels.empty() ) {
         return size;
+    }
 
-    auto paneSize = m_childPanels[0]->RequestedSize(sizes);
+    auto paneSize = m_childPanels[0]->RequestedSize( sizes );
     size += paneSize;
 
     return size;
 }
 
 // We are a simple border!
-int UIPaddingPanel::SetSize(point size)
+int UIPaddingPanel::SetSize( point size )
 {
     m_thisSize = size;
 
-    if (m_childPanels.empty())
+    if( m_childPanels.empty() ) {
         return 0;
+    }
     auto newSize = size;
-    
-    if (m_drawBorder)
+
+    if( m_drawBorder )
         newSize -= { 2, 2 };
-     
-    m_childPanels[0]->SetSize(newSize);
+
+    m_childPanels[0]->SetSize( newSize );
     return 0;
 }
 
-void UIPaddingPanel::DrawEverything(WINDOW *wf_win, point offset)
+void UIPaddingPanel::DrawEverything( WINDOW *wf_win, point offset )
 {
-    
-    if (m_drawBorder)
-        UIUtils::DrawBorder(wf_win, offset, m_thisSize);
 
-    if (!m_childPanels.empty())
-    {
+    if( m_drawBorder ) {
+        UIUtils::DrawBorder( wf_win, offset, m_thisSize );
+    }
+
+    if( !m_childPanels.empty() ) {
         point offset;
-        
-        if (m_drawBorder)
+
+        if( m_drawBorder )
             offset += { 1, 1 };
-        
-        m_childPanels[0]->DrawEverything(wf_win, offset);
+
+        m_childPanels[0]->DrawEverything( wf_win, offset );
     }
 
     m_lastSize = m_thisSize;
 }
 
 
-UIPaddingPanel::UIPaddingPanel(bool drawBorder)
+UIPaddingPanel::UIPaddingPanel( bool drawBorder )
 {
     m_drawBorder = drawBorder;
 }
 
 std::vector<std::shared_ptr<UIPanel>> UITabPanel::GetChild() const
 {
-    return m_childPanels; 
+    return m_childPanels;
 }
 
-void UITabPanel::AddChild(std::shared_ptr<UIPanel> panel)
+void UITabPanel::AddChild( std::shared_ptr<UIPanel> panel )
 {
-    AddChild(std::to_string(m_childPanels.size()), panel);
+    AddChild( std::to_string( m_childPanels.size() ), panel );
 }
 
-void UITabPanel::AddChild(std::string name, std::shared_ptr<UIPanel> panel)
+void UITabPanel::AddChild( std::string name, std::shared_ptr<UIPanel> panel )
 {
-    m_childPanels.push_back(panel);
-    m_childPanelNames.push_back(name);
+    m_childPanels.push_back( panel );
+    m_childPanelNames.push_back( name );
 }
 
-void UITabPanel::RemoveChild(size_t index)
+void UITabPanel::RemoveChild( size_t index )
 {
-    if (index != m_childPanels.size() -1)
-        m_childPanels[index] = std::move(m_childPanels.back());
+    if( index != m_childPanels.size() - 1 ) {
+        m_childPanels[index] = std::move( m_childPanels.back() );
+    }
     m_childPanels.pop_back();
 
-    if (index != m_childPanelNames.size() -1)
-        m_childPanelNames[index] = std::move(m_childPanelNames.back());
+    if( index != m_childPanelNames.size() - 1 ) {
+        m_childPanelNames[index] = std::move( m_childPanelNames.back() );
+    }
     m_childPanelNames.pop_back();
 }
 
-point UITabPanel::RequestedSize(Sizes sizes)
+point UITabPanel::RequestedSize( Sizes sizes )
 {
     point size;
-   
+
     size += { 0, 2 };
 
-    if (m_drawBorder) 
+    if( m_drawBorder )
         size += { 2, 2 };
 
-    if (m_childPanels.empty())
+    if( m_childPanels.empty() ) {
         return size;
-
-    auto paneSize = m_childPanels[m_currentTab]->RequestedSize(sizes);
-    size += paneSize;
-    
-    int len = 2;
-    
-    for (auto tx : m_childPanelNames)
-    {
-        len += utf8_width(tx) + 6;
     }
 
-    if (len > size.x) 
-        size.x = len;
+    auto paneSize = m_childPanels[m_currentTab]->RequestedSize( sizes );
+    size += paneSize;
 
-    query_yn(std::to_string(size.x).c_str());
-    query_yn(std::to_string(size.y).c_str());
+    int len = 2;
+
+    for( auto tx : m_childPanelNames ) {
+        len += utf8_width( tx ) + 6;
+    }
+
+    if( len > size.x ) {
+        size.x = len;
+    }
+
+    query_yn( std::to_string( size.x ).c_str() );
+    query_yn( std::to_string( size.y ).c_str() );
 
     return size;
 }
 
-int UITabPanel::SetSize(point size)
+int UITabPanel::SetSize( point size )
 {
     m_thisSize = size;
 
-    if (m_childPanels.empty())
+    if( m_childPanels.empty() ) {
         return 0;
+    }
     auto newSize = size;
-    
-    if (m_drawBorder)
+
+    if( m_drawBorder )
         newSize -= { 2, 2 };
-     
+
     newSize -= { 0, 2 };
-    
-    m_childPanels[m_currentTab]->SetSize(newSize);
+
+    m_childPanels[m_currentTab]->SetSize( newSize );
     return 0;
 }
 
-void UITabPanel::DrawEverything(WINDOW *wf_win, point offset)
+void UITabPanel::DrawEverything( WINDOW *wf_win, point offset )
 {
-    if (m_drawBorder)
-    {
+    if( m_drawBorder ) {
         auto bOffset = offset;
         bOffset += { 0, 2 };
 
         auto bSize = m_thisSize;
         bSize -= { 0, 2 };
 
-        UIUtils::DrawBorder(wf_win, bOffset, bSize);
+        UIUtils::DrawBorder( wf_win, bOffset, bSize );
     }
 
     int toffset = 2;
-    for (size_t i = 0; i < m_childPanels.size(); i++)
-    {
+    for( size_t i = 0; i < m_childPanels.size(); i++ ) {
         toffset += 1;
 
-        UIUtils::DrawTab(wf_win, offset, toffset, (i == m_currentTab), m_childPanelNames[i]);
-    
-        toffset += 5 + utf8_width(m_childPanelNames[i]);
+        UIUtils::DrawTab( wf_win, offset, toffset, ( i == m_currentTab ), m_childPanelNames[i] );
+
+        toffset += 5 + utf8_width( m_childPanelNames[i] );
     }
-    
-    if (!m_childPanels.empty())
-    {
+
+    if( !m_childPanels.empty() ) {
         point offset;
-        
-        if (m_drawBorder)
+
+        if( m_drawBorder )
             offset += { 1, 1 };
-        
+
         offset += { 0, 2 };
-        
-        m_childPanels[m_currentTab]->DrawEverything(wf_win, offset);
+
+        m_childPanels[m_currentTab]->DrawEverything( wf_win, offset );
     }
 
     m_lastSize = m_thisSize;
 }
 
-void UITabPanel::SwitchTab(size_t tab)
+void UITabPanel::SwitchTab( size_t tab )
 {
     m_currentTab = tab;
 
     // Regenerate size for children
-    SetSize(m_thisSize);
+    SetSize( m_thisSize );
 }
 
-UITabPanel::UITabPanel(bool drawBorder)
+UITabPanel::UITabPanel( bool drawBorder )
 {
     m_drawBorder = drawBorder;
 }
 
-void UIUtils::DrawBorder(WINDOW *wf_win, point offset, point m_thisSize)
+void UIUtils::DrawBorder( WINDOW *wf_win, point offset, point m_thisSize )
 {
     // Bottom and top border
-    for (int i = 1; i < m_thisSize.x - 1; i++) 
-    {
-        mvwputch(wf_win, offset.y                   , i + offset.x, BORDER_COLOR, LINE_OXOX);
-        mvwputch(wf_win, offset.y + m_thisSize.y - 1, i + offset.x, BORDER_COLOR, LINE_OXOX);
+    for( int i = 1; i < m_thisSize.x - 1; i++ ) {
+        mvwputch( wf_win, offset.y, i + offset.x, BORDER_COLOR, LINE_OXOX );
+        mvwputch( wf_win, offset.y + m_thisSize.y - 1, i + offset.x, BORDER_COLOR, LINE_OXOX );
     }
 
     // Right and left border
-    for (int i = 1; i < m_thisSize.y - 1; i++) 
-    {
-        mvwputch(wf_win, i + offset.y, offset.x                    , BORDER_COLOR, LINE_XOXO);
-        mvwputch(wf_win, i + offset.y, offset.x + m_thisSize.x - 1 , BORDER_COLOR, LINE_XOXO);
+    for( int i = 1; i < m_thisSize.y - 1; i++ ) {
+        mvwputch( wf_win, i + offset.y, offset.x, BORDER_COLOR, LINE_XOXO );
+        mvwputch( wf_win, i + offset.y, offset.x + m_thisSize.x - 1, BORDER_COLOR, LINE_XOXO );
     }
-        
+
     // Corners
-    mvwputch(wf_win, offset.y                    , offset.x                      , BORDER_COLOR, LINE_OXXO); // |^
-    mvwputch(wf_win, offset.y                    , offset.x + m_thisSize.x - 1   , BORDER_COLOR, LINE_OOXX); // ^|
-    mvwputch(wf_win, offset.y + m_thisSize.y - 1 , offset.x                      , BORDER_COLOR, LINE_XXOO); // |_
-    mvwputch(wf_win, offset.y + m_thisSize.y - 1 , offset.x + m_thisSize.x - 1   , BORDER_COLOR, LINE_XOOX); // _|
+    mvwputch( wf_win, offset.y, offset.x, BORDER_COLOR,
+              LINE_OXXO );                                         // |^
+    mvwputch( wf_win, offset.y, offset.x + m_thisSize.x - 1, BORDER_COLOR,
+              LINE_OOXX );                      // ^|
+    mvwputch( wf_win, offset.y + m_thisSize.y - 1, offset.x, BORDER_COLOR,
+              LINE_XXOO );                      // |_
+    mvwputch( wf_win, offset.y + m_thisSize.y - 1, offset.x + m_thisSize.x - 1, BORDER_COLOR,
+              LINE_XOOX ); // _|
 }
 
-void UIUtils::DrawTab(WINDOW *wf_win, point offset, int tabOffset, bool tabActive, std::string text)
+void UIUtils::DrawTab( WINDOW *wf_win, point offset, int tabOffset, bool tabActive,
+                       std::string text )
 {
-    int tabOffsetRight = tabOffset + utf8_width(text) + 1;
+    int tabOffsetRight = tabOffset + utf8_width( text ) + 1;
 
-    mvwputch(wf_win, offset.y    , offset.x + tabOffset       , c_ltgray, LINE_OXXO); // |^
-    mvwputch(wf_win, offset.y    , offset.x + tabOffsetRight  , c_ltgray, LINE_OOXX); // ^|
-    mvwputch(wf_win, offset.y + 1, offset.x + tabOffset       , c_ltgray, LINE_XOXO); // |
-    mvwputch(wf_win, offset.y + 1, offset.x + tabOffsetRight  , c_ltgray, LINE_XOXO); // |
+    mvwputch( wf_win, offset.y, offset.x + tabOffset, c_ltgray, LINE_OXXO );          // |^
+    mvwputch( wf_win, offset.y, offset.x + tabOffsetRight, c_ltgray, LINE_OOXX );     // ^|
+    mvwputch( wf_win, offset.y + 1, offset.x + tabOffset, c_ltgray, LINE_XOXO );      // |
+    mvwputch( wf_win, offset.y + 1, offset.x + tabOffsetRight, c_ltgray, LINE_XOXO ); // |
 
-    mvwprintz(wf_win, offset.y + 1, offset.x + tabOffset + 1, ( tabActive ) ? h_ltgray : c_ltgray, "%s", text.c_str() );
+    mvwprintz( wf_win, offset.y + 1, offset.x + tabOffset + 1, ( tabActive ) ? h_ltgray : c_ltgray,
+               "%s", text.c_str() );
 
-    for (int i = tabOffset + 1; i < tabOffsetRight; i++) 
-    {
-        mvwputch(wf_win, offset.y, offset.x + i, c_ltgray, LINE_OXOX);  // -
+    for( int i = tabOffset + 1; i < tabOffsetRight; i++ ) {
+        mvwputch( wf_win, offset.y, offset.x + i, c_ltgray, LINE_OXOX ); // -
     }
 
-    if (tabActive) 
-    {
-        mvwputch(wf_win, offset.y + 1, offset.x + tabOffset - 1     , h_ltgray, '<');
-        mvwputch(wf_win, offset.y + 1, offset.x + tabOffsetRight + 1, h_ltgray, '>');
+    if( tabActive ) {
+        mvwputch( wf_win, offset.y + 1, offset.x + tabOffset - 1, h_ltgray, '<' );
+        mvwputch( wf_win, offset.y + 1, offset.x + tabOffsetRight + 1, h_ltgray, '>' );
 
-        for(int i = tabOffset + 1; i < tabOffsetRight; i++) 
-        {
-            mvwputch(wf_win, offset.y + 2, offset.x + i, c_black, ' ');
+        for( int i = tabOffset + 1; i < tabOffsetRight; i++ ) {
+            mvwputch( wf_win, offset.y + 2, offset.x + i, c_black, ' ' );
         }
 
-        mvwputch(wf_win, offset.y + 2, offset.x + tabOffset,      c_ltgray, LINE_XOOX); // _|
-        mvwputch(wf_win, offset.y + 2, offset.x + tabOffsetRight, c_ltgray, LINE_XXOO); // |_
+        mvwputch( wf_win, offset.y + 2, offset.x + tabOffset,      c_ltgray, LINE_XOOX ); // _|
+        mvwputch( wf_win, offset.y + 2, offset.x + tabOffsetRight, c_ltgray, LINE_XXOO ); // |_
 
-    }
-    else 
-    {
-        mvwputch(wf_win, offset.y + 2, offset.x + tabOffset,      c_ltgray, LINE_XXOX); // _|_
-        mvwputch(wf_win, offset.y + 2, offset.x + tabOffsetRight, c_ltgray, LINE_XXOX); // _|_
+    } else {
+        mvwputch( wf_win, offset.y + 2, offset.x + tabOffset,      c_ltgray, LINE_XXOX ); // _|_
+        mvwputch( wf_win, offset.y + 2, offset.x + tabOffsetRight, c_ltgray, LINE_XXOX ); // _|_
     }
 
 }
--- src/UIHandler.h
+++ src/UIHandler.h
@@ -10,120 +10,122 @@
 #include "enums.h"
 #include <memory>
 
-enum class Sizes
-{
+enum class Sizes {
     Minimum,
     Prefered
 };
 
-class UIUtils 
+class UIUtils
 {
-public:
-    static void DrawBorder(WINDOW *wf_win, point offset, point m_thisSize);
-    static void DrawTab(WINDOW *wf_win, point offset, int tabOffset, bool tabActive, std::string text);
+    public:
+        static void DrawBorder( WINDOW *wf_win, point offset, point m_thisSize );
+        static void DrawTab( WINDOW *wf_win, point offset, int tabOffset, bool tabActive,
+                             std::string text );
 };
 
 class UIPanel
 {
-public:
-    virtual std::vector<std::shared_ptr<UIPanel>> GetChild() const = 0;
-    virtual void AddChild(std::shared_ptr<UIPanel> panel) = 0;
-    virtual void RemoveChild(size_t index) = 0;
+    public:
+        virtual std::vector<std::shared_ptr<UIPanel>> GetChild() const = 0;
+        virtual void AddChild( std::shared_ptr<UIPanel> panel ) = 0;
+        virtual void RemoveChild( size_t index ) = 0;
 
-    virtual point RequestedSize(Sizes sizes) = 0;
-    virtual int SetSize(point size) = 0;
+        virtual point RequestedSize( Sizes sizes ) = 0;
+        virtual int SetSize( point size ) = 0;
 
-    virtual void DrawEverything(WINDOW *wf_window, point offset) = 0;
+        virtual void DrawEverything( WINDOW *wf_window, point offset ) = 0;
 };
 
 class UIPaddingPanel : public UIPanel
 {
-public:
-    UIPaddingPanel(bool drawBorder);
-    std::vector<std::shared_ptr<UIPanel>> GetChild() const;
-    void AddChild(std::shared_ptr<UIPanel> panel);
+    public:
+        UIPaddingPanel( bool drawBorder );
+        std::vector<std::shared_ptr<UIPanel>> GetChild() const;
+        void AddChild( std::shared_ptr<UIPanel> panel );
 
-    // We swap this with the last then push back
-    // Previous Indexes are not maintained!!!
-    void RemoveChild(size_t index);
+        // We swap this with the last then push back
+        // Previous Indexes are not maintained!!!
+        void RemoveChild( size_t index );
 
-    point RequestedSize(Sizes sizes);
-    int SetSize(point size);
+        point RequestedSize( Sizes sizes );
+        int SetSize( point size );
 
-    void DrawEverything(WINDOW *wf_win, point offset);
-private:
-    point m_thisSize;
-    point m_lastSize;
+        void DrawEverything( WINDOW *wf_win, point offset );
+    private:
+        point m_thisSize;
+        point m_lastSize;
 
-    std::vector<std::shared_ptr<UIPanel>> m_childPanels;
-    bool m_drawBorder;
+        std::vector<std::shared_ptr<UIPanel>> m_childPanels;
+        bool m_drawBorder;
 };
 
 class UITabPanel : public UIPanel
 {
-public:
-    UITabPanel(bool drawBorder);
-    std::vector<std::shared_ptr<UIPanel>> GetChild() const;
-    
-    //Please don't, use one below and give it a proper name. 
-    void AddChild(std::shared_ptr<UIPanel> panel);
-    void AddChild(std::string name, std::shared_ptr<UIPanel> panel);
+    public:
+        UITabPanel( bool drawBorder );
+        std::vector<std::shared_ptr<UIPanel>> GetChild() const;
 
-    // We swap this with the last then push back
-    // Previous Indexes are not maintained!!!
-    void RemoveChild(size_t index);
+        //Please don't, use one below and give it a proper name.
+        void AddChild( std::shared_ptr<UIPanel> panel );
+        void AddChild( std::string name, std::shared_ptr<UIPanel> panel );
 
-    point RequestedSize(Sizes sizes);
-    int SetSize(point size);
+        // We swap this with the last then push back
+        // Previous Indexes are not maintained!!!
+        void RemoveChild( size_t index );
 
-    void DrawEverything(WINDOW *wf_win, point offset);
+        point RequestedSize( Sizes sizes );
+        int SetSize( point size );
 
-    void SwitchTab(size_t tab);
-private:
-    size_t m_currentTab;
+        void DrawEverything( WINDOW *wf_win, point offset );
 
-    point m_thisSize;
-    point m_lastSize;
+        void SwitchTab( size_t tab );
+    private:
+        size_t m_currentTab;
 
-    // Could use std::map
-    // But then GetChild function would get way more complex
-    // I think this is easier
-    std::vector<std::shared_ptr<UIPanel>> m_childPanels;
-    std::vector<std::string> m_childPanelNames;
+        point m_thisSize;
+        point m_lastSize;
 
-    bool m_drawBorder;
+        // Could use std::map
+        // But then GetChild function would get way more complex
+        // I think this is easier
+        std::vector<std::shared_ptr<UIPanel>> m_childPanels;
+        std::vector<std::string> m_childPanelNames;
+
+        bool m_drawBorder;
 };
 
-class UIWindow {
-public:
-    // TODO REMOVE EVENTUALY!!!
-    WINDOW* LegacyWindow() {return m_wf_win;};
+class UIWindow
+{
+    public:
+        // TODO REMOVE EVENTUALY!!!
+        WINDOW *LegacyWindow() {
+            return m_wf_win;
+        };
 
-    enum class Location
-    {
-        Centered,
+        enum class Location {
+            Centered,
 
-        Undefined = 0,
-        Total
-    };
+            Undefined = 0,
+            Total
+        };
 
-    UIWindow(int minSizeX, int minSizeY, Location location, bool drawBorder);
-    int UpdateWindow();
+        UIWindow( int minSizeX, int minSizeY, Location location, bool drawBorder );
+        int UpdateWindow();
 
-    void DrawEverything();
-    
-    std::unique_ptr<UIPanel> m_panel;
-private:
-    Location m_thisLocation;
-    Location m_lastLocation;
+        void DrawEverything();
 
-    point m_minSize;
-    point m_thisSize;
-    point m_lastSize;
-    point m_offset;
+        std::unique_ptr<UIPanel> m_panel;
+    private:
+        Location m_thisLocation;
+        Location m_lastLocation;
 
-    WINDOW *m_wf_win = nullptr;
-    WINDOW_PTR m_wf_winptr = nullptr;
+        point m_minSize;
+        point m_thisSize;
+        point m_lastSize;
+        point m_offset;
+
+        WINDOW *m_wf_win = nullptr;
+        WINDOW_PTR m_wf_winptr = nullptr;
 };
 
 #endif // UIHANDLER_H
