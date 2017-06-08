9a7d8b3ff9da0fed7aace7c54f1be0dee9b1436f
Alec White Valentine <doncuentacuentos@hotmail.com>
2017-06-08 17:24:58 +0200

    Fixed linker error in Visual Studio (#21204)
    * Fixed string linker error
    
    * Changed style

--- src/string_id_null_ids.cpp
+++ src/string_id_null_ids.cpp
@@ -2,7 +2,7 @@
 
 // These are centralized in this file because they must appear in one and only one compilation unit,
 // and it's problematic to define them in the respective cpp files for each class.
-// Very repitious, so define them with a macro.
+// Very repititious, so define them with a macro.
 #define MAKE_NULL_ID( type, ... ) \
 class type; \
 template<> const string_id<type> &string_id<type>::NULL_ID() { \
@@ -10,25 +10,34 @@ template<> const string_id<type> &string_id<type>::NULL_ID() { \
     return id; \
 }
 
-MAKE_NULL_ID( activity_type, "ACT_NULL", -1 );
-MAKE_NULL_ID( harvest_list);
-MAKE_NULL_ID( effect_type, "null");
-MAKE_NULL_ID( furn_t, "f_null", 0 );
-MAKE_NULL_ID( material_type, "null", 0 );
-MAKE_NULL_ID( MonsterGroup, "GROUP_NULL" );
-MAKE_NULL_ID( mtype, "mon_null" );
-MAKE_NULL_ID( mutation_branch );
-MAKE_NULL_ID( oter_t, "", 0 );
-MAKE_NULL_ID( oter_type_t, "", 0 );
-MAKE_NULL_ID( requirement_data, "null" );
-MAKE_NULL_ID( species_type, "spec_null" );
-MAKE_NULL_ID( ter_t, "t_null", 0 );
-MAKE_NULL_ID( trap, "tr_null" );
+MAKE_NULL_ID(activity_type, "ACT_NULL", -1);
+MAKE_NULL_ID(harvest_list);
+MAKE_NULL_ID(effect_type, "null");
+MAKE_NULL_ID(material_type, "null", 0);
 
-MAKE_NULL_ID( overmap_special, "", 0 );
-MAKE_NULL_ID( Skill, "none" );
-MAKE_NULL_ID( mission_type, "mission_type" );
-MAKE_NULL_ID( npc_class, "NC_NONE" );
-MAKE_NULL_ID( ammunition_type, "NULL" );
-MAKE_NULL_ID( vpart_info, "null" );
-MAKE_NULL_ID( emit, "null" );
+MAKE_NULL_ID(overmap_special, "", 0);
+MAKE_NULL_ID(Skill, "none");
+MAKE_NULL_ID(npc_class, "NC_NONE");
+MAKE_NULL_ID(ammunition_type, "NULL");
+MAKE_NULL_ID(vpart_info, "null");
+MAKE_NULL_ID(emit, "null");
+
+
+#define MAKE_NULL_ID2( type, ... ) \
+struct type; \
+template<> const string_id<type> &string_id<type>::NULL_ID() { \
+    static string_id<type> id = string_id<type>( __VA_ARGS__ ); \
+    return id; \
+}
+
+MAKE_NULL_ID2(mtype, "mon_null");
+MAKE_NULL_ID2(oter_t, "", 0);
+MAKE_NULL_ID2(oter_type_t, "", 0);
+MAKE_NULL_ID2(ter_t, "t_null", 0);
+MAKE_NULL_ID2(trap, "tr_null");
+MAKE_NULL_ID2(furn_t, "f_null", 0);
+MAKE_NULL_ID2(MonsterGroup, "GROUP_NULL");
+MAKE_NULL_ID2(mission_type, "mission_type");
+MAKE_NULL_ID2(species_type, "spec_null");
+MAKE_NULL_ID2(mutation_branch);
+MAKE_NULL_ID2(requirement_data, "null");
