ee427e650b6ccf084ca119bfcfdd02ca853988a9
AlecWhite <doncuentacuentos@hotmail.com>
2017-06-18 22:04:49 +0200

    Fixed linting

--- src/monexamine.cpp
+++ src/monexamine.cpp
@@ -10,14 +10,14 @@
 
 const efftype_id effect_milked( "milked" );
 
-void monexamine::milk_source( monster *source_mon )
+void monexamine::milk_source( monster &source_mon )
 {
     item milk( "milk", 0, 1 );
-    if(  source_mon->get_effect_dur( effect_milked ) < HOURS( 24 )  ) {
-        g->handle_liquid( milk, nullptr, 0, nullptr, nullptr, source_mon );
-        add_msg( _( "You milk the %s." ), source_mon->get_name().c_str() );
+    if(  source_mon.get_effect_dur( effect_milked ) < HOURS( 24 )  ) {
+        g->handle_liquid( milk, nullptr, 0, nullptr, nullptr, &source_mon );
+        add_msg( _( "You milk the %s." ), source_mon.get_name().c_str() );
     } else {
-        add_msg( _( "The %s's udders run dry." ), source_mon->get_name().c_str() );
+        add_msg( _( "The %s's udders run dry." ), source_mon.get_name().c_str() );
     }
 }
 
