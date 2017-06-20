CleverRaven/Cataclysm-DDA

commit 921cea091a330db6fa14be62f6fdf442934bc306

Hack to address mingw warning. (#21286)
This is a stab in the dark, I'm wondering if the compiler is attempting to unroll this loop and aliasing tmp to examp,

src/iexamine.cpp
@@ -1303,7 +1303,7 @@ void iexamine::fswitch(player &p, const tripoint &examp)
     }
     ter_id terid = g->m.ter(examp);
     p.moves -= 100;
-    tripoint tmp = examp;
+    tripoint tmp;
     for (tmp.y = examp.y; tmp.y <= examp.y + 5; tmp.y++ ) {
         for (tmp.x = 0; tmp.x < SEEX * MAPSIZE; tmp.x++) {
             if ( terid == t_switch_rg ) {

commit 4e529c311157ac1d5864008f19793ea10f8de16d

Set tripoint z member.
Fixes regression caused by #21286
Z member was previously set implicitly by initializing tmp from examp, but mingw didn't like that and warned about a bizarre optimization problem. This restores the functionality by initializing the z parameter.

src/iexamine.cpp
@@ -1304,6 +1304,7 @@ void iexamine::fswitch(player &p, const tripoint &examp)
     ter_id terid = g->m.ter(examp);
     p.moves -= 100;
     tripoint tmp;
+    tmp.z = examp.z;
     for (tmp.y = examp.y; tmp.y <= examp.y + 5; tmp.y++ ) {
         for (tmp.x = 0; tmp.x < SEEX * MAPSIZE; tmp.x++) {
             if ( terid == t_switch_rg ) {