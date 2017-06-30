ed3bcab768a46f38a7136ce3a768028d2c650b41
Coolthulhu <Coolthulhu@gmail.com>
2017-07-01 00:09:44 +0200

    Add useful messages

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -4369,11 +4369,13 @@ void iuse::cut_log_into_planks(player *p)
     int wasted_planks = max_planks - planks;
     int scraps = rng( wasted_planks, wasted_planks * 3 );
     planks = std::min( planks, max_planks );
+    p->i_add_or_drop( plank, planks );
+    p->i_add_or_drop( scrap, scraps );
+    p->add_msg_if_player( m_good, _( "You produce %d planks." ), planks );
+    p->add_msg_if_player( m_good, _( "You produce %d splinters." ), scraps );
     if( planks < max_planks / 2 ) {
         add_msg( m_bad, _("You waste a lot of the wood.") );
     }
-    p->i_add_or_drop( plank, planks );
-    p->i_add_or_drop( scrap, scraps );
 }
 
 int iuse::lumber(player *p, item *it, bool t, const tripoint& )
