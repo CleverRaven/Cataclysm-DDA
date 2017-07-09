23bc85ff63de783a86da603d73119792552ad11e
Brett Dong <brett.browning.dong@gmail.com>
2017-07-09 16:52:55 +0800

    Use clamp function to simplify the code

--- src/output.cpp
+++ src/output.cpp
@@ -1759,12 +1759,7 @@ get_hp_bar( const int cur_hp, const int max_hp, const bool is_mon )
 std::pair<std::string, nc_color> get_light_level( const float light )
 {
     using pair_t = std::pair<std::string, nc_color>;
-    int light_level = ceil( light );
-    if( light_level < 0 ) {
-        light_level = 0;
-    } else if( light_level > 5 ) {
-        light_level = 5;
-    }
+    const int light_level = clamp( ceil( light ), 0, 5 );
     switch( light_level ) {
         case 0:
             return pair_t{ _( "unknown" ), c_pink };
