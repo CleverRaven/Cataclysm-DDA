2b4520e69441073f60440d62cdc2d040649dea80
Vlad <vlad082006@yandex.ru>
2017-07-23 00:15:13 +0500

    Style fix 2

--- src/descriptions.cpp
+++ src/descriptions.cpp
@@ -90,7 +90,7 @@ c to describe creatures, f to describe furniture, t to describe terrain, esc/ent
 
         std::string signage = m.get_signage( p );
         if (signage.size() > 0) {
-            desc += string_format( _("\nSign: %s"), signage.c_str());
+            desc += string_format( _("\nSign: %s"), signage.c_str() );
         }
 
         werase( w_main );
