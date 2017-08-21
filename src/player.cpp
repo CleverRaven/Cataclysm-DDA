df0002843deb0b150984286464fa119b3c62e9a8
Night-Pryanik <Night_Pryanik@mail.ru>
2017-08-21 21:54:55 +0400

    Forgot about the bionic_id

--- src/player.cpp
+++ src/player.cpp
@@ -5868,7 +5868,7 @@ void player::suffer()
             else focus_pool --;
         }
         if( !( ( (worn_with_flag( "SUN_GLASSES" ) ) || worn_with_flag( "BLIND" ) ) && ( wearing_something_on( bp_eyes ) ) )
-            && !has_bionic( "bio_sunglasses" ) ) {
+            && !has_bionic( bionic_id( "bio_sunglasses" ) ) ) {
             add_msg( m_bad, _( "The sunlight is really irritating your eyes." ) );
             if( one_in(10) ) {
                 mod_pain(1);
