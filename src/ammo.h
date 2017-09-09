d62ce7982c23393894253e2a368310f7f4c0b4c9
BevapDin <Tho_Ki@gmx.de>
2017-09-09 12:20:17 +0200

    Translate the ammunition name when needed.

--- src/ammo.h
+++ src/ammo.h
@@ -13,15 +13,13 @@ class ammunition_type
         ammunition_type() = default;
         explicit ammunition_type( std::string name ) : name_( std::move( name ) ) { }
 
-        std::string const &name() const {
-            return name_;
-        }
+        std::string name() const;
 
         std::string const &default_ammotype() const {
             return default_ammotype_;
         }
+
     private:
-        // Localized name
         std::string name_;
         std::string default_ammotype_;
 
