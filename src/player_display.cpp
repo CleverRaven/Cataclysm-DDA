dc2633c99f61ce49f83a5bb3a1196c7038328ef7
Kevin Granade <kevin.granade@gmail.com>
2017-09-16 21:51:23 +0000

    Added line to hand encumbrance info about adjustment to aim speed.

--- src/player_display.cpp
+++ src/player_display.cpp
@@ -175,6 +175,8 @@ std::string get_encumbrance_description( const player &p, body_part bp, bool com
             s += reload_cost_text( ( eff_encumbrance / 10 ) * 15 );
             s += string_format( _( "Dexterity %+.1f when throwing items;\n" ), -( eff_encumbrance / 10.0f ) );
             s += melee_cost_text( eff_encumbrance / 2 );
+            s += "\n";
+            s += string_format( _( "Reduces aim speed of guns by %.1f." ), p.aim_speed_encumbrance_modifier() );
             break;
         case bp_leg_l:
         case bp_leg_r:
