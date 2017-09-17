21b616bc45c303cd5d6992400aaa05b5d8cbff9b
ryan <rslego@gmail.com>
2017-09-17 19:05:20 -0400

    Added threshold breach chance test

--- tests/mutation_test.cpp
+++ tests/mutation_test.cpp
@@ -17,7 +17,6 @@ void clear_mutations( player &p )
 // one they ultimately end up with depends on the order they were loaded from JSON
 void give_all_mutations( player &p, std::string category, bool include_postthresh )
 {
-    const std::vector<trait_id> &mutations = p.get_mutations();
     const std::vector<trait_id> category_mutations = mutations_category["MUTCAT_" + category];
 
     // Add the threshold mutation first
@@ -45,31 +44,71 @@ int get_total_category_strength( player &p )
     return total;
 }
 
+// Returns the list of mutations a player has as a string, for debugging
+std::string get_mutations_as_string( player &p )
+{
+    std::ostringstream s;
+    for (auto &m : p.get_mutations()) {
+        s << (std::string) m << " ";
+    }
+    return s.str();
+}
 
-TEST_CASE( "Having all mutations give correct highest category" )
+
+TEST_CASE( "Having all mutations give correct highest category", "[mutations]" )
 {
     player &dummy = g->u;
 
     for ( auto &cat : mutation_category_traits ) {
         auto cat_id = cat.second.category;
         
-        SECTION( cat_id ) {
-            GIVEN( "The player has all pre-threshold mutations for " + cat_id ) {
-                clear_mutations( dummy );
-                give_all_mutations( dummy, cat_id, false );
+        GIVEN( "The player has all pre-threshold mutations for " + cat_id ) {
+            clear_mutations( dummy );
+            give_all_mutations( dummy, cat_id, false );
 
-                THEN( cat_id + " is the strongest category" ) {
-                    CHECK( dummy.get_highest_category() == "MUTCAT_" + cat_id );
-                }
+            THEN( cat_id + " is the strongest category" ) {
+                INFO( "MUTATIONS: " << get_mutations_as_string( dummy ) );
+                CHECK( dummy.get_highest_category() == "MUTCAT_" + cat_id );
             }
+        }
 
-            GIVEN( "The player has all mutations for " + cat_id ) {
-                clear_mutations( dummy );
-                give_all_mutations( dummy, cat_id, true );
+        GIVEN( "The player has all mutations for " + cat_id ) {
+            clear_mutations( dummy );
+            give_all_mutations( dummy, cat_id, true );
 
-                THEN( cat_id + " is the strongest category" ) {
-                    CHECK( dummy.get_highest_category() == "MUTCAT_" + cat_id );
-                }
+            THEN( cat_id + " is the strongest category" ) {
+                INFO( "MUTATIONS: " << get_mutations_as_string( dummy ) );
+                CHECK( dummy.get_highest_category() == "MUTCAT_" + cat_id );
+            }
+        }
+    }
+}
+
+TEST_CASE( "Having all pre-threshold mutations gives a sensible threshold breach chance", "[mutations]" )
+{
+    player &dummy = g->u;
+
+    const float BREACH_CHANCE_MIN = 0.2f;
+    const float BREACH_CHANCE_MAX = 0.4f;
+
+    for ( auto &cat : mutation_category_traits ) {
+        auto cat_id = cat.second.category;
+        
+        GIVEN( "The player has all pre-threshold mutations for " + cat_id ) {
+            clear_mutations( dummy );
+            give_all_mutations( dummy, cat_id, false );
+
+            int category_strength = dummy.mutation_category_level["MUTCAT_" + cat_id];
+            int total_strength = get_total_category_strength( dummy );
+            float breach_chance = category_strength / (float) total_strength;
+
+            THEN( "Threshold breach chance is at least 0.2" ) {
+                INFO( "MUTATIONS: " << get_mutations_as_string( dummy ) );
+                CHECK( breach_chance >= BREACH_CHANCE_MIN );
+            }
+            THEN( "Threshold breach chance is at most 0.4" ) {
+                INFO( "MUTATIONS: " << get_mutations_as_string( dummy ) );
+                CHECK( breach_chance <= BREACH_CHANCE_MAX );
             }
         }
     }
