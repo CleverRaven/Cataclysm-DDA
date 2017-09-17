b31b2018563170c34572c446e71cfc16146549f7
ryan <rslego@gmail.com>
2017-09-17 19:29:33 -0400

    astyle

--- tests/mutation_test.cpp
+++ tests/mutation_test.cpp
@@ -7,7 +7,7 @@
 
 void clear_mutations( player &p )
 {
-    while ( !p.get_mutations().empty() ) {
+    while( !p.get_mutations().empty() ) {
         // Use the "real" way to remove mutations to ensure we don't get into a bad state
         p.remove_mutation( p.get_mutations()[0] );
     }
@@ -20,14 +20,14 @@ void give_all_mutations( player &p, std::string category, bool include_postthres
     const std::vector<trait_id> category_mutations = mutations_category["MUTCAT_" + category];
 
     // Add the threshold mutation first
-    if (include_postthresh) {
-        p.set_mutation(trait_id( "THRESH_" + category ));
+    if( include_postthresh ) {
+        p.set_mutation( trait_id( "THRESH_" + category ) );
     }
 
-    for (auto &m : category_mutations) {
+    for( auto &m : category_mutations ) {
         const auto &mdata = m.obj();
-        if ( include_postthresh || (!mdata.threshold && mdata.threshreq.empty()) ) {
-            while ( p.mutation_ok( m, false, false ) ) {
+        if( include_postthresh || ( !mdata.threshold && mdata.threshreq.empty() ) ) {
+            while( p.mutation_ok( m, false, false ) ) {
                 p.mutate_towards( m );
             }
         }
@@ -37,7 +37,7 @@ void give_all_mutations( player &p, std::string category, bool include_postthres
 int get_total_category_strength( player &p )
 {
     int total = 0;
-    for (auto &i : p.mutation_category_level) {
+    for( auto &i : p.mutation_category_level ) {
         total += i.second;
     }
 
@@ -48,8 +48,8 @@ int get_total_category_strength( player &p )
 std::string get_mutations_as_string( player &p )
 {
     std::ostringstream s;
-    for (auto &m : p.get_mutations()) {
-        s << (std::string) m << " ";
+    for( auto &m : p.get_mutations() ) {
+        s << ( std::string ) m << " ";
     }
     return s.str();
 }
@@ -59,9 +59,9 @@ TEST_CASE( "Having all mutations give correct highest category", "[mutations]" )
 {
     player &dummy = g->u;
 
-    for ( auto &cat : mutation_category_traits ) {
+    for( auto &cat : mutation_category_traits ) {
         auto cat_id = cat.second.category;
-        
+
         GIVEN( "The player has all pre-threshold mutations for " + cat_id ) {
             clear_mutations( dummy );
             give_all_mutations( dummy, cat_id, false );
@@ -84,23 +84,24 @@ TEST_CASE( "Having all mutations give correct highest category", "[mutations]" )
     }
 }
 
-TEST_CASE( "Having all pre-threshold mutations gives a sensible threshold breach chance", "[mutations]" )
+TEST_CASE( "Having all pre-threshold mutations gives a sensible threshold breach chance",
+           "[mutations]" )
 {
     player &dummy = g->u;
 
     const float BREACH_CHANCE_MIN = 0.2f;
     const float BREACH_CHANCE_MAX = 0.4f;
 
-    for ( auto &cat : mutation_category_traits ) {
+    for( auto &cat : mutation_category_traits ) {
         auto cat_id = cat.second.category;
-        
+
         GIVEN( "The player has all pre-threshold mutations for " + cat_id ) {
             clear_mutations( dummy );
             give_all_mutations( dummy, cat_id, false );
 
             int category_strength = dummy.mutation_category_level["MUTCAT_" + cat_id];
             int total_strength = get_total_category_strength( dummy );
-            float breach_chance = category_strength / (float) total_strength;
+            float breach_chance = category_strength / ( float ) total_strength;
 
             THEN( "Threshold breach chance is at least 0.2" ) {
                 INFO( "MUTATIONS: " << get_mutations_as_string( dummy ) );
