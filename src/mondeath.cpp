80e12abf817d26ecf676019c452ce35de96da19a
AncientSion <rolandthegunslinger@web.de>
2017-10-10 21:20:45 +0200

    lint, comment
    lint, comment

--- src/mondeath.cpp
+++ src/mondeath.cpp
@@ -165,15 +165,13 @@ void mdeath::acid(monster *z)
     g->m.add_field(z->pos(), fd_acid, 3, 0);
 }
 
-void mdeath::acidburst(monster *z)
+void mdeath::acidburst(monster *z) // makes monster w/ flag spawn acid upon death
 {
     if (g->u.sees(*z)) {
         add_msg(m_mixed, _("The %s bursts, showering the nearby area with acid."), z->name().c_str());
     }
-
     const tripoint origin = z->pos();
-    const int radius = 2;
-
+    const int radius = 2; // 2 so ranged attacks arent safe per se
     g->m.add_field(origin, fd_acid, 3, 0);
 
     for (int i = 0; i < rng(8, 11); i++) {
