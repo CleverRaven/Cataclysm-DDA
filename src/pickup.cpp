e6a8a0078065c8fc1a88b8607bd0f4d6d40d1b17
angoddu <andrew.goddu@gmail.com>
2017-10-16 13:30:49 -0400

    Accidental change fixed

--- src/pickup.cpp
+++ src/pickup.cpp
@@ -78,7 +78,7 @@ interact_results interact_with_vehicle( vehicle *veh, const tripoint &pos,
     const bool has_purify = ( veh->part_with_feature( veh_root_part, "WATER_PURIFIER" ) >= 0 );
     const bool has_controls = ( ( veh->part_with_feature( veh_root_part, "CONTROLS" ) >= 0 ) ||
                                 ( veh->part_with_feature( veh_root_part, "CTRL_ELECTRONIC" ) >= 0 ) );
-    const int cargo_part = veh->part_with_feature( veh_root_part, "CARGO" );
+    const int cargo_part = veh->part_with_feature( veh_root_part, "CARGO", false );
     const bool from_vehicle = veh && cargo_part >= 0 && !veh->get_items( cargo_part ).empty();
     const bool can_be_folded = veh->is_foldable();
     const bool is_convertible = ( veh->tags.count( "convertible" ) > 0 );
