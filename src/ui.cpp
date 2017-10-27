39f67aadce5d5a25e5cd0977e9dd88885fb6d098
BevapDin <Tho_Ki@gmx.de>
2017-10-27 18:22:28 +0200

    Fix using boolean as parameter to string formatting function
    It doesn't support boolean directly, and it does not convert them to int automatically.

--- src/ui.cpp
+++ src/ui.cpp
@@ -336,7 +336,7 @@ void uimenu::setup()
 
     if ( desc_enabled && !(w_auto && h_auto) ) {
         desc_enabled = false; // give up
-        debugmsg("desc_enabled without w_auto and h_auto (h: %d, w: %d)", h_auto, w_auto);
+        debugmsg( "desc_enabled without w_auto and h_auto (h: %d, w: %d)", static_cast<int>( h_auto ), static_cast<int>( w_auto ) );
     }
 
     max_entry_len = 0;
