CleverRaven/Cataclysm-DDA

commit 69e46d57fdce854202f8c872119e73ca86009152

Cleanup

src/translations.cpp
@@ -49,7 +49,6 @@ const char *npgettext( const char *const context, const char *const msgid,
 
 bool isValidLanguage( const std::string &lang )
 {
-    debugmsg( "%s", lang.c_str() );
     const auto languages = get_options().get_option( "USE_LANG" ).getItems();
     return std::find_if( languages.begin(),
     languages.end(), [&lang]( const std::pair<std::string, std::string> &pair ) {

src/main.cpp
@@ -405,7 +405,7 @@ int main(int argc, char *argv[])
     setupDebug();
 
     if (setlocale(LC_ALL, "") == NULL) {
-        DebugLog(D_WARNING, D_MAIN) << "Error while setlocale(LC_ALL, ''). ";
+        DebugLog(D_WARNING, D_MAIN) << "Error while setlocale(LC_ALL, '').";
     } else {
         try {
             std::locale::global( std::locale( "" ) );
@@ -496,9 +496,7 @@ int main(int argc, char *argv[])
         }
     }
 #endif
-
-    if( get_option<std::string>( "USE_LANG" ).empty() && ( lang.empty() ||
-        getenv( "LANGUAGE" ) == NULL || !isValidLanguage( lang ) ) ) {
+    if( get_option<std::string>( "USE_LANG" ).empty() && ( lang.empty() || !isValidLanguage( lang ) ) ) {
         select_language();
         set_language();
     }

commit 12f1a8132def6df5d0c50c35329ebc66abe43928

Fix unused variables on macOS
Build error introduced in #22176

src/translations.cpp
@@ -172,12 +172,12 @@ void set_language()
 #include <cstring> // strcmp
 #include <map>
 
-bool isValidLanguage( const std::string &lang )
+bool isValidLanguage( const std::string &/*lang*/ )
 {
     return true;
 }
 
-std::string getLangFromLCID( const int &lcid )
+std::string getLangFromLCID( const int &/*lcid*/ )
 {
     return "";
 }