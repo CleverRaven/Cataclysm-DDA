6c9358f1f9a9c16de762eda4147b74f3727cdec9
BevapDin <Tho_Ki@gmx.de>
2017-12-03 19:19:21 +0100

    Explicitly use the catacurses namespace when calling erase().

--- src/main.cpp
+++ src/main.cpp
@@ -578,7 +578,7 @@ void exit_handler(int s)
     const int old_timeout = inp_mngr.get_timeout();
     inp_mngr.reset_timeout();
     if (s != 2 || query_yn(_("Really Quit? All unsaved changes will be lost."))) {
-        erase(); // Clear screen
+        catacurses::erase(); // Clear screen
 
         deinitDebug();
 
