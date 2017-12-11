d19db61bfbf1531fd87ea5a2bd554f7bb8aba30e
Anton Burmistrov <Night_Pryanik@mail.ru>
2017-12-11 21:22:29 +0300

    Replace g->u with u; properly truncate long messages

--- src/game.cpp
+++ src/game.cpp
@@ -7854,12 +7854,10 @@ void game::print_terrain_info( const tripoint &lp, WINDOW *w_look, int column, i
         wprintz(w_look, ll.second, ll.first.c_str());
     }
 
-    std::string signage = g->u.has_trait( trait_ILLITERATE ) ? _( "???" ) : m.get_signage( lp );
-    if( !signage.empty() && signage.size() < 36 ) {
-        mvwprintw( w_look, ++line, column, _( "Sign: %s" ), signage.c_str() );
-    } else if( !signage.empty() ) {
-        // Truncate to width of window as a guesstimate.
-        mvwprintw( w_look, ++line, column, _( "Sign: %s..." ), signage.substr( 0, 32 ).c_str() );
+    const int max_width = getmaxx( w_look ) - column - 1;
+    std::string signage = u.has_trait( trait_ILLITERATE ) ? _( "???" ) : m.get_signage( lp );
+    if( !signage.empty() ) {
+        trim_and_print( w_look, ++line, column, max_width, light_gray, _( "Sign: %s" ), signage.c_str() );
     }
 
     if( m.has_zlevels() && lp.z > -OVERMAP_DEPTH && !m.has_floor( lp ) ) {
