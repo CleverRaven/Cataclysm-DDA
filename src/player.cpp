267cf7b735f78bfb5b29aa35f1fb262ff351b55e
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2018-01-05 19:21:09 +0300

    Removed bionics from fire-related if-else

--- src/player.cpp
+++ src/player.cpp
@@ -6819,12 +6819,6 @@ bool player::has_fire( const int quantity ) const
                 return true;
             }
         }
-    } else if( has_active_bionic( bio_tools ) && power_level > quantity * 5 ) {
-        return true;
-    } else if( has_bionic( bio_lighter ) && power_level > quantity * 5 ) {
-        return true;
-    } else if( has_bionic( bio_laser ) && power_level > quantity * 5 ) {
-        return true;
     } else if( is_npc() ) {
         // A hack to make NPCs use their molotovs
         return true;
@@ -6853,15 +6847,6 @@ void player::use_fire(const int quantity)
                 return;
             }
         }
-    } else if( has_active_bionic( bio_tools ) && power_level > quantity * 5 ) {
-        charge_power( -quantity * 5 );
-        return;
-    } else if( has_bionic( bio_lighter ) && power_level > quantity * 5 ) {
-        charge_power( -quantity * 5 );
-        return;
-    } else if( has_bionic( bio_laser ) && power_level > quantity * 5 ) {
-        charge_power( -quantity * 5 );
-        return;
     }
 }
 
