2f8c1e28e20e610f96c2f80ed740eb8d0a29a703
Firestorm01X2 <firestorm01x2@gmail.com>
2018-01-11 22:32:51 +0300

    refactoring and comments
    refactoring and comments

--- src/ranged.cpp
+++ src/ranged.cpp
@@ -1575,9 +1575,11 @@ dispersion_sources player::get_weapon_dispersion( const item &obj ) const
 
     if( get_option< bool >( "IMPROVED_RANGED_ACCURACY" ) )
     {
-        double cbmBonus =  has_bionic( bionic_id( "bio_targeting" ) ) ? 2 : 0;
-        double skillEffect = 1 - std::min( ( double( get_skill_level( skill_gun ) + double( get_skill_level(
-            obj.gun_skill() ) ) ) ) / 2 + cbmBonus, double( MAX_SKILL ) ) * 0.0833;
+        double cbmBonus =  has_bionic( bionic_id( "bio_targeting" ) ) ? 2 : 0;// insalled cbm work as bonus skill levels
+        double perSkillMult = 0.0833;// = 0.25/3 to get one bio_targeting effect every 3 levels of avgSkill
+        double avgSkill = ( double( get_skill_level( skill_gun ) + double( get_skill_level(
+                                     obj.gun_skill() ) ) ) ) / 2;
+        double skillEffect = 1 - std::min( avgSkill + cbmBonus, double( MAX_SKILL ) ) * perSkillMult;
 
         dispersion.add_multiplier( skillEffect );
     }
