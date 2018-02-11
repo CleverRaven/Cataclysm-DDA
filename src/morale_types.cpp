90110b047bb3172280b00e69702ec856c493f39c
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2018-02-11 16:33:32 +0300

    Removed new morale type from legacy morales

--- src/morale_types.cpp
+++ src/morale_types.cpp
@@ -73,8 +73,6 @@ const morale_type &morale_type_data::convert_legacy( int lmt )
 
             morale_type( "morale_perm_filthy" ),
 
-            morale_type( "morale_perm_debug" ),
-
             morale_type( "morale_null" )
         }
     };
