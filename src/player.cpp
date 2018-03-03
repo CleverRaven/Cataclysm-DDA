22515f5fa97415a716310536348b26d2036703b7
ymber <mtaylor1252dev@gmail.com>
2018-03-03 18:54:58 +0000

    astyle new code

--- src/player.cpp
+++ src/player.cpp
@@ -7758,10 +7758,11 @@ bool player::wield( item& target )
     }
 
     // Query whether to draw an item from a holster when attempting to wield the holster
-    if( target.get_use( "holster" ) && ( target.contents.size() > 0 ) ) {
+    if( target.get_use( "holster" ) && ( target.contents.size() > 0 ) )
+    {
         if( query_yn( string_format( _( "Draw %s from %s?" ),
-                                     target.get_contained().tname().c_str(),
-                                     target.tname().c_str()) ) ) {
+                                    target.get_contained().tname().c_str(),
+                                    target.tname().c_str() ) ) ) {
             invoke_item( &target );
             return true;
         }
