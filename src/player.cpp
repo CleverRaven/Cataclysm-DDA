2c2f96d7115c23514199f04fc6bca00274d4f6af
Ruslan K <rkay94@gmail.com>
2018-03-18 06:29:39 -0400

    This seems to be the originally intended behavior

--- src/player.cpp
+++ src/player.cpp
@@ -8493,9 +8493,11 @@ bool player::takeoff( const item &it, std::list<item> *res )
         if( volume_carried() + it.volume() > volume_capacity_reduced_by( it.get_storage() ) ) {
             if( is_npc() || query_yn( _( "No room in inventory for your %s.  Drop it?" ), it.tname().c_str() ) ) {
                 drop( get_item_position( &it ) );
-                return true;
             }
-            return false;
+            else
+            {
+                return false;
+            }
         }
         iter->on_takeoff( *this );
         inv.add_item_keep_invlet( it );
