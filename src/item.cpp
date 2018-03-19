a4cdaf9cd1425f66df1a02cc47caaf22be1c6c08
Kevin Granade <kevin.granade@gmail.com>
2018-03-19 00:57:40 +0000

    Refactor reload_option a bit.

--- src/item.cpp
+++ src/item.cpp
@@ -4685,27 +4685,15 @@ item::reload_option &item::reload_option::operator=( const reload_option &rhs )
     return *this;
 }
 
-item::reload_option::reload_option( const player *who, const item *target, const item *parent, item_location&& ammo ) :
+item::reload_option::reload_option( const player *who, const item *target, const item *parent,
+                                    item_location&& ammo ) :
     who( who ), target( target ), ammo( std::move( ammo ) ), parent( parent )
 {
     if( this->target->is_ammo_belt() && this->target->type->magazine->linkage != "NULL" ) {
         max_qty = this->who->charges_of( this->target->type->magazine->linkage );
     }
-
-    // magazine, ammo or ammo container
-    item& tmp = ( this->ammo->is_ammo_container() || this->ammo->is_watertight_container() )
-        ? this->ammo->contents.front()
-        : *this->ammo;
-
-    if( this->ammo->is_watertight_container() ||
-        ( tmp.is_ammo() && !target->has_flag( "RELOAD_ONE" ) ) ||
-        ( tmp.is_magazine() && tmp.has_flag( "SPEEDLOADER" ) ) ) {
-        qty( tmp.ammo_remaining() );
-    } else {
-        qty( 1 );
-    }
+    qty( max_qty );
 }
-
 
 int item::reload_option::moves() const
 {
@@ -4722,38 +4710,35 @@ int item::reload_option::moves() const
 
 void item::reload_option::qty( long val )
 {
-    if( !ammo->has_flag( "SPEEDLOADER" ) &&
-        ( ammo->is_magazine() || target->has_flag( "RELOAD_ONE" ) ) ) {
-        qty_ = 1L;
+    bool ammo_in_container = ammo->is_ammo_container();
+    bool ammo_in_liquid_container = ammo->is_watertight_container();
+    item &ammo_obj = ( ammo_in_container || ammo_in_liquid_container ) ?
+        ammo->contents.front() : *ammo;
+
+    if( ( ammo_in_container && !ammo_obj.is_ammo() ) ||
+        ( ammo_in_liquid_container && !ammo_obj.made_of( LIQUID ) ) ) {
+        debugmsg( "Invalid reload option: %s", ammo_obj.tname().c_str() );
         return;
     }
 
-    item* obj = nullptr;
-    bool is_ammo_container = ammo->is_ammo_container();
-    bool is_liquid_container = ammo->is_watertight_container();
-    if ( is_ammo_container || is_liquid_container ) {
-        obj = &( ammo->contents.front() );
-    } else {
-        obj = &*ammo;
+    // Checking ammo capacity implicitly limits guns with removable magazines to capacity 0.
+    // This gets rounded up to 1 later.
+    long remaining_capacity = target->is_watertight_container() ?
+        target->get_remaining_capacity_for_liquid( ammo_obj, true ) :
+        target->ammo_capacity() - target->ammo_remaining();
+    if( target->has_flag( "RELOAD_ONE" ) && !ammo->has_flag( "SPEEDLOADER" ) ) {
+        remaining_capacity = 1;
     }
-
-    if( ( is_ammo_container && !obj->is_ammo() ) ||
-        ( is_liquid_container && !obj->made_of( LIQUID ) ) ) {
-        debugmsg( "Invalid reload option: %s", obj->tname().c_str() );
-        return;
-    }
-
-    long limit = is_liquid_container
-            ? target->get_remaining_capacity_for_liquid( *obj, true )
-            : target->ammo_capacity() - target->ammo_remaining();
-
     if( target->ammo_type() == ammotype( "plutonium" ) ) {
-        limit = limit / PLUTONIUM_CHARGES + ( limit % PLUTONIUM_CHARGES != 0 );
+        remaining_capacity = remaining_capacity / PLUTONIUM_CHARGES +
+            ( remaining_capacity % PLUTONIUM_CHARGES != 0 );
     }
 
+    bool ammo_by_charges = ammo_obj.is_ammo() || ammo_in_liquid_container;
+    long available_ammo = ammo_by_charges ? ammo_obj.charges : ammo_obj.ammo_remaining();
     // constrain by available ammo, target capacity and other external factors (max_qty)
     // @ref max_qty is currently set when reloading ammo belts and limits to available linkages
-    qty_ = std::min( { val, obj->ammo_remaining(), limit, max_qty } );
+    qty_ = std::min( { val, available_ammo, remaining_capacity, max_qty } );
 
     // always expect to reload at least one charge
     qty_ = std::max( qty_, 1L );
