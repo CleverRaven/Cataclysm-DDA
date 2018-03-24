73273f016604feb91c3ea471adc8dfd4c5ba16ae
Dmitry Serov <D.E.Serov@gmail.com>
2018-03-24 16:59:44 +0300

    remove unused includes

--- src/sidebar.h
+++ src/sidebar.h
@@ -2,14 +2,12 @@
 #ifndef SIDEBAR_H
 #define SIDEBAR_H
 
-#include "player.h"
-
+class player;
 namespace catacurses
 {
 class window;
 } // namespace catacurses
 
-/** Print the player's stamina bar. **/
 void print_stamina_bar( const player &p, const catacurses::window &w );
 
 #endif
