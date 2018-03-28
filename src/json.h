eab273f6a1921245e5fb01386b9d00986e4a87b4
Patrik Stridvall <patrik@stridvall.se>
2018-03-28 21:52:13 +0200

    json: JsonSerDesAdapter: New class

--- src/json.h
+++ src/json.h
@@ -1052,6 +1052,25 @@ class JsonSerDes : public JsonSerializer, public JsonDeserializer
     JsonSerDes &operator=(const JsonSerDes &) = default;
 };
 
+/* JsonSerDesAdapter
+ * ===============
+ */
+template<typename T>
+class JsonSerDesAdapter: public JsonSerDes
+{
+    protected:
+        T &data;
+    public:
+        JsonSerDesAdapter( T &data ) : data( data ) { };
+    public:
+        virtual void serialize( JsonOut &json ) const override {
+            data.serialize( json );
+        };
+        virtual void deserialize( JsonIn &jsin ) override {
+            data.deserialize( jsin );
+        };
+};
+
 std::ostream &operator<<( std::ostream &stream, const JsonError &err );
 
 #endif
