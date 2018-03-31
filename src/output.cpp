9a8adec0bff0c31fc175f87fca1eb07bdc53bfae
Patrik Stridvall <patrik@stridvall.se>
2018-04-01 00:47:11 +0200

    output: draw_subtab: Support for showing as disabled

--- src/output.cpp
+++ src/output.cpp
@@ -1203,15 +1203,24 @@ void draw_tab( const catacurses::window &w, int iOffsetX, std::string sText, boo
 }
 
 void draw_subtab( const catacurses::window &w, int iOffsetX, std::string sText, bool bSelected,
-                  bool bDecorate )
+                  bool bDecorate, bool bDisabled )
 {
     int iOffsetXRight = iOffsetX + utf8_width( sText ) + 1;
 
-    mvwprintz( w, 0, iOffsetX + 1, ( bSelected ) ? h_light_gray : c_light_gray, sText );
+    if( ! bDisabled ) {
+        mvwprintz( w, 0, iOffsetX + 1, ( bSelected ) ? h_light_gray : c_light_gray, sText );
+    } else {
+        mvwprintz( w, 0, iOffsetX + 1, ( bSelected ) ? h_dark_gray : c_dark_gray, sText );
+    }
 
     if( bSelected ) {
-        mvwputch( w, 0, iOffsetX - bDecorate,      h_light_gray, '<' );
-        mvwputch( w, 0, iOffsetXRight + bDecorate, h_light_gray, '>' );
+        if( ! bDisabled ) {
+            mvwputch( w, 0, iOffsetX - bDecorate,      h_light_gray, '<' );
+            mvwputch( w, 0, iOffsetXRight + bDecorate, h_light_gray, '>' );
+        } else {
+            mvwputch( w, 0, iOffsetX - bDecorate,      h_dark_gray, '<' );
+            mvwputch( w, 0, iOffsetXRight + bDecorate, h_dark_gray, '>' );
+        }
 
         for( int i = iOffsetX + 1; bDecorate && i < iOffsetXRight; i++ ) {
             mvwputch( w, 1, i, c_black, ' ' );
