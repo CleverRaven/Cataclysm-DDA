1eae702a4bb0d8d91886699bc8eb487c75aee191
Firestorm01X2 <firestorm01x2@gmail.com>
2018-04-02 20:33:02 +0300

    Ranged critical hits damage bonus reduction
    Ranged critical hits damage bonus reduction.

--- src/creature.cpp
+++ src/creature.cpp
@@ -486,13 +486,13 @@ void Creature::deal_projectile_attack( Creature *source, dealt_projectile_attack
     if( goodhit < accuracy_headshot ) {
         message = _("Headshot!");
         gmtSCTcolor = m_headshot;
-        damage_mult *= rng_float(2.45, 3.35);
+        damage_mult *= rng_float(1.95, 2.05);
         bp_hit = bp_head; // headshot hits the head, of course
 
     } else if( goodhit < accuracy_critical ) {
         message = _("Critical!");
         gmtSCTcolor = m_critical;
-        damage_mult *= rng_float(1.75, 2.3);
+        damage_mult *= rng_float(1.5, 2.0);
 
     } else if( goodhit < accuracy_goodhit ) {
         message = _("Good hit!");
