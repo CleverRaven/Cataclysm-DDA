f1b69b35b146f9a47d523362f00f46e83510d640
RustyPotato <kylerfawcett@gmail.com>
2018-04-19 00:50:25 -0400

    Replaced rng_int in ballistics.cpp

--- src/ballistics.cpp
+++ src/ballistics.cpp
@@ -263,7 +263,7 @@ dealt_projectile_attack projectile_attack( const projectile &proj_arg, const tri
     const float projectile_skip_multiplier = 0.1;
     // Randomize the skip so that bursts look nicer
     int projectile_skip_calculation = range * projectile_skip_multiplier;
-    int projectile_skip_current_frame = rng( 0, projectile_skip_calculation );
+    int projectile_skip_current_frame = rng_int( 0, projectile_skip_calculation );
     bool has_momentum = true;
 
     for( size_t i = 1; i < traj_len && ( has_momentum || stream ); ++i ) {
