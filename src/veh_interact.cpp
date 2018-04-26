e065f06bf7dabb0dcee5180fbe77a0d9f8afd60f
NotFuji <andyfuji778@gmail.com>
2018-04-26 09:08:04 -0500

    Simplified

--- src/veh_interact.cpp
+++ src/veh_interact.cpp
@@ -75,57 +75,32 @@ player_activity veh_interact::serialize_activity()
     if( sel_cmd == 'q' || sel_cmd == ' ' || !vp ) {
         return player_activity();
     }
+	int time = 1000;
 
-    int time = 1000;
-    switch( sel_cmd ) {
-        case 'i':
-			if( !g->u.has_trait( trait_id( "DEBUG_HS" ) ) )
-			{
-			    time = vp->install_time( g->u );
-			} else
-			{
-			    time = 1;
-			}
-			break;
-        case 'r':
-            if( pt->is_broken() ) {
-				if( !g->u.has_trait( trait_id( "DEBUG_HS" ) ) )
-				{
-				    time = vp->install_time( g->u );
-				} else
-				{
-				    time = 1;
+	if (!g->u.has_trait(trait_id("DEBUG_HS"))) {
+		switch( sel_cmd ) {
+			case 'i':
+				time = vp->install_time( g->u );
+				break;
+			case 'r':
+				if( pt->is_broken() ) {
+					time = vp->install_time( g->u );
+				} else {
+					assert( pt->base.max_damage() > 0 ); // why repairing part that cannot be damaged?
+					time = vp->repair_time(g->u) * double(pt->base.damage()) / pt->base.max_damage();
 				}
-            } else {
-                assert( pt->base.max_damage() > 0 ); // why repairing part that cannot be damaged?
-				if( !g->u.has_trait( trait_id( "DEBUG_HS" ) ) )
-				{
-				    time = vp->repair_time( g->u ) * double( pt->base.damage() ) / pt->base.max_damage();
-				} else
-				{
-				    time = 1;
-				}
-            }
-            break;
-        case 'o':
-			if( !g->u.has_trait( trait_id( "DEBUG_HS" ) ) )
-			{
-			    time = vp->removal_time( g->u );
-			} else
-			{
-			    time = 1;
-			}
-            break;
-        case 'c':
-			if( !g->u.has_trait( trait_id( "DEBUG_HS" ) ) )
-			{
-			    time = vp->removal_time( g->u ) + vp->install_time( g->u );
-			} else
-			{
-			    time = 1;
-			}
-            break;
-    }
+				break;
+			case 'o':
+				time = vp->removal_time(g->u);
+				break;
+			case 'c':
+				time = vp->removal_time(g->u) + vp->install_time(g->u);
+				break;
+		}
+	}
+	else {
+		time = 1;
+	}
     player_activity res( activity_id( "ACT_VEHICLE" ), time, (int) sel_cmd );
 
     // if we're working on an existing part, use that part as the reference point
