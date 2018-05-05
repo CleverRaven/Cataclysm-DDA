7d8651f46ace9addad9f62650f68cb3c35c5fb48
Pausbrak <rslego@gmail.com>
2018-05-05 14:39:41 -0400

    spelling

--- src/player.cpp
+++ src/player.cpp
@@ -7955,7 +7955,7 @@ bool player::takeoff( const item &it, std::list<item> *res )
         if( volume_carried() + it.volume() > volume_capacity_reduced_by( it.get_storage() ) ) {
             if( is_npc() || query_yn( _( "No room in inventory for your %s.  Drop it?" ), it.tname().c_str() ) ) {
                 drop( get_item_position( &it ) );
-                return true; // the drop activity ends up taking off the item anyway so shouldn't try to do it agazn here
+                return true; // the drop activity ends up taking off the item anyway so shouldn't try to do it again here
             } else {
                 return false;
             }
