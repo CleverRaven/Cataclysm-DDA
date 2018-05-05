f76e82cc1f81f98b9da220751a2692f755912a40
NotFuji <andyfuji778@gmail.com>
2018-05-05 01:12:30 -0500

    Bracket style

--- src/field.cpp
+++ src/field.cpp
@@ -463,7 +463,9 @@ field_id field_from_ident(const std::string &field_ident)
 
 void map::create_burnproducts( const tripoint p, const item &fuel ) {
     std::vector<material_id> all_mats = fuel.made_of();
-    if ( all_mats.empty() ) { return; }
+    if ( all_mats.empty() ) { 
+        return; 
+    }
     units::mass fuel_weight = fuel.weight( false );
     //Items that are multiple materials are assumed to be equal parts each.
     units::mass by_weight = fuel_weight / all_mats.size();
@@ -473,7 +475,9 @@ void map::create_burnproducts( const tripoint p, const item &fuel ) {
             float eff = bp.second;
             int n = floor( eff * ( by_weight / item::find_type( id )->weight ) );
 
-            if( n <= 0 ) continue;
+            if( n <= 0 ) {
+                continue;
+            }
             spawn_item( p, id, n, 1, calendar::turn );
         }
     }
