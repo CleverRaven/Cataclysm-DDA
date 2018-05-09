9493522ed021eaff955c6910d65570d64b274a00
NotFuji <andyfuji778@gmail.com>
2018-05-09 02:29:12 -0500

    Buffed fire weapons

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -3437,7 +3437,7 @@ int iuse::molotov_lit(player *p, item *it, bool t, const tripoint &pos)
     } else {
         if( !t ) {
             for( auto &&pt : g->m.points_in_radius( pos, 1, 0 ) ) {
-                const int density = 1 + one_in( 3 ) + one_in( 5 );
+                const int density = 2 + one_in( 3 ) + one_in( 5 );
                 g->m.add_field( pt, fd_fire, density );
             }
         }
