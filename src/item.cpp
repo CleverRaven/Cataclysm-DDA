64acb08ce98327f33889f2f3f53af92ba8c35736
John Colburn <they.called.him.crazy@gmail.com>
2018-05-12 13:40:51 -0500

    Re-formatting to better match existing code.

--- src/item.cpp
+++ src/item.cpp
@@ -2280,24 +2280,18 @@ std::string item::tname( unsigned int quantity, bool with_prefix ) const
         maintext = ret.str();
     } else if( contents.size() == 1 ) {
         if( contents.front().made_of( LIQUID ) ) {
-            maintext = string_format(
-                pgettext( "item name", "%s in %s" ),
-                contents.front().tname( quantity, with_prefix ).c_str(),
-            	label( quantity ).c_str()
-            );
+            maintext = string_format( pgettext( "item name", "%s in %s" ),
+                                      contents.front().tname( quantity, with_prefix ).c_str(),
+                                      label( quantity ).c_str() );
         } else if( contents.front().is_food() ) {
             const unsigned contents_count = contents.front().charges > 1 ? contents.front().charges : quantity;
-            maintext = string_format(
-                pgettext( "item name", "%s in %s" ),
-                contents.front().tname( contents_count, with_prefix ).c_str(),
-                label( quantity ).c_str()
-            );
+            maintext = string_format( pgettext( "item name", "%s in %s" ),
+                                      contents.front().tname( contents_count, with_prefix ).c_str(),
+                                      label( quantity ).c_str() );
         } else {
-            maintext = string_format(
-                pgettext( "item name", "%s in %s" ),
-                contents.front().tname( quantity, with_prefix ).c_str(),
-                label( quantity ).c_str()
-            );
+            maintext = string_format( pgettext( "item name", "%s in %s" ),
+                                      contents.front().tname( quantity, with_prefix ).c_str(),
+                                      label( quantity ).c_str() );
         }
     } else if( !contents.empty() ) {
         maintext = string_format( pgettext( "item name", "%s, full" ), label( quantity ).c_str() );
