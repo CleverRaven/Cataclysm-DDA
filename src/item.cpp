aa64b8c3405fed668779a22a1364e1b87a9d08de
Firestorm01X2 <firestorm01x2@gmail.com>
2018-05-21 23:16:43 +0300

    unhardcode dsipersion and add range to shotguns
    unhardcode dsipersion and add range to shotguns

--- src/item.cpp
+++ src/item.cpp
@@ -4054,10 +4054,7 @@ int item::gun_dispersion( bool with_ammo ) const
         return 0;
     }
     int dispersion_sum = type->gun->dispersion;
-    if ( type->gun->skill_used == "shotgun" ) {
-        dispersion_sum += 400;
-    }
-
+    
     for( const auto mod : gunmods() ) {
         dispersion_sum += mod->type->gunmod->dispersion;
     }
