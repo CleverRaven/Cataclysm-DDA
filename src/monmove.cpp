f7753413821d2ede1b4a663371efd5ba9d32f650
Rob Bolton <rob.a.bolton@gmail.com>
2018-05-29 03:57:23 +0100

    Fixes #23883 creatures teleporting up z-levels
    Creatures may no longer climb z-levels without stairs or ladders
    unless they have the climb or fly flags set.
    This prevents zombies running up walls and in through windows to
    ruin a player's day.
    Climbing creatures may strand themselves on z-levels that they can
    climb to but which do not have stairs/ladders. This is likely less
    detrimental than previous behavior but may need to be revisited.

--- src/monmove.cpp
+++ src/monmove.cpp
@@ -554,6 +554,7 @@ void monster::move()
 
     tripoint next_step;
     const bool staggers = has_flag( MF_STUMBLES );
+    const bool can_climb = has_flag( MF_CLIMBS );
     if( moved ) {
         // Implement both avoiding obstacles and staggering.
         moved = false;
@@ -570,9 +571,13 @@ void monster::move()
                     can_z_move = false;
                 }
 
-                if( can_z_move && !can_fly && candidate.z > posz() && !g->m.has_floor_or_support( candidate ) ) {
-                    // Can't "jump" up a whole z-level
-                    can_z_move = false;
+                // If we're trying to go up but can't fly, check if we can climb. If we can't, then don't
+                // This prevents non-climb/fly enemies running up walls
+                if( candidate.z > posz() && !can_fly ) {
+                    if( !can_climb || !g->m.has_floor_or_support( candidate ) ) {
+                        // Can't "jump" up a whole z-level
+                        can_z_move = false;
+                    }
                 }
 
                 // Last chance - we can still do the z-level stair teleport bullshit that isn't removed yet
