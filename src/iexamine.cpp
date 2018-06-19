60bde4e93256d1e2e22e8f2f0535a327b19fc129
Night-Pryanik <Night_Pryanik@mail.ru>
2018-06-20 00:15:23 +0400

    Display id when there is no accociated item for bionic (faulty one)

--- src/iexamine.cpp
+++ src/iexamine.cpp
@@ -3558,9 +3558,14 @@ void iexamine::autodoc( player &p, const tripoint &examp )
                 if( std::find( bionic_types.begin(), bionic_types.end(), bio.id.str() ) == bionic_types.end() ) {
                     if( bio.id != bionic_id( "bio_power_storage" ) ||
                         bio.id != bionic_id( "bio_power_storage_mkII" ) ) {
-                        bionic_to_uninstall = item( bio.id.str(), 0 );
                         bionic_types.push_back( bio.id.str() );
-                        bionic_names.push_back( bionic_to_uninstall.tname() );
+                        if( item::type_is_defined( bio.id.str() ) ) {
+                            bionic_to_uninstall = item( bio.id.str(), 0 );
+                            bionic_names.push_back( bionic_to_uninstall.tname() );
+                        } else {
+                            // @todo Display name instead of id
+                            bionic_names.push_back( bio.id.str() );
+                        }
                     }
                 }
             }
