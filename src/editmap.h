b725020e6ece91f686ea59ba5f2893222bdc4c7a
acidia <stm.vol@gmail.com>
2018-06-27 21:06:28 -0400

    mapgen_set change_sensitive limits overwriting

--- src/editmap.h
+++ src/editmap.h
@@ -58,7 +58,7 @@ class editmap
         int edit_mapgen();
         void cleartmpmap( tinymap &tmpmap );
         int mapgen_preview( real_coords &tc, uimenu &gmenu );
-        bool mapgen_set( std::string om_name, tripoint omt_tgt, int r = 0);
+        bool mapgen_set( std::string om_name, tripoint omt_tgt, int r = 0, bool change_sensitive = true );
         vehicle* mapgen_veh_query( tripoint omt_tgt, vehicle* car_target = NULL, bool destroy = false);
         int mapgen_retarget();
         int select_shape( shapetype shape, int mode = -1 );
