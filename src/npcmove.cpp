f3e70f1fbca7e9f8753db299558909bc854a08bf
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2018-07-04 23:29:42 +0300

    Code optimizations reported by static code analysis (Step 3)  (#24024)
    * Pass function parameters by const references instead of values
    
    * Use `side` enum instead of string
    
    * Pass function parameters by const references instead of values (Step 2)

--- src/npcmove.cpp
+++ src/npcmove.cpp
@@ -2215,13 +2215,13 @@ void npc::drop_items( int weight, int volume )
         g->m.add_item_or_charges( pos(), dropped );
     }
     // Finally, describe the action if u can see it
-    std::string item_name_str = item_name.str();
     if( g->u.sees( *this ) ) {
         if( num_items_dropped >= 3 ) {
             add_msg( ngettext( "%s drops %d item.", "%s drops %d items.",
                                num_items_dropped ), name.c_str(),
                      num_items_dropped );
         } else {
+            std::string item_name_str = item_name.str();
             add_msg( _( "%1$s drops a %2$s." ), name.c_str(),
                      item_name_str.c_str() );
         }
