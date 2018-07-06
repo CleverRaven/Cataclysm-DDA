0ff04cc32664bf2ed6fd48ed69f9cff154fe7ed8
vasyan2006 <dpuris@gmail.com>
2018-07-06 16:35:00 +0300

    Rename stamina_penalty to stamina_pool_recovery.

--- src/item_factory.cpp
+++ src/item_factory.cpp
@@ -1445,7 +1445,7 @@ void Item_factory::load( islot_comestible &slot, JsonObject &jo, const std::stri
     assign( jo, "quench", slot.quench, strict );
     assign( jo, "fun", slot.fun, strict );
     assign( jo, "stim", slot.stim, strict );
-    assign( jo, "stamina_penalty", slot.stamina_penalty, strict );
+    assign( jo, "stamina_pool_recovery", slot.stamina_pool_recovery, strict );
     assign( jo, "healthy", slot.healthy, strict );
     assign( jo, "parasites", slot.parasites, strict, 0 );
     assign( jo, "spoils_in", slot.spoils, strict, 1_hours );
