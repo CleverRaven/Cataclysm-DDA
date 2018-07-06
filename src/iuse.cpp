4043447553072c4655a701776c76061238c06a01
Soufian Salim <soufi@nsal.im>
2018-07-06 14:14:01 +0200

    correctly updates starvation when mutagen is ingested

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -1161,9 +1161,16 @@ int iuse::mutagen( player *p, item *it, bool, const tripoint & )
         for( int i = 0; i < mut_count; i++ ) {
             p->mutate_category( mutation_category );
             p->mod_pain( m_category.mutagen_pain * rng( 1, 5 ) );
-            p->mod_hunger( m_category.mutagen_hunger );
             p->mod_thirst( m_category.mutagen_thirst );
             p->mod_fatigue( m_category.mutagen_fatigue );
+
+            if ( m_category.mutagen_hunger + p->get_hunger() > 300 ) {
+                // in this case starvation is directly updated
+                p->mod_starvation( m_category.mutagen_hunger - ( 300 - p->get_hunger() ) );
+                p->set_hunger(300);
+            } else {
+                p->mod_hunger( m_category.mutagen_hunger) ;
+            }
         }
 
         return it->type->charges_to_use();
