26dec0ec7328ab4d564e2bad245426384db04d09
Anton Burmistrov <Night_Pryanik@mail.ru>
2018-07-11 08:28:41 +0300

    Remove irremovable mods after the gun is destroyed by fire (#24246)
    * Remove irremovable mods after the gun is destroyed by fire

--- src/field.cpp
+++ src/field.cpp
@@ -27,6 +27,7 @@
 #include "map_iterator.h"
 
 #include <queue>
+#include <algorithm>
 
 const species_id FUNGUS( "FUNGUS" );
 
@@ -916,9 +917,12 @@ bool map::process_fields_in_submap( submap *const current_submap,
 
                                 if( destroyed ) {
                                     // If we decided the item was destroyed by fire, remove it.
-                                    // But remember its contents
+                                    // But remember its contents, except for irremovable mods, if any
                                     std::copy( fuel->contents.begin(), fuel->contents.end(),
                                                std::back_inserter( new_content ) );
+                                    new_content.erase( std::remove_if( new_content.begin(), new_content.end(), [&]( const item & i ) {
+                                        return i.is_irremovable();
+                                    } ), new_content.end() );
                                     fuel = items_here.erase( fuel );
                                     consumed++;
                                 } else {
