d3f04d32ef4f7ba1e8934a4935d0e027f70d6325
Jason Jones <jasonedwardjones@gmail.com>
2018-08-06 20:35:52 -0800

    Update vehicleDef.py to produce better vehicle templates
    This script previously made the assumption that the order in which parts
    were listed in the save file was valid for a vehicle template, but that
    assumption is easily invalidated if the user is using Hammerspace to
    build the vehicle.
    
    To cut down on the amount of manual work required to get a good vehicle
    template, I updated this script to sort the vehicle frame parts such
    that they are listed in a connected order (e.g. each frame is adjacent
    to another existing frame).
    
    I also updated the script to use OrderedDict to ensure that the json
    keys retained their original order, which keeps the id, type, name, and
    part x, y, and type all in the expected order.

--- tools/vehicleDef.py
+++ tools/vehicleDef.py
@@ -1,5 +1,8 @@
 import json
 import argparse
+from collections import OrderedDict
+from math import hypot
+import re
 
 args = argparse.ArgumentParser(description="Generate a json definition for a vehicle in a Cataclysm DDA save file.")
 args.add_argument("save", action="store", help="specify save file containing vehicle")
@@ -18,17 +21,47 @@ def findVehicle():
 def writeDef(vehicle):
     partsDef = []
     for part in vehicle["parts"]:
-        partsDef.append({"x": part["mount_dx"],
-                         "y": part["mount_dy"],
-                         "part": part["id"]})
+        partsDef.append(OrderedDict([
+            ("x", part["mount_dx"]),
+            ("y", part["mount_dy"]), 
+            ("part", part["id"])
+        ]))
 
-    vehicleDef = {"id": argsDict["vehicle"],
-                   "type": "vehicle",
-                   "name": argsDict["vehicle"],
-                   "parts": partsDef}
+    frames = [p for p in partsDef if re.match(r'(xl)?frame', p["part"]) != None]
+    everythingElse = [p for p in partsDef if re.match(r'(xl)?frame', p["part"]) == None]
+
+    frames = sortFrames(frames)
+
+    vehicleDef = OrderedDict([
+        ("id", argsDict["vehicle"]),
+        ("type", "vehicle"),
+        ("name", argsDict["vehicle"]),
+        ("parts", frames + everythingElse)
+    ])
 
     with open("{}.json".format(argsDict['vehicle']), "w") as vehicleDefJson:
         json.dump(vehicleDef, vehicleDefJson, indent=4)
         print("Vehicle def written.")
+
+def sortFrames(frames):
+    sortedFrames = []
+    sortedFrames.append(frames.pop())
+
+    while len(frames) > 0:
+        nextFrame = frames.pop()
+        found = False
+        for lastFrame in sortedFrames:
+            if adjacent(lastFrame, nextFrame):
+                sortedFrames.append(nextFrame)
+                found = True
+                break
+
+        if not found:
+            frames.insert(0, nextFrame)
+
+    return sortedFrames
+    
+def adjacent(frame1, frame2):
+    return hypot(frame1["x"] - frame2["x"], frame1["y"] - frame2["y"]) == 1
 
 writeDef(findVehicle())
