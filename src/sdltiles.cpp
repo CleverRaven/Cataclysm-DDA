CleverRaven/Cataclysm-DDA

commit 7c385d080eb7f21447d69ffd82f5f8ae1c6c4f70

Duplicate key presses on SDL 2.0.5 (#22171)
Tapping arrow keys, enter, etc. would register twice on my Ubuntu 17.04, apparently due to an SDL bug: https://stackoverflow.com/questions/42056515/sdl2-key-repeat-not-working.

I haven't tested this on SDL versions other than 2.0.5 or on platforms other than linux/x11, but at worst it should only break repeating keys with sdl_keysym_to_curses > 0 :D

src/sdltiles.cpp
@@ -1241,6 +1241,9 @@ void CheckMessages()
                 if( lc <= 0 ) {
                     // a key we don't know in curses and won't handle.
                     break;
+                } else if( SDL_COMPILEDVERSION == SDL_VERSIONNUM( 2, 0, 5 ) && ev.key.repeat ) {
+                    // https://bugzilla.libsdl.org/show_bug.cgi?id=3637
+                    break;
                 } else if( add_alt_code( lc ) ) {
                     // key was handled
                 } else {

commit 05ddb058e540b083db61db0912f8360faba8702c

Properly work-around broken SDL 2.0.5 key events on Linux
When using SDL 2.0.5 compiled without ibus or fcitx headers available, and
ibus via XIM is requested through the XMODIFIERS environment variable, SDL
will emit broken keyboard events under X11. Specifically, pressing a key
once will emit two KEYDOWN events, and further holding a key down will
emit erroneous KEYUP events for every repeat cycle.

To fix this, detect when XMODIFIERS is "@im=ibus", and set it to "im=none"
before initializing SDL. This prevents the use of ibus via XIM so the
problem does not occur. Refer to the SDL bug report for more details:
https://bugzilla.libsdl.org/show_bug.cgi?id=3472#c5

This was previously worked around in Cataclysm by ignoring keydown events
with repeat == true whenever running on Linux with SDL 2.0.5. However,
that also caused legitimate key repeats to be ignored, which meant that
holding down keys that are handled by KEYDOWN events (such as arrow keys)
would not work correctly (GitHub: #22182).

Fixes: 7c385d080eb7 (Duplicate key presses on SDL 2.0.5 (#22171))

src/sdltiles.cpp
@@ -33,6 +33,10 @@
 #include <stdexcept>
 #include <limits>
 
+#ifdef __linux__
+#   include <cstdlib> // getenv()/setenv()
+#endif
+
 #if (defined _WIN32 || defined WINDOWS)
 #   include "platform_win.h"
 #   include <shlwapi.h>
@@ -269,6 +273,16 @@ bool InitSDL()
 
 #ifdef SDL_HINT_WINDOWS_DISABLE_THREAD_NAMING
     SDL_SetHint(SDL_HINT_WINDOWS_DISABLE_THREAD_NAMING, "1");
+#endif
+
+#ifdef __linux__
+    // https://bugzilla.libsdl.org/show_bug.cgi?id=3472#c5
+    if( SDL_COMPILEDVERSION == SDL_VERSIONNUM( 2, 0, 5 ) ) {
+        const char *xmod = getenv( "XMODIFIERS" );
+        if( xmod && strstr( xmod, "@im=ibus" ) != NULL ) {
+            setenv( "XMODIFIERS", "@im=none", 1 );
+        }
+    }
 #endif
 
     ret = SDL_Init( init_flags );
@@ -1398,11 +1412,6 @@ void CheckMessages()
                 if( lc <= 0 ) {
                     // a key we don't know in curses and won't handle.
                     break;
-#ifdef __linux__
-                } else if( SDL_COMPILEDVERSION == SDL_VERSIONNUM( 2, 0, 5 ) && ev.key.repeat ) {
-                    // https://bugzilla.libsdl.org/show_bug.cgi?id=3637
-                    break;
-#endif
                 } else if( add_alt_code( lc ) ) {
                     // key was handled
                 } else {