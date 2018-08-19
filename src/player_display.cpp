7b6d9f6e90b7d3032b28be554679079c11911825
Oren Audeles <orenaudeles@gmail.com>
2018-08-19 00:09:05 -0500

    Cached calls to game::get_temperature in player_display.cpp player::disp_info. Possibly many calls to get_temperature. Modified call from g->get_temperature( g->u.pos() ) to g->get_temperature( pos() ) to display specific player temperature information.

--- src/player_display.cpp
+++ src/player_display.cpp
@@ -697,21 +697,23 @@ Strength - 4;    Dexterity - 4;    Intelligence - 4;    Perception - 4" ) );
                    ( pen < 10 ? " " : "" ), pen );
         line++;
     }
-    if( has_trait( trait_id( "COLDBLOOD4" ) ) && g->get_temperature( g->u.pos() ) > 65 ) {
-        pen = ( g->get_temperature( g->u.pos() ) - 65 ) / 2;
+    /* Cache result of calculation, possibly used multiple times later. */
+    const auto player_local_temp = g->get_temperature( pos() );
+    if( has_trait( trait_id( "COLDBLOOD4" ) ) && player_local_temp > 65 ) {
+        pen = ( player_local_temp - 65 ) / 2;
         mvwprintz( w_speed, line, 1, c_green, _( "Cold-Blooded        +%s%d%%" ),
                    ( pen < 10 ? " " : "" ), pen );
         line++;
     }
     if( ( has_trait( trait_id( "COLDBLOOD" ) ) || has_trait( trait_id( "COLDBLOOD2" ) ) ||
           has_trait( trait_id( "COLDBLOOD3" ) ) || has_trait( trait_id( "COLDBLOOD4" ) ) ) &&
-        g->get_temperature( g->u.pos() ) < 65 ) {
+        player_local_temp < 65 ) {
         if( has_trait( trait_id( "COLDBLOOD3" ) ) || has_trait( trait_id( "COLDBLOOD4" ) ) ) {
-            pen = ( 65 - g->get_temperature( g->u.pos() ) ) / 2;
+            pen = ( 65 - player_local_temp ) / 2;
         } else if( has_trait( trait_id( "COLDBLOOD2" ) ) ) {
-            pen = ( 65 - g->get_temperature( g->u.pos() ) ) / 3;
+            pen = ( 65 - player_local_temp ) / 3;
         } else {
-            pen = ( 65 - g->get_temperature( g->u.pos() ) ) / 5;
+            pen = ( 65 - player_local_temp ) / 5;
         }
         mvwprintz( w_speed, line, 1, c_red, _( "Cold-Blooded        -%s%d%%" ),
                    ( pen < 10 ? " " : "" ), pen );
