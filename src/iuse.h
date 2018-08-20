922a92528e52ed6a55c2b488ffa36bb70ffa1de7
Night-Pryanik <Night_Pryanik@mail.ru>
2018-08-20 17:55:09 +0400

    Remove fun_hallu item use function from iuse.cpp/.h and from magic shroom and dried magic shroom items

--- src/iuse.h
+++ src/iuse.h
@@ -51,7 +51,6 @@ class iuse
         int vaccine( player *, item *, bool, const tripoint & );
         int flu_vaccine( player *, item *, bool, const tripoint & );
         int poison( player *, item *, bool, const tripoint & );
-        int fun_hallu( player *, item *, bool, const tripoint & );
         int meditate( player *, item *, bool, const tripoint & );
         int thorazine( player *, item *, bool, const tripoint & );
         int prozac( player *, item *, bool, const tripoint & );
