2a9d9c3f8c169cf9fa935c677d4c88f8835c2e6f
nexusmrsep <nexus.mrsep@gmail.com>
2018-08-30 13:41:52 +0200

    infrastructure to highlight menu entries + applied to MUSHY flag

--- src/game_inventory.cpp
+++ src/game_inventory.cpp
@@ -23,8 +23,17 @@ typedef std::function<bool( const item_location & )> item_location_filter;
 namespace
 {
 
-std::string good_bad_none( int value )
+std::string good_bad_none( int value, bool warning = false )
 {
+    if( warning ) {
+        if( value > 0 ) {
+            return string_format( "<color_c_yellow_green>+%d</color>", value );
+        } else if( value < 0 ) {
+            return string_format( "<color_c_yellow_red>%d</color>", value );
+        } else if( value == 0 ) {
+            return string_format( "<color_c_yellow>%d</color>", value );
+        }
+    }
     if( value > 0 ) {
         return string_format( "<good>+%d</good>", value );
     } else if( value < 0 ) {
@@ -359,7 +368,12 @@ class comestible_inventory_preset : public inventory_selector_preset
             }, _( "QUENCH" ) );
 
             append_cell( [ p, this ]( const item_location & loc ) {
-                return good_bad_none( p.fun_for( get_comestible_item( loc ) ).first );
+                const item it = get_comestible_item( loc );
+                if( it.has_flag( "MUSHY" ) ) {
+                    return good_bad_none( p.fun_for( get_comestible_item( loc ) ).first, true );
+                } else{
+                    return good_bad_none( p.fun_for( get_comestible_item( loc ) ).first );
+                }
             }, _( "JOY" ) );
 
             append_cell( [ this ]( const item_location & loc ) {
