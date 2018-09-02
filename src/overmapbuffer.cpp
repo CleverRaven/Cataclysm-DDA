1a2508f055350a32d852dbeda61e5783bc6795a6
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2018-09-02 12:55:14 +0300

    Removed const qualifier from some function parameters

--- src/overmapbuffer.cpp
+++ src/overmapbuffer.cpp
@@ -36,7 +36,7 @@ int city_reference::get_distance_from_bounds() const
     return distance - omt_to_sm_copy( city->s );
 }
 
-std::string overmapbuffer::terrain_filename( int const x, int const y )
+std::string overmapbuffer::terrain_filename( int x, int y )
 {
     std::ostringstream filename;
 
@@ -46,7 +46,7 @@ std::string overmapbuffer::terrain_filename( int const x, int const y )
     return filename.str();
 }
 
-std::string overmapbuffer::player_filename( int const x, int const y )
+std::string overmapbuffer::player_filename( int x, int y )
 {
     std::ostringstream filename;
 
@@ -55,7 +55,7 @@ std::string overmapbuffer::player_filename( int const x, int const y )
     return filename.str();
 }
 
-overmap &overmapbuffer::get( const int x, const int y )
+overmap &overmapbuffer::get( int x, int y )
 {
     point const p { x, y };
 
@@ -81,8 +81,7 @@ overmap &overmapbuffer::get( const int x, const int y )
     return *new_om;
 }
 
-void overmapbuffer::create_custom_overmap( int const x, int const y,
-        overmap_special_batch &specials )
+void overmapbuffer::create_custom_overmap( int x, int y, overmap_special_batch &specials )
 {
     overmap *new_om = new overmap( x, y );
     if( last_requested_overmap != nullptr ) {
