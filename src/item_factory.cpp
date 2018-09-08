52c231bee16b70eece63867499bc64345d0b5c71
Brian Lefler <apathia@gmail.com>
2018-09-07 23:24:11 -0400

    Injectable mutagen finales, targetable purifier smart shots. (#25019)
    * Injectable mutagen finales, targetable purifier smart shots.
    
    * Two spaces after periods.
    
    * Smart shots to do nothing when user has no mutations.
    
    * Improve language in lab notes.

--- src/item_factory.cpp
+++ src/item_factory.cpp
@@ -643,6 +643,7 @@ void Item_factory::init()
     add_iuse( "PROZAC", &iuse::prozac );
     add_iuse( "PURIFIER", &iuse::purifier );
     add_iuse( "PURIFY_IV", &iuse::purify_iv );
+    add_iuse( "PURIFY_SMART", &iuse::purify_smart );
     add_iuse( "RADGLOVE", &iuse::radglove );
     add_iuse( "RADIOCAR", &iuse::radiocar );
     add_iuse( "RADIOCARON", &iuse::radiocaron );
