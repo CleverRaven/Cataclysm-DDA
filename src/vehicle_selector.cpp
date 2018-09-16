0ed2d833543cfaeb02b54d7385781551398f39fc
Unknown <rkay94@gmail.com>
2018-09-16 18:00:29 -0400

    Make crane determine reachability by LoS not walkability

--- src/vehicle_selector.cpp
+++ src/vehicle_selector.cpp
@@ -4,10 +4,12 @@
 #include "vpart_position.h"
 #include "map.h"
 
-vehicle_selector::vehicle_selector( const tripoint &pos, int radius, bool accessible )
+vehicle_selector::vehicle_selector( const tripoint &pos, int radius, bool accessible,
+                                    bool for_crane_lift )
 {
     for( const auto &e : closest_tripoints_first( radius, pos ) ) {
-        if( !accessible || g->m.clear_path( pos, e, radius, 1, 100 ) ) {
+        if( !accessible ||
+            ( for_crane_lift ? g->m.sees( pos, e, radius ) : g->m.clear_path( pos, e, radius, 1, 100 ) ) ) {
             if( const optional_vpart_position vp = g->m.veh_at( e ) ) {
                 data.emplace_back( vp->vehicle(), vp->part_index() );
             }
