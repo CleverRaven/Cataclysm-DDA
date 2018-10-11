66291251153237a9b7fcacef5261d93254ebdf92
OzoneH3 <satori.jj@gmail.com>
2018-10-11 11:27:41 +0200

    Use maps on zlevel 0 so they actually reveal things.

--- src/iuse_actor.cpp
+++ src/iuse_actor.cpp
@@ -974,7 +974,7 @@ long reveal_map_actor::use( player &p, item &it, bool, const tripoint & ) const
     }
     const auto center = p.global_omt_location();
     for( auto &omt : omt_types ) {
-        reveal_targets( center, omt, 0 );
+        reveal_targets( tripoint( center.x, center.y, 0 ), omt, 0 );
     }
     if( !message.empty() ) {
         p.add_msg_if_player( m_good, "%s", _( message.c_str() ) );
