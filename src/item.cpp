e529906bbae32fb922d67c101650f46123ff2701
Zetsukaze <indolent.guzzy@gmail.com>
2018-10-31 01:34:14 +0800

    Simplify casting to reduce code length

--- src/item.cpp
+++ src/item.cpp
@@ -535,10 +535,8 @@ long item::charges_per_volume( const units::volume &vol ) const
     if( type->volume == 0 ) {
         return INFINITE_CHARGES; // TODO: items should not have 0 volume at all!
     }
-    // Type cast for prevention of integer overflow with large volume containers like the cargo dimension
-    return count_by_charges() ? static_cast<int64_t>(units::to_milliliter(vol)) *
-                                static_cast<int64_t>(type->stack_size) /
-                                static_cast<int64_t>(units::to_milliliter(type->volume))
+    // Type cast to prevent integer overflow with large volume containers like the cargo dimension
+    return count_by_charges() ? vol * static_cast<int64_t>(type->stack_size) / type->volume
                                 : vol / volume();
 }
 
