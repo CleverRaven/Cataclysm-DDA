8c2361da8c113292981bd0abc5fa4816d0eebfd4
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2018-11-02 07:35:33 -0700

    astyle and unneeded cast
    Co-Authored-By: Karthas077 <Karthas077@users.noreply.github.com>

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -433,7 +433,7 @@ int iuse::ecig( player *p, item *it, bool, const tripoint & )
             p->add_msg_if_player( m_neutral,
                                   _( "You inhale some vapor from your advanced electronic cigarette." ) );
             p->use_charges( "nicotine_liquid", 1 );
-            item dummy_ecig = item("ecig", int(calendar::turn));
+            item dummy_ecig = item( "ecig", calendar::turn );
             p->consume_effects( dummy_ecig );
         } else {
             p->add_msg_if_player( m_info, _( "You don't have any nicotine liquid!" ) );
