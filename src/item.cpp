0c873ad742ca68f4ba266b7607b45bbdc0e20505
Zetsukaze <indolent.guzzy@gmail.com>
2018-11-08 21:02:49 +0800

    Fix whitespaces and comments

--- src/item.cpp
+++ src/item.cpp
@@ -536,7 +536,8 @@ long item::charges_per_volume( const units::volume &vol ) const
     if( type->volume == 0 ) {
         return INFINITE_CHARGES; // TODO: items should not have 0 volume at all!
     }
-    return count_by_charges() ? vol * static_cast<int64_t>(type->stack_size) / type->volume
+    // Type cast to prevent integer overflow with large volume containers like the cargo dimension
+    return count_by_charges() ? vol * static_cast<int64_t>( type->stack_size ) / type->volume
                               : vol / volume();
 }
 
