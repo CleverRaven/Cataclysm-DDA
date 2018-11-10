298f1638611fd9933b7356898e66e999027f26c2
josephosss@gmail.com <josephosss@gmail.com>
2018-11-11 00:01:52 +0100

    Fixed red tinted shadows.

--- src/cata_tiles.cpp
+++ src/cata_tiles.cpp
@@ -2551,7 +2551,7 @@ bool cata_tiles::draw_item_highlight( const tripoint &pos )
 
 SDL_Surface_Ptr create_tile_surface( const int w, const int h )
 {
-    return CreateRGBSurface( 0, w, h, 32, 0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF );
+    return CreateRGBSurface( 0, w, h, 32, 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000 );
 }
 
 SDL_Surface_Ptr cata_tiles::create_tile_surface()
