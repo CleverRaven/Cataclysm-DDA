72dbbd0302e0a3531c9edbad76dcb270f927a99f
John Bytheway <jbytheway@gmail.com>
2018-11-17 19:08:05 +0000

    Treat apt-get update warnings as errors
    If apt-get update fails we want to retry (with travis_retry).  Currently
    it can fail in a manner that only generates a warning and does not
    trigger a retry.  This is an attempt to retry in such cases.

--- build-scripts/requirements.sh
+++ build-scripts/requirements.sh
@@ -14,7 +14,13 @@ if [ -n "${MXE_TARGET}" ]; then
     | sudo tee /etc/apt/sources.list.d/mxeapt.list
   travis_retry sudo apt-key adv --keyserver x-hkp://keyserver.ubuntu.com:80 \
     --recv-keys D43A795B73B16ABE9643FE1AFD8FFF16DB45C6AB
-  travis_retry sudo apt-get update
+  # We need to treat apt-get update warnings as errors for which the exit code
+  # is not sufficient.  The following workaround inspired by
+  # https://unix.stackexchange.com/questions/175146/apt-get-update-exit-status/
+  exec {fd}>&2
+  travis_retry bash -o pipefail -c \
+      "sudo apt-get update 2>&1 | tee /dev/fd/$fd | ( ! grep -q -e '^Err:' -e '^[WE]:' )"
+  exec {fd}>&-
 
   MXE2_TARGET=$(echo "$MXE_TARGET" | sed 's/_/-/g')
   export MXE_DIR=/usr/lib/mxe/usr/bin
