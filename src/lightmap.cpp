0c2f273064ff89b700a87b5cc758e58d4c3b4ec1
John Bytheway <jbytheway@gmail.com>
2018-11-22 16:37:42 +0000

    Cast natural light regardless of brightness
    Previously, natural light was only cast onto walls and into buildings
    during the day.  This had strange side-effects, like walls being almost
    invisible at night.
    
    Remove that check and simply always cast the light.
    
    Fixes #13786

--- src/lightmap.cpp
+++ src/lightmap.cpp
@@ -224,7 +224,7 @@ void map::generate_lightmap( const int zlev )
                     const int y = sy + smy * SEEY;
                     const tripoint p( x, y, zlev );
                     // Project light into any openings into buildings.
-                    if( natural_light > LIGHT_SOURCE_BRIGHT && !outside_cache[p.x][p.y] ) {
+                    if( !outside_cache[p.x][p.y] ) {
                         // Apply light sources for external/internal divide
                         for( int i = 0; i < 4; ++i ) {
                             if( INBOUNDS( p.x + dir_x[i], p.y + dir_y[i] ) &&
