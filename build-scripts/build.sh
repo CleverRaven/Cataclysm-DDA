4886d6111fff5788d5f9761d83c4bc0b00605544
John Bytheway <jbytheway@gmail.com>
2018-11-30 22:14:11 +0000

    Add cmake support in build.sh
    With a view to cmake builds in Travis

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-set -ev
+# Build script intended for use in Travis CI
 
-make -j3 RELEASE=1 BACKTRACE=1 DEBUG_SYMBOLS=1 CROSS="$CROSS_COMPILATION"
-$WINE ./tests/cata_test -r cata --rng-seed `shuf -i 0-1000000000 -n 1`
-if [ -n "$MODS" ]
+set -ex
+
+if [ -n "$CMAKE" ]
 then
-    $WINE ./tests/cata_test -r cata --rng-seed `shuf -i 0-1000000000 -n 1` $MODS
+    mkdir build
+    cd build
+    cmake \
+        -DBACKTRACE=ON \
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+        -DTILES=${TILES:-0} \
+        -DSOUND=${SOUND:-0} \
+        ..
+    make -j3
+    ctest --output-on-failure
+else
+    make -j3 RELEASE=1 BACKTRACE=1 DEBUG_SYMBOLS=1 CROSS="$CROSS_COMPILATION"
+    $WINE ./tests/cata_test -r cata --rng-seed `shuf -i 0-1000000000 -n 1`
+    if [ -n "$MODS" ]
+    then
+        $WINE ./tests/cata_test -r cata --rng-seed `shuf -i 0-1000000000 -n 1` $MODS
+    fi
 fi
