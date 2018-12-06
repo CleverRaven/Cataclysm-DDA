d77f8b9a31fa6f749f7e8f92ff0db1e4536d343b
Karthas077 <paranoidandroid077@gmail.com>
2018-12-06 10:41:35 -0800

    Combine vehicle and terrain loops

--- src/activity_item_handling.cpp
+++ src/activity_item_handling.cpp
@@ -932,23 +932,20 @@ void activity_on_turn_move_loot( player_activity &, player &p )
 
         //Check source for cargo part
         //map_stack and vehicle_stack are different types but inherit from item_stack
-        //TODO: use one for loop
+        item_stack *src_items;
         if( const cata::optional<vpart_reference> vp = g->m.veh_at( src_loc ).part_with_feature( "CARGO",
                 false ) ) {
             src_veh = &vp->vehicle();
             src_part = vp->part_index();
-            for( auto &it : src_veh->get_items( src_part ) ) {
-                if( !it.made_of_from_type( LIQUID ) ) { // skip unpickable liquid
-                    items.push_back( &it );
-                }
-            }
+            src_items = &src_veh->get_items( src_part );
         } else {
             src_veh = nullptr;
             src_part = -1;
-            for( auto &it : g->m.i_at( src_loc ) ) {
-                if( !it.made_of_from_type( LIQUID ) ) { // skip unpickable liquid
-                    items.push_back( &it );
-                }
+            src_items = &g->m.i_at( src_loc );
+        }
+        for( auto &it : *src_items ) {
+            if( !it.made_of_from_type( LIQUID ) ) { // skip unpickable liquid
+                items.push_back( &it );
             }
         }
 
