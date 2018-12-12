2d067b9a873dada11d99578d57fbfed8b1c81f9a
John Bytheway <jbytheway@gmail.com>
2018-12-12 15:38:55 +0000

    More flat_set tests

--- tests/flat_set_test.cpp
+++ tests/flat_set_test.cpp
@@ -13,7 +13,7 @@ BOOST_CONCEPT_ASSERT( ( boost::UniqueAssociativeContainer<cata::flat_set<int>> )
 BOOST_CONCEPT_ASSERT( ( boost::ReversibleContainer<cata::flat_set<int>> ) );
 #endif
 
-TEST_CASE( "flat_set_insertion", "[flat_set]" )
+TEST_CASE( "flat_set", "[flat_set]" )
 {
     cata::flat_set<int> s;
     s.insert( 2 );
@@ -21,6 +21,34 @@ TEST_CASE( "flat_set_insertion", "[flat_set]" )
     s.insert( 4 );
     s.insert( 3 );
     std::vector<int> ref{ 1, 2, 3, 4 };
+    REQUIRE( s.size() == ref.size() );
+    CHECK( std::equal( s.begin(), s.end(), ref.begin() ) );
+    CHECK( s.count( 0 ) == 0 );
+    CHECK( s.count( 1 ) == 1 );
+    CHECK( s.count( 2 ) == 1 );
+    CHECK( s.count( 3 ) == 1 );
+    CHECK( s.count( 4 ) == 1 );
+    CHECK( s.count( 5 ) == 0 );
+
+    CHECK( s.equal_range( 0 ) == std::make_pair( s.begin(), s.begin() ) );
+    CHECK( s.equal_range( 1 ) == std::make_pair( s.begin(), s.begin() + 1 ) );
+    CHECK( s.equal_range( 2 ) == std::make_pair( s.begin() + 1, s.begin() + 2 ) );
+    CHECK( s.equal_range( 5 ) == std::make_pair( s.end(), s.end() ) );
+
+    CHECK( s.find( 0 ) == s.end() );
+    CHECK( s.find( 1 ) == s.begin() );
+    CHECK( s.find( 2 ) == s.begin() + 1 );
+    CHECK( s.find( 5 ) == s.end() );
+}
+
+TEST_CASE( "reversed_flat_set_insertion", "[flat_set]" )
+{
+    cata::flat_set<int, std::greater<int>> s;
+    s.insert( 2 );
+    s.insert( 1 );
+    s.insert( 4 );
+    s.insert( 3 );
+    std::vector<int> ref{ 4, 3, 2, 1 };
     REQUIRE( s.size() == ref.size() );
     CHECK( std::equal( s.begin(), s.end(), ref.begin() ) );
     CHECK( s.count( 0 ) == 0 );
