4e752f758fdda4d77e64a23807e021d25f8b9c27
John Bytheway <jbytheway@gmail.com>
2018-12-13 15:06:41 +0000

    Test on npcs as well as player

--- tests/encumbrance_test.cpp
+++ tests/encumbrance_test.cpp
@@ -1,9 +1,10 @@
 #include "catch/catch.hpp"
 
 #include "game.h"
+#include "npc.h"
 #include "player.h"
 
-void test_encumbrance(
+void test_encumbrance_on(
     player &p,
     const std::vector<itype_id> &clothing,
     const std::string &body_part,
@@ -20,7 +21,24 @@ void test_encumbrance(
     CHECK( enc.encumbrance == expected_encumbrance );
 }
 
+void test_encumbrance(
+    const std::vector<itype_id> &clothing,
+    const std::string &body_part,
+    int expected_encumbrance
+)
+{
+    {
+        INFO( "testing on player" );
+        test_encumbrance_on( g->u, clothing, body_part, expected_encumbrance );
+    }
+    {
+        INFO( "testing on npc" );
+        npc example_npc;
+        test_encumbrance_on( example_npc, clothing, body_part, expected_encumbrance );
+    }
+}
+
 TEST_CASE( "regular_clothing_encumbrance", "[encumbrance]" )
 {
-    test_encumbrance( g->u, { "longshirt" }, "TORSO", 3 );
+    test_encumbrance( { "longshirt" }, "TORSO", 3 );
 }
