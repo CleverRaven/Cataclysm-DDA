38851b0fa1fd46b5b362e684b94707d7303ba447
serv-inc <serv-inc@users.noreply.github.com>
2018-12-14 10:43:47 +0100

    Change json-check-script to fail fast, return non-zero exit status and echo the name of the offending file

--- build-scripts/lint-json.sh
+++ build-scripts/lint-json.sh
@@ -1,1 +1,1 @@
-find . -name *json -type f -exec python -m json.tool {} >/dev/null \;
+find . -name *json -type f -exec sh -c 'python -m json.tool "$0" >/dev/null || (echo JSON parsing failed on "$0" && kill $PPID)' \{\} \;
