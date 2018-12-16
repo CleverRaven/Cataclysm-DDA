9c952e82e8102efab50fc75546ae7c51165caa5e
tenmillimaster <mitchell.amstutz@gmail.com>
2018-12-16 14:48:32 -0600

    Non-aux gun mode gunmod fix
    This should fix gunmods that add  Non-auxiliary gun modes, while not breaking bayonets.

--- src/item.cpp
+++ src/item.cpp
@@ -5333,9 +5333,15 @@ std::map<gun_mode_id, gun_mode> item::gun_all_modes() const
             }
 
             // non-auxiliary gunmods may provide additional modes for the base item
+
+
         } else if( e->is_gunmod() ) {
-            for( auto m : e->type->gunmod->mode_modifier ) {
-                res.emplace( m.first, gun_mode { m.second.name(), const_cast<item *>( e ),
+            for( auto m : e->type->gunmod->mode_modifier )
+                  if( m.first == ("REACH"))   {
+				res.emplace( m.first, gun_mode { m.second.name(), const_cast<item *>( e ),
+                                                 m.second.qty(), m.second.flags() } );
+            } else {
+        		res.emplace( m.first, gun_mode { m.second.name(), const_cast<item *>( this ),
                                                  m.second.qty(), m.second.flags() } );
             }
         }
