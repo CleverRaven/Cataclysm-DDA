5ca28c3797079665127dbb3adfb842de37cc79b2
halfahermit <halfahermit@riseup.net>
2018-12-20 18:48:40 -0600

    Block ascending an upstair when there's no corresponding downstair
    One-way upstairs can trap the player
    
    references #27195

--- src/game.cpp
+++ src/game.cpp
@@ -11842,6 +11842,10 @@ cata::optional<tripoint> game::find_or_make_stairs( map &mp, const int z_after, 
     }
 
     if( movez > 0 ) {
+        if( !mp.has_flag( "GOES_DOWN", *stairs ) ) {
+            popup( _( "Halfway up, the way up becomes blocked off." ) );
+            return cata::nullopt;
+        }
         // Manhole covers need this to work
         // Maybe require manhole cover here and fail otherwise?
         return stairs;
