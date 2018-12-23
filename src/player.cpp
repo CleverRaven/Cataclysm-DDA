1567bab4d08b5f3f577cd2dfa89411f4daea36df
NotFuji <andyfuji778@gmail.com>
2018-12-23 13:44:50 -0600

    morestyle

--- src/player.cpp
+++ src/player.cpp
@@ -8905,7 +8905,8 @@ bool player::gunmod_remove( item &gun, item& mod )
                 if( the_mod->type->gunmod->location == slot.first && free_slots < 0 ) {
                     gunmod_remove( gun, *the_mod );
                     free_slots++;
-                } else if( mod_locations.find( the_mod->type->gunmod->location ) == mod_locations.end() ) {
+                } else if( mod_locations.find( the_mod->type->gunmod->location ) == 
+                           mod_locations.end() ) {
                     gunmod_remove( gun, *the_mod );
                 }
             }
