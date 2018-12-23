8df70e148344cd125ec9d69b1f119786eb269ed1
NotFuji <andyfuji778@gmail.com>
2018-12-22 21:42:26 -0600

    Removed commented code

--- src/regional_settings.cpp
+++ src/regional_settings.cpp
@@ -845,19 +845,6 @@ void building_bin::finalize()
             current_id = overmap_specials::create_building_from( converted_id );
         }
         const overmap_special &cur_special = current_id.obj();
-        for( const overmap_special_terrain &ter : cur_special.terrains ) {
-            const tripoint &p = ter.p;
-            /*
-            if( p.x != 0 || p.y != 0 ) {
-                debugmsg( "Tried to add city building %s, but it has a part with non-zero X or Y coordinates (not supported yet)",
-                          current_id.c_str() );
-                skip = true;
-                break;
-            }*/
-        }
-        if( skip ) {
-            continue;
-        }
         buildings.add( current_id, pr.second );
     }
 
