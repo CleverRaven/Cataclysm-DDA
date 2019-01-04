1cb2ec6d5ba126ad446f4e728220687dfcc5f705
KorGgenT <curtis.r.merrill@gmail.com>
2019-01-04 18:43:32 -0500

    astyle

--- src/consumption.cpp
+++ src/consumption.cpp
@@ -102,7 +102,7 @@ int player::nutrition_for( const item &comest ) const
     if( comest.components.size() > 0 && !comest.has_flag( "NUTRIENT_OVERRIDE" ) ) {
         int byproduct_multiplier;
         for( item component : comest.components ) {
-            component.has_flag("BYPRODUCT") ? byproduct_multiplier = -1 : byproduct_multiplier = 1;
+            component.has_flag( "BYPRODUCT" ) ? byproduct_multiplier = -1 : byproduct_multiplier = 1;
             nutr += this->nutrition_for( component ) * component.charges * byproduct_multiplier;
         }
         nutr /= comest.recipe_charges;
@@ -225,8 +225,9 @@ std::map<vitamin_id, int> player::vitamins_from( const item &it ) const
             comp.has_flag( "BYPRODUCT" ) ? byproduct_multiplier = -1 : byproduct_multiplier = 1;
             std::map<vitamin_id, int> component_map = this->vitamins_from( comp );
             for( const auto &vit : component_map ) {
-                res.operator[]( vit.first ) += byproduct_multiplier * ceil( static_cast<float>( vit.second ) / static_cast<float>
-                                                     ( it.type->charges_default() ) );
+                res.operator[]( vit.first ) += byproduct_multiplier * ceil( static_cast<float>
+                                               ( vit.second ) / static_cast<float>
+                                               ( it.type->charges_default() ) );
             }
         }
     } else {
