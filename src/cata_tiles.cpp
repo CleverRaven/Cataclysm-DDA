fdffa3915ca65e50be848eca70ca2c46e4dc9ee8
Kevin Granade <kevin.granade@gmail.com>
2019-01-11 06:07:34 +0000

    Fix ASCII fallback for corpses.

--- src/cata_tiles.cpp
+++ src/cata_tiles.cpp
@@ -1835,7 +1835,12 @@ bool cata_tiles::draw_from_id_string( std::string id, TILE_CATEGORY category,
                 col = t.color;
             }
         } else if( category == C_ITEM ) {
-            const auto tmp = item( id, 0 );
+            item tmp;
+            if( 0 == id.compare( 0, 7, "corpse_" ) ) {
+                tmp = item( "corpse", 0 );
+            } else {
+                tmp = item( id, 0 );
+            }
             sym = tmp.symbol().empty() ? ' ' : tmp.symbol().front();
             col = tmp.color();
         }
