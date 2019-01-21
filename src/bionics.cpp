104d7ade0b82a63c5f6d1f349cabce20a8fa4dbf
Firestorm01X2 <firestorm01x2@gmail.com>
2019-01-21 20:27:06 +0300

    Excited not slighltly excited. Why not? You are crazy mutant after all
    Excited not slighltly excited. Why not? You are crazy mutant after all

--- src/bionics.cpp
+++ src/bionics.cpp
@@ -1556,7 +1556,7 @@ void player::introduce_into_anesthesia( const time_duration &duration, player &i
         //post-threshold medical mutants do not fear operations.
         if( has_trait( trait_THRESH_MEDICAL ) ) {
             add_msg_if_player( m_mixed,
-                               _( "You feel calm and even slightly excited, as the Autodoc slices painlessly into you.  You pass out when the blades reach your line of sight." ) );
+                               _( "You feel calm and even excited, as the Autodoc slices painlessly into you.  You pass out when the blades reach your line of sight." ) );
         } else {
             add_msg_if_player( m_mixed,
                                _( "You stay very, very still, focusing intently on an interesting rock on the ceiling, as the Autodoc slices painlessly into you.  Mercifully, you pass out when the blades reach your line of sight." ) );
