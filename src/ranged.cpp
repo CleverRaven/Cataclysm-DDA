95d1126b3ff57b0bcf06c4622dad654fa6b207f9
tenmillimaster <mitchell.amstutz@gmail.com>
2019-01-22 22:49:55 -0600

    Update ranged.cpp
    Added exceptions for laser or plasma. RNG fuzz max lowered. Inconsequential damage raised. Got rid of redundant check for consumable flag, replaced with nulling of player message if damage state didn't actually change

--- src/ranged.cpp
+++ src/ranged.cpp
@@ -191,19 +191,20 @@ bool player::handle_gun_damage( item &it )
         // This is mostly for crappy handmade expedient stuff  or things that rarely receive damage during normal usage.
         // Default chance is 1/10000 unless set via json, damage is proportional to caliber(see below).
         // Can be toned down with 'consume_divisor.'
-        } else if( it.has_flag( "CONSUMABLE" ) ) {
+      } else if( it.has_flag( "CONSUMABLE" ) ) &&  !curammo_effects.count( "LASER" ) &&  !curammo_effects.count( "PLASMA" ) {
             int uncork = ( ( 10 * it.ammo_data()->ammo->loudness ) + ( it.ammo_data()->ammo->recoil / 2 ) ) / 100;
             uncork = std::pow( uncork, 3 ) * 6.5;
             for( auto mod : it.gunmods() ) {
                 if( mod->has_flag( "CONSUMABLE" ) ) {
                     int dmgamt = uncork / mod->type->gunmod->consume_divisor;
                     int modconsume = mod->type->gunmod->consume_chance;
+                    int initstate = it.damage();
                     // fuzz damage if it's small
                     if( dmgamt < 1000 ) {
-                        dmgamt = rng( dmgamt, 1300 );
+                        dmgamt = rng( dmgamt, 1100 );
                         // ignore damage if inconsequential.
                     }
-                    if( dmgamt < 600 ) {
+                    if( dmgamt < 800 ) {
                         dmgamt = 0;
                     }
                     if( one_in( modconsume ) ) {
@@ -212,7 +213,7 @@ bool player::handle_gun_damage( item &it )
                                                    _( "<npcname>'s attached %s is destroyed by their shot!" ),
                                                    mod->tname().c_str() );
                             i_rem( mod );
-                        } else if( mod->has_flag( "CONSUMABLE" )  ) {
+                        } else if( it.damage() > initstate ) {
                             add_msg_player_or_npc( m_bad,  _( "Your attached %s is damaged by your shot!" ),
                                                    _( "<npcname>'s %s is damaged by their shot!" ),
                                                    mod->tname().c_str() );
