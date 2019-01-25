f0dd466ae63616bb52bbcae800076fd19a3a4981
John Bytheway <jbytheway@gmail.com>
2019-01-25 08:27:30 +0000

    Move M_PI workaround to math_defines.h
    Turns out M_PI isn't standard C, it's POSIX.  In particular, it's not
    present on Mingw.  So we need to provide our own fallback definition.

--- src/math_defines.h
+++ src/math_defines.h
@@ -15,4 +15,10 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 
+// And on mingw even that doesn't work, so we are forced to have our own
+// fallback definition.
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 #endif // CATA_MATH_DEFINES_H
