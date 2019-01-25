1c1d7bf5cc627a6e99a5f2133de2532137973713
Kevin Granade <kevin.granade@gmail.com>
2019-01-25 04:45:55 +0000

    Add (failing) map bounds checking test

--- tests/map_test.cpp
+++ tests/map_test.cpp
@@ -22,3 +22,43 @@ TEST_CASE( "destroy_grabbed_furniture" )
         }
     }
 }
+
+TEST_CASE( "map_bounds_checking" )
+{
+    map m;
+    m.load( 0, 0, 0, false );
+    for( int x = -1; x <= MAPSIZE_X; ++x ) {
+        for( int y = -1; y <= MAPSIZE_Y; ++y ) {
+            for( int z = -OVERMAP_DEPTH - 1; z <= OVERMAP_HEIGHT + 1; ++z ) {
+                INFO( "( " << x << ", " << y << ", " << z << " )" );
+                if( x < 0 || x >= MAPSIZE_X ||
+                    y < 0 || y >= MAPSIZE_Y ||
+                    z < -OVERMAP_DEPTH || z > OVERMAP_HEIGHT ) {
+                    CHECK( !m.ter( { x, y, z } ) );
+                } else {
+                    CHECK( m.ter( { x, y, z } ) );
+                }
+            }
+        }
+    }
+}
+
+TEST_CASE( "tinymap_bounds_checking" )
+{
+    tinymap m;
+    m.load( 0, 0, 0, false );
+    for( int x = -1; x <= SEEX * 2; ++x ) {
+        for( int y = -1; y <= SEEY * 2; ++y ) {
+            for( int z = -OVERMAP_DEPTH - 1; z <= OVERMAP_HEIGHT + 1; ++z ) {
+                INFO( "( " << x << ", " << y << ", " << z << " )" );
+                if( x < 0 || x >= SEEX * 2 ||
+                    y < 0 || y >= SEEY * 2 ||
+                    z < -OVERMAP_DEPTH || z > OVERMAP_HEIGHT ) {
+                    CHECK( !m.ter( { x, y, z } ) );
+                } else {
+                    CHECK( m.ter( { x, y, z } ) );
+                }
+            }
+        }
+    }
+}
