75b2a871788623d8d2d4024b8b8c2960b2a10b48
Hirmuolio <hirmuolio@gmail.com>
2019-02-02 19:56:17 +0200

    Tabs -> spaces, order of stings

--- src/iexamine.cpp
+++ src/iexamine.cpp
@@ -1735,7 +1735,7 @@ void iexamine::egg_sack_generic( player &p, const tripoint &examp,
         return;
     }
     g->m.furn_set( examp, f_egg_sacke );
-	int monster_count = 0;
+    int monster_count = 0;
     if( one_in( 2 ) ) {
         const std::vector<tripoint> pts = closest_tripoints_first( 1, examp );
         for( const auto &pt : pts ) {
@@ -1744,14 +1744,8 @@ void iexamine::egg_sack_generic( player &p, const tripoint &examp,
                 monster_count++;
             }
         }
-        if( monster_count == 1 ) {
-            add_msg( m_warning, _( "A spiderling bursts from the %s!" ), old_furn_name.c_str() );
-        } else if( monster_count >= 1 ) {
-            add_msg( m_warning, _( "Spiderlings burst from the %s!" ), old_furn_name.c_str() );
-        }
     }
-
-	int roll = round( rng_float( 1, 5 ) );
+    int roll = round( rng_float( 1, 5 ) );
     for( int i = 0; i < roll; i++ ) {
         if( monster_count == 0 && p.can_pickWeight( item( "spider_egg" ), true ) && p.can_pickVolume( item( "spider_egg" ), true ) ){
             p.i_add( item( "spider_egg" ) );
@@ -1760,6 +1754,11 @@ void iexamine::egg_sack_generic( player &p, const tripoint &examp,
             g->m.add_item_or_charges( p.pos(), item( "spider_egg" ) );
             p.add_msg_if_player( _( "You harvest and drop: spider egg" ) );
         }
+    }
+    if( monster_count == 1 ) {
+        add_msg( m_warning, _( "A spiderling bursts from the %s!" ), old_furn_name.c_str() );
+    } else if( monster_count >= 1 ) {
+        add_msg( m_warning, _( "Spiderlings burst from the %s!" ), old_furn_name.c_str() );
     }
 }
 
