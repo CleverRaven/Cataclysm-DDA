2370e2061690af692962cac396d4814b9f4d632e
Kevin Granade <kevin.granade@gmail.com>
2019-02-08 15:38:26 -0800

    Astyle

--- src/monster.cpp
+++ src/monster.cpp
@@ -2014,7 +2014,7 @@ void monster::drop_items_on_death()
         return;
     }
     const auto dropped = g->m.put_items_from_loc( type->death_drops, pos(),
-                                                  calendar::time_of_cataclysm );
+                         calendar::time_of_cataclysm );
 
     if( has_flag( MF_FILTHY ) ) {
         for( const auto &it : dropped ) {
