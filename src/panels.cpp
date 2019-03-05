c094cab57c3725e551674c9b13b952847d5e4b88
KorGgenT <curtis.r.merrill@gmail.com>
2019-03-05 15:32:28 -0500

    Revert "add character to force window to render"
    This reverts commit e1fd512ddb56ede064114a9a469ae311952f80c6.

--- src/panels.cpp
+++ src/panels.cpp
@@ -1494,7 +1494,6 @@ void draw_messages_classic( player &, const catacurses::window &w )
 void draw_mminimap( player &, const catacurses::window &w )
 {
     werase( w );
-    mvwputch( w, 0, 0, c_black, " " ); // fakeout!
     g->draw_pixel_minimap( w );
     wrefresh( w );
 }
