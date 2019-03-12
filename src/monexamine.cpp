948f2f55620b60ee021850753178423b8c769ee8
Mark Langsdorf <mlangsdo@redhat.com>
2019-03-12 10:50:09 -0500

    monsters: allow pets to wear pet_armor
    add options to the pet_menu to let pets wear armor that is suitable for
    their body type and body volume.  Pet armor can add storage just like a
    bag but is not removed when a bag is dumped.  Pet armor can also be
    removed.  Pet armor provides 100% coverage protection according to its
    material properties.

--- src/monexamine.cpp
+++ src/monexamine.cpp
@@ -23,6 +23,7 @@ const species_id ZOMBIE( "ZOMBIE" );
 const efftype_id effect_controlled( "controlled" );
 const efftype_id effect_has_bag( "has_bag" );
 const efftype_id effect_milked( "milked" );
+const efftype_id effect_monster_armor( "monster_armor" );
 const efftype_id effect_tied( "tied" );
 
 bool monexamine::pet_menu( monster &z )
@@ -34,6 +35,8 @@ bool monexamine::pet_menu( monster &z )
         attach_bag,
         drop_all,
         give_items,
+        mon_armor_add,
+        mon_armor_remove,
         play_with_pet,
         pheromone,
         milk,
@@ -52,11 +55,17 @@ bool monexamine::pet_menu( monster &z )
     amenu.addentry( swap_pos, true, 's', _( "Swap positions" ) );
     amenu.addentry( push_zlave, true, 'p', _( "Push %s" ), pet_name );
     amenu.addentry( rename, true, 'e', _( "Rename" ) );
-    if( z.has_effect( effect_has_bag ) ) {
+    if( z.has_effect( effect_has_bag ) || z.has_effect( effect_monster_armor ) ) {
         amenu.addentry( give_items, true, 'g', _( "Place items into bag" ) );
-        amenu.addentry( drop_all, true, 'd', _( "Drop all items" ) );
-    } else {
+        amenu.addentry( drop_all, true, 'd', _( "Drop all items except armor" ) );
+    }
+    if( !z.has_effect( effect_has_bag ) ) {
         amenu.addentry( attach_bag, true, 'b', _( "Attach bag" ) );
+    }
+    if( z.has_effect( effect_monster_armor ) ) {
+        amenu.addentry( mon_armor_remove, true, 'a', _( "Remove armor from %s" ), pet_name );
+    } else {
+        amenu.addentry( mon_armor_add, true, 'a', _( "Equip %s with armor" ), pet_name );
     }
     if( z.has_flag( MF_BIRDFOOD ) || z.has_flag( MF_CATFOOD ) || z.has_flag( MF_DOGFOOD ) ) {
         amenu.addentry( play_with_pet, true, 'y', _( "Play with %s" ), pet_name );
@@ -101,6 +110,12 @@ bool monexamine::pet_menu( monster &z )
         case give_items:
             return give_items_to( z );
             break;
+        case mon_armor_add:
+            return add_armor( z );
+            break;
+        case mon_armor_remove:
+            remove_armor( z );
+            break;
         case play_with_pet:
             if( query_yn( _( "Spend a few minutes to play with your %s?" ), pet_name ) ) {
                 play_with( z );
@@ -121,6 +136,16 @@ bool monexamine::pet_menu( monster &z )
             break;
     }
     return true;
+}
+
+int monexamine::pet_armor_pos( monster &z )
+{
+    int pos = g->inv_for_filter( _( "Pet armor" ), [z]( const item & it ) {
+        return z.type->bodytype == it.get_pet_armor_bodytype() &&
+               z.get_volume() >= it.get_pet_armor_min_vol() &&
+               z.get_volume() <= it.get_pet_armor_max_vol();
+    } );
+    return pos;
 }
 
 void monexamine::swap( monster &z )
@@ -190,7 +215,8 @@ void monexamine::attach_bag_to( monster &z )
     }
 
     item &it = g->u.i_at( pos );
-    z.add_item( it );
+    // force it to the front of the monster's inventory in case they have armor on
+    z.inv.insert( z.inv.begin(), it );
     add_msg( _( "You mount the %1$s on your %2$s, ready to store gear." ),
              it.display_name(), pet_name );
     g->u.i_rem( pos );
@@ -198,16 +224,30 @@ void monexamine::attach_bag_to( monster &z )
     g->u.moves -= 200;
 }
 
-
 void monexamine::dump_items( monster &z )
 {
     std::string pet_name = z.get_name();
+    int armor_index = 0;
+    bool found_armor = false;
     for( auto &it : z.inv ) {
-        g->m.add_item_or_charges( z.pos(), it );
+        if( z.has_effect( effect_monster_armor ) && it.is_pet_armor( true ) ) {
+            found_armor = true;
+        } else {
+            armor_index += 1;
+            g->m.add_item_or_charges( z.pos(), it );
+        }
+    }
+    item armor;
+    if( found_armor ) {
+        armor = z.inv[ armor_index ];
     }
 
     z.inv.clear();
     z.remove_effect( effect_has_bag );
+    if( found_armor ) {
+        armor.set_var( "pet_armor", "true" );
+        z.add_item( armor );
+    }
     add_msg( _( "You dump the contents of the %s's bag on the ground." ), pet_name );
     g->u.moves -= 200;
 }
@@ -220,15 +260,27 @@ bool monexamine::give_items_to( monster &z )
         return true;
     }
 
-    item &it = z.inv[0];
-
-    if( !it.is_armor() ) {
-        add_msg( _( "There is no container on your %s to put things in!" ), pet_name );
-        return true;
+    int armor_index = INT_MIN;
+    if( z.has_effect( effect_monster_armor ) ) {
+        armor_index = 0;
+        for( auto &it : z.inv ) {
+            if( it.is_pet_armor( true ) ) {
+                break;
+            } else {
+                armor_index += 1;
+            }
+        }
     }
 
-    units::volume max_cap = it.get_storage();
-    units::mass max_weight = z.weight_capacity() - it.weight();
+    // might be a bag, might be armor
+    item &storage = z.inv[0];
+    units::volume max_cap = storage.get_storage();
+    units::mass max_weight = z.weight_capacity() - storage.weight();
+    if( armor_index != 0 && armor_index != INT_MIN ) {
+        item &armor = z.inv[armor_index];
+        max_cap += armor.get_storage() + armor.volume();
+        max_weight -= armor.weight();
+    }
 
     if( z.inv.size() > 1 ) {
         for( auto &i : z.inv ) {
@@ -239,12 +291,12 @@ bool monexamine::give_items_to( monster &z )
 
     if( max_weight <= 0_gram ) {
         add_msg( _( "%1$s is overburdened. You can't transfer your %2$s." ),
-                 pet_name, it.tname( 1 ) );
+                 pet_name, storage.tname( 1 ) );
         return true;
     }
     if( max_cap <= 0_ml ) {
         add_msg( _( "There's no room in your %1$s's %2$s for that, it's too bulky!" ),
-                 pet_name, it.tname( 1 ) );
+                 pet_name, storage.tname( 1 ) );
         return true;
     }
 
@@ -255,6 +307,60 @@ bool monexamine::give_items_to( monster &z )
         return true;
     }
     return false;
+}
+
+bool monexamine::add_armor( monster &z )
+{
+    std::string pet_name = z.get_name();
+    int pos = pet_armor_pos( z );
+
+    if( pos == INT_MIN ) {
+        add_msg( _( "Never mind." ) );
+        return true;
+    }
+
+    item &armor = g->u.i_at( pos );
+    units::mass max_weight = z.weight_capacity();
+    for( auto &i : z.inv ) {
+        max_weight -= i.weight();
+    }
+    if( max_weight <= 0_gram ) {
+        add_msg( _( "Your %1$s is too heavy for your %2$s." ), armor.tname( 1 ), pet_name );
+        return true;
+    }
+
+    armor.set_var( "pet_armor", "true" );
+    z.add_item( armor );
+    add_msg( _( "You put the %1$s on your %2$s, protecting it from future harm." ),
+             armor.display_name(), pet_name );
+    g->u.i_rem( pos );
+    z.add_effect( effect_monster_armor, 1_turns, num_bp, true );
+    g->u.moves -= 200;
+    return true;
+}
+
+void monexamine::remove_armor( monster &z )
+{
+    std::string pet_name = z.get_name();
+    bool found_armor = false;
+    int pos = 0;
+    for( auto &it : z.inv ) {
+        if( it.is_pet_armor( true ) ) {
+            found_armor = true;
+            it.erase_var( "pet_armor" );
+            g->m.add_item_or_charges( z.pos(), it );
+            add_msg( m_info, "You remove the %1$s from %2$s.", it.tname( 1 ), pet_name );
+            z.inv.erase( z.inv.begin() + pos );
+            g->u.moves -= 200;
+            break;
+        } else {
+            pos += 1;
+        }
+    }
+    if( !found_armor ) {
+        add_msg( m_bad, _( "Your %1$s isn't wearing armor!" ), pet_name );
+    }
+    z.remove_effect( effect_monster_armor );
 }
 
 void monexamine::play_with( monster &z )
