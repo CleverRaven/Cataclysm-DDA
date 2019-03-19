be05bb29c6c8e25cee5b6749fdd314ad7297fc89
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2019-03-19 18:57:35 +0300

    Allow wet armor items

--- src/newcharacter.cpp
+++ src/newcharacter.cpp
@@ -568,6 +568,10 @@ bool player::create( character_type type, const std::string &tempname )
     std::list<item> prof_items = prof->items( male, get_mutations() );
 
     for( item &it : prof_items ) {
+        if( it.has_flag( "WET" ) ) {
+            it.active = true;
+            it.item_counter = 450; // Give it some time to dry off
+        }
         // TODO: debugmsg if food that isn't a seed is inedible
         if( it.has_flag( "no_auto_equip" ) ) {
             it.unset_flag( "no_auto_equip" );
@@ -575,10 +579,6 @@ bool player::create( character_type type, const std::string &tempname )
         } else if( it.is_armor() ) {
             // TODO: debugmsg if wearing fails
             wear_item( it, false );
-        } else if( it.has_flag( "WET" ) ) {
-            it.active = true;
-            it.item_counter = 450; // Give it some time to dry off
-            inv.push_back( it );
         } else {
             inv.push_back( it );
         }
