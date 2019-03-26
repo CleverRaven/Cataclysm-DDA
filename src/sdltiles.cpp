de93058080949d47f3eda15480303a862d8f629e
Wokko1 <45448310+Wokko1@users.noreply.github.com>
2019-03-26 03:29:07 -0700

    Update sdltiles.cpp

--- src/sdltiles.cpp
+++ src/sdltiles.cpp
@@ -2437,6 +2437,7 @@ void CheckMessages()
                     // Only prioritize movement options if we're not driving.
                     if( !g->u.controlling_vehicle ) {
                         actions.insert( ACTION_TOGGLE_MOVE );
+                        actions.insert( ACTION_TOGGLE_CROUCHING );
                     }
                     // Only prioritize fire weapon options if we're wielding a ranged weapon.
                     if( g->u.weapon.is_gun() || g->u.weapon.has_flag( "REACH_ATTACK" ) ) {
@@ -2448,10 +2449,19 @@ void CheckMessages()
                 if( g->u.move_mode != "walk" ) {
                     actions.insert( ACTION_TOGGLE_MOVE );
                 }
+                
+                // If we're already crouching, make it simple to toggle crouching to off.
+                if( g->u.is_crouching ) {
+                    actions.insert( ACTION_TOGGLE_CROUCHING );
+                }
 
                 // We're not already running or in combat, so remove toggle walk/run
                 if( std::find( actions.begin(), actions.end(), ACTION_TOGGLE_MOVE ) == actions.end() ) {
                     actions_remove.insert( ACTION_TOGGLE_MOVE );
+                }
+                
+                if( std::find( actions.begin(), actions.end(), ACTION_TOGGLE_CROUCHING ) == actions.end() ) {
+                    actions_remove.insert( ACTION_TOGGLE_CROUCHING );
                 }
 
                 // Check if we can perform one of our actions on nearby terrain. If so,
