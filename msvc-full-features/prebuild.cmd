f55c38a327f2af2223a58ef5c01f625ecef1ec54
Crunchepillar <rnixon98@gmail.com>
2019-03-28 23:43:28 -0500

    boring update

--- msvc-full-features/prebuild.cmd
+++ msvc-full-features/prebuild.cmd
@@ -7,7 +7,6 @@ echo Done
 echo Generating "version.h"...
 for /F "tokens=*" %%i in ('git describe --tags --always --dirty --match "[0-9]*.*"') do set VERSION=%%i
 if "%VERSION%"=="" (
-set VERSION=Please install `git` to generate VERSION
-)
+set VERSION=Caterpillar Edition)
 echo VERSION defined as "%VERSION%"
 >..\src\version.h echo #define VERSION "%VERSION%"
