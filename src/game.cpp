bafbb9e231c600719c08e2274e00f156b22b52cc
Ilysen <ashe.f.higgs+git@gmail.com>
2019-04-14 15:13:37 -0400

    Debug menu easter egg has a 1% chance to appear

--- src/game.cpp
+++ src/game.cpp
@@ -2889,8 +2889,13 @@ void game::write_memorial_file( std::string sLastWords )
 
 void game::debug()
 {
+    std::string debugText = "Debug Functions - Using these is CHEATING!";
+    if( one_in( 100 ) ) {
+        debugText =
+            "Using these will cheat not only the game, but yourself.  You won't grow. You won't improve.  Taking this shortcut will gain you nothing. Your victory will be hollow.  Nothing will be risked and nothing will be gained.";
+    }
     int action = uilist(
-    _( "Debug Functions - Using these will cheat not only the game, but yourself.  You won't grow. You won't improve.  Taking this shortcut will gain you nothing. Your victory will be hollow.  Nothing will be risked and nothing will be gained." ), {
+    _( debugText ), {
         _( "Wish for an item" ),                // 0
         _( "Teleport - Short Range" ),          // 1
         _( "Teleport - Long Range" ),           // 2
