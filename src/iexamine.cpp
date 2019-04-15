3516c06fa9cc16b8f80ae09a6df38610321a6347
Fris0uman <sthapplz@gmail.com>
2019-04-15 15:48:58 +0200

    fix conflict

--- src/iexamine.cpp
+++ src/iexamine.cpp
@@ -4040,16 +4040,7 @@ void iexamine::autodoc( player &p, const tripoint &examp )
                 comps.push_back( item_comp( it->typeId(), 1 ) );
                 p.consume_items( comps, 1, is_crafting_component );
                 if( needs_anesthesia ) {
-<<<<<<< HEAD
-                    p.consume_items( acomps, 1 );
-<<<<<<< HEAD
-                    item empty_kit( "anesthesia_used" );
-                    g->m.add_item_or_charges( examp, empty_kit );
-=======
                     p.consume_items( acomps, 1, is_crafting_component );
->>>>>>> c00528a4ff1009debff84ecf182ec9fdcd5f5d57
-=======
->>>>>>> parent of 1bf69ac9a6... d
                 }
             }
             break;
