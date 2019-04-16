b58634ba68ac27e1b147296327f689d80dfd1e56
Kevin Granade <kevin.granade@gmail.com>
2019-04-15 17:19:10 -0700

    Fix a dumb copy paste error

--- build-scripts/lint-json.sh
+++ build-scripts/lint-json.sh
@@ -1,1 +1,1 @@
-find tmp -name *json -a -type f -print0 | xargs -0 -L 1 -P 10 python -m json.tool >/dev/null
+find . -name *json -a -type f -print0 | xargs -0 -L 1 -P 10 python -m json.tool >/dev/null
