2111c76488baef15717c5ad091c7c660eae39d3b
Kevin Granade <kevin.granade@gmail.com>
2019-04-18 20:02:03 -0700

    time some variants

--- build-scripts/lint-json.sh
+++ build-scripts/lint-json.sh
@@ -1,1 +1,6 @@
-find . -name *json -a -type f -print0 | xargs -0 -L 1 -P 10 python -m json.tool >/dev/null
+time find . -name *json -type f -exec python -m json.tool {} >/dev/null \;
+time find . -name *json -a -type f -print0 | xargs -0 -L 1 -P 2 python -m json.tool >/dev/null
+time find . -name *json -a -type f -print0 | xargs -0 -L 1 -P 4 python -m json.tool >/dev/null
+time find . -name *json -a -type f -print0 | xargs -0 -L 1 -P 8 python -m json.tool >/dev/null
+time find . -name *json -a -type f -print0 | xargs -0 -L 1 -P 12 python -m json.tool >/dev/null
+time find . -name *json -a -type f -print0 | xargs -0 -L 1 -P 16 python -m json.tool >/dev/null
