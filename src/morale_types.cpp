CleverRaven/Cataclysm-DDA

commit 3b10ab5c18f357e132e0fae6ca0eeae50b2f26a3

Gravedigging (#28803)
* body bag

* gravedigger morale

* grave in mapgen

* terrain - grave

* grave digging

* memorial log

* looks like for body bag

* spiritual allowed to dig at greater penalties

* spiced up info feedback

* make grave clothes filthy

src/morale_types.cpp
@@ -85,6 +85,7 @@ const morale_type &morale_type_data::convert_legacy( int lmt )
             morale_type( "morale_perm_filthy" ),
 
             morale_type( "morale_butcher" ),
+            morale_type( "moreale_gravedigger" ),
 
             morale_type( "morale_null" )
         }
@@ -166,6 +167,7 @@ const morale_type MORALE_PYROMANIA_NOFIRE( "morale_pyromania_nofire" );
 const morale_type MORALE_PERM_FILTHY( "morale_perm_filthy" );
 const morale_type MORALE_PERM_DEBUG( "morale_perm_debug" );
 const morale_type MORALE_BUTCHER( "morale_butcher" );
+const morale_type MORALE_GRAVEDIGGER( "morale_gravedigger" );
 
 namespace
 {

src/activity_handlers.cpp
@@ -2878,8 +2878,40 @@ void activity_handlers::dig_finish( player_activity *act, player *p )
     const std::string byproducts_item_group = act->str_values[0];
     const int byproducts_count = act->values[0];
     const tripoint dump_loc = act->coords[0];
+    const tripoint &pos = act->placement;
+    const bool grave = g->m.ter( pos ) == t_grave;
 
-    g->m.ter_set( act->placement, result_terrain );
+    if( grave ) {
+        if( one_in( 10 ) ) {
+            static const std::array<mtype_id, 5> monids = { {
+                    mtype_id( "mon_zombie" ), mtype_id( "mon_zombie_fat" ),
+                    mtype_id( "mon_zombie_rot" ), mtype_id( "mon_skeleton" ),
+                    mtype_id( "mon_zombie_crawler" )
+                }
+            };
+
+            g->summon_mon( random_entry( monids ), dump_loc );
+            g->m.furn_set( pos, f_coffin_o );
+            p->add_msg_if_player( m_warning, _( "Something crawls out of the coffin!" ) );
+        } else {
+            g->m.spawn_item( pos, "bone_human", rng( 5, 15 ) );
+            g->m.furn_set( pos, f_coffin_c );
+        }
+        std::vector<item *> dropped;
+        g->m.place_items( "grave", 25, pos, pos, false, calendar::turn );
+        g->m.place_items( "jewelry_front", 20, pos, pos, false, calendar::turn );
+        dropped = g->m.place_items( "allclothes", 50, pos, pos, false, calendar::turn );
+        for( const auto &it : dropped ) {
+            if( it->is_armor() ) {
+                it->item_tags.insert( "FILTHY" );
+                it->set_damage( rng( 1, it->max_damage() - 1 ) );
+            }
+        }
+        g->u.add_memorial_log( pgettext( "memorial_male", "Exhumed a grave." ),
+                               pgettext( "memorial_female", "Exhumed a grave." ) );
+    }
+
+    g->m.ter_set( pos, result_terrain );
 
     for( int i = 0; i < byproducts_count; i++ ) {
         g->m.spawn_items( dump_loc, item_group::items_from( byproducts_item_group, calendar::turn ) );
@@ -2890,8 +2922,12 @@ void activity_handlers::dig_finish( player_activity *act, player *p )
     p->mod_hunger( 5 - helpersize );
     p->mod_thirst( 5 - helpersize );
     p->mod_fatigue( 10 - ( helpersize * 2 ) );
-    p->add_msg_if_player( m_good, _( "You finish digging the %s." ),
-                          g->m.ter( act->placement ).obj().name() );
+    if( grave ) {
+        p->add_msg_if_player( m_good, _( "You finish exhuming a grave." ) );
+    } else {
+        p->add_msg_if_player( m_good, _( "You finish digging the %s." ),
+                              g->m.ter( act->placement ).obj().name() );
+    }
 
     act->set_to_null();
 }

src/iuse.cpp
@@ -2638,12 +2638,17 @@ int iuse::dig( player *p, item *it, bool t, const tripoint & )
             _( "You can't dig a pit in this location.  Ensure it is clear diggable ground with no items or obstacles." ) );
         return 0;
     }
-
     const bool can_deepen = g->m.has_flag( "DIGGABLE_CAN_DEEPEN", dig_point );
+    const bool grave = g->m.ter( dig_point ) == t_grave;
 
-    if( can_deepen && !p->crafting_inventory().has_quality( DIG, 2 ) ) {
-        p->add_msg_if_player( _( "You can't deepen this pit without a proper shovel." ) );
-        return 0;
+    if( !p->crafting_inventory().has_quality( DIG, 2 ) ) {
+        if( can_deepen ) {
+            p->add_msg_if_player( _( "You can't deepen this pit without a proper shovel." ) );
+            return 0;
+        } else if( grave ) {
+            p->add_msg_if_player( _( "You can't exhume a grave without a proper shovel." ) );
+            return 0;
+        }
     }
 
     const std::function<bool( tripoint )> f = []( tripoint p ) {
@@ -2663,14 +2668,35 @@ int iuse::dig( player *p, item *it, bool t, const tripoint & )
         return 0;
     }
 
+    if( grave ) {
+        if( g->u.has_trait_flag( "SPIRITUAL" ) && !g->u.has_trait_flag( "PSYCHOPATH" ) &&
+            g->u.query_yn( _( "Would you really touch the sacred resting place of the dead?" ) ) ) {
+            add_msg( m_info, _( "Exhuming a grave is really against your beliefs." ) );
+            g->u.add_morale( MORALE_GRAVEDIGGER, -50, -100, 48_hours, 12_hours );
+            if( one_in( 3 ) ) {
+                g->u.vomit();
+            }
+        } else if( g->u.has_trait_flag( "PSYCHOPATH" ) ) {
+            p->add_msg_if_player( m_good,
+                                  _( "Exhuming a grave is fun now, where there is no one to object." ) );
+            p->add_morale( MORALE_GRAVEDIGGER, 25, 50, 2_hours, 1_hours );
+        } else if( !g->u.has_trait_flag( "EATDEAD" ) && !g->u.has_trait_flag( "SAPROVORE" ) ) {
+            p->add_msg_if_player( m_bad, _( "Exhuming this grave is utterly disgusting!" ) );
+            p->add_morale( MORALE_GRAVEDIGGER, -25, -50, 2_hours, 1_hours );
+            if( one_in( 5 ) ) {
+                p->vomit();
+            }
+        }
+    }
+
     const std::vector<npc *> helpers = g->u.get_crafting_helpers();
     for( const npc *np : helpers ) {
         add_msg( m_info, _( "%s helps with this task..." ), np->name.c_str() );
         break;
     }
 
-    digging_moves_and_byproducts moves_and_byproducts = dig_pit_moves_and_byproducts( p, it, can_deepen,
-            false );
+    digging_moves_and_byproducts moves_and_byproducts = dig_pit_moves_and_byproducts( p, it,
+            can_deepen, false );
 
     player_activity act( activity_id( "ACT_DIG" ), moves_and_byproducts.moves, -1,
                          p->get_item_position( it ) );

src/mapdata.cpp
@@ -438,7 +438,7 @@ bool map_data_common_t::connects( int &ret ) const
 ter_id t_null,
        t_hole, // Real nothingness; makes you fall a z-level
        // Ground
-       t_dirt, t_sand, t_clay, t_dirtmound, t_pit_shallow, t_pit,
+       t_dirt, t_sand, t_clay, t_dirtmound, t_pit_shallow, t_pit, t_grave,
        t_pit_corpsed, t_pit_covered, t_pit_spiked, t_pit_spiked_covered, t_pit_glass, t_pit_glass_covered,
        t_rock_floor,
        t_grass, t_grass_long, t_grass_tall, t_grass_golf, t_grass_dead, t_grass_white,
@@ -561,6 +561,7 @@ void set_ter_ids()
     t_sand = ter_id( "t_sand" );
     t_clay = ter_id( "t_clay" );
     t_dirtmound = ter_id( "t_dirtmound" );
+    t_grave = ter_id( "t_grave" );
     t_pit_shallow = ter_id( "t_pit_shallow" );
     t_pit = ter_id( "t_pit" );
     t_pit_corpsed = ter_id( "t_pit_corpsed" );

src/mapdata.h
@@ -357,7 +357,7 @@ t_basalt
 extern ter_id t_null,
        t_hole, // Real nothingness; makes you fall a z-level
        // Ground
-       t_dirt, t_sand, t_clay, t_dirtmound, t_pit_shallow, t_pit,
+       t_dirt, t_sand, t_clay, t_dirtmound, t_pit_shallow, t_pit, t_grave,
        t_pit_corpsed, t_pit_covered, t_pit_spiked, t_pit_spiked_covered, t_pit_glass, t_pit_glass_covered,
        t_rock_floor,
        t_grass, t_grass_long, t_grass_tall, t_grass_golf, t_grass_dead, t_grass_white,

src/morale_types.h
@@ -107,5 +107,6 @@ extern const morale_type MORALE_PYROMANIA_NOFIRE;
 extern const morale_type MORALE_PERM_FILTHY;
 extern const morale_type MORALE_PERM_DEBUG;
 extern const morale_type MORALE_BUTCHER;
+extern const morale_type MORALE_GRAVEDIGGER;
 
 #endif

src/mutation.cpp
@@ -11,6 +11,7 @@
 #include "map_iterator.h"
 #include "mapdata.h"
 #include "monster.h"
+#include "morale_types.h"
 #include "output.h"
 #include "player.h"
 #include "translations.h"
@@ -359,6 +360,26 @@ void player::activate_mutation( const trait_id &mut )
                         moves = MINUTES( 30 ) * 100;
                     } else if( g->m.has_flag( "DIGGABLE", pnt ) ) {
                         moves = MINUTES( 10 ) * 100;
+                        if( g->m.ter( pnt ) == t_grave ) {
+                            moves *= 2; // 6 feet under
+                            if( g->u.has_trait_flag( "SPIRITUAL" ) && !g->u.has_trait_flag( "PSYCHOPATH" ) &&
+                                g->u.query_yn( _( "Would you really touch the sacred resting place of the dead?" ) ) ) {
+                                add_msg( m_info, _( "You are really going against your beliefs here." ) );
+                                g->u.add_morale( MORALE_GRAVEDIGGER, -50, -100, 48_hours, 12_hours );
+                                if( one_in( 3 ) ) {
+                                    g->u.vomit();
+                                }
+                            } else if( g->u.has_trait_flag( "PSYCHOPATH" ) ) {
+                                g->u.add_msg_if_player( m_good, _( "This is fun!" ) );
+                                g->u.add_morale( MORALE_GRAVEDIGGER, 25, 50, 2_hours, 1_hours );
+                            } else if( !g->u.has_trait_flag( "EATDEAD" ) && !g->u.has_trait_flag( "SAPROVORE" ) ) {
+                                g->u.add_msg_if_player( m_bad, _( "This is utterly disgusting!" ) );
+                                g->u.add_morale( MORALE_GRAVEDIGGER, -25, -50, 2_hours, 1_hours );
+                                if( one_in( 5 ) ) {
+                                    g->u.vomit();
+                                }
+                            }
+                        }
                     } else {
                         add_msg_if_player( _( "You can't dig a pit on this ground." ) );
                         return;

commit 8b375c37b6eecc0185f5594fb916cd611c110b80

fixed gravedigger morale typo
introduced by #28803

src/morale_types.cpp
@@ -95,7 +95,7 @@ const morale_type &morale_type_data::convert_legacy( int lmt )
             morale_type( "morale_perm_filthy" ),
 
             morale_type( "morale_butcher" ),
-            morale_type( "moreale_gravedigger" ),
+            morale_type( "morale_gravedigger" ),
 
             morale_type( "morale_null" )
         }