a8b72f9e634f7fdff6c0662e52f77a49aed715a8
Fris0uman <sthapplz@gmail.com>
2019-04-26 20:23:20 +0200

    Revert "don't unset destination after reaching couch"
    This reverts commit 7cd081276befc781d3d66b1efd35a486dab7be1d.

--- src/monmove.cpp
+++ src/monmove.cpp
@@ -396,6 +396,7 @@ void monster::plan( const mfactions &factions )
                     if( rl_dist( pos(), couch_pos ) == 1 && !has_effect( effect_operating ) ) {
                         if( g->u.has_effect( effect_grabbed ) ) {
                             g->u.setpos( couch_pos );
+                            unset_dest();
                             add_msg( m_bad, _( "The %s slowy but firmly puts you down onto the autodoc couch." ), name() );
                             if( !has_effect( effect_countdown ) ) {
                                 add_effect( effect_countdown, 3_turns );
