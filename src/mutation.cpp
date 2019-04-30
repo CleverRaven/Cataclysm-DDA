28282fc41f8782266ce45dc5a599b9c74e155ffa
Kevin Granade <kevin.granade@gmail.com>
2019-04-30 04:01:38 +0000

    Astyle

--- src/mutation.cpp
+++ src/mutation.cpp
@@ -537,22 +537,23 @@ void player::activate_mutation( const trait_id &mut )
             add_msg_if_player( m_info, _( "You can only do that next to a tree." ) );
             return;
         }
-        
+
         if( has_trait( trait_ROOTS2 ) || has_trait( trait_ROOTS3 ) ) {
             add_msg_if_player( _( "You reach out to the trees with your roots." ) );
         } else {
-            add_msg_if_player( _( "You lay next to the trees letting your hair roots tangle with the trees." ) );
+            add_msg_if_player(
+                _( "You lay next to the trees letting your hair roots tangle with the trees." ) );
         }
 
         assign_activity( activity_id( "ACT_TREE_COMMUNION" ) );
 
         if( has_trait( trait_ROOTS2 ) || has_trait( trait_ROOTS3 ) ) {
             const time_duration startup_time = has_trait( trait_ROOTS3 ) ? rng( 15_minutes,
-                                           30_minutes ) : rng( 60_minutes, 90_minutes );
+                                               30_minutes ) : rng( 60_minutes, 90_minutes );
             activity.values.push_back( to_turns<int>( startup_time ) );
             return;
         } else {
-            const time_duration startup_time = rng ( 120_minutes, 180_minutes);
+            const time_duration startup_time = rng( 120_minutes, 180_minutes );
             activity.values.push_back( to_turns<int>( startup_time ) );
             return;
         }
