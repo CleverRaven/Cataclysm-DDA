a848e6d63bdc47094cb243072b9c276365a52d3d
Hirmuolio <hirmuolio@gmail.com>
2019-05-05 15:03:05 +0300

    fix test

--- tests/temperature_test.cpp
+++ tests/temperature_test.cpp
@@ -29,7 +29,7 @@ TEST_CASE( "Item spawns with right thermal attributes" )
     CHECK( D.temperature == 0 );
     CHECK( D.specific_energy == -10 );
 
-    D.process_temperature_rot( 122, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+    D.process_temperature_rot( 122, 1, tripoint( 0, 0, 0 ), nullptr );
 
     CHECK( is_nearly( D.temperature, 323.15 * 100000 ) );
 }
@@ -64,26 +64,26 @@ TEST_CASE( "Rate of temperature change" )
         item water1( "water" );
         item water2( "water" );
 
-        water1.process_temperature_rot( 131, 1, tripoint( 0, 0, 0 ), nullptr, "" );
-        water2.process_temperature_rot( 131, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        water1.process_temperature_rot( 131, 1, tripoint( 0, 0, 0 ), nullptr );
+        water2.process_temperature_rot( 131, 1, tripoint( 0, 0, 0 ), nullptr );
 
         // 55 C
         CHECK( is_nearly( water1.temperature, 328.15 * 100000 ) );
 
 
         calendar::turn = to_turn<int>( calendar::turn + 11_minutes );
-        water1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        water1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
 
         calendar::turn = to_turn<int>( calendar::turn + 20_minutes );
-        water1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        water1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
 
         calendar::turn = to_turn<int>( calendar::turn + 29_minutes );
-        water1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
-        water2.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        water1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
+        water2.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
 
         calendar::turn = to_turn<int>( calendar::turn + 15_minutes );
-        water1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
-        water2.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        water1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
+        water2.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
 
         // 29.4 C
         CHECK( is_nearly( water1.temperature, 30259330 ) );
@@ -105,28 +105,28 @@ TEST_CASE( "Rate of temperature change" )
         item meat1( "meat_cooked" );
         item meat2( "meat_cooked" );
 
-        meat1.process_temperature_rot( 122, 1, tripoint( 0, 0, 0 ), nullptr, "" );
-        meat2.process_temperature_rot( 122, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( 122, 1, tripoint( 0, 0, 0 ), nullptr );
+        meat2.process_temperature_rot( 122, 1, tripoint( 0, 0, 0 ), nullptr );
 
         // 50 C
         CHECK( is_nearly( meat1.temperature, 323.15 * 100000 ) );
         CHECK( meat1.item_tags.count( "HOT" ) );
 
         calendar::turn = to_turn<int>( calendar::turn + 11_minutes );
-        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
 
         // 33.5 C
         CHECK( is_nearly( meat1.temperature, 30673432 ) );
         CHECK( !meat1.item_tags.count( "HOT" ) );
 
         calendar::turn = to_turn<int>( calendar::turn + 11_minutes );
-        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
         calendar::turn = to_turn<int>( calendar::turn + 11_minutes );
-        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
 
         calendar::turn = to_turn<int>( calendar::turn + 20_minutes );
-        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
-        meat2.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
+        meat2.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
         // 0C
         // not frozen
         CHECK( is_nearly( meat1.temperature, 27315000 ) );
@@ -135,11 +135,11 @@ TEST_CASE( "Rate of temperature change" )
         CHECK( !meat2.item_tags.count( "FROZEN" ) );
 
         calendar::turn = to_turn<int>( calendar::turn + 45_minutes );
-        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
-        meat2.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
+        meat2.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
         calendar::turn = to_turn<int>( calendar::turn + 45_minutes );
-        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
-        meat2.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
+        meat2.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
 
         // 0C
         // frozen
@@ -150,13 +150,13 @@ TEST_CASE( "Rate of temperature change" )
         CHECK( is_nearly( meat1.specific_energy, meat2.specific_energy ) );
 
         calendar::turn = to_turn<int>( calendar::turn + 11_minutes );
-        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
         calendar::turn = to_turn<int>( calendar::turn + 20_minutes );
-        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
 
         calendar::turn = to_turn<int>( calendar::turn + 20_minutes );
-        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
-        meat2.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
+        meat2.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
 
         // -7.2 C
         // frozen
@@ -182,8 +182,8 @@ TEST_CASE( "Rate of temperature change" )
         item meat1( "meat_cooked" );
         item meat2( "meat_cooked" );
 
-        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
-        meat2.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
+        meat2.process_temperature_rot( -4, 1, tripoint( 0, 0, 0 ), nullptr );
 
         // -20 C
         CHECK( is_nearly( meat1.temperature, 253.15 * 100000 ) );
@@ -191,19 +191,19 @@ TEST_CASE( "Rate of temperature change" )
 
 
         calendar::turn = to_turn<int>( calendar::turn + 11_minutes );
-        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
         // -5.2 C
         CHECK( is_nearly( meat1.temperature, 26789608 ) );
 
         calendar::turn = to_turn<int>( calendar::turn + 11_minutes );
-        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
 
         calendar::turn = to_turn<int>( calendar::turn + 11_minutes );
-        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
 
         calendar::turn = to_turn<int>( calendar::turn + 20_minutes );
-        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
-        meat2.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
+        meat2.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
 
         // 0C
         // same temp
@@ -214,12 +214,12 @@ TEST_CASE( "Rate of temperature change" )
         CHECK( meat2.item_tags.count( "FROZEN" ) );
 
         calendar::turn = to_turn<int>( calendar::turn + 45_minutes );
-        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
-        meat2.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
+        meat2.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
 
         calendar::turn = to_turn<int>( calendar::turn + 45_minutes );
-        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
-        meat2.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
+        meat2.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
 
         // 0C
         // same tmep
@@ -229,13 +229,13 @@ TEST_CASE( "Rate of temperature change" )
         CHECK( !meat1.item_tags.count( "FROZEN" ) );
 
         calendar::turn = to_turn<int>( calendar::turn + 11_minutes );
-        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
         calendar::turn = to_turn<int>( calendar::turn + 20_minutes );
-        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
 
         calendar::turn = to_turn<int>( calendar::turn + 20_minutes );
-        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
-        meat2.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr, "" );
+        meat1.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
+        meat2.process_temperature_rot( 68, 1, tripoint( 0, 0, 0 ), nullptr );
         // 13.3 C
         // same temp
         CHECK( is_nearly( meat1.temperature, 28654986 ) );
