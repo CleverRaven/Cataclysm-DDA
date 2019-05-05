533bd5366694166e2a7496db63078fcc6540b3e1
Hirmuolio <hirmuolio@gmail.com>
2019-05-05 22:23:05 +0300

    updated comment

--- src/item.h
+++ src/item.h
@@ -690,7 +690,7 @@ class item : public visitable<item>
          * @param insulation Amount of insulation item has from surroundings
          * @param pos The current position
          * @param carrier The current carrier
-         * @param flag is used to tell that the item is in a temperature controlled place. "heater", "fridge", "freezer", "root_cellar"
+         * @param flag to specify special temperature situations
          */
         void process_temperature_rot( int temp, float insulation, const tripoint pos, player *carrier,
                                       const temperature_flag flag = temperature_flag::TEMP_NORMAL );
