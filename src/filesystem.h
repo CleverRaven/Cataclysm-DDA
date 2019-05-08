9ca5591f0628301a57a78e9157d06fb7ff341bdc
neitsa <neitsa@gmail.com>
2019-05-08 03:42:43 +0200

    Change ensure_valid_file_name() with actually used parameter.

--- src/filesystem.h
+++ src/filesystem.h
@@ -53,14 +53,11 @@ std::vector<std::string> get_directories_with( const std::string &pattern,
 bool copy_file( const std::string &source_path, const std::string &dest_path );
 
 /**
- *  Replace invalid characters in a string with a replacement character; can be used to ensure that a file name is compliant with most file systems.
+ *  Replace invalid characters in a string with a default character; can be used to ensure that a file name is compliant with most file systems.
  *  @param file_name Name of the file to check.
- *  @param change_made On function return, indicates if any change was made.
- *  @param invalid_chars A string indicating which characters are deemed invalid.
- *  @param replacement_char A single character used to replace invalid characters.
  *  @return A string with all invalid characters replaced with the replacement character, if any change was made.
+ *  @note  The default replacement character is space (0x20) and the invalid characters are "\\/:?\"<>|".
  */
-std::string ensure_valid_file_name( const std::string &file_name, bool *change_made = nullptr,
-                                    const std::string &invalid_chars = "\\/:?\"<>|", char replacement_char = ' ' );
+std::string ensure_valid_file_name( const std::string &file_name );
 
 #endif //CATA_FILE_SYSTEM_H
