eda200034ae2de8e413abc03346efba5ef756a89
Mark Langsdorf <mark.langsdorf@gmail.com>
2019-05-08 10:35:28 +0200

    Update src/player.h
    Co-Authored-By: Fris0uman <41293484+Fris0uman@users.noreply.github.com>

--- src/player.h
+++ src/player.h
@@ -264,7 +264,7 @@ class player : public Character
         /**Estimate effect duration based on player relevant skill*/
         time_duration estimate_effect_dur( const skill_id &relevant_skill, const efftype_id &effect,
                                            const time_duration &error_magnitude,
-                                           int threshold, Creature *target );
+                                           int threshold, const Creature &target ) const;
 
         /** Resets stats, and applies effects in an idempotent manner */
         void reset_stats() override;
