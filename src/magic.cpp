d0ab6aaabdea551149d4707d7db84fe0f473c528
KorGgenT <curtis.r.merrill@gmail.com>
2019-05-10 00:36:43 -0400

    most spell function definitions

--- src/magic.cpp
+++ src/magic.cpp
@@ -195,3 +195,350 @@ bool spell_type::is_valid() const
 {
     return spell_factory.is_valid( this->id );
 }
+
+// spell
+
+spell::spell( const spell_type *sp, int xp )
+{
+    type = sp;
+    experience = xp;
+}
+
+spell::spell( spell_id sp, int xp ) : spell( &sp.obj(), xp ) {}
+
+spell_id spell::id() const
+{
+    return type->id;
+}
+
+int spell::damage() const
+{
+    if( type->min_damage >= 0 ) {
+        return std::min( static_cast<int>( type->min_damage + round( get_level() *
+                                           type->damage_increment ) ), type->max_damage );
+    } else { // if it's negative, min and max work differently
+        return std::max( static_cast<int>( type->min_damage + round( get_level() *
+                                           type->damage_increment ) ), type->max_damage );
+    }
+}
+
+int spell::aoe() const
+{
+    return std::min( static_cast<int>( type->min_aoe + round( get_level() * type->aoe_increment ) ),
+                     type->max_aoe );
+}
+
+int spell::range() const
+{
+    return std::min( static_cast<int>( type->min_range + round( get_level() * type->range_increment ) ),
+                     type->max_range );
+}
+
+int spell::duration() const
+{
+    return std::min( static_cast<int>( type->min_duration + round( get_level() *
+                                       type->duration_increment ) ),
+                     type->max_duration );
+}
+
+time_duration spell::duration_turns() const
+{
+    return 1_turns * duration() / 100;
+}
+
+void spell::gain_level()
+{
+    gain_exp( exp_to_next_level() );
+}
+
+bool spell::is_max_level() const
+{
+    return get_level() >= type->max_level;
+}
+
+bool spell::can_learn( const player &p ) const
+{
+    if( type->spell_class == trait_id( "NONE" ) ) {
+        return true;
+    }
+    return p.has_trait( type->spell_class );
+}
+
+int spell::energy_cost() const
+{
+    // todo: formula to vary energy cost
+    return type->base_energy_cost;
+}
+
+int spell::get_difficulty() const
+{
+    return type->difficulty;
+}
+
+int spell::casting_time() const
+{
+    // todo: formula for casting time
+    return type->base_casting_time;
+}
+
+std::string spell::name() const
+{
+    return _( type->name );
+}
+
+float spell::spell_fail( const player &p ) const
+{
+    // formula is based on the following:
+    // exponential curve
+    // effective skill of 0 or less is 100% failure
+    // effective skill of 8 (8 int, 0 spellcraft, 0 spell level, spell difficulty 0) is ~50% failure
+    // effective skill of 30 is 0% failure
+    const float effective_skill = 2 * ( get_level() - get_difficulty() ) + p.get_int() +
+                                  p.get_skill_level( skill_id( "SPELLCRAFT" ) );
+    // add an if statement in here because sufficiently large numbers will definitely overflow because of exponents
+    if( effective_skill > 30.0f ) {
+        return 0.0f;
+    } else if( effective_skill < 0.0f ) {
+        return 1.0f;
+    }
+    const float fail_chance = pow( ( effective_skill - 30.0f ) / 30.0f, 2 );
+    return clamp( fail_chance, 0.0f, 1.0f );
+}
+
+std::string spell::colorized_fail_percent( const player &p ) const
+{
+    const float fail_fl = spell_fail( p ) * 100.0f;
+    std::string fail_str;
+    fail_fl == 100.0f ? fail_str = _( "Difficult!" ) : fail_str = _( string_format( "%.1f %% %s",
+                                   fail_fl, "Failure Chance" ) );
+    nc_color color;
+    if( fail_fl > 90.0f ) {
+        color = c_magenta;
+    } else if( fail_fl > 75.0f ) {
+        color = c_red;
+    } else if( fail_fl > 60.0f ) {
+        color = c_light_red;
+    } else if( fail_fl > 35.0f ) {
+        color = c_yellow;
+    } else if( fail_fl > 15.0f ) {
+        color = c_green;
+    } else {
+        color = c_light_green;
+    }
+    return colorize( fail_str, color );
+}
+
+int spell::xp() const
+{
+    return experience;
+}
+
+void spell::gain_exp( int nxp )
+{
+    experience += nxp;
+}
+
+std::string spell::energy_string() const
+{
+    switch( type->energy_source ) {
+        case hp_energy:
+            return _( "health" );
+        case mana_energy:
+            return _( "mana" );
+        case stamina_energy:
+            return _( "stamina" );
+        case bionic_energy:
+            return _( "bionic power" );
+        default:
+            return "";
+    }
+}
+
+std::string spell::energy_cost_string( const player &p ) const
+{
+    if( energy_source() == none_energy ) {
+        return _( "none" );
+    }
+    if( energy_source() == bionic_energy || energy_source() == mana_energy ) {
+        return colorize( to_string( energy_cost() ), c_light_blue );
+    }
+    if( energy_source() == hp_energy ) {
+        auto pair = get_hp_bar( energy_cost(), p.get_hp_max() / num_hp_parts );
+        return colorize( pair.first, pair.second );
+    }
+    if( energy_source() == stamina_energy ) {
+        auto pair = get_hp_bar( energy_cost(), p.get_stamina_max() );
+        return colorize( pair.first, pair.second );
+    }
+    debugmsg( _( "ERROR: Spell %s has invalid energy source." ), id().c_str() );
+    return _( "error: energy_type" );
+}
+
+bool spell::is_valid() const
+{
+    if( type == nullptr ) {
+        return false;
+    }
+    return type->is_valid();
+}
+
+std::string spell::effect() const
+{
+    return type->effect;
+}
+
+energy_type spell::energy_source() const
+{
+    return type->energy_source;
+}
+
+bool spell::is_valid_target( valid_target t ) const
+{
+    return type->valid_targets[t];
+}
+
+bool spell::is_valid_target( const tripoint &p ) const
+{
+    bool valid = false;
+    if( Creature *const cr = g->critter_at<Creature>( p ) ) {
+        Creature::Attitude cr_att = cr->attitude_to( g->u );
+        valid = valid || ( cr_att != Creature::A_FRIENDLY && is_valid_target( target_hostile ) ) ||
+                ( cr_att == Creature::A_FRIENDLY && is_valid_target( target_ally ) );
+    }
+    if( p == g->u.pos() ) {
+        valid = valid || is_valid_target( target_self );
+    }
+    return valid || is_valid_target( target_ground );
+}
+
+std::string spell::description() const
+{
+    return _( type->description );
+}
+
+nc_color spell::damage_type_color() const
+{
+    switch( dmg_type() ) {
+        case DT_HEAT:
+            return c_red;
+        case DT_ACID:
+            return c_light_green;
+        case DT_BASH:
+            return c_magenta;
+        case DT_BIOLOGICAL:
+            return c_green;
+        case DT_COLD:
+            return c_white;
+        case DT_CUT:
+            return c_light_gray;
+        case DT_ELECTRIC:
+            return c_light_blue;
+        case DT_STAB:
+            return c_light_red;
+        case DT_TRUE:
+            return c_dark_gray;
+        default:
+            return c_black;
+    }
+}
+
+// constants defined below are just for the formula to be used,
+// in order for the inverse formula to be equivalent
+static const float a = 6200.0;
+static const float b = 0.146661;
+static const float c = -62.5;
+
+int spell::get_level() const
+{
+    // you aren't at the next level unless you have the requisite xp, so floor
+    return std::max( static_cast<int>( floor( log( experience + a ) / b + c ) ), 0 );
+}
+
+int spell::get_max_level() const
+{
+    return type->max_level;
+}
+
+// helper function to calculate xp needed to be at a certain level
+// pulled out as a helper function to make it easier to either be used in the future
+// or easier to tweak the formula
+int exp_for_level( int level )
+{
+    // level 0 never needs xp
+    if( level == 0 ) {
+        return 0;
+    }
+    return ceil( exp( ( level - c ) * b ) ) - a;
+}
+
+int spell::exp_to_next_level() const
+{
+    return exp_for_level( get_level() + 1 ) - xp();
+}
+
+std::string spell::exp_progress() const
+{
+    const int level = get_level();
+    const int this_level_xp = exp_for_level( level );
+    const int next_level_xp = exp_for_level( level + 1 );
+    const int denominator = next_level_xp - this_level_xp;
+    const float progress = static_cast<float>( xp() - this_level_xp ) / std::max( 1.0f,
+                           static_cast<float>( denominator ) );
+    return string_format( "%i%%", clamp( static_cast<int>( round( progress * 100 ) ), 0, 99 ) );
+}
+
+float spell::exp_modifier( const player &p ) const
+{
+    const float int_modifier = ( p.get_int() - 8.0f ) / 8.0f;
+    const float difficulty_modifier = get_difficulty() / 20.0f;
+    const float spellcraft_modifier = p.get_skill_level( skill_id( "SPELLCRAFT" ) ) / 10.0f;
+
+    return int_modifier + difficulty_modifier + spellcraft_modifier + 1.0f;
+}
+
+int spell::casting_exp( const player &p ) const
+{
+    // the amount of xp you would get with no modifiers
+    const int base_casting_xp = 75;
+
+    return round( p.adjust_for_focus( base_casting_xp * exp_modifier( p ) ) );
+}
+
+damage_type spell::dmg_type() const
+{
+    return type->dmg_type;
+}
+
+damage_instance spell::get_damage_instance() const
+{
+    damage_instance dmg;
+    dmg.add_damage( dmg_type(), damage() );
+    return dmg;
+}
+
+dealt_damage_instance spell::get_dealt_damage_instance() const
+{
+    dealt_damage_instance dmg;
+    dmg.set_damage( dmg_type(), damage() );
+    add_msg( "%i", damage() );
+    return dmg;
+}
+
+std::string spell::effect_data() const
+{
+    return type->effect_str;
+}
+
+int spell::heal( const tripoint &target ) const
+{
+    monster *const mon = g->critter_at<monster>( target );
+    if( mon ) {
+        return mon->heal( -damage() );
+    }
+    player *const p = g->critter_at<player>( target );
+    if( p ) {
+        p->healall( -damage() );
+        return -damage();
+    }
+    return -1;
+}
