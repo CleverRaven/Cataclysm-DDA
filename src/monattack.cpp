af19c3e6b1c4e99b44ebbd7f386881b2ef8579a6
Joseph Carrington <joseph.carrington@gmail.com>
2019-05-12 13:50:03 -0600

    Remove unnecessary var

--- src/monattack.cpp
+++ src/monattack.cpp
@@ -3831,10 +3831,9 @@ bool mattack::absorb_meat( monster *z )
                     g->m.use_charges( p, 0, current_item.type->get_id(), meat_absorbed );
                 } else {
                     //Only absorb one meaty item
-                    long meat_absorbed = 1;
                     const int hp_to_heal = meat_absorbed * ml_per_charge * meat_absorption_factor;
                     z->heal( hp_to_heal, true );
-                    g->m.use_amount( p, 0, current_item.type->get_id(), meat_absorbed );
+                    g->m.use_amount( p, 0, current_item.type->get_id(), 1 );
                 }
                 if( g->u.sees( *z ) ) {
                     add_msg( m_warning, _( "The %1$s absorbs the %2$s, growing larger." ), z->name(),
