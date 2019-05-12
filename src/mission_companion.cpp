2ada1499c489a3380af6a7392f26b3bffa9708b8
Mark Langsdorf <mark.langsdorf@gmail.com>
2019-05-12 17:20:22 -0500

    companion missions: increase the size of the window
    use the same algorithm to size the mission companion window that is used
    to size the dialogue window.

--- src/mission_companion.cpp
+++ src/mission_companion.cpp
@@ -376,12 +376,13 @@ bool talk_function::display_and_choose_opts( mission_data &mission_key, const tr
 
     camp_tab_mode tab_mode = TAB_MAIN;
 
-    size_t part_y = ( TERMY > FULL_SCREEN_HEIGHT ) ? ( TERMY - FULL_SCREEN_HEIGHT ) / 2 : 0;
-    size_t part_x = ( TERMX > FULL_SCREEN_WIDTH ) ? ( TERMX - FULL_SCREEN_WIDTH ) / 2 : 0;
-    catacurses::window w_list = catacurses::newwin( FULL_SCREEN_HEIGHT, FULL_SCREEN_WIDTH,
-                                part_y + TITLE_TAB_HEIGHT, part_x );
-    catacurses::window w_tabs = catacurses::newwin( TITLE_TAB_HEIGHT, FULL_SCREEN_WIDTH,
-                                part_y, part_x );
+    size_t part_y = ( TERMY > FULL_SCREEN_HEIGHT ) ? ( TERMY - FULL_SCREEN_HEIGHT ) / 4 : 0;
+    size_t part_x = ( TERMX > FULL_SCREEN_WIDTH ) ? ( TERMX - FULL_SCREEN_WIDTH ) / 4 : 0;
+    size_t maxy = part_y ? TERMY - 2 * part_y : FULL_SCREEN_HEIGHT;
+    size_t maxx = part_x ? TERMX - 2 * part_x : FULL_SCREEN_WIDTH;
+
+    catacurses::window w_list = catacurses::newwin( maxy, maxx, part_y + TITLE_TAB_HEIGHT, part_x );
+    catacurses::window w_tabs = catacurses::newwin( TITLE_TAB_HEIGHT, maxx, part_y, part_x );
 
     size_t sel = 0;
     int offset = 0;
@@ -389,8 +390,8 @@ bool talk_function::display_and_choose_opts( mission_data &mission_key, const tr
 
     // The following are for managing the right pane scrollbar.
     size_t info_offset = 0;
-    size_t info_height = FULL_SCREEN_HEIGHT - 3;
-    size_t info_width = FULL_SCREEN_WIDTH - 1 - MAX_FAC_NAME_SIZE;
+    size_t info_height = maxy - 3;
+    size_t info_width = maxx - 1 - MAX_FAC_NAME_SIZE;
     size_t end_line = 0;
     nc_color col = c_white;
     std::vector<std::string> mission_text;
@@ -440,10 +441,9 @@ bool talk_function::display_and_choose_opts( mission_data &mission_key, const tr
             mvwprintz( w_list, 1, 1, c_white, name_mission_tabs( omt_pos, role_id, title,
                        tab_mode ) );
 
-            calcStartPos( offset, sel, FULL_SCREEN_HEIGHT - 3, cur_key_list.size() );
+            calcStartPos( offset, sel, info_height, cur_key_list.size() );
 
-            for( size_t i = 0; static_cast<int>( i ) < FULL_SCREEN_HEIGHT - 3 &&
-                 ( i + offset ) < cur_key_list.size(); i++ ) {
+            for( size_t i = 0; i < info_height && ( i + offset ) < cur_key_list.size(); i++ ) {
                 size_t  current = i + offset;
                 nc_color col = ( current == sel ? h_white : c_white );
                 //highlight important missions
@@ -461,7 +461,7 @@ bool talk_function::display_and_choose_opts( mission_data &mission_key, const tr
                 mvwprintz( w_list, i + 2, 1, col, "  %s", cur_key_list[current].name_display );
             }
 
-            draw_scrollbar( w_list, sel, FULL_SCREEN_HEIGHT - 2, cur_key_list.size(), 1 );
+            draw_scrollbar( w_list, sel, info_height + 1, cur_key_list.size(), 1 );
             wrefresh( w_list );
             werase( w_info );
 
@@ -486,7 +486,8 @@ bool talk_function::display_and_choose_opts( mission_data &mission_key, const tr
 
             // Display the current subset of the mission text.
             for( size_t start_line = 0; start_line < end_line; start_line++ ) {
-                print_colored_text( w_info, start_line, 0, col, col, mission_text[start_line + info_offset] );
+                print_colored_text( w_info, start_line, 0, col, col,
+                                    mission_text[start_line + info_offset] );
             }
 
             wrefresh( w_info );
