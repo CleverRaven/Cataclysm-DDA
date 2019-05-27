75185274d9599f562cc16864ef4fd6462a95a149
Fris0uman <sthapplz@gmail.com>
2019-05-27 20:10:51 +0200

    Cardreader foodplace

--- src/iexamine.cpp
+++ src/iexamine.cpp
@@ -805,36 +805,49 @@ void iexamine::cardreader_robofac( player &p, const tripoint &examp )
     }
 }
 
-void iexamine::cardreader_corp( player &p, const tripoint &examp )
+void iexamine::cardreader_foodplace( player &p, const tripoint &examp )
 {
     bool open = false;
-    itype_id card_type = "foodperson_mask";
-    if( p.is_wearing( card_type ) && query_yn( _( "Press mask on the reader?" ) ) ) {
+    if( ( p.is_wearing( itype_id( "foodperson_mask" ) ) ||
+          p.is_wearing( itype_id( "foodperson_mask_on" ) ) ) &&
+        query_yn( _( "Press mask on the reader?" ) ) ) {
         p.mod_moves( -100 );
         for( const tripoint &tmp : g->m.points_in_radius( examp, 3 ) ) {
             if( g->m.ter( tmp ) == t_door_metal_locked ) {
-                g->m.ter_set( tmp, t_floor );
+                g->m.ter_set( tmp, t_door_metal_c );
                 open = true;
-            }
-        }
-        for( monster &critter : g->all_monsters() ) {
-            // Check 1) same overmap coords, 2) turret, 3) hostile
-            if( ms_to_omt_copy( g->m.getabs( critter.pos() ) ) == ms_to_omt_copy( g->m.getabs( examp ) ) &&
-                ( critter.type->id == mon_turret ||
-                  critter.type->id == mon_turret_rifle ) &&
-                critter.attitude_to( p ) == Creature::Attitude::A_HOSTILE ) {
-                g->remove_zombie( critter );
             }
         }
         if( open ) {
             add_msg( _( "You press your face on the reader." ) );
-            add_msg( m_good, _( "The nearby doors slide into the floor." ) );
+            add_msg( m_good, _( "The nearby doors are unlocked." ) );
             sounds::sound( examp, 6, sounds::sound_t::speech, "\"Hello Foodperson.  Welcome Home.\"", true,
                            "speech", "welcome" );
         } else {
-            add_msg( _( "The nearby doors are already opened." ) );
+            add_msg( _( "The nearby doors are already unlocked." ) );
+            if( query_yn( _( "Lock doors?" ) ) ) {
+                for( const tripoint &tmp : g->m.points_in_radius( examp, 3 ) ) {
+                    if( g->m.ter( tmp ) == t_door_metal_o || g->m.ter( tmp ) == t_door_metal_c ) {
+                        if( p.pos() == tmp ) {
+                            p.add_msg_if_player( m_bad, _( "You are in the way of the door, move before trying again." ) );
+                        } else {
+                            g->m.ter_set( tmp, t_door_metal_locked );
+                            open = false;
+                        }
+
+                    }
+                }
+            }
         }
+    } else if( p.has_amount( itype_id( "foodperson_mask" ), 1 ) ||
+               p.has_amount( itype_id( "foodperson_mask_on" ), 1 ) ) {
+        sounds::sound( examp, 6, sounds::sound_t::speech,
+                       "\"FOODPERSON DETECTED.  Please make yourself presentable.\"", true,
+                       "speech", "welcome" );
     } else {
+        sounds::sound( examp, 6, sounds::sound_t::speech,
+                       "\"Your face is inadequate.  Please go away.\"", true,
+                       "speech", "welcome" );
         p.assign_activity( activity_id( "ACT_HACK_DOOR" ), to_moves<int>( 5_minutes ) );
         p.activity.placement = examp;
     }
@@ -5280,6 +5293,7 @@ iexamine_function iexamine_function_from_string( const std::string &function_nam
             { "controls_gate", &iexamine::controls_gate },
             { "cardreader", &iexamine::cardreader },
             { "cardreader_robofac", &iexamine::cardreader_robofac },
+            { "cardreader_fp", &iexamine::cardreader_foodplace },
             { "intercom", &iexamine::intercom },
             { "rubble", &iexamine::rubble },
             { "chainfence", &iexamine::chainfence },
