48504491ab97eeb58716698eb80c706de5d66110
David Brown <davidpwbrown@gmail.com>
2019-06-06 17:43:52 +0100

    sanity check in deserialize

--- src/savegame_json.cpp
+++ src/savegame_json.cpp
@@ -3103,6 +3103,16 @@ void basecamp::deserialize( JsonIn &jsin )
                 e.provides[ id ] = amount;
             }
         }
+        // incase of save corruption, sanity check provides from expansions
+        if( e.type == "kitchen" ){
+            e.provides[ "faction_base_kitchen_0" ] = 1;
+        } else if( e.type == "farm" ){
+            e.provides[ "faction_base_farm_0" ] = 1;
+        } else if( e.type == "garage" ){
+            e.provides[ "faction_base_garage_0" ] = 1;
+        } else if( e.type == "blacksmith" ){
+            e.provides[ "faction_base_blacksmith_0" ] = 1;
+        }
         edata.read( "pos", e.pos );
         expansions[ dir ] = e;
         if( dir != "[B]" ) {
