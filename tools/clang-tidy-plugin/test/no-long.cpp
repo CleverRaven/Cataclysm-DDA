85a19076a8590d825f47e2aeccef621fb74c36cd
John Bytheway <jbytheway@gmail.com>
2019-06-08 13:30:56 +0100

    More tests for cata-no-long check

--- tools/clang-tidy-plugin/test/no-long.cpp
+++ tools/clang-tidy-plugin/test/no-long.cpp
@@ -1,4 +1,15 @@
 // RUN: %check_clang_tidy %s cata-no-long %t -- -plugins=%cata_plugin --
 
+#include <stdint.h>
+
 long a;
-// CHECK-MESSAGES: :[[@LINE-1]]:6: warning: Variable 'a' declared as long. Prefer int or int64_t. [cata-no-long]
+// CHECK-MESSAGES: warning: Variable 'a' declared as long. Prefer int or int64_t. [cata-no-long]
+
+unsigned long b;
+// CHECK-MESSAGES: warning: Variable 'b' declared as unsigned long. Prefer unsigned int or uint64_t. [cata-no-long]
+
+int64_t c;
+uint64_t d;
+
+void f(long e);
+// CHECK-MESSAGES: warning: Variable 'e' declared as long. Prefer int or int64_t. [cata-no-long]
