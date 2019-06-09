7430500a4950b013af5eca2e3f625c65f610701d
KorGgenT <curtis.r.merrill@gmail.com>
2019-06-09 10:03:01 -0400

    astyle item.cpp

--- src/item.cpp
+++ src/item.cpp
@@ -1786,13 +1786,13 @@ void item::gun_info( const item *mod, std::vector<iteminfo> &info, const iteminf
         }
         if( mod.reload_modifier != 1 && parts->test( iteminfo_parts::GUNMOD_RELOAD ) ) {
             info.emplace_back( "GUNMOD", _( "Reload modifier: " ), "",
-                                           iteminfo::no_newline | iteminfo::is_decimal | iteminfo::lower_is_better,
-                                           *mod.reload_modifier );
+                               iteminfo::no_newline | iteminfo::is_decimal | iteminfo::lower_is_better,
+                               *mod.reload_modifier );
         }
         if( mod.min_str_required_mod != 1 && parts->test( iteminfo_parts::GUNMOD_STRENGTH ) ) {
             info.emplace_back( "GUNMOD", _( "Minimum strength required modifier: " ), "",
-                                           iteminfo::no_newline | iteminfo::is_decimal,
-                                           *mod.min_str_required_mod );
+                               iteminfo::no_newline | iteminfo::is_decimal,
+                               *mod.min_str_required_mod );
         }
         if( !mod.add_mod.empty() && parts->test( iteminfo_parts::GUNMOD_ADD_MOD ) ) {
             insert_separation_line();
@@ -9242,7 +9242,7 @@ int item::get_min_str() const
     if( type->gun ) {
         int min_str = type->min_str;
         for( const auto mod : gunmods() ) {
-            static_cast<int>(min_str *= mod->type->gunmod->min_str_required_mod);
+            static_cast<int>( min_str *= mod->type->gunmod->min_str_required_mod );
         }
         return min_str > 0 ? min_str : 0;
     } else {
