f0fc4c20fcbdca48c474ad2d227547f98235feb4
John Bytheway <jbytheway@gmail.com>
2019-06-11 21:10:42 +0100

    Change workaround for implicit conversion
    item_location had a hacky workaround to suppress implicit conversion to
    int (give that it had a conversion to bool).
    
    This probably dates to an old compiler or something like that.  Removed
    the workaround and simply made the conversion to bool explicit.

--- src/npcmove.cpp
+++ src/npcmove.cpp
@@ -1455,7 +1455,7 @@ bool npc::can_reload_current()
         return false;
     }
 
-    return find_usable_ammo( weapon );
+    return static_cast<bool>( find_usable_ammo( weapon ) );
 }
 
 item_location npc::find_usable_ammo( const item &weap )
