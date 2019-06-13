3bcb88095ccbdb1897765b9c301915351bd5ceee
John Bytheway <jbytheway@gmail.com>
2019-06-13 20:24:39 +0100

    Clean up use of unsigned long in translations
    The underlying translation APIs take 'unsigned long' as an argument, and
    we can't change that.  However, it's natural to want to pass a size_t
    container size (which may be unsigned long long) so we want that to work
    without the need for a cast.
    
    To that end, add an ngettext overload taking unsigned long long, and
    make npgettext take an unsigned long long

--- src/translations.h
+++ src/translations.h
@@ -55,11 +55,24 @@ inline std::string _( const std::string &msg )
     return _( msg.c_str() );
 }
 
+// ngettext overload taking an unsigned long long so that people don't need
+// to cast at call sites.  This is particularly relevant on 64-bit Windows where
+// size_t is bigger than unsigned long, so MSVC will try to encourage you to
+// add a cast.
+template<typename T, typename = std::enable_if_t<std::is_same<T, unsigned long long>::value>>
+ATTRIBUTE_FORMAT_ARG( 1 )
+inline const char *ngettext( const char *msgid, const char *msgid_plural, T n )
+{
+    // Leaving this long because it matches the underlying API.
+    // NOLINTNEXTLINE(cata-no-long)
+    return ngettext( msgid, msgid_plural, static_cast<unsigned long>( n ) );
+}
+
 const char *pgettext( const char *context, const char *msgid ) ATTRIBUTE_FORMAT_ARG( 2 );
 
 // same as pgettext, but supports plural forms like ngettext
 const char *npgettext( const char *context, const char *msgid, const char *msgid_plural,
-                       unsigned long int n ) ATTRIBUTE_FORMAT_ARG( 2 );
+                       unsigned long long n ) ATTRIBUTE_FORMAT_ARG( 2 );
 
 #else // !LOCALIZE
 
