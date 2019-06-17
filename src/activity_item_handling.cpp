d07a1311fa9a4e10b0d3f07a2e5161f3fc925d70
davidpwbrown <39344466+davidpwbrown@users.noreply.github.com>
2019-06-17 17:55:46 +0100

    Update src/activity_item_handling.cpp
    Co-Authored-By: Curtis Merrill <curtis.r.merrill@gmail.com>

--- src/activity_item_handling.cpp
+++ src/activity_item_handling.cpp
@@ -1094,7 +1094,7 @@ void activity_on_turn_blueprint_move( player_activity &, player &p )
         bool found_any_pre = false;
 
         for( const zone_data &zone : zones ) {
-            const auto options = dynamic_cast<const blueprint_options &>( zone.get_options() );
+            const blueprint_options options = dynamic_cast<const blueprint_options &>( zone.get_options() );
             const int index = options.get_index();
             const std::vector<construction> &list_constructions = get_constructions();
             const construction &built = list_constructions[index];
