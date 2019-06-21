f15ba6fb7effee6a0b936ce31b45f49bc1a2b590
Jason Jones <jasonedwardjones@gmail.com>
2019-06-20 22:25:40 -0800

    Fix is_ot_type matching incorrect locations

--- src/map_extras.cpp
+++ src/map_extras.cpp
@@ -591,10 +591,10 @@ static void mx_bandits_block( map &m, const tripoint &abs_sub )
     const oter_id &west = overmap_buffer.ter( abs_sub.x - 1, abs_sub.y, abs_sub.z );
     const oter_id &east = overmap_buffer.ter( abs_sub.x + 1, abs_sub.y, abs_sub.z );
 
-    const bool forest_at_north = is_ot_type( "forest", north );
-    const bool forest_at_south = is_ot_type( "forest", south );
-    const bool forest_at_west = is_ot_type( "forest", west );
-    const bool forest_at_east = is_ot_type( "forest", east );
+    const bool forest_at_north = is_ot_prefix( "forest", north );
+    const bool forest_at_south = is_ot_prefix( "forest", south );
+    const bool forest_at_west = is_ot_prefix( "forest", west );
+    const bool forest_at_east = is_ot_prefix( "forest", east );
 
     const bool road_at_north = is_ot_type( "road", north );
     const bool road_at_south = is_ot_type( "road", south );
--- src/overmap.h
+++ src/overmap.h
@@ -427,6 +427,7 @@ class overmap
 bool is_river( const oter_id &ter );
 bool is_river_or_lake( const oter_id &ter );
 bool is_ot_type( const std::string &otype, const oter_id &oter );
+bool is_ot_prefix( const std::string &otype, const oter_id &oter );
 // Matches any oter_id that contains the substring passed in, useful when oter can be a suffix, not just a prefix.
 bool is_ot_subtype( const char *otype, const oter_id &oter );
 
