0e06572b00a32efc01cc2e96eeeffa3f3859d561
sah4001 <50809250+sah4001@users.noreply.github.com>
2019-06-22 01:51:14 -0500

    Fermenting Multifluid change (#31708)
    * Fermenting Multifluid change

--- src/activity_handlers.cpp
+++ src/activity_handlers.cpp
@@ -1384,9 +1384,12 @@ void activity_handlers::fill_liquid_do_turn( player_activity *act, player *p )
                         add_msg( _( "With a clang and a shudder, the %s pump goes silent." ),
                                  liquid.type_name( 1 ) );
                     } else if( g->m.furn( source_pos ).obj().examine == &iexamine::fvat_full ) {
-                        g->m.furn_set( source_pos, f_fvat_empty );
                         add_msg( _( "You squeeze the last drops of %s from the vat." ),
                                  liquid.type_name( 1 ) );
+                        map_stack items_here = g->m.i_at( source_pos );
+                        if( items_here.empty() ) {
+                            g->m.furn_set( source_pos, f_fvat_empty );
+                        }
                     }
                     act_ref.set_to_null();
                 }
