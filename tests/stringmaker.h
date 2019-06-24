7493e9b5ade06299ea10e58f7efe31844c06fdce
John Bytheway <jbytheway@gmail.com>
2019-06-24 21:29:16 +0100

    Closing namespace comment

--- tests/stringmaker.h
+++ tests/stringmaker.h
@@ -9,12 +9,14 @@
 
 namespace Catch
 {
+
 template<>
 struct StringMaker<item> {
     static std::string convert( const item &i ) {
         return string_format( "item( \"%s\" )", i.typeId() );
     }
 };
-}
+
+} // namespace Catch
 
 #endif // CATA_TESTS_STRINGMAKER_H
