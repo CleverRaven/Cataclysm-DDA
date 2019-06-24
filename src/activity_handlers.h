fc9f2bdfa86ca60e6db6b63b28240ca737a8a4ae
nexusmrsep <nexus.mrsep@gmail.com>
2019-06-24 21:36:15 +0200

    Wait option: Wait until you regain stamina

--- src/activity_handlers.h
+++ src/activity_handlers.h
@@ -101,6 +101,7 @@ void robot_control_do_turn( player_activity *act, player *p );
 void tree_communion_do_turn( player_activity *act, player *p );
 void spellcasting_do_turn( player_activity *act, player *p );
 void study_spell_do_turn( player_activity *act, player *p );
+void wait_stamina_do_turn( player_activity *act, player *p );
 
 // defined in activity_handlers.cpp
 extern const std::map< activity_id, std::function<void( player_activity *, player * )> >
@@ -134,6 +135,7 @@ void read_finish( player_activity *act, player *p );
 void wait_finish( player_activity *act, player *p );
 void wait_weather_finish( player_activity *act, player *p );
 void wait_npc_finish( player_activity *act, player *p );
+void wait_stamina_finish( player_activity *act, player *p );
 void socialize_finish( player_activity *act, player *p );
 void try_sleep_finish( player_activity *act, player *p );
 void disassemble_finish( player_activity *act, player *p );
