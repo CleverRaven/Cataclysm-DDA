CleverRaven/Cataclysm-DDA

commit 27deb2c8b97013dab3c69a71519211cd3cc1c954

Magiclysm: add translocators (#31789)
* Magiclysm: add translocators

src/game.cpp
@@ -4595,6 +4595,7 @@ const T *game::critter_at( const tripoint &p, bool allow_hallucination ) const
 template const monster *game::critter_at<monster>( const tripoint &, bool ) const;
 template const npc *game::critter_at<npc>( const tripoint &, bool ) const;
 template const player *game::critter_at<player>( const tripoint &, bool ) const;
+template const avatar *game::critter_at<avatar>( const tripoint &, bool ) const;
 template const Character *game::critter_at<Character>( const tripoint &, bool ) const;
 template Character *game::critter_at<Character>( const tripoint &, bool );
 template const Creature *game::critter_at<Creature>( const tripoint &, bool ) const;

src/activity_handlers.cpp
@@ -4003,6 +4003,8 @@ void activity_handlers::spellcasting_finish( player_activity *act, player *p )
         casting.make_sound( target );
     } else if( fx == "summon" ) {
         spell_effect::spawn_summoned_monster( casting, p->pos(), target );
+    } else if( fx == "translocate" ) {
+        spell_effect::translocate( casting, p->pos(), target, g->u.translocators );
     } else {
         debugmsg( "ERROR: Spell effect not defined properly." );
     }

src/avatar.h
@@ -5,6 +5,8 @@
 #include "enums.h"
 #include "player.h"
 
+#include "magic_teleporter_list.h"
+
 class avatar : public player
 {
     public:
@@ -106,6 +108,8 @@ class avatar : public player
          * @param target Target NPC to steal from
          */
         void steal( npc &target );
+
+        teleporter_list translocators;
     private:
         map_memory player_map_memory;
         bool show_map_memory;

src/iexamine.cpp
@@ -272,6 +272,20 @@ void iexamine::gaspump( player &p, const tripoint &examp )
     add_msg( m_info, _( "Out of order." ) );
 }
 
+void iexamine::translocator( player &, const tripoint &examp )
+{
+    const tripoint omt_loc = ms_to_omt_copy( g->m.getabs( examp ) );
+    const bool activated = g->u.translocators.knows_translocator( examp );
+    if( !activated ) {
+        g->u.translocators.activate_teleporter( omt_loc, examp );
+        add_msg( m_info, _( "Translocator gate active." ) );
+    } else {
+        if( query_yn( _( "Do you want to deactivate this active Translocator?" ) ) ) {
+            g->u.translocators.deactivate_teleporter( omt_loc, examp );
+        }
+    }
+}
+
 namespace
 {
 //--------------------------------------------------------------------------------------------------
@@ -5357,6 +5371,7 @@ iexamine_function iexamine_function_from_string( const std::string &function_nam
             { "harvest_ter", &iexamine::harvest_ter },
             { "harvested_plant", &iexamine::harvested_plant },
             { "shrub_marloss", &iexamine::shrub_marloss },
+            { "translocator", &iexamine::translocator },
             { "tree_marloss", &iexamine::tree_marloss },
             { "tree_hickory", &iexamine::tree_hickory },
             { "tree_maple", &iexamine::tree_maple },

src/iexamine.h
@@ -104,6 +104,7 @@ void sign( player &p, const tripoint &examp );
 void pay_gas( player &p, const tripoint &examp );
 void ledge( player &p, const tripoint &examp );
 void autodoc( player &p, const tripoint &examp );
+void translocator( player &p, const tripoint &examp );
 void on_smoke_out( const tripoint &examp,
                    const time_point &start_time ); //activates end of smoking effects
 void mill_finalize( player &, const tripoint &examp, const time_point &start_time );

src/magic.cpp
@@ -20,6 +20,7 @@
 #include "projectile.h"
 #include "rng.h"
 #include "sounds.h"
+#include "magic_teleporter_list.h"
 #include "translations.h"
 #include "ui.h"
 

src/magic.h
@@ -20,6 +20,7 @@ class player;
 class JsonObject;
 class JsonOut;
 class JsonIn;
+class teleporter_list;
 class time_duration;
 class nc_color;
 
@@ -371,6 +372,8 @@ std::set<tripoint> spell_effect_line( const spell &, const tripoint &source,
 void spawn_ethereal_item( spell &sp );
 void recover_energy( spell &sp, const tripoint &target );
 void spawn_summoned_monster( spell &sp, const tripoint &source, const tripoint &target );
+void translocate( spell &sp, const tripoint &source, const tripoint &target,
+                  teleporter_list &tp_list );
 } // namespace spell_effect
 
 class spellbook_callback : public uilist_callback

src/magic_spell_effect.cpp
@@ -467,3 +467,9 @@ void spell_effect::spawn_summoned_monster( spell &sp, const tripoint &source,
         area.erase( iter );
     }
 }
+
+void spell_effect::translocate( spell &sp, const tripoint &source, const tripoint &target,
+                                teleporter_list &tp_list )
+{
+    tp_list.translocate( spell_effect_area( sp, source, target, spell_effect_blast, true ) );
+}

commit 64930007a20e5cb5bdb8c0fb5131163a91eb3e67

Fix compiling error introduced in #31789

src/game.cpp
@@ -4588,6 +4588,7 @@ template const monster *game::critter_at<monster>( const tripoint &, bool ) cons
 template const npc *game::critter_at<npc>( const tripoint &, bool ) const;
 template const player *game::critter_at<player>( const tripoint &, bool ) const;
 template const avatar *game::critter_at<avatar>( const tripoint &, bool ) const;
+template avatar *game::critter_at<avatar>( const tripoint &, bool );
 template const Character *game::critter_at<Character>( const tripoint &, bool ) const;
 template Character *game::critter_at<Character>( const tripoint &, bool );
 template const Creature *game::critter_at<Creature>( const tripoint &, bool ) const;