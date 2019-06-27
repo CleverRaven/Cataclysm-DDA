0e43a377dd1a463a1a226f5ce38eb741c0f43d9c
Fris0uman <sthapplz@gmail.com>
2019-06-27 22:30:15 +0200

    destroying autodoc cancel activity

--- src/activity_handlers.cpp
+++ src/activity_handlers.cpp
@@ -2783,11 +2783,14 @@ void activity_handlers::uninstall_operation_do_turn( player_activity *act, playe
     const time_duration op_duration = difficulty * 10_minutes;
     time_duration time_left = time_duration::from_turns( act->moves_left / 100 ) ;
 
+    const std::list<tripoint> autodocs = g->m.find_furnitures_in_radius( p->pos(), 1,
+                                         furn_str_id( "f_autodoc" ) );
     if( g->m.furn( p->pos() ) != furn_str_id( "f_autodoc_couch" ) ) {
         if( act->values.size() > 5 ) {
             for( size_t i = 5; i < act->values.size() - 1; i = i + 2 ) {
-                add_msg( m_bad, _( "Moving from the Autodoc mid-operation rips your %s open." ),
-                         body_part_name_accusative( body_part( act->values[i] ) ) );
+                p->add_msg_player_or_npc( m_bad, _( "Moving from the Autodoc mid-operation rips your %s open." ),
+                                          _( "Moving from the Autodoc mid-operation rips <npcname>'s %s open." ),
+                                          body_part_name_accusative( body_part( act->values[i] ) ) );
                 p->add_effect( effect_bleed, 1_turns, body_part( act->values[i] ), true, difficulty );
                 if( body_part( act->values[i] ) == bp_eyes ) {
                     p->add_effect( effect_blind, 1_hours, num_bp );
@@ -2795,9 +2798,19 @@ void activity_handlers::uninstall_operation_do_turn( player_activity *act, playe
                 p->remove_effect( effect_under_op, body_part( act->values[i] ) );
 
             }
+        } else {
+            p->add_effect( effect_bleed, 1_turns, num_bp, true, difficulty );
+            p->add_msg_player_or_npc( m_bad, _( "Moving from the Autodoc mid-operation rips you %s open." ),
+                                      _( "Moving from the Autodoc mid-operation rips <npcname> %s open." ) );
         }
         p->remove_effect( effect_under_op );
         act->set_to_null();
+    }
+
+    if( autodocs.empty() ) {
+        add_msg( "The Autodoc is gone, the operation stops." );
+        p->remove_effect( effect_under_op );
+        p->cancel_activity();
     }
 
     if( time_left >  op_duration ) {
