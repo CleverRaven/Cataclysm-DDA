abcf1f2888b903d8e55e0cdb95fab7efce173eb5
Oren Audeles <orenaudeles@gmail.com>
2019-06-29 05:26:26 -0500

    perf_field_scent: temporary storage for scent modifications
    scent_map::set( tripoint, int ) is called a lot when there are gaseous
    fields spreading, up to nine times per call to <lambda>'spread_gas'
    The structure does a test on construction for all points it can possibly
    touch, then during 'commit_modifications' calls set once per tile.

--- src/map_field.cpp
+++ src/map_field.cpp
@@ -1,5 +1,6 @@
 #include "field.h"
 
+#include <array>
 #include <cmath>
 #include <cstddef>
 #include <algorithm>
@@ -150,6 +151,93 @@ static int ter_furn_movecost( const ter_t &ter, const furn_t &furn )
 
     return ter.movecost + furn.movecost;
 }
+
+struct scent_block {
+    template<typename T>
+    using data_block = std::array<std::array<T, SEEY + 2>, SEEX + 2>;
+
+    enum data_mode {
+        NONE = 0,
+        SET = 1,
+        MAX = 2
+    };
+
+    struct datum {
+        data_mode mode;
+        int intensity;
+    };
+
+    data_block<bool> assignable;
+    data_block<datum> assignment;
+
+    tripoint origin;
+    scent_map &scents;
+
+    scent_block( int subx, int suby, int subz, scent_map &scents )
+    : origin( subx * SEEX - 1, suby * SEEY - 1, subz ), scents(scents) {
+        for( int x = 0; x < SEEX + 2; ++x ) {
+            for( int y = 0; y < SEEY + 2; ++y ) {
+                assignable[x][y] = scents.inbounds( origin + tripoint( x, y, 0 ) );
+                assignment[x][y] = { NONE, 0 };
+            }
+        }
+    }
+
+    void commit_modifications() {
+        for( int x = 0; x < SEEX + 2; ++x ) {
+            for( int y = 0; y < SEEY + 2; ++y ) {
+                if( assignable[x][y] ) {
+                    switch( assignment[x][y].mode ) {
+                        case NONE: break;
+                        case SET: {
+                            scents.set( origin + tripoint( x, y, 0 ), assignment[x][y].intensity );
+                            break;
+                        }
+                        case MAX: {
+                            tripoint p = origin + tripoint( x, y, 0 );
+                            scents.set( p, std::max( assignment[x][y].intensity, scents.get( p ) ) );
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    inline point index( const tripoint &p ) {
+        return point( p.x - origin.x, p.y - origin.y );
+    }
+
+    // We should be working entirely within the range, so don't range check here
+    void apply_gas( const tripoint &p ) {
+        const point ndx = index( p );
+        assignment[ndx.x][ndx.y].mode = SET;
+        assignment[ndx.x][ndx.y].intensity = 0;
+    }
+    void apply_slime( const tripoint &p, int intensity ) {
+        const point ndx = index( p );
+        datum &dat = assignment[ndx.x][ndx.y];
+        switch( dat.mode ) {
+            case NONE: {
+                // we don't know what the current intensity is, so we must do a max operation
+                dat.mode = MAX;
+                dat.intensity = intensity;
+                break;
+            }
+            case SET: {
+                // new intensity is going to be dat.intensity, so we just need to make it larger
+                // but cannot change
+                dat.intensity = std::max( dat.intensity, intensity );
+                break;
+            }
+            case MAX: {
+                // Already max for some reason, shouldn't occur. If it does we want to grow if possible
+                dat.intensity = std::max( dat.intensity, intensity );
+                break;
+            }
+        }
+    }
+};
 
 /*
 Function: process_fields_in_submap
