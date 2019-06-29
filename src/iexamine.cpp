97e105863021d8595d3c8d623c3e7a5bb0152a14
Fris0uman <sthapplz@gmail.com>
2019-06-29 11:47:42 +0200

    check effect instead of activity

--- src/iexamine.cpp
+++ src/iexamine.cpp
@@ -4094,7 +4094,7 @@ void iexamine::autodoc( player &p, const tripoint &examp )
             popup( _( "No patient found located on the connected couches.  Operation impossible.  Exiting." ) );
             return;
         }
-    } else if( patient.activity.id() == "ACT_OPERATION_REMOVE" ) {
+    } else if( patient.has_effect( effect_under_op ) ) {
         popup( _( "Operation underway.  Please wait until the end of the current procedure.  Estimated time remaining: %s." ),
                to_string( patient.get_effect_dur( effect_under_op ) ) );
         p.add_msg_if_player( m_info, _( "The autodoc is working on %s." ), patient.disp_name() );
@@ -4232,9 +4232,9 @@ void iexamine::autodoc( player &p, const tripoint &examp )
                     }
 
                 }
+                installer.mod_moves( -to_moves<int>( 1_minutes ) );
                 patient.add_effect( effect_under_op, duration, num_bp );
                 patient.uninstall_bionic( bid, installer, true );
-                installer.mod_moves( -to_moves<int>( 1_minutes ) );
             }
             break;
         }
