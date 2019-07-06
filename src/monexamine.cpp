a07bfe5f85126d5142829422e6fd2874f2ad1261
Curtis Merrill <curtis.r.merrill@gmail.com>
2019-07-06 14:26:27 -0400

    Update src/monexamine.cpp
    Co-Authored-By: Isaac Freund <isaac.a.freund@gmail.com>

--- src/monexamine.cpp
+++ src/monexamine.cpp
@@ -510,7 +510,7 @@ void monexamine::tie_or_untie( monster &z )
     if( z.has_effect( effect_tied ) ) {
         z.remove_effect( effect_tied );
         if( z.tied_item ) {
-            g->u.i_add( *z.tied_item, false );
+            g->u.i_add( *z.tied_item );
             z.tied_item = cata::nullopt;
         }
     } else {
