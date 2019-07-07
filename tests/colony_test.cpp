faa5eb15b85a694c1baff51919de16be888cfed9
Isaac Freund <isaac.a.freund@gmail.com>
2019-07-07 14:55:46 +0200

    Move colony and list test helpers to new header

--- tests/colony_test.cpp
+++ tests/colony_test.cpp
@@ -6,25 +6,7 @@
 
 #include "catch/catch.hpp"
 #include "colony.h"
-
-// Fast xorshift+128 random number generator function
-// original: https://codingforspeed.com/using-faster-psudo-random-generator-xorshift/
-static unsigned int xor_rand()
-{
-    static unsigned int x = 123456789;
-    static unsigned int y = 362436069;
-    static unsigned int z = 521288629;
-    static unsigned int w = 88675123;
-
-    const unsigned int t = x ^ ( x << 11 );
-
-    // Rotate the static values (w rotation in return statement):
-    x = y;
-    y = z;
-    z = w;
-
-    return w = w ^ ( w >> 19 ) ^ ( t ^ ( t >> 8 ) );
-}
+#include "colony_list_test_helpers.h"
 
 TEST_CASE( "colony basics", "[colony]" )
 {
@@ -888,18 +870,6 @@ TEST_CASE( "colony insertion methods", "[colony]" )
     CHECK( sum == 12060 );
 }
 
-struct perfect_forwarding_test {
-    const bool success;
-
-    perfect_forwarding_test( int && /*perfect1*/, int &perfect2 ) : success( true ) {
-        perfect2 = 1;
-    }
-
-    template <typename T, typename U>
-    perfect_forwarding_test( T && /*imperfect1*/, U && /*imperfect2*/ ) : success( false )
-    {}
-};
-
 TEST_CASE( "colony perfect forwarding", "[colony]" )
 {
     cata::colony<perfect_forwarding_test> test_colony;
@@ -912,18 +882,6 @@ TEST_CASE( "colony perfect forwarding", "[colony]" )
     CHECK( ( *test_colony.begin() ).success );
     CHECK( lvalueref == 1 );
 }
-
-struct small_struct {
-    double *empty_field_1;
-    double unused_number;
-    unsigned int empty_field2;
-    double *empty_field_3;
-    int number;
-    unsigned int empty_field4;
-
-    small_struct( const int num ) noexcept:
-        number( num ) {};
-};
 
 TEST_CASE( "colony emplace", "[colony]" )
 {
