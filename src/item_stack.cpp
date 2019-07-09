494e023efe5b61126e632b018999ceea1b03c965
John Bytheway <jbytheway@gmail.com>
2019-07-09 23:28:59 +0100

    Update various literal suffixes
    Update all integer literal suffixes containing a lower-case 'l'.
    
    In some cases the suffix was unnecessary, in which case remove it.
    
    In others, capitalize it.
    
    These were found by clang-tidy's uppercase-literal-suffix check.

--- src/item_stack.cpp
+++ src/item_stack.cpp
@@ -111,7 +111,7 @@ int item_stack::amount_can_fit( const item &it ) const
     const item *here = it.count_by_charges() ? stacks_with( it ) : nullptr;
 
     if( violates_count && !here ) {
-        return 0l;
+        return 0;
     }
     // Call max because a tile may have been overfilled to begin with (e.g. #14115)
     const int ret = std::max( 0, it.charges_per_volume( free_volume() ) );
