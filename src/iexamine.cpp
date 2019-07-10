a0a0c75df0a85f09f362fe18fcdfdb4309bb0067
Fris0uman <sthapplz@gmail.com>
2019-07-10 21:50:37 +0200

    unused var

--- src/iexamine.cpp
+++ src/iexamine.cpp
@@ -2383,7 +2383,7 @@ void iexamine::autoclave_empty( player &p, const tripoint &examp )
     }
 }
 
-void iexamine::autoclave_full( player &p, const tripoint &examp )
+void iexamine::autoclave_full( player &, const tripoint &examp )
 {
     furn_id cur_autoclave_type = g->m.furn( examp );
     furn_id next_autoclave_type = f_null;
