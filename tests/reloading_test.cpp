0c31bf1ee58333ea6306c51a2322af02a04d79ce
ymber <mtaylor1252dev@gmail.com>
2019-07-14 10:47:01 +0100

    Update specific reloading tests

--- tests/reloading_test.cpp
+++ tests/reloading_test.cpp
@@ -18,6 +18,28 @@
 #include "player_activity.h"
 #include "type_id.h"
 
+TEST_CASE( "reload_integral_magazine_of gun", "[reload],[gun]" )
+{
+    player &dummy = g->u;
+
+    clear_player();
+    // Make sure the player doesn't drop anything :P
+    dummy.wear_item( item( "backpack", 0 ) );
+
+    item &ammo = dummy.i_add( item( "762_54R", 0, item::default_charges_tag{} ) );
+    item &gun = dummy.i_add( item( "mosin91_30", 0, item::default_charges_tag{} ) );
+    int ammo_pos = dummy.inv.position_by_item( &ammo );
+
+    REQUIRE( ammo_pos != INT_MIN );
+    REQUIRE( gun.ammo_remaining() == 0 );
+    REQUIRE( !gun.integral_magazines().empty() );
+
+    bool success = gun.magazine_current()->reload( dummy, item_location( dummy, &ammo ), ammo.charges );
+
+    REQUIRE( success );
+    REQUIRE( gun.ammo_remaining() == gun.ammo_capacity() );
+}
+
 TEST_CASE( "reload_gun_with_integral_magazine", "[reload],[gun]" )
 {
     player &dummy = g->u;
@@ -26,18 +48,55 @@ TEST_CASE( "reload_gun_with_integral_magazine", "[reload],[gun]" )
     // Make sure the player doesn't drop anything :P
     dummy.wear_item( item( "backpack", 0 ) );
 
-    item &ammo = dummy.i_add( item( "40sw", 0, item::default_charges_tag{} ) );
-    item &gun = dummy.i_add( item( "sw_610", 0, item::default_charges_tag{} ) );
+    item &ammo = dummy.i_add( item( "762_54R", 0, item::default_charges_tag{} ) );
+    item &gun = dummy.i_add( item( "mosin91_30", 0, item::default_charges_tag{} ) );
     int ammo_pos = dummy.inv.position_by_item( &ammo );
 
     REQUIRE( ammo_pos != INT_MIN );
     REQUIRE( gun.ammo_remaining() == 0 );
-    REQUIRE( gun.magazine_integral() );
+    REQUIRE( !gun.integral_magazines().empty() );
 
     bool success = gun.reload( dummy, item_location( dummy, &ammo ), ammo.charges );
 
+    // The gun item itself should not be reloadable
+    REQUIRE( !success );
+    REQUIRE( gun.ammo_remaining() == 0 );
+}
+
+TEST_CASE( "reload_integral_magazine_of_gun_using_speedloader", "[reload],[gun]" )
+{
+    player &dummy = g->u;
+
+    clear_player();
+    // Make sure the player doesn't drop anything :P
+    dummy.wear_item( item( "backpack", 0 ) );
+
+    item &ammo = dummy.i_add( item( "762_54R", 0, item::default_charges_tag{} ) );
+    item &speedloader = dummy.i_add( item( "762R_clip", 0, false ) );
+    int loader_pos = dummy.inv.position_by_item( &speedloader );
+    item &gun = dummy.i_add( item( "mosin91_30", 0, false ) );
+    int ammo_pos = dummy.inv.position_by_item( &ammo );
+
+    REQUIRE( ammo_pos != INT_MIN );
+    REQUIRE( gun.ammo_remaining() == 0 );
+    REQUIRE( !gun.integral_magazines().empty() );
+    REQUIRE( loader_pos != INT_MIN );
+    REQUIRE( speedloader.ammo_remaining() == 0 );
+    REQUIRE( speedloader.has_flag( "SPEEDLOADER" ) );
+
+    bool speedloader_success = speedloader.reload( dummy, item_location( dummy, &ammo ), ammo.charges );
+
+    REQUIRE( speedloader_success );
+    REQUIRE( speedloader.ammo_remaining() == speedloader.ammo_capacity() );
+
+    bool success = gun.magazine_current()->reload( dummy, item_location( dummy, &speedloader ),
+                   speedloader.ammo_remaining() );
+
     REQUIRE( success );
     REQUIRE( gun.ammo_remaining() == gun.ammo_capacity() );
+    // Speedloader should be empty and still in inventory.
+    REQUIRE( dummy.inv.position_by_item( &speedloader ) != INT_MIN );
+    REQUIRE( speedloader.ammo_remaining() == 0 );
 }
 
 TEST_CASE( "reload_gun_with_integral_magazine_using_speedloader", "[reload],[gun]" )
@@ -48,15 +107,15 @@ TEST_CASE( "reload_gun_with_integral_magazine_using_speedloader", "[reload],[gun
     // Make sure the player doesn't drop anything :P
     dummy.wear_item( item( "backpack", 0 ) );
 
-    item &ammo = dummy.i_add( item( "38_special", 0, item::default_charges_tag{} ) );
-    item &speedloader = dummy.i_add( item( "38_speedloader", 0, false ) );
+    item &ammo = dummy.i_add( item( "762_54R", 0, item::default_charges_tag{} ) );
+    item &speedloader = dummy.i_add( item( "762R_clip", 0, false ) );
     int loader_pos = dummy.inv.position_by_item( &speedloader );
-    item &gun = dummy.i_add( item( "sw_619", 0, false ) );
+    item &gun = dummy.i_add( item( "mosin91_30", 0, false ) );
     int ammo_pos = dummy.inv.position_by_item( &ammo );
 
     REQUIRE( ammo_pos != INT_MIN );
     REQUIRE( gun.ammo_remaining() == 0 );
-    REQUIRE( gun.magazine_integral() );
+    REQUIRE( !gun.integral_magazines().empty() );
     REQUIRE( loader_pos != INT_MIN );
     REQUIRE( speedloader.ammo_remaining() == 0 );
     REQUIRE( speedloader.has_flag( "SPEEDLOADER" ) );
@@ -69,13 +128,14 @@ TEST_CASE( "reload_gun_with_integral_magazine_using_speedloader", "[reload],[gun
     bool success = gun.reload( dummy, item_location( dummy, &speedloader ),
                                speedloader.ammo_remaining() );
 
-    REQUIRE( success );
-    REQUIRE( gun.ammo_remaining() == gun.ammo_capacity() );
-    // Speedloader is still in inventory.
+    // The gun item itself should not be reloadable
+    REQUIRE( !success );
+    REQUIRE( gun.ammo_remaining() == 0 );
+    // Speedloader should be loaded and still in inventory.
     REQUIRE( dummy.inv.position_by_item( &speedloader ) != INT_MIN );
 }
 
-TEST_CASE( "reload_gun_with_swappable_magazine", "[reload],[gun]" )
+TEST_CASE( "reload_gun_with_empty_detachable_magazine", "[reload],[gun]" )
 {
     player &dummy = g->u;
 
@@ -83,44 +143,72 @@ TEST_CASE( "reload_gun_with_swappable_magazine", "[reload],[gun]" )
     // Make sure the player doesn't drop anything :P
     dummy.wear_item( item( "backpack", 0 ) );
 
-    item &ammo = dummy.i_add( item( "9mm", 0, item::default_charges_tag{} ) );
-    const cata::optional<islot_ammo> &ammo_type = ammo.type->ammo;
-    REQUIRE( ammo_type );
-
-    const item mag( "glockmag", 0, 0 );
-    const cata::optional<islot_magazine> &magazine_type = mag.type->magazine;
-    REQUIRE( magazine_type );
-    REQUIRE( magazine_type->type.count( ammo_type->type ) != 0 );
-
-    item &gun = dummy.i_add( item( "glock_19", 0, item::default_charges_tag{} ) );
-    REQUIRE( gun.ammo_types().count( ammo_type->type ) != 0 );
-
-    gun.put_in( mag );
-
-    int gun_pos = dummy.inv.position_by_type( "glock_19" );
+    item &mag = dummy.i_add( item( "glockmag", 0, false ) );
+    item &gun = dummy.i_add( item( "glock_19", 0, false ) );
+    int mag_pos = dummy.inv.position_by_item( &mag );
+    int gun_pos = dummy.inv.position_by_item( &gun );
+    REQUIRE( gun.magazine_compatible().count( mag.type->get_id() ) != 0 );
+    REQUIRE( mag_pos != INT_MIN );
     REQUIRE( gun_pos != INT_MIN );
-    // We're expecting the magazine to end up in the inventory.
-    g->unload( gun_pos );
-    int magazine_pos = dummy.inv.position_by_type( "glockmag" );
-    REQUIRE( magazine_pos != INT_MIN );
-    item &magazine = dummy.inv.find_item( magazine_pos );
-    REQUIRE( magazine.ammo_remaining() == 0 );
 
+    REQUIRE( mag.ammo_remaining() == 0 );
+
+    bool success = gun.reload( dummy, item_location( dummy, &mag ), 1 );
+
+    // The gun should be loaded with an empty magazine
+    REQUIRE( success );
+    REQUIRE( gun.ammo_remaining() == 0 );
+    REQUIRE( gun.contents.front().type->get_id() == mag.type->get_id() );
+
+    // The magazine should no longer be in the player's inventory
+    int magazine_pos = dummy.inv.position_by_type( "glockmag" );
+    REQUIRE( magazine_pos == INT_MIN );
+}
+
+TEST_CASE( "reload_gun_with_loaded_detachable_magazine", "[reload],[gun]" )
+{
+    player &dummy = g->u;
+
+    clear_player();
+    // Make sure the player doesn't drop anything :P
+    dummy.wear_item( item( "backpack", 0 ) );
+
+    item &mag = dummy.i_add( item( "glockmag", 0, false ) );
+    item &gun = dummy.i_add( item( "glock_19", 0, false ) );
+    item &ammo = dummy.i_add( item( "9mm", 0, item::default_charges_tag{} ) );
+    int mag_pos = dummy.inv.position_by_item( &mag );
+    int gun_pos = dummy.inv.position_by_item( &gun );
     int ammo_pos = dummy.inv.position_by_item( &ammo );
+    REQUIRE( mag.type->magazine->type.count( ammo.type->ammo->type ) != 0 );
+    REQUIRE( gun.type->gun->ammo.count( ammo.type->ammo->type ) != 0 );
+    REQUIRE( gun.magazine_compatible().count( mag.type->get_id() ) != 0 );
+    REQUIRE( mag_pos != INT_MIN );
+    REQUIRE( gun_pos != INT_MIN );
     REQUIRE( ammo_pos != INT_MIN );
 
-    bool magazine_success = magazine.reload( dummy, item_location( dummy, &ammo ), ammo.charges );
+    REQUIRE( mag.ammo_remaining() == 0 );
+    REQUIRE( mag.is_reloadable_with( ammo.type->get_id() ) );
 
-    REQUIRE( magazine_success );
-    REQUIRE( magazine.ammo_remaining() == magazine.ammo_capacity() );
+    bool mag_success = mag.reload( dummy, item_location( dummy, &ammo ), ammo.charges );
+
+    // The magazine should be loaded to full capacity
+    REQUIRE( mag_success );
+    REQUIRE( mag.ammo_remaining() == mag.ammo_capacity() );
+    REQUIRE( mag.contents.front().type->get_id() == ammo.type->get_id() );
 
     REQUIRE( gun.ammo_remaining() == 0 );
-    REQUIRE( gun.magazine_integral() == false );
+    REQUIRE( gun.is_reloadable_with( mag.type->get_id() ) );
 
-    bool gun_success = gun.reload( dummy, item_location( dummy, &magazine ), 1 );
+    bool gun_success = gun.reload( dummy, item_location( dummy, &mag ), 1 );
 
-    CHECK( gun_success );
+    // The gun should be loaded with a full magazine
+    REQUIRE( gun_success );
     REQUIRE( gun.ammo_remaining() == gun.ammo_capacity() );
+    REQUIRE( gun.contents.front().type->get_id() == mag.type->get_id() );
+
+    // The magazine should no longer be in the player's inventory
+    mag_pos = dummy.inv.position_by_type( "glockmag" );
+    REQUIRE( mag_pos == INT_MIN );
 }
 
 static void reload_a_revolver( player &dummy, item &gun, item &ammo )
