7841cf4f320a0123f0d03acf849f97f117fc6b3a
KorGgenT <curtis.r.merrill@gmail.com>
2019-07-19 20:13:02 -0400

    astyle

--- src/monster.cpp
+++ src/monster.cpp
@@ -1153,7 +1153,7 @@ bool monster::is_immune_effect( const efftype_id &effect ) const
                ( !made_of( material_id( "flesh" ) ) && !made_of( material_id( "iflesh" ) ) );
     }
 
-    if ( effect == effect_stunned ) {
+    if( effect == effect_stunned ) {
         return has_flag( MF_STUN_IMMUNE );
     }
 
