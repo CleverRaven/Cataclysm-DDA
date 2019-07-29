13922c4743cc7024f5f58d83e65e8310863cb448
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2019-07-29 03:42:54 +0300

    Allow map memory drawing mode selection through options

--- src/options.cpp
+++ src/options.cpp
@@ -1674,6 +1674,17 @@ void options_manager::add_options_graphics()
 
     get_option( "TILES" ).setPrerequisite( "USE_TILES" );
 
+    mOptionsSort["graphics"]++;
+
+    add( "MEMORY_MAP_MODE", "graphics", translate_marker( "Memory map drawing mode" ),
+    translate_marker( "Specified the mode in which the memory map is drawn.  Requires restart." ), {
+        { "color_pixel_gray_out", translate_marker( "Classic (Gray)" ) },
+        { "color_pixel_sepia", translate_marker( "Modern (Sepia)" ) }
+    }, "color_pixel_sepia", COPT_CURSES_HIDE
+       );
+
+    mOptionsSort["graphics"]++;
+
     add( "PIXEL_MINIMAP", "graphics", translate_marker( "Pixel minimap" ),
          translate_marker( "If true, shows the pixel-detail minimap in game after the save is loaded.  Use the 'Toggle Pixel Minimap' action key to change its visibility during gameplay." ),
          true, COPT_CURSES_HIDE
