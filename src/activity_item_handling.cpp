d30aa4bd13b0c25064ec652bbdfd1638f2a1d351
Dpwb <davidpwbrown@gmail.com>
2019-08-03 22:38:21 +0200

    check existing trap ( pit ) before adding partial_con trap

--- src/activity_item_handling.cpp
+++ src/activity_item_handling.cpp
@@ -1141,7 +1141,9 @@ void activity_on_turn_blueprint_move( player_activity &, player &p )
         pc.id = built_chosen.id;
         pc.counter = 0;
         // Set the trap that has the examine function
-        g->m.trap_set( src_loc, tr_unfinished_construction );
+        if( g->m.tr_at( src_loc ).loadid == tr_null ) {
+            g->m.trap_set( src_loc, tr_unfinished_construction );
+        }
         // Use up the components
         for( const std::vector<item_comp> &it : built_chosen.requirements->get_components() ) {
             std::list<item> tmp = p.consume_items( it, 1, is_crafting_component );
