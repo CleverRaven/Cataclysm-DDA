8f6e78a7376258bb5e18e8ef1f6c68a3a9352019
ipcyborg <ipcyborg@gmail.com>
2019-08-08 11:39:24 +0300

    Fixed "This corpse is already field dressed" upon successful field dressing #33013

--- src/activity_handlers.cpp
+++ src/activity_handlers.cpp
@@ -1194,6 +1194,7 @@ void activity_handlers::butcher_finish( player_activity *act, player *p )
                 }
 
             }
+            act->targets.pop_back();
             break;
         case SKIN:
             switch( rng( 1, 4 ) ) {
@@ -1215,6 +1216,7 @@ void activity_handlers::butcher_finish( player_activity *act, player *p )
                     break;
             }
             corpse_item.set_flag( "SKINNED" );
+            act->targets.pop_back();
             break;
         case DISMEMBER:
             switch( rng( 1, 3 ) ) {
