976ebb07a6437942e97c04787f61d3b2d2800d81
Kevin Granade <kevin.granade@gmail.com>
2019-08-09 15:31:19 -0700

    Misc wincurses fixes
    Use new style casts.
    Update to use point member of cursesport::WINDOW.

--- src/wincurse.cpp
+++ src/wincurse.cpp
@@ -163,7 +163,7 @@ static void create_backbuffer()
     bmi.bmiHeader.biSizeImage    = WindowWidth * WindowHeight * 1;
     bmi.bmiHeader.biClrUsed      = color_loader<RGBQUAD>::COLOR_NAMES_COUNT; // Colors in the palette
     bmi.bmiHeader.biClrImportant = color_loader<RGBQUAD>::COLOR_NAMES_COUNT; // Colors in the palette
-    backbit = CreateDIBSection( 0, &bmi, DIB_RGB_COLORS, ( void ** ) &dcbits, NULL, 0 );
+    backbit = CreateDIBSection( 0, &bmi, DIB_RGB_COLORS, static_cast<void **>( &dcbits ), NULL, 0 );
     DeleteObject( SelectObject( backbuffer, backbit ) ); //load the buffer into DC
 }
 
@@ -338,7 +338,7 @@ LRESULT CALLBACK ProcessMessages( HWND__ *hWnd, unsigned int Msg,
             return 0;
 
         case WM_SYSCHAR:
-            add_alt_code( ( char )wParam );
+            add_alt_code( static_cast<char>( wParam ) );
             return 0;
 
         case WM_SYSKEYDOWN:
@@ -414,13 +414,13 @@ void cata_cursesport::curses_drawwindow( const catacurses::window &w )
     int drawx = 0;
     int drawy = 0;
     wchar_t tmp;
-    RECT update = {win->x * fontwidth, -1,
-                   ( win->x + win->width ) *fontwidth, -1
+    RECT update = {win->pos.x * fontwidth, -1,
+                   ( win->pos.x + win->width ) *fontwidth, -1
                   };
 
     for( j = 0; j < win->height; j++ ) {
         if( win->line[j].touched ) {
-            update.bottom = ( win->y + j + 1 ) * fontheight;
+            update.bottom = ( win->pos.y + j + 1 ) * fontheight;
             if( update.top == -1 ) {
                 update.top = update.bottom - fontheight;
             }
@@ -432,8 +432,8 @@ void cata_cursesport::curses_drawwindow( const catacurses::window &w )
                 if( cell.ch.empty() ) {
                     continue; // second cell of a multi-cell character
                 }
-                drawx = ( ( win->x + i ) * fontwidth );
-                drawy = ( ( win->y + j ) * fontheight ); //-j;
+                drawx = ( ( win->pos.x + i ) * fontwidth );
+                drawy = ( ( win->pos.y + j ) * fontheight ); //-j;
                 if( drawx + fontwidth > WindowWidth || drawy + fontheight > WindowHeight ) {
                     // Outside of the display area, would not render anyway
                     continue;
@@ -465,7 +465,7 @@ void cata_cursesport::curses_drawwindow( const catacurses::window &w )
                         ExtTextOutW( backbuffer, drawx, drawy, 0, NULL, utf16.c_str(), utf16.length(), NULL );
                     }
                 } else {
-                    switch( ( unsigned char )win->line[j].chars[i].ch[0] ) {
+                    switch( static_cast<unsigned char>( win->line[j].chars[i].ch[0] ) ) {
                         // box bottom/top side (horizontal line)
                         case LINE_OXOX_C:
                             HorzLineDIB( drawx, drawy + halfheight, drawx + fontwidth, 1, FG );
@@ -639,7 +639,7 @@ void catacurses::init_interface()
 static uint64_t GetPerfCount()
 {
     uint64_t Count;
-    QueryPerformanceCounter( ( PLARGE_INTEGER )&Count );
+    QueryPerformanceCounter( static_cast<PLARGE_INTEGER>( &Count ) );
     return Count;
 }
 
@@ -649,7 +649,7 @@ input_event input_manager::get_input_event()
     // see, e.g., http://linux.die.net/man/3/getch
     // so although it's non-obvious, that refresh() call (and maybe InvalidateRect?) IS supposed to be there
     uint64_t Frequency;
-    QueryPerformanceFrequency( ( PLARGE_INTEGER )&Frequency );
+    QueryPerformanceFrequency( static_cast<PLARGE_INTEGER>( &Frequency ) );
     wrefresh( catacurses::stdscr );
     InvalidateRect( WindowHandle, NULL, true );
     lastchar = ERR;
