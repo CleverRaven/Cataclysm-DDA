a3ad5b20d86023b333c7d212d7d232aadfa51ad3
eso <eso@esotericist.org>
2019-08-08 23:40:10 -0700

    update vehicle efficiency test with new ambulance mass

--- tests/vehicle_efficiency.cpp
+++ tests/vehicle_efficiency.cpp
@@ -439,7 +439,7 @@ TEST_CASE( "vehicle_efficiency", "[vehicle] [engine]" )
     test_vehicle( "quad_bike", 264745, 75950, 75950, 35080, 35080 );
     test_vehicle( "scooter", 62287, 266900, 258300, 195200, 189700 );
     test_vehicle( "superbike", 241785, 72120, 46710, 34580, 21720 );
-    test_vehicle( "ambulance", 1783889, 433100, 351300, 65530, 48600 );
+    test_vehicle( "ambulance", 1783929, 433100, 351300, 65530, 48600 );
     test_vehicle( "fire_engine", 2563241, 1171000, 970800, 248500, 219000 );
     test_vehicle( "fire_truck", 6259233, 308500, 200800, 19950, 4747 );
     test_vehicle( "truck_swat", 5939334, 505800, 349500, 30060, 7719 );
