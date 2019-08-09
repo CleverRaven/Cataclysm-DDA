1479c3a339422b4b9b93220244f024eb52fa027f
John Bytheway <jbytheway@gmail.com>
2019-08-09 14:43:55 -0400

    Fix verify_copy_from_gets_damage_reduction test
    I was assuming that the zero damage type was valid, but that is in fact
    DT_NULL, and resistance to the null damage type is an uninitialized
    float value (from load_damage_array not initializing ret).  So this test
    would pass or fail at random.

--- tests/vehicle_part_test.cpp
+++ tests/vehicle_part_test.cpp
@@ -7,5 +7,5 @@ TEST_CASE( "verify_copy_from_gets_damage_reduction", "[vehicle]" )
     // Picking halfboard_horizontal as a vpart which is likely to remain
     // defined via copy-from, and which should have non-zero damage reduction.
     const vpart_info &vp = vpart_id( "halfboard_horizontal" ).obj();
-    CHECK( vp.damage_reduction[0] != 0 );
+    CHECK( vp.damage_reduction[DT_BASH] != 0 );
 }
