2126048ab3daa6aada318c8a59974548d6f884f7
Kevin Granade <kevin.granade@gmail.com>
2019-08-12 16:26:31 -0700

    Update monattack.cpp

--- src/monattack.cpp
+++ src/monattack.cpp
@@ -13,8 +13,6 @@
 #include <set>
 #include <string>
 #include <utility>
-#include <vector>
-
 #include "avatar.h"
 #include "ballistics.h"
 #include "bodypart.h"
@@ -2774,7 +2772,7 @@ bool mattack::fear_paralyze( monster *z )
         } else if( rng( 0, 20 ) > g->u.get_int() ) {
             add_msg( m_bad, _( "The terrifying visage of the %s paralyzes you." ), z->name() );
             g->u.add_effect( effect_fearparalyze, 5_turns );
-            g->u.moves = 4 * g->u.get_speed();
+            g->u.moves -= 4 * g->u.get_speed();
         } else {
             add_msg( _( "You manage to avoid staring at the horrendous %s." ), z->name() );
         }
