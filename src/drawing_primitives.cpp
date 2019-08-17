4b6fe47aca34019a89ab7d14facc04d27639f91b
John Bytheway <jbytheway@gmail.com>
2019-08-16 21:03:41 -0400

    drawing_primitives functions now take point arguments
    Had to add some more map overloads for this.

--- src/drawing_primitives.cpp
+++ src/drawing_primitives.cpp
@@ -8,16 +8,16 @@
 #include "rng.h"
 #include "point.h"
 
-void draw_line( std::function<void( const int, const int )>set, const point &p1, const point &p2 )
+void draw_line( std::function<void( const point & )>set, const point &p1, const point &p2 )
 {
     std::vector<point> line = line_to( p1, p2, 0 );
     for( auto &i : line ) {
-        set( i.x, i.y );
+        set( i );
     }
-    set( p1.x, p1.y );
+    set( p1 );
 }
 
-void draw_square( std::function<void( const int, const int )>set, point p1, point p2 )
+void draw_square( std::function<void( const point & )>set, point p1, point p2 )
 {
     if( p1.x > p2.x ) {
         std::swap( p1.x, p2.x );
@@ -27,39 +27,39 @@ void draw_square( std::function<void( const int, const int )>set, point p1, poin
     }
     for( int x = p1.x; x <= p2.x; x++ ) {
         for( int y = p1.y; y <= p2.y; y++ ) {
-            set( x, y );
+            set( point( x, y ) );
         }
     }
 }
 
-void draw_rough_circle( std::function<void( const int, const int )>set, const point &p, int rad )
+void draw_rough_circle( std::function<void( const point & )>set, const point &p, int rad )
 {
     for( int i = p.x - rad; i <= p.x + rad; i++ ) {
         for( int j = p.y - rad; j <= p.y + rad; j++ ) {
             if( trig_dist( p.x, p.y, i, j ) + rng( 0, 3 ) <= rad ) {
-                set( i, j );
+                set( point( i, j ) );
             }
         }
     }
 }
 
-void draw_circle( std::function<void( const int, const int )>set, const rl_vec2d &p, double rad )
+void draw_circle( std::function<void( const point & )>set, const rl_vec2d &p, double rad )
 {
     for( int i = p.x - rad - 1; i <= p.x + rad + 1; i++ ) {
         for( int j = p.y - rad - 1; j <= p.y + rad + 1; j++ ) {
             if( ( p.x - i ) * ( p.x - i ) + ( p.y - j ) * ( p.y - j ) <= rad * rad ) {
-                set( i, j );
+                set( point( i, j ) );
             }
         }
     }
 }
 
-void draw_circle( std::function<void( const int, const int )>set, const point &p, int rad )
+void draw_circle( std::function<void( const point & )>set, const point &p, int rad )
 {
     for( int i = p.x - rad; i <= p.x + rad; i++ ) {
         for( int j = p.y - rad; j <= p.y + rad; j++ ) {
             if( trig_dist( p.x, p.y, i, j ) <= rad ) {
-                set( i, j );
+                set( point( i, j ) );
             }
         }
     }
--- src/drawing_primitives.h
+++ src/drawing_primitives.h
@@ -7,14 +7,14 @@
 struct point;
 struct rl_vec2d;
 
-void draw_line( std::function<void( const int, const int )>set, const point &p1, const point &p2 );
+void draw_line( std::function<void( const point & )>set, const point &p1, const point &p2 );
 
-void draw_square( std::function<void( const int, const int )>set, point p1, point p2 );
+void draw_square( std::function<void( const point & )>set, point p1, point p2 );
 
-void draw_rough_circle( std::function<void( const int, const int )>set, const point &p, int rad );
+void draw_rough_circle( std::function<void( const point & )>set, const point &p, int rad );
 
-void draw_circle( std::function<void( const int, const int )>set, const rl_vec2d &p, double rad );
+void draw_circle( std::function<void( const point & )>set, const rl_vec2d &p, double rad );
 
-void draw_circle( std::function<void( const int, const int )>set, const point &p, int rad );
+void draw_circle( std::function<void( const point & )>set, const point &p, int rad );
 
 #endif
