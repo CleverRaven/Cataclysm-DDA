1f21e48b7fc1069f95b341126a24753ed144ba89
John Bytheway <jbytheway@gmail.com>
2019-08-17 18:58:31 -0400

    Use float division when assigning to float
    This is a follow-up to changes in #32983 and #33209.

--- src/game.cpp
+++ src/game.cpp
@@ -9051,7 +9051,7 @@ bool game::walk_move( const tripoint &dest_loc )
             return false;
         }
         const double base_moves = u.run_cost( mcost, diag ) * 100.0 / crit->get_speed();
-        const double encumb_moves = u.get_weight() / 4800_gram;
+        const double encumb_moves = u.get_weight() / 4800.0_gram;
         u.moves -= static_cast<int>( ceil( base_moves + encumb_moves ) );
         if( u.movement_mode_is( PMM_WALK ) ) {
             crit->use_mech_power( -2 );
