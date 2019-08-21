5bbe0e078da6f1f9064cbb64dbc93be17d0fe84e
Qrox <qrox@sina.com>
2019-08-21 18:25:18 +0800

    Suppress warning when nothing translatable is found in objects copying from other objects

--- lang/extract_json_strings.py
+++ lang/extract_json_strings.py
@@ -983,7 +983,7 @@ def extract(item, infilename):
     if "footsteps" in item:
        writestr(outfile, item["footsteps"], **kwargs)
        wrote = True
-    if not wrote:
+    if not wrote and not "copy-from" in item:
         if not warning_supressed(infilename):
             print("WARNING: {}: nothing translatable found in item: {}".format(infilename, item))
 
