4b4e3f4ea45b185ae841e3a17f6e200bedb7a58e
alkemann <alek.morland@gmail.com>
2019-08-24 01:14:34 +0200

    Minor astyle fix

--- src/magic.cpp
+++ src/magic.cpp
@@ -199,7 +199,7 @@ void spell_type::load( JsonObject &jo, const std::string & )
     mandatory( jo, was_loaded, "id", id );
     mandatory( jo, was_loaded, "name", name );
     mandatory( jo, was_loaded, "description", description );
-    translation default_msg("You cast %s!");
+    translation default_msg( "You cast %s!" );
     optional( jo, was_loaded, "message", message, default_msg );
     mandatory( jo, was_loaded, "effect", effect_name );
     const auto found_effect = effect_map.find( effect_name );
