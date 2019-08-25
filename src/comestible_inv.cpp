c749bafe2fa489eb41d8233cd5b0e6d43e4fc2f6
Alexey <alexeydk@hotmail.com>
2019-08-25 09:12:12 -0400

    astyle

--- src/comestible_inv.cpp
+++ src/comestible_inv.cpp
@@ -206,7 +206,7 @@ void comestible_inventory::init()
         pane.default_sortby = COLUMN_EXPIRES;
     } else {
         pane.special_filter = []( const item & it ) {
-            return !g->u.can_consume( it ) || g->u.get_acquirable_energy(it) <= 0;
+            return !g->u.can_consume( it ) || g->u.get_acquirable_energy( it ) <= 0;
         };
         pane.title = g->u.bionic_at_index( uistate.comestible_save.bio ).id.obj().name;
         pane.default_sortby = COLUMN_ENERGY;
