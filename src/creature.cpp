517e0f21f1a0284e24325c0bc8b4f9932bbfcb80
SourSlime <firnenyt@gmail.com>
2019-08-26 05:53:53 +1000

    Forgot something

--- src/creature.cpp
+++ src/creature.cpp
@@ -210,7 +210,6 @@ bool Creature::sees( const Creature &critter ) const
     } else if( ( wanted_range > (perception / 3) && critter.has_flag( MF_NIGHT_INVISIBILITY ) && g->m.light_at( critter.pos() ) <= LL_LOW ) ) {
         return false;
     } else if( ( wanted_range > 1 && critter.digging() ) ||
-               ( critter.has_flag( MF_NIGHT_INVISIBILITY ) && g->m.light_at( critter.pos() ) <= LL_LOW ) ||
                ( critter.is_underwater() && !is_underwater() && g->m.is_divable( critter.pos() ) ) ||
                ( g->m.has_flag_ter_or_furn( TFLAG_HIDE_PLACE, critter.pos() ) &&
                  !( abs( posx() - critter.posx() ) <= 1 && abs( posy() - critter.posy() ) <= 1 &&
