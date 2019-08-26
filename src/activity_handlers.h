74203f678271a714b355a69f5e30a30048d625f8
David Brown <davidpwbrown@gmail.com>
2019-08-27 00:16:14 +0200

    clang-tidy satisfaction - not my code I think, but still remove a const from declaration

--- src/activity_handlers.h
+++ src/activity_handlers.h
@@ -38,7 +38,7 @@ void activity_on_turn_move_loot( player_activity &act, player &p );
 void activity_on_turn_blueprint_move( player_activity &, player &p );
 void activity_on_turn_pickup();
 void activity_on_turn_wear( player_activity &act, player &p );
-void try_fuel_fire( player_activity &act, player &p, const bool starting_fire = false );
+void try_fuel_fire( player_activity &act, player &p, bool starting_fire = false );
 
 enum class item_drop_reason {
     deliberate,
