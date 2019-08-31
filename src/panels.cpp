ecff996ae76aaafd20aa6746757778c8dadc08f5
alkemann <alek.morland@gmail.com>
2019-08-31 15:41:35 +0200

    Fixed the labels panel for needs to have room for everything

--- src/panels.cpp
+++ src/panels.cpp
@@ -1427,7 +1427,7 @@ static void draw_needs_narrow( const avatar &u, const catacurses::window &w )
     wrefresh( w );
 }
 
-static void draw_needs_wide( const avatar &u, const catacurses::window &w )
+static void draw_needs_labels( const avatar &u, const catacurses::window &w )
 {
     werase( w );
     std::pair<std::string, nc_color> hunger_pair = u.get_hunger_description();
@@ -1436,19 +1436,21 @@ static void draw_needs_wide( const avatar &u, const catacurses::window &w )
     std::pair<nc_color, std::string> temp_pair = temp_stat( u );
     std::pair<std::string, nc_color> pain_pair = u.get_pain_description();
     // NOLINTNEXTLINE(cata-use-named-point-constants)
-    mvwprintz( w, point( 1, 0 ), c_light_gray, _( "Rest :" ) );
-    mvwprintz( w, point( 16, 0 ), c_light_gray, _( "Pain :" ) );
-    mvwprintz( w, point( 31, 0 ), c_light_gray, _( "Heat :" ) );
+    mvwprintz( w, point( 1, 0 ), c_light_gray, _( "Pain :" ) );
+    mvwprintz( w, point( 8, 0 ), pain_pair.second, pain_pair.first );
+    mvwprintz( w, point( 23, 0 ), c_light_gray, _( "Drink:" ) );
+    mvwprintz( w, point( 30, 0 ), thirst_pair.second, thirst_pair.first );
+
     // NOLINTNEXTLINE(cata-use-named-point-constants)
-    mvwprintz( w, point( 1, 1 ), c_light_gray, _( "Food :" ) );
-    mvwprintz( w, point( 23, 1 ), c_light_gray, _( "Drink:" ) );
-    mvwprintz( w, point( 8, 0 ), rest_pair.second, rest_pair.first );
-    mvwprintz( w, point( 23, 0 ), pain_pair.second, pain_pair.first );
-    mvwprintz( w, point( 38, 0 ), temp_pair.first, temp_pair.second );
-    mvwprintz( w, point( 8, 1 ), hunger_pair.second, hunger_pair.first );
-    mvwprintz( w, point( 30, 1 ), thirst_pair.second, thirst_pair.first );
+    mvwprintz( w, point( 1, 1 ), c_light_gray, _( "Rest :" ) );
+    mvwprintz( w, point( 8, 1 ), rest_pair.second, rest_pair.first );
+    mvwprintz( w, point( 23, 1 ), c_light_gray, _( "Food :" ) );
+    mvwprintz( w, point( 30, 1 ), hunger_pair.second, hunger_pair.first );
+    mvwprintz( w, point( 1, 2 ), c_light_gray, _( "Heat :" ) );
+    mvwprintz( w, point( 8, 2 ), temp_pair.first, temp_pair.second );
     wrefresh( w );
 }
+
 static void draw_env_compact( avatar &u, const catacurses::window &w )
 {
     werase( w );
@@ -2040,7 +2042,7 @@ static std::vector<window_panel> initialize_default_label_panels()
     ret.emplace_back( window_panel( draw_wind_padding, translate_marker( "Wind" ), 1, 44, false ) );
     ret.emplace_back( window_panel( draw_loc_wide, translate_marker( "Location Alt" ), 5, 44, false ) );
     ret.emplace_back( window_panel( draw_weapon_labels, translate_marker( "Weapon" ), 2, 44, true ) );
-    ret.emplace_back( window_panel( draw_needs_wide, translate_marker( "Needs" ), 2, 44, true ) );
+    ret.emplace_back( window_panel( draw_needs_labels, translate_marker( "Needs" ), 3, 44, true ) );
     ret.emplace_back( window_panel( draw_messages, translate_marker( "Log" ), -2, 44, true ) );
     ret.emplace_back( window_panel( draw_moon_wide, translate_marker( "Moon" ), 1, 44, false ) );
     ret.emplace_back( window_panel( draw_armor_padding, translate_marker( "Armor" ), 5, 44, false ) );
