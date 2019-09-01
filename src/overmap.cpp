2659978c19279af27e19a0d8d5474da043dd85c2
John Candlebury <johncandlebury@gmail.com>
2019-08-31 23:28:50 -0600

    Allow opening depot doors

--- src/overmap.cpp
+++ src/overmap.cpp
@@ -1492,7 +1492,7 @@ bool overmap::generate_sub( const int z )
             oter_id oter_ground = ter( tripoint( p.xy(), 0 ) );
             //oter_id oter_sewer = ter(i, j, -1);
             //oter_id oter_underground = ter(i, j, -2);
-            
+
             if( is_ot_match( "microlab_sub_connector", ter( p ), ot_match_type::type ) ) {
                 om_direction::type rotation = ter( p )->get_dir();
                 ter( p ) = oter_id( "subway_end_north" )->get_rotated( rotation );;
