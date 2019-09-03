b4ae872639c3d550fb0e5622e3f75a99dd5ba092
Fris0uman <sthapplz@gmail.com>
2019-09-03 08:37:45 +0200

    doc

--- src/bionics.h
+++ src/bionics.h
@@ -88,7 +88,7 @@ struct bionic_data {
     float fuel_efficiency;
     /**If true this bionic emits heat when producing power*/
     bool exothermic_power_gen = false;
-    /**If true this bionic emits smoke when producing power*/
+    /**Type of field emitted by this bionic when it produces energy*/
     emit_id power_gen_emission = emit_id::NULL_ID();
     /**Amount of environemental protection offered by this bionic*/
     std::map<body_part, size_t> env_protec;
