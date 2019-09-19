9bd3fb5aff608c1f42781aa93f7f8eb875afc4e8
Jeremy <jeremylshannon@gmail.com>
2019-09-19 10:31:17 -0700

    Update src/artifact.cpp
    Co-Authored-By: ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>

--- src/artifact.cpp
+++ src/artifact.cpp
@@ -1285,7 +1285,6 @@ void it_artifact_armor::deserialize( JsonObject &jo )
     armor->encumber = jo.get_int( "encumber" );
     // Old saves don't have max_encumber, so set it to base encumbrance value
     armor->max_encumber = jo.get_int( "max_encumber", armor->encumber );
-    if( jo.has_int( "max_encumber" ) ) {
         armor->max_encumber = jo.get_int( "max_encumber" );
     } else {
         armor->max_encumber = armor->encumber;
