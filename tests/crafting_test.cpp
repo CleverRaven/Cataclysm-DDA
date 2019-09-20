9b784a0edafde3c895f267d9a5194f82b7b63cb0
David Brown <davidpwbrown@gmail.com>
2019-09-20 17:01:06 +0200

    Revert "fix for crafting test"
    This reverts commit d2c7aacfe1bf39e005ac6a3a304c401b10d6c290.

--- tests/crafting_test.cpp
+++ tests/crafting_test.cpp
@@ -155,10 +155,7 @@ TEST_CASE( "available_recipes", "[recipes]" )
         REQUIRE_FALSE( dummy.knows_recipe( r ) );
 
         WHEN( "the player read it and has an appropriate skill" ) {
-            player *pl = dynamic_cast<player *>( &dummy );
-            if( pl ){
-                dummy.do_read( cookbook );
-            }
+            dummy.do_read( cookbook );
             dummy.set_skill_level( r->skill_used, 2 );
 
             AND_WHEN( "he searches for the recipe in the book" ) {
