2da3f5e9bfbc8a5a7e91e2d4ef0be7c81e2f6023
davidpwbrown <39344466+davidpwbrown@users.noreply.github.com>
2019-09-21 18:25:35 +0200

    Fix npc death of last member removing your_followers faction entirely (#34150)
    * fix npc death of last member removing your_followers faction entirely
    
    * putting iterator back to the way it was
    
    * extend no-delete clause to all templated factions.
    
    * revert pickup.cpp code back to how it was
    
    * redundant assignment

--- src/faction.cpp
+++ src/faction.cpp
@@ -124,6 +124,13 @@ void faction::remove_member( const character_id &guy_id )
         }
     }
     if( members.empty() ) {
+        for( const faction_template &elem : npc_factions::all_templates ) {
+            // This is a templated base faction - dont delete it, just leave it as zero members for now.
+            // Only want to delete dynamically created factions.
+            if( elem.id == id ) {
+                return;
+            }
+        }
         g->faction_manager_ptr->remove_faction( id );
     }
 }
