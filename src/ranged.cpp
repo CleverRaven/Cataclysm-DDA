2f3c9ac6351f33aa82ca412f3b2e19f4df8a42c2
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2019-09-26 23:54:34 +0300

    Check for EMP ammo effect just as for LASER and PLASMA

--- src/ranged.cpp
+++ src/ranged.cpp
@@ -232,8 +232,8 @@ bool player::handle_gun_damage( item &it )
         // Default chance is 1/10000 unless set via json, damage is proportional to caliber(see below).
         // Can be toned down with 'consume_divisor.'
 
-    } else if( it.has_flag( "CONSUMABLE" )  && !curammo_effects.count( "LASER" ) &&
-               !curammo_effects.count( "PLASMA" ) ) {
+    } else if( it.has_flag( "CONSUMABLE" ) && !curammo_effects.count( "LASER" ) &&
+               !curammo_effects.count( "PLASMA" ) && !curammo_effects.count( "EMP" ) ) {
         int uncork = ( ( 10 * it.ammo_data()->ammo->loudness )
                        + ( it.ammo_data()->ammo->recoil / 2 ) ) / 100;
         uncork = std::pow( uncork, 3 ) * 6.5;
