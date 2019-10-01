a8dfd5986cc25918d0ef422d1737cec5a57671ab
Ckpyt <ckpyt@bk.ru>
2019-10-02 03:29:28 +1300

    Heavy mining mod added. Add a mining car. (#34229)
    * Heavy mining mod added. Add possibility to mine from a car. Add a mining car too, but it has some bugs with rotation.

--- src/vehicle_use.cpp
+++ src/vehicle_use.cpp
@@ -240,6 +240,7 @@ void vehicle::set_electronics_menu_options( std::vector<uilist_entry> &options,
     add_toggle( _( "reaper" ), keybind( "TOGGLE_REAPER" ), "REAPER" );
     add_toggle( _( "planter" ), keybind( "TOGGLE_PLANTER" ), "PLANTER" );
     add_toggle( _( "rockwheel" ), keybind( "TOGGLE_PLOW" ), "ROCKWHEEL" );
+    add_toggle( _( "rockhead" ), keybind( "TOGGLE_PLOW" ), "ROADHEAD" );
     add_toggle( _( "scoop" ), keybind( "TOGGLE_SCOOP" ), "SCOOP" );
     add_toggle( _( "water purifier" ), keybind( "TOGGLE_WATER_PURIFIER" ), "WATER_PURIFIER" );
 
@@ -1091,6 +1092,40 @@ void vehicle::play_chimes()
                        _( "a simple melody blaring from the loudspeakers." ), false, "vehicle", "chimes" );
     }
 }
+
+void vehicle::crash_terrain_around()
+{
+    if( total_power_w() <= 0 ) {
+        return;
+    }
+    for( const vpart_reference &vp : get_enabled_parts( "CRASH_TERRAIN_AROUND" ) ) {
+        tripoint crush_target( 0, 0, -OVERMAP_LAYERS );
+        const tripoint start_pos = vp.pos();
+        const transform_terrain_data &ttd = vp.info().transform_terrain;
+        for( size_t i = 0; i < eight_horizontal_neighbors.size() &&
+             !g->m.inbounds_z( crush_target.z ); i++ ) {
+            tripoint cur_pos = start_pos + eight_horizontal_neighbors.at( i );
+            bool busy_pos = false;
+            for( const vpart_reference &vp_tmp : get_all_parts() ) {
+                busy_pos |= vp_tmp.pos() == cur_pos;
+            }
+            for( const std::string &flag : ttd.pre_flags ) {
+                if( g->m.has_flag( flag, cur_pos ) && !busy_pos ) {
+                    crush_target = cur_pos;
+                    break;
+                }
+            }
+        }
+        if( g->m.inbounds_z( crush_target.z ) ) { //target chosen
+            velocity = 0;
+            cruise_velocity = 0;
+            g->m.destroy( crush_target );
+            sounds::sound( crush_target, 500, sounds::sound_t::combat, _( "Clanggggg!" ), false,
+                           "smash_success", "hit_vehicle" );
+        }
+    }
+}
+
 
 void vehicle::transform_terrain()
 {
