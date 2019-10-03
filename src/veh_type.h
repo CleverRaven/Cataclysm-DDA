762298ac09bf50e09039d6ffad23b533257f87b5
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2019-10-03 09:41:06 +0300

    Update veh_type.h

--- src/veh_type.h
+++ src/veh_type.h
@@ -279,7 +279,6 @@ class vpart_info
         transform_terrain_data transform_terrain;
 
         /*Comfort data for sleeping in vehicles*/
-
         int comfort = 0;
         int floor_bedding_warmth = 0;
         int bonus_fire_warmth_feet = 300;
