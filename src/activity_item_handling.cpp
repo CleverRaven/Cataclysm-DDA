67ab5406b1e57c453dd696688131f02a4b36ccef
Kybe <rkay94@gmail.com>
2019-10-07 02:24:31 -0400

    Populate coords for activity ACT_PICKUP with player position at the start of the activity, then bail out of the activity if player pos is different at the start of activity_on_turn_pickup()

--- src/activity_item_handling.cpp
+++ src/activity_item_handling.cpp
@@ -668,6 +668,13 @@ void activity_on_turn_pickup()
         return;
     }
 
+    // If the player moves while picking up (ie: in a moving vehicle) cancel the activity, only populate coords when grabbing from the ground
+    if (g->u.activity.coords.size() > 0 && g->u.activity.coords.at(0) != g->u.pos())
+    {
+        g->u.cancel_activity();
+        return;
+    }
+
     // Auto_resume implies autopickup.
     const bool autopickup = g->u.activity.auto_resume;
 
