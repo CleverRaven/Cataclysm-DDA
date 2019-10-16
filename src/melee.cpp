9690ede12c88a0b49387ee6e466e87cec63ba901
Ramza13 <52087122+Ramza13@users.noreply.github.com>
2019-10-15 23:15:45 -0400

    Astyle

--- src/melee.cpp
+++ src/melee.cpp
@@ -469,7 +469,7 @@ void player::melee_attack( Creature &t, bool allow_special, const matec_id &forc
         const bool seen = g->u.sees( t );
         // Start of attacks.
         damage_instance d;
-        roll_all_damage( critical_hit, d, false, cur_weapon );        
+        roll_all_damage( critical_hit, d, false, cur_weapon );
 
         const bool has_force_technique = !force_technique.str().empty();
 
@@ -485,9 +485,9 @@ void player::melee_attack( Creature &t, bool allow_special, const matec_id &forc
 
         // if you have two broken arms you aren't doing any martial arts
         // and your hits are not going to hurt very much
-        if (get_working_arm_count() < 1) {
+        if( get_working_arm_count() < 1 ) {
             technique_id = tec_none;
-            d.mult_damage(.1);
+            d.mult_damage( .1 );
         }
 
         const ma_technique &technique = technique_id.obj();
