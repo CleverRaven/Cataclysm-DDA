c75cf5de9f4746d6c4a804e49bb025b429cbb9d4
Anton Burmistrov <Night_Pryanik@mail.ru>
2019-10-16 09:57:36 +0300

    Allow auto targeting mode for turrets only with installed turret control unit (#34202)
    * Added turret control unit
    
    * Made it craftable (autolearn)
    
    * Added corresponding vehicle part
    
    * Added flag to bitflags set
    
    * Added documentation
    
    * Forcibly set turrets' targeting mode to manual if no turret control unit is present on turret's tile on loading save
    
    * Allow changing turrets' targeting mode only if turret control unit is present
    
    Also reworded targeting mode options a bit to reduce confusing.
    Also removed a check for MANUAL flag as there is no items or vehicle parts with this flag.
    Also reworded reason of why you can't aim automatic turrets and added a missing period.
    
    * Turret controls must be installed on a turret
    
    Also, on removing turret controls forcibly set targeting mode of affected turret to manual.
    
    * Set turret control unit's state equal to turret's targeting mode
    
    * Increased control unit power draw
    
    * Added a requirement for laptop computer
    
    Also inreased craft time to 2 hours.
    
    * Added mechanics skill req for crafting TCU
    
    Also increased its power consumption from 100W to 250W.

--- src/savegame_json.cpp
+++ src/savegame_json.cpp
@@ -2514,6 +2514,15 @@ void vehicle::deserialize( JsonIn &jsin )
 
     for( const vpart_reference &vp : get_any_parts( "TURRET" ) ) {
         install_part( vp.mount(), vpart_id( "turret_mount" ), false );
+
+        //Forcibly set turrets' targeting mode to manual if no turret control unit is present on turret's tile on loading save
+        if( !has_part( global_part_pos3( vp.part() ), "TURRET_CONTROLS" ) ) {
+            vp.part().enabled = false;
+        }
+        //Set turret control unit's state equal to turret's targeting mode on loading save
+        for( const vpart_reference &turret_part : get_any_parts( "TURRET_CONTROLS" ) ) {
+            turret_part.part().enabled = vp.part().enabled;
+        }
     }
 
     // Add vehicle mounts to cars that are missing them.
