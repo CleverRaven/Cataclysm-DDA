0bfc279d8c3357c6f84fcc980801cba991c06b03
davidpwbrown <39344466+davidpwbrown@users.noreply.github.com>
2019-10-21 00:56:03 +0100

    Update src/savegame_json.cpp
    Co-Authored-By: Curtis Merrill <curtis.r.merrill@gmail.com>

--- src/savegame_json.cpp
+++ src/savegame_json.cpp
@@ -304,7 +304,7 @@ void requirement_data::serialize( JsonOut &json ) const
 
     if( !is_null() ) {
         json.member( "blacklisted", blacklisted );
-        std::vector<std::vector<item_comp>> req_comps = get_components();
+        const std::vector<std::vector<item_comp>> req_comps = get_components();
         std::vector<std::vector<tool_comp>> tool_comps = get_tools();
         std::vector<std::vector<quality_requirement>> quality_comps = get_qualities();
 
