8b4e5b9cb14f346f4794683fdfa92f45618624a9
Karthas077 <paranoidandroid077@gmail.com>
2019-10-22 16:35:38 -0700

    Astyle

--- src/savegame.cpp
+++ src/savegame.cpp
@@ -793,9 +793,9 @@ void overmap::convert_terrain( const std::unordered_map<tripoint, std::string> &
         } else if( old == "megastore_entrance" ) {
             const std::string megastore = "megastore";
             const std::string megastore_entrance = "megastore_entrance";
-            const auto ter_test_n = needs_conversion.find( pos + point(  0, -2 ) );
-            const auto ter_test_s = needs_conversion.find( pos + point(  0,  2 ) );
-            const auto ter_test_e = needs_conversion.find( pos + point(  2,  0 ) );
+            const auto ter_test_n = needs_conversion.find( pos + point( 0, -2 ) );
+            const auto ter_test_s = needs_conversion.find( pos + point( 0,  2 ) );
+            const auto ter_test_e = needs_conversion.find( pos + point( 2,  0 ) );
             const auto ter_test_w = needs_conversion.find( pos + point( -2,  0 ) );
             //North
             if( ter_test_n != needs_conversion.end() && ter_test_n->second == megastore ) {
@@ -819,15 +819,15 @@ void overmap::convert_terrain( const std::unordered_map<tripoint, std::string> &
                 ter_set( pos + point(  0,  2 ), oter_id( megastore + "_1_0_0_south" ) );
                 ter_set( pos + point(  1,  2 ), oter_id( megastore + "_0_0_0_south" ) );
             } else if( ter_test_e != needs_conversion.end() && ter_test_e->second == megastore ) {
-                ter_set( pos + point(  0, -1 ), oter_id( megastore + "_0_2_0_east" ) );
-                ter_set( pos + point(  1, -1 ), oter_id( megastore + "_0_1_0_east" ) );
-                ter_set( pos + point(  2, -1 ), oter_id( megastore + "_0_0_0_east" ) );
-                ter_set( pos + point(  0,  0 ), oter_id( megastore + "_1_2_0_east" ) );
-                ter_set( pos + point(  1,  0 ), oter_id( megastore + "_1_1_0_east" ) );
-                ter_set( pos + point(  2,  0 ), oter_id( megastore + "_1_0_0_east" ) );
-                ter_set( pos + point(  0,  1 ), oter_id( megastore + "_2_2_0_east" ) );
-                ter_set( pos + point(  1,  1 ), oter_id( megastore + "_2_1_0_east" ) );
-                ter_set( pos + point(  2,  1 ), oter_id( megastore + "_2_0_0_east" ) );
+                ter_set( pos + point( 0, -1 ), oter_id( megastore + "_0_2_0_east" ) );
+                ter_set( pos + point( 1, -1 ), oter_id( megastore + "_0_1_0_east" ) );
+                ter_set( pos + point( 2, -1 ), oter_id( megastore + "_0_0_0_east" ) );
+                ter_set( pos + point( 0,  0 ), oter_id( megastore + "_1_2_0_east" ) );
+                ter_set( pos + point( 1,  0 ), oter_id( megastore + "_1_1_0_east" ) );
+                ter_set( pos + point( 2,  0 ), oter_id( megastore + "_1_0_0_east" ) );
+                ter_set( pos + point( 0,  1 ), oter_id( megastore + "_2_2_0_east" ) );
+                ter_set( pos + point( 1,  1 ), oter_id( megastore + "_2_1_0_east" ) );
+                ter_set( pos + point( 2,  1 ), oter_id( megastore + "_2_0_0_east" ) );
             } else if( ter_test_w != needs_conversion.end() && ter_test_w->second == megastore ) {
                 ter_set( pos + point( -2, -1 ), oter_id( megastore + "_2_0_0_west" ) );
                 ter_set( pos + point( -1, -1 ), oter_id( megastore + "_2_1_0_west" ) );
