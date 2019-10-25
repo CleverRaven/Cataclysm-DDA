abb2001c1fc9c7211ad4173982c9936e98b4f412
Hymore246 <Hymore246@users.noreply.github.com>
2019-10-24 21:15:52 -0500

    Cleaned up valid_weapon check

--- src/martialarts.cpp
+++ src/martialarts.cpp
@@ -407,13 +407,19 @@ bool ma_requirements::is_valid_player( const player &u ) const
     bool cqb = u.has_active_bionic( bionic_id( "bio_cqb" ) );
     // There are 4 different cases of "armedness":
     // Truly unarmed, unarmed weapon, style-allowed weapon, generic weapon
-    bool valid_unarmed = ( !u.style_selected.obj().strictly_melee && unarmed_allowed &&
-        ( !u.is_armed() || ( u.is_armed() && u.used_weapon().has_flag( "UNARMED_WEAPON" ) &&
-            unarmed_weapons_allowed ) ) );
+    bool melee_style = u.style_selected.obj().strictly_melee;
+    bool is_armed = u.is_armed();
+    bool unarmed_weapon = u.used_weapon().has_flag( "UNARMED_WEAPON" );
+    bool forced_unarmed = u.style_selected.obj().force_unarmed;
+    bool ok_weapon = is_valid_weapon( u.weapon );
+    bool style_weapon = u.style_selected.obj().has_weapon( u.weapon.typeId() );
+    bool all_weapons = u.style_selected.obj().allow_melee;
 
-    bool valid_melee = !strictly_unarmed && ( u.style_selected.obj().force_unarmed || ( melee_allowed &&
-        is_valid_weapon( u.weapon ) && ( u.style_selected.obj().has_weapon( u.weapon.typeId() ) ||
-            u.style_selected.obj().allow_melee ) ) );
+    bool valid_unarmed = !melee_style && unarmed_allowed && ( !is_armed || ( is_armed &&
+        unarmed_weapon && unarmed_weapons_allowed ) );
+
+    bool valid_melee = !strictly_unarmed && ( forced_unarmed || ( melee_allowed && ok_weapon &&
+        ( style_weapon || all_weapons ) ) );
 
     bool valid_weapon = valid_unarmed || valid_melee;
 
