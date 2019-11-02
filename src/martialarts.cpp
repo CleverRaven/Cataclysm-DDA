57fe56d7d2d04d28c40ebfad2af565d13f9a6fb9
Hymore246 <Hymore246@users.noreply.github.com>
2019-11-02 07:55:03 -0500

    and again

--- src/martialarts.cpp
+++ src/martialarts.cpp
@@ -486,7 +486,7 @@ std::string ma_requirements::get_description( bool buff ) const
                                ngettext( "Skill", "Skills", min_skill.size() ) );
 
         dump << enumerate_as_string( min_skill.begin(),
-        min_skill.end(), []( const std::pair<skill_id, int> &pr ) {
+        min_skill.end(), []( const std::pair<skill_id, int>  &pr ) {
             return string_format( "%s: <stat>%d</stat>", pr.first->name(), pr.second );
         }, enumeration_conjunction::none ) << std::endl;
     }
@@ -498,7 +498,7 @@ std::string ma_requirements::get_description( bool buff ) const
             "<bold>Damage types required: </bold>", min_damage.size() );
 
         dump << enumerate_as_string( min_damage.begin(),
-        min_damage.end(), []( const std::pair<damage_type, int> &pr ) {
+        min_damage.end(), []( const std::pair<damage_type, int>  &pr ) {
             return string_format( _( "%s: <stat>%d</stat>" ), name_by_dt( pr.first ), pr.second );
         }, enumeration_conjunction::none ) << std::endl;
     }
