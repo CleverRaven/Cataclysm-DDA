b2dbaf00db55da16bad20fcdec3d79706d4037a1
misterprimus <45959491+misterprimus@users.noreply.github.com>
2019-11-07 23:31:56 -0500

    Update src/player_activity.h
    Co-Authored-By: Jianxiang Wang (王健翔) <qrox@sina.com>

--- src/player_activity.h
+++ src/player_activity.h
@@ -42,7 +42,6 @@ class player_activity
         int position;
         /** An activity specific value. */
         std::string name;
-        std::string flags;
         std::vector<item_location> targets;
         std::vector<int> values;
         std::vector<std::string> str_values;
