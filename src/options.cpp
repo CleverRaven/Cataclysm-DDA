6a9c494af0e566615afc7c8e05e9f2ea31852d58
Anton Burmistrov <Night_Pryanik@mail.ru>
2019-11-16 19:04:33 +0400

    Make AIM width customizable (#35521)
    * Make AIM width customizable
    
    * Special case for really low resolutions
    
    * Rewrote description

--- src/options.cpp
+++ src/options.cpp
@@ -1491,6 +1491,11 @@ void options_manager::add_options_interface()
     "Vertical"
        );
 
+    add( "AIM_WIDTH", "interface", translate_marker( "Full screen Advanced Inventory Manager" ),
+        translate_marker( "If true, Advanced Inventory Manager menu will fit full screen, otherwise it will leave sidebar visible." ),
+        false
+    );
+
     mOptionsSort["interface"]++;
 
     add( "MOVE_VIEW_OFFSET", "interface", translate_marker( "Move view offset" ),
