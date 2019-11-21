d576c3305117f7bd603cafc80b41b245c1376145
KorGgenT <curtis.r.merrill@gmail.com>
2019-11-21 10:05:29 -0500

    Update avatar.h

--- src/avatar.h
+++ src/avatar.h
@@ -177,8 +177,6 @@ class avatar : public player
 
         bool wield( item &target ) override;
 
-        void use_item( item_location &loc ); // Use item; also tries E,R,W  'a'
-        void use_item();
         using Character::invoke_item;
         bool invoke_item( item *, const tripoint &pt ) override;
         bool invoke_item( item * ) override;
