d0c4f8e00ab00457706d34fc4800e87b799a62aa
dpwb <davidpwbrown@gmail.com>
2019-11-28 15:51:18 +0000

    wind and rain
    reduce drench wetness
    
    remove snow forming form light drizzle
    
    fallthrough fix
    
    fallthrough fix
    
    use time since epoch for rng seed

--- src/handle_action.cpp
+++ src/handle_action.cpp
@@ -195,7 +195,7 @@ input_context game::get_player_input( std::string &action )
                 Location to add rain drop animation bits! Since it refreshes w_terrain it can be added to the animation section easily
                 Get tile information from above's weather information:
                 WEATHER_ACID_DRIZZLE | WEATHER_ACID_RAIN = "weather_acid_drop"
-                WEATHER_DRIZZLE | WEATHER_RAINY | WEATHER_THUNDER | WEATHER_LIGHTNING = "weather_rain_drop"
+                WEATHER_DRIZZLE | WEATHER_LIGHT_DRIZZLE | WEATHER_RAINY | WEATHER_THUNDER | WEATHER_LIGHTNING = "weather_rain_drop"
                 WEATHER_FLURRIES | WEATHER_SNOW | WEATHER_SNOWSTORM = "weather_snowflake"
                 */
 
