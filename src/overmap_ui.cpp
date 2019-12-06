a7bc0617b3026f680a080e0bd2e4b751564ee45b
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2019-12-06 09:56:23 +0300

    Appease clang

--- src/overmap_ui.cpp
+++ src/overmap_ui.cpp
@@ -1095,7 +1095,7 @@ static bool search( tripoint &curs, const tripoint &orig, const bool show_explor
                        .title( _( "Search term:" ) )
                        // NOLINTNEXTLINE(cata-text-style): literal comma
                        .description( string_format(
-                                         _( "Multiple entries separated with , Excludes starting with -\n"
+                                         _( "Multiple entries separated with comma (,).  Excludes starting with hyphen (-)\n"
                                             "Current search radius is %d.  It can be changed in options." ),
                                          radius ) )
                        .query_string();
