f242d65d51b124ff434972c173837067fd177cd5
congq <congq@umich.edu>
2019-12-10 19:33:44 -0500

    handle liquid in-progress crafting

--- src/pickup.cpp
+++ src/pickup.cpp
@@ -270,10 +270,9 @@ bool pick_one_up( item_location &loc, int quantity, bool &got_water, bool &offer
         if( !( got_water = !( u.crush_frozen_liquid( newloc ) ) ) ) {
             option = STASH;
         }
-    } else if( newit.made_of_from_type( LIQUID ) && !newit.is_frozen_liquid() ) {
+    } else if( newit.made_of_from_type( LIQUID ) && !newit.is_frozen_liquid() ||
+               newit.is_craft() && newit.made_of( LIQUID ) ) {
         got_water = true;
-    } else if( newit.is_craft() && newit.made_of( LIQUID )) {
-        got_water = true; //Handles in progress liquid craft
     } else if( !u.can_pickWeight( newit, false ) ) {
         if( !autopickup ) {
             const std::string &explain = string_format( _( "The %s is too heavy!" ),
