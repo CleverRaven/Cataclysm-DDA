deb994eb9d27955ea9407e7710343b98432c0725
Ramza13 <52087122+Ramza13@users.noreply.github.com>
2019-12-12 07:50:39 -0500

    Astyle

--- src/options.cpp
+++ src/options.cpp
@@ -1140,7 +1140,8 @@ void options_manager::add_options_general()
          false
        );
 
-    add( "DANGEROUS_TERRAIN_WARNING_PROMPT", "general", translate_marker( "Dangerous terrain warning prompt" ),
+    add( "DANGEROUS_TERRAIN_WARNING_PROMPT", "general",
+         translate_marker( "Dangerous terrain warning prompt" ),
          translate_marker( "Always: You will be prompted to move onto dangerous tiles.  Running: You will only be able to move onto dangerous tiles while running and will be prompted.  Crouching: You will only be able to move onto a dangerous tile while crouching and will be prompted.  Never:  You will not be able to move onto a dangerous tile unless running and will not be warned or prompted." ),
     { { "ALWAYS", to_translation( "Always" ) }, { "RUNNING", translate_marker( "Running" ) }, { "CROUCHING", translate_marker( "Crouching" ) }, { "NEVER", translate_marker( "Never" ) } },
     "RUNNING"
