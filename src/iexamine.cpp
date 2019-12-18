3657051c41951422fc5a1eea4953363f10c0f285
Fris0uman <sthapplz@gmail.com>
2019-12-18 20:28:45 +0100

    has_flag_furn

--- src/iexamine.cpp
+++ src/iexamine.cpp
@@ -4242,7 +4242,7 @@ static player &player_on_couch( player &p, const tripoint &autodoc_loc, player &
                                 bool &adjacent_couch, tripoint &couch_pos )
 {
     for( const auto &couch_loc : g->m.points_in_radius( autodoc_loc, 1 ) ) {
-        if( g->m.furn( couch_loc )->has_flag( "AUTODOC_COUCH" ) ) {
+        if( g->m.has_flag_furn( "AUTODOC_COUCH", couch_loc ) ) {
             adjacent_couch = true;
             couch_pos = couch_loc;
             if( p.pos() == couch_loc ) {
@@ -4262,7 +4262,7 @@ static Character &operator_present( Character &p, const tripoint &autodoc_loc,
                                     Character &null_patient )
 {
     for( const auto &loc : g->m.points_in_radius( autodoc_loc, 1 ) ) {
-        if( !g->m.furn( loc )->has_flag("AUTODOC_COUCH")) {
+        if( !g->m.has_flag_furn( "AUTODOC_COUCH", loc ) ) {
             if( p.pos() == loc ) {
                 return p;
             }
