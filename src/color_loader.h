040ae761772e3ec4d5cd36380c1dd936ee2bf58d
BevapDin <tho_ki@gmx.de>
2019-12-27 13:29:23 +0100

    Fix spelling mistake. Thanks for pointing that out.
    Co-Authored-By: ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>

--- src/color_loader.h
+++ src/color_loader.h
@@ -71,7 +71,7 @@ class color_loader
             try {
                 load_colorfile( custom_path );
             } catch( const JsonError &err ) {
-                debgmsg( "Failed to load color data from \"%s\": %s", custom_path, err.what() );
+                debugmsg( "Failed to load color data from \"%s\": %s", custom_path, err.what() );
 
                 // this should succeed, otherwise the installation is botched
                 load_colorfile( default_path );
