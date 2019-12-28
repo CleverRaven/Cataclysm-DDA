8ced1f557e676fffab686739b7f9a92a778b3a36
Nikolay Ekaterinin <nikolai_e@rambler.ru>
2019-12-28 14:09:51 +0300

    Editing pickup.cpp 6

--- src/pickup.cpp
+++ src/pickup.cpp
@@ -568,8 +568,8 @@ void Pickup::pick_up( const tripoint &p, int min, from_where get_items_from )
         int pickupW = 44;
 
         //find max length of item name and resize pickup window width
-        for( size_t cur_it = 0; cur_it < stacked_here.size(); cur_it++ ) {
-            const item &this_item = *stacked_here[cur_it].front();
+        for( const std::list<item_stack::iterator> &cur_list : stacked_here ) {
+            const item &this_item = *cur_list.front();
             int item_len = utf8_width( remove_color_tags( this_item.display_name() ) ) + 10;
             if( item_len > pickupW && item_len < TERMX ) {
                 pickupW = item_len;
