8532b6262fdab272e3f890e0908a225640c7a1b5
Ramza13 <52087122+Ramza13@users.noreply.github.com>
2020-01-07 12:15:40 -0500

    Properly fix build

--- src/safemode_ui.h
+++ src/safemode_ui.h
@@ -33,9 +33,9 @@ class safemode
                 std::string rule;
                 bool active;
                 bool whitelist;
+                Creature::Attitude attitude;
                 int proximity;
                 Categories category;
-                Creature::Attitude attitude;                
 
                 rules_class() : active( false ), whitelist( false ), attitude( Creature::A_HOSTILE ),
                     proximity( 0 ), category( Categories::HOSTILE ) {}
