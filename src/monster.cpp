54c9fe6ae74110994c57520b24a90751e516fed6
Derik Baumgartner <dbaumgartner21@gmail.com>
2020-01-09 15:33:19 -0600

    Fix compile error

--- src/monster.cpp
+++ src/monster.cpp
@@ -351,7 +351,7 @@ void monster::try_upgrade( bool pin_time )
         return;
     }
 
-    const int current_day = to_days<int>( calendar::turn );
+    const int current_day = to_days<int>( calendar::turn - calendar::turn_zero );
     //This should only occur when a monster is created or upgraded to a new form
     if( upgrade_time < 0 ) {
         upgrade_time = next_upgrade_time();
