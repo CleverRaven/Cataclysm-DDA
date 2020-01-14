9950e16818fdf084fa951d36f0b23eb6f67f31d5
Saint of Grey <42332565+Saint-of-Grey@users.noreply.github.com>
2020-01-14 14:49:47 -0800

    Ranged balance tests have archery in them now

--- tests/ranged_balance.cpp
+++ tests/ranged_balance.cpp
@@ -273,6 +273,11 @@ TEST_CASE( "unskilled_shooter_accuracy", "[ranged] [balance]" )
         test_shooting_scenario( shooter, 5, 9, 25 );
         test_fast_shooting( shooter, 80, 0.2 );
     }
+    SECTION("an unskilled shooter with an inaccurate bow") {
+        arm_shooter(shooter, "selfbow");
+        test_shooting_scenario(shooter, 4, 5, 17);
+        test_fast_shooting(shooter, 50, 0.2);
+    }
 }
 
 TEST_CASE( "competent_shooter_accuracy", "[ranged] [balance]" )
@@ -302,6 +307,11 @@ TEST_CASE( "competent_shooter_accuracy", "[ranged] [balance]" )
         test_shooting_scenario( shooter, 10, 22, 48 );
         test_fast_shooting( shooter, 85, 0.3 );
     }
+    SECTION("a skilled shooter with an accurate bow") {
+        arm_shooter(shooter, "recurbow", { "bow_sight_pin" });
+        test_shooting_scenario(shooter, 12, 18, 40);
+        test_fast_shooting(shooter, 50, 0.4);
+    }
 }
 
 TEST_CASE( "expert_shooter_accuracy", "[ranged] [balance]" )
@@ -330,6 +340,11 @@ TEST_CASE( "expert_shooter_accuracy", "[ranged] [balance]" )
         arm_shooter( shooter, "browning_blr", { "rifle_scope" } );
         test_shooting_scenario( shooter, 25, 60, 900 );
         test_fast_shooting( shooter, 100, 0.4 );
+    }
+    SECTION("an expert shooter with an excellent bow") {
+        arm_shooter(shooter, "compbow_high", { "bow_scope" });
+        test_shooting_scenario(shooter, 20, 60, 400);
+        test_fast_shooting(shooter, 40, 0.6);
     }
 }
 
