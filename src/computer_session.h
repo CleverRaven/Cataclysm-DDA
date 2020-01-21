2761f23b53fefa4e38681c20e6fc9b825f3d775c
Qrox <qrox@sina.com>
2020-01-21 13:28:41 +0800

    Split computer actions into functions

--- src/computer_session.h
+++ src/computer_session.h
@@ -2,6 +2,7 @@
 #ifndef COMPUTER_SESSION_H
 #define COMPUTER_SESSION_H
 
+#include <map>
 #include <vector>
 
 #include "computer.h"
@@ -75,7 +76,55 @@ class computer_session
         template<typename ...Args>
         bool query_any( const std::string &text, Args &&... args );
 
-        void mark_refugee_center();
+        void action_amigara_log();
+        void action_amigara_start();
+        void action_blood_anal();
+        void action_cascade();
+        void action_complete_disable_external_power();
+        void action_conveyor();
+        void action_data_anal();
+        void action_deactivate_shock_vent();
+        void action_disconnect();
+        void action_download_software();
+        void action_elevator_on();
+        void action_emerg_mess();
+        void action_emerg_ref_center();
+        void action_extract_rad_source();
+        void action_geiger();
+        void action_irradiator();
+        void action_list_bionics();
+        void action_lock();
+        void action_map_sewer();
+        void action_map_subway();
+        void action_maps();
+        void action_miss_disarm();
+        void action_open();
+        void action_open_disarm();
+        void action_portal();
+        void action_radio_archive();
+        void action_release();
+        void action_release_bionics();
+        void action_release_disarm();
+        void action_repeater_mod();
+        void action_research();
+        void action_sample();
+        void action_shutters();
+        void action_sr1_mess();
+        void action_sr2_mess();
+        void action_sr3_mess();
+        void action_sr4_mess();
+        void action_srcf_1_mess();
+        void action_srcf_2_mess();
+        void action_srcf_3_mess();
+        void action_srcf_elevator();
+        void action_srcf_seal();
+        void action_srcf_seal_order();
+        void action_terminate();
+        void action_toll();
+        void action_tower_unresponsive();
+        void action_unlock();
+        void action_unlock_disarm();
+        static const std::map<computer_action, void( computer_session::* )()> computer_action_functions;
 };
 
 #endif
