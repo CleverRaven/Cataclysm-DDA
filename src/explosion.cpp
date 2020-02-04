e63714230942cad6b25c6d2123c6c5ba6924b5f1
Brian-Otten <47374323+Brian-Otten@users.noreply.github.com>
2020-02-04 11:20:53 +0100

    Update explosion.cpp

--- src/explosion.cpp
+++ src/explosion.cpp
@@ -448,9 +448,9 @@ static std::vector<tripoint> shrapnel( const tripoint &src, int power,
         }
         if( g->m.impassable( target ) ) {
             if( optional_vpart_position vp = g->m.veh_at( target ) ) {
-                vp->vehicle().damage( vp->part_index(), damage / 50 );
+                vp->vehicle().damage( vp->part_index(), damage / 80 );
             } else {
-                g->m.bash( target, damage / 50, true );
+                g->m.bash( target, damage / 80, true );
             }
         }
     }
