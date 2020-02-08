77b27be65493a36161386cd019ac19c75dba04ff
Eric Pierce <wapcaplet88@gmail.com>
2020-02-07 19:21:28 -0700

    Enhance and clean up iteminfo tests
    Include price and material in basic info test; refactor armor_info tests
    into one test case with two sections. All passing.

--- tests/iteminfo_test.cpp
+++ tests/iteminfo_test.cpp
@@ -16,51 +16,53 @@ static void iteminfo_test( const item &i, const iteminfo_query &q, const std::st
     CHECK( info == reference );
 }
 
-TEST_CASE( "show basic item info", "[item][iteminfo]" )
+TEST_CASE( "item description and physical attributes", "[item][iteminfo]" )
 {
     iteminfo_query q( { iteminfo_parts::DESCRIPTION, iteminfo_parts::BASE_CATEGORY,
-                        iteminfo_parts::BASE_VOLUME, iteminfo_parts::BASE_WEIGHT } );
+                        iteminfo_parts::BASE_PRICE, iteminfo_parts::BASE_VOLUME,
+                        iteminfo_parts::BASE_WEIGHT, iteminfo_parts::BASE_MATERIAL } );
     iteminfo_test(
         item( "jug_plastic" ), q,
         "A standard plastic jug used for milk and household cleaning chemicals.\n"
         "--\n"
-        "Category: <color_c_magenta>CONTAINERS</color>"
+        "Category: <color_c_magenta>CONTAINERS</color>  Price: $<color_c_yellow>0.00</color>\n"
         "<color_c_white>Volume</color>: <color_c_yellow>3.750</color> L"
-        "  Weight: <color_c_yellow>0.42</color> lbs\n" );
+        "  Weight: <color_c_yellow>0.42</color> lbs\n"
+        "--\n"
+        "Material: <color_c_light_blue>Plastic</color>\n" );
+
 }
 
-TEST_CASE( "armor_info", "[item][iteminfo]" )
-{
-    // Just a generic typical piece of clothing
-    iteminfo_query q( { iteminfo_parts::ARMOR_BODYPARTS, iteminfo_parts::ARMOR_LAYER,
-                        iteminfo_parts::ARMOR_COVERAGE, iteminfo_parts::ARMOR_WARMTH,
-                        iteminfo_parts::ARMOR_ENCUMBRANCE, iteminfo_parts::ARMOR_PROTECTION
-                      } );
-    iteminfo_test(
-        item( "longshirt" ), q,
-        "--\n"
-        // NOLINTNEXTLINE(cata-text-style)
-        "Covers: The <color_c_cyan>torso</color>. The <color_c_cyan>arms</color>. \n"
-        // NOLINTNEXTLINE(cata-text-style)
-        "Layer: <color_c_light_blue>Normal</color>. \n"
-        "Coverage: <color_c_yellow>90</color>%  Warmth: <color_c_yellow>5</color>\n"
-        "--\n"
-        "<color_c_white>Encumbrance</color>: <color_c_yellow>3</color> <color_c_red>(poor fit)</color>\n"
-        "<color_c_white>Protection</color>: Bash: <color_c_yellow>1</color>  Cut: <color_c_yellow>1</color>\n"
-        "  Acid: <color_c_yellow>0</color>  Fire: <color_c_yellow>0</color>  Environmental: <color_c_yellow>0</color>\n" );
-}
-
-TEST_CASE( "if_covers_nothing_omit_irreelevant_info", "[item][iteminfo]" )
+TEST_CASE( "armor info", "[item][iteminfo]" )
 {
     iteminfo_query q( { iteminfo_parts::ARMOR_BODYPARTS, iteminfo_parts::ARMOR_LAYER,
                         iteminfo_parts::ARMOR_COVERAGE, iteminfo_parts::ARMOR_WARMTH,
                         iteminfo_parts::ARMOR_ENCUMBRANCE, iteminfo_parts::ARMOR_PROTECTION
                       } );
-    iteminfo_test(
-        item( "ear_plugs" ), q,
-        "--\n"
-        "Covers: <color_c_cyan>Nothing</color>.\n" );
+
+    SECTION( "shows coverage and protection values" ) {
+        iteminfo_test(
+            item( "longshirt" ), q,
+            "--\n"
+            // NOLINTNEXTLINE(cata-text-style)
+            "Covers: The <color_c_cyan>torso</color>. The <color_c_cyan>arms</color>. \n"
+            // NOLINTNEXTLINE(cata-text-style)
+            "Layer: <color_c_light_blue>Normal</color>. \n"
+            "Coverage: <color_c_yellow>90</color>%  Warmth: <color_c_yellow>5</color>\n"
+            "--\n"
+            "<color_c_white>Encumbrance</color>: <color_c_yellow>3</color> <color_c_red>(poor fit)</color>\n"
+            "<color_c_white>Protection</color>: Bash: <color_c_yellow>1</color>  Cut: <color_c_yellow>1</color>\n"
+            "  Acid: <color_c_yellow>0</color>  Fire: <color_c_yellow>0</color>  Environmental: <color_c_yellow>0</color>\n" );
+    }
+
+    SECTION( "omits irrelevant info if it covers nothing" ) {
+        iteminfo_test(
+            item( "ear_plugs" ), q,
+            "--\n"
+            "Covers: <color_c_cyan>Nothing</color>.\n" );
+    }
 }
+
 
 TEST_CASE( "gun_lists_default_ammo", "[item][iteminfo]" )
 {
@@ -113,7 +115,7 @@ TEST_CASE( "nutrient_ranges_for_recipe_exemplars", "[item][iteminfo]" )
         "Vitamin A (3-11%), Vitamin B12 (2-6%), and Vitamin C (1-85%)\n" );
 }
 
-TEST_CASE( "show item conductivity", "[item][iteminfo]" )
+TEST_CASE( "item conductivity", "[item][iteminfo]" )
 {
     iteminfo_query q( { iteminfo_parts::DESCRIPTION_CONDUCTIVITY } );
 
@@ -140,7 +142,7 @@ TEST_CASE( "show item conductivity", "[item][iteminfo]" )
     }
 }
 
-TEST_CASE( "show item qualities", "[item][iteminfo]" )
+TEST_CASE( "list of item qualities", "[item][iteminfo]" )
 {
     iteminfo_query q( { iteminfo_parts::QUALITIES } );
 
