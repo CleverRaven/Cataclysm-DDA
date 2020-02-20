f94e93b9b9b23f73e729190c5e7bd7568ac60989
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2020-02-21 01:44:53 +0300

    Update cata_string_consts.h

--- src/cata_string_consts.h
+++ src/cata_string_consts.h
@@ -921,7 +921,6 @@ static const mtype_id mon_zombie_technician( "mon_zombie_technician" );
 static const mtype_id mon_zombie_tough( "mon_zombie_tough" );
 static const mtype_id mon_zombie_waif( "mon_zombie_waif" );
 
-
 static const std::string flag_ACID( "ACID" );
 static const std::string flag_ACID_IMMUNE( "ACID_IMMUNE" );
 static const std::string flag_ACT_IN_FIRE( "ACT_IN_FIRE" );
@@ -1399,7 +1398,6 @@ static const std::string flag_WORKBENCH( "WORKBENCH" );
 static const std::string flag_WRITE_MESSAGE( "WRITE_MESSAGE" );
 static const std::string flag_YOUNG( "YOUNG" );
 static const std::string flag_ZOOM( "ZOOM" );
-
 
 static const skill_id skill_archery( "archery" );
 static const skill_id skill_barter( "barter" );
