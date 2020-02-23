0d061b0ead193545fbbab0ac0504f9a19dafb43e
Qrox <qrox@sina.com>
2020-02-23 11:24:20 +0800

    Remove reinitialize_framebuffer now that we refresh main menu properly using ui_manager

--- src/sdltiles.cpp
+++ src/sdltiles.cpp
@@ -3215,9 +3215,6 @@ static void CheckMessages()
         // on focus gain. This seems to mess up the first redraw and
         // causes black screen that lasts ~0.5 seconds before the screen
         // contents are redrawn in the following code.
-
-        // Main menu redraw
-        reinitialize_framebuffer();
         window_dimensions dim = get_window_dimensions( catacurses::stdscr );
         ui_manager::invalidate( rectangle( point_zero, dim.window_size_pixel ) );
         ui_manager::redraw();
