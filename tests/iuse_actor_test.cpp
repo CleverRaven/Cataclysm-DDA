c4fe905a845c316fca0cf9601c9bf70b69218cbe
Eric Pierce <wapcaplet88@gmail.com>
2020-03-01 16:23:12 -0700

    Move manhack test, replace eyedrops test
    Add underwater test to eyedrops, and remove old eyedrops test;
    the only other assertions it was doing had to do with `position_by_item`
    (part of inventory methods), so not really part of iuse tests.
    
    Manhack test was moved to iuse_actor_test.cpp since it appears to be
    handled by functions in iuse_actor.cpp.

---
+++ tests/iuse_actor_test.cpp
@@ -0,0 +1,65 @@
+#include <climits>
+#include <list>
+#include <memory>
+
+#include "catch/catch.hpp"
+#include "avatar.h"
+#include "bodypart.h"
+#include "game.h"
+#include "inventory.h"
+#include "monster.h"
+#include "player.h"
+
+static player &get_sanitized_player( )
+{
+    player &dummy = g->u;
+
+    // Remove first worn item until there are none left.
+    std::list<item> temp;
+    while( dummy.takeoff( dummy.i_at( -2 ), &temp ) );
+    dummy.inv.clear();
+    dummy.remove_weapon();
+
+    return dummy;
+}
+
+static monster *find_adjacent_monster( const tripoint &pos )
+{
+    tripoint target = pos;
+    for( target.x = pos.x - 1; target.x <= pos.x + 1; target.x++ ) {
+        for( target.y = pos.y - 1; target.y <= pos.y + 1; target.y++ ) {
+            if( target == pos ) {
+                continue;
+            }
+            if( monster *const candidate = g->critter_at<monster>( target ) ) {
+                return candidate;
+            }
+        }
+    }
+    return nullptr;
+}
+
+TEST_CASE( "manhack", "[iuse_actor][manhack]" )
+{
+    player &dummy = get_sanitized_player();
+
+    g->clear_zombies();
+    item &test_item = dummy.i_add( item( "bot_manhack", 0, item::default_charges_tag{} ) );
+
+    int test_item_pos = dummy.inv.position_by_item( &test_item );
+    REQUIRE( test_item_pos != INT_MIN );
+
+    monster *new_manhack = find_adjacent_monster( dummy.pos() );
+    REQUIRE( new_manhack == nullptr );
+
+    dummy.invoke_item( &test_item );
+
+    test_item_pos = dummy.inv.position_by_item( &test_item );
+    REQUIRE( test_item_pos == INT_MIN );
+
+    new_manhack = find_adjacent_monster( dummy.pos() );
+    REQUIRE( new_manhack != nullptr );
+    REQUIRE( new_manhack->type->id == mtype_id( "mon_manhack" ) );
+    g->clear_zombies();
+}
+
--- tests/iuse_test.cpp
+++ tests/iuse_test.cpp
@@ -1,69 +1,16 @@
-#include <climits>
-#include <list>
-#include <memory>
-
 #include "avatar.h"
-#include "cata_string_consts.h" // for flag_WET
+#include "bodypart.h"
+#include "cata_string_consts.h"
 #include "catch/catch.hpp"
 #include "game.h"
-#include "monster.h"
-#include "morale_types.h" // For MORALE_WET
-#include "mtype.h"
-#include "player.h"
-#include "bodypart.h"
-#include "calendar.h"
-#include "inventory.h"
 #include "item.h"
-#include "itype.h" // for efftype_id
+#include "itype.h"
+#include "morale_types.h"
+#include "point.h"
 #include "string_id.h"
 #include "type_id.h"
-#include "point.h"
 
-static player &get_sanitized_player( )
-{
-    player &dummy = g->u;
-
-    // Remove first worn item until there are none left.
-    std::list<item> temp;
-    while( dummy.takeoff( dummy.i_at( -2 ), &temp ) );
-    dummy.inv.clear();
-    dummy.remove_weapon();
-
-    return dummy;
-}
-
-TEST_CASE( "eyedrops1", "[iuse][eyedrops]" )
-{
-    player &dummy = get_sanitized_player();
-
-    item &test_item = dummy.i_add( item( "saline", 0, item::default_charges_tag{} ) );
-
-    REQUIRE( test_item.charges == 5 );
-
-    dummy.add_env_effect( efftype_id( "boomered" ), bp_eyes, 3, 12_turns );
-
-    item_location loc = item_location( dummy, &test_item );
-    REQUIRE( loc );
-    int test_item_pos = dummy.inv.position_by_item( &test_item );
-    REQUIRE( test_item_pos != INT_MIN );
-
-    dummy.consume( loc );
-
-    test_item_pos = dummy.inv.position_by_item( &test_item );
-    REQUIRE( test_item_pos != INT_MIN );
-    REQUIRE( test_item.charges == 4 );
-    REQUIRE( !dummy.has_effect( efftype_id( "boomered" ) ) );
-
-    dummy.consume( loc );
-    dummy.consume( loc );
-    dummy.consume( loc );
-    dummy.consume( loc );
-
-    test_item_pos = dummy.inv.position_by_item( &test_item );
-    REQUIRE( test_item_pos == INT_MIN );
-}
-
-TEST_CASE( "eyedrops2", "[iuse][eyedrops]" )
+TEST_CASE( "eyedrops", "[iuse][eyedrops]" )
 {
     avatar dummy;
 
@@ -87,46 +34,18 @@ TEST_CASE( "eyedrops2", "[iuse][eyedrops]" )
             }
         }
     }
-}
 
-static monster *find_adjacent_monster( const tripoint &pos )
-{
-    tripoint target = pos;
-    for( target.x = pos.x - 1; target.x <= pos.x + 1; target.x++ ) {
-        for( target.y = pos.y - 1; target.y <= pos.y + 1; target.y++ ) {
-            if( target == pos ) {
-                continue;
-            }
-            if( monster *const candidate = g->critter_at<monster>( target ) ) {
-                return candidate;
+    GIVEN( "player is underwater" ) {
+        dummy.set_underwater( true );
+
+        WHEN( "they try to use eye drops" ) {
+            bool used = dummy.invoke_item( &eyedrops );
+
+            THEN( "it can't be used underwater" ) {
+                CHECK_FALSE( used );
             }
         }
     }
-    return nullptr;
-}
-
-TEST_CASE( "manhack", "[iuse][manhack]" )
-{
-    player &dummy = get_sanitized_player();
-
-    g->clear_zombies();
-    item &test_item = dummy.i_add( item( "bot_manhack", 0, item::default_charges_tag{} ) );
-
-    int test_item_pos = dummy.inv.position_by_item( &test_item );
-    REQUIRE( test_item_pos != INT_MIN );
-
-    monster *new_manhack = find_adjacent_monster( dummy.pos() );
-    REQUIRE( new_manhack == nullptr );
-
-    dummy.invoke_item( &test_item );
-
-    test_item_pos = dummy.inv.position_by_item( &test_item );
-    REQUIRE( test_item_pos == INT_MIN );
-
-    new_manhack = find_adjacent_monster( dummy.pos() );
-    REQUIRE( new_manhack != nullptr );
-    REQUIRE( new_manhack->type->id == mtype_id( "mon_manhack" ) );
-    g->clear_zombies();
 }
 
 TEST_CASE( "antifungal", "[iuse][antifungal]" )
