a0483d6c76d3c812f326f1c0ea6be5f82b5abfb4
Kevin Granade <kevin.granade@gmail.com>
2020-03-01 02:53:51 +0000

    Move size handling to character.

--- src/character.cpp
+++ src/character.cpp
@@ -228,6 +228,19 @@ int Character::get_fat_to_hp() const
     return mut_fat_hp * ( get_bmi() - character_weight_category::normal );
 }
 
+m_size Character::get_size() const
+{
+    if( has_trait( trait_id( "SMALL2" ) ) || has_trait( trait_id( "SMALL_OK" ) ) ||
+        has_trait( trait_id( "SMALL" ) ) ) {
+        return MS_SMALL;
+    } else if( has_trait( trait_LARGE ) || has_trait( trait_LARGE_OK ) ) {
+        return MS_LARGE;
+    } else if( has_trait( trait_HUGE ) || has_trait( trait_HUGE_OK ) ) {
+        return MS_HUGE;
+    }
+    return MS_MEDIUM;
+}
+
 std::string Character::disp_name( bool possessive, bool capitalize_first ) const
 {
     if( !possessive ) {
