f09154600f0708c0231c92b4ac5bb95eb7295aca
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2020-03-02 17:18:29 +0300

    Remove empty lines

--- src/cata_string_consts.h
+++ src/cata_string_consts.h
@@ -5,7 +5,6 @@
 #include "type_id.h"
 #include "player_activity.h"
 
-
 static const activity_id ACT_ADV_INVENTORY( "ACT_ADV_INVENTORY" );
 static const activity_id ACT_AIM( "ACT_AIM" );
 static const activity_id ACT_ARMOR_LAYERS( "ACT_ARMOR_LAYERS" );
@@ -104,7 +103,6 @@ static const activity_id ACT_WAIT_STAMINA( "ACT_WAIT_STAMINA" );
 static const activity_id ACT_WAIT_WEATHER( "ACT_WAIT_WEATHER" );
 static const activity_id ACT_WASH( "ACT_WASH" );
 static const activity_id ACT_WEAR( "ACT_WEAR" );
-
 
 static const bionic_id bio_adrenaline( "bio_adrenaline" );
 static const bionic_id bio_ads( "bio_ads" );
@@ -924,7 +922,6 @@ static const mtype_id mon_zombie_swimmer( "mon_zombie_swimmer" );
 static const mtype_id mon_zombie_technician( "mon_zombie_technician" );
 static const mtype_id mon_zombie_tough( "mon_zombie_tough" );
 static const mtype_id mon_zombie_waif( "mon_zombie_waif" );
-
 
 static const std::string flag_ACID( "ACID" );
 static const std::string flag_ACT_IN_FIRE( "ACT_IN_FIRE" );
