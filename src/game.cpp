1cfe8ce1b2841f9856f5a53d71711e48629df711
KorGgenT <curtis.r.merrill@gmail.com>
2020-03-08 14:48:32 -0400

    create new item_location::item_in_container

--- src/game.cpp
+++ src/game.cpp
@@ -8636,6 +8636,11 @@ void game::wield( item_location &loc )
     loc.remove_item();
     if( !u.wield( to_wield ) ) {
         switch( location_type ) {
+            case item_location::type::container:
+                // this will not cause things to spill, as it is inside another item
+                loc = loc.obtain( g->u );
+                wield( loc );
+                break;
             case item_location::type::character:
                 if( worn_index != INT_MIN ) {
                     auto it = u.worn.begin();
