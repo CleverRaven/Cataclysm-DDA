e52d6b40226e942ff652f2d8e50cccfeff94841e
Eric Pierce <wapcaplet88@gmail.com>
2020-03-11 17:21:11 -0600

    Add tests for item health bar and weapon fouling

--- tests/item_tname_test.cpp
+++ tests/item_tname_test.cpp
@@ -262,8 +262,136 @@ TEST_CASE( "wheel diameter", "[item][tname][wheel]" )
     CHECK( wheel32.tname() == "32\" armored wheel" );
 }
 
+TEST_CASE( "item health or damage bar", "[item][tname][health][damage]" )
+{
+    GIVEN( "some clothing" ) {
+        item shirt( "longshirt" );
+        REQUIRE( shirt.is_armor() );
 
-// item health bar
+        // Ensure the health bar option is enabled
+        override_option opt( "ITEM_HEALTH_BAR", "true" );
+
+        // Damage bar uses a scale of 0 `||` to 4 `XX`, in increments of 25%
+        int dam25 = shirt.max_damage() / 4;
+
+        WHEN( "it is undamaged" ) {
+            shirt.set_damage( 0 );
+            REQUIRE( shirt.damage() == 0 );
+            REQUIRE( shirt.damage_level( 4 ) == 0 );
+
+            // green `||`
+            THEN( "it appears undamaged" ) {
+                CHECK( shirt.tname() == "<color_c_light_green>||\u00A0</color>long-sleeved shirt (poor fit)" );
+            }
+        }
+
+        WHEN( "is is one-quarter damaged" ) {
+            shirt.set_damage( dam25 );
+            REQUIRE( shirt.damage() == dam25 );
+            REQUIRE( shirt.damage_level( 4 ) == 1 );
+
+            // yellow `|\`
+            THEN( "it appears slightly damaged" ) {
+                CHECK( shirt.tname() == "<color_c_yellow>|\\\u00A0</color>long-sleeved shirt (poor fit)" );
+            }
+        }
+
+        WHEN( "it is half damaged" ) {
+            shirt.set_damage( dam25 * 2 );
+            REQUIRE( shirt.damage() == dam25 * 2 );
+            REQUIRE( shirt.damage_level( 4 ) == 2 );
+
+            // magenta `|.`
+            THEN( "it appears moderately damaged" ) {
+                CHECK( shirt.tname() == "<color_c_magenta>|.\u00A0</color>long-sleeved shirt (poor fit)" );
+            }
+        }
+
+        WHEN( "it is three-quarters damaged" ) {
+            shirt.set_damage( dam25 * 3 );
+            REQUIRE( shirt.damage() == dam25 * 3 );
+            REQUIRE( shirt.damage_level( 4 ) == 3 );
+
+            // red `\.`
+            THEN( "it appears heavily damaged" ) {
+                CHECK( shirt.tname() == "<color_c_light_red>\\.\u00A0</color>long-sleeved shirt (poor fit)" );
+            }
+        }
+
+        WHEN( "it is totally damaged" ) {
+            shirt.set_damage( dam25 * 4 );
+            REQUIRE( shirt.damage() == dam25 * 4 );
+            REQUIRE( shirt.damage_level( 4 ) == 4 );
+
+            // dark gray `XX`
+            THEN( "it appears almost destroyed" ) {
+                CHECK( shirt.tname() == "<color_c_dark_gray>XX\u00A0</color>long-sleeved shirt (poor fit)" );
+            }
+        }
+    }
+
+    GIVEN( "ITEM_HEALTH_BAR option is disabled" ) {
+        override_option opt( "ITEM_HEALTH_BAR", "false" );
+
+        THEN( "clothing health bars are hidden" ) {
+            item shirt( "longshirt" );
+            REQUIRE( shirt.is_armor() );
+
+            CHECK( shirt.tname() == "long-sleeved shirt (poor fit)" );
+        }
+    }
+}
+
+TEST_CASE( "weapon fouling", "[item][tname][fouling][dirt]" )
+{
+
+    GIVEN( "a gun with potential fouling" ) {
+        item gun( "hk_mp5" );
+
+        WHEN( "it is perfectly clean" ) {
+            gun.set_var( "dirt", 0 );
+            CHECK( gun.tname() == "H&K MP5A2" );
+        }
+
+        WHEN( "it is fouled" ) {
+            gun.faults.insert( fault_gun_dirt );
+            REQUIRE( gun.has_fault( fault_gun_dirt ) );
+
+            // Max dirt is 10,000
+
+            THEN( "minimal fouling is not indicated" ) {
+                gun.set_var( "dirt", 1000 );
+                CHECK( gun.tname() == "H&K MP5A2" );
+            }
+            // U+2581 'Lower one eighth block'
+            THEN( "20%% fouling is indicated with a thin white bar" ) {
+                gun.set_var( "dirt", 2000 );
+                CHECK( gun.tname() == "<color_white>\u2581</color>H&K MP5A2" );
+            }
+            // U+2583 'Lower three eighths block'
+            THEN( "40%% fouling is indicated with a slight gray bar" ) {
+                gun.set_var( "dirt", 4000 );
+                CHECK( gun.tname() == "<color_light_gray>\u2583</color>H&K MP5A2" );
+            }
+            // U+2585 'Lower five eighths block'
+            THEN( "60%% fouling is indicated with a medium gray bar" ) {
+                gun.set_var( "dirt", 6000 );
+                CHECK( gun.tname() == "<color_light_gray>\u2585</color>H&K MP5A2" );
+            }
+            // U+2585 'Lower seven eighths block'
+            THEN( "80%% fouling is indicated with a tall dark gray bar" ) {
+                gun.set_var( "dirt", 8000 );
+                CHECK( gun.tname() == "<color_dark_gray>\u2587</color>H&K MP5A2" );
+            }
+            // U+2588 'Full block'
+            THEN( "100%% fouling is indicated with a full brown bar" ) {
+                gun.set_var( "dirt", 10000 );
+                CHECK( gun.tname() == "<color_brown>\u2588</color>H&K MP5A2" );
+            }
+        }
+    }
+}
+
 
 // clothing with +1 suffix
 
@@ -272,7 +400,6 @@ TEST_CASE( "wheel diameter", "[item][tname][wheel]" )
 // is_bionic: (sterile), (packed)
 
 // is_tool, UPS: (UPS)
-
 
 // tname tests to consider:
 // - indicates gun fouling level
@@ -294,7 +421,6 @@ TEST_CASE( "wheel diameter", "[item][tname][wheel]" )
 //   - (old)
 //   - (fresh)
 
-//
 // RADIO_MOD (RBG)
 //
 // misc flags: used, lit, plugged in, active, sawn-off
@@ -307,4 +433,5 @@ TEST_CASE( "wheel diameter", "[item][tname][wheel]" )
 //   - (too small)
 //   - (tiny!)
 //   - (poor fit)
-//
+
+
