1b55c8fa0ca93b80d4799856167119002de2e189
Curtis Merrill <curtis.r.merrill@gmail.com>
2020-04-03 19:40:09 -0400

    rearrange spellcasting_finish (#39226)

--- src/activity_handlers.cpp
+++ src/activity_handlers.cpp
@@ -4755,27 +4755,23 @@ static void blood_magic( player *p, int cost )
     p->mod_pain( std::max( 1, cost / 3 ) );
 }
 
-static void player_or_item_spell( player *p, const spell_id &sp, int level, spell &casting )
-{
-    // if level is -1 then we know it is a player spell
-    if( level == -1 ) {
-        casting = p->magic.get_spell( sp );
-        // so we return early
-        return;
-    }
-    // otherwise we build the spell from the id and level
-    casting = spell( sp );
-    while( casting.get_level() < level && !casting.is_max_level() ) {
-        casting.gain_level();
-    }
-}
-
 void activity_handlers::spellcasting_finish( player_activity *act, player *p )
 {
     act->set_to_null();
     const int level_override = act->get_value( 0 );
-    spell spell_being_cast;
-    player_or_item_spell( p, spell_id( act->name ), level_override, spell_being_cast );
+    spell_id sp( act->name );
+
+    // if level is -1 then we know it's a player spell, otherwise we build it from the ground up
+    spell temp_spell( sp );
+    spell &spell_being_cast = ( level_override == -1 ) ? p->magic.get_spell( sp ) : temp_spell;
+
+    // if level != 1 then we need to set the spell's level
+    if( level_override != -1 ) {
+        while( spell_being_cast.get_level() < level_override && !spell_being_cast.is_max_level() ) {
+            spell_being_cast.gain_level();
+        }
+    }
+
     const bool no_fail = act->get_value( 1 ) == 1;
     const bool no_mana = act->get_value( 2 ) == 0;
 
