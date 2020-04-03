d4fc561bf86a319d0e38aa35d8f693d82f44eadd
Eric Pierce <wapcaplet88@gmail.com>
2020-04-02 20:09:22 -0600

    Use CHECK_FALSE/REQUIRE_FALSE where appropriate

--- tests/npc_talk_test.cpp
+++ tests/npc_talk_test.cpp
@@ -592,46 +592,46 @@ TEST_CASE( "npc_talk_items", "[npc_talk]" )
     d.add_topic( "TALK_TEST_EFFECTS" );
     gen_response_lines( d, 19 );
     // add and remove effect
-    REQUIRE( !g->u.has_effect( effect_infection ) );
+    REQUIRE_FALSE( g->u.has_effect( effect_infection ) );
     talk_effect_t &effects = d.responses[1].success;
     effects.apply( d );
     CHECK( g->u.has_effect( effect_infection ) );
     CHECK( g->u.get_effect_dur( effect_infection ) == time_duration::from_turns( 10 ) );
-    REQUIRE( !talker_npc.has_effect( effect_infection ) );
+    REQUIRE_FALSE( talker_npc.has_effect( effect_infection ) );
     effects = d.responses[2].success;
     effects.apply( d );
     CHECK( talker_npc.has_effect( effect_infection ) );
     CHECK( talker_npc.get_effect( effect_infection ).is_permanent() );
     effects = d.responses[3].success;
     effects.apply( d );
-    CHECK( !g->u.has_effect( effect_infection ) );
+    CHECK_FALSE( g->u.has_effect( effect_infection ) );
     effects = d.responses[4].success;
     effects.apply( d );
-    CHECK( !talker_npc.has_effect( effect_infection ) );
+    CHECK_FALSE( talker_npc.has_effect( effect_infection ) );
     // add and remove trait
-    REQUIRE( !g->u.has_trait( trait_PROF_FED ) );
+    REQUIRE_FALSE( g->u.has_trait( trait_PROF_FED ) );
     effects = d.responses[5].success;
     effects.apply( d );
     CHECK( g->u.has_trait( trait_PROF_FED ) );
-    REQUIRE( !talker_npc.has_trait( trait_PROF_FED ) );
+    REQUIRE_FALSE( talker_npc.has_trait( trait_PROF_FED ) );
     effects = d.responses[6].success;
     effects.apply( d );
     CHECK( talker_npc.has_trait( trait_PROF_FED ) );
     effects = d.responses[7].success;
     effects.apply( d );
-    CHECK( !g->u.has_trait( trait_PROF_FED ) );
+    CHECK_FALSE( g->u.has_trait( trait_PROF_FED ) );
     effects = d.responses[8].success;
     effects.apply( d );
-    CHECK( !talker_npc.has_trait( trait_PROF_FED ) );
+    CHECK_FALSE( talker_npc.has_trait( trait_PROF_FED ) );
     // buying and spending
     talker_npc.op_of_u.owed = 1000;
-    REQUIRE( !has_beer_bottle( g->u, 2 ) );
+    REQUIRE_FALSE( has_beer_bottle( g->u, 2 ) );
     REQUIRE( talker_npc.op_of_u.owed == 1000 );
     effects = d.responses[9].success;
     effects.apply( d );
     CHECK( talker_npc.op_of_u.owed == 500 );
     CHECK( has_beer_bottle( g->u, 2 ) );
-    REQUIRE( !has_item( g->u, "bottle_plastic", 1 ) );
+    REQUIRE_FALSE( has_item( g->u, "bottle_plastic", 1 ) );
     effects = d.responses[10].success;
     effects.apply( d );
     CHECK( has_item( g->u, "bottle_plastic", 1 ) );
@@ -641,10 +641,10 @@ TEST_CASE( "npc_talk_items", "[npc_talk]" )
     CHECK( talker_npc.op_of_u.owed == 0 );
     talker_npc.op_of_u.owed = 1000;
     // effect chains
-    REQUIRE( !g->u.has_effect( effect_infected ) );
-    REQUIRE( !talker_npc.has_effect( effect_infected ) );
-    REQUIRE( !g->u.has_trait( trait_PROF_SWAT ) );
-    REQUIRE( !talker_npc.has_trait( trait_PROF_SWAT ) );
+    REQUIRE_FALSE( g->u.has_effect( effect_infected ) );
+    REQUIRE_FALSE( talker_npc.has_effect( effect_infected ) );
+    REQUIRE_FALSE( g->u.has_trait( trait_PROF_SWAT ) );
+    REQUIRE_FALSE( talker_npc.has_trait( trait_PROF_SWAT ) );
     effects = d.responses[12].success;
     effects.apply( d );
     CHECK( g->u.has_effect( effect_infected ) );
@@ -657,11 +657,11 @@ TEST_CASE( "npc_talk_items", "[npc_talk]" )
     CHECK( talker_npc.get_attitude() == NPCATT_KILL );
     // opinion changes
     talker_npc.op_of_u = npc_opinion();
-    REQUIRE( !talker_npc.op_of_u.trust );
-    REQUIRE( !talker_npc.op_of_u.fear );
-    REQUIRE( !talker_npc.op_of_u.value );
-    REQUIRE( !talker_npc.op_of_u.anger );
-    REQUIRE( !talker_npc.op_of_u.owed );
+    REQUIRE_FALSE( talker_npc.op_of_u.trust );
+    REQUIRE_FALSE( talker_npc.op_of_u.fear );
+    REQUIRE_FALSE( talker_npc.op_of_u.value );
+    REQUIRE_FALSE( talker_npc.op_of_u.anger );
+    REQUIRE_FALSE( talker_npc.op_of_u.owed );
     effects = d.responses[13].success;
     REQUIRE( effects.opinion.trust == 10 );
     effects.apply( d );
@@ -708,13 +708,13 @@ TEST_CASE( "npc_talk_items", "[npc_talk]" )
     REQUIRE( g->u.wield( g->u.i_at( g->u.inv.position_by_type( "bottle_glass" ) ) ) );
     effects = d.responses[14].success;
     effects.apply( d );
-    CHECK( !has_item( g->u, "bottle_plastic", 1 ) );
-    CHECK( !has_item( g->u, "beer", 1 ) );
+    CHECK_FALSE( has_item( g->u, "bottle_plastic", 1 ) );
+    CHECK_FALSE( has_item( g->u, "beer", 1 ) );
     CHECK( has_item( talker_npc, "bottle_plastic", 1 ) );
     CHECK( has_item( talker_npc, "beer", 2 ) );
     effects = d.responses[15].success;
     effects.apply( d );
-    CHECK( !has_item( talker_npc, "beer", 2 ) );
+    CHECK_FALSE( has_item( talker_npc, "beer", 2 ) );
     CHECK( has_item( talker_npc, "beer", 1 ) );
     effects = d.responses[16].success;
     effects.apply( d );
@@ -722,7 +722,7 @@ TEST_CASE( "npc_talk_items", "[npc_talk]" )
     effects = d.responses[17].success;
     effects.apply( d );
     CHECK( has_item( g->u, "beer", 0 ) );
-    CHECK( !has_item( g->u, "beer", 1 ) );
+    CHECK_FALSE( has_item( g->u, "beer", 1 ) );
 }
 
 TEST_CASE( "npc_talk_combat_commands", "[npc_talk]" )
