ff79a9ac6731f1400a75eec35b568fb1b87c2615
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2020-04-04 16:47:29 +0300

    Combined variable declaration and assignment

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -5872,14 +5872,12 @@ bool vehicle::tow_cable_too_far() const
         debugmsg( "checking tow cable length on a vehicle that has no towing vehicle" );
         return false;
     }
-    tripoint towing_point;
-    tripoint towed_point;
     int index = get_tow_part();
     if( index == -1 ) {
         debugmsg( "towing data exists but no towing part" );
         return false;
     }
-    towing_point = g->m.getabs( global_part_pos3( index ) );
+    tripoint towing_point = g->m.getabs( global_part_pos3( index ) );
     if( !tow_data.get_towed_by()->tow_data.get_towed() ) {
         debugmsg( "vehicle %s has data for a towing vehicle, but that towing vehicle does not have %s listed as towed",
                   disp_name(), disp_name() );
@@ -5890,7 +5888,7 @@ bool vehicle::tow_cable_too_far() const
         debugmsg( "towing data exists but no towing part" );
         return false;
     }
-    towed_point = g->m.getabs( tow_data.get_towed_by()->global_part_pos3( other_index ) );
+    tripoint towed_point = g->m.getabs( tow_data.get_towed_by()->global_part_pos3( other_index ) );
     if( towing_point == tripoint_zero || towed_point == tripoint_zero ) {
         debugmsg( "towing data exists but no towing part" );
         return false;
@@ -5905,14 +5903,12 @@ bool vehicle::no_towing_slack() const
     if( !tow_data.get_towed() ) {
         return false;
     }
-    tripoint towing_point;
-    tripoint towed_point;
     int index = get_tow_part();
     if( index == -1 ) {
         debugmsg( "towing data exists but no towing part" );
         return false;
     }
-    towing_point = g->m.getabs( global_part_pos3( index ) );
+    tripoint towing_point = g->m.getabs( global_part_pos3( index ) );
     if( !tow_data.get_towed()->tow_data.get_towed_by() ) {
         debugmsg( "vehicle %s has data for a towed vehicle, but that towed vehicle does not have %s listed as tower",
                   disp_name(), disp_name() );
@@ -5923,7 +5919,7 @@ bool vehicle::no_towing_slack() const
         debugmsg( "towing data exists but no towing part" );
         return false;
     }
-    towed_point = g->m.getabs( tow_data.get_towed()->global_part_pos3( other_index ) );
+    tripoint towed_point = g->m.getabs( tow_data.get_towed()->global_part_pos3( other_index ) );
     if( towing_point == tripoint_zero || towed_point == tripoint_zero ) {
         debugmsg( "towing data exists but no towing part" );
         return false;
