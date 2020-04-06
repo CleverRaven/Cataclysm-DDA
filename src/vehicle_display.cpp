4f526274177dd3b42c211554660f79af93c7cdf4
Mark Langsdorf <mark.langsdorf@gmail.com>
2020-04-06 11:24:36 -0500

    vehicles: check that the return of part_displayed_at exists
    part_id_string() calls part_displayed_at() but doesn't check that
    the return value is valid, even though part_displayed_at can return
    negative values.  Check for an invalid return and handle it.

--- src/vehicle_display.cpp
+++ src/vehicle_display.cpp
@@ -47,8 +47,9 @@ char vehicle::part_sym( const int p, const bool exact ) const
     }
 }
 
-// similar to part_sym(int p) but for use when drawing SDL tiles. Called only by cata_tiles during draw_vpart
-// vector returns at least 1 element, max of 2 elements. If 2 elements the second denotes if it is open or damaged
+// similar to part_sym(int p) but for use when drawing SDL tiles. Called only by cata_tiles
+// during draw_vpart vector returns at least 1 element, max of 2 elements. If 2 elements the
+// second denotes if it is open or damaged
 vpart_id vehicle::part_id_string( const int p, char &part_mod ) const
 {
     part_mod = 0;
@@ -57,6 +58,11 @@ vpart_id vehicle::part_id_string( const int p, char &part_mod ) const
     }
 
     int displayed_part = part_displayed_at( parts[p].mount );
+    if( displayed_part < 0 || displayed_part >= static_cast<int>( parts.size() ) ||
+        parts[ displayed_part ].removed ) {
+        return vpart_id::NULL_ID();
+    }
+
     const vpart_id idinfo = parts[displayed_part].id;
 
     if( part_flag( displayed_part, VPFLAG_OPENABLE ) && parts[displayed_part].open ) {
