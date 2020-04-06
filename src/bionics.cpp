87bdd84fab9780dad59f112633e30540bdf83034
Kevin Granade <kevin.granade@gmail.com>
2020-04-06 18:35:19 +0000

    Update src/bionics.cpp

--- src/bionics.cpp
+++ src/bionics.cpp
@@ -2118,20 +2118,11 @@ bool Character::can_install_bionics( const itype &type, player &installer, bool 
 
 float Character::env_surgery_bonus( int radius )
 {
-    float bonus = 0.0;
-    std::vector<float> bonus_all;
+    float bonus = 1.0;
     for( const tripoint &cell : g->m.points_in_radius( pos(), radius ) ) {
         if( g->m.furn( cell )->surgery_skill_multiplier ) {
-            bonus_all.emplace_back( *g->m.furn( cell )->surgery_skill_multiplier );
+            bonus = std::max( bonus, g->m.furn( cell )->surgery_skill_multiplier );
         }
-    }
-    for( const float bn : bonus_all ) {
-        if( bn > bonus ) {
-            bonus = bn;
-        }
-    }
-    if( bonus_all.empty() ) {
-        bonus = 1.0;
     }
     return bonus;
 }
