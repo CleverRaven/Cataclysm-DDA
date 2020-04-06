6db60985c9184d76be232e9659ede643abcaeaed
roloa <marshkip@gmail.com>
2020-04-06 16:37:17 +0900

    強化車載ヒーターを移植

--- src/field_type.cpp
+++ src/field_type.cpp
@@ -355,6 +355,7 @@ field_type_id fd_null,
               fd_hot_air2,
               fd_hot_air3,
               fd_hot_air4,
+              fd_hot_air_vehicle_heater,
               fd_fungicidal_gas,
               fd_insecticidal_gas,
               fd_smoke_vent,
@@ -412,6 +413,7 @@ void field_types::set_field_type_ids()
     fd_hot_air2 = field_type_id( "fd_hot_air2" );
     fd_hot_air3 = field_type_id( "fd_hot_air3" );
     fd_hot_air4 = field_type_id( "fd_hot_air4" );
+    fd_hot_air_vehicle_heater = field_type_id( "fd_hot_air_vehicle_heater" );
     fd_fungicidal_gas = field_type_id( "fd_fungicidal_gas" );
     fd_insecticidal_gas = field_type_id( "fd_insecticidal_gas" );
     fd_smoke_vent = field_type_id( "fd_smoke_vent" );
