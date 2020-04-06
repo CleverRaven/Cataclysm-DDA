96ddc9b5d040a9070da9a9e4270eb0ae4244da67
roloa <marshkip@gmail.com>
2020-04-06 16:21:38 +0900

    衣服の香りの移植し忘れorz

--- src/map.cpp
+++ src/map.cpp
@@ -4312,9 +4312,13 @@ static void process_vehicle_items( vehicle &cur_veh, int part )
         for( auto &n : cur_veh.get_items( part ) ) {
             const time_duration washing_time = 90_minutes;
             const time_duration time_left = washing_time - n.age();
+            static const std::string fragrant( "FRAGRANT" );
             static const std::string filthy( "FILTHY" );
             if( time_left <= 0_turns ) {
                 n.item_tags.erase( filthy );
+                if( n.is_armor() ){
+                    n.item_tags.insert( fragrant );
+                }
                 washing_machine_finished = true;
                 cur_veh.parts[part].enabled = false;
             } else if( calendar::once_every( 15_minutes ) ) {
