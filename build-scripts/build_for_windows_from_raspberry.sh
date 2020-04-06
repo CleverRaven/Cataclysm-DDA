fe8ea405758449a34862e67201aaf2c73bf93ebd
roloa <marshkip@gmail.com>
2020-04-06 17:59:39 +0900

    ビルドスクリプト間違えた

--- build-scripts/build_for_windows_from_raspberry.sh
+++ build-scripts/build_for_windows_from_raspberry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-PLATFORM="i686-w64-mingw32.static"
+PLATFORM="x86_64-w64-mingw32.static"
 
 # mxe directory and cataclysm repogitry is in same depth
 make CROSS="../mxe/usr/bin/${PLATFORM}-" TILES=1 SOUND=1 RELEASE=1 LOCALIZE=1 LINTJSON=0 ASTYLE=0 RUNTESTS=0 BACKTRACE=0
