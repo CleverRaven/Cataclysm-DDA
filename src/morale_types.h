12afb0a1929af33023f494e6e2d09fd730b6fd48
roloa <marshkip@gmail.com>
2020-04-06 16:48:54 +0900

    なおしわすれ

--- src/morale_types.h
+++ src/morale_types.h
@@ -118,7 +118,7 @@ extern const morale_type MORALE_ACCOMPLISHMENT;
 extern const morale_type MORALE_FAILURE;
 extern const morale_type MORALE_PLAY_WITH_LITTLEMAID;
 extern const morale_type MORALE_ECSTASY;
-extern const morale_type MORALE_TAKE_BATH;
+extern const morale_type MORALE_TAKE_SHOWER;
 extern const morale_type MORALE_TAKE_WASHLET;
 extern const morale_type MORALE_USE_TOILETPAPER;
 extern const morale_type MORALE_EXCRETE;
