0671b00e4e8930178a8e4338ebdc4ee4154c7c73
Competently-Incompetent <63202272+Competently-Incompetent@users.noreply.github.com>
2020-04-06 01:41:08 +0100

    attempt at functional trait
    This might make the trait work?

--- src/handle_action.cpp
+++ src/handle_action.cpp
@@ -96,6 +96,7 @@ static const bionic_id bio_remote( "bio_remote" );
 static const trait_id trait_HIBERNATE( "HIBERNATE" );
 static const trait_id trait_PROF_CHURL( "PROF_CHURL" );
 static const trait_id trait_PROF_HELI_PILOT( "PROF_HELI_PILOT" );
+static const trait_id trait_PROF_MILI_Pilot( "MILI_PILOT" );
 static const trait_id trait_SHELL2( "SHELL2" );
 static const trait_id trait_WAYFARER( "WAYFARER" );
 
@@ -469,6 +470,11 @@ static void pldrive( int x, int y, int z = 0 )
         }
     }
     if( !remote && z != 0 && !u.has_trait( trait_PROF_HELI_PILOT ) ) {
+        u.add_msg_if_player( m_info, _( "You have no idea how to make the vehicle fly." ) );
+        return;
+    }
+        }
+    if( !remote && z != 0 && !u.has_trait( trait_MILI_PILOT ) ) {
         u.add_msg_if_player( m_info, _( "You have no idea how to make the vehicle fly." ) );
         return;
     }
