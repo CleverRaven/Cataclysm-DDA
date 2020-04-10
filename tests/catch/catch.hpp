dad99aaeca0e0b9c3916079b1e9b3e94cae5a5bf
John Bytheway <jbytheway@gmail.com>
2020-04-10 16:07:13 -0400

    Suppress guard change for catch.hpp
    We don't want to change the header guard for catch.hpp.

--- tests/catch/catch.hpp
+++ tests/catch/catch.hpp
@@ -8,6 +8,7 @@
  *  Distributed under the Boost Software License, Version 1.0. (See accompanying
  *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
+// NOLINTNEXTLINE(cata-header-guard)
 #ifndef TWOBLUECUBES_SINGLE_INCLUDE_CATCH_HPP_INCLUDED
 #define TWOBLUECUBES_SINGLE_INCLUDE_CATCH_HPP_INCLUDED
 // start catch.hpp
