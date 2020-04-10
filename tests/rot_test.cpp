17d95cb7a802492ab2aaeb67eca2cc5b6988f584
Hirmuolio <hirmuolio@gmail.com>
2020-04-10 17:45:24 +0300

    preserved and in freezer rot test

--- tests/rot_test.cpp
+++ tests/rot_test.cpp
@@ -27,24 +27,47 @@ static void set_map_temperature( int new_temperature )
 
 TEST_CASE( "Rate of rotting" )
 {
-    SECTION( "65 F" ) {
+    SECTION( "Passage of time" ) {
         // Item rot is a time duration.
         // At 65 F (18,3 C) item rots at rate of 1h/1h
         // So the level of rot should be about same as the item age
+        // In preserving containers and in freezer the item should not rot at all
 
-        item test_item( "meat_cooked" );
+        item normal_item( "meat_cooked" );
+
+        item freeze_item( "offal_canned" );
+
+        item sealed_item( "offal_canned" );
+        sealed_item = sealed_item.in_its_container();
 
         set_map_temperature( 65 ); // 18,3 C
 
-        test_item.process_temperature_rot( 1, tripoint_zero, nullptr );
+        normal_item.process( nullptr, tripoint_zero, false, 1, temperature_flag::TEMP_NORMAL );
+        sealed_item.process( nullptr, tripoint_zero, false, 1, temperature_flag::TEMP_NORMAL );
+        freeze_item.process( nullptr, tripoint_zero, false, 1, temperature_flag::TEMP_FREEZER );
 
         // Item should exist with no rot when it is brand new
-        CHECK( to_turns<int>( test_item.get_rot() ) == 0 );
+        CHECK( to_turns<int>( normal_item.get_rot() ) == 0 );
+        CHECK( to_turns<int>( sealed_item.get_rot() ) == 0 );
+        CHECK( to_turns<int>( freeze_item.get_rot() ) == 0 );
 
         calendar::turn = to_turn<int>( calendar::turn + 20_minutes );
-        test_item.process_temperature_rot( 1, tripoint_zero, nullptr );
+        normal_item.process( nullptr, tripoint_zero, false, 1, temperature_flag::TEMP_NORMAL );
+        sealed_item.process( nullptr, tripoint_zero, false, 1, temperature_flag::TEMP_NORMAL );
+        freeze_item.process( nullptr, tripoint_zero, false, 1, temperature_flag::TEMP_FREEZER );
 
-        // After 20 minutes the item should have 20 minutes of rot
-        CHECK( is_nearly( to_turns<int>( test_item.get_rot() ), to_turns<int>( 20_minutes ) ) );
+        // After 20 minutes the normal item should have 20 minutes of rot
+        CHECK( is_nearly( to_turns<int>( normal_item.get_rot() ), to_turns<int>( 20_minutes ) ) );
+        // Item in freezer and in preserving container should have no rot
+        CHECK( to_turns<int>( sealed_item.get_rot() ) == 0 );
+        CHECK( to_turns<int>( freeze_item.get_rot() ) == 0 );
+
+        // Move time 110 minutes
+        calendar::turn = to_turn<int>( calendar::turn + 110_minutes );
+        sealed_item.process( nullptr, tripoint_zero, false, 1, temperature_flag::TEMP_NORMAL );
+        freeze_item.process( nullptr, tripoint_zero, false, 1, temperature_flag::TEMP_FREEZER );
+        // In freezer and in preserving container still should be no rot
+        CHECK( to_turns<int>( sealed_item.get_rot() ) == 0 );
+        CHECK( to_turns<int>( freeze_item.get_rot() ) == 0 );
     }
 }
