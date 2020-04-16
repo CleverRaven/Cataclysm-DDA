f9b00dd861d4d795a714c0df527ae563bf2f6bea
roloa <marshkip@gmail.com>
2020-04-16 12:42:20 +0900

    Revert "引数を使ってないと怒られた。これは予想GUY"
    This reverts commit 557836741e707d76da26f5e70142ec00a2a17f27.

--- src/weather_gen.cpp
+++ src/weather_gen.cpp
@@ -271,7 +271,7 @@ weather_type weather_generator::get_weather_conditions( const w_point &w ) const
     return r;
 }
 
-weather_type weather_generator::get_variant_modded_weather_conditions( const w_point & ) const
+weather_type weather_generator::get_variant_modded_weather_conditions( const w_point &w ) const
 {
     // rainbw!!!
     return WEATHER_RAINBOW;
