3355144678bd8ac4ea677df27ac05a49a2edaf5d
Justen Robertson <justen@justen.us>
2020-05-05 20:24:33 -0700

    fix: add KorG's improved debug wrap in legacy_front

--- src/item_contents.cpp
+++ src/item_contents.cpp
@@ -667,21 +667,20 @@ std::list<const item *> item_contents::all_items_ptr() const
 
 item &item_contents::legacy_front()
 {
-    item *front = all_items_top().front();
-    if( !front ) {
-        debugmsg( "naively asked for first content item and will get a null" );
+    if( empty() ) {
+        debugmsg( "naively asked for first content item and will get a nullptr" );
+        return null_item_reference();
     }
-    return *front;
+    return *all_items_top().front();
 }
 
 const item &item_contents::legacy_front() const
 {
-    const item *front = all_items_top().front();
-    if( !front ) {
-        debugmsg( "naively asked for first content item and will get a null" );
+    if( empty() ) {
+        debugmsg( "naively asked for first content item and will get a nullptr" );
+        return null_item_reference();
     }
-    return *front;
-
+    return *all_items_top().front();
 }
 
 std::vector<item *> item_contents::gunmods()
