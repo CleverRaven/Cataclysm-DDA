08e1a07cf6fc275f18e8b1aa55a64ff29cd6b27d
Qrox <qrox@sina.com>
2020-05-08 15:26:14 +0800

    Use setup callbacks in magic.cpp

--- src/magic.cpp
+++ src/magic.cpp
@@ -1834,10 +1834,16 @@ int known_magic::select_spell( const Character &guy )
     std::vector<spell *> known_spells = get_spells();
 
     uilist spell_menu;
-    const int w_width = std::max( 80, TERMX * 3 / 8 );
-    spell_menu.w_height_setup = clamp( static_cast<int>( known_spells.size() ), 24, TERMY * 9 / 10 );
-    spell_menu.w_width_setup = w_width;
-    spell_menu.pad_right_setup = w_width - max_spell_name_length - 5;
+    spell_menu.w_height_setup = [&]() -> int {
+        return clamp( static_cast<int>( known_spells.size() ), 24, TERMY * 9 / 10 );
+    };
+    const auto calc_width = []() -> int {
+        return std::max( 80, TERMX * 3 / 8 );
+    };
+    spell_menu.w_width_setup = calc_width;
+    spell_menu.pad_right_setup = [&]() -> int {
+        return calc_width() - max_spell_name_length - 5;
+    };
     spell_menu.title = _( "Choose a Spell" );
     spell_menu.hilight_disabled = true;
     spellcasting_callback cb( known_spells, casting_ignore );
