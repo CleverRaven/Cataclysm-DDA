c62eaca1a7560683e8326ee013b7302ab3115345
Unknown <rkay94@gmail.com>
2020-05-11 16:50:33 -0400

    Breakout doors into own file, update script

--- tools/make_vehicle_parts_directions.py
+++ tools/make_vehicle_parts_directions.py
@@ -1,16 +1,16 @@
-import argparse
 import json
 import os
 import subprocess
 
-default_path = "data/json/vehicleparts/rams.json"
-default_output = "data/json/vehicleparts/rams2.json"
+default_path = "data/json/vehicleparts/doors.json"
+default_output = "data/json/vehicleparts/doors.json"
 ids_created_output = "data/json/vehicleparts/new_ids.txt"
 
 ids_to_create_directional_parts_for = []
 list_of_created_ids = []
 
 variants = {
+    "default_look" : "M",
     "nw" : "y",
     "vertical" : "j",
     "ne" : "u",
@@ -29,16 +29,18 @@ def gen_new(path):
         json_data = json.load(json_file)
         for jo in json_data:
             if "id" in jo:
-                jo["abstract"] = jo["id"]
+                jo["abstract"] = jo["id"] + "_abstract"
                 jo.pop("id")
                 ids_to_create_directional_parts_for.append(jo["abstract"])
 
         for key, value in variants.items():
             for ram in ids_to_create_directional_parts_for:
                 new_ram_id = ram + "_" + key
+                if(key == "default_look"):
+                    new_ram_id = ram
                 new_ram_data = {
                     "id": new_ram_id,
-                    "copy-from": ram,
+                    "copy-from": ram + "_abstract",
                     "symbol": value,
                     "type": "vehicle_part"
                 }
@@ -49,11 +51,12 @@ def gen_new(path):
 path = os.path.join(os.getcwd(), default_path)
 output_path = os.path.join(os.getcwd(), default_output)
 result = gen_new(path)
-print(*ids_to_create_directional_parts_for, sep = "\n")
+
+json_formatter_path = os.path.join(os.getcwd(), "tools/format/json_formatter.exe")
 
 with open(output_path, "w") as jf:
     json.dump(result, jf, ensure_ascii=False, sort_keys=True)
-    subprocess.Popen(['D:/Cataclysm-DDA/tools/format/json_formatter.exe', output_path], shell=True)
+    subprocess.Popen([json_formatter_path, output_path], shell=True)
 
 with open(ids_created_output, "w") as jf:
     for id in list_of_created_ids:
