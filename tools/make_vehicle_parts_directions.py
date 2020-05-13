cbe138879cffcabcf896079f8c8caf9b2436ee6b
Unknown <rkay94@gmail.com>
2020-05-12 23:10:25 -0400

    Keep default symbols for default parts

--- tools/make_vehicle_parts_directions.py
+++ tools/make_vehicle_parts_directions.py
@@ -2,8 +2,8 @@ import json
 import os
 import subprocess
 
-default_path = "data/json/vehicleparts/seats.json"
-default_output = "data/json/vehicleparts/seats.json"
+default_path = "data/json/vehicleparts/doors.json"
+default_output = "data/json/vehicleparts/doors.json"
 ids_created_output = "data/json/vehicleparts/new_ids.txt"
 
 ids_to_create_directional_parts_for = []
@@ -24,21 +24,35 @@ variants = {
     "box" : "#"
 }
 
+#variants = {
+#    "default_look" : "M",
+#    "up" : "n",
+#    "down" : "u",
+#    "left" : "d",
+#    "right" : "b",
+#}
+
 def gen_new(path):
     with open(path, "r") as json_file:
         json_data = json.load(json_file)
         for jo in json_data:
             if "id" in jo and jo["type"] == "vehicle_part":
                 jo["abstract"] = jo["id"] + "_abstract"
-                ids_to_create_directional_parts_for.append(jo["id"])
+                original_symbol = ""
+                if "symbol" in jo:
+                    original_symbol = jo["symbol"]
+                
+                ids_to_create_directional_parts_for.append({"id":jo["id"], "symbol": original_symbol})
                 jo.pop("id")
 
         for key, value in variants.items():
             for ram in ids_to_create_directional_parts_for:
-                new_ram_id = ram + "_" + key
-                copyfrom = ram + "_abstract"
+                new_ram_id = ram["id"] + "_" + key
+                copyfrom = ram["id"] + "_abstract"
                 if(key == "default_look"):
-                    new_ram_id = ram
+                    new_ram_id = ram["id"]
+                    if(ram["symbol"] != ""):
+                        value = ram["symbol"]
 
                 new_ram_data = {
                     "id": new_ram_id,
