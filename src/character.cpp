96b6e0db59f1b0e4a287102813d5f779934c6a55
Kevin Granade <kevin.granade@gmail.com>
2020-05-15 21:17:03 +0000

    Eliminate "Peckish" (#39760)
    * Eliminate "Peckish"
    
    * Remove uneeded stomach contents check

--- src/character.cpp
+++ src/character.cpp
@@ -4301,10 +4301,6 @@ std::pair<std::string, nc_color> Character::get_hunger_description() const
         } else if( recently_ate && contains >= cap * 3 / 8 ) {
             hunger_string = _( "Full" );
             hunger_color = c_green;
-        } else if( ( stomach.time_since_ate() > 90_minutes && contains < cap / 8 && recently_ate ) ||
-                   ( just_ate && contains > 0_ml && contains < cap * 3 / 8 ) ) {
-            hunger_string = _( "Peckish" );
-            hunger_color = c_dark_gray;
         } else if( !just_ate && ( recently_ate || contains > 0_ml ) ) {
             hunger_string.clear();
         } else {
