5312655ed3ba5e8febd658221dc3bb95cd086653
Eric Pierce <wapcaplet88@gmail.com>
2020-05-22 15:06:41 -0600

    Add lunar_month(), enum_to_string( moon_phase )
    The duration of a synodic month was hard-coded inside get_moon_phase,
    making it difficult to access for testing, and the moon_phase enum is
    not very readable in its numerical form, which also complicates testing.
    
    This commit adds a lunar_month() accessor function for the synodic month
    time duration, and adds an enum_to_string() template returning the
    string equivalent of each moon_phase, modeled after existing templates.

--- src/calendar.cpp
+++ src/calendar.cpp
@@ -8,6 +8,7 @@
 #include <limits>
 
 #include "debug.h"
+#include "enum_conversions.h"
 #include "options.h"
 #include "rng.h"
 #include "string_formatter.h"
@@ -60,11 +61,37 @@ double default_daylight_level()
     return 100.0;
 }
 
+time_duration lunar_month()
+{
+    return 29.530588853 * 1_days;
+}
+
+namespace io
+{
+// *INDENT-OFF*
+template<>
+std::string enum_to_string<moon_phase>( moon_phase phase_num )
+{
+    switch( phase_num ) {
+        case moon_phase::MOON_NEW: return "MOON_NEW";
+        case moon_phase::MOON_WAXING_CRESCENT: return "MOON_WAXING_CRESCENT";
+        case moon_phase::MOON_HALF_MOON_WAXING: return "MOON_HALF_MOON_WAXING";
+        case moon_phase::MOON_WAXING_GIBBOUS: return "MOON_WAXING_GIBBOUS";
+        case moon_phase::MOON_FULL: return "MOON_FULL";
+        case moon_phase::MOON_WANING_CRESCENT: return "MOON_WANING_CRESCENT";
+        case moon_phase::MOON_HALF_MOON_WANING: return "MOON_HALF_MOON_WANING";
+        case moon_phase::MOON_WANING_GIBBOUS: return "MOON_WANING_GIBBOUS";
+        case moon_phase::MOON_PHASE_MAX: break;
+    }
+    debugmsg( "Invalid moon_phase %d", phase_num );
+    abort();
+}
+// *INDENT-ON*
+} // namespace io
+
 moon_phase get_moon_phase( const time_point &p )
 {
-    static constexpr time_duration synodic_month = 29.530588853 * 1_days;
-    const time_duration moon_phase_duration =
-        calendar::season_from_default_ratio() * synodic_month;
+    const time_duration moon_phase_duration = calendar::season_from_default_ratio() * lunar_month();
     // Switch moon phase at noon so it stays the same all night
     const int num_middays = to_days<int>( p - calendar::turn_zero + 1_days / 2 );
     const time_duration nearest_midnight = num_middays * 1_days;
