CleverRaven/Cataclysm-DDA

commit fcd45f8987658785de7d5116e9e9fa9bf434b24d

Define monster starting ammo in mapgen (#40516)

src/map.cpp
@@ -7332,6 +7332,13 @@ void map::spawn_monsters_submap( const tripoint &gp, bool ignore_sight )
             if( i.friendly ) {
                 tmp.friendly = -1;
             }
+            if( !i.data.ammo.empty() ) {
+                for( std::pair<std::string, jmapgen_int> ap : i.data.ammo ) {
+                    tmp.ammo.emplace( ap.first, ap.second.get() );
+                }
+            } else {
+                tmp.ammo = tmp.type->starting_ammo;
+            }
 
             const auto valid_location = [&]( const tripoint & p ) {
                 // Checking for creatures via g is only meaningful if this is the main game map.

src/map.h
@@ -29,6 +29,7 @@
 #include "line.h"
 #include "lru_cache.h"
 #include "mapdata.h"
+#include "mapgen.h"
 #include "point.h"
 #include "rng.h"
 #include "shadowcasting.h"
@@ -1350,7 +1351,7 @@ class map
         void apply_faction_ownership( const point &p1, const point &p2, const faction_id &id );
         void add_spawn( const mtype_id &type, int count, const tripoint &p,
                         bool friendly = false, int faction_id = -1, int mission_id = -1,
-                        const std::string &name = "NONE" ) const;
+                        const std::string &name = "NONE", spawn_data data = spawn_data() ) const;
         void do_vehicle_caching( int z );
         // Note: in 3D mode, will actually build caches on ALL z-levels
         void build_map_cache( int zlev, bool skip_lightmap = false );

src/mapgen.cpp
@@ -1244,6 +1244,7 @@ class jmapgen_monster : public jmapgen_piece
         bool friendly;
         std::string name;
         bool target;
+        struct spawn_data data;
         jmapgen_monster( const JsonObject &jsi ) :
             chance( jsi, "chance", 100, 100 )
             , pack_size( jsi, "pack_size", 1, 1 )
@@ -1283,6 +1284,16 @@ class jmapgen_monster : public jmapgen_piece
                 }
                 ids.add( id, 100 );
             }
+
+            if( jsi.has_object( "spawn_data" ) ) {
+                const JsonObject &sd = jsi.get_object( "spawn_data" );
+                if( sd.has_array( "ammo" ) ) {
+                    const JsonArray &ammos = sd.get_array( "ammo" );
+                    for( const JsonObject &adata : ammos ) {
+                        data.ammo.emplace( adata.get_string( "ammo_id" ), jmapgen_int( adata, "qty" ) );
+                    }
+                }
+            }
         }
         void apply( mapgendata &dat, const jmapgen_int &x, const jmapgen_int &y ) const override {
 
@@ -1321,11 +1332,11 @@ class jmapgen_monster : public jmapgen_piece
                 MonsterGroupResult spawn_details = MonsterGroupManager::GetResultFromGroup( m_id );
                 dat.m.add_spawn( spawn_details.name, spawn_count * pack_size.get(),
                 { x.get(), y.get(), dat.m.get_abs_sub().z },
-                friendly, -1, mission_id, name );
+                friendly, -1, mission_id, name, data );
             } else {
                 dat.m.add_spawn( *( ids.pick() ), spawn_count * pack_size.get(),
                 { x.get(), y.get(), dat.m.get_abs_sub().z },
-                friendly, -1, mission_id, name );
+                friendly, -1, mission_id, name, data );
             }
         }
 };
@@ -2796,11 +2807,11 @@ void jmapgen_objects::apply( mapgendata &dat, const point &offset ) const
         return;
     }
 
-    for( auto &obj : objects ) {
-        auto where = obj.first;
+    for( const jmapgen_obj &obj : objects ) {
+        jmapgen_place where = obj.first;
         where.offset( -offset );
 
-        const auto &what = *obj.second;
+        const jmapgen_piece &what = *obj.second;
         // The user will only specify repeat once in JSON, but it may get loaded both
         // into the what and where in some cases--we just need the greater value of the two.
         const int repeat = std::max( where.repeat.get(), what.repeat.get() );
@@ -5634,8 +5645,14 @@ void map::place_spawns( const mongroup_id &group, const int chance,
         // Pick a monster type
         MonsterGroupResult spawn_details = MonsterGroupManager::GetResultFromGroup( group, &num );
 
+        const MonsterGroup &mg = group.obj();
+        auto mon_selected = std::find_if( mg.monsters.begin(), mg.monsters.end(),
+        [spawn_details]( const MonsterGroupEntry & mon ) {
+            return mon.name == spawn_details.name;
+        } );
+
         add_spawn( spawn_details.name, spawn_details.pack_size, { x, y, abs_sub.z },
-                   friendly, -1, mission_id, name );
+                   friendly, -1, mission_id, name, mon_selected->data );
     }
 }
 
@@ -5775,7 +5792,7 @@ std::vector<item *> map::put_items_from_loc( const items_location &loc, const tr
 }
 
 void map::add_spawn( const mtype_id &type, int count, const tripoint &p, bool friendly,
-                     int faction_id, int mission_id, const std::string &name ) const
+                     int faction_id, int mission_id, const std::string &name, spawn_data data ) const
 {
     if( p.x < 0 || p.x >= SEEX * my_MAPSIZE || p.y < 0 || p.y >= SEEY * my_MAPSIZE ) {
         debugmsg( "Bad add_spawn(%s, %d, %d, %d)", type.c_str(), count, p.x, p.y );
@@ -5792,7 +5809,7 @@ void map::add_spawn( const mtype_id &type, int count, const tripoint &p, bool fr
     if( MonsterGroupManager::monster_is_blacklisted( type ) ) {
         return;
     }
-    spawn_point tmp( type, count, offset, faction_id, mission_id, friendly, name );
+    spawn_point tmp( type, count, offset, faction_id, mission_id, friendly, name, data );
     place_on_submap->spawns.push_back( tmp );
 }
 

src/mongroup.cpp
@@ -368,8 +368,18 @@ void MonsterGroupManager::LoadMonsterGroup( const JsonObject &jo )
             if( mon.has_member( "ends" ) ) {
                 ends = tdfactor * mon.get_int( "ends" ) * ( mon_upgrade_factor > 0 ? mon_upgrade_factor : 1 );
             }
-            MonsterGroupEntry new_mon_group = MonsterGroupEntry( name, freq, cost, pack_min, pack_max, starts,
-                                              ends );
+            spawn_data data;
+            if( mon.has_object( "spawn_data" ) ) {
+                const JsonObject &sd = mon.get_object( "spawn_data" );
+                if( sd.has_array( "ammo" ) ) {
+                    const JsonArray &ammos = sd.get_array( "ammo" );
+                    for( const JsonObject &adata : ammos ) {
+                        data.ammo.emplace( adata.get_string( "ammo_id" ), jmapgen_int( adata, "qty" ) );
+                    }
+                }
+            }
+            MonsterGroupEntry new_mon_group = MonsterGroupEntry( name, freq, cost, pack_min, pack_max, data,
+                                              starts, ends );
             if( mon.has_member( "conditions" ) ) {
                 for( const std::string line : mon.get_array( "conditions" ) ) {
                     new_mon_group.conditions.push_back( line );

src/mongroup.h
@@ -9,6 +9,7 @@
 
 #include "calendar.h"
 #include "io_tags.h"
+#include "mapgen.h"
 #include "monster.h"
 #include "point.h"
 #include "type_id.h"
@@ -29,6 +30,7 @@ struct MonsterGroupEntry {
     int cost_multiplier;
     int pack_minimum;
     int pack_maximum;
+    spawn_data data;
     std::vector<std::string> conditions;
     time_duration starts;
     time_duration ends;
@@ -36,13 +38,15 @@ struct MonsterGroupEntry {
         return ends <= 0_turns;
     }
 
-    MonsterGroupEntry( const mtype_id &id, int new_freq, int new_cost,
-                       int new_pack_min, int new_pack_max, const time_duration &new_starts, const time_duration &new_ends )
+    MonsterGroupEntry( const mtype_id &id, int new_freq, int new_cost, int new_pack_min,
+                       int new_pack_max, spawn_data new_data, const time_duration &new_starts,
+                       const time_duration &new_ends )
         : name( id )
         , frequency( new_freq )
         , cost_multiplier( new_cost )
         , pack_minimum( new_pack_min )
         , pack_maximum( new_pack_max )
+        , data( new_data )
         , starts( new_starts )
         , ends( new_ends ) {
     }
@@ -63,7 +67,7 @@ struct MonsterGroupResult {
 struct MonsterGroup {
     mongroup_id name;
     mtype_id defaultMonster;
-    FreqDef  monsters;
+    FreqDef monsters;
     bool IsMonsterInGroup( const mtype_id &id ) const;
     bool is_animal = false;
     // replaces this group after a period of

src/iuse_actor.cpp
@@ -915,7 +915,10 @@ int place_monster_iuse::use( player &p, item &it, bool, const tripoint & ) const
                                  newmon.name() );
             amdef.second = ammo_item.charges;
         }
+    } else {
+        newmon.ammo = newmon.type->starting_ammo;
     }
+
     int skill_offset = 0;
     for( skill_id sk : skills ) {
         skill_offset += p.get_skill_level( sk ) / 2;

src/mapgen.h
@@ -126,6 +126,12 @@ struct jmapgen_setmap {
     bool has_vehicle_collision( mapgendata &dat, const point &offset ) const;
 };
 
+struct spawn_data {
+    std::map<std::string, jmapgen_int> ammo;
+
+    spawn_data() : ammo() {};
+};
+
 /**
  * Basic mapgen object. It is supposed to place or do something on a specific square on the map.
  * Inherit from this class and implement the @ref apply function.

src/mattack_actors.h
@@ -147,7 +147,7 @@ class gun_actor : public mattack_actor
         /** Specify weapon mode to use at different engagement distances */
         std::map<std::pair<int, int>, gun_mode_id> ranges;
 
-        int max_ammo = INT_MAX; /** limited also by monster starting_ammo */
+        int max_ammo = INT_MAX; /** limited also by monster starting ammo */
 
         /** Description of the attack being run */
         std::string description;
@@ -155,7 +155,7 @@ class gun_actor : public mattack_actor
         /** Message to display (if any) for failures to fire excluding lack of ammo */
         std::string failure_msg;
 
-        /** Sound (if any) when either starting_ammo depleted or max_ammo reached */
+        /** Sound (if any) when either ammo depleted or max_ammo reached */
         std::string no_ammo_sound;
 
         /** Number of moves required for each attack */

commit 1416b5017218d2a38aecbc33df9ad2a50c95fc62

Refactor monster spawning code
Fixes #40899.

The problem was introduced in #40516 due to the way it passed spawn_data
around. It only worked for some types of monster spawning. In
particular, not when the default monster from a group was selected, nor
when a monster from an upgraded group was selected.

Avoid the need to re-derive spawn data in map::place_spawns by passing
it along in the MonsterGroupResult.

src/map.cpp
@@ -6776,7 +6776,7 @@ void map::rotten_item_spawn( const item &item, const tripoint &pnt )
                                          get_option<float>( "CARRION_SPAWNRATE" ) );
     if( rng( 0, 100 ) < chance ) {
         MonsterGroupResult spawn_details = MonsterGroupManager::GetResultFromGroup( mgroup );
-        add_spawn( spawn_details.name, 1, pnt, false );
+        add_spawn( spawn_details, pnt );
         if( g->u.sees( pnt ) ) {
             if( item.is_seed() ) {
                 add_msg( m_warning, _( "Something has crawled out of the %s plants!" ), item.get_plant_name() );

src/map.h
@@ -72,6 +72,7 @@ class npc_template;
 class tileray;
 class vpart_reference;
 struct mongroup;
+struct MonsterGroupResult;
 struct projectile;
 struct veh_collision;
 template<typename T>
@@ -1352,6 +1353,7 @@ class map
         void add_spawn( const mtype_id &type, int count, const tripoint &p,
                         bool friendly = false, int faction_id = -1, int mission_id = -1,
                         const std::string &name = "NONE", const spawn_data &data = spawn_data() ) const;
+        void add_spawn( const MonsterGroupResult &spawn_details, const tripoint &p ) const;
         void do_vehicle_caching( int z );
         // Note: in 3D mode, will actually build caches on ALL z-levels
         void build_map_cache( int zlev, bool skip_lightmap = false );

src/map_field.cpp
@@ -1105,7 +1105,7 @@ bool map::process_fields_in_submap( submap *const current_submap,
                         [this]( const tripoint & n ) {
                         return passable( n );
                         } ) ) {
-                            add_spawn( spawn_details.name, spawn_details.pack_size, *spawn_point );
+                            add_spawn( spawn_details, *spawn_point );
                         }
                     }
                 }

src/mapgen.cpp
@@ -186,7 +186,7 @@ void map::generate( const tripoint &p, const time_point &when )
             random_point( *this, [this]( const tripoint & n ) {
             return passable( n );
             } ) ) {
-                add_spawn( spawn_details.name, spawn_details.pack_size, *pt );
+                add_spawn( spawn_details, *pt );
             }
         }
     }
@@ -5642,15 +5642,8 @@ void map::place_spawns( const mongroup_id &group, const int chance,
 
         // Pick a monster type
         MonsterGroupResult spawn_details = MonsterGroupManager::GetResultFromGroup( group, &num );
-
-        const MonsterGroup &mg = group.obj();
-        auto mon_selected = std::find_if( mg.monsters.begin(), mg.monsters.end(),
-        [spawn_details]( const MonsterGroupEntry & mon ) {
-            return mon.name == spawn_details.name;
-        } );
-
         add_spawn( spawn_details.name, spawn_details.pack_size, { x, y, abs_sub.z },
-                   friendly, -1, mission_id, name, mon_selected->data );
+                   friendly, -1, mission_id, name, spawn_details.data );
     }
 }
 
@@ -5787,6 +5780,12 @@ std::vector<item *> map::put_items_from_loc( const items_location &loc, const tr
 {
     const auto items = item_group::items_from( loc, turn );
     return spawn_items( p, items );
+}
+
+void map::add_spawn( const MonsterGroupResult &spawn_details, const tripoint &p ) const
+{
+    add_spawn( spawn_details.name, spawn_details.pack_size, p, false, -1, -1, "NONE",
+               spawn_details.data );
 }
 
 void map::add_spawn( const mtype_id &type, int count, const tripoint &p, bool friendly,

src/mongroup.cpp
@@ -174,9 +174,9 @@ MonsterGroupResult MonsterGroupManager::GetResultFromGroup(
             //If the monsters frequency is greater than the spawn_chance, select this spawn rule
             if( it->frequency >= spawn_chance ) {
                 if( it->pack_maximum > 1 ) {
-                    spawn_details = MonsterGroupResult( it->name, rng( it->pack_minimum, it->pack_maximum ) );
+                    spawn_details = MonsterGroupResult( it->name, rng( it->pack_minimum, it->pack_maximum ), it->data );
                 } else {
-                    spawn_details = MonsterGroupResult( it->name, 1 );
+                    spawn_details = MonsterGroupResult( it->name, 1, it->data );
                 }
                 //And if a quantity pointer with remaining value was passed, will modify the external value as a side effect
                 //We will reduce it by the spawn rule's cost multiplier

src/mongroup.h
@@ -55,12 +55,13 @@ struct MonsterGroupEntry {
 struct MonsterGroupResult {
     mtype_id name;
     int pack_size;
+    spawn_data data;
 
     MonsterGroupResult() : name( mtype_id::NULL_ID() ), pack_size( 0 ) {
     }
 
-    MonsterGroupResult( const mtype_id &id, int new_pack_size )
-        : name( id ), pack_size( new_pack_size ) {
+    MonsterGroupResult( const mtype_id &id, int new_pack_size, spawn_data new_data = spawn_data() )
+        : name( id ), pack_size( new_pack_size ), data( new_data ) {
     }
 };
 