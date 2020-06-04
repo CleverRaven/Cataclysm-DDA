6b508396a0b0795ddbf98d49374e22a2e0e87603
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2020-06-05 01:14:30 +0300

    Apply suggestions from code review

--- src/event.h
+++ src/event.h
@@ -152,7 +152,7 @@ struct event_spec_character_item {
     };
 };
 
-static_assert( static_cast<int>( event_type::num_event_types ) == 69,
+static_assert( static_cast<int>( event_type::num_event_types ) == 67,
                "This static_assert is to remind you to add a specialization for your new "
                "event_type below" );
 
