949a272b663798ea98bf9bd747f10c26fa8ae802
Qrox <qrox@sina.com>
2020-06-06 18:05:18 +0800

    Fix resizing of NPC/faction epilogues

--- src/output.h
+++ src/output.h
@@ -324,8 +324,6 @@ void center_print( const catacurses::window &w, int y, const nc_color &FG,
                    const std::string &text );
 int right_print( const catacurses::window &w, int line, int right_indent,
                  const nc_color &FG, const std::string &text );
-void scrollable_text( const catacurses::window &w, const std::string &title,
-                      const std::string &text );
 void scrollable_text( const std::function<catacurses::window()> &init_window,
                       const std::string &title, const std::string &text );
 std::string name_and_value( const std::string &name, int value, int field_width );
