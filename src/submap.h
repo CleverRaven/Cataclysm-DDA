92aea1701b9e7b69ca7b8e595ed9aea97d8be129
John Bytheway <jbytheway@gmail.com>
2020-06-09 06:19:23 -0400

    Convert maptile fields to a point

--- src/submap.h
+++ src/submap.h
@@ -268,16 +268,15 @@ struct maptile {
         friend map; // To allow "sliding" the tile in x/y without bounds checks
         friend submap;
         submap *const sm;
-        size_t x;
-        size_t y;
+        point pos_;
         point pos() const {
-            return point( x, y );
+            return pos_;
         }
 
         maptile( submap *sub, const size_t nx, const size_t ny ) :
-            sm( sub ), x( nx ), y( ny ) { }
+            sm( sub ), pos_( nx, ny ) { }
         maptile( submap *sub, const point &p ) :
-            sm( sub ), x( p.x ), y( p.y ) { }
+            sm( sub ), pos_( p ) { }
     public:
         trap_id get_trap() const {
             return sm->get_trap( pos() );
