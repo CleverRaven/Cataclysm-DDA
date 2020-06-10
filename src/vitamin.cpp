4f55255bbd2edd6de6f48eafb03c5affccdda8db
nexusmrsep <nexus.mrsep@gmail.com>
2020-06-10 19:12:51 +0200

    allow negative vitamin rates

--- src/vitamin.cpp
+++ src/vitamin.cpp
@@ -69,11 +69,7 @@ void vitamin::load_vitamin( const JsonObject &jo )
         jo.throw_error( "vitamin must have a vitamin type", "vit_type" );
     }
     vit.type_ = jo.get_enum_value<vitamin_type>( "vit_type" );
-
-    if( vit.rate_ < 0_turns ) {
-        jo.throw_error( "vitamin consumption rate cannot be negative", "rate" );
-    }
-
+    
     for( JsonArray e : jo.get_array( "disease" ) ) {
         vit.disease_.emplace_back( e.get_int( 0 ), e.get_int( 1 ) );
     }
