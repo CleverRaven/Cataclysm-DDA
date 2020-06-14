d2623596a8fac6b22e86fa167ad5e72510d47652
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2020-06-14 20:06:18 +0300

    Appease clang-tidy

--- src/character.cpp
+++ src/character.cpp
@@ -1589,10 +1589,7 @@ bool Character::move_effects( bool attacking )
             remove_effect( effect_in_pit );
         }
     }
-    if( has_effect( effect_grabbed ) && !attacking && !try_remove_grab() ) {
-        return false;
-    }
-    return true;
+    return has_effect( effect_grabbed ) && !attacking && !try_remove_grab();
 }
 
 void Character::wait_effects( bool attacking )
