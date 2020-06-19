10e32417b28add122d8c5e8b2724769ac8127662
Charles Engen <owenengen@gmail.com>
2020-06-19 07:31:15 -0600

    Update vehicle_part.cpp

--- src/vehicle_part.cpp
+++ src/vehicle_part.cpp
@@ -373,7 +373,7 @@ bool vehicle_part::can_reload( const item &obj ) const
         return true;
     }
 
-    return is_tank () &&
+    return is_tank() &&
            ammo_remaining() <= ammo_capacity( item::find_type( ammo_current() )->ammo->type );
 }
 
