b022831706ddde8d10769462b9513e7f6efb07e2
Eric Pierce <wapcaplet88@gmail.com>
2020-06-18 21:09:42 -0600

    Include place_nested from mission_definition

--- tools/json_tools/orphans.py
+++ tools/json_tools/orphans.py
@@ -4,19 +4,19 @@
 # For now just prints "nested_mapgen_id" values unused by any mapgen "chunks"
 
 import util
+from collections import Counter, OrderedDict
 
 # TODO:
 # - Add command-line options to customiz output and do filtering
 # - Be able to detect other kinds of unused or dead JSON data
 
-def get_nested_object_chunks(json_item):
-    """Given a JSON item like { "object": { "place_nested": [ ... ] } },
+def get_nested_object_chunks(jo):
+    """Given a JSON object with "nested": { ... } or "place_nested": [ ... ],
     pick out all the nested mapgen IDs for objects in the list with "chunks",
     and return them in a list.
     """
 
     # Get all "nested" and "place_nested" objects
-    jo = json_item["object"]
     nested_objects = []
     if "nested" in jo:
         nested_objects.extend(jo["nested"].values())
@@ -57,19 +57,32 @@ def mapgen_ids_with_no_chunks(json_data):
     nested_ids = []
     chunk_refs = []
 
-    # Read "nested_mapgen_id" and "place_nested" chunks from "mapgen" types
+    # Read "nested_mapgen_id", "nested", and "place_nested" chunks
+    # from "mapgen" and "mission_definition" types
     for item in json_data:
-        if item.get("type") != "mapgen":
-            continue
+        if item.get("type") == "mapgen":
+            if "nested_mapgen_id" in item:
+                nested_ids.append(item["nested_mapgen_id"])
 
-        if "nested_mapgen_id" in item:
-            nested_ids.append(item["nested_mapgen_id"])
+            if "object" in item:
+                obj = item["object"]
+                if "nested" in obj or "place_nested" in obj:
+                    chunk_refs.extend(get_nested_object_chunks(obj))
 
-        if "object" in item and "nested" in item["object"]:
-            chunk_refs.extend(get_nested_object_chunks(item))
+        elif item.get("type") == "mission_definition":
+            if "start" in item and "update_mapgen" in item["start"]:
+                mapgens = item["start"]["update_mapgen"]
+                if type(mapgens) != list:
+                    mapgens = [mapgens]
+                for mapgen in mapgens:
+                    chunk_refs.extend(get_nested_object_chunks(mapgen))
 
-        if "object" in item and "place_nested" in item["object"]:
-            chunk_refs.extend(get_nested_object_chunks(item))
+            if "end" in item and "update_mapgen" in item["end"]:
+                mapgens = item["end"]["update_mapgen"]
+                if type(mapgens) != list:
+                    mapgens = [mapgens]
+                for mapgen in mapgens:
+                    chunk_refs.extend(get_nested_object_chunks(mapgen))
 
     # Remove duplicates
     nested_ids = list(set(nested_ids))
