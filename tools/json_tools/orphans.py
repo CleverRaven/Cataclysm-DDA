b84b6210c91b8e1a1cf0554778d423fa93c8f9ce
Eric Pierce <wapcaplet88@gmail.com>
2020-06-18 19:34:57 -0600

    Add script for detecting orphaned JSON data
    First draft: Look for nested_mapgen_id not used by any chunks

---
+++ tools/json_tools/orphans.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python2
+"""Detect and print orphaned or unused JSON data.
+"""
+# For now just prints "nested_mapgen_id" values unused by any mapgen "chunks"
+
+import util
+
+# TODO:
+# - Add command-line options to customiz output and do filtering
+# - Be able to detect other kinds of unused or dead JSON data
+
+def get_nested_object_chunks(json_item):
+    """Given a JSON item like { "object": { "place_nested": [ ... ] } },
+    pick out all the nested mapgen IDs for objects in the list with "chunks",
+    and return them in a list.
+    """
+    all_chunks = []
+
+    # Get nested object chunks that are lists
+    nest_chunk_lists = [
+        nest["chunks"]
+        for nest in json_item["object"]["place_nested"]
+        if "chunks" in nest and type(nest["chunks"]) == list
+    ]
+
+    for chunks in nest_chunk_lists:
+        # Simple list of strings
+        if all(isinstance(c, basestring) for c in chunks):
+            # Extend chunk refs with all strings
+            all_chunks.extend(chunks)
+
+        # List of [ "nest_id", num ]
+        elif all(isinstance(c, list) for c in chunks):
+            # Get the first value from each sub-list, ensuring string
+            ids = [c[0] for c in chunks if isinstance(c[0], basestring)]
+            all_chunks.extend(ids)
+
+        else:
+            print("Unexpected chunks: %s" % chunks)
+
+    return all_chunks
+
+
+def mapgen_ids_with_no_chunks(json_data):
+    """Return a list of "nested_mapgen_id" values not used by any mapgen "chunks".
+    """
+    # Keep track of IDs and references to them
+    nested_ids = []
+    chunk_refs = []
+
+    # Read "nested_mapgen_id" and "place_nested" chunks from "mapgen" types
+    for item in json_data:
+        if item.get("type") != "mapgen":
+            continue
+
+        if "nested_mapgen_id" in item:
+            nested_ids.append(item["nested_mapgen_id"])
+
+        if "object" in item and "place_nested" in item["object"]:
+            chunk_refs.extend(get_nested_object_chunks(item))
+
+    # Remove duplicates
+    nested_ids = list(set(nested_ids))
+    chunk_refs = list(set(chunk_refs))
+
+    # Get a sorted list of IDs that aren't in any chunks
+    unused_ids = sorted(list(set(nested_ids) - set(chunk_refs)))
+    return unused_ids
+
+
+if __name__ == "__main__":
+    json_data, load_errors = util.import_data()
+
+    print("Nested mapgen IDs (nested_mapgen_id) not appearing in any chunks:")
+    print("\n".join(mapgen_ids_with_no_chunks(json_data)))
+
