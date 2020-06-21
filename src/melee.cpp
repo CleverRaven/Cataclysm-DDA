7cbcfa360bd7b98e07be9e9078d75901e5750c04
CodeBandit <63547126+CodeBandit@users.noreply.github.com>
2020-06-21 19:37:41 -0400

    Dodge decreases linearly to 0 from 50% stamina (#41461)
    * Dodge decreases linearly to 0 from 50% stamina
    
    * Add test for stamina's effect on dodge

--- src/melee.cpp
+++ src/melee.cpp
@@ -862,6 +862,12 @@ float player::get_dodge() const
         ret *= speed_stat / 100.0f;
     }
 
+    //Dodge decreases linearly to 0 when below 50% stamina.
+    const float stamina_ratio = static_cast<float>( get_stamina() ) / get_stamina_max();
+    if( stamina_ratio <= .5 ) {
+        ret *= 2 * stamina_ratio;
+    }
+
     return std::max( 0.0f, ret );
 }
 
