a183e506e3ac6437e9eeff94580fc7bf17c2cd1a
martinrhan <53336429+martinrhan@users.noreply.github.com>
2020-06-27 08:55:35 +0800

    Update src/input.h
    Co-authored-by: BevapDin <tho_ki@gmx.de>

--- src/input.h
+++ src/input.h
@@ -638,7 +638,7 @@ class input_context
          * Get coordinate of text level from mouse input, difference between this and get_coordinates is that one is getting pixel level coordinate.
          * Return false if the result is not in the window.
          */
-        std::pair<point, bool> get_coordinates_text( const catacurses::window &capture_win ) const;
+        cata::optional<point> get_coordinates_text( const catacurses::window &capture_win ) const;
 
         /**
          * Get the human-readable name for an action.
