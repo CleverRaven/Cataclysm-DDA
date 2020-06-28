98d608b6dc3051823bc2607ecae1b9430299c2ba
U-N\N <zczczc1680@gmail.com>
2020-06-28 08:35:59 +0200

    Modify existing class instead of adding new one

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -9799,7 +9799,7 @@ int iuse::wash_items( player *p, bool soft_items, bool hard_items )
             }};
     };
     // TODO: this should also search surrounding area, not just player inventory.
-    inventory_process_selector inv_s( *p, _( "ITEMS TO CLEAN" ), preset, make_raw_stats );
+    inventory_iuse_selector inv_s( *p, _( "ITEMS TO CLEAN" ), preset, make_raw_stats );
     inv_s.add_character_items( *p );
     inv_s.set_title( _( "Multiclean" ) );
     inv_s.set_hint( _( "To clean x items, type a number before selecting." ) );
