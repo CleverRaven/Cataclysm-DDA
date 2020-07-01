6cd86bf5fc727fcef12976dac126f9a90d4270a5
Kevin Granade <kevin.granade@gmail.com>
2020-06-30 18:38:04 -0700

    Update for new bodypart functionality

--- src/activity_actor.cpp
+++ src/activity_actor.cpp
@@ -1319,25 +1319,29 @@ void workout_activity_actor::start( player_activity &act, Character &who )
     // broken limbs as long as they are not involved by the machine
     bool hand_equipment = g->m.has_flag_furn( "WORKOUT_ARMS", location );
     bool leg_equipment = g->m.has_flag_furn( "WORKOUT_LEGS", location );
-    if( hand_equipment && ( ( who.is_limb_broken( hp_arm_l ) ) ||
-                            who.is_limb_broken( hp_arm_r ) ) ) {
+    static const bodypart_id arm_l = bodypart_id( "arm_l" );
+    static const bodypart_id arm_r = bodypart_id( "arm_r" );
+    static const bodypart_id leg_l = bodypart_id( "leg_l" );
+    static const bodypart_id leg_r = bodypart_id( "leg_r" );
+    if( hand_equipment && ( ( who.is_limb_broken( arm_l ) ) ||
+                            who.is_limb_broken( arm_r ) ) ) {
         who.add_msg_if_player( _( "You cannot train here with a broken arm." ) );
         act_id = activity_id::NULL_ID();
         act.set_to_null();
         return;
     }
-    if( leg_equipment && ( ( who.is_limb_broken( hp_leg_l ) ) ||
-                           who.is_limb_broken( hp_leg_r ) ) ) {
+    if( leg_equipment && ( ( who.is_limb_broken( leg_l ) ) ||
+                           who.is_limb_broken( leg_r ) ) ) {
         who.add_msg_if_player( _( "You cannot train here with a broken leg." ) );
         act_id = activity_id::NULL_ID();
         act.set_to_null();
         return;
     }
     if( !hand_equipment && !leg_equipment &&
-        ( who.is_limb_broken( hp_arm_l ) ||
-          who.is_limb_broken( hp_arm_r ) ||
-          who.is_limb_broken( hp_leg_l ) ||
-          who.is_limb_broken( hp_leg_r ) ) ) {
+        ( who.is_limb_broken( arm_l ) ||
+          who.is_limb_broken( arm_r ) ||
+          who.is_limb_broken( leg_l ) ||
+          who.is_limb_broken( leg_r ) ) ) {
         who.add_msg_if_player( _( "You cannot train freely with a broken limb." ) );
         act_id = activity_id::NULL_ID();
         act.set_to_null();
