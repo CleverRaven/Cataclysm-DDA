ee1b310f4b8d5652aa67227dc15ed1f9f21b242a
barsoosayque <shtoshich@gmail.com>
2020-07-18 00:15:37 +0700

    use new geometry classes in sdl rendering related files

--- src/sdltiles.h
+++ src/sdltiles.h
@@ -24,8 +24,6 @@ extern bool alt_rect_tex_enabled;
 extern std::unique_ptr<cata_tiles> tilecontext;
 extern std::array<SDL_Color, color_loader<SDL_Color>::COLOR_NAMES_COUNT> windowsPalette;
 
-void draw_alt_rect( const SDL_Renderer_Ptr &renderer, const SDL_Rect &rect,
-                    Uint32 r, Uint32 g, Uint32 b );
 void load_tileset();
 void rescale_tileset( int size );
 bool save_screenshot( const std::string &file_path );
