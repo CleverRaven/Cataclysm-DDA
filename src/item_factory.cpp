4fc22a40cd975784011ec0e166f59d6c8ab88126
andrei <a12l+git@runbox.com>
2020-07-22 08:17:37 +0300

    items: remove ups_based_armor

--- src/item_factory.cpp
+++ src/item_factory.cpp
@@ -1039,7 +1039,6 @@ void Item_factory::init()
     add_actor( std::make_unique<reveal_map_actor>() );
     add_actor( std::make_unique<salvage_actor>() );
     add_actor( std::make_unique<unfold_vehicle_iuse>() );
-    add_actor( std::make_unique<ups_based_armor_actor>() );
     add_actor( std::make_unique<place_trap_actor>() );
     add_actor( std::make_unique<emit_actor>() );
     add_actor( std::make_unique<saw_barrel_actor>() );
