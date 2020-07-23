d7a1dacd7241ebc00157efdb8f86c9d1828ef2f4
mqrause <38702195+mqrause@users.noreply.github.com>
2020-07-23 13:08:33 +0200

    Check parent containers when inserting into items (#42320)
    * - check if item fits in parent pocket before inserting
    - streamline method calls to avoid duplicate calls
    
    * fix small oversight
    
    * don't show items that don't fit
    
    Co-authored-by: mqrause <martin@qrause.de>

--- src/game_inventory.cpp
+++ src/game_inventory.cpp
@@ -1312,18 +1312,21 @@ void game_menus::inv::insert_items( avatar &you, item_location &holster )
         item &it = *holstered_item.first;
         bool success = false;
         if( !it.count_by_charges() || it.count() == holstered_item.second ) {
-            if( holster->contents.can_contain( it ).success() ) {
-                holster->put_in( it, item_pocket::pocket_type::CONTAINER );
-                holstered_item.first.remove_item();
-                success = true;
+            if( holster.parents_can_contain_recursive( &it ) ) {
+                success = holster->put_in( it, item_pocket::pocket_type::CONTAINER ).success();
+                if( success ) {
+                    holstered_item.first.remove_item();
+                }
             }
         } else {
             item item_copy( it );
             item_copy.charges = holstered_item.second;
-            if( holster->can_contain( item_copy ) ) {
-                holster->put_in( item_copy, item_pocket::pocket_type::CONTAINER );
-                it.charges -= holstered_item.second;
-                success = true;
+
+            if( holster.parents_can_contain_recursive( &item_copy ) ) {
+                success = holster->put_in( item_copy, item_pocket::pocket_type::CONTAINER ).success();
+                if( success ) {
+                    it.charges -= holstered_item.second;
+                }
             }
         }
 
