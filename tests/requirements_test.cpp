098842ed89ed5d8381469644ba19c0c42ce40eb9
Zukulini <dragon.fuego.yo@gmail.com>
2020-07-24 10:37:53 -0500

    Update requirements_test.cpp

--- tests/requirements_test.cpp
+++ tests/requirements_test.cpp
@@ -6,7 +6,6 @@
 
 #include "catch/catch.hpp"
 
-static const itype_id itype_acid( "chem_sulphuric_acid" );
 static const itype_id itype_ash( "ash" );
 static const itype_id itype_lye( "lye" );
 static const itype_id itype_rock( "rock" );
