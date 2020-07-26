b1321fdb2a4d1331ec847ea00dcb6822ee01c35c
Kevin Granade <kevin.granade@gmail.com>
2020-07-26 03:39:09 +0000

    Apply suggestions from code review

--- tests/algo_test.cpp
+++ tests/algo_test.cpp
@@ -1,5 +1,7 @@
 #pragma GCC diagnostic ignored "-Wunused-macros"
+#ifndef CATCH_CONFIG_ENABLE_PAIR_STRINGMAKER
 #define CATCH_CONFIG_ENABLE_PAIR_STRINGMAKER
+#endif
 #include "catch/catch.hpp"
 
 #include <algorithm>
