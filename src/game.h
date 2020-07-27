229eb4b238af7bf0c40f7d7e188012b09bf7e51f
EvgenijM86 <EvgenijM86@yandex.com>
2020-07-27 05:12:25 +0300

    fix_spawn

--- src/game.h
+++ src/game.h
@@ -331,7 +331,7 @@ class game
         monster *place_critter_at( const shared_ptr_fast<monster> &mon, const tripoint &p );
         monster *place_critter_around( const mtype_id &id, const tripoint &center, int radius );
         monster *place_critter_around( const shared_ptr_fast<monster> &mon, const tripoint &center,
-                                       int radius );
+                                       int radius, bool forced = false );
         monster *place_critter_within( const mtype_id &id, const tripoint_range<tripoint> &range );
         monster *place_critter_within( const shared_ptr_fast<monster> &mon,
                                        const tripoint_range<tripoint> &range );
