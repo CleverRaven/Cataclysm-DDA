96f3a03e3ac6152715e162272e0d6fee8a5ebf8f
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2020-08-01 00:25:58 +0300

    Report failing starting location

--- src/start_location.cpp
+++ src/start_location.cpp
@@ -208,7 +208,8 @@ tripoint_abs_omt start_location::find_player_initial_location() const
         }
     }
     // Should never happen, if it does we messed up.
-    popup( _( "Unable to generate a valid starting location, please report this failure." ) );
+    popup( _( "Unable to generate a valid starting location %s [%s] in a radius of %d overmaps, please report this failure." ),
+           name(), id.str(), radius );
     return overmap::invalid_tripoint;
 }
 
