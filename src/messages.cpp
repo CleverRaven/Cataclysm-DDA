cbbab458cceca0b6da998b971abec5b869b44a25
Eric Pierce <wapcaplet88@gmail.com>
2020-08-07 18:09:04 -0600

    Add message limit option for saving log history
    The message log where all significant in-game events are printed had a
    hard-coded limit of 255 lines (with the maximum lines being visible in
    the `P` log history popup). Depending how spammy the messages are,
    events just a few minutes past can be lost.
    
    This commit adds a new option to the Interface tab with the other
    message options. The default of 255 matches the old limit, and a value
    of 0 will do no limiting of the message log.

--- src/messages.cpp
+++ src/messages.cpp
@@ -22,9 +22,8 @@
 
 #if defined(__ANDROID__)
 #include <SDL_keyboard.h>
-
-#include "options.h"
 #endif
+#include "options.h"
 
 #include <algorithm>
 #include <deque>
@@ -193,7 +192,8 @@ class messages_impl
                 return;
             }
 
-            while( messages.size() > 255 ) {
+            unsigned int message_limit = get_option<int>( "MESSAGE_LIMIT" );
+            while( messages.size() > message_limit ) {
                 messages.pop_front();
             }
 
