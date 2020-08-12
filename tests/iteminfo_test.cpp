9f184d5aad628c2571a899b48192220ad619ff25
LcsTen <lcs10.twinoid@gmail.com>
2020-08-12 22:12:22 +0200

    Fix iteminfo test for test_soldering_iron
    The previous change of the recipe test_soldering_iron changed its
    description, changed the test value accordingly

--- tests/iteminfo_test.cpp
+++ tests/iteminfo_test.cpp
@@ -2230,7 +2230,7 @@ TEST_CASE( "disassembly time and yield", "[iteminfo][disassembly]" )
            "<color_c_white>Disassembly</color> takes about 20 minutes, requires 1 tool"
            " with <color_c_cyan>cutting of 1</color> or more and 1 tool with"
            " <color_c_cyan>screw driving of 1</color> or more and <color_c_white>might"
-           " yield</color>: 2 electronic scraps, copper (1), scrap metal (1), and copper"
+           " yield</color>: 2 electronic scraps, copper (2), scrap metal (1), and copper"
            " wire (5).\n" );
 
     CHECK( item_info_str( metal, disassemble ) ==
