c3489f41e2f536d08f60f4ea7f3b82b2a72a973f
souricelle <67675144+souricelle@users.noreply.github.com>
2020-08-14 05:23:12 -0700

    Update butchery_requirements.cpp

--- src/butchery_requirements.cpp
+++ src/butchery_requirements.cpp
@@ -96,7 +96,6 @@ void butchery_requirements::check_consistency()
 std::pair<float, requirement_id> butchery_requirements::get_fastest_requirements(
     const inventory &crafting_inv, creature_size size, butcher_type butcher ) const
 {
-    
     for( const std::pair<float, std::map<creature_size, std::map<butcher_type, requirement_id>>>
         &riter : requirements ) {
         if( riter.second.at( size ).at( butcher )->can_make_with_inventory( crafting_inv,
