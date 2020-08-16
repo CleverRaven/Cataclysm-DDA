7e1d8d6b22d169bf8d94fc99bc3b44379238840d
Curtis Merrill <curtis.r.merrill@gmail.com>
2020-08-16 11:57:17 -0400

    change all architectures to double

--- src/magic.cpp
+++ src/magic.cpp
@@ -1070,18 +1070,11 @@ std::string spell::damage_type_string() const
     return name_by_dt( dmg_type() );
 }
 
-#ifndef __aarch64__
 // constants defined below are just for the formula to be used,
 // in order for the inverse formula to be equivalent
-constexpr float a = 6200.0f;
-constexpr float b = 0.146661f;
-constexpr float c = -62.5f;
-#else
-// ARM64 needs double
 constexpr double a = 6200.0;
 constexpr double b = 0.146661;
 constexpr double c = -62.5;
-#endif
 
 int spell::get_level() const
 {
