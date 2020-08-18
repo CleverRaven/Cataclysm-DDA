81d0cb9987fb186e00e5bdcbfadb7ed0dc33dc22
Alexey Kim <git@karmaflux.com>
2020-08-19 00:45:09 +0300

    Add vehicle interaction progress percent

--- src/player_activity.cpp
+++ src/player_activity.cpp
@@ -203,6 +203,7 @@ cata::optional<std::string> player_activity::get_progress_message( const avatar 
             type == activity_id( "ACT_JACKHAMMER" ) ||
             type == activity_id( "ACT_PICKAXE" ) ||
             type == activity_id( "ACT_DISASSEMBLE" ) ||
+            type == activity_id( "ACT_VEHICLE" ) ||
             type == activity_id( "ACT_FILL_PIT" ) ||
             type == activity_id( "ACT_DIG" ) ||
             type == activity_id( "ACT_DIG_CHANNEL" ) ||
