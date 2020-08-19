e60b59cbe9f1cb4fb08126214bda0c5f1adf0078
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2020-08-19 09:45:46 +0300

    Astyle

--- src/worldfactory.cpp
+++ src/worldfactory.cpp
@@ -1496,7 +1496,7 @@ bool WORLD::load_options()
     using namespace std::placeholders;
     const std::string path = folder_path() + "/" + PATH_INFO::worldoptions();
     return read_from_file_optional_json( path, [this]( JsonIn & jsin ) {
-    this->load_options( jsin );
+        this->load_options( jsin );
     } );
 }
 
