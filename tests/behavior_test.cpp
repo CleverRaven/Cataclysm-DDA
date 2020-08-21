959886d18496010d9b87edb89f038df377ab9453
Kevin Granade <kevin.granade@gmail.com>
2020-08-21 05:33:46 +0000

    Update behavior_test.cpp

--- tests/behavior_test.cpp
+++ tests/behavior_test.cpp
@@ -155,7 +155,7 @@ TEST_CASE( "check_npc_behavior_tree", "[npc][behavior]" )
         weather_manager &weather = get_weather();
         weather.temperature = 0;
         weather.clear_temp_cache();
-        REQUIRE( weather.get_temperature() == 0 );
+        REQUIRE( weather.get_temperature( test_npc.pos() ) == 0 );
         test_npc.update_bodytemp();
         REQUIRE( oracle.needs_warmth_badly( "" ) == behavior::status_t::running );
         CHECK( npc_needs.tick( &oracle ) == "idle" );
