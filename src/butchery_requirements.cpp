a1a0bf03b48414ae76fa5744db981949cbbff22b
souricelle <67675144+souricelle@users.noreply.github.com>
2020-08-21 20:00:27 -0700

    Butchery requirements 3 (#42837)
    * Jsonized butchery reqs
    
    This is a first pass, still a bug left to catch.
    
    * More bugfixing
    
    Added CUT requirements and removed a few buggy lines
    
    * More bugfixes
    
    Added no_surface to the no surface butchery IDs.
    
    * Update butchery.json
    
    * Update butchery_requirements.json
    
    * Update butchery_requirements.json
    
    Fiddling with the butchery time modifiers.
    
    * Changed get_fastest_requirements
    
    the function was arranging values in reverse accidentally
    
    * Update butchery_requirements.json
    
    * Update butchery_requirements.json
    
    * lint
    
    * Update butchery_requirements.cpp
    
    * Update butchery_requirements.cpp
    
    * Update butchery_requirements.cpp
    
    * Update butchery_requirements.cpp
    
    Co-authored-by: KorGgenT <curtis.r.merrill@gmail.com>

--- src/butchery_requirements.cpp
+++ src/butchery_requirements.cpp
@@ -96,13 +96,14 @@ void butchery_requirements::check_consistency()
 std::pair<float, requirement_id> butchery_requirements::get_fastest_requirements(
     const inventory &crafting_inv, creature_size size, butcher_type butcher ) const
 {
-    for( auto riter = requirements.rbegin(); riter != requirements.rend(); ++riter ) {
-        if( riter->second.at( size ).at( butcher )->can_make_with_inventory( crafting_inv,
+    for( const std::pair<const float, std::map<creature_size, std::map<butcher_type, requirement_id>>>
+         &riter : requirements ) {
+        if( riter.second.at( size ).at( butcher )->can_make_with_inventory( crafting_inv,
                 is_crafting_component ) ) {
-            return std::make_pair( riter->first, riter->second.at( size ).at( butcher ) );
+            return std::make_pair( riter.first, riter.second.at( size ).at( butcher ) );
         }
     }
     // we didn't find anything we could "craft", so return the requirement that's the fastest
-    const auto first = requirements.begin();
+    const auto first = requirements.end();
     return std::make_pair( first->first, first->second.at( size ).at( butcher ) );
 }
