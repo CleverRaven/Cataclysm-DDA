28e6a88a59db1c10287a2ce7dfd94dda6bac8b42
TheGoatGod <56883303+TheGoatGod@users.noreply.github.com>
2020-08-26 21:59:46 +0100

    Update src/iexamine.cpp
    Co-authored-by: Alexey Mostovoy <1931904+AMurkin@users.noreply.github.com>

--- src/iexamine.cpp
+++ src/iexamine.cpp
@@ -4004,7 +4004,7 @@ void iexamine::curtains( player &p, const tripoint &examp )
     } else if( choice == 1 ) {
         // Mr. Gorbachev, tear down those curtains!
         if (here.ter(examp)->has_curtains()) {
-            here.ter_set(examp, here.ter(examp)->curtain_transform();
+            here.ter_set(examp, here.ter(examp)->curtain_transform;
         }
 
         here.spawn_item( p.pos(), itype_nail, 1, 4, calendar::turn );
