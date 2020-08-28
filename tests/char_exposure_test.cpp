fb13634ee80bddc1091911805754036b5a472803
Eric Pierce <wapcaplet88@gmail.com>
2020-08-28 15:26:51 -0600

    Remove unnecessary percent-sign escaping

--- tests/char_exposure_test.cpp
+++ tests/char_exposure_test.cpp
@@ -29,14 +29,14 @@ TEST_CASE( "character body part exposure", "[char][bodypart][exposure]" )
     GIVEN( "nothing is worn" ) {
         dummy.worn.clear();
 
-        THEN( "exposure is 100%% on all body parts" ) {
+        THEN( "exposure is 100% on all body parts" ) {
             bp_exposure = dummy.bodypart_exposure();
             for( std::pair<const bodypart_id, float> &bp_exp : bp_exposure ) {
                 CHECK( bp_exp.second == 1.0 );
             }
         }
 
-        WHEN( "wearing an item with 100%% coverage on some parts" ) {
+        WHEN( "wearing an item with 100% coverage on some parts" ) {
             item socks( "test_socks" );
             REQUIRE( socks.get_coverage( foot_l ) == 100 );
             REQUIRE( socks.get_coverage( foot_r ) == 100 );
@@ -49,13 +49,13 @@ TEST_CASE( "character body part exposure", "[char][bodypart][exposure]" )
                 CHECK( bp_exposure[foot_l] == 0.0f );
             }
 
-            THEN( "exposure is 100%% on exposed parts" ) {
+            THEN( "exposure is 100% on exposed parts" ) {
                 CHECK( bp_exposure[hand_l] == 1.0f );
                 CHECK( bp_exposure[hand_r] == 1.0f );
             }
         }
 
-        WHEN( "wearing an item with 50%% coverage on some parts" ) {
+        WHEN( "wearing an item with 50% coverage on some parts" ) {
             item croc_socks( "test_croc_socks" );
             REQUIRE( croc_socks.get_coverage( foot_l ) == 50 );
             REQUIRE( croc_socks.get_coverage( foot_r ) == 50 );
@@ -63,25 +63,25 @@ TEST_CASE( "character body part exposure", "[char][bodypart][exposure]" )
 
             bp_exposure = dummy.bodypart_exposure();
 
-            THEN( "exposure is 50%% on covered parts" ) {
+            THEN( "exposure is 50% on covered parts" ) {
                 CHECK( bp_exposure[foot_l] == 0.5f );
                 CHECK( bp_exposure[foot_r] == 0.5f );
             }
 
-            AND_WHEN( "wearing another item with 50%% coverage on the same parts" ) {
+            AND_WHEN( "wearing another item with 50% coverage on the same parts" ) {
                 item croc_socks2( "test_croc_socks" );
                 dummy.wear_item( croc_socks2, false );
 
                 bp_exposure = dummy.bodypart_exposure();
 
-                THEN( "exposure is reduced to 25%% on doubly-covered parts" ) {
+                THEN( "exposure is reduced to 25% on doubly-covered parts" ) {
                     CHECK( bp_exposure[foot_l] == 0.25f );
                     CHECK( bp_exposure[foot_r] == 0.25f );
                 }
             }
         }
 
-        WHEN( "wearing an item with 90%% coverage on some parts" ) {
+        WHEN( "wearing an item with 90% coverage on some parts" ) {
             item shirt( "test_longshirt" );
             REQUIRE( shirt.get_coverage( torso ) == 90 );
             REQUIRE( shirt.get_coverage( arm_l ) == 90 );
@@ -90,12 +90,12 @@ TEST_CASE( "character body part exposure", "[char][bodypart][exposure]" )
 
             bp_exposure = dummy.bodypart_exposure();
 
-            THEN( "exposure is 10%% on covered parts" ) {
+            THEN( "exposure is 10% on covered parts" ) {
                 CHECK( bp_exposure[torso] == Approx( 0.1f ) );
                 CHECK( bp_exposure[arm_l] == Approx( 0.1f ) );
                 CHECK( bp_exposure[arm_r] == Approx( 0.1f ) );
             }
-            THEN( "exposure is 100%% on exposed parts" ) {
+            THEN( "exposure is 100% on exposed parts" ) {
                 CHECK( bp_exposure[head] == 1.0f );
                 CHECK( bp_exposure[leg_l] == 1.0f );
                 CHECK( bp_exposure[leg_r] == 1.0f );
@@ -105,13 +105,13 @@ TEST_CASE( "character body part exposure", "[char][bodypart][exposure]" )
                 CHECK( bp_exposure[foot_r] == 1.0f );
             }
 
-            AND_WHEN( "wearing another item with 90%% coverage on the same parts" ) {
+            AND_WHEN( "wearing another item with 90% coverage on the same parts" ) {
                 item shirt2( "test_longshirt" );
                 dummy.wear_item( shirt2, false );
 
                 bp_exposure = dummy.bodypart_exposure();
 
-                THEN( "exposure is reduced to 1%% on doubly-covered parts" ) {
+                THEN( "exposure is reduced to 1% on doubly-covered parts" ) {
                     CHECK( bp_exposure[torso] == Approx( 0.01f ) );
                     CHECK( bp_exposure[arm_l] == Approx( 0.01f ) );
                     CHECK( bp_exposure[arm_r] == Approx( 0.01f ) );
