4439925d91ee84b7d69663beb11b7131b7184bcd
John Bytheway <jbytheway@gmail.com>
2020-09-11 00:56:03 -0400

    Add debugmsg problem matcher
    This problem matcher is intended to highlight errors coming from hitting
    debugmsg in CI.

--- build-scripts/requirements.sh
+++ build-scripts/requirements.sh
@@ -16,6 +16,10 @@ function just_json
     return 0
 }
 
+# Enable GitHub actions problem matcher
+# (See https://github.com/actions/toolkit/blob/master/docs/problem-matchers.md)
+echo "::add-matcher::build-scripts/problem-matchers/debugmsg.json"
+
 if which travis_retry &>/dev/null
 then
     travis_retry=travis_retry
