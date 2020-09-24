9cafcdfc71cfc115752f046410f6d79b796716cd
George Karfakis <gk819@ic.ac.uk>
2020-09-24 17:52:27 +0300

    to satisfy the holy compiler.

--- src/game_inventory.cpp
+++ src/game_inventory.cpp
@@ -535,8 +535,8 @@ class comestible_inventory_preset : public inventory_selector_preset
             }, _( "VOLUME" ) );
 
             // Title of this cell. Defined here in order to preserve proper padding and alignment of values in the lambda.
-            constexpr char *nourishment_title = "NOURISHMENT";
-            append_cell( [&p, nourishment_title]( const item_location & loc ) {
+            constexpr char *nourishment_title = (char*)"NOURISHMENT";
+            append_cell( [&p, &nourishment_title]( const item_location & loc ) {
                 /* Understanding how Calories Per Effective Volume are calculated requires a dive into the
                 stomach fullness source code. Look at issue #44365*/
                 const item &it = *loc;
