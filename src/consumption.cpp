a5fdc6c7d8511a1218271f34104a74565ff0a5f8
George Karfakis <george.karfakis@gmail.com>
2020-09-29 22:22:03 +0100

    astyle and a zero

--- src/consumption.cpp
+++ src/consumption.cpp
@@ -1271,21 +1271,21 @@ double Character::compute_effective_food_volume_ratio( const item &food ) const
 }
 
 // Used when displaying effective food satiation values.
-int Character::compute_calories_per_effective_volume(const item& food) const
+int Character::compute_calories_per_effective_volume( const item &food ) const
 {
     /* Understanding how Calories Per Effective Volume are calculated requires a dive into the
     stomach fullness source code. Look at issue #44365*/
-    const nutrients nutr = compute_effective_nutrients(food);
+    const nutrients nutr = compute_effective_nutrients( food );
     const int kcalories = nutr.kcal;
-    units::volume water_vol = (food.type->comestible->quench > 0) ? food.type->comestible->quench *
-        5_ml : 0_ml;
+    units::volume water_vol = ( food.type->comestible->quench > 0 ) ? food.type->comestible->quench *
+                              5_ml : 0_ml;
     // Water volume is ignored.
     units::volume food_vol = food.volume() - water_vol * food.count();
     // Divide by 1000 to convert to L. Final quantity is essentially dimensionless, so unit of measurement does not matter.
-    const double converted_volume = round_up((food_vol.value() / food.count()) / 1000, 2);
-    const double energy_density_ratio = compute_effective_food_volume_ratio(food);
+    const double converted_volume = round_up( ( food_vol.value() / food.count() ) * 0.001, 2 );
+    const double energy_density_ratio = compute_effective_food_volume_ratio( food );
     const double effective_volume = converted_volume * energy_density_ratio;
-    return std::round(kcalories / effective_volume);
+    return std::round( kcalories / effective_volume );
 }
 
 
