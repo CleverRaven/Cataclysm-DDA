e503cb22e8f354fd14611f9ee49d6c14fbd80f0b
Aivean <aiveeen@gmail.com>
2020-09-29 18:14:45 -0700

    fix a bug when one of the tiles lighted by the sun shining through a single-tile hole appears to be dark

--- src/map.h
+++ src/map.h
@@ -175,7 +175,13 @@ struct level_cache {
     // To prevent redundant ray casting into neighbors: precalculate bulk light source positions.
     // This is only valid for the duration of generate_lightmap
     float light_source_buffer[MAPSIZE_X][MAPSIZE_Y];
+
+    // if false, means tile is under the roof ("inside"), true means tile is "outside"
+    // "inside" tiles are protected from sun, rain, etc. (see "INDOORS" flag)
     bool outside_cache[MAPSIZE_X][MAPSIZE_Y];
+
+    // false, if corresponding terrain has flag "NO_FLOOR", true otherwise
+    // i.e. true == has floor
     bool floor_cache[MAPSIZE_X][MAPSIZE_Y];
 
     // stores cached transparency of the tiles
