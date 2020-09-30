6bba669290bd6cc16e9eee9bd9cfe83f0844d99c
Hirmuolio <hirmuolio@gmail.com>
2020-09-30 10:44:18 +0300

    renamed alternator belt fault flag

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -1192,7 +1192,7 @@ bool vehicle::is_alternator_on( const int a ) const
         //fuel_left checks that the engine can produce power to be absorbed
         return eng.mount == alt.mount && eng.is_available() && eng.enabled &&
                fuel_left( eng.fuel_current() ) &&
-               !eng.has_fault_flag( "NO_ALTERNATOR_BELT" );
+               !eng.has_fault_flag( "NO_ALTERNATOR_CHARGE" );
     } );
 }
 
