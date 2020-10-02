1cc8a3aa53a71f5f6c954c185d9c7d44c20dd661
George Karfakis <george.karfakis@gmail.com>
2020-10-02 17:52:00 +0100

    Initial attempt at making it work for the crafting window as well.
    The old compute_calories_per_effective volume now gets an optional parameter that lets it accept an external nutrient (kcal) value, and as such, it works with the crafting menus calorie ranges min_cal - max_cal.

--- src/item.cpp
+++ src/item.cpp
@@ -1831,8 +1831,16 @@ void item::food_info( const item *food_item, std::vector<iteminfo> &info,
                                       food_item->get_comestible()->quench ) );
         }
         if( parts->test( iteminfo_parts::FOOD_SATIATION ) ) {
-            info.push_back( iteminfo( "FOOD", _( "<bold>Satiety: </bold>" ),
-                                      satiety_bar( player_character.compute_calories_per_effective_volume( *food_item ) ) ) );
+
+            if( max_nutr.kcal == min_nutr.kcal ) {
+                info.push_back( iteminfo( "FOOD", _( "<bold>Satiety: </bold>" ),
+                                          satiety_bar( player_character.compute_calories_per_effective_volume( *food_item ) ) ) );
+            } else {
+                info.push_back( iteminfo( "FOOD", _( "<bold>Satiety: </bold>" ), "", iteminfo::no_newline,
+                                          satiety_bar( player_character.compute_calories_per_effective_volume( *food_item, &min_nutr ) ) ) );
+                info.push_back( iteminfo( "FOOD", _( "-" ),
+                                          satiety_bar( player_character.compute_calories_per_effective_volume( *food_item, &max_nutr ) ) ) );
+            }
         }
     }
 
