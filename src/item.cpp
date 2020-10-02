ee29b0d5ac31e4bf3dbf6805f0a27bb949359edd
George Karfakis <george.karfakis@gmail.com>
2020-10-02 16:48:18 +0100

    Init

--- src/item.cpp
+++ src/item.cpp
@@ -1830,6 +1830,10 @@ void item::food_info( const item *food_item, std::vector<iteminfo> &info,
             info.push_back( iteminfo( "FOOD", space + _( "Quench: " ),
                                       food_item->get_comestible()->quench ) );
         }
+        if( parts->test( iteminfo_parts::FOOD_SATIATION ) ) {
+            info.push_back( iteminfo( "FOOD", _( "<bold>Satiety: </bold> " ),
+                            satiety_bar( player_character.compute_calories_per_effective_volume( *food_item ) ) ) );
+        }
     }
 
     const std::pair<int, int> fun_for_food_item = player_character.fun_for( *food_item );
