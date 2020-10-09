f094e09f998d7dc497575f6891232581498ff409
mqrause <martin@qrause.de>
2020-10-09 12:41:40 +0200

    use remaining_ammo_capacity in item_pocket::can_contain

--- src/item_pocket.cpp
+++ src/item_pocket.cpp
@@ -1009,26 +1009,13 @@ ret_val<item_pocket::contain_code> item_pocket::can_contain( const item &it ) co
         const ammotype it_ammo = it.ammo_type();
         const auto ammo_restriction_iter = data->ammo_restriction.find( it.ammo_type() );
 
-        if( ammo_restriction_iter == data->ammo_restriction.end() ) {
+        if( ammo_restriction_iter == data->ammo_restriction.end() || ( !contents.empty() &&
+                it_ammo != contents.front().ammo_type() ) ) {
             return ret_val<item_pocket::contain_code>::make_failure(
                        contain_code::ERR_AMMO, _( "item is not the correct ammo type" ) );
         }
 
-        // how much ammo is inside the pocket
-        int internal_count = 0;
-        // the ammo must match what's inside
-        if( !contents.empty() ) {
-            if( it_ammo != contents.front().ammo_type() ) {
-                return ret_val<item_pocket::contain_code>::make_failure(
-                           contain_code::ERR_AMMO, _( "item is not the correct ammo type" ) );
-            } else {
-                for( const item &cont_it : contents ) {
-                    internal_count += cont_it.count();
-                }
-            }
-        }
-
-        if( it.count() + internal_count > ammo_restriction_iter->second ) {
+        if( it.count() > remaining_ammo_capacity( it_ammo ) ) {
             return ret_val<item_pocket::contain_code>::make_failure(
                        contain_code::ERR_NO_SPACE, _( "tried to put too many charges of ammo in item" ) );
         }
