662d1cc9f9ce0dad1ee2f9bd803d478004ab1c32
Ramza13 <52087122+Ramza13@users.noreply.github.com>
2020-10-18 20:25:26 -0400

    Update activity_item_handling.cpp

--- src/activity_item_handling.cpp
+++ src/activity_item_handling.cpp
@@ -2868,7 +2868,7 @@ int get_auto_consume_moves( player &p, const bool food )
         }
 
         const optional_vpart_position vp = here.veh_at( here.getlocal( loc ) );
-        std::vector<item*> items_here;
+        std::vector<item *> items_here;
         if( vp ) {
             vehicle_stack vehitems = vp->vehicle().get_items( vp->vehicle().part_with_feature( vp->part_index(),
                                      "CARGO", false ) );
