34ac2b18d1f5d13c38df4d10d5c051eef472d93a
Henrik "Tivec" Bergvin <henrik@bergvin.com>
2020-10-21 09:24:08 +0200

    astyle

--- src/activity_item_handling.cpp
+++ src/activity_item_handling.cpp
@@ -111,7 +111,7 @@ static const zone_type_id zone_type_FARM_PLOT( "FARM_PLOT" );
 static const zone_type_id zone_type_FISHING_SPOT( "FISHING_SPOT" );
 static const zone_type_id zone_type_LOOT_CORPSE( "LOOT_CORPSE" );
 static const zone_type_id zone_type_LOOT_IGNORE( "LOOT_IGNORE" );
-static const zone_type_id zone_type_LOOT_IGNORE_FAVORITES("LOOT_IGNORE_FAVORITES");
+static const zone_type_id zone_type_LOOT_IGNORE_FAVORITES( "LOOT_IGNORE_FAVORITES" );
 static const zone_type_id zone_type_MINING( "MINING" );
 static const zone_type_id zone_type_LOOT_UNSORTED( "LOOT_UNSORTED" );
 static const zone_type_id zone_type_LOOT_WOOD( "LOOT_WOOD" );
@@ -2017,10 +2017,10 @@ void activity_on_turn_move_loot( player_activity &act, player &p )
             }
 
             // skip favorite items in ignore favorite zones
-            if (thisitem.is_favorite && mgr.has(zone_type_LOOT_IGNORE_FAVORITES, src)) {
+            if( thisitem.is_favorite && mgr.has( zone_type_LOOT_IGNORE_FAVORITES, src ) ) {
                 continue;
             }
-                
+
 
             // Only if it's from a vehicle do we use the vehicle source location information.
             vehicle *this_veh = it->second ? src_veh : nullptr;
