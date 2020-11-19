bd3225d6d250d61d3da54f4f16e179b8d2a32529
firestorm01x2 <firestorm01x2gmail.com>
2020-11-19 17:06:30 +0300

    astyle
    astyle

--- src/weather_gen.cpp
+++ src/weather_gen.cpp
@@ -330,10 +330,10 @@ void weather_generator::test_weather( unsigned seed = 1000 ) const
 weather_generator weather_generator::load( const JsonObject &jo )
 {
     weather_generator ret;
-    ret.spring_temp = jo.get_int("spring_temp", 0);
-    ret.summer_temp = jo.get_int("summer_temp", 0);
-    ret.autumn_temp = jo.get_int("autumn_temp", 0);
-    ret.winter_temp = jo.get_int("winter_temp", 0);
+    ret.spring_temp = jo.get_int( "spring_temp", 0 );
+    ret.summer_temp = jo.get_int( "summer_temp", 0 );
+    ret.autumn_temp = jo.get_int( "autumn_temp", 0 );
+    ret.winter_temp = jo.get_int( "winter_temp", 0 );
     ret.base_humidity = jo.get_float( "base_humidity", 50.0 );
     ret.base_pressure = jo.get_float( "base_pressure", 0.0 );
     ret.base_acid = jo.get_float( "base_acid", 0.0 );
