09dcb8ec464ed2c24cef810982222eb2317f9d43
firestorm01x2 <firestorm01x2gmail.com>
2020-11-19 21:22:10 +0300

    Warning attemp
    Warning attemp

--- src/weather_gen.cpp
+++ src/weather_gen.cpp
@@ -331,10 +331,14 @@ void weather_generator::test_weather( unsigned seed = 1000 ) const
 weather_generator weather_generator::load( const JsonObject &jo )
 {
     weather_generator ret;
-    ret.spring_temp = jo.get_float( "spring_temp", 0.0f );
-    ret.summer_temp = jo.get_float( "summer_temp", 0.0f );
-    ret.autumn_temp = jo.get_float( "autumn_temp", 0.0f );
-    ret.winter_temp = jo.get_float( "winter_temp", 0.0f );
+    ret.spring_temp = jo.get_float( "spring_temp", 7.0f );
+    ret.summer_temp = jo.get_float( "summer_temp", 16.0f );
+    ret.autumn_temp = jo.get_float( "autumn_temp", 6.0f );
+    ret.winter_temp = jo.get_float( "winter_temp", -14.0f );
+    if( jo.has_number( "base_temperature" ) ) {
+        jo.throw_error(
+            std::string( "base_temperature is oudtaded. Use spring_temp, summer_temp, autumn_temp, winter_temp instead " ) );
+    }
     ret.base_humidity = jo.get_float( "base_humidity", 50.0 );
     ret.base_pressure = jo.get_float( "base_pressure", 0.0 );
     ret.base_acid = jo.get_float( "base_acid", 0.0 );
