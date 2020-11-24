c35b11cf350a5529cf1f51f9f74c60764e312861
Тимур Кашафутдинов <leemuar@yandex-team.ru>
2020-11-24 21:59:29 +0300

    Fix typo in item id

--- src/bionics.cpp
+++ src/bionics.cpp
@@ -909,7 +909,7 @@ bool Character::activate_bionic( int b, bool eff_only )
                     add_msg_if_player( m_info,
                                        _( "You have a cable plugged to a portable power source, but you need to plug it in to the CBM." ) );
                 }
-                if( state == "pay_oyt_cable" ) {
+                if( state == "pay_out_cable" ) {
                     add_msg_if_player( m_info,
                                        _( "You have a cable plugged to a vehicle, but you need to plug it in to the CBM." ) );
                 }
