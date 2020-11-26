e53fa4753f9deb9d85056d9c2ba688faa510d2fd
PatrikLundell <j.patrik.r.lundell@gmail.com>
2020-11-26 18:46:55 +0100

    swap variables to use of swap operation

--- src/faction_camp.cpp
+++ src/faction_camp.cpp
@@ -547,18 +547,11 @@ recipe_id base_camps::select_camp_option( const std::map<recipe_id, translation>
         return pos_name_ids.front();
     }
 
-    std::string swap_name;
-    recipe_id swap_id;
-
     for( size_t i = 0; i < pos_names.size() - 1; i++ ) {
         for( size_t k = i + 1; k < pos_names.size(); k++ ) {
             if( localized_compare( pos_names[k], pos_names[i] ) ) {
-                swap_name = pos_names[k];
-                swap_id = pos_name_ids[k];
-                pos_names[k] = pos_names[i];
-                pos_name_ids[k] = pos_name_ids[i];
-                pos_names[i] = swap_name;
-                pos_name_ids[i] = swap_id;
+                std::swap( pos_names[i], pos_names[k] );
+                std::swap( pos_name_ids[i], pos_name_ids[k] );
             }
         }
     }
