bbf91c510c85b9dd32192ed4b5511063563c182c
andrei <a12l+git@runbox.com>
2020-11-28 16:22:36 +0200

    character: slightly improve find_ammo helper
    only skip nodes if they are actually loaded as magazines
    
    fixes #45689

--- src/character.cpp
+++ src/character.cpp
@@ -3281,10 +3281,6 @@ void find_ammo_helper( T &src, const item &obj, bool empty, Output out, bool nes
         std::set<ammotype> ammo = obj.ammo_types();
 
         src.visit_items( [&src, &nested, &out, ammo]( item * node, item * parent ) {
-            if( node->is_gun() || node->is_tool() ) {
-                // guns/tools never contain usable ammo so most efficient to skip them now
-                return VisitResponse::SKIP;
-            }
             if( !node->made_of_from_type( phase_id::SOLID ) && parent == nullptr ) {
                 // some liquids are ammo but we can't reload with them unless within a container or frozen
                 return VisitResponse::SKIP;
@@ -3313,7 +3309,9 @@ void find_ammo_helper( T &src, const item &obj, bool empty, Output out, bool nes
                     out = item_location( src, node );
                 }
             }
-            if( node->is_magazine() && node->has_flag( flag_SPEEDLOADER ) ) {
+            if( node->is_magazine() &&
+                ( parent == nullptr || node != parent->magazine_current() ) &&
+                node->has_flag( flag_SPEEDLOADER ) ) {
                 if( node->ammo_remaining() ) {
                     out = item_location( src, node );
                 }
@@ -3324,11 +3322,9 @@ void find_ammo_helper( T &src, const item &obj, bool empty, Output out, bool nes
         // find compatible magazines excluding those already loaded in tools/guns
         const auto mags = obj.magazine_compatible();
 
-        src.visit_items( [&src, &nested, &out, mags, empty]( item * node ) {
-            if( node->is_gun() || node->is_tool() ) {
-                return VisitResponse::SKIP;
-            }
-            if( node->is_magazine() ) {
+        src.visit_items( [&src, &nested, &out, mags, empty]( item * node, item * parent ) {
+            if( node->is_magazine() &&
+                ( parent == nullptr || node != parent->magazine_current() ) ) {
                 if( mags.count( node->typeId() ) && ( node->ammo_remaining() || empty ) ) {
                     out = item_location( src, node );
                 }
