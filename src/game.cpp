d5faf79d5c6bc51780d8c2c1fc8fe031a71ce8cd
Valiant <Night_Pryanik@mail.ru>
2020-11-28 19:32:23 +0400

    Astyle

--- src/game.cpp
+++ src/game.cpp
@@ -6419,8 +6419,8 @@ void game::print_graffiti_info( const tripoint &lp, const catacurses::window &w_
     const int max_width = getmaxx( w_look ) - column - 2;
     if( m.has_graffiti_at( lp ) ) {
         const int lines = fold_and_print( w_look, point( column, ++line ), max_width, c_light_gray,
-                        m.ter( lp ) == t_grave_new ? _( "Graffiti: %s" ) : _( "Inscription: %s" ),
-                        m.graffiti_at( lp ) );
+                                          m.ter( lp ) == t_grave_new ? _( "Graffiti: %s" ) : _( "Inscription: %s" ),
+                                          m.graffiti_at( lp ) );
         line += lines - 1;
     }
 }
