18529187b152ea2f9ae3f8689449dd0d20a44c2f
Fris0uman <sthapplz@gmail.com>
2020-12-02 18:54:36 +0100

    use correct for loop

--- src/crafting.cpp
+++ src/crafting.cpp
@@ -1738,14 +1738,14 @@ std::list<item> Character::consume_items( const std::vector<item_comp> &componen
 
 bool Character::consume_software_container( const itype_id &software_id )
 {
-    for( auto it = all_items_loc().begin(); it != all_items_loc().end(); ++it ) {
-        if( !it->get_item() ) {
+    for( item_location it : all_items_loc() ) {
+        if( !it.get_item() ) {
             continue;
         }
-        if( it->get_item()->is_software_storage() ) {
-            for( const item *soft : it->get_item()->softwares() ) {
+        if( it.get_item()->is_software_storage() ) {
+            for( const item *soft : it.get_item()->softwares() ) {
                 if( soft->typeId() == software_id ) {
-                    it->remove_item();
+                    it.remove_item();
                     return true;
                 }
             }
