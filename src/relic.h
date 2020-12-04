bcb50eeb7fbd65a980aace78286141cd2786d2b9
Middo <53809752+MikuMiddo@users.noreply.github.com>
2020-12-04 09:11:43 +0800

    Update src/relic.h
    Co-authored-by: anothersimulacrum <anothersimulacrum@gmail.com>

--- src/relic.h
+++ src/relic.h
@@ -224,7 +224,7 @@ class relic
         // what is the power level of this artifact, given a specific ruleset
         int power_level( const relic_procgen_id &ruleset ) const;
 
-        friend bool operator==( relic source_relic, relic &target_relic );
+        friend bool operator==( const relic &source_relic, const relic &target_relic );
 };
 
 template <typename E> struct enum_traits;
