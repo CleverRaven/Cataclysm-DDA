9d0b60e2b28c9f0e0aa7127146c9f7b920afee5b
Ryan Breuer <breuerryan@gmail.com>
2020-12-06 18:22:28 -0600

    astyle fixes

--- src/string_formatter.h
+++ src/string_formatter.h
@@ -282,14 +282,14 @@ class string_formatter
                 case 'i':
                     add_long_long_length_modifier();
                     return do_formatting( get_nth_arg_as<signed long long int, 0>( format_arg_index,
-                                         std::forward<Args>( args )... ) );
+                                          std::forward<Args>( args )... ) );
                 case 'o':
                 case 'u':
                 case 'x':
                 case 'X':
                     add_long_long_length_modifier();
                     return do_formatting( get_nth_arg_as<unsigned long long int, 0>( format_arg_index,
-                                         std::forward<Args>( args )... ) );
+                                          std::forward<Args>( args )... ) );
                 case 'a':
                 case 'A':
                 case 'g':
@@ -298,13 +298,14 @@ class string_formatter
                 case 'F':
                 case 'e':
                 case 'E':
-                    return do_formatting( get_nth_arg_as<double, 0>( format_arg_index, std::forward<Args>( args )... ) );
+                    return do_formatting( get_nth_arg_as<double, 0>( format_arg_index,
+                                          std::forward<Args>( args )... ) );
                 case 'p':
                     return do_formatting( get_nth_arg_as<void *, 0>( format_arg_index,
-                                         std::forward<Args>( args )... ) );
+                                          std::forward<Args>( args )... ) );
                 case 's':
                     return do_formatting( get_nth_arg_as<const char *, 0>( format_arg_index,
-                                         std::forward<Args>( args )... ) );
+                                          std::forward<Args>( args )... ) );
                 default:
                     throw_error( "Unsupported format conversion: " + std::string( 1, c ) );
             }
