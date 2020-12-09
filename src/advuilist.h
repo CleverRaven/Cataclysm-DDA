CleverRaven/Cataclysm-DDA

commit 4ef891838b1dc539ea2d74a75e5562b5445f343f

advuilist (and friends): screen resizing support
why is it so ugly. who wrote this crap?

src/advuilist.h
@@ -67,7 +67,7 @@ class advuilist
         using select_t = std::vector<selection_t>;
 
         advuilist( Container *list, point size = { -9, -9 }, point origin = { -9, -9 },
-                   std::string const &ctxtname = advuilist_literals::CTXT_DEFAULT, bool init = true );
+                   std::string const &ctxtname = advuilist_literals::CTXT_DEFAULT );
 
         /// sets up columns and optionally sets up implict sorters
         ///@param columns
@@ -92,6 +92,9 @@ class advuilist
         /// if set, func gets called after all internal printing calls and before wnoutrefresh(). This
         /// is meant to be used for drawing extra decorations or stats
         void on_redraw( fdraw_t const &func );
+        /// if set, func will get called on screen resize instead of running resize() directly. Use
+        /// this if window size/pos depends on external variables such as TERMX/Y
+        void on_resize( fdraw_t const &func );
         /// sets the filtering function
         void setfilterf( filter_t const &func );
         select_t select();
@@ -106,6 +109,7 @@ class advuilist
         /// pre-initialize or reset the internal ui_adaptor;
         void initui();
         void hide();
+        void resize( point size, point origin );
 
         input_context *get_ctxt();
         catacurses::window *get_window();
@@ -114,11 +118,6 @@ class advuilist
 
         void savestate( advuilist_save_state *state );
         void loadstate( advuilist_save_state *state, bool reb = true );
-
-    protected:
-        // semi-hacks: functions needed due to initialization order in advuilist_sourced
-        void _init();
-        void _resize( point size, point origin );
 
     private:
         /// pair of index, pointer. index is used for "none" sorting mode and is not meant to represent
@@ -136,8 +135,8 @@ class advuilist
         using groupercont_t = std::vector<grouper_t>;
         using pagecont_t = std::vector<page_t>;
 
-        point _size;
-        point _origin;
+        point _size, _osize;
+        point _origin, _oorigin;
         std::size_t _pagesize = 0;
         list_t _list;
         colscont_t _columns;
@@ -148,7 +147,7 @@ class advuilist
         ffilter_t _ffilter;
         fcounter_t _fcounter;
         frebuild_t _frebuild;
-        fdraw_t _fdraw;
+        fdraw_t _fdraw, _fresize;
         fctxt_t _fctxt;
         std::string _filter;
         std::string _filterdsc;
@@ -209,13 +208,9 @@ class advuilist
 
 template <class Container, typename T>
 advuilist<Container, T>::advuilist( Container *list, point size, point origin,
-                                    std::string const &ctxtname, bool init )
-    : _size( size.x > 0 ? size.x : TERMX / 4, size.y > 0 ? size.y : TERMY / 4 ),
-      _origin( origin.x >= 0 ? origin.x : TERMX / 2 - _size.x / 2,
-               origin.y >= 0 ? origin.y : TERMY / 2 - _size.y / 2 ),
-      // leave space for decorations and column headers
-      _pagesize(
-          static_cast<std::size_t>( std::max( 0, _size.y - ( _headersize + _footersize + 1 ) ) ) ),
+                                    std::string const &ctxtname)
+    : _osize( size ),
+      _oorigin( origin ),
       // insert dummy sorter for "none" sort mode
       _sorters{ sorter_t{ "none", fsort_t() } },
       // insert dummy grouper for "none" grouping mode
@@ -236,10 +231,7 @@ advuilist<Container, T>::advuilist( Container *list, point size, point origin,
 // *INDENT-ON*
 
 {
-    if( init )
-    {
-        _init();
-    }
+    _initctxt();
 }
 
 template <class Container, typename T>
@@ -320,6 +312,12 @@ template <class Container, typename T>
 void advuilist<Container, T>::on_redraw( fdraw_t const &func )
 {
     _fdraw = func;
+}
+
+template <class Container, typename T>
+void advuilist<Container, T>::on_resize( fdraw_t const &func )
+{
+    _fresize = func;
 }
 
 template <class Container, typename T>
@@ -434,6 +432,11 @@ void advuilist<Container, T>::initui()
 {
     _ui = std::make_shared<ui_adaptor>();
     _ui->on_screen_resize( [&]( ui_adaptor & ui ) {
+        if( _fresize ) {
+            _fresize( this );
+        } else {
+            resize( _osize, _oorigin );
+        }
         _w = catacurses::newwin( _size.y, _size.x, _origin );
         ui.position_from_window( _w );
     } );
@@ -454,6 +457,29 @@ template <class Container, typename T>
 void advuilist<Container, T>::hide()
 {
     _ui.reset();
+}
+
+template <class Container, typename T>
+void advuilist<Container, T>::resize( point size, point origin )
+{
+    _size = { size.x > 0 ? size.x > TERMX ? TERMX : size.x : TERMX / 4,
+              size.y > 0 ? size.y > TERMY ? TERMY : size.y : TERMY / 4
+            };
+    _origin = { origin.x >= 0 ? origin.x + size.x > TERMX ? 0 : origin.x : TERMX / 2 - _size.x / 2,
+                origin.y >= 0 ? origin.y + size.y > TERMY ? 0 : origin.y
+                : TERMY / 2 - _size.y / 2
+              };
+    // leave space for decorations and column headers
+    _pagesize =
+        static_cast<std::size_t>( std::max( 0, _size.y - ( _headersize + _footersize + 1 ) ) );
+
+    _innerw = _size.x - _firstcol * 2;
+    std::size_t const npagesize =
+        static_cast<std::size_t>( std::max( 0, _size.y - ( _headersize + _footersize + 1 ) ) );
+    if( npagesize != _pagesize ) {
+        _pagesize = npagesize;
+        rebuild( _olist );
+    }
 }
 
 template <class Container, typename T>
@@ -535,23 +561,6 @@ template <class Container, typename T>
 std::size_t advuilist<Container, T>::_peekcount()
 {
     return _count( _cidx );
-}
-
-template <class Container, typename T>
-void advuilist<Container, T>::_init()
-{
-    rebuild( _olist );
-    _initctxt();
-}
-
-template <class Container, typename T>
-void advuilist<Container, T>::_resize( point size, point origin )
-{
-    _size = size;
-    _innerw = _size.x - _firstcol * 2;
-    _origin = origin;
-    _pagesize =
-        static_cast<std::size_t>( std::max( 0, _size.y - ( _headersize + _footersize + 1 ) ) );
 }
 
 template <class Container, typename T>

src/advanced_inv.cpp
@@ -12,6 +12,24 @@
 #include "panels.h"            // for panel_manager
 #include "transaction_ui.h"    // for transaction_ui<>::advuilist_t, transa...
 
+namespace
+{
+std::pair<point, point> AIM_size( bool full_screen )
+{
+    int const min_w_width = FULL_SCREEN_WIDTH;
+    int const max_w_width = full_screen ? TERMX : std::max( 120,
+                            TERMX - 2 * ( panel_manager::get_manager().get_width_right() +
+                                          panel_manager::get_manager().get_width_left() ) );
+
+    int const width = TERMX < min_w_width
+                      ? min_w_width
+                      : TERMX > max_w_width ? max_w_width : TERMX;
+    int const originx = TERMX > width ? ( TERMX - width ) / 2 : 0;
+
+    return { point{width, TERMY}, point{originx, 0} };
+}
+} // namespace
+
 void create_advanced_inv()
 {
     using namespace advuilist_helpers;
@@ -23,20 +41,16 @@ void create_advanced_inv()
     static aim_stats_t rstats{ 0_kilogram, 0_liter };
     static bool full_screen{ get_option<bool>( "AIM_WIDTH" ) };
     bool const _fs = get_option<bool>( "AIM_WIDTH" );
-    if( !mytrui or full_screen != _fs ) {
-        full_screen = _fs;
-        int const min_w_width = FULL_SCREEN_WIDTH;
-        int const max_w_width = full_screen ? TERMX : std::max( 120,
-                                TERMX - 2 * ( panel_manager::get_manager().get_width_right() +
-                                              panel_manager::get_manager().get_width_left() ) );
+    if( !mytrui ) {
+        std::pair<point, point> size = AIM_size( full_screen );
 
-        int const width = TERMX < min_w_width
-                          ? min_w_width
-                          : TERMX > max_w_width ? max_w_width : TERMX;
-        int const originx = TERMX > width ? ( TERMX - width ) / 2 : 0;
-
-        mytrui = std::make_unique<mytrui_t>( aimlayout, point{ width, TERMY }, point{ originx, 0 },
+        mytrui = std::make_unique<mytrui_t>( aimlayout, size.first, size.second,
                                              "ADVANCED_INVENTORY" );
+        mytrui->on_resize( [&]( mytrui_t *ui ) {
+            std::pair<point, point> size = AIM_size( full_screen );
+            ui->resize( size.first, size.second );
+        } );
+
         setup_for_aim( mytrui->left(), &lstats );
         setup_for_aim( mytrui->right(), &rstats );
         add_aim_sources( mytrui->left(), &pane_mutex );
@@ -46,6 +60,12 @@ void create_advanced_inv()
             aim_ctxthandler( ui, action, &pane_mutex );
         } );
         mytrui->loadstate( &uistate.transfer_save );
+
+    } else if( full_screen != _fs ) {
+        full_screen = _fs;
+        std::pair<point, point> size = AIM_size( full_screen );
+        mytrui->resize( size.first, size.second );
+
     } else {
         aim_advuilist_sourced_t::slotidx_t lidx, ridx;
         aim_advuilist_sourced_t::icon_t licon, ricon;

src/advuilist_sourced.h
@@ -27,6 +27,7 @@ class advuilist_sourced : public advuilist<Container, T>
         using fsource_t = std::function<Container()>;
         // source availability function
         using fsourceb_t = std::function<bool()>;
+        using fdraw_t = typename advuilist<Container, T>::fdraw_t;
         using icon_t = char;
         using slotidx_t = std::size_t;
         using getsource_t = std::pair<slotidx_t, icon_t>;
@@ -51,6 +52,8 @@ class advuilist_sourced : public advuilist<Container, T>
         void rebuild();
         void initui();
         void hide();
+        void resize( point size, point origin );
+        void on_resize( fdraw_t const &func );
 
         void setctxthandler( fctxt_t const &func );
 
@@ -67,8 +70,9 @@ class advuilist_sourced : public advuilist<Container, T>
         Container _container;
         srccont_t _sources;
         fctxt_t _fctxt;
-        point _size;
-        point _origin;
+        fdraw_t _fresize;
+        point _size, _osize;
+        point _origin, _oorigin;
         point _map_size;
         slotidx_t _cslot = 0;
         bool needsinit = true;
@@ -93,19 +97,14 @@ class advuilist_sourced : public advuilist<Container, T>
 template <class Container, typename T>
 advuilist_sourced<Container, T>::advuilist_sourced( point const &srclayout, point size,
                                                     point origin, std::string const &ctxtname )
-    : advuilist<Container, T>( &_container, size, origin, ctxtname, false ),
-      _size( size.x > 0 ? size.x : TERMX / 2, size.y > 0 ? size.y : TERMY ),
-      _origin( origin.x >= 0 ? origin.x : TERMX / 2 - _size.x / 2, origin.y >= 0 ? origin.y : 0 ),
+    : advuilist<Container, T>( &_container, size, origin, ctxtname ), 
+      _size( size ),
+      _origin( origin ), 
       _map_size( srclayout )
 // *INDENT-ON*
 
 {
     using namespace advuilist_literals;
-    // leave room for source map window
-    point const offset( 0, _headersize + _footersize + _map_size.y );
-    advuilist<Container, T>::_resize( _size - offset, _origin + offset );
-
-    advuilist<Container, T>::_init();
 
     advuilist<Container, T>::setctxthandler(
         [this]( advuilist<Container, T> *ui, std::string const & action )
@@ -116,6 +115,8 @@ advuilist_sourced<Container, T>::advuilist_sourced( point const &srclayout, poin
     advuilist<Container, T>::get_ctxt()->register_action( ACTION_CYCLE_SOURCES );
     advuilist<Container, T>::get_ctxt()->register_action( ACTION_NEXT_SLOT );
     advuilist<Container, T>::get_ctxt()->register_action( ACTION_PREV_SLOT );
+    // use a dummy on_resize for base class since we're handling everything in resize()
+    advuilist<Container, T>::on_resize( []( advuilist<Container, T> * /**/ ) {} );
 }
 
 template <class Container, typename T>
@@ -196,6 +197,11 @@ void advuilist_sourced<Container, T>::initui()
 {
     _mapui = std::make_shared<ui_adaptor>();
     _mapui->on_screen_resize( [&]( ui_adaptor & ui ) {
+        if( _fresize ) {
+            _fresize( this );
+        } else {
+            resize( _osize, _oorigin );
+        }
         _w = catacurses::newwin( _headersize + _footersize + _map_size.y, _size.x, _origin );
         ui.position_from_window( _w );
     } );
@@ -216,6 +222,29 @@ void advuilist_sourced<Container, T>::hide()
 {
     advuilist<Container, T>::hide();
     _mapui.reset();
+}
+
+template <class Container, typename T>
+void advuilist_sourced<Container, T>::resize( point size, point origin )
+{
+
+    _size = { size.x > 0 ? size.x > TERMX ? TERMX : size.x : TERMX / 4,
+              size.y > 0 ? size.y > TERMY ? TERMY : size.y : TERMY / 4
+            };
+    _origin = { origin.x >= 0 ? origin.x + size.x > TERMX ? 0 : origin.x : TERMX / 2 - _size.x / 2,
+                origin.y >= 0 ? origin.y + size.y > TERMY ? 0 : origin.y
+                : TERMY / 2 - _size.y / 2
+              };
+
+    // leave room for source map window
+    point const offset( 0, _headersize + _footersize + _map_size.y );
+    advuilist<Container, T>::resize( _size - offset, _origin + offset );
+}
+
+template <class Container, typename T>
+void advuilist_sourced<Container, T>::on_resize( fdraw_t const &func )
+{
+    _fresize = func;
 }
 
 template <class Container, typename T>

src/transaction_ui.h
@@ -23,6 +23,7 @@ class transaction_ui
         using select_t = typename advuilist_t::select_t;
         using fselect_t = std::function<void( transaction_ui<Container, T> *, select_t )>;
         using fctxt_t = std::function<void( transaction_ui<Container, T> *, std::string const & )>;
+        using fresize_t = std::function<void( transaction_ui<Container, T> * )>;
 
         enum class event { QUIT = 0, SWITCH = 1, NEVENTS = 2 };
 
@@ -36,9 +37,11 @@ class transaction_ui
 
         void setctxthandler( fctxt_t const &func );
         void on_select( fselect_t const &func );
+        void on_resize( fresize_t const &func );
         void pushevent( event const &ev );
 
         void show();
+        void resize( point size, point origin );
         void savestate( transaction_ui_save_state *state );
         void loadstate( transaction_ui_save_state *state );
 
@@ -54,6 +57,7 @@ class transaction_ui
         point _origin;
         panecont_t _panes;
         fselect_t _fselect;
+        fresize_t _fresize;
         std::queue<event> _queue;
         fctxt_t _fctxt;
         typename panecont_t::size_type _cpane = 0;
@@ -74,13 +78,10 @@ class transaction_ui
 template <class Container, typename T>
 transaction_ui<Container, T>::transaction_ui( point const &srclayout, point size, point origin,
                                               std::string const &ctxtname )
-    : _size( size.x > 0 ? size.x : ( TERMX * 3 ) / 4, size.y > 0 ? size.y : TERMY ),
-      _origin( origin.x >= 0 ? origin.x : TERMX / 2 - _size.x / 2, origin.y >= 0 ? origin.y : 0 ),
-      _panes{ advuilist_t{ srclayout, { _size.x / 2, _size.y }, _origin, ctxtname },
-              advuilist_t{ srclayout,
-                           { _size.x / 2, _size.y },
-                           { _origin.x + _size.x / 2, _origin.y },
-                           ctxtname } }
+    : _size( size ),
+      _origin( origin ),
+      _panes{ advuilist_t{ srclayout, _size, _origin, ctxtname },
+              advuilist_t{ srclayout, _size, _origin, ctxtname } }
 
 #ifdef __clang__
 #pragma clang diagnostic pop
@@ -89,6 +90,7 @@ transaction_ui<Container, T>::transaction_ui( point const &srclayout, point size
 // *INDENT-ON*
 
 {
+    resize( _size, _origin );
     using namespace advuilist_literals;
     for( advuilist_t &v : _panes )
     {
@@ -98,6 +100,7 @@ transaction_ui<Container, T>::transaction_ui( point const &srclayout, point size
         v.setctxthandler( [this]( advuilist<Container, T> *ui, std::string const & action ) {
             this->_ctxthandler( ui, action );
         } );
+        v.on_resize( []( advuilist<Container, T> * /**/ ) {} );
     }
 }
 
@@ -138,6 +141,12 @@ void transaction_ui<Container, T>::on_select( fselect_t const &func )
 }
 
 template <class Container, typename T>
+void transaction_ui<Container, T>::on_resize( fresize_t const &func )
+{
+    _fresize = func;
+}
+
+template <class Container, typename T>
 void transaction_ui<Container, T>::pushevent( event const &ev )
 {
     _queue.emplace( ev );
@@ -152,6 +161,15 @@ void transaction_ui<Container, T>::show()
 
     _exit = false;
 
+    ui_adaptor dummy;
+    dummy.on_screen_resize( [&]( ui_adaptor & /*ui*/ ) {
+        if( _fresize ) {
+            _fresize( this );
+        } else {
+            resize( _size, _origin );
+        }
+    } );
+
     while( !_exit ) {
         typename advuilist_t::select_t selection = _panes[_cpane].select();
         if( _fselect and !selection.empty() ) {
@@ -164,6 +182,20 @@ void transaction_ui<Container, T>::show()
             _process( ev );
         }
     }
+}
+
+template <class Container, typename T>
+void transaction_ui<Container, T>::resize( point size, point origin )
+{
+    point const nsize = { size.x > 0 ? size.x > TERMX ? TERMX : size.x : ( TERMX * 3 ) / 4,
+                          size.y > 0 ? size.y > TERMY ? TERMY : size.y : TERMY
+                        };
+    point const norigin = {
+        origin.x >= 0 ? origin.x + size.x > TERMX ? 0 : origin.x : TERMX / 2 - _size.x / 2,
+        origin.y >= 0 ? origin.y + size.y > TERMY ? 0 : origin.y : TERMY / 2 - _size.y / 2
+    };
+    _panes[_left].resize( { nsize.x / 2, nsize.y }, norigin );
+    _panes[_right].resize( { nsize.x / 2, nsize.y }, { norigin.x + nsize.x / 2, norigin.y } );
 }
 
 template <class Container, typename T>

commit cbd79e9eea4c6516b9f81ae6fc3344f0bb8b509a

advuilist: fix an initialization issue 🤦
introduced by
4ef891838b advuilist (and friends): screen resizing support

also add an extra sanity check (copied from advuilist_sourced)

src/advuilist.h
@@ -165,6 +165,7 @@ class advuilist
         /// number of lines at the bottom of the window reserved for decorations
         int _footersize = 1;
         bool _exit = true;
+        bool _needsinit = true;
 
         input_context _ctxt;
         catacurses::window _w;
@@ -341,6 +342,9 @@ typename advuilist<Container, T>::select_t advuilist<Container, T>::select()
     if( !_ui ) {
         initui();
     }
+    if( _needsinit ) {
+        rebuild();
+    }
 
     while( !_exit ) {
 
@@ -419,6 +423,7 @@ void advuilist<Container, T>::rebuild( Container *list )
     _sort( _csort );
     _setidx( _cidx );
     _cpage = _idxtopage( _cidx );
+    _needsinit = false;
 }
 
 template <class Container, typename T>
@@ -477,11 +482,9 @@ void advuilist<Container, T>::resize( point size, point origin, point reserved_r
               };
     _headersize = reserved_rows.x > 0 ? reserved_rows.x : _headersize;
     _footersize = reserved_rows.y > 0 ? reserved_rows.y : _footersize;
-    // leave space for decorations and column headers
-    _pagesize =
-        static_cast<std::size_t>( std::max( 0, _size.y - ( _headersize + _footersize + 1 ) ) );
 
     _innerw = _size.x - _firstcol * 2;
+    // leave space for decorations and column headers
     std::size_t const npagesize =
         static_cast<std::size_t>( std::max( 0, _size.y - ( _headersize + _footersize + 1 ) ) );
     if( npagesize != _pagesize ) {