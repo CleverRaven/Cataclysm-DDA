12585e855fe9431a3bf0099f338154b1247c80fa
Hirmuolio <hirmuolio@gmail.com>
2020-12-12 14:17:35 +0200

    remove test message

--- src/ranged.cpp
+++ src/ranged.cpp
@@ -649,7 +649,6 @@ bool player::handle_gun_damage( item &it )
         }
         // These are the dirtying/fouling mechanics
         if( !curammo_effects.count( "NON_FOULING" ) && !it.has_flag( flag_NON_FOULING ) ) {
-			debugmsg( _( "FOULING" ), it.tname() );
             if( dirt < static_cast<int>( dirt_max_dbl ) ) {
                 dirtadder = curammo_effects.count( "BLACKPOWDER" ) * ( 200 - firing.blackpowder_tolerance *
                             2 );
