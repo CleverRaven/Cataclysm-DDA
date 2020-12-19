52192322d0a988de3deda335bfe26e36d55c6431
Eric Pierce <wapcaplet88@gmail.com>
2020-12-19 14:44:15 -0700

    Do not sort zones by name; it erases player config
    Zones can be manually sorted using the +/- keys in zone manager. The
    implicit sorting at the top of this function wipes out any
    player-defined zone sorting.
    
    Fix #46158

--- src/game.cpp
+++ src/game.cpp
@@ -6608,11 +6608,6 @@ void game::zones_manager()
             }
         }
         zone_cnt = static_cast<int>( zones.size() );
-        // Sort zones by name
-        std::sort( zones.begin(), zones.end(),
-        []( const zone_manager::ref_zone_data & lhs, const zone_manager::ref_zone_data & rhs ) {
-            return localized_compare( lhs.get().get_name(), rhs.get().get_name() );
-        } );
         return zones;
     };
 
