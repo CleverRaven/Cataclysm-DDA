7de9f8321797ec7391684cddf97490fd0def7a5f
Venera3 <72006894+Venera3@users.noreply.github.com>
2020-12-19 21:44:52 +0100

    Bleed

--- src/monster.cpp
+++ src/monster.cpp
@@ -1285,8 +1285,7 @@ bool monster::is_immune_effect( const efftype_id &effect ) const
     }
 
     if( effect == effect_bleed ) {
-        return !has_flag( MF_WARM ) ||
-               !made_of( material_id( "flesh" ) );
+        return !made_of( material_id( "flesh" ) ) && !made_of( material_id( "iflesh" ) );
     }
 
     if( effect == effect_paralyzepoison ||
