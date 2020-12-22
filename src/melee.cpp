a357a95cd94f6a665ed11eca22bf1d2f962c8bd9
Venera3 <72006894+Venera3@users.noreply.github.com>
2020-12-22 20:11:10 +0100

    Additional effects random

--- src/melee.cpp
+++ src/melee.cpp
@@ -647,18 +647,22 @@ void Character::melee_attack( Creature &t, bool allow_special, const matec_id &f
                         dealt_dam.type_damage( damage_type::STAB ) > 0 ) ) ) {
                 if( has_trait( trait_POISONOUS ) ) {
                     add_msg_if_player( m_good, _( "You poison %s!" ), t.disp_name() );
+                    if( x_in_y( 1, 5 ) ) {
+                        t.add_effect( effect_stunned, 1_turns );
+                    }
                     if( t.is_monster() ) {
                         t.add_effect( effect_venom_player1, 1_minutes );
                     } else {
                         t.add_effect( effect_venom_dmg, 10_minutes );
-                        t.add_effect( effect_stunned, 1_turns );
                     }
                 } else if( has_trait( trait_POISONOUS2 ) ) {
                     add_msg_if_player( m_good, _( "You inject your venom into %s!" ),
                                        t.disp_name() );
+                    if( x_in_y( 1, 5 ) ) {
+                        t.add_effect( effect_downed, 1_turns );
+                    }
                     if( t.is_monster() ) {
                         t.add_effect( effect_venom_player2, 1_minutes );
-                        t.add_effect( effect_stunned, 2_turns );
                     } else {
                         t.add_effect( effect_venom_dmg, 15_minutes );
                         t.add_effect( effect_venom_weaken, 5_minutes );
