85dfa50b7a6283e79531ae063566a4a26049a53c
Eric Pierce <wapcaplet88@gmail.com>
2020-12-26 16:30:42 -0700

    See NPC's current activity before speaking to them (#46326)
    * Know NPC current activity before speaking to them
    
    Before speaking to an NPC, be able to see their current activity.
    
    - Add npc::name_and_activity(), returning NPC name with activity
    - Use new function from NPC Chat menu, to see what each NPC is doing
    
    * Add comment for translators

--- src/npc.cpp
+++ src/npc.cpp
@@ -3350,6 +3350,17 @@ std::string npc::describe_mission() const
     } // switch (mission)
 }
 
+std::string npc::name_and_activity() const
+{
+    if( current_activity_id ) {
+        const std::string activity_name = current_activity_id.obj().verb().translated();
+        //~ %1$s - npc name, %2$s - npc current activity name.
+        return string_format( _( "%1$s (%2$s)" ), name, activity_name );
+    } else {
+        return name;
+    }
+}
+
 std::unique_ptr<talker> get_talker_for( npc &guy )
 {
     return std::make_unique<talker_npc>( &guy );
