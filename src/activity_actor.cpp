084eeaafb64e6b68f8964639b444adcd2c00fde3
Roy Berube <royberube1965@gmail.com>
2021-01-02 15:57:07 -0700

    Astyle
    Co-authored-by: Binrui Dong <brett.browning.dong@gmail.com>

--- src/activity_actor.cpp
+++ src/activity_actor.cpp
@@ -1724,7 +1724,8 @@ void craft_activity_actor::do_turn( player_activity &act, Character &crafter )
     const double base_total_moves = std::max( static_cast<int64_t>( 1 ), rec.batch_time( crafter,
                                     craft.charges, 1.0f, 0 ) );
     // Current expected total moves, includes crafting speed modifiers and assistants
-    const double cur_total_moves = std::max( static_cast< int64_t >( 1 ), rec.batch_time( crafter, craft.charges, crafting_speed,
+    const double cur_total_moves = std::max( static_cast< int64_t >( 1 ), rec.batch_time( crafter,
+                                   craft.charges, crafting_speed,
                                    assistants ) );
     // Delta progress in moves adjusted for current crafting speed /
     //crafter.exertion_adjusted_move_multiplier( exertion_level() )
