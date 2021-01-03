1f028d6472821b0eff4a80ddd4b874dd5da0cf9c
Salty Panda <zjadaczwatrob@gmail.com>
2021-01-03 18:20:10 +0100

    Fix sarcophagus elevator reactivation

--- src/computer_session.cpp
+++ src/computer_session.cpp
@@ -68,6 +68,8 @@ static const mtype_id mon_secubot( "mon_secubot" );
 
 static const std::string flag_CONSOLE( "CONSOLE" );
 
+static const itype_id itype_sarcophagus_access_code( "sarcophagus_access_code" );
+
 static catacurses::window init_window()
 {
     const int width = FULL_SCREEN_WIDTH;
@@ -931,19 +933,68 @@ void computer_session::action_srcf_seal()
 
 void computer_session::action_srcf_elevator()
 {
-    if( !g->u.has_amount( "sarcophagus_access_code", 1 ) ) {
-        print_error( _( "Access code required!" ) );
-    } else {
-        g->u.use_amount( "sarcophagus_access_code", 1 );
+    Character &player_character = g->u;
+    map &here = g->m;
+    tripoint surface_elevator;
+    tripoint underground_elevator;
+    bool is_surface_elevator_on = false;
+    bool is_surface_elevator_exist = false;
+    bool is_underground_elevator_on = false;
+    bool is_underground_elevator_exist = false;
+
+    for( const tripoint &p : here.points_on_zlevel( 0 ) ) {
+        if( here.ter( p ) == t_elevator_control_off || here.ter( p ) == t_elevator_control ) {
+            surface_elevator = p;
+            is_surface_elevator_on = here.ter( p ) == t_elevator_control;
+            is_surface_elevator_exist = true;
+        }
+    }
+    for( const tripoint &p : here.points_on_zlevel( -2 ) ) {
+        if( here.ter( p ) == t_elevator_control_off || here.ter( p ) == t_elevator_control ) {
+            underground_elevator = p;
+            is_underground_elevator_on = here.ter( p ) == t_elevator_control;
+            is_underground_elevator_exist = true;
+        }
+    }
+
+    //If some are destroyed
+    if( !is_surface_elevator_exist || !is_underground_elevator_exist ) {
+        reset_terminal();
+        print_error(
+            _( "\nElevator control network unreachable!\n\n" ) );
+    }
+
+    //If both are disabled try to enable
+    else if( !is_surface_elevator_on && !is_underground_elevator_on ) {
+        if( !player_character.has_amount( itype_sarcophagus_access_code, 1 ) ) {
+            print_error( _( "Access code required!\n\n" ) );
+        } else {
+            player_character.use_amount( itype_sarcophagus_access_code, 1 );
+            here.ter_set( surface_elevator, t_elevator_control );
+            is_surface_elevator_on = true;
+            here.ter_set( underground_elevator, t_elevator_control );
+            is_underground_elevator_on = true;
+        }
+    }
+
+    //If only one is enabled, enable the other one. Fix for before this change
+    else if( is_surface_elevator_on && !is_underground_elevator_on && is_underground_elevator_exist ) {
+        here.ter_set( underground_elevator, t_elevator_control );
+        is_underground_elevator_on = true;
+    }
+
+    else if( is_underground_elevator_on && !is_surface_elevator_on && is_surface_elevator_exist ) {
+        here.ter_set( surface_elevator, t_elevator_control );
+        is_surface_elevator_on = true;
+    }
+
+    //If the elevator is working
+    if( is_surface_elevator_on && is_underground_elevator_on ) {
         reset_terminal();
         print_line(
             _( "\nPower:         Backup Only\nRadiation Level:  Very Dangerous\nOperational:   Overridden\n\n" ) );
-        for( const tripoint &p : g->m.points_on_zlevel() ) {
-            if( g->m.ter( p ) == t_elevator_control_off ) {
-                g->m.ter_set( p, t_elevator_control );
-            }
-        }
     }
+
     query_any( _( "Press any key…" ) );
 }
 
