f4034a42bae3a19d5d111895bf3758621d36411b
andrei <a12l+git@runbox.com>
2021-01-09 22:49:44 +0200

    advanced_inv: spank advuilist_helpers
    move all AIM-specific code to advanced_inv as there is currently no need
    for any of these functions to be available elsewhere
    
    this ruins git-blame so maybe it's not really worth it...

--- src/advuilist_const.h
+++ src/advuilist_const.h
@@ -9,7 +9,6 @@ namespace advuilist_literals
 
 constexpr char const *ACTION_CYCLE_SOURCES = "CYCLE_SOURCES";
 constexpr char const *ACTION_DOWN = "DOWN";
-constexpr char const *ACTION_EXAMINE = "EXAMINE";
 constexpr char const *ACTION_FILTER = "FILTER";
 constexpr char const *ACTION_HELP_KEYBINDINGS = "HELP_KEYBINDINGS";
 constexpr char const *ACTION_NEXT_SLOT = "ACTION_NEXT_SLOT";
@@ -27,12 +26,8 @@ constexpr char const *ACTION_SOURCE_PRFX = "SOURCE_";
 constexpr char const *ACTION_SWITCH_PANES = "SWITCH_PANES";
 constexpr char const *ACTION_UP = "UP";
 constexpr char const *CTXT_DEFAULT = "UILIST";
-constexpr char const *ITEMS_DEFAULT = "ITEMS_DEFAULT";
 constexpr char const *PANE_LEFT = "LEFT";
 constexpr char const *PANE_RIGHT = "RIGHT";
-constexpr char const *SAVE_DEFAULT = "SAVE_DEFAULT";
-constexpr char const *TOGGLE_AUTO_PICKUP = "TOGGLE_AUTO_PICKUP";
-constexpr char const *TOGGLE_FAVORITE = "TOGGLE_FAVORITE";
 constexpr std::size_t const ACTION_SOURCE_PRFX_len = 7; // where is c++20 when you need it?
 
 } // namespace advuilist_literals
--- src/advuilist_helpers.h
+++ src/advuilist_helpers.h
@@ -1,8 +1,6 @@
 #ifndef CATA_SRC_ADVUILIST_HELPERS_H
 #define CATA_SRC_ADVUILIST_HELPERS_H
 
-#include <array>      // for array
-#include <cstddef>    // for size_t
 #include <functional> // for function
 #include <string>     // for string, allocator
 #include <utility>    // for pair
@@ -12,6 +10,7 @@
 #include "advuilist_sourced.h" // for advuilist_sourced
 #include "item_location.h"     // for item_location
 #include "transaction_ui.h"    // for transaction_ui
+#include "vpart_position.h"    // for vpart_reference
 #include "units_fwd.h"         // for mass, volume
 
 class Character;
@@ -42,46 +41,6 @@ using aim_advuilist_sourced_t = advuilist_sourced<aim_container_t, iloc_entry>;
 using aim_transaction_ui_t = transaction_ui<aim_container_t, iloc_entry>;
 using aim_stats_t = std::pair<units::mass, units::volume>;
 
-constexpr std::size_t const aim_nsources = 18;
-constexpr point const aimlayout( 6, 3 );
-
-constexpr char const *SOURCE_ALL = "Surrounding area";
-constexpr char const SOURCE_ALL_i = 'A';
-constexpr char const *SOURCE_CENTER = "Directly below you";
-constexpr char const SOURCE_CENTER_i = '5';
-constexpr char const *SOURCE_CONT = "Container";
-constexpr char const SOURCE_CONT_i = 'C';
-constexpr char const *SOURCE_DRAGGED = "Grabbed Vehicle";
-constexpr char const SOURCE_DRAGGED_i = 'D';
-constexpr char const *SOURCE_E = "East";
-constexpr char const SOURCE_E_i = '6';
-constexpr char const *SOURCE_INV = "Inventory";
-constexpr char const SOURCE_INV_i = 'I';
-constexpr char const *SOURCE_N = "North";
-constexpr char const SOURCE_N_i = '8';
-constexpr char const *SOURCE_NE = "North East";
-constexpr char const SOURCE_NE_i = '9';
-constexpr char const *SOURCE_NW = "North West";
-constexpr char const SOURCE_NW_i = '7';
-constexpr char const *SOURCE_S = "South";
-constexpr char const SOURCE_S_i = '2';
-constexpr char const *SOURCE_SE = "South East";
-constexpr char const SOURCE_SE_i = '3';
-constexpr char const *SOURCE_SW = "South West";
-constexpr char const SOURCE_SW_i = '1';
-constexpr char const *SOURCE_W = "West";
-constexpr char const SOURCE_W_i = '4';
-constexpr char const *SOURCE_WORN = "Worn Items";
-constexpr char const SOURCE_WORN_i = 'W';
-constexpr char const *SOURCE_VEHICLE = "Vehicle";
-constexpr char const SOURCE_VEHICLE_i = 'V';
-
-using pane_mutex_t = std::array<bool, aim_nsources * 2>;
-void reset_mutex( pane_mutex_t *mutex );
-void reset_mutex( aim_transaction_ui_t *ui, pane_mutex_t *mutex );
-void set_mutex( pane_mutex_t *mutex, aim_advuilist_sourced_t::slotidx_t slot,
-                aim_advuilist_sourced_t::icon_t icon, bool val );
-
 using filoc_t = std::function<item_location( item * )>;
 item_location iloc_map_cursor( map_cursor const &cursor, item *it );
 item_location iloc_tripoint( tripoint const &loc, item *it );
@@ -96,7 +55,6 @@ std::string iloc_entry_count( iloc_entry const &it );
 std::string iloc_entry_weight( iloc_entry const &it );
 std::string iloc_entry_volume( iloc_entry const &it );
 std::string iloc_entry_name( iloc_entry const &it );
-std::string iloc_entry_src( iloc_entry const &it );
 
 bool iloc_entry_count_sorter( iloc_entry const &l, iloc_entry const &r );
 bool iloc_entry_weight_sorter( iloc_entry const &l, iloc_entry const &r );
@@ -114,6 +72,7 @@ bool iloc_entry_filter( iloc_entry const &it, std::string const &filter );
 void iloc_entry_stats( aim_stats_t *stats, bool first, advuilist_helpers::iloc_entry const &it );
 void iloc_entry_examine( catacurses::window *w, iloc_entry const &it );
 
+cata::optional<vpart_reference> veh_cargo_at( tripoint const &loc );
 aim_container_t source_ground_all( Character *guy, int radius );
 aim_container_t source_ground( tripoint const &loc );
 aim_container_t source_vehicle( tripoint const &loc );
@@ -121,17 +80,6 @@ aim_container_t source_char_inv( Character *guy );
 aim_container_t source_char_worn( Character *guy );
 
 bool source_vehicle_avail( tripoint const &loc );
-
-void aim_all_columns( aim_advuilist_t *myadvuilist );
-void aim_default_columns( aim_advuilist_t *myadvuilist );
-void aim_rebuild( aim_transaction_ui_t *ui, pane_mutex_t *mutex );
-
-void setup_for_aim( aim_advuilist_t *myadvuilist, aim_stats_t *stats );
-void add_aim_sources( aim_advuilist_sourced_t *myadvuilist, pane_mutex_t *mutex );
-void aim_add_return_activity();
-void aim_transfer( aim_transaction_ui_t *ui, aim_transaction_ui_t::select_t const &select );
-void aim_ctxthandler( aim_transaction_ui_t *ui, std::string const &action, pane_mutex_t *mutex );
-void aim_stats_printer( aim_advuilist_t *ui, aim_stats_t *stats );
 
 // for map::i_at()
 extern template iloc_stack_t get_stacks<>( map_stack items, filoc_t const &iloc_helper );
