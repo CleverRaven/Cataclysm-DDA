055c4134d8895bfbb63a6face7d14cb6bfacc6e1
Hirmuolio <hirmuolio@gmail.com>
2021-01-19 11:36:42 +0200

    units::to_liter

--- src/consumption.cpp
+++ src/consumption.cpp
@@ -1268,7 +1268,7 @@ int Character::compute_calories_per_effective_volume( const item &food,
     } else {
         kcalories = compute_effective_nutrients( food ).kcal;
     }
-    double food_vol = round_up( units::to_milliliter( masticated_volume( food ) ) * 0.001, 2 );
+    double food_vol = round_up( units::to_liter( masticated_volume( food ) ), 2 );
     const double energy_density_ratio = compute_effective_food_volume_ratio( food );
     const double effective_volume = food_vol * energy_density_ratio;
     return std::round( kcalories / effective_volume );
