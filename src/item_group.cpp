a77f080e2c88f1d2e753feda2d0e0509e41abb21
Williham Williham Totland <williham.totland@gmail.com>
2021-01-23 12:44:27 +0100

    Resolves an issue where containers for spawned liquids would only be partially full if charges were not specified.

--- src/item_group.cpp
+++ src/item_group.cpp
@@ -181,6 +181,8 @@ item Single_item_creator::create_single( const time_point &birthday, RecursionLi
         int qty = tmp.charges;
         if( modifier ) {
             qty = rng( modifier->charges.first, modifier->charges.second );
+        } else {
+          qty = item::INFINITE_CHARGES;
         }
         // TODO: change the spawn lists to contain proper references to containers
         tmp = tmp.in_its_container( qty );
