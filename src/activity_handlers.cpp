cd391de692bcd721a9da452ed5de70f1c3677269
NeviNovat <51816321+NeviNovat@users.noreply.github.com>
2021-01-26 02:50:14 +0200

    Replace dirt with scrap half-wall, retry.

--- src/activity_handlers.cpp
+++ src/activity_handlers.cpp
@@ -2285,7 +2285,7 @@ void activity_handlers::oxytorch_finish( player_activity *act, player *p )
         here.spawn_item( pos, itype_steel_plate, rng( 0, 1 ) );
         here.spawn_item( pos, itype_steel_chunk, rng( 3, 8 ) );
     } else if( ter == t_wall_metal ) {
-        here.ter_set( pos, t_dirt );
+        here.ter_set( pos, t_scrap_wall_halfway );
         here.spawn_item( pos, itype_steel_plate, rng( 2, 3 ) );
         here.spawn_item( pos, itype_steel_chunk, rng( 12, 20 ) );
     } else if( ter == t_window_enhanced || ter == t_window_enhanced_noglass ) {
