50bc506af686d0b047dd9c69f279a562c9b1b1ee
Jakub Cieplinski <cieplinski.jakubsp@gmail.com>
2021-01-26 10:11:41 +0100

    Update src/avatar_action.cpp
    Co-authored-by: Jianxiang Wang (王健翔) <qrox@sina.com>

--- src/avatar_action.cpp
+++ src/avatar_action.cpp
@@ -95,7 +95,7 @@ bool avatar_action::move( avatar &you, map &m, const tripoint &d )
         return false;
     }
 
-    //If both legs broken without crutches and not already on the ground topple over
+    // If any leg broken without crutches and not already on the ground topple over
     if( ( you.get_working_leg_count() < 2 && !you.weapon.has_flag( flag_CRUTCHES ) ) &&
         !you.is_prone() ) {
         you.set_movement_mode( move_mode_id( "prone" ) );
