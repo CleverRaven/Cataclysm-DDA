e0d7a483477f41969bf779079391baa114c14ded
Kenshkrix <42079267+Kenshkrix@users.noreply.github.com>
2021-01-31 19:08:43 -0500

    Splitting up the teleport function

--- src/teleport.h
+++ src/teleport.h
@@ -13,5 +13,6 @@ bool teleport( Creature &critter, int min_distance = 2, int max_distance = 12,
                bool safe = false,
                bool add_teleglow = true );
 } // namespace teleport
+bool teleport_to_point( Creature &critter, const tripoint &target, bool safe, bool add_teleglow );
 
 #endif // CATA_SRC_TELEPORT_H
