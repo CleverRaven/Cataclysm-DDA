ee6766eadd1cc039bcdb252b1b1935ba80a69ea1
Qrox <qrox@sina.com>
2021-02-02 20:22:45 +0800

    Check texture pointer is not null before putting it back to texture pool

--- src/pixel_minimap.cpp
+++ src/pixel_minimap.cpp
@@ -147,8 +147,10 @@ class pixel_minimap::shared_texture_pool
         //releases the provided texture back into the inactive pool to be used again
         //called automatically in the submap cache destructor
         void release_tex( size_t index, SDL_Texture_Ptr &&ptr ) {
-            inactive_index.push_back( index );
-            texture_pool[index] = std::move( ptr );
+            if( ptr ) {
+                inactive_index.push_back( index );
+                texture_pool[index] = std::move( ptr );
+            }
         }
 
     private:
@@ -183,6 +185,7 @@ struct pixel_minimap::submap_cache {
         pool.release_tex( texture_index, std::move( chunk_tex ) );
     }
 
+    submap_cache( const submap_cache & ) = delete;
     submap_cache( submap_cache && ) = default;
 
     SDL_Color &color_at( const point &p ) {
@@ -342,7 +345,7 @@ pixel_minimap::submap_cache &pixel_minimap::get_cache_at( const tripoint &abs_sm
     auto it = cache.find( abs_sm_pos );
 
     if( it == cache.end() ) {
-        it = cache.emplace( abs_sm_pos, *tex_pool ).first;
+        it = cache.emplace( abs_sm_pos, submap_cache( *tex_pool ) ).first;
     }
 
     return it->second;
