20386248396b97e1fd2b1261118b5898dcae1ec1
ToxiClay <ToxiClay@users.noreply.github.com>
2021-02-03 14:28:07 -0800

    Validated AStyle linting
    When I click "Details" on the failed AStyle check, it really needs to be better at clearly identifying the regression.
    
    Co-authored-by: Binrui Dong <brett.browning.dong@gmail.com>

--- src/item.cpp
+++ src/item.cpp
@@ -8003,7 +8003,8 @@ ret_val<bool> item::is_gunmod_compatible( const item &mod ) const
     } else if( mod.typeId() == itype_brass_catcher && has_flag( flag_RELOAD_EJECT ) ) {
         return ret_val<bool>::make_failure( _( "cannot have a brass catcher" ) );
 
-    } else if ( mod.type->gunmod->location.name() == "magazine" || mod.type->gunmod->location.name() == "mechanism" ) {
+    } else if( mod.type->gunmod->location.name() == "magazine" ||
+               mod.type->gunmod->location.name() == "mechanism" ) {
         return ret_val<bool>::make_failure( _( "must be unloaded before installing this mod" ) );
     }
 
