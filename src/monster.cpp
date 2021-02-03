f31d3ede4e00d34da2d0388d56fd13e74a6aee10
Saicchi <47158232+Saicchi@users.noreply.github.com>
2021-02-03 14:31:12 -0300

    braces around initialization
    Co-authored-by: Binrui Dong <brett.browning.dong@gmail.com>

--- src/monster.cpp
+++ src/monster.cpp
@@ -657,7 +657,7 @@ static std::pair<std::string, nc_color> speed_description( float mon_speed_ratin
         return std::make_pair( _( "It is immobile." ), c_green );
     }
 
-    const std::array<std::tuple<float, nc_color, std::string>, 8> cases = {
+    const std::array<std::tuple<float, nc_color, std::string>, 8> cases = {{
         std::make_tuple( 1.30f, c_red, _( "It is much faster than you." ) ),
         std::make_tuple( 1.00f, c_yellow, _( "It is faster than you." ) ),
         std::make_tuple( 0.70f, c_white_yellow, _( "It is a bit faster than you." ) ),
@@ -666,7 +666,7 @@ static std::pair<std::string, nc_color> speed_description( float mon_speed_ratin
         std::make_tuple( 0.40f, c_cyan, _( "It is slower than you." ) ),
         std::make_tuple( 0.20f, c_green, _( "It is much slower than you." ) ),
         std::make_tuple( 0.00f, c_green, _( "It is practically immobile." ) )
-    };
+    }};
 
     const float player_speed_rating = get_player_character().speed_rating();
     const float ratio = player_speed_rating == 0 ?
