2f8f32f1a3846d764de5f8fd358845f6394061b1
actual-nh <74678550+actual-nh@users.noreply.github.com>
2021-02-05 21:26:21 -0500

    Set stored kcal to healthy kcal for testing (#46941)
    * Set stored kcal to healthy kcal for testing
    
    Randomizing test orders reveals that one type of problematic
    interference is due to variable stored calories from prior activity.
    
    (cherry picked from commit 306bfb4cbd817d4db60e92807025549050a576ff)
    
    * Match (some) weary testing to kcal resetting
    
    Previous weary testing was distored by variable incoming kcal
    stored, with the current default test order giving more kcal
    than the expected 55,000.
    
    * Update numbers to match consensus test results
    
    At least currently, two different sources (Github General build g++-7
    and Appveyor) are giving the same numbers for the 24-hour digging
    weary test. This updates the target test numbers to match these,
    with a bit of wiggle room in the direction of the earlier results
    (partially since Travis hasn't given any results yet).
    
    * Clear_avatar before initial debug_weary_info
    
    Variable initial heights and weights is making having debug_weary_info
    before do_activity not useful. This adds a clear_avatar before them;
    unless do_activity is modified to take a flag as to whether to clear
    the avatar itself, this is likely temporary.
    
    * Change the last target for the vehicle weary test
    
    See comments in #46941 - most specifically issuecomment-770297143.
    
    Co-authored-by: actual-nh <actual-nh@users.noreply.github.com>

--- tests/player_helpers.cpp
+++ tests/player_helpers.cpp
@@ -72,6 +72,8 @@ void clear_character( player &dummy )
     // This sets HP to max, clears addictions and morale,
     // and sets hunger, thirst, fatigue and such to zero
     dummy.environmental_revert_effect();
+    // However, the above does not set stored kcal
+    dummy.set_stored_kcal( dummy.get_healthy_kcal() );
 
     dummy.empty_skills();
     dummy.martial_arts_data->clear_styles();
