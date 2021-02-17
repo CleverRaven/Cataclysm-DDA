da313cb6885b2461d2e1dbe1bd3a9821f1982661
Jamuro-g <toenli.e@gmail.com>
2021-02-17 21:21:55 +0100

    Fix power armor didn't show contained items

--- src/item.cpp
+++ src/item.cpp
@@ -4695,7 +4695,7 @@ std::string item::tname( unsigned int quantity, bool with_prefix, unsigned int t
 
     if( is_corpse() || typeId() == itype_blood || item_vars.find( "name" ) != item_vars.end() ) {
         maintext = type_name( quantity );
-    } else if( is_gun() || is_tool() || is_magazine() ) {
+    } else if( ( is_gun() || is_tool() || is_magazine() ) && !is_power_armor() ) {
         int amt = 0;
         maintext = label( quantity );
         for( const item *mod : is_gun() ? gunmods() : toolmods() ) {
