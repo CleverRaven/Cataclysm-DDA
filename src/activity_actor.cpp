40f914d25597ce9501b850366e013ff85e46e973
Jamuro-g <76928284+Jamuro-g@users.noreply.github.com>
2021-02-20 15:12:41 +0100

    Update src/activity_actor.cpp
    Co-authored-by: Anton Burmistrov <Night_Pryanik@mail.ru>

--- src/activity_actor.cpp
+++ src/activity_actor.cpp
@@ -2403,7 +2403,7 @@ void insert_item_activity_actor::finish( player_activity &act, Character &who )
                 success = result > 0;
 
                 if( success ) {
-                    add_msg( string_format( _( "You insert your %s into the1 %s." ),
+                    add_msg( string_format( _( "You insert your %s into the %s." ),
                                             holstered_item.first->display_name( result ), holster->type->nname( 1 ) ) );
                     handler.add_unsealed( holster );
                     handler.unseal_pocket_containing( holstered_item.first );
