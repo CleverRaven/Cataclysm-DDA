adf310b4911615ae2754753e0587dd9238f2ec8a
Charlie Gardai <32105250+Moltenhead@users.noreply.github.com>
2021-02-23 08:58:57 +0100

    Stand up peek (#47257)

--- src/game.h
+++ src/game.h
@@ -125,6 +125,14 @@ struct look_around_result {
     cata::optional<tripoint> position;
     cata::optional<peek_act> peek_action;
 };
+struct look_around_params {
+    const bool show_window;
+    tripoint &center;
+    const tripoint &start_point;
+    bool has_first_point;
+    bool select_zone;
+    bool peeking;
+};
 
 struct w_map {
     int id;
@@ -572,6 +580,7 @@ class game
         cata::optional<tripoint> look_around();
         look_around_result look_around( bool show_window, tripoint &center,
                                         const tripoint &start_point, bool has_first_point, bool select_zone, bool peeking );
+        look_around_result look_around( look_around_params );
 
         // Shared method to print "look around" info
         void pre_print_all_tile_info( const tripoint &lp, const catacurses::window &w_info,
