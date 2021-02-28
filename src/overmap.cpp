034005e99f71eb69d7e3dd8036e09f6524fcfd79
Bella <surrealrose@outlook.com>
2021-02-27 20:08:38 -0600

    inital unhardcoding

--- src/overmap.cpp
+++ src/overmap.cpp
@@ -744,9 +744,7 @@ bool oter_t::is_hardcoded() const
         "spider_pit_under",
         "temple",
         "temple_finale",
-        "temple_stairs",
-        "triffid_finale",
-        "triffid_roots"
+        "temple_stairs"
     };
 
     return hardcoded_mapgen.find( get_mapgen_id() ) != hardcoded_mapgen.end();
