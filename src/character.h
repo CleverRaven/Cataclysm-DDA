6ea866ffb256fe6d4aa01acd2bf200577cd1cb28
aerrant <76838929+aerrant@users.noreply.github.com>
2021-03-04 01:59:18 +0300

    Stop offering non-comestible liquids for consumption (#47782)
    * Stop offering inappropriate liquids for consumption
    
    * Fix regression in Character::can_consume()

--- src/character.h
+++ src/character.h
@@ -2394,12 +2394,12 @@ class Character : public Creature, public visitable
                 const nutrients *nutrient = nullptr ) const;
         /** Handles the effects of consuming an item */
         bool consume_effects( item &food );
-        /** Check character's capability of consumption overall */
+        /** Check whether the character can consume this very item */
+        bool can_consume_as_is( const item &it ) const;
+        /** Check whether the character can consume this item or any of its contents */
         bool can_consume( const item &it ) const;
         /** True if the character has enough skill (in cooking or survival) to estimate time to rot */
         bool can_estimate_rot() const;
-        /** Check whether character can consume this very item */
-        bool can_consume_as_is( const item &it ) const;
         /**
          * Returns a reference to the item itself (if it's consumable),
          * the first of its contents (if it's consumable) or null item otherwise.
