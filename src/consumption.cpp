3bb7bcc8d062108b821c9d97c1ff865c653c9112
Jamuro-g <toenli.e@gmail.com>
2021-03-05 19:39:10 +0100

    removed redundant check

--- src/consumption.cpp
+++ src/consumption.cpp
@@ -1028,13 +1028,11 @@ void Character::modify_stimulation( const islot_comestible &comest )
         return;
     }
     const int current_stim = get_stim();
-    if( comest.stim != 0 || sgn( current_stim ) != sgn( comest.stim ) ) {
-        if( ( std::abs( comest.stim ) * 3 ) > std::abs( current_stim ) ) {
-            mod_stim( comest.stim );
-        } else {
-            comest.stim > 0 ? mod_stim( std::max( comest.stim / 2, 1 ) ) : mod_stim( std::min( comest.stim / 2,
-                    -1 ) );
-        }
+    if( ( std::abs( comest.stim ) * 3 ) > std::abs( current_stim ) ) {
+        mod_stim( comest.stim );
+    } else {
+        comest.stim > 0 ? mod_stim( std::max( comest.stim / 2, 1 ) ) : mod_stim( std::min( comest.stim / 2,
+                -1 ) );
     }
     if( has_trait( trait_STIMBOOST ) && ( current_stim > 30 ) &&
         ( ( comest.add == add_type::CAFFEINE ) || ( comest.add == add_type::SPEED ) ||
