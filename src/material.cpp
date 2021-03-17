1ca026a889bba97e3735e877f808c9d23a932629
Kevin Granade <kevin.granade@gmail.com>
2021-03-17 11:26:26 -0700

    Remove recycler and associated infrastructure (#48096)

--- src/material.cpp
+++ src/material.cpp
@@ -103,10 +103,6 @@ void material_type::load( const JsonObject &jsobj, const std::string & )
     optional( jsobj, was_loaded, "fuel_data", fuel );
 
     jsobj.read( "burn_products", _burn_products, true );
-
-    optional( jsobj, was_loaded, "compact_accepts", _compact_accepts,
-              auto_flags_reader<material_id>() );
-    optional( jsobj, was_loaded, "compacts_into", _compacts_into, auto_flags_reader<itype_id>() );
 }
 
 void material_type::check() const
@@ -122,17 +118,6 @@ void material_type::check() const
     }
     if( !item::type_is_defined( _repaired_with ) ) {
         debugmsg( "invalid \"repaired_with\" %s for %s.", _repaired_with.c_str(), id.c_str() );
-    }
-    for( const material_id &ca : _compact_accepts ) {
-        if( !ca.is_valid() ) {
-            debugmsg( "invalid \"compact_accepts\" %s for %s.", ca.c_str(), id.c_str() );
-        }
-    }
-    for( const itype_id &ci : _compacts_into ) {
-        if( !item::type_is_defined( ci ) ||
-            !item( ci, calendar::turn_zero ).only_made_of( std::set<material_id> { id } ) ) {
-            debugmsg( "invalid \"compacts_into\" %s for %s.", ci.c_str(), id.c_str() );
-        }
     }
 }
 
@@ -272,16 +257,6 @@ const mat_burn_products &material_type::burn_products() const
     return _burn_products;
 }
 
-const material_id_list &material_type::compact_accepts() const
-{
-    return _compact_accepts;
-}
-
-const mat_compacts_into &material_type::compacts_into() const
-{
-    return _compacts_into;
-}
-
 void materials::load( const JsonObject &jo, const std::string &src )
 {
     material_data.load( jo, src );
@@ -300,17 +275,6 @@ void materials::reset()
 material_list materials::get_all()
 {
     return material_data.get_all();
-}
-
-material_list materials::get_compactable()
-{
-    material_list all = get_all();
-    material_list compactable;
-    std::copy_if( all.begin(), all.end(),
-    std::back_inserter( compactable ), []( const material_type & mt ) {
-        return !mt.compacts_into().empty();
-    } );
-    return compactable;
 }
 
 std::set<material_id> materials::get_rotting()
