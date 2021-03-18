376b1b066689e3a03c0e070408e26555193572f3
Moltenhead <ectopneo@gmail.com>
2021-03-18 08:45:36 +0100

    fix?: private public shenanigans

--- src/inventory_ui.h
+++ src/inventory_ui.h
@@ -137,11 +137,10 @@ class inventory_entry
         void update_cache();
         bool highlight_as_parent = false;
         bool highlight_as_child = false;
-        /*const item_category *custom_category = nullptr;*/
 
     private:
-        bool enabled = true;
         const item_category *custom_category = nullptr;
+        bool enabled = true;
     protected:
         // indents the entry if it is contained in an item
         bool _indent = true;
