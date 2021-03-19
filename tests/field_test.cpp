c522f0088c151e36d60dce45e1ee50e6a78bc808
actual-nh <actual-nh@users.noreply.github.com>
2021-03-18 20:06:34 -0400

    Use <= instead of < in one field test
    The radioactive field test is repeatedly failing due to the test being
    for less-than instead of less-than-or-equal (300 vs 300 turns). Due to
    test logs not being searchable on either Github or Travis, I am not
    able to locate some past cases I have seen, but I can locate
    https://travis-ci.org/github/CleverRaven/Cataclysm-DDA/jobs/763202025
    and
    https://github.com/CleverRaven/Cataclysm-DDA/pull/48119/checks?check_run_id=2143797637.
    (I am grateful to wapcaplet for the PR enabling actually seeing what
    the numbers were.)

--- tests/field_test.cpp
+++ tests/field_test.cpp
@@ -262,7 +262,7 @@ TEST_CASE( "fd_fire and fd_fire_vent test", "[field]" )
     }
 
     {
-        INFO( string_format( "Should've converted to flame burst in under %d turns", time_limit_turns ) );
+        INFO( string_format( "Should've converted to flame burst in below %d turns", time_limit_turns ) );
         CHECK( fields_test_turns() < time_limit_turns );
     }
 
@@ -355,8 +355,8 @@ TEST_CASE( "radioactive field", "[field]" )
         m.process_fields();
     }
     {
-        INFO( string_format( "Terrain should be irradiated under %d turns", time_limit_turns ) );
-        CHECK( fields_test_turns() < time_limit_turns );
+        INFO( string_format( "Terrain should be irradiated in no more than %d turns", time_limit_turns ) );
+        CHECK( fields_test_turns() <= time_limit_turns );
     }
 
     // cleanup
@@ -386,7 +386,7 @@ TEST_CASE( "fungal haze test", "[field]" )
         }
     }
     {
-        INFO( string_format( "Terrain should be fungalized under %d turns", time_limit_turns ) );
+        INFO( string_format( "Terrain should be fungalized in below %d turns", time_limit_turns ) );
         CHECK( fields_test_turns() < time_limit_turns );
     }
 
@@ -423,7 +423,7 @@ TEST_CASE( "player_in_field test", "[field][player]" )
         is_field_alive = sap_field && sap_field->is_field_alive();
     }
     {
-        INFO( string_format( "Sap should disappear in under %d turns", time_limit_turns ) );
+        INFO( string_format( "Sap should disappear in below %d turns", time_limit_turns ) );
         CHECK( fields_test_turns() < time_limit_turns );
     }
 
