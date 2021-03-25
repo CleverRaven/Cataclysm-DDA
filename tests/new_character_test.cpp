6d7bb8297866c13737475e1e164a1f5f1c95d8e1
John Bytheway <jbytheway@gmail.com>
2021-03-25 13:57:05 -0400

    Better feedback on starting_items test
    When the test fails, report the scenario and the traits used by it.

--- tests/new_character_test.cpp
+++ tests/new_character_test.cpp
@@ -17,6 +17,7 @@
 #include "profession.h"
 #include "scenario.h"
 #include "string_formatter.h"
+#include "stringmaker.h" // IWYU pragma: keep
 #include "type_id.h"
 #include "visitable.h"
 
@@ -138,9 +139,12 @@ TEST_CASE( "starting_items", "[slow]" )
 
     std::vector<trait_id> traits = next_subset( mutations );
     for( ; !traits.empty(); traits = next_subset( mutations ) ) {
+        CAPTURE( traits );
         for( const auto &pair : scen_prof_combos ) {
             set_scenario( pair.first );
+            INFO( "Scenario = " + pair.first->ident().str() );
             for( const string_id<profession> &prof : pair.second ) {
+                CAPTURE( prof );
                 player_character.prof = &prof.obj();
                 if( !try_set_traits( traits ) ) {
                     continue; // Trait conflict: this prof/scen/trait combo is impossible to attain
