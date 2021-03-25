2f1ca4b61af2d37e0688416e2ce9f69aa08bf15c
Jamuro-g <76928284+Jamuro-g@users.noreply.github.com>
2021-03-25 08:15:54 +0100

    Fix allows siphon action to transfer tank contents inside same vehicle (#47647)

--- src/handle_liquid.cpp
+++ src/handle_liquid.cpp
@@ -250,14 +250,24 @@ static bool get_liquid_target( item &liquid, const item *const source, const int
         }
     }
     for( vehicle *veh : opts ) {
-        if( veh == source_veh ) {
-            continue;
+        if( veh == source_veh && veh->has_part( "FLUIDTANK", false ) ) {
+            for( const vpart_reference &vp : veh->get_avail_parts( "FLUIDTANK" ) ) {
+                if( vp.part().get_base().is_reloadable_with( liquid.typeId() ) ) {
+                    menu.addentry( -1, true, MENU_AUTOASSIGN, _( "Fill avaliable tank" ) );
+                    actions.emplace_back( [ &, veh]() {
+                        target.veh = veh;
+                        target.dest_opt = LD_VEH;
+                    } );
+                    break;
+                }
+            }
+        } else {
+            menu.addentry( -1, true, MENU_AUTOASSIGN, _( "Fill nearby vehicle %s" ), veh->name );
+            actions.emplace_back( [ &, veh]() {
+                target.veh = veh;
+                target.dest_opt = LD_VEH;
+            } );
         }
-        menu.addentry( -1, true, MENU_AUTOASSIGN, _( "Fill nearby vehicle %s" ), veh->name );
-        actions.emplace_back( [ &, veh]() {
-            target.veh = veh;
-            target.dest_opt = LD_VEH;
-        } );
     }
 
     for( const tripoint &target_pos : here.points_in_radius( player_character.pos(), 1 ) ) {
