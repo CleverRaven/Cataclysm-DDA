ce394e20c066c56f4f5d47bd3050b9bdd40e80ff
actual-nh <actual-nh@users.noreply.github.com>
2021-03-25 20:32:32 -0400

    Wipe visibility, map caches in monster_attack
    This is in case something is left over in one of these caches that's
    causing problems.

--- tests/monster_attack_test.cpp
+++ tests/monster_attack_test.cpp
@@ -6,6 +6,7 @@
 #include "catch/catch.hpp"
 #include "character.h"
 #include "game.h"
+#include "line.h"
 #include "map.h"
 #include "map_helpers.h"
 #include "monster.h"
@@ -22,17 +23,37 @@ static void test_monster_attack( const tripoint &target_offset, bool expect_atta
                                  bool expect_vision )
 {
     int day_hour = hour_of_day<int>( calendar::turn );
-    clear_creatures();
     CAPTURE( day_hour );
     REQUIRE( is_day( calendar::turn ) );
+    clear_creatures();
     // Monster adjacent to target.
     const std::string monster_type = "mon_zombie";
     const tripoint target_location = attacker_location + target_offset;
+    int distance = rl_dist( attacker_location, target_location );
+    CAPTURE( distance );
+    int a_zlev = attacker_location.z;
+    int t_zlev = target_location.z;
     Character &you = get_player_character();
     clear_avatar();
     you.setpos( target_location );
-    you.recalc_sight_limits();
     monster &test_monster = spawn_test_monster( monster_type, attacker_location );
+    map &here = get_map();
+    // Why twice? See vision_test.cpp
+    here.update_visibility_cache( a_zlev );
+    here.invalidate_map_cache( a_zlev );
+    here.build_map_cache( a_zlev );
+    here.update_visibility_cache( a_zlev );
+    here.invalidate_map_cache( a_zlev );
+    here.build_map_cache( a_zlev );
+    if( a_zlev != t_zlev ) {
+        here.update_visibility_cache( t_zlev );
+        here.invalidate_map_cache( t_zlev );
+        here.build_map_cache( t_zlev );
+        here.update_visibility_cache( t_zlev );
+        here.invalidate_map_cache( t_zlev );
+        here.build_map_cache( t_zlev );
+    }
+    you.recalc_sight_limits();
     // Trigger basic attack.
     CAPTURE( attacker_location );
     CAPTURE( target_location );
@@ -43,8 +64,22 @@ static void test_monster_attack( const tripoint &target_offset, bool expect_atta
     clear_creatures();
     clear_avatar();
     you.setpos( attacker_location );
-    you.recalc_sight_limits();
     monster &target_monster = spawn_test_monster( monster_type, target_location );
+    here.update_visibility_cache( a_zlev );
+    here.invalidate_map_cache( a_zlev );
+    here.build_map_cache( a_zlev );
+    here.update_visibility_cache( a_zlev );
+    here.invalidate_map_cache( a_zlev );
+    here.build_map_cache( a_zlev );
+    if( a_zlev != t_zlev ) {
+        here.update_visibility_cache( t_zlev );
+        here.invalidate_map_cache( t_zlev );
+        here.build_map_cache( t_zlev );
+        here.update_visibility_cache( t_zlev );
+        here.invalidate_map_cache( t_zlev );
+        here.build_map_cache( t_zlev );
+    }
+    you.recalc_sight_limits();
     CHECK( you.sees( target_monster ) == expect_vision );
     CHECK( you.melee_attack( target_monster, false ) == expect_attack );
 }
@@ -79,12 +114,13 @@ TEST_CASE( "monster_attack" )
     clear_map();
     restore_on_out_of_scope<time_point> restore_calendar_turn( calendar::turn );
     calendar::turn = daylight_time( calendar::turn ) + 2_hours;
-    get_weather().weather_override = WEATHER_CLEAR; // Do I need to put this back?
+    get_weather().weather_override = WEATHER_CLEAR; // Restore?
     SECTION( "attacking on open ground" ) {
         // Adjacent can attack of course.
         for( const tripoint &offset : eight_horizontal_neighbors ) {
             test_monster_attack( offset, true, true );
         }
+        clear_map();
         // Too far away cannot.
         test_monster_attack( { 2, 2, 0 }, false, true );
         test_monster_attack( { 2, 1, 0 }, false, true );
