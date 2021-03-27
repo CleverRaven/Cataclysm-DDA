094480b55801c9e8fefe722ad352351330f92149
Alessandro Foresi <alefor@gmail.com>
2021-03-27 13:20:10 +0100

    Fix string formatter test in MSVC >= 16.2
    Uses Microsoft's suggested stdio float rounding object file

--- tests/string_formatter_test.cpp
+++ tests/string_formatter_test.cpp
@@ -101,10 +101,7 @@ void mingw_test( const char *const old_pattern, const char *const new_pattern, c
     CHECK( original_result == new_result );
 }
 
-// Marking mayfail due to failure in Appveyor.  Looks like a bug in the Visual
-// Studio runtime libraries.  Once that failure stops showing up on Appveyor,
-// this can cease to be marked thus.
-TEST_CASE( "string_formatter", "[!mayfail]" )
+TEST_CASE( "string_formatter" )
 {
     test_typed_printf<signed char>( "%hhi", "%i" );
     test_typed_printf<unsigned char>( "%hhu", "%u" );
