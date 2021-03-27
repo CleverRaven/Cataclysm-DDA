c90ae42aa53023b0da65ee84d00a2e19744e6bc7
actual-nh <actual-nh@users.noreply.github.com>
2021-03-27 09:32:33 -0400

    Wrong syntax for scoped_weather_override.
    As mentioned before, I'm still learning regarding C++ differences
    from C.

--- tests/monster_attack_test.cpp
+++ tests/monster_attack_test.cpp
@@ -114,7 +114,7 @@ TEST_CASE( "monster_attack" )
     clear_map();
     restore_on_out_of_scope<time_point> restore_calendar_turn( calendar::turn );
     calendar::turn = daylight_time( calendar::turn ) + 2_hours;
-    scoped_weather_override( WEATHER_CLEAR );
+    scoped_weather_override weather_clear( WEATHER_CLEAR );
     SECTION( "attacking on open ground" ) {
         // Adjacent can attack of course.
         for( const tripoint &offset : eight_horizontal_neighbors ) {
