c36aab887775f9d1c108cec6449ccd80c47086bd
Eric <52087122+Ramza13@users.noreply.github.com>
2021-03-26 22:34:11 -0400

    Change weather to use effect_on_conditions (#48178)
    * Move weather into effect_on_conditions
    
    * Update effect_on_condition.json
    
    * LGTM issue

--- src/debug_menu.cpp
+++ src/debug_menu.cpp
@@ -2663,8 +2663,7 @@ void debug()
             }
             break;
         case debug_menu_index::TEST_WEATHER: {
-            get_weather().get_cur_weather_gen().test_weather( g->get_seed(),
-                    get_weather().next_instance_allowed );
+            get_weather().get_cur_weather_gen().test_weather( g->get_seed() );
         }
         break;
 
