e878370bd07d3b6e0cbd5b6db0aa3fe549073141
actual-nh <actual-nh@users.noreply.github.com>
2021-03-27 20:36:49 -0400

    Use is_adjacent with fov_3d for melee attacks
    This changes the melee_attack code in melee.cpp for
    Character::melee_attack to consider non-adjacent anything across
    z-levels when fov_3d is false. (It is possible that an actual sees
    check will be needed, but let's see how this does - no pun intended.)
    reach_attack may be a problem still.

--- src/melee.cpp
+++ src/melee.cpp
@@ -17,6 +17,7 @@
 
 #include "avatar.h"
 #include "bodypart.h"
+#include "cached_options.h"
 #include "calendar.h"
 #include "cata_utility.h"
 #include "character.h"
@@ -476,7 +477,7 @@ bool Character::melee_attack( Creature &t, bool allow_special, const matec_id &f
         add_msg_if_player( m_info, _( "You lack the substance to affect anything." ) );
         return false;
     }
-    if( !is_adjacent( &t, true ) ) {
+    if( !is_adjacent( &t, fov_3d ) ) {
         return false;
     }
     return melee_attack_abstract( t, allow_special, force_technique, allow_unarmed );
