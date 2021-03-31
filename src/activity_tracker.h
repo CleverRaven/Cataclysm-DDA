b4a8b6a201d66fc61dc74096c7c5795da50fe9bb
Zhilkin Serg <ZhilkinSerg@users.noreply.github.com>
2021-03-31 11:32:30 +0300

    Apply suggestions from code review
    Co-authored-by: Alex Mooney <alexmooney@fastmail.com>

--- src/activity_tracker.h
+++ src/activity_tracker.h
@@ -21,9 +21,9 @@ class activity_tracker
         // Weariness metadata.
         int tracker = 0;
         int intake = 0;
-        // Semi-consecutive 5 minute ticks of low activity (or 2.5 if we're sleeping)
+        // Semi-consecutive 5 minute ticks of low activity (or 2.5 if we're sleeping).
         int low_activity_ticks = 0;
-        // How many ticks since we've decreased intake
+        // How many ticks since we've decreased intake.
         int tick_counter = 0;
     public:
         // Logs activity level. If called multiple times in one turn, will preserve the highest.
@@ -32,13 +32,13 @@ class activity_tracker
         void new_turn();
         // Resets accumulated activity level.
         void reset_activity_level();
-        // outputs player activity level to a printable string
+        // Outputs player activity level to a printable string.
         std::string activity_level_str() const;
         // Returns activity level recorded for the current turn.
         float activity() const;
         // Returns average of activity level for the current period.
         float average_activity() const;
-        // Returns the previous turn's activity level until an action is tanken on the current turn.
+        // Returns the previous turn's activity level until an action is taken on the current turn.
         float instantaneous_activity_level() const;
 
         int weariness() const;
