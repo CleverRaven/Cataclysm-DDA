de94ec61bfe8606d94321eacb00311a1a56b551a
Eric <52087122+Ramza13@users.noreply.github.com>
2021-04-06 01:19:34 -0400

    Unhardcode bio_drain bionic faults (#48295)

--- src/talker_character.h
+++ src/talker_character.h
@@ -56,6 +56,7 @@ class talker_character: public talker
         int int_cur() const override;
         int per_cur() const override;
         int pain_cur() const override;
+        units::energy power_cur() const override;
         bool has_trait( const trait_id &trait_to_check ) const override;
         void set_mutation( const trait_id &new_trait ) override;
         void unset_mutation( const trait_id &old_trait ) override;
