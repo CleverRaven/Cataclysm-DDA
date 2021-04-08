CleverRaven/Cataclysm-DDA

commit e8b95ea30a0bd31571dbf7e27e2775b06163af24

Better debug learn spell menu (#47946)
Co-authored-by: Jianxiang Wang (王健翔) <qrox@sina.com>

src/magic.cpp
@@ -1260,7 +1260,7 @@ int spell::get_max_level() const
 // helper function to calculate xp needed to be at a certain level
 // pulled out as a helper function to make it easier to either be used in the future
 // or easier to tweak the formula
-static int exp_for_level( int level )
+int spell::exp_for_level( int level )
 {
     // level 0 never needs xp
     if( level == 0 ) {
@@ -1825,7 +1825,7 @@ class spellcasting_callback : public uilist_callback
         }
 };
 
-static bool casting_time_encumbered( const spell &sp, const Character &guy )
+bool spell_desc::casting_time_encumbered( const spell &sp, const Character &guy )
 {
     int encumb = 0;
     if( !sp.has_flag( spell_flag::NO_LEGS ) ) {
@@ -1841,7 +1841,7 @@ static bool casting_time_encumbered( const spell &sp, const Character &guy )
     return encumb > 0;
 }
 
-static bool energy_cost_encumbered( const spell &sp, const Character &guy )
+bool spell_desc::energy_cost_encumbered( const spell &sp, const Character &guy )
 {
     if( !sp.has_flag( spell_flag::NO_HANDS ) ) {
         return std::max( 0, guy.encumb( bodypart_id( "hand_l" ) ) + guy.encumb(
@@ -1853,7 +1853,7 @@ static bool energy_cost_encumbered( const spell &sp, const Character &guy )
 
 // this prints various things about the spell out in a list
 // including flags and things like "goes through walls"
-static std::string enumerate_spell_data( const spell &sp )
+std::string spell_desc::enumerate_spell_data( const spell &sp )
 {
     std::vector<std::string> spell_data;
     if( sp.has_flag( spell_flag::CONCENTRATE ) ) {
@@ -1904,7 +1904,7 @@ void spellcasting_callback::draw_spell_info( const spell &sp, const uilist *menu
     line++;
 
     line += fold_and_print( w_menu, point( h_col1, line ), info_width, gray,
-                            enumerate_spell_data( sp ) );
+                            spell_desc::enumerate_spell_data( sp ) );
     if( line <= win_height / 3 ) {
         line++;
     }
@@ -1930,7 +1930,7 @@ void spellcasting_callback::draw_spell_info( const spell &sp, const uilist *menu
         line++;
     }
 
-    const bool cost_encumb = energy_cost_encumbered( sp, player_character );
+    const bool cost_encumb = spell_desc::energy_cost_encumbered( sp, player_character );
     std::string cost_string = cost_encumb ? _( "Casting Cost (impeded)" ) : _( "Casting Cost" );
     std::string energy_cur = sp.energy_source() == magic_energy_type::hp ? "" :
                              string_format( _( " (%s current)" ), sp.energy_cur_string( player_character ) );
@@ -1941,7 +1941,7 @@ void spellcasting_callback::draw_spell_info( const spell &sp, const uilist *menu
     print_colored_text( w_menu, point( h_col1, line++ ), gray, gray,
                         string_format( "%s: %s %s%s", cost_string,
                                        sp.energy_cost_string( player_character ), sp.energy_string(), energy_cur ) );
-    const bool c_t_encumb = casting_time_encumbered( sp, player_character );
+    const bool c_t_encumb = spell_desc::casting_time_encumbered( sp, player_character );
     print_colored_text( w_menu, point( h_col1, line++ ), gray, gray, colorize(
                             string_format( "%s: %s", c_t_encumb ? _( "Casting Time (impeded)" ) : _( "Casting Time" ),
                                            moves_to_string( sp.casting_time( player_character ) ) ),

src/debug_menu.h
@@ -16,6 +16,7 @@ template<typename T>
 class optional;
 } // namespace cata
 
+class Character;
 class player;
 
 namespace debug_menu
@@ -80,14 +81,15 @@ enum class debug_menu_index : int {
     DISPLAY_REACHABILITY_ZONES,
     DISPLAY_RADIATION,
     HOUR_TIMER,
-    LEARN_SPELLS,
-    LEVEL_SPELLS,
+    CHANGE_SPELLS,
     TEST_MAP_EXTRA_DISTRIBUTION,
     NESTED_MAPGEN,
     VEHICLE_BATTERY_CHARGE,
     GENERATE_EFFECT_LIST,
     last
 };
+
+void change_spells( Character &character );
 
 void teleport_short();
 void teleport_long();

src/magic.h
@@ -286,7 +286,7 @@ class spell_type
         // increment of energy cost per spell level
         float energy_increment = 0.0f;
         // max or min energy cost, based on sign of energy_increment
-        int final_energy_cost = 0.0f;
+        int final_energy_cost = 0;
 
         // spell is restricted to being cast by only this class
         // if spell_class is empty, spell is unrestricted
@@ -380,6 +380,14 @@ class spell_type
         static const float casting_time_increment_default;
 };
 
+// functions for spell description
+namespace spell_desc
+{
+bool casting_time_encumbered( const spell &sp, const Character &guy );
+bool energy_cost_encumbered( const spell &sp, const Character &guy );
+std::string enumerate_spell_data( const spell &sp );
+} // namespace spell_desc
+
 class spell
 {
     private:
@@ -410,6 +418,7 @@ class spell
         // sets the message to be different than the spell_type specifies
         void set_message( const translation &msg );
 
+        static int exp_for_level( int level );
         // how much exp you need for the spell to gain a level
         int exp_to_next_level() const;
         // progress to the next level, expressed as a percent
@@ -600,6 +609,10 @@ class known_magic
         int select_spell( Character &guy );
         // get all known spells
         std::vector<spell *> get_spells();
+        // directly get the character known spells
+        std::map<spell_id, spell> &get_spellbook() {
+            return spellbook;
+        }
         // how much mana is available to use to cast spells
         int available_mana() const;
         // max mana vailable

src/output.cpp
@@ -616,7 +616,7 @@ border_helper::border_info &border_helper::add_border()
     return border_info_list.front();
 }
 
-void border_helper::draw_border( const catacurses::window &win )
+void border_helper::draw_border( const catacurses::window &win, nc_color border_color )
 {
     if( !border_connection_map.has_value() ) {
         border_connection_map.emplace();
@@ -654,7 +654,7 @@ void border_helper::draw_border( const catacurses::window &win )
     for( const std::pair<const point, border_connection> &conn : border_connection_map.value() ) {
         if( conn.first.x >= win_beg.x && conn.first.x < win_end.x &&
             conn.first.y >= win_beg.y && conn.first.y < win_end.y ) {
-            mvwputch( win, conn.first - win_beg, BORDER_COLOR, conn.second.as_curses_line() );
+            mvwputch( win, conn.first - win_beg, border_color, conn.second.as_curses_line() );
         }
     }
 }

src/output.h
@@ -411,7 +411,7 @@ class border_helper
         };
 
         border_info &add_border();
-        void draw_border( const catacurses::window &win );
+        void draw_border( const catacurses::window &win, nc_color border_color = BORDER_COLOR );
 
         friend class border_info;
     private:

commit b5e806396c4906d5dbc82bea044621f64d361a16

Fix compilation of magic.cpp
This got broken by #47946 because exp_for_level was made a member
function but one call to it was not updated.  Fix that.

src/magic.cpp
@@ -2388,7 +2388,7 @@ spell fake_spell::get_spell( int min_level_override ) const
         debugmsg( "ERROR: fake spell %s has higher min_level than max_level", id.c_str() );
         return sp;
     }
-    sp.set_exp( exp_for_level( level_of_spell ) );
+    sp.set_exp( sp.exp_for_level( level_of_spell ) );
 
     return sp;
 }