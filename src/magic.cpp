8188dfd4bc5ab7630cf1871fe79573e3b7ddf117
John Bytheway <jbytheway@gmail.com>
2021-04-08 11:06:26 -0400

    Fix compilation of magic.cpp (#48430)
    This got broken by #47946 because exp_for_level was made a member
    function but one call to it was not updated.  Fix that.

--- src/magic.cpp
+++ src/magic.cpp
@@ -2388,7 +2388,7 @@ spell fake_spell::get_spell( int min_level_override ) const
         debugmsg( "ERROR: fake spell %s has higher min_level than max_level", id.c_str() );
         return sp;
     }
-    sp.set_exp( exp_for_level( level_of_spell ) );
+    sp.set_exp( sp.exp_for_level( level_of_spell ) );
 
     return sp;
 }
