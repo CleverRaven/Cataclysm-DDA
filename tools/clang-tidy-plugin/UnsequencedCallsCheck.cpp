4bea7883422ea8238b1fa30acf5c1a787d75cbc0
John Bytheway <jbytheway@gmail.com>
2021-04-13 19:32:54 -0400

    Exempt more things from unsequenced calls check
    Treat member functions called begin, end, find as special and
    effectively const.
    
    Treat the ternary operator as sequencing its args.

--- tools/clang-tidy-plugin/UnsequencedCallsCheck.cpp
+++ tools/clang-tidy-plugin/UnsequencedCallsCheck.cpp
@@ -39,12 +39,15 @@ void UnsequencedCallsCheck::registerMatchers( MatchFinder *Finder )
 // a non-expression, return the previous expression.
 // This is a vague approximation to finding the AST node which represents
 // everything between two sequence points.
-static const Expr *getContainingStatement(
+static const Expr *getContainingSequenceStatement(
     const ast_matchers::MatchFinder::MatchResult &Result, const Expr *Node )
 {
     for( const ast_type_traits::DynTypedNode &parent : Result.Context->getParents( *Node ) ) {
+        if( parent.get<ConditionalOperator>() ) {
+            return Node;
+        }
         if( const Expr *Candidate = parent.get<Expr>() ) {
-            return getContainingStatement( Result, Candidate );
+            return getContainingSequenceStatement( Result, Candidate );
         }
     }
 
@@ -61,25 +64,39 @@ void UnsequencedCallsCheck::CheckCall( const MatchFinder::MatchResult &Result )
         return;
     }
 
-    const Expr *ContainingStatement = getContainingStatement( Result, MemberCall );
+    const Expr *ContainingStatement = getContainingSequenceStatement( Result, MemberCall );
 
     CallContext context{ ContainingStatement, On };
     calls_[context].push_back( MemberCall );
 }
 
+static bool IsEffectivelyConstCall( const CXXMemberCallExpr *Call )
+{
+    const CXXMethodDecl *method = Call->getMethodDecl();
+    if( method->isConst() ) {
+        return true;
+    }
+    StringRef Name = method->getName();
+    if( Name == "begin" || Name == "end" || Name == "find" ) {
+        return true;
+    }
+    return false;
+}
+
 void UnsequencedCallsCheck::onEndOfTranslationUnit()
 {
-    for( const std::pair<const CallContext, std::vector<const CXXMemberCallExpr *>> &p : calls_ ) {
+    for( std::pair<const CallContext, std::vector<const CXXMemberCallExpr *>> &p : calls_ ) {
         const CallContext &context = p.first;
-        const std::vector<const CXXMemberCallExpr *> &calls = p.second;
+        std::vector<const CXXMemberCallExpr *> &calls = p.second;
+        std::sort( calls.begin(), calls.end() );
+        calls.erase( std::unique( calls.begin(), calls.end() ), calls.end() );
 
         if( calls.size() < 2 ) {
             continue;
         }
         bool any_non_const = false;
         for( const CXXMemberCallExpr *member_call : calls ) {
-            const CXXMethodDecl *method = member_call->getMethodDecl();
-            if( !method->isConst() ) {
+            if( !IsEffectivelyConstCall( member_call ) ) {
                 any_non_const = true;
                 break;
             }
@@ -90,6 +107,17 @@ void UnsequencedCallsCheck::onEndOfTranslationUnit()
                 context.stmt->getBeginLoc(),
                 "Unsequenced calls to member functions of %0, at least one of which is non-const."
             ) << context.on;
+
+            for( const CXXMemberCallExpr *member_call : calls ) {
+                const CXXMethodDecl *method = member_call->getMethodDecl();
+                if( IsEffectivelyConstCall( member_call ) ) {
+                    diag( member_call->getBeginLoc(), "Call to const member function %0",
+                          DiagnosticIDs::Note ) << method;
+                } else {
+                    diag( member_call->getBeginLoc(), "Call to non-const member function %0",
+                          DiagnosticIDs::Note ) << method;
+                }
+            }
         }
     }
 }
