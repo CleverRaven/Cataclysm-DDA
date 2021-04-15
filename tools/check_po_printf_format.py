82abe2e16f3565fad0a87aa8b3c01c8f45b8d287
Binrui Dong <brett.browning.dong@gmail.com>
2021-04-15 12:37:59 -0400

    Coloured output and highlighted diagnostic message

--- tools/check_po_printf_format.py
+++ tools/check_po_printf_format.py
@@ -36,6 +36,7 @@ def decompose_format_strings(msg):
     indices = list(findall("%", msg))
     non_positional = []
     positional = []
+    segments = []
     index_iter = iter(indices)
     for i in index_iter:
         if i + 1 == len(msg):
@@ -46,6 +47,7 @@ def decompose_format_strings(msg):
         if msg[i + 1].isnumeric() and msg[i + 2] == "$":
             # positional format strings %1$s, %2$d, etc.
             positional.append(msg[i:i + 4])
+            segments.append((i, i + 4))
         else:
             # non-positional format strings %s, %3d, etc.
             idx = i + 1
@@ -55,37 +57,42 @@ def decompose_format_strings(msg):
                 break
             # ignore the cases where "%d" is translated to "%2d"
             non_positional.append(re.sub(r'[0-9]+', '', msg[i:idx + 1]))
-    return (non_positional, positional)
+            segments.append((i, idx + 1))
+    return (non_positional, positional, segments)
 
 
-def check_message(msgid, msgstr):
+def check_message(entry):
+    msgid = entry.msgid
+    msgstr = entry.msgstr
     if not is_formatted_entry(msgid) and not is_formatted_entry(msgstr):
-        return True
-    (non_pos_msgid, pos_msgid) = decompose_format_strings(msgid)
-    (non_pos_msgstr, pos_msgstr) = decompose_format_strings(msgstr)
+        return (True, [], [])
+    (non_pos_msgid, pos_msgid, seg_msgid) = decompose_format_strings(msgid)
+    (non_pos_msgstr, pos_msgstr, seg_msgstr) = decompose_format_strings(msgstr)
     if non_pos_msgid == non_pos_msgstr and \
        sorted(pos_msgid) == sorted(pos_msgstr):
-        return True
+        return (True, [], [])
     # Allow "%1$s %2$d" translated to "%s %d"
     # as long as the order of types is preserved
     if len(non_pos_msgid) == 0 and len(pos_msgid) > 0 and \
        len(non_pos_msgstr) == len(pos_msgid) and len(pos_msgstr) == 0:
         for i in range(len(pos_msgid)):
             if pos_msgid[i][-1] != non_pos_msgstr[i][-1]:
-                return False
-        return True
+                return (False, seg_msgid, seg_msgstr)
+        return (True, [], [])
     # otherwise it's an error
-    return False
+    return (False, seg_msgid, seg_msgstr)
 
 
 def check_po_file(file):
     pofile = polib.pofile(file)
     errors = []
     for entry in pofile.translated_entries():
-        # TODO: implement proper check for plural messages
-        if not entry.msgid_plural:
-            if not check_message(entry.msgid, entry.msgstr):
-                errors.append((entry.msgid, entry.msgstr))
+        if entry.msgid_plural:
+            # TODO: implement proper check for plural messages
+            continue
+        (ok, seg_msgid, seg_msgstr) = check_message(entry)
+        if not ok:
+            errors.append((entry.msgid, entry.msgstr, seg_msgid, seg_msgstr))
     return errors
 
 
@@ -101,22 +108,35 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
+def print_message(msg, segments):
+    beg = [x for (x, y) in segments]
+    end = [y for (x, y) in segments]
+    for i in range(len(msg)):
+        if i in beg:
+            print(bcolors.FAIL, end="")
+            print(bcolors.UNDERLINE, end="")
+        elif i in end:
+            print(bcolors.ENDC, end="")
+        print(msg[i], end="")
+    print()
+
+
 num_errors = 0
-for file in os.listdir("lang/po"):
-    if file.endswith(".po"):
-        print("Checking", file)
+for file in sorted(os.listdir("lang/po")):
+    if file.endswith(".po") and not file.endswith("en.po"):
+        print("Checking", file, end="", flush=True)
         errors = check_po_file("lang/po/" + file)
         n = len(errors)
         num_errors += n
         if n > 0:
-            print(f"{bcolors.FAIL}{n} error(s) detected:{bcolors.ENDC}")
-            for (msgid, msgstr) in errors:
-                print(f"{bcolors.BOLD}original  :{bcolors.ENDC}",
-                      msgid.replace("\n\n", "\n"))
-                print(f"{bcolors.BOLD}translated:{bcolors.ENDC}",
-                      msgstr.replace("\n\n", "\n"))
+            print(f" => {bcolors.FAIL}{n} error(s) detected:{bcolors.ENDC}")
+            for (msgid, msgstr, seg_msgid, seg_msgstr) in errors:
+                print(f"{bcolors.BOLD}original  :{bcolors.ENDC}", end="")
+                print_message(msgid.replace("\n\n", "\n"), seg_msgid)
+                print(f"{bcolors.BOLD}translated:{bcolors.ENDC}", end="")
+                print_message(msgstr.replace("\n\n", "\n"), seg_msgstr)
                 print()
         else:
-            print(f"{bcolors.OKGREEN}No error detected.{bcolors.ENDC}")
+            print(f" => {bcolors.OKGREEN}No error detected.{bcolors.ENDC}")
         print()
 exit(num_errors)
