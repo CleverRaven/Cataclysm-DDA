ac446bbe15644fb299521026115d0983842d36ba
Salty Panda <zjadaczwatrob@gmail.com>
2021-04-26 21:02:34 +0200

    Update src/vehicle_move.cpp
    Co-authored-by: mqrause <38702195+mqrause@users.noreply.github.com>

--- src/vehicle_move.cpp
+++ src/vehicle_move.cpp
@@ -1071,7 +1071,7 @@ veh_collision vehicle::part_collision( int part, const tripoint &p,
             }
         }
 
-        if( critter == nullptr || ( critter != nullptr && !critter->is_hallucination() ) ) {
+        if( critter == nullptr || !critter->is_hallucination() ) {
             coll_velocity = vel1_a * 100;
         }
         // Stop processing when sign inverts, not when we reach 0
