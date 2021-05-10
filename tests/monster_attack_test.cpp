421cefdea511f641e361efa90fc938ba9a72b80e
Kevin Granade <kevin.granade@gmail.com>
2021-05-10 00:50:18 -0700

    Add test for monster reach attacks

--- tests/monster_attack_test.cpp
+++ tests/monster_attack_test.cpp
@@ -9,6 +9,7 @@
 #include "line.h"
 #include "map.h"
 #include "map_helpers.h"
+#include "monattack.h"
 #include "monster.h"
 #include "options_helpers.h"
 #include "player_helpers.h"
@@ -42,7 +43,7 @@ static void reset_caches( int a_zlev, int t_zlev )
 }
 
 static void test_monster_attack( const tripoint &target_offset, bool expect_attack,
-                                 bool expect_vision )
+                                 bool expect_vision, bool( *special_attack )( monster *x ) = nullptr )
 {
     int day_hour = hour_of_day<int>( calendar::turn );
     CAPTURE( day_hour );
@@ -59,13 +60,18 @@ static void test_monster_attack( const tripoint &target_offset, bool expect_atta
     clear_avatar();
     you.setpos( target_location );
     monster &test_monster = spawn_test_monster( monster_type, attacker_location );
+    test_monster.set_goal( target_location );
     reset_caches( a_zlev, t_zlev );
     // Trigger basic attack.
     CAPTURE( attacker_location );
     CAPTURE( target_location );
     CAPTURE( fov_3d );
     CHECK( test_monster.sees( target_location ) == expect_vision );
-    CHECK( test_monster.attack_at( target_location ) == expect_attack );
+    if( special_attack == nullptr ) {
+        CHECK( test_monster.attack_at( target_location ) == expect_attack );
+    } else {
+        CHECK( special_attack( &test_monster ) == expect_attack );
+    }
     // Then test the reverse.
     clear_creatures();
     clear_avatar();
@@ -73,7 +79,9 @@ static void test_monster_attack( const tripoint &target_offset, bool expect_atta
     monster &target_monster = spawn_test_monster( monster_type, target_location );
     reset_caches( a_zlev, t_zlev );
     CHECK( you.sees( target_monster ) == expect_vision );
-    CHECK( you.melee_attack( target_monster, false ) == expect_attack );
+    if( special_attack == nullptr ) {
+        CHECK( you.melee_attack( target_monster, false ) == expect_attack );
+    }
 }
 
 static void monster_attack_zlevel( const std::string &title, const tripoint &offset,
@@ -139,4 +147,23 @@ TEST_CASE( "monster_attack", "[vision][reachability]" )
 
     monster_attack_zlevel( "attack up ledge", tripoint_above, "t_floor", "t_floor", false );
     monster_attack_zlevel( "attack down ledge", tripoint_below, "t_floor", "t_floor", false );
+}
+
+TEST_CASE( "monster_special_attack", "[vision][reachability]" )
+{
+    clear_map();
+    restore_on_out_of_scope<time_point> restore_calendar_turn( calendar::turn );
+    calendar::turn = daylight_time( calendar::turn ) + 2_hours;
+    scoped_weather_override weather_clear( WEATHER_CLEAR );
+    restore_on_out_of_scope<bool> restore_fov_3d( fov_3d );
+    fov_3d = GENERATE( false, true );
+    override_option opt( "FOV_3D", fov_3d ? "true" : "false" );
+    get_map().ter_set( attacker_location + tripoint{ 2, 0, 0 }, ter_id( "t_wall" ) );
+    get_map().ter_set( attacker_location + tripoint{ 2, 0, 1 }, ter_id( "t_floor" ) );
+    get_map().ter_set( attacker_location + tripoint{ 1, 0, 0 }, ter_id( "t_wall" ) );
+    get_map().ter_set( attacker_location + tripoint{ 1, 0, 1 }, ter_id( "t_floor" ) );
+    // Adjacent should be visible if 3d vision is on, but it's too close to attack.
+    //test_monster_attack( { 1, 0, 1 },  false, fov_3d, mattack::stretch_attack );
+    // At a distance of 2, the ledge should block los and line of attack.
+    test_monster_attack( { 2, 0, 1 },  false, false, mattack::stretch_attack );
 }
