d697d7ab61b06cbe4b7b709a7da1f426d55f4351
John Bytheway <jbytheway@gmail.com>
2021-05-14 14:45:12 -0400

    clang-tidy modernize-use-emplace (#48661)
    * Port code to emplace_back
    
    This is clang-tidy making automated changes to convert push_back into
    emplace_back whenever the argument to push_back is either a constructor
    call for the type in question of an expression of a type implicitly
    convertible to the type in question.
    
    This should eliminate one move-construction of the inserted object for
    each call.
    
    * Enable clang-tidy modernize-use-emplace

--- src/lightmap.cpp
+++ src/lightmap.cpp
@@ -493,7 +493,7 @@ void map::generate_lightmap( const int zlev )
                         }
                         const float light_override = cur->get_intensity_level().local_light_override;
                         if( light_override >= 0.0f ) {
-                            lm_override.push_back( std::pair<tripoint, float>( p, light_override ) );
+                            lm_override.emplace_back( p, light_override );
                         }
                     }
                 }
