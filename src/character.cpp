2f85fca7c0ba823721785e031a3820706f39f215
Welp <jtf515@gmail.com>
2021-05-19 09:26:03 -0700

    Drop time estimate, Add some debug info, add minimum error to estimate_effect_dur()

--- src/character.cpp
+++ src/character.cpp
@@ -13152,14 +13152,14 @@ bool Character::has_flag( const json_character_flag &flag ) const
 
 time_duration Character::estimate_effect_dur( const skill_id &relevant_skill,
         const efftype_id &target_effect, const time_duration &error_magnitude,
-        int threshold, const Creature &target ) const
+        const time_duration &minimum_error, int threshold, const Creature &target ) const
 {
     const time_duration zero_duration = 0_turns;
 
     int skill_lvl = get_skill_level( relevant_skill );
 
     time_duration estimate = std::max( zero_duration, target.get_effect_dur( target_effect ) +
-                                       rng_float( -1, 1 ) * error_magnitude *
-                                       std::max( 0.0, static_cast<double>( threshold - skill_lvl ) / threshold ) );
+                                       rng_float( -1, 1 ) * ( minimum_error + ( error_magnitude - minimum_error ) *
+                                               std::max( 0.0, static_cast<double>( threshold - skill_lvl ) / threshold ) ) );
     return estimate;
 }
