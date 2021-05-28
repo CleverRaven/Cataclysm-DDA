daa7cb996cf19ba9eaf5af088a7f94e2c332f37f
Curtis Merrill <curtis.r.merrill@gmail.com>
2021-05-28 06:41:36 -0400

    [Magiclysm] Fix ring stacking by adding ONE_PER_LAYER flag (#47518)

--- src/item.cpp
+++ src/item.cpp
@@ -736,6 +736,38 @@ bool item::covers( const bodypart_id &bp ) const
     return does_cover;
 }
 
+cata::optional<side> item::covers_overlaps( const item &rhs ) const
+{
+    if( get_layer() != rhs.get_layer() ) {
+        return cata::nullopt;
+    }
+    const islot_armor *armor = find_armor_data();
+    if( armor == nullptr ) {
+        return cata::nullopt;
+    }
+    const islot_armor *rhs_armor = rhs.find_armor_data();
+    if( rhs_armor == nullptr ) {
+        return cata::nullopt;
+    }
+    body_part_set this_covers;
+    for( const armor_portion_data &data : armor->data ) {
+        if( data.covers.has_value() ) {
+            this_covers.unify_set( *data.covers );
+        }
+    }
+    body_part_set rhs_covers;
+    for( const armor_portion_data &data : rhs_armor->data ) {
+        if( data.covers.has_value() ) {
+            rhs_covers.unify_set( *data.covers );
+        }
+    }
+    if( this_covers.intersect_set( rhs_covers ).any() ) {
+        return rhs.get_side();
+    } else {
+        return cata::nullopt;
+    }
+}
+
 body_part_set item::get_covered_body_parts() const
 {
     return get_covered_body_parts( get_side() );
