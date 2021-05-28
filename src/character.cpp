04d3377d1887151a948ae093109029379ac51a40
Zhilkin Serg <ZhilkinSerg@users.noreply.github.com>
2021-05-28 22:18:09 +0300

    Fix crash when dropping items

--- src/character.cpp
+++ src/character.cpp
@@ -3267,7 +3267,7 @@ void Character::drop( const drop_locations &what, const tripoint &target,
     const cata::optional<vpart_reference> vp = get_map().veh_at(
                 target ).part_with_feature( "CARGO", false );
     if( rl_dist( pos(), target ) > 1 || !( stash || get_map().can_put_items( target ) )
-        || vp->part().is_cleaner_on() ) {
+        || ( vp.has_value() && vp->part().is_cleaner_on() ) ) {
         add_msg_player_or_npc( m_info, _( "You can't place items here!" ),
                                _( "<npcname> can't place items here!" ) );
         return;
