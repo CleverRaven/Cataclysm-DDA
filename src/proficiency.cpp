b5452500884f8bdb109d9537e91df049068ddd99
eltank <8000047+eltank@users.noreply.github.com>
2021-05-28 20:39:34 -0700

    Implement sum-of-squared-logs algorithm for combining prof mitigations
    Co-authored-by: John Bytheway <jbytheway@gmail.com>

--- src/proficiency.cpp
+++ src/proficiency.cpp
@@ -418,28 +418,28 @@ book_proficiency_bonuses &book_proficiency_bonuses::operator+=( const book_profi
 
 float book_proficiency_bonuses::fail_factor( const proficiency_id &id ) const
 {
-    float ret = 0.0f;
+    double sum = 0;
 
     for( const book_proficiency_bonus &bonus : bonuses ) {
         if( id != bonus.id ) {
             continue;
         }
-        ret += std::pow( bonus.fail_factor, 2 );
+        sum += std::pow( std::log( 1.0 - bonus.fail_factor ), 2 );
     }
 
-    return std::min( std::sqrt( ret ), 1.0f );
+    return static_cast<float>( 1.0 - std::exp( -std::sqrt( sum ) ) );
 }
 
 float book_proficiency_bonuses::time_factor( const proficiency_id &id ) const
 {
-    float ret = 0.0f;
+    double sum = 0;
 
     for( const book_proficiency_bonus &bonus : bonuses ) {
         if( id != bonus.id ) {
             continue;
         }
-        ret += std::pow( bonus.time_factor, 2 );
+        sum += std::pow( std::log( 1.0 - bonus.time_factor ), 2 );
     }
 
-    return std::min( std::sqrt( ret ), 1.0f );
+    return static_cast<float>( 1.0 - std::exp( -std::sqrt( sum ) ) );
 }
