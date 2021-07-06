5e3fe590d88b38f6ca170dcff3726d20f0c6448e
anothersimulacrum <anothersimulacrum@gmail.com>
2021-07-06 10:22:07 -0700

    Clean convert_to_portions.py up a bit (#49402)
    * Clean up convert_to_portions.py
    
    Replace the class with a dict, and remove all the now uncessary handling
    and parsing code.
    
    More cleanly handle invalid JSON errors and the file being incorrectly
    formatted for the script to parse.
    
    Condense failure conditions in our JSON parsing before extracting the
    portion data, and drop a level of indentation in the driver code.
    
    * Fix handling of encumbrance with only max_encumbr
    
    It's legal to specify max_encumbrance without specifying encumbrance in
    the old format. Handle that correctly.

--- tools/json_tools/convert_to_portions.py
+++ tools/json_tools/convert_to_portions.py
@@ -9,69 +9,28 @@ args.add_argument("dir", action="store", help="specify json directory")
 args_dict = vars(args.parse_args())
 
 
-class portion_data:
-    encumbrance = -1
-    max_encumbrance = -1
-    coverage = -1
-    covers = []
-    sided = False
-
-
-def portion_null(dat):
-    ret = dat.encumbrance == -1
-    ret = ret and dat.max_encumbrance == -1
-    ret = ret and dat.coverage == -1
-    ret = ret and len(dat.covers) == 0
-    ret = ret and not dat.sided
-    return ret
-
-
-def gen_entry(dat):
-    added = dict()
-
-    if dat.encumbrance != -1:
-        added["encumbrance"] = dat.encumbrance
-        if dat.max_encumbrance != -1:
-            added["encumbrance"] = [dat.encumbrance, dat.max_encumbrance]
-    if dat.coverage != -1:
-        added["coverage"] = dat.coverage
-    if len(dat.covers) != 0:
-        added["covers"] = dat.covers
-    if dat.sided:
-        added["sided"] = True
-
-    return added
-
-
 def portionize(jo):
-    dat = portion_data()
-    if "encumbrance" in jo:
-        dat.encumbrance = jo["encumbrance"]
-        del jo["encumbrance"]
-    if "max_encumbrance" in jo:
-        dat.max_encumbrance = jo["max_encumbrance"]
-        del jo["max_encumbrance"]
-    if "coverage" in jo:
-        dat.coverage = jo["coverage"]
-        del jo["coverage"]
-    if "covers" in jo:
-        dat.covers = jo["covers"]
-        del jo["covers"]
-    if "sided" in jo:
-        dat.sided = jo["sided"]
-        del jo["sided"]
+    dat = dict()
 
-    if portion_null(dat):
+    for member in ["encumbrance", "max_encumbrance",
+                   "coverage", "covers", "sided"]:
+        if member in jo:
+            dat[member] = jo.pop(member)
+
+    if not dat:
         return None
 
+    if "max_encumbrance" in dat:
+        if "encumbrance" not in dat:
+            dat["encumbrance"] = 0
+        dat["encumbrance"] = [dat["encumbrance"], dat.pop("max_encumbrance")]
+
     if "armor_portion_data" not in jo:
-        added = gen_entry(dat)
-        jo["armor_portion_data"] = [added]
+        jo["armor_portion_data"] = [dat]
     else:
-        added = gen_entry(dat)
-        added["//0"] = "Autogenned for item with non-portion and portion data"
-        added["//1"] = "Ensure that this works correctly!"
-        jo["armor_portion_data"].insert(0, added)
+        dat["//0"] = "Autogenned for item with non-portion and portion data"
+        dat["//1"] = "Ensure that this works correctly!"
+        jo["armor_portion_data"].insert(0, dat)
 
     return jo
 
@@ -82,11 +41,23 @@ def gen_new(path):
     # Uncomment below to find the file it's halting on
     #print(path)
     with open(path, "r") as json_file:
-        json_data = json.load(json_file)
+        try:
+            json_data = json.load(json_file)
+        except json.JSONDecodeError:
+            print(f"Json Decode Error at {path}")
+            print("Ensure that the file is a JSON file consisting of"
+                  " an array of objects!")
+            return None
+
         for jo in json_data:
-            if "type" not in jo:
-                continue
-            if (jo["type"] != "ARMOR" and
+            if type(jo) is not dict:
+                print(f"Incorrectly formatted JSON data at {path}")
+                print("Ensure that the file is a JSON file consisting"
+                      " of an array of objects!")
+                break
+
+            if ("type" not in jo or
+                jo["type"] != "ARMOR" and
                 jo["type"] != "TOOL_ARMOR" and
                "armor_data" not in jo):
                 continue
@@ -109,10 +80,12 @@ def gen_new(path):
 
 for root, directories, filenames in os.walk(args_dict["dir"]):
     for filename in filenames:
+        if not filename.endswith(".json"):
+            continue
+
         path = os.path.join(root, filename)
-        if path.endswith(".json"):
-            new = gen_new(path)
-            if new is not None:
-                with open(path, "w") as jf:
-                    json.dump(new, jf, ensure_ascii=False)
-                os.system(f"./tools/format/json_formatter.cgi {path}")
+        new = gen_new(path)
+        if new is not None:
+            with open(path, "w") as jf:
+                json.dump(new, jf, ensure_ascii=False)
+            os.system(f"./tools/format/json_formatter.cgi {path}")
