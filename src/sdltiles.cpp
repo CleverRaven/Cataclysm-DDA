aba46769b3b1f501408d3841012bad15bb99a840
KorGgenT <curtis.r.merrill@gmail.com>
2021-07-10 08:38:16 -0400

    use correct int for horde size tiles

--- src/sdltiles.cpp
+++ src/sdltiles.cpp
@@ -881,7 +881,7 @@ void cata_tiles::draw_om( const point &dest, const tripoint_abs_omt &center_abs_
                 if( showhordes && los && horde_size >= HORDE_VISIBILITY_SIZE ) {
                     // a little bit of hardcoded fallbacks for hordes
                     if( find_tile_with_season( id ) ) {
-                        draw_from_id_string( string_format( "overmap_horde_%d", HORDE_VISIBILITY_SIZE ),
+                        draw_from_id_string( string_format( "overmap_horde_%d", horde_size ),
                                              pos, 0, 0, lit_level::LIT, false );
                     } else {
                         switch( horde_size ) {
