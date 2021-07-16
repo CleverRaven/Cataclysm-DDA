5a3427bb9bc35896842e0675d22d9e7a8b9b157d
John Bytheway <52664+jbytheway@users.noreply.github.com>
2021-07-16 10:22:30 -0400

    Expand test cases for debug translations

--- tools/clang-tidy-plugin/test/translations-in-debug-messages.cpp
+++ tools/clang-tidy-plugin/test/translations-in-debug-messages.cpp
@@ -3,7 +3,6 @@
 #include "mock-translation.h"
 
 // dummy messages interface
-class Creature;
 class time_duration;
 class tripoint;
 
@@ -16,6 +15,13 @@ enum debug_filter : int {
 }
 
 struct game_message_params;
+
+class Creature
+{
+    public:
+        void add_msg_debug_if_player( debugmode::debug_filter, const std::string & ) const;
+        void add_msg_debug_if_npc( debugmode::debug_filter, const std::string & ) const;
+};
 
 void add_msg_debug( debugmode::debug_filter type, std::string msg );
 template<typename ...Args>
@@ -78,11 +84,35 @@ void add_msg_debug_if_player_sees( const Creature &target, debugmode::debug_filt
 
 std::string to_string( const time_duration &d, bool compact = false );
 
-static void f( const time_duration &duration )
+static void f( const time_duration &duration, Creature &c )
 {
     char *skill_level;
     add_msg_debug( debugmode::DF_ACT_BUTCHER, _( "Skill: %s" ), skill_level );
     // CHECK-MESSAGES: [[@LINE-1]]:47: warning: string arguments to debug message functions should not be translated, because this is an unnecessary performance cost. [cata-translations-in-debug-messages]
+
+    add_msg_debug( debugmode::DF_ACT_BUTCHER, _( "Skill" ) );
+    // CHECK-MESSAGES: [[@LINE-1]]:47: warning: string arguments to debug message functions should not be translated, because this is an unnecessary performance cost. [cata-translations-in-debug-messages]
+
+    add_msg_debug( debugmode::DF_ACT_BUTCHER, pgettext( "Skill", "foo" ) );
+    // CHECK-MESSAGES: [[@LINE-1]]:47: warning: string arguments to debug message functions should not be translated, because this is an unnecessary performance cost. [cata-translations-in-debug-messages]
+
+    add_msg_debug( debugmode::DF_ACT_BUTCHER, ngettext( "Skill", "Skills", 0 ) );
+    // CHECK-MESSAGES: [[@LINE-1]]:47: warning: string arguments to debug message functions should not be translated, because this is an unnecessary performance cost. [cata-translations-in-debug-messages]
+
+    add_msg_debug( debugmode::DF_ACT_BUTCHER, "%s", to_translation( "Skill" ) );
+    // CHECK-MESSAGES: [[@LINE-1]]:53: warning: string arguments to debug message functions should not be translated, because this is an unnecessary performance cost. [cata-translations-in-debug-messages]
+
+    add_msg_debug_if_player_sees( c, debugmode::DF_ACT_BUTCHER, _( "Skill: %s" ), skill_level );
+    // CHECK-MESSAGES: [[@LINE-1]]:65: warning: string arguments to debug message functions should not be translated, because this is an unnecessary performance cost. [cata-translations-in-debug-messages]
+
+    add_msg_debug_if_player_sees( c, debugmode::DF_ACT_BUTCHER, _( "Skill" ) );
+    // CHECK-MESSAGES: [[@LINE-1]]:65: warning: string arguments to debug message functions should not be translated, because this is an unnecessary performance cost. [cata-translations-in-debug-messages]
+
+    c.add_msg_debug_if_player( debugmode::DF_ACT_BUTCHER, _( "Skill" ) );
+    // CHECK-MESSAGES: [[@LINE-1]]:59: warning: string arguments to debug message functions should not be translated, because this is an unnecessary performance cost. [cata-translations-in-debug-messages]
+
+    c.add_msg_debug_if_npc( debugmode::DF_ACT_BUTCHER, _( "Skill" ) );
+    // CHECK-MESSAGES: [[@LINE-1]]:56: warning: string arguments to debug message functions should not be translated, because this is an unnecessary performance cost. [cata-translations-in-debug-messages]
 
     add_msg_debug( debugmode::DF_ACT_BUTCHER, "Duration %s", to_string( duration ) );
     // CHECK-MESSAGES: [[@LINE-1]]:62: warning: string arguments to debug message functions should not be translated, because this is an unnecessary performance cost.  This call to to_string might involve a translation; consider using to_string_writable instead. [cata-translations-in-debug-messages]
