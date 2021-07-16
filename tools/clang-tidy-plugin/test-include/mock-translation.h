148eb89444cd76268a876b28658629f28a019774
John Bytheway <52664+jbytheway@users.noreply.github.com>
2021-07-16 09:18:44 -0400

    Update translation-related clang-tidy checks
    Some of these checks were not working for the current definition of _
    (the translation macro).  Tweak that definition to make it easier to
    detect in a clang-tidy check and update the checks accordingly.

--- tools/clang-tidy-plugin/test-include/mock-translation.h
+++ tools/clang-tidy-plugin/test-include/mock-translation.h
@@ -15,11 +15,45 @@ string operator+( const string &, const string & );
 } // namespace std
 
 // check_clang_tidy uses -nostdinc++, so we add dummy translation interface here instead of including translations.h
-std::string _( const std::string & );
+namespace detail {
+
+template<typename T>
+struct local_translation_cache
+{
+    T operator()( T arg );
+};
+
+static inline local_translation_cache<const char *> get_local_translation_cache( const char * )
+{
+    return local_translation_cache<const char *>();
+}
+static inline local_translation_cache<std::string> get_local_translation_cache(
+    const std::string & )
+{
+    return local_translation_cache<std::string>();
+}
+
+} // namespace detail
+
+template<typename T>
+inline const T &translation_argument_identity( const T &t )
+{
+    return t;
+}
+
+#define _( msg ) \
+    ( ( []( const auto & arg ) { \
+        static auto cache = detail::get_local_translation_cache( arg ); \
+        return cache( arg ); \
+    } )( translation_argument_identity( msg ) ) )
+
 std::string gettext( const std::string & );
 std::string pgettext( const std::string &, const std::string & );
 std::string ngettext( const std::string &, const std::string &, int );
 std::string npgettext( const std::string &, const std::string &, const std::string &, int );
+
+#define translate_marker( s ) ( s )
+#define translate_marker_context( c, s ) ( s )
 
 class translation
 {
