4a51846f074a357ebd63f102b32025631df3f3b4
eltank <8000047+eltank@users.noreply.github.com>
2021-07-18 02:01:27 -0700

    Clean up omt path selection code and store path only in vehicle object
    The path selection code had some weird unnecessary recalculation going
    on. It also stored duplicate information in the player object and the
    vehicle object.

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -908,9 +908,8 @@ autodrive_result vehicle::do_autodrive( Character &driver )
     // TODO: fix point types
     tripoint_abs_omt veh_omt_pos( ms_to_omt_copy( here.getabs( vehpos ) ) );
     // we're at or close to the waypoint, pop it out and look for the next one.
-    if( ( is_autodriving && !driver.omt_path.empty() && !omt_path.empty() ) &&
+    if( ( is_autodriving && !omt_path.empty() ) &&
         veh_omt_pos == omt_path.back() ) {
-        driver.omt_path.pop_back();
         omt_path.pop_back();
     }
     if( omt_path.empty() ) {
