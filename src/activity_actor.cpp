0ca16b281424fae84d38f9ab2f8ddb6272f96fc9
Welp <jtf515@gmail.com>
2021-07-18 18:10:51 -0700

    Fix

--- src/activity_actor.cpp
+++ src/activity_actor.cpp
@@ -3543,6 +3543,7 @@ void disassemble_activity_actor::start( player_activity &act, Character &who )
         target = who.create_in_progress_disassembly( act.targets.back() );
     } else {
         target = act.targets.back();
+        act.position = target->charges;
     }
     act.targets.pop_back();
 
