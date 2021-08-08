551447228ee1d0759e5b3978630a32a1a787c5c7
Binrui Dong <brett.browning.dong@gmail.com>
2021-08-08 10:54:30 +0800

    Update floating_point_arithmetic test

--- tests/float_point_test.cpp
+++ tests/float_point_test.cpp
@@ -5,14 +5,16 @@
 
 TEST_CASE( "floating_point_arithmetic" )
 {
-    volatile int a = 45000;
+    volatile int a = 45000000;
     volatile float b = 1.01f;
-    float c = a * b;
+    volatile unsigned long long c = 1000ULL;
+    auto d = a * b / c;
+    INFO( "type of \"a*b/c\" is " << typeid( d ).name() );
     std::ostringstream oss;
     oss.precision( 20 );
     oss << "b = " << std::fixed << b << std::endl;
-    oss << "c = " << std::fixed << c << std::endl;
+    oss << "d = " << std::fixed << d << std::endl;
     INFO( oss.str() );
-    int d = static_cast<int>( c );
-    CHECK( d == 45450 );
+    int e = static_cast<int>( d );
+    CHECK( e == 45450 );
 }
