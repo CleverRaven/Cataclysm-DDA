6b72612d369536bbb9df1f658edd1cc348316d05
Binrui Dong <brett.browning.dong@gmail.com>
2021-08-10 20:37:58 +0800

    Deliberately fail MinGW test

--- tests/float_point_test.cpp
+++ tests/float_point_test.cpp
@@ -7,14 +7,18 @@ TEST_CASE( "floating_point_arithmetic" )
 {
     volatile int a = 45000000;
     volatile float b = 1.01f;
-    volatile unsigned long long c = 1000ULL;
-    auto d = a * b / c;
-    INFO( "type of \"a*b/c\" is " << typeid( d ).name() );
+    volatile long long c = 1000ULL;
+    INFO( "type of \"a*b/c\" is " << typeid( a * b / c ).name() );
+    float d = a * b / c;
     std::ostringstream oss;
     oss.precision( 20 );
     oss << "b = " << std::fixed << b << std::endl;
     oss << "d = " << std::fixed << d << std::endl;
     INFO( oss.str() );
     int e = static_cast<int>( d );
+#if defined(_WIN32)
+    CHECK( e == 42 );
+#else
     CHECK( e == 45450 );
+#endif
 }
