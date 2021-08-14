0ef911946ca7645a5c0f707cc5ef41adbb09798f
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2021-08-15 01:11:21 +0300

    Backporting #49904 Fix weapons dropping without message on reload

--- src/game.cpp
+++ src/game.cpp
@@ -9099,7 +9099,7 @@ void game::reload( item_location &loc, bool prompt, bool empty )
     }
 
     bool use_loc = true;
-    if( !it->has_flag( flag_ALLOWS_REMOTE_USE ) ) {
+    if( !u.has_item( *it ) && !it->has_flag( flag_ALLOWS_REMOTE_USE ) ) {
         it = loc.obtain( u ).get_item();
         if( !it ) {
             add_msg( _( "Never mind." ) );
