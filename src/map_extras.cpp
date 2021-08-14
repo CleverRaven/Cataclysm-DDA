c2ac25e23293b60c33b7bc76cc2b0413ac128695
ZhilkinSerg <ZhilkinSerg@users.noreply.github.com>
2021-08-15 01:11:35 +0300

    Backporting #48794 Ability to configure user-defined map extra symbols

--- src/map_extras.cpp
+++ src/map_extras.cpp
@@ -3210,24 +3210,30 @@ void apply_function( const string_id<map_extra> &id, map &m, const tripoint &abs
     // TODO: fix point types
     overmap_buffer.add_extra( tripoint_abs_omt( sm_to_omt_copy( abs_sub ) ), id );
 
-    auto_notes::auto_note_settings &autoNoteSettings = get_auto_notes_settings();
+    auto_notes::auto_note_settings &auto_note_settings = get_auto_notes_settings();
 
     // The player has discovered a map extra of this type.
-    autoNoteSettings.set_discovered( id );
+    auto_note_settings.set_discovered( id );
 
     if( get_option<bool>( "AUTO_NOTES" ) && get_option<bool>( "AUTO_NOTES_MAP_EXTRAS" ) ) {
 
         // Only place note if the user has not disabled it via the auto note manager
-        if( autoNoteSettings.has_auto_note_enabled( id ) ) {
-            const std::string mx_note =
-                string_format( "%s:%s;<color_yellow>%s</color>: <color_white>%s</color>",
-                               extra.get_symbol(),
-                               get_note_string_from_color( extra.color ),
-                               extra.name(),
-                               extra.description() );
-            // TODO: fix point types
-            overmap_buffer.add_note( tripoint_abs_omt( sm_to_omt_copy( abs_sub ) ), mx_note );
+        if( !auto_note_settings.has_auto_note_enabled( id ) ) {
+            return;
         }
+
+        const cata::optional<auto_notes::custom_symbol> &symbol =
+            auto_note_settings.get_custom_symbol( extra.id );
+        const std::string note_symbol = symbol ? ( *symbol ).get_symbol_string() : extra.get_symbol();
+        const nc_color note_color = symbol ? ( *symbol ).get_color() : extra.color;
+        const std::string mx_note =
+            string_format( "%s:%s;<color_yellow>%s</color>: <color_white>%s</color>",
+                           note_symbol,
+                           get_note_string_from_color( note_color ),
+                           extra.name(),
+                           extra.description() );
+        // TODO: fix point types
+        overmap_buffer.add_note( tripoint_abs_omt( sm_to_omt_copy( abs_sub ) ), mx_note );
     }
 }
 
