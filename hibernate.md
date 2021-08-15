61c4b43cc1dc1dd890bb5174c8a823178516d6c4
Saicchi <Saicchi@users.noreply.github.com>
2021-08-15 14:47:25 -0300

    replace double-quotes with quotes when possible

---
+++ hibernate.md
@@ -0,0 +1,13 @@
+# Describe the Bug
+
+# Title
+
+# Steps to Reproduce
+
+# Expected behavior
+
+# Screenshots
+
+# Versions and configuration
+
+# Additional context
--- tools/tileset_test.py
+++ tools/tileset_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""
+'''
 Creates a map folder with one of every item, furniture, terrain and trap.
 Everything is spawned starting from submap 0,0.
 Run this script with -h for full usage information.
@@ -10,36 +10,36 @@ Examples:
     %(prog)s data/mods/Magiclysm .
     %(prog)s data/mods/Magiclysm data/mods/Aftershock .
     %(prog)s . /tmp
-"""
+'''
 
 import argparse
 import pathlib
 import json
 import math
 
-"""
+'''
 A map file is divided into 4 sections of 12x12 tiles each
-"""
+'''
 
 JSON_INFO = {
-    "items": {
-        "allowed_types": ["GENERIC", "AMMO", "MAGAZINE",
-                          "MAGAZINE", "TOOL", "PET_ARMOR", "BOOK",
-                          "BIONIC_ITEM", "COMESTIBLE", "GUN", "GUNMOD",
-                          "BATTERY"],
-        "tiles_per": 1
+    'items': {
+        'allowed_types': ['GENERIC', 'AMMO', 'MAGAZINE',
+                          'MAGAZINE', 'TOOL', 'PET_ARMOR', 'BOOK',
+                          'BIONIC_ITEM', 'COMESTIBLE', 'GUN', 'GUNMOD',
+                          'BATTERY'],
+        'tiles_per': 1
     },
-    "furniture": {
-        "allowed_types": ["furniture"],
-        "tiles_per": 9
+    'furniture': {
+        'allowed_types': ['furniture'],
+        'tiles_per': 9
     },
-    "terrain": {
-        "allowed_types": ["terrain"],
-        "tiles_per": 9
+    'terrain': {
+        'allowed_types': ['terrain'],
+        'tiles_per': 9
     },
-    "trap": {
-        "allowed_types": ["trap"],
-        "tiles_per": 9
+    'trap': {
+        'allowed_types': ['trap'],
+        'tiles_per': 9
     }
 }
 
@@ -58,7 +58,7 @@ def parse_json(path: list) -> dict:
         parsed_json[_type] = []
 
     for data_folder in data_folders:
-        for file in data_folder.glob("**/*.json"):
+        for file in data_folder.glob('**/*.json'):
             json_file = None
             with open(file, 'r') as f:
                 json_file = json.load(f)
@@ -68,13 +68,13 @@ def parse_json(path: list) -> dict:
 
             for _type, info in JSON_INFO.items():
                 for entry in json_file:
-                    if ("type" not in entry or
+                    if ('type' not in entry or
                         # abstract item definitions
-                        "id" not in entry or
-                            entry["type"] not in info["allowed_types"]):
+                        'id' not in entry or
+                            entry['type'] not in info['allowed_types']):
                         continue
 
-                    parsed_json[_type].append(entry["id"])
+                    parsed_json[_type].append(entry['id'])
 
     return parsed_json
 
@@ -84,7 +84,7 @@ class QuarterMap:
     def __init__(self, row, column):
         self.index = 0
         self.version = 33
-        self.terrain = ["t_grass"] * 144
+        self.terrain = ['t_grass'] * 144
         self.furniture = []
         self.traps = []
         self.items = []
@@ -92,11 +92,11 @@ class QuarterMap:
 
     def add_item(self, itype: str) -> None:
         self.items.append({
-            "row": self.index % 12,
-            "column": self.index // 12,
-            "typeid": itype
+            'row': self.index % 12,
+            'column': self.index // 12,
+            'typeid': itype
         })
-        self.terrain[self.index] = "t_concrete"
+        self.terrain[self.index] = 't_concrete'
         self.index += 1
 
     def add_furniture(self, ftype: str) -> None:
@@ -105,12 +105,12 @@ class QuarterMap:
         furn_index += index_values[self.index % 4]
 
         self.furniture.append({
-            "row": furn_index % 12,
-            "column": furn_index // 12,
-            "typeid": ftype
+            'row': furn_index % 12,
+            'column': furn_index // 12,
+            'typeid': ftype
         })
 
-        self.terrain[furn_index] = "t_concrete"
+        self.terrain[furn_index] = 't_concrete'
         self.index += 1
 
     def add_terrain(self, ttype: str) -> None:
@@ -129,12 +129,12 @@ class QuarterMap:
         trap_index += index_values[self.index % 4]
 
         self.traps.append({
-            "row": trap_index % 12,
-            "column": trap_index // 12,
-            "typeid": trtype
+            'row': trap_index % 12,
+            'column': trap_index // 12,
+            'typeid': trtype
         })
 
-        self.terrain[trap_index] = "t_concrete"
+        self.terrain[trap_index] = 't_concrete'
         self.index += 1
 
     def as_string(self, ident=0) -> str:
@@ -146,15 +146,15 @@ class QuarterMap:
         retstr += identstr + f'\t\t{self.coordinate[0]},\n'
         retstr += identstr + f'\t\t{self.coordinate[1]},\n'
         retstr += identstr + f'\t\t{self.coordinate[2]}\n'
-        retstr += identstr + "\t],\n"
+        retstr += identstr + '\t],\n'
 
         retstr += identstr * ident + '\t"terrain": [\n'
         for key in range(144):
             if key < len(self.terrain):
                 entry = self.terrain[key]
             else:
-                entry = "t_grass"
-            retstr += "\t\t"
+                entry = 't_grass'
+            retstr += '\t\t'
             if type(entry) is str:
                 retstr += identstr + f'"{entry}"'
             else:
@@ -166,7 +166,7 @@ class QuarterMap:
 
         retstr += identstr * ident + '\t"items": [\n'
         for key, entry in enumerate(self.items):
-            retstr += "\t\t"
+            retstr += '\t\t'
             retstr += identstr + \
                 f'{entry["row"]}, {entry["column"]}, ' \
                 f'[{{ "typeid": "{entry["typeid"]}" }}]'
@@ -186,7 +186,7 @@ class QuarterMap:
             retstr += '\n'
         retstr += identstr + '\t],\n'
 
-        retstr += identstr * ident + '\t"traps": [\n'
+        retstr += identstr * ident + '\t"traps" [\n'
         for key, entry in enumerate(self.traps):
             retstr += identstr
             retstr += "\t\t[ " + \
@@ -254,7 +254,7 @@ class FullMap:
             i += 1
 
     def __str__(self) -> str:
-        retstr = "[\n"
+        retstr = '[\n'
         for i in range(4):
             qret = self._map[i].as_string(1)
             if i != 3:
@@ -266,17 +266,17 @@ class FullMap:
 
 class PathNotExist(Exception):
     def __init__(self, path: pathlib.Path):
-        super().__init__(f"path does not exist: {path}")
+        super().__init__(f'path does not exist: {path}')
 
 
 class MapsFolderExists(Exception):
     def __init__(self, path: pathlib.Path):
-        super().__init__(f"maps folder already exists: {path}")
+        super().__init__(f'maps folder already exists: {path}')
 
 
 class HandlerUnhandledType(Exception):
     def __init__(self, key):
-        super().__init__(f"key not handled by Handler: {key}")
+        super().__init__(f'key not handled by Handler: {key}')
 
 
 class Handler:
@@ -285,8 +285,8 @@ class Handler:
         self.maps_folder = pathlib.Path(args.path)
         if not self.maps_folder.exists():
             raise PathNotExist(self.maps_folder)
-        self.maps_folder = self.maps_folder.joinpath("maps")
-        if self.maps_folder.exists() and not args.force:
+        self.maps_folder = self.maps_folder.joinpath('maps')
+        if self.maps_folder.exists() and not (args.dry_run or args.force):
             raise MapsFolderExists(self.maps_folder)
 
         self._maps = dict()
@@ -297,9 +297,9 @@ class Handler:
 
             # 576 = 4 * 12 * 12 = Tiles in a .map file
             total_squares = math.ceil(
-                JSON_INFO[key]["tiles_per"] * len(value) / 576)
+                JSON_INFO[key]['tiles_per'] * len(value) / 576)
             square_length = math.ceil(math.sqrt(total_squares))
-            amount = 576 // JSON_INFO[key]["tiles_per"]
+            amount = 576 // JSON_INFO[key]['tiles_per']
 
             counter = 0
             for i in range(total_squares):
@@ -307,16 +307,16 @@ class Handler:
                 column = i // square_length
                 fmap = FullMap(2 * row, 2 * column)
 
-                if key == "items":
+                if key == 'items':
                     fmap.fill_items(value[counter:counter + amount])
-                elif key == "furniture":
+                elif key == 'furniture':
                     fmap.fill_furniture(value[counter:counter + amount])
-                elif key == "terrain":
+                elif key == 'terrain':
                     fmap.fill_terrain(value[counter:counter + amount])
-                elif key == "trap":
+                elif key == 'trap':
                     fmap.fill_traps(value[counter:counter + amount])
 
-                self._maps[f"{row}.{column}"] = fmap
+                self._maps[f'{row}.{column}'] = fmap
                 counter += amount  # 4 x ( 12 x 12 )
 
             square_offset += square_length
@@ -326,39 +326,39 @@ class Handler:
             row = int(_map.split('.')[0])
             column = int(_map.split('.')[1])
             folder = pathlib.Path.joinpath(self.maps_folder,
-                                           f"{row//32}.{column//32}.0")
+                                           f'{row//32}.{column//32}.0')
             if not dry_run:
                 folder.mkdir(parents=True, exist_ok=True)
 
-            file_path = folder.joinpath(f"{row}.{column}.0.map")
+            file_path = folder.joinpath(f'{row}.{column}.0.map')
             if not dry_run:
                 with open(file_path, 'w') as f:
                     f.write(self._maps[_map].__str__())
                     f.write('\n')
             else:
                 print(file_path)
-                print(self._maps[_map], end="\n\n")
+                print(self._maps[_map], end='\n\n')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawTextHelpFormatter,
     )
 
-    parser.add_argument("data", nargs="+", type=str,
-                        help="json files location")
+    parser.add_argument('data', nargs='+', type=str,
+                        help='json files location')
 
     parser.add_argument(
-        "path", type=str, help="folder where the maps folder will be created"
+        'path', type=str, help='folder where the maps folder will be created'
     )
 
-    parser.add_argument("-f", "--force", action='store_true',
-                        help="overwrite the maps folder if it already exists")
+    parser.add_argument('-f', '--force', action='store_true',
+                        help='overwrite the maps folder if it already exists')
 
-    parser.add_argument("-n", "--dry_run",
+    parser.add_argument('-n', '--dry_run',
                         action='store_true',
-                        help="print to terminal instead of creating files")
+                        help='print to terminal instead of creating files')
 
     args = parser.parse_args()
     handler = Handler(args.path, parse_json(args.data))
