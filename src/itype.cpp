7b032ddd733ea1142367e467b176b27f6d1be5a2
U-N\N <zczczc1680@gmail.com>
2021-08-22 19:33:16 +0200

    Astyle

--- src/itype.cpp
+++ src/itype.cpp
@@ -116,11 +116,10 @@ cata::optional<int> itype::invoke( Character &p, item &it, const tripoint &pos )
     if( !has_use() ) {
         return 0;
     }
-    if (use_methods.find("transform") != use_methods.end()) {
-        return  invoke(p, it, pos, "transform");
-    }
-    else {
-        return invoke(p, it, pos, use_methods.begin()->first);
+    if( use_methods.find( "transform" ) != use_methods.end() ) {
+        return  invoke( p, it, pos, "transform" );
+    } else {
+        return invoke( p, it, pos, use_methods.begin()->first );
     }
 }
 
