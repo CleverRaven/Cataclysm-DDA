ba0a36f2de79961b32c07cf9b4717c439d913beb
Kevin Granade <kevin.granade@gmail.com>
2021-08-27 11:46:57 -0700

    Apply suggestions from code review

--- src/monster.cpp
+++ src/monster.cpp
@@ -2634,8 +2634,7 @@ void monster::process_effects()
     //If this monster has the ability to heal in combat, do it now.
     int regeneration_amount = type->regenerates;
     //Apply effect-triggered regeneartion modifers
-    for( const auto &regeneration_modifier :
-         type->regeneration_modifiers ) {
+    for( const auto &regeneration_modifier : type->regeneration_modifiers ) {
         if( has_effect( regeneration_modifier.first ) ) {
             regeneration_amount += regeneration_modifier.second;
         }
