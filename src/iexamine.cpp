93029e1366fc394c6d04492fd6838ce67142c68f
eltank <8000047+eltank@users.noreply.github.com>
2021-08-28 00:57:23 -0700

    Exchange cash for e-cash at ATMs (#51090)

--- src/iexamine.cpp
+++ src/iexamine.cpp
@@ -110,6 +110,8 @@ static const activity_id ACT_FORAGE( "ACT_FORAGE" );
 static const activity_id ACT_OPERATION( "ACT_OPERATION" );
 static const activity_id ACT_PLANT_SEED( "ACT_PLANT_SEED" );
 
+static const ammotype ammo_money( "money" );
+
 static const efftype_id effect_antibiotic( "antibiotic" );
 static const efftype_id effect_bite( "bite" );
 static const efftype_id effect_bleed( "bleed" );
@@ -492,7 +494,7 @@ class atm_menu
     public:
         // menu choices
         enum options : int {
-            cancel, purchase_card, deposit_money, withdraw_money, transfer_all_money
+            cancel, purchase_card, deposit_money, withdraw_money, exchange_cash, transfer_all_money
         };
 
         atm_menu()                           = delete;
@@ -516,6 +518,9 @@ class atm_menu
                         break;
                     case withdraw_money:
                         result = do_withdraw_money();
+                        break;
+                    case exchange_cash:
+                        result = do_exchange_cash();
                         break;
                     case transfer_all_money:
                         result = do_transfer_all_money();
@@ -582,6 +587,10 @@ class atm_menu
             } else {
                 add_info( deposit_money,
                           _( "You need a charged cash card before you can deposit money!" ) );
+            }
+
+            if( card_count >= 1 && you.has_item_with_flag( flag_OLD_CURRENCY ) ) {
+                add_choice( exchange_cash, _( "Exchange Cash for eCash (1%% fee)" ) );
             }
 
             if( card_count >= 2 && charge_count ) {
@@ -682,7 +691,6 @@ class atm_menu
 
             int inserted = 0;
             int remaining = amount;
-            ammotype money( "money" );
 
             std::sort( cash_cards_on_hand.begin(), cash_cards_on_hand.end(), []( item * one, item * two ) {
                 int balance_one = one->ammo_remaining();
@@ -694,7 +702,7 @@ class atm_menu
                 if( inserted == amount ) {
                     break;
                 }
-                int max_cap = cc->ammo_capacity( money ) - cc->ammo_remaining();
+                int max_cap = cc->ammo_capacity( ammo_money ) - cc->ammo_remaining();
                 int to_insert = std::min( max_cap, remaining );
                 // insert whatever there's room for + the old balance.
                 cc->ammo_set( cc->ammo_default(), to_insert + cc->ammo_remaining() );
@@ -714,6 +722,59 @@ class atm_menu
             return true;
         }
 
+        //!Deposit pre-cataclysm currency and receive equivalent amount minus fees on a card.
+        bool do_exchange_cash() {
+            item *dst;
+            if( you.activity.id() == ACT_ATM ) {
+                dst = you.activity.targets.front().get_item();
+                you.activity.set_to_null();
+                if( dst->is_null() || dst->typeId() != itype_cash_card ) {
+                    debugmsg( "do_exchange_cash lost the destination card" );
+                    return false;
+                }
+            } else {
+                const std::vector<item *> cash_cards = you.items_with( []( const item & i ) {
+                    return i.typeId() == itype_cash_card;
+                } );
+                if( cash_cards.empty() ) {
+                    popup( _( "You do not have a cash card." ) );
+                    return false;
+                }
+                dst = *std::max_element( cash_cards.begin(), cash_cards.end(), []( const item * a,
+                const item * b ) {
+                    return a->ammo_remaining() < b->ammo_remaining();
+                } );
+                if( !query_yn( _( "Exchange all paper bills and coins in inventory?" ) ) ) {
+                    return false;
+                }
+            }
+
+            item *cash_item = nullptr;
+            you.visit_items( [&]( item * e, const item * ) {
+                if( e->type->has_flag( flag_OLD_CURRENCY ) ) {
+                    cash_item = e;
+                    return VisitResponse::ABORT;
+                }
+                return VisitResponse::NEXT;
+            } );
+            if( !cash_item ) {
+                return false;
+            }
+            // Feeding a bill into the machine takes at least one turn
+            you.moves -= std::max( 100, you.moves );
+            int value = units::to_cent( cash_item->type->price );
+            value *= 0.99;  // subtract fee
+            if( value > dst->ammo_capacity( ammo_money ) - dst->ammo_remaining() ) {
+                popup( _( "Destination card is full." ) );
+                return false;
+            }
+            item_location( you, cash_item ).remove_item();
+            dst->ammo_set( dst->ammo_default(), dst->ammo_remaining() + value );
+            you.assign_activity( ACT_ATM, 0, exchange_cash );
+            you.activity.targets.emplace_back( you, dst );
+            return true;
+        }
+
         //!Move the money from all the cash cards in inventory to a single card.
         bool do_transfer_all_money() {
             item *dst;
@@ -721,9 +782,10 @@ class atm_menu
                 return i.typeId() == itype_cash_card;
             } );
             if( you.activity.id() == ACT_ATM ) {
-                you.activity.set_to_null(); // stop for now, if required, it will be created again.
                 dst = you.activity.targets.front().get_item();
+                you.activity.set_to_null(); // stop for now, if required, it will be created again.
                 if( dst->is_null() || dst->typeId() != itype_cash_card ) {
+                    debugmsg( "do_transfer_all_money lost the destination card" );
                     return false;
                 }
             } else {
@@ -747,6 +809,10 @@ class atm_menu
                     break;
                 }
                 // should we check for max capacity here?
+                if( i->ammo_remaining() > dst->ammo_capacity( ammo_money ) - dst->ammo_remaining() ) {
+                    popup( _( "Destination card is full." ) );
+                    return false;
+                }
                 dst->ammo_set( dst->ammo_default(), i->ammo_remaining() + dst->ammo_remaining() );
                 i->ammo_set( i->ammo_default(), 0 );
                 you.moves -= 10;
