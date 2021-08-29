c37b8c2a8a2b961c62e40fdcb9c05a2be459530b
jim-huynh <jim.huynh@outlook.com>
2021-08-28 19:08:20 -0700

    Make tunneling viable again.  Make downward staircases less likely to collapse stranding the player. (#51114)
    * Switch check to check the surrounding points rather than the current point multiple times.  Adjust wall support value to be more supportive for higher  up floors.  Make stairs up support roofs because the connected downstairs is the roof being supported.
    Co-authored-by: Anton Burmistrov <Night_Pryanik@mail.ru>

--- src/map.cpp
+++ src/map.cpp
@@ -2909,10 +2909,10 @@ int map::collapse_check( const tripoint &p )
     if( p.z > -OVERMAP_DEPTH ) {
         const tripoint &pbelow = tripoint( p.xy(), p.z - 1 );
         for( const tripoint &tbelow : points_in_radius( pbelow, 1 ) ) {
-            if( has_flag( "SUPPORTS_ROOF", pbelow ) ) {
+            if( has_flag( "SUPPORTS_ROOF", tbelow ) ) {
                 num_supports += 1;
-                if( has_flag( "WALL", pbelow ) ) {
-                    num_supports = 2;
+                if( has_flag( "WALL", tbelow ) ) {
+                    num_supports += 2;
                 }
                 if( tbelow == pbelow ) {
                     num_supports += 2;
@@ -2983,8 +2983,12 @@ void map::collapse_at( const tripoint &p, const bool silent, const bool was_supp
             // this tile used to support a roof, now it doesn't, which means there is only
             // open air above us
             if( zlevels ) {
-                ter_set( tz, t_open_air );
-                furn_set( tz, f_null );
+                // ensure that the layer below this one is not a wall, otherwise you have a ledge dropping onto
+                // a wall which doesn't make sense.
+                if( !has_flag( "WALL", t ) ) {
+                    ter_set( tz, t_open_air );
+                    furn_set( tz, f_null );
+                }
             }
         }
     }
