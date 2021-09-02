a16edcd91c6ab11a39ac5e445bf729959917ebb9
actual-nh <actual-nh@users.noreply.github.com>
2021-09-01 21:18:06 -0400

    Clamp mod_size_bonus to within enum limits
    This should prevent any input to mod_size_bonus from causing it to
    go outside of the enum limits. I wound up doing this in a slightly odd
    fashion, since Creature::get_size() is virtual only (the Creature
    structure does not itself include a size, AFAICT); instead of getting
    a base size and preventing size_bonus from pushing it beyond tiny/huge,
    I limited the adjustment to size_bonus so that it wouldn't push the
    current result of get_size() beyond the limits.

--- src/creature.cpp
+++ src/creature.cpp
@@ -2342,6 +2342,7 @@ void Creature::mod_cut_bonus( int ncut )
 }
 void Creature::mod_size_bonus( int nsize )
 {
+    nsize = clamp( nsize, creature_size::huge - get_size(), creature_size::tiny - get_size() );
     size_bonus += nsize;
 }
 
