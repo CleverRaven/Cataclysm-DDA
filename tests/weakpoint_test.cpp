fd2a65d95a029c9b91a6ee823b499e1c4ff5ea7f
R. Stephens Summerlin <st.summerlin@gmail.com>
2021-09-06 19:30:55 -0400

    Using weakpoint hit access to get more specific and reliable

--- tests/weakpoint_test.cpp
+++ tests/weakpoint_test.cpp
@@ -7,28 +7,91 @@
 #include "point.h"
 #include "type_id.h"
 
-static void damage_monster( const std::string &target_type, const damage_instance &dam, float ave )
-{
-    CAPTURE( target_type );
-    int total = 0;
-    for( int i = 0; i < 100; i++ ) {
-        monster target{ mtype_id( target_type ), tripoint_zero };
-        target.deal_damage( NULL, bodypart_id( "torso" ), dam );
-        int damage_dealt = target.get_hp_max() - target.get_hp();
-        total += damage_dealt;
-        CAPTURE( damage_dealt );
+struct weakpoint_report_item {
+    std::string weakpoint;
+    int totaldamage;
+    int hits;
+    weakpoint_report_item( std::string wp, int dam ) : weakpoint{ wp }, totaldamage{ dam }, hits{ 1 } { }
+    weakpoint_report_item() : weakpoint_report_item( "", 0 ) { }
+};
+
+struct weakpoint_report {
+    int hits;
+    std::map<std::string, weakpoint_report_item> items;
+
+    void Accumulate( dealt_damage_instance dd ) {
+        if( items.find( dd.wp_hit ) == items.end() ) {
+            items[dd.wp_hit] = weakpoint_report_item( dd.wp_hit, dd.total_damage() );
+        } else {
+            items[dd.wp_hit].hits += 1;
+            items[dd.wp_hit].totaldamage += dd.total_damage();
+        }
+        hits += 1;
     }
-    CHECK( ( float )total / 100.0f == Approx( ave ).epsilon( 0.15f ) );
+
+    float PercHits( std::string wp ) {
+        if( items.find( wp ) == items.end() || hits == 0 ) {
+            return 0.0f;
+        } else {
+            return ( float )items[wp].hits / ( float )hits;
+        }
+    }
+
+    float AveDam( std::string wp ) {
+        if( items.find( wp ) == items.end() ) {
+            return 0.0f;
+        } else {
+            return ( float )items[wp].totaldamage / ( float )items[wp].hits;
+        }
+    }
+
+    void Reset() {
+        hits = 0;
+        items.clear();
+    }
+};
+
+static weakpoint_report damage_monster( const std::string &target_type, const damage_instance &dam,
+                                        int attacks )
+{
+    weakpoint_report ret{};
+    for( int i = 0; i < attacks; i++ ) {
+        monster target{ mtype_id( target_type ), tripoint_zero };
+        ret.Accumulate( target.deal_damage( NULL, bodypart_id( "torso" ), dam ) );
+    }
+
+    return ret;
 }
 
 TEST_CASE( "monster_weakpoint", "[monster]" )
 {
-    // Debug Monster should hit weakpoint 25% of time, and do 75 damage up from 0 when not hitting weakpoint, so average of 18.75
-    damage_monster( "debug_mon", damage_instance( damage_type::BULLET, 100, 0.0f ), 18.75f );
-    // 25 arm pen adds to average
-    damage_monster( "debug_mon", damage_instance( damage_type::BULLET, 100, 25.0f ), 18.75f + 25.0f );
-    // 50 arm pen only helps non-weakpoint hits, weakpoint saturates at 25 AP
-    damage_monster( "debug_mon", damage_instance( damage_type::BULLET, 100, 50.0f ), 62.5f );
+    // Debug Monster has two weakpoints at 25% each, one leaves 0 armor the other 25 bullet armor, down from 100 bullet armor
+    weakpoint_report wr1 = damage_monster( "debug_mon", damage_instance( damage_type::BULLET, 100.0f,
+                                           0.0f ), 1000 );
+    CHECK( wr1.PercHits( "the knee" ) == Approx( 0.25f ).epsilon( 0.15f ) );
+    CHECK( wr1.AveDam( "the knee" ) == Approx( 75.0f ).epsilon( 0.15f ) ); // 25 armor
+    CHECK( wr1.PercHits( "the bugs" ) == Approx( 0.25f ).epsilon( 0.15f ) );
+    CHECK( wr1.AveDam( "the bugs" ) == Approx( 100.0f ).epsilon( 0.15f ) ); // No armor
+    CHECK( wr1.PercHits( "" ) == Approx( 0.50f ).epsilon( 0.15f ) );
+    CHECK( wr1.AveDam( "" ) == Approx( 0.0f ).epsilon( 0.15f ) ); // Full 100 armor
+
+    // With 25 ap, adjust no weakpoint and the knee damage
+    weakpoint_report wr2 = damage_monster( "debug_mon", damage_instance( damage_type::BULLET, 100.0f,
+                                           25.0f ), 1000 );
+    CHECK( wr2.PercHits( "the knee" ) == Approx( 0.25f ).epsilon( 0.15f ) );
+    CHECK( wr2.AveDam( "the knee" ) == Approx( 100.0f ).epsilon( 0.15f ) ); // 25 armor with 25 ap
+    CHECK( wr2.PercHits( "the bugs" ) == Approx( 0.25f ).epsilon( 0.15f ) );
+    CHECK( wr2.AveDam( "the bugs" ) == Approx( 100.0f ).epsilon( 0.15f ) ); // No armor with 25 ap
+    CHECK( wr2.PercHits( "" ) == Approx( 0.50f ).epsilon( 0.15f ) );
+    CHECK( wr2.AveDam( "" ) == Approx( 25.0f ).epsilon( 0.15f ) ); // Full 100 armor with 25 ap
+
     // No cut armor
-    damage_monster( "debug_mon", damage_instance( damage_type::CUT, 100, 0.0f ), 100.0f );
+    weakpoint_report wr3 = damage_monster( "debug_mon", damage_instance( damage_type::CUT, 100.0f,
+                                           0.0f ), 1000 );
+    CHECK( wr3.PercHits( "the knee" ) == Approx( 0.25f ).epsilon( 0.15f ) );
+    CHECK( wr3.AveDam( "the knee" ) == Approx( 100.0f ).epsilon( 0.15f ) );
+    CHECK( wr3.PercHits( "the bugs" ) == Approx( 0.25f ).epsilon( 0.15f ) );
+    CHECK( wr3.AveDam( "the bugs" ) == Approx( 100.0f ).epsilon( 0.15f ) );
+    CHECK( wr3.PercHits( "" ) == Approx( 0.50f ).epsilon( 0.15f ) );
+    CHECK( wr3.AveDam( "" ) == Approx( 100.0f ).epsilon( 0.15f ) );
 }
