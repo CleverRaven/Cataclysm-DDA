f11ce9b422a2d8db23657b14e8c79ed4e8eb9e45
Brambor <tondadrd@seznam.cz>
2021-09-16 21:45:36 +0200

    use string_format instead of custom made n_digits
    Co-authored-by: John Bytheway <52664+jbytheway@users.noreply.github.com>

--- src/game_inventory.cpp
+++ src/game_inventory.cpp
@@ -78,47 +78,6 @@ using item_location_filter = std::function<bool ( const item_location & )>;
 namespace
 {
 
-/**
- * Return digits in base 10 ignoring minus sign.
- *
- * It exists as a faster alternative to *_justify for justifying integers.
- * source: https://stackoverflow.com/a/4143288/5057078
- */
-int n_digits( int i )
-{
-    if( i < 0 ) {
-        i = -i;
-    }
-    if( i <         10 ) {
-        return 1;
-    }
-    if( i <        100 ) {
-        return 2;
-    }
-    if( i <       1000 ) {
-        return 3;
-    }
-    if( i <      10000 ) {
-        return 4;
-    }
-    if( i <     100000 ) {
-        return 5;
-    }
-    if( i <    1000000 ) {
-        return 6;
-    }
-    if( i <   10000000 ) {
-        return 7;
-    }
-    if( i <  100000000 ) {
-        return 8;
-    }
-    if( i < 1000000000 ) {
-        return 9;
-    }
-    return log10( i ) + 1;
-}
-
 std::string good_bad_none( int value )
 {
     if( value > 0 ) {
@@ -617,13 +576,13 @@ class comestible_inventory_preset : public inventory_selector_preset
                 int max_joy = you.fun_for( *loc, true ).first;
                 // max_joy should be 3 wide for aligment of '/'
                 if( max_joy == 0 ) {
-                    // this rarely will not be empty string
+                    // this will not be an empty string when food's joy can go below 0 by consumption (very rare)
                     return joy_str;
                 } else if( max_joy == joy ) {
                     return joy_str + std::string( joy > 0 ? "<good>/  =</good>" : "<bad>/  =</bad>" );
                 } else {
-                    // max_joy has a sign, so up to two spaces have to be right_justified for max_joy to be 3 wide
-                    return joy_str + "/" + std::string( 2 - n_digits( max_joy ), ' ' ) + good_bad_none( max_joy );
+                    return string_format( max_joy > 0 ? "%s/<good>%+3d</good>" : "%s/<bad>%3d</bad>",
+                                          joy_str, max_joy );
                 }
             }, _( "JOY/MAX" ) );
 
