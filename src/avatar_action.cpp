624de50c4715b674529d386eabe69b20a33329cd
Valiant <Night_Pryanik@mail.ru>
2021-09-25 13:02:32 +0400

    Drown if you're out of stamina

--- src/avatar_action.cpp
+++ src/avatar_action.cpp
@@ -68,6 +68,7 @@ static const efftype_id effect_pet( "pet" );
 static const efftype_id effect_relax_gas( "relax_gas" );
 static const efftype_id effect_ridden( "ridden" );
 static const efftype_id effect_stunned( "stunned" );
+static const efftype_id effect_winded( "winded" );
 
 static const itype_id itype_swim_fins( "swim_fins" );
 
@@ -596,7 +597,7 @@ void avatar_action::swim( map &m, avatar &you, const tripoint &p )
 
     int movecost = you.swim_speed();
     you.practice( skill_swimming, you.is_underwater() ? 2 : 1 );
-    if( movecost >= 500 ) {
+    if( movecost >= 500 || you.has_effect( effect_winded ) ) {
         if( !you.is_underwater() &&
             !( you.shoe_type_count( itype_swim_fins ) == 2 ||
                ( you.shoe_type_count( itype_swim_fins ) == 1 && one_in( 2 ) ) ) ) {
