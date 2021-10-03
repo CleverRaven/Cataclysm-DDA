edc6ca93f778f7a722138b7ef1b3c4f53ab60c29
kwl01skz <kwl01skz@gmail.com>
2021-10-03 22:47:17 +0800

    Update outdated ice cream test code

--- tests/iteminfo_test.cpp
+++ tests/iteminfo_test.cpp
@@ -1715,14 +1715,14 @@ TEST_CASE( "nutrients in food", "[iteminfo][food]" )
                "--\n"
                "Nutrition will <color_cyan>vary with chosen ingredients</color>.\n"
                "<color_c_white>Calories (kcal)</color>:"
-               " <color_c_yellow>126</color>-<color_c_yellow>535</color>"
+               " <color_c_yellow>56</color>-<color_c_yellow>532</color>"
                "  Quench: <color_c_yellow>0</color>\n" );
 
         CHECK( item_info_str( ice_cream, { iteminfo_parts::FOOD_VITAMINS } ) ==
                "--\n"
                "Nutrition will <color_cyan>vary with chosen ingredients</color>.\n"
-               "Vitamins (RDA): Calcium (7-31%), Iron (0-89%), "
-               "Vitamin A (3-11%), Vitamin B12 (2-6%), and Vitamin C (1-85%)\n" );
+               "Vitamins (RDA): Calcium (7-37%), Iron (0-131%), "
+               "Vitamin A (3-11%), Vitamin B12 (2-6%), and Vitamin C (1-79%)\n" );
     }
 }
 
