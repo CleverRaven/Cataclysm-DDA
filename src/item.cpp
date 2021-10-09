945ae3360708d1b8631fa083c7894d755560df72
Anton Burmistrov <Night_Pryanik@mail.ru>
2021-10-09 17:48:45 +0400

    Colorize relics in inventory in pink

--- src/item.cpp
+++ src/item.cpp
@@ -4816,6 +4816,8 @@ nc_color item::color_in_inventory( const Character *const ch ) const
         ret = c_red;
     } else if( is_filthy() || has_own_flag( flag_DIRTY ) ) {
         ret = c_brown;
+    } else if( is_relic() ) {
+        ret = c_pink;
     } else if( is_bionic() ) {
         if( !player_character.has_bionic( type->bionic->id ) || type->bionic->id->dupes_allowed ) {
             ret = player_character.bionic_installation_issues( type->bionic->id ).empty() ? c_green : c_red;
