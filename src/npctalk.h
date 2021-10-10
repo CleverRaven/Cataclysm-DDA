48931db9bd534d4942e66ac042f33720888e96b3
Saicchi <Saicchi@users.noreply.github.com>
2021-10-10 01:04:48 -0300

    npc mopping

--- src/npctalk.h
+++ src/npctalk.h
@@ -41,6 +41,7 @@ void start_trade( npc & );
 void sort_loot( npc & );
 void do_construction( npc & );
 void do_mining( npc & );
+void do_mopping( npc & );
 void do_read( npc & );
 void do_chop_plank( npc & );
 void do_vehicle_deconstruct( npc & );
