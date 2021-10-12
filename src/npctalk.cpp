e5c16c96b011ef36e156557b9e94fb75d153c969
Saicchi <47158232+Saicchi@users.noreply.github.com>
2021-10-12 01:35:14 -0300

    Mopping zone (#52228)

--- src/npctalk.cpp
+++ src/npctalk.cpp
@@ -3441,6 +3441,7 @@ void talk_effect_t::parse_string_effect( const std::string &effect_id, const Jso
             WRAP( do_fishing ),
             WRAP( do_construction ),
             WRAP( do_mining ),
+            WRAP( do_mopping ),
             WRAP( do_read ),
             WRAP( do_butcher ),
             WRAP( do_farming ),
