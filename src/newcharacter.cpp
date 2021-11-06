20575a0fb5212b7cbcc7baa89a4bc613dc17bf3b
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2021-11-06 17:52:11 -0600

    hardcoded ref to freakishly huge

--- src/newcharacter.cpp
+++ src/newcharacter.cpp
@@ -81,6 +81,7 @@ static const trait_id trait_SMELLY( "SMELLY" );
 static const trait_id trait_WEAKSCENT( "WEAKSCENT" );
 static const trait_id trait_XS( "XS" );
 static const trait_id trait_XXXL( "XXXL" );
+static const trait_id trait_HUGE( "HUGE" );
 
 // Responsive screen behavior for small terminal sizes
 static bool isWide = false;
@@ -139,7 +140,7 @@ static int stat_points_used( const avatar &u )
 {
     int used = 0;
     for( int stat : {
-             u.str_max, u.dex_max, u.int_max, u.per_max
+             calculate_nonmutated_strength(), u.dex_max, u.int_max, u.per_max
          } ) {
         used += stat + std::max( 0, stat - HIGH_STAT );
     }
@@ -555,6 +556,11 @@ static int calculate_cumulative_experience( int level )
     }
 
     return sum;
+}
+
+static int calculate_nonmutated_strength()
+{
+    return u.str_max - 4 * has_trait( trait_HUGE );
 }
 
 bool avatar::create( character_type type, const std::string &tempname )
@@ -1052,7 +1058,7 @@ tab_direction set_stats( avatar &u, pool_type pool )
             case 1:
                 mvwprintz( w, point( 2, 5 ), COL_SELECT, _( "Strength:" ) );
                 mvwprintz( w, point( 16, 5 ), c_light_gray, "%2d", u.str_max );
-                if( u.str_max >= HIGH_STAT ) {
+                if( calculate_nonmutated_strength() >= HIGH_STAT ) {
                     mvwprintz( w, point( iSecondColumn, 3 ), c_light_red,
                                _( "Increasing Str further costs 2 points" ) );
                 }
@@ -1144,7 +1150,7 @@ tab_direction set_stats( avatar &u, pool_type pool )
                 sel = 4;
             }
         } else if( action == "LEFT" ) {
-            if( sel == 1 && u.str_max > 4 ) {
+            if( sel == 1 && calculate_nonmutated_strength() > 4 ) {
                 u.str_max--;
             } else if( sel == 2 && u.dex_max > 4 ) {
                 u.dex_max--;
@@ -1154,7 +1160,7 @@ tab_direction set_stats( avatar &u, pool_type pool )
                 u.per_max--;
             }
         } else if( action == "RIGHT" ) {
-            if( sel == 1 && u.str_max < max_stat_points ) {
+            if( sel == 1 && calculate_nonmutated_strength() < max_stat_points ) {
                 u.str_max++;
             } else if( sel == 2 && u.dex_max < max_stat_points ) {
                 u.dex_max++;
