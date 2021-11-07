edaa63548095c55200acaf35be023377a346f944
Eric Pierce <wapcaplet88@gmail.com>
2021-11-07 08:30:19 -0700

    Restore tile selector highlight for ACTION_PICKUP
    Restore can_pickup_at and the corresponding ACTION_PICKUP case in
    can_interact_at, to allow the pickup directional prompt to highlight
    nearby tiles with items.

--- src/action.cpp
+++ src/action.cpp
@@ -665,6 +665,18 @@ bool can_examine_at( const tripoint &p )
     return here.can_see_trap_at( p, get_player_character() );
 }
 
+static bool can_pickup_at( const tripoint &p )
+{
+    bool veh_has_items = false;
+    map &here = get_map();
+    const optional_vpart_position vp = here.veh_at( p );
+    if( vp ) {
+        const int cargo_part = vp->vehicle().part_with_feature( vp->part_index(), "CARGO", false );
+        veh_has_items = cargo_part >= 0 && !vp->vehicle().get_items( cargo_part ).empty();
+    }
+    return ( !here.has_flag( ter_furn_flag::TFLAG_SEALED, p ) && here.has_items( p ) ) || veh_has_items;
+}
+
 bool can_interact_at( action_id action, const tripoint &p )
 {
     map &here = get_map();
@@ -687,6 +699,8 @@ bool can_interact_at( action_id action, const tripoint &p )
             return can_move_vertical_at( p, -1 );
         case ACTION_EXAMINE:
             return can_examine_at( p );
+        case ACTION_PICKUP:
+            return can_pickup_at( p );
         default:
             return false;
     }
