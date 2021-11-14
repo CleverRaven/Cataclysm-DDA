7ff1ffdb8005bedab27e63e71be43bdbfd6f2292
andrei <a12l+git@runbox.com>
2021-11-14 17:52:57 +0200

    npctrade: destroy trade ui if trade is accepted

--- src/npctrade.cpp
+++ src/npctrade.cpp
@@ -258,10 +258,11 @@ bool npc_trading::trade( npc &np, int cost, const std::string &deal )
     //               np.volume_carried() - np.volume_capacity() );
     np.drop_invalid_inventory();
 
-    trade_ui tradeui( get_avatar(), np, cost, deal );
-    trade_ui::trade_result_t trade_result = tradeui.perform_trade();
+    std::unique_ptr<trade_ui> tradeui = std::make_unique<trade_ui>( get_avatar(), np, cost, deal );
+    trade_ui::trade_result_t trade_result = tradeui->perform_trade();
 
     if( trade_result.traded ) {
+        tradeui.reset();
 
         std::list<item_location *> from_map;
 
