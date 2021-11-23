0d336c9d54c8a806c05567b693d7d103477abee5
David Seguin <davidseguin@live.ca>
2021-11-23 11:33:15 -0500

    Include non-empty containers in crafting inventory

--- src/crafting.cpp
+++ src/crafting.cpp
@@ -564,10 +564,6 @@ const inventory &Character::crafting_inventory( const tripoint &src_pos, int rad
     // TODO: Add a const overload of all_items_loc() that returns something like
     // vector<const_item_location> in order to get rid of the const_cast here.
     for( const item_location &it : const_cast<Character *>( this )->all_items_loc() ) {
-        // can't craft with containers that have items in them
-        if( !it->empty_container() ) {
-            continue;
-        }
         crafting_cache.crafting_inventory->add_item( *it );
     }
 
