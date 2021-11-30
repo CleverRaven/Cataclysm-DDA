2f68f9fcc2f1384139f742a8983c0672c8e22718
David Seguin <davidseguin@live.ca>
2021-11-29 22:58:25 -0500

    Update lang/string_extractor/parsers/difficulty_impact.py
    Co-authored-by: BrettDong <brett.browning.dong@gmail.com>

--- lang/string_extractor/parsers/difficulty_impact.py
+++ lang/string_extractor/parsers/difficulty_impact.py
@@ -2,4 +2,5 @@ from ..write_text import write_text
 
 
 def parse_difficulty_impact(json, origin):
-    write_text(json["name"], origin, comment="Aspect of gameplay affected by difficulty")
+    write_text(json["name"], origin,
+               comment="Aspect of gameplay affected by difficulty")
