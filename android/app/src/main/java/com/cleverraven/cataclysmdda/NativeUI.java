b2b6064a4487a51f5e9f7e89398b9818ae23913e
Binrui Dong <brett.browning.dong@gmail.com>
2021-12-03 10:06:04 +0800

    Native Android UI for single choice list menu

--- android/app/src/main/java/com/cleverraven/cataclysmdda/NativeUI.java
+++ android/app/src/main/java/com/cleverraven/cataclysmdda/NativeUI.java
@@ -82,6 +82,69 @@ public class NativeUI {
         }
     }
 
+    private class SingleChoiceList {
+        private Semaphore semaphore = new Semaphore(0, true);
+
+        private int singleChoiceResponse;
+
+        private final int cancel = -1;
+        private final int init = -2;
+
+        private String[] mixOptionsText(final String[] options, final boolean[] enabled) {
+            String[] result = new String[options.length];
+            for (int i = 0; i < options.length; i++) {
+                if (enabled[i]) {
+                    result[i] = options[i];
+                } else {
+                    result[i] = String.format("%s [%s]", options[i], activity.getString(R.string.unavailable));
+                }
+            }
+            return result;
+        }
+
+        public int singleChoiceList(final String text, final String[] options, final boolean[] enabled) {
+            singleChoiceResponse = init;
+
+            final String[] choices = mixOptionsText(options, enabled);
+
+            while (singleChoiceResponse == init
+                    || (singleChoiceResponse != cancel && enabled[singleChoiceResponse] == false)) {
+                activity.runOnUiThread(new Runnable() {
+                    public void run() {
+                        AlertDialog dialog = new AlertDialog.Builder(activity, R.style.AlertDialogTheme)
+                                .setTitle(text)
+                                .setItems(choices, new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        singleChoiceResponse = which;
+                                        semaphore.release();
+                                    }
+                                })
+                                .setOnCancelListener(new DialogInterface.OnCancelListener() {
+                                    public void onCancel(DialogInterface dialog) {
+                                        singleChoiceResponse = cancel;
+                                        semaphore.release();
+                                    }
+                                })
+                                .create();
+                        dialog.show();
+                    }
+                });
+
+                try {
+                    semaphore.acquire();
+                } catch (InterruptedException ex) {
+                    // No-op
+                }
+
+                if (singleChoiceResponse != cancel && enabled[singleChoiceResponse] == false) {
+                    new Popup().popup(activity.getString(R.string.unavailableOption));
+                }
+            }
+
+            return singleChoiceResponse;
+        }
+    }
+
     public void popup(final String message) {
         final Popup popup = new Popup();
         popup.popup(message);
@@ -90,5 +153,10 @@ public class NativeUI {
     public boolean queryYN(final String message) {
         final QueryYN queryYN = new QueryYN();
         return queryYN.queryYN(message);
+    }
+
+    public int singleChoiceList(final String text, final String[] options, final boolean[] enabled) {
+        final SingleChoiceList singleChoiceList = new SingleChoiceList();
+        return singleChoiceList.singleChoiceList(text, options, enabled);
     }
 }
