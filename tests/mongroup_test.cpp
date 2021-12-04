94e9f5d0a31310c1d56869f4fdba9de3b08671d3
David Seguin <davidseguin@live.ca>
2021-12-03 22:38:00 -0500

    Event based monsters: expand unit tests
    Added additional tests to check spawns for event-only mongroups

--- tests/mongroup_test.cpp
+++ tests/mongroup_test.cpp
@@ -23,8 +23,23 @@ static void spawn_x_monsters( int x, const mongroup_id &grp, const std::vector<m
     std::set<mtype_id> rand_results;
     calendar::turn = time_point( 1 );
     for( int i = 0; i < x; i++ ) {
-        rand_gets.emplace( MonsterGroupManager::GetRandomMonsterFromGroup( grp ) );
-        rand_results.emplace( MonsterGroupManager::GetResultFromGroup( grp ).name );
+        const mtype_id &tmp_get = MonsterGroupManager::GetRandomMonsterFromGroup( grp );
+        if( !tmp_get.is_null() ) {
+            rand_gets.emplace( tmp_get );
+        }
+
+        const mtype_id &tmp_res = MonsterGroupManager::GetResultFromGroup( grp ).name;
+        if( !tmp_res.is_null() ) {
+            rand_results.emplace( tmp_res );
+        }
+    }
+    if( nospawn.empty() ) {
+        CHECK( !rand_gets.empty() );
+        CHECK( !rand_results.empty() );
+    }
+    if( yesspawn.empty() ) {
+        CHECK( rand_gets.empty() );
+        CHECK( rand_results.empty() );
     }
     for( const mtype_id &m : nospawn ) {
         CHECK( rand_gets.find( m ) == rand_gets.end() );
@@ -47,6 +62,7 @@ TEST_CASE( "Event-based monster spawns do not spawn outside event", "[monster][m
 
         WHEN( "current holiday == none" ) {
             holiday cur_event = get_holiday_from_time( 1614574800L, true );
+            REQUIRE( cur_event == holiday::none );
             THEN( "only spawn non-event monsters" ) {
                 std::vector<mtype_id> mons = MonsterGroupManager::GetMonstersFromGroup( test_group, true );
                 CHECK( mons.size() == 1 );
@@ -60,6 +76,7 @@ TEST_CASE( "Event-based monster spawns do not spawn outside event", "[monster][m
 
         WHEN( "current holiday == christmas" ) {
             holiday cur_event = get_holiday_from_time( 1640408400L, true );
+            REQUIRE( cur_event == holiday::christmas );
             THEN( "only spawn christmas and non-event monsters" ) {
                 std::vector<mtype_id> mons = MonsterGroupManager::GetMonstersFromGroup( test_group, true );
                 CHECK( mons.size() == 4 );
@@ -72,12 +89,50 @@ TEST_CASE( "Event-based monster spawns do not spawn outside event", "[monster][m
 
         WHEN( "current holiday == halloween" ) {
             holiday cur_event = get_holiday_from_time( 1635652800L, true );
+            REQUIRE( cur_event == holiday::halloween );
             THEN( "only spawn halloween and non-event monsters" ) {
                 std::vector<mtype_id> mons = MonsterGroupManager::GetMonstersFromGroup( test_group, true );
                 CHECK( mons.size() == 3 );
 
                 const std::vector<mtype_id> ylist = { mon_test_speed_desc_base_immobile, mon_test_CBM, mon_test_speed_desc_base };
                 const std::vector<mtype_id> nlist = { mon_test_shearable, mon_test_non_shearable, mon_test_bovine };
+                spawn_x_monsters( max_iters, test_group, ylist, nlist );
+            }
+        }
+    }
+}
+
+TEST_CASE( "Event-based monsters from an event-only mongroup", "[monster][mongroup]" )
+{
+    override_option ev_spawn_opt( "EVENT_SPAWNS", "monsters" );
+    REQUIRE( get_option<std::string>( "EVENT_SPAWNS" ) == "monsters" );
+
+    GIVEN( "a monster group with entries from a single event" ) {
+        mongroup_id test_group( "test_event_only" );
+        REQUIRE( test_group->monsters.size() == 3 );
+
+        WHEN( "current holiday == none" ) {
+            holiday cur_event = get_holiday_from_time( 1614574800L, true );
+            REQUIRE( cur_event == holiday::none );
+            THEN( "don't spawn any monsters" ) {
+                std::vector<mtype_id> mons = MonsterGroupManager::GetMonstersFromGroup( test_group, true );
+                CHECK( mons.size() == 0 );
+
+                const std::vector<mtype_id> ylist = {};
+                const std::vector<mtype_id> nlist = { mon_test_non_shearable, mon_test_shearable, mon_test_bovine };
+                spawn_x_monsters( max_iters, test_group, ylist, nlist );
+            }
+        }
+
+        WHEN( "current holiday == christmas" ) {
+            holiday cur_event = get_holiday_from_time( 1640408400L, true );
+            REQUIRE( cur_event == holiday::christmas );
+            THEN( "spawn all monsters" ) {
+                std::vector<mtype_id> mons = MonsterGroupManager::GetMonstersFromGroup( test_group, true );
+                CHECK( mons.size() == 3 );
+
+                const std::vector<mtype_id> ylist = { mon_test_non_shearable, mon_test_shearable, mon_test_bovine };
+                const std::vector<mtype_id> nlist = {};
                 spawn_x_monsters( max_iters, test_group, ylist, nlist );
             }
         }
