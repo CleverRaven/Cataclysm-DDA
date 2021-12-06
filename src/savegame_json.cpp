eec7ceec0db6a0fc29c817d4f18124830ae7eaad
bombasticSlacks <dillon.matchett@gmail.com>
2021-12-06 16:54:38 -0400

    should be done

--- src/savegame_json.cpp
+++ src/savegame_json.cpp
@@ -279,6 +279,8 @@ void item_pocket::favorite_settings::serialize( JsonOut &json ) const
     json.member( "category_whitelist", category_whitelist );
     json.member( "category_blacklist", category_blacklist );
     json.member( "collapsed", collapsed );
+    json.member( "disabled", disabled );
+    json.member( "unload", unload );
     json.end_object();
 }
 
@@ -292,6 +294,12 @@ void item_pocket::favorite_settings::deserialize( const JsonObject &data )
     data.read( "category_blacklist", category_blacklist );
     if( data.has_member( "collapsed" ) ) {
         data.read( "collapsed", collapsed );
+    }
+    if( data.has_member( "disabled" ) ) {
+        data.read( "disabled", disabled );
+    }
+    if( data.has_member( "unload" ) ) {
+        data.read( "unload", unload );
     }
 }
 
