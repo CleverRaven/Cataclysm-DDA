88f7457963612f3b9330e3a3128da9e422d13e00
Serhiy Zahoriya <serhiy.int@gmail.com>
2021-12-09 13:27:09 +0200

    generate_image function

--- tools/json_tools/generate_overmap_sprites.py
+++ tools/json_tools/generate_overmap_sprites.py
@@ -6,10 +6,15 @@ import argparse
 import json
 
 from pathlib import Path
-from PIL import Image
 from typing import Optional, Union
 
+from PIL import Image
+
 from util import import_data
+
+
+TERRAIN_COLOR_NAMES = {}
+SCHEME = None
 
 
 def get_first_valid(
@@ -47,6 +52,34 @@ class TupleJSONDecoder(json.JSONDecoder):
         return tuple(values), end
 
 
+def generate_image(
+    rows: list,
+    terrain_dict: dict,
+    fill_ter: str,
+) -> Image:
+    """
+    Generate sprite from rows
+    """
+    image = Image.new('RGBA', [len(rows[0]), len(rows)], 255)
+    image_data = image.load()
+    for index_x, row in enumerate(rows):
+        for index_y, char in enumerate(row):
+            terrain_id = get_first_valid(terrain_dict.get(char), fill_ter)
+            color = SCHEME['terrain'].get(terrain_id)
+            if not color:
+                color = SCHEME['colors'].get(
+                    TERRAIN_COLOR_NAMES.get(terrain_id))
+            if not color:
+                color = 0, 0, 0, 0
+            try:
+                image_data[index_y, index_x] = color
+            except TypeError:
+                print(color)
+                raise
+
+    return image
+
+
 def main():
     """
     main
@@ -61,18 +94,18 @@ def main():
     )
     arg_parser.add_argument(
         'color_scheme', type=Path,
-        help='path to a JSON color scheme',
+        help='path to a JSON color SCHEME',
     )
     args_dict = vars(arg_parser.parse_args())
     output_dir = args_dict.get('output_dir')
     color_scheme_path = args_dict.get('color_scheme')
     with open(color_scheme_path) as filehandler:
-        scheme = json.load(filehandler, cls=TupleJSONDecoder)
+        global SCHEME
+        SCHEME = json.load(filehandler, cls=TupleJSONDecoder)
 
     # TODO: support fallback_predecessor_mapgen
-    scheme['terrain'][None] = (0, 0, 0, 0)
+    SCHEME['terrain'][None] = (0, 0, 0, 0)
 
-    terrain_colors = {}
     terrain_data, errors = import_data(
         json_dir=Path('../../data/json/furniture_and_terrain/'),
         json_fmatch='terrain*.json'
@@ -88,7 +121,7 @@ def main():
             terrain_color = terrain_color[0]
 
         if terrain_type == 'terrain' and terrain_id and terrain_color:
-            terrain_colors[terrain_id] = terrain_color
+            TERRAIN_COLOR_NAMES[terrain_id] = terrain_color
 
     mapgen_data, errors = import_data(
         json_dir=Path('../../data/json/mapgen/'),
@@ -106,8 +139,7 @@ def main():
         if method != 'json':
             continue
 
-        # FIXME: support splitting
-        om_id = get_first_valid(entry.get('om_terrain'))
+        om_id = entry.get('om_terrain')
         if not om_id:
             continue
 
@@ -126,26 +158,10 @@ def main():
 
         fill_ter = mapgen.get('fill_ter')
 
-        image = Image.new('RGBA', [len(rows[0]), len(rows)], 255)
-        image_data = image.load()
+        om_id = get_first_valid(om_id)
 
-        for index_x, row in enumerate(rows):
-            for index_y, char in enumerate(row):
-                terrain_id = get_first_valid(terrain_dict.get(char), fill_ter)
-
-                color = scheme['terrain'].get(terrain_id)
-                if not color:
-                    color = scheme['colors'].get(
-                        terrain_colors.get(terrain_id)
-                    )
-                if not color:
-                    # print(om_id, terrain_id)
-                    color = (0, 0, 0, 0)
-                try:
-                    image_data[index_y, index_x] = color
-                except TypeError:
-                    print(color)
-                    raise
+        image = generate_image(
+            rows=rows, terrain_dict=terrain_dict, fill_ter=fill_ter)
 
         image.save(output_dir / f'{om_id}.png')
 
