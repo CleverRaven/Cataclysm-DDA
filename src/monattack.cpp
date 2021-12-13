ce4017822cd75d19c1f42cc12948a3b1dd41a767
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2021-12-12 23:45:51 -0700

    lower enemy tazer damage, make it check torso bash

--- src/monattack.cpp
+++ src/monattack.cpp
@@ -3314,21 +3314,30 @@ void mattack::taze( monster *z, Creature *target )
 {
     // It takes a while
     z->moves -= 200;
-    if( target == nullptr || target->uncanny_dodge() ) {
+    if( target == nullptr ) {
         return;
     };
 
     /** @EFFECT_DODGE increases chance of dodging a tazer attack */
     const bool tazer_was_dodged = dice( 10, 10 ) < dice( target->get_dodge(), 10 );
-    if( tazer_was_dodged ) {
+    const int tazer_resistance = target->get_armor_bash( bodypart_id( "torso" ) );
+    const bool tazer_was_armored = dice( numdice, 10 ) < dice( tazer_resistance, 10 );
+    
+    if( tazer_was_dodged || target->uncanny_dodge() ) {
         target->add_msg_if_player( m_bad, _( "The %s attempts to shock you but you dodge." ),
                                    z->name() );
         return;
     }
 
+    if( tazer_was_armored ) {
+        target->add_msg_if_player( m_bad, _( "The %s attempts to shock you but your thick armor stops it." ),
+                                   z->name() );
+        return;
+    }   
+
     int dam = target->deal_damage( z, bodypart_id( "torso" ), damage_instance( damage_type::ELECTRIC,
                                    rng( 1,
-                                        5 ) ) ).total_damage();
+                                        1 ) ) ).total_damage();
     if( dam == 0 ) {
         target->add_msg_player_or_npc( _( "The %s unsuccessfully attempts to shock you." ),
                                        _( "The %s unsuccessfully attempts to shock <npcname>." ),
