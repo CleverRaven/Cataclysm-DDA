cb48879106c2194b7620cc9e390ddcc8a8e4ede5
Dillon Matchett <dillon.matchett@gmail.com>
2021-12-15 00:22:15 -0400

    "Personal" Sorting Zones (#53307)
    * localization sorta working
    
    * In a decent place
    
    * everything appears to now be working
    
    * calm clang down
    
    * clang will be appeased

--- src/handle_action.cpp
+++ src/handle_action.cpp
@@ -1172,6 +1172,11 @@ static void loot()
     auto &mgr = zone_manager::get_manager();
     const bool has_fertilizer = player_character.has_item_with_flag( flag_FERTILIZER );
 
+    // cache should only happen if we have personal zones defined
+    if( mgr.has_personal_zones() ) {
+        mgr.cache_data();
+    }
+
     // Manually update vehicle cache.
     // In theory this would be handled by the related activity (activity_on_turn_move_loot())
     // but with a stale cache we never get that far.
