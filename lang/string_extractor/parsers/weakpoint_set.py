6ee7a08ec06d2702054aa41c369e954d177b9acf
David Seguin <davidseguin@live.ca>
2021-12-16 18:42:36 -0500

    Fix string extractor for "weakpoint_set"

--- lang/string_extractor/parsers/weakpoint_set.py
+++ lang/string_extractor/parsers/weakpoint_set.py
@@ -2,5 +2,18 @@ from ..write_text import write_text
 
 
 def parse_weakpoint_set(json, origin):
-    if "name" in json:
-        write_text(json["name"], origin, comment="Weakpoint name")
+    if "weakpoints" in json:
+        for wp in json["weakpoints"]:
+            id = ""
+            if "id" in wp:
+                id = wp["id"]
+            if "name" in wp:
+                comment = "Sentence fragment describing " \
+                          "the \"{}\" weakpoint".format(id)
+                write_text(wp["name"], origin, comment=comment)
+            if "effects" in wp:
+                for fx in wp["effects"]:
+                    if "message" in fx:
+                        comment = "Message describing the effect of hitting " \
+                                  "the \"{}\" weakpoint".format(id)
+                        write_text(fx["message"], origin, comment=comment)
