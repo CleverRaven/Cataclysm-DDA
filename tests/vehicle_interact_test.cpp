a9529d852582888ee37b0570c69daaa6d5d14839
Aleksandr Folts <qzer56@gmail.com>
2021-12-25 23:07:32 +0200

    fix crashing test

--- tests/vehicle_interact_test.cpp
+++ tests/vehicle_interact_test.cpp
@@ -72,15 +72,15 @@ TEST_CASE( "repair_vehicle_part" )
         std::vector<item> tools;
         tools.push_back( tool_with_ammo( "welder", 500 ) );
         tools.emplace_back( "goggles_welding" );
+        tools.emplace_back( "hammer" );
+        tools.insert( tools.end(), 150, item( "scrap" ) );
+        tools.insert( tools.end(), 10, item( "material_aluminium_ingot" ) );
         tools.insert( tools.end(), 50, item( "welding_rod_steel" ) );
         tools.insert( tools.end(), 50, item( "welding_wire_steel" ) );
         tools.insert( tools.end(), 50, item( "brazing_rod_bronze" ) );
         tools.insert( tools.end(), 50, item( "welding_rod_alloy" ) );
         tools.insert( tools.end(), 50, item( "welding_wire_alloy" ) );
         tools.insert( tools.end(), 50, item( "brazing_rod_alloy" ) );
-        tools.insert( tools.end(), 150, item( "scrap" ) );
-        tools.insert( tools.end(), 10, item( "material_aluminium_ingot" ) );
-        tools.emplace_back( "hammer" );
         test_repair( tools, true );
     }
     SECTION( "UPS_modded_welder" ) {
@@ -96,15 +96,15 @@ TEST_CASE( "repair_vehicle_part" )
         tools.push_back( ups );
 
         tools.emplace_back( "goggles_welding" );
+        tools.emplace_back( "hammer" );
+        tools.insert( tools.end(), 150, item( "scrap" ) );
+        tools.insert( tools.end(), 10, item( "material_aluminium_ingot" ) );
         tools.insert( tools.end(), 50, item( "welding_rod_steel" ) );
         tools.insert( tools.end(), 50, item( "welding_wire_steel" ) );
         tools.insert( tools.end(), 50, item( "brazing_rod_bronze" ) );
         tools.insert( tools.end(), 50, item( "welding_rod_alloy" ) );
         tools.insert( tools.end(), 50, item( "welding_wire_alloy" ) );
         tools.insert( tools.end(), 50, item( "brazing_rod_alloy" ) );
-        tools.insert( tools.end(), 150, item( "scrap" ) );
-        tools.insert( tools.end(), 10, item( "material_aluminium_ingot" ) );
-        tools.emplace_back( "hammer" );
         test_repair( tools, true );
     }
     SECTION( "welder_missing_goggles" ) {
