9e05c258d81cce81d27ca1ca9bc9cd58e4317cd1
Nicolas Appriou <nicolas.appriou@gmail.com>
2021-12-31 11:32:24 +0100

    astyle formatting pass

--- src/main.cpp
+++ src/main.cpp
@@ -218,7 +218,7 @@ void printVersionMessage()
             "data dir: %s\nuser dir: %s\n",
             getVersionString(),
             hasTiles ? '+' : '-',
-            hasSound ? '+' : '-' ,
+            hasSound ? '+' : '-',
             PATH_INFO::datadir().c_str(),
             PATH_INFO::user_dir().c_str() );
 }
