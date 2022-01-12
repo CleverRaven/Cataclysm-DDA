46e2a99b2e3124254a254ef7c0b18e82f27d6f46
Kevin Granade <kevin.granade@gmail.com>
2022-01-11 23:34:21 -0800

    fix bad edit

--- src/handle_action.cpp
+++ src/handle_action.cpp
@@ -2833,6 +2833,7 @@ bool game::handle_action()
             if( !get_option<bool>( "NO_UNKNOWN_COMMAND_MSG" ) ) {
                 std::string msg = string_format( _( "Unknown command: \"%s\" (%ld)" ), evt.long_description(), ch );
                 if( const cata::optional<std::string> hint =
+                        press_x_if_bound( ACTION_KEYBINDINGS ) ) {
                     msg += string_format( "\n%s",
                                           _( "%s at any time to see and edit keybindings relevant to "
                                              "the current context." ), *hint );
