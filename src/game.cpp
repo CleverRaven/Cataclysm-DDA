6fab68dca573a2a0d40992e7414287bd0e7be3ae
Andrey Bienkowski <hexagon-recursion@posteo.net>
2022-01-14 00:43:16 +0300

    Switch graveyard and screenshots to use local time
    https://github.com/CleverRaven/Cataclysm-DDA/pull/54006 makes it seam
    like this is the desired behavior

--- src/game.cpp
+++ src/game.cpp
@@ -2557,14 +2557,14 @@ void game::death_screen()
 }
 
 // A timestamp that can be used to make unique file names
-// Date format is a somewhat ISO-8601 compliant GMT time date (except we use '-' instead of ':' probably because of Windows file name rules).
+// Date format is a somewhat ISO-8601 compliant local time date (except we use '-' instead of ':' probably because of Windows file name rules).
 // XXX: removed the timezone suffix due to an mxe bug
 // See: https://github.com/mxe/mxe/issues/2749
 static std::string timestamp_now()
 {
     std::time_t time = std::time( nullptr );
     std::stringstream date_buffer;
-    date_buffer << std::put_time( std::gmtime( &time ), "%Y-%m-%dT%H-%M-%S" );
+    date_buffer << std::put_time( std::localtime( &time ), "%Y-%m-%dT%H-%M-%S" );
     return date_buffer.str();
 }
 
