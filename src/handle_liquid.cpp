0f8eb0dda8a103273799d40e02122d3dcadf0900
Paul Fenwick <pjf@perltraining.com.au>
2022-01-12 23:57:28 -0800

    Use part location rather than vehicle location for liquid handling (#54341)
    `serialize_liquid_source` would record the vehicle position, but if the
    vehicle had a hole in it then asking "what vehicle is here?" in other
    code could come back empty.
    
    We now serialize the position of the part that contains the liquid, to
    avoid the possibility of returning a vehicle hole location.
    
    Fixes #51582

--- src/handle_liquid.cpp
+++ src/handle_liquid.cpp
@@ -51,7 +51,7 @@ static void serialize_liquid_source( player_activity &act, const vehicle &veh, c
 {
     act.values.push_back( static_cast<int>( liquid_source_type::VEHICLE ) );
     act.values.push_back( part_num );
-    act.coords.push_back( veh.global_pos3() );
+    act.coords.push_back( veh.global_part_pos3( part_num ) );
     act.str_values.push_back( serialize( liquid ) );
 }
 
