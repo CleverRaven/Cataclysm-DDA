b4e1b29bb9aa3bbe047934553850337c046c4985
Mikenyes <25803648+Mikenyes@users.noreply.github.com>
2022-01-27 04:27:04 -0500

    Update debug_menu.cpp

--- src/debug_menu.cpp
+++ src/debug_menu.cpp
@@ -2483,7 +2483,7 @@ void debug()
             int artifact_max_negative_value;
             if( relic_menu.ret >= 0 && relic_menu.ret < static_cast<int>( relic_list.size() ) ) {
                 if( query_int( artifact_max_attributes, _( "Enter max attributes:" ) )
-                    && query_int( artifact_power_level, _( "Enter power level:" )
+                    && query_int( artifact_power_level, _( "Enter power level:" ) )
                 && query_int( artifact_max_negative_value, _( "Enter negative power limit:" ) ) {
                 if( const cata::optional<tripoint> center = g->look_around() ) {
                         here.spawn_artifact( *center, relic_list[relic_menu.ret], artifact_max_attributes,
