7208445697b875e484fc7efbedeed987dfa5c523
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2022-02-01 20:46:13 -0800

    support for RECOIL enchant

--- src/character.cpp
+++ src/character.cpp
@@ -10503,7 +10503,13 @@ double Character::recoil_vehicle() const
 
 double Character::recoil_total() const
 {
-    return recoil + recoil_vehicle();
+    double recoil_final = recoil + recoil_vehicle();
+    float recoil_mod = enchantment_cache->modify_value( enchant_vals::mod::RECOIL,
+                            1.0f );
+    if( recoil_mod != 1.0f ) {
+        recoil_final *= recoil_mod;
+    
+    return recoil_final;
 }
 
 bool Character::is_hallucination() const
