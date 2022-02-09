b3a003084cdcf259c099852414571931802adc10
Mikenyes <25803648+Mikenyes@users.noreply.github.com>
2022-02-08 22:33:06 -0500

    Removed orphaned iuse actions

--- src/item_factory.cpp
+++ src/item_factory.cpp
@@ -1525,8 +1525,6 @@ void Item_factory::init()
     add_actor( std::make_unique<molle_detach_actor>() );
     add_actor( std::make_unique<install_bionic_actor>() );
     add_actor( std::make_unique<detach_gunmods_actor>() );
-    add_actor( std::make_unique<mutagen_actor>() );
-    add_actor( std::make_unique<mutagen_iv_actor>() );
     add_actor( std::make_unique<deploy_tent_actor>() );
     add_actor( std::make_unique<learn_spell_actor>() );
     add_actor( std::make_unique<cast_spell_actor>() );
