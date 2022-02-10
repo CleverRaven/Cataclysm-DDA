60b49cab7f5a97d91b1f4266671caf9eb6333d9f
Fris0uman <sthapplz@gmail.com>
2022-02-10 18:20:37 +0100

    clean up

--- src/construction.cpp
+++ src/construction.cpp
@@ -1347,6 +1347,7 @@ void construct::done_appliance( const tripoint &p )
 {
     map &here = get_map();
     vehicle *veh = here.add_vehicle( vehicle_prototype_none, p, 270_degrees, 0, 0 );
+
     if( !veh ) {
         debugmsg( "error constructing vehicle" );
         return;
