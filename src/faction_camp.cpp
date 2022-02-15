31279619f5c9df9a70e1fa80b0d167148b651443
PatrikLundell <j.patrik.r.lundell@gmail.com>
2022-02-15 15:09:39 +0100

    attempting to satisfy clang-tidy weirdo demands

--- src/faction_camp.cpp
+++ src/faction_camp.cpp
@@ -131,9 +131,6 @@ static const mtype_id mon_turkey( "mon_turkey" );
 static const mtype_id mon_weasel( "mon_weasel" );
 static const mtype_id mon_wolf( "mon_wolf" );
 
-static const std::string faction_hide_site_0_string = "faction_hide_site_0";
-
-//static const oter_str_id oter_faction_hide_site_0(faction_hide_site_0_string);
 static const oter_str_id oter_forest_wet( "forest_wet" );
 
 static const oter_type_str_id oter_type_forest_trail( "forest_trail" );
@@ -172,6 +169,7 @@ static const zone_type_id zone_type_CAMP_STORAGE( "CAMP_STORAGE" );
 
 //  Moved the constant compound "string" declaration into a jumble here rather than where they belong
 //  because placing them together with their context is rejected by 'cata-static-string_id-constants, -warnings-as-errors'
+static const std::string faction_hide_site_0_string = "faction_hide_site_0";
 static const oter_str_id oter_faction_hide_site_0( faction_hide_site_0_string );
 static const update_mapgen_id update_mapgen_faction_wall_level_N_1(
     faction_wall_level_n_1_string.c_str() );
