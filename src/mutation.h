d9840e8da1163a8ee4a440c4e3a7326e4fee5a9d
Dillon Matchett <dillon.matchett@gmail.com>
2022-02-25 22:27:14 -0400

    untested sweat multiplier implementation (#55676)

--- src/mutation.h
+++ src/mutation.h
@@ -233,6 +233,8 @@ struct mutation_branch {
         cata::optional<float> stomach_size_multiplier = cata::nullopt;
         // the modifier for the vomit chance
         cata::optional<float> vomit_multiplier = cata::nullopt;
+        // the modifier for sweat ammount
+        cata::optional<float> sweat_multiplier = cata::nullopt;
 
         // Adjusts sight range on the overmap. Positives make it farther, negatives make it closer.
         cata::optional<float> overmap_sight = cata::nullopt;
