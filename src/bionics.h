99461f150bb8219169887f07689174451053643f
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2022-03-04 14:08:14 -0800

    power_trickle

--- src/bionics.h
+++ src/bionics.h
@@ -91,6 +91,9 @@ struct bionic_data {
     /**Amount of bullet protection offered by this bionic*/
     std::map<bodypart_str_id, size_t> bullet_protec;
 
+    /** The bionic provides this much power every second passively and at all times */
+    units::energy power_trickle = 0_kJ;
+
     float vitamin_absorb_mod = 1.0f;
 
     // Bonus or penalty to social checks (additive).  50 adds 50% to success, -25 subtracts 25%
