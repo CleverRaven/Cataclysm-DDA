39148e4644398f4b589bd44a357636690a9f761e
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2022-03-04 19:52:15 -0800

    Update bionics.cpp

--- src/bionics.cpp
+++ src/bionics.cpp
@@ -340,7 +340,7 @@ void bionic_data::load( const JsonObject &jsobj, const std::string & )
     assign( jsobj, "act_cost", power_activate, false, 0_kJ );
     assign( jsobj, "deact_cost", power_deactivate, false, 0_kJ );
     assign( jsobj, "trigger_cost", power_trigger, false, 0_kJ );
-    assign( jsobj, "power_trickle", power_trickle, false, 0_kJ );
+    assign( jsobj, "trickle_amount", power_trickle, false, 0_kJ );
     
     optional( jsobj, was_loaded, "time", charge_time, 0 );
 
@@ -1766,7 +1766,7 @@ void Character::process_bionic( bionic &bio )
         return;
     }
 
-    mod_power_level( units::from_kilojoule( bio.power_trickle ) );
+    mod_power_level( units::from_kilojoule( bio.trickle_amount ) );
 
     if( bio.get_uid() == get_weapon_bionic_uid() ) {
         const bool wrong_weapon_wielded = weapon.typeId() != bio.get_weapon().typeId() ||
