ef13520cf746dd233daf485ddd4024e65c2ac0ec
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2022-03-04 19:57:40 -0800

    Update bionics.cpp

--- src/bionics.cpp
+++ src/bionics.cpp
@@ -340,7 +340,7 @@ void bionic_data::load( const JsonObject &jsobj, const std::string & )
     assign( jsobj, "act_cost", power_activate, false, 0_kJ );
     assign( jsobj, "deact_cost", power_deactivate, false, 0_kJ );
     assign( jsobj, "trigger_cost", power_trigger, false, 0_kJ );
-    assign( jsobj, "trickle_amount", power_trickle, false, 0_kJ );
+    assign( jsobj, "power_trickle", trickle_amount, false, 0_kJ );
     
     optional( jsobj, was_loaded, "time", charge_time, 0 );
 
