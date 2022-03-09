9b3ee362a182ff057fa7f355629eea3bf89d57a1
xuvatilavv <18742123+xuvatilavv@users.noreply.github.com>
2022-03-09 21:34:50 +0000

    Add context

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -9748,7 +9748,7 @@ cata::optional<int> iuse::wash_items( Character *p, bool soft_items, bool hard_i
         auto to_string = []( int val ) -> std::string {
             if( val == INT_MAX )
             {
-                return _( "inf" );
+                return pgettext( "short for infinity", "inf" );
             }
             return string_format( "%3d", val );
         };
