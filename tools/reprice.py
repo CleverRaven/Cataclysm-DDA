1e90967cb5eef71eb0b3bdae1f80655175c73e98
sulyi <sulyi.gbox@gmail.com>
2022-03-11 04:04:03 +0100

    Fix name lookup logic

--- tools/reprice.py
+++ tools/reprice.py
@@ -9,5 +9,5 @@ from json_tools.util import import_data
 (data, errors) = import_data()
 reprice = [item for item in data
            if 'price' in item and 'price_postapoc' not in item]
-ids_and_names = [(r['id'], r['name'].get('str', r['name'].get('str_sp')))
+ids_and_names = [(r['id'], r['name'].get('str') or r['name'].get('str_sp'))
                  for r in reprice if 'abstract' not in r]
