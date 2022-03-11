8f92704fc9d34fe1d2ad1f487a46e49bf3829153
sulyi <sulyi.gbox@gmail.com>
2022-03-11 03:21:16 +0100

    Add tool listing candidate items for repricing

---
+++ tools/reprice.py
@@ -0,0 +1,13 @@
+"""Script listing items potentally needing a `price_postapoc` key
+
+It stores candidates in `reprice` and
+extracts item ids and names in `ids_and_names`.
+"""
+
+from json_tools.util import import_data
+
+(data, errors) = import_data()
+reprice = [item for item in data
+              if 'price' in item and 'price_postapoc' not in item]
+ids_and_names = [(r['id'], r['name']['str' if 'str' in r['name'] else 'str_sp'])
+                    for r in reprice if 'abstract' not in r]
