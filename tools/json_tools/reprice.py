527e836522a6f8aa6417fdd06f03f32f67757a2c
sulyi <sulyi.gbox@gmail.com>
2022-03-11 05:58:09 +0100

    Add script execution

--- tools/json_tools/reprice.py
+++ tools/json_tools/reprice.py
@@ -1,14 +1,37 @@
-"""Script listing items potentally needing a `price_postapoc` key
+#!/usr/bin/env python3
 
-It stores candidates in `reprice` and
-extracts item ids and names in `ids_and_names`.
+"""List items potentally needing a `price_postapoc` key
+
+Script prints `id` and `name` of each item in following lines separating
+them with an empty line.
 """
+# TODO: add example
 
+
+import sys
 from util import import_data
 
-(data, errors) = import_data()
-reprice = [item for item in data
-           if 'price' in item and 'price_postapoc' not in item]
-ids_and_names = [(r['id'], r['name'].get('str') or
-                  r['name'].get('str_sp') or r['name'].get('str_pl'))
-                 for r in reprice if 'abstract' not in r]
+
+def parse_args():
+    # TODO: add option to load a blacklist
+    # TODO: add option to load different json dir
+    # TODO: add option to match json files
+    pass
+
+
+if __name__ == '__main__':
+    parse_args()
+
+    (data, errors) = import_data()
+    reprice = [item for item in data
+               if 'price' in item and 'price_postapoc' not in item and
+               'id' in item]
+    print('\n\n'.join("\"id\": \"{id:s}\"\n\"name\": \"{name:s}\"".format(
+        id=r['id'],
+        name=r['name'].get('str') or r['name'].get('str_sp') or
+        r['name'].get('str_pl') if 'name' in r else "") for r in reprice)
+    )
+
+    if errors:
+        print("ERROR: following occured loading JSON data:", file=sys.stderr)
+        print(errors, file=sys.stderr)
