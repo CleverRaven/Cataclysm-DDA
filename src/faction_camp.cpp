b98c1cd0f5f24da197718af36bc0c39e46041ef1
Faalagorn <faalagorn@gmail.com>
2022-03-31 20:20:58 +0200

    Mark missing basecamp strings for translation

--- src/faction_camp.cpp
+++ src/faction_camp.cpp
@@ -191,8 +191,8 @@ struct mass_volume {
 
 namespace base_camps
 {
-static const std::string recover_ally_string = "Recover Ally, ";
-static const std::string expansion_string = " Expansion";
+static const std::string recover_ally_string = _( "Recover Ally, " );
+static const std::string expansion_string = _( " Expansion" );
 
 
 recipe_id select_camp_option( const std::map<recipe_id, translation> &pos_options,
