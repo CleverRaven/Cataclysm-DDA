43abf5136b27aad931f0414fe53ddd2fee2693b2
Anton Burmistrov <Night_Pryanik@mail.ru>
2022-04-22 17:02:31 +0400

    Mutable "generic" mine (#56555)
    * Moved artifacts to its own file
    
    * Created "generic" mine mapgen and nested chunks for it
    
    * Created mutable mine OMS definition
    
    * Removed Mine Entrance OMS and created an empty migration for it
    
    Also reduced chance for spawn for "themed" mines twofold.
    
    * Obsoleted `mine`, `mine_down` OMTs and made `mine_finale` OMT rotatable
    
    * Added `GROUP_MINERS` monster group (for mapgen)
    
    * Removed now-unused `GROUP_DOG_THING` monster group
    
    * Removed old hardcoded `overmap::build_mine` and `map::draw_mine` functions
    
    * Added migration for old `mine_finale` OMT
    
    * More strict conditions
    
    * Resolve conflicts
    
    * Trying to fix the failing tests
    
    * Optimized, removed unneeded
    
    * Fixed tests
    
    * Removed Mine Entrance specials from mods
    
    Co-authored-by: Binrui Dong <brett.browning.dong@gmail.com>

--- src/savegame.cpp
+++ src/savegame.cpp
@@ -389,7 +389,8 @@ void overmap::convert_terrain(
         if( old == "fema" || old == "fema_entrance" || old == "fema_1_3" ||
             old == "fema_2_1" || old == "fema_2_2" || old == "fema_2_3" ||
             old == "fema_3_1" || old == "fema_3_2" || old == "fema_3_3" ||
-            old == "s_lot" || old == "mine_entrance" || old == "triffid_finale" ) {
+            old == "s_lot" || old == "mine_entrance" || old == "mine_finale" ||
+            old == "triffid_finale" ) {
             ter_set( pos, oter_id( old + "_north" ) );
         } else if( old.compare( 0, 6, "bridge" ) == 0 ) {
             ter_set( pos, oter_id( old ) );
