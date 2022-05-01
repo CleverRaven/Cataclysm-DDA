7efb9a22435456ad6c5061e6cc792829dfe46c44
alef <alef@users.noreply.github.com>
2022-05-01 18:23:26 +0200

    Use CMAKE_CXX_FLAGS_INIT instead of add_compile_options()

--- build-scripts/MSVC.cmake
+++ build-scripts/MSVC.cmake
@@ -2,22 +2,34 @@ if (NOT $ENV{DevEnvDir})
  set(ENV{DevEnvDir} "C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE")
 endif()
 set(CMAKE_MAKE_PROGRAM $ENV{DevEnvDir}/CommonExtensions/Microsoft/CMake/Ninja/ninja.exe CACHE PATH "")
-add_compile_options(
-        /MP # cl.exe build with multiple processes
-        /utf-8 # set source and execution character sets to UTF-8
-        /bigobj # increase # of sections in object files
-        /permissive- # to allow alternative operators ("and", "or", "not")
-        /sdl- # disable additional security checks
-        /GS- # disable buffer security checks
-        /wd4068 # unknown pragma
-        /wd4146 # negate unsigned
-        /wd4819 # codepage?
-        /wd6237 # short-circuit eval
-        /wd6319 # a, b: unused a
-        /wd26444 # unnamed objects
-        /wd26451 # overflow
-        /wd26495 # uninitialized member
+set(CMAKE_CXX_FLAGS_INIT "\
+/MP /utf-8 /bigobj /permissive- /sdl- /FC /Gd /GS- /Gy \
+/wd4068 /wd4146 /wd4819 /wd6237 /wd6319 /wd26444 /wd26451 /wd26495 /WX- /W1 \
+/TP /Zc:forScope /Zc:inline /Zc:wchar_t"
 )
+# /MP     # cl.exe build with multiple processes
+# /utf-8  # set source and execution character sets to UTF-8
+# /bigobj # increase # of sections in object files
+# /permissive- # to allow alternative operators ("and", "or", "not")
+# /sdl-   # disable additional security checks
+# /FC     # full path in compiler messages
+# /Gd     # __cdecl
+# /GS-    # disable buffer security checks
+# /Gy     # Enable Function-Level Linking
+# /wd4068 # unknown pragma
+# /wd4146 # negate unsigned
+# /wd4819 # codepage?
+# /wd6237 # short-circuit eval
+# /wd6319 # a, b: unused a
+# /wd26444 # unnamed objects
+# /wd26451 # overflow
+# /wd26495 # uninitialized member
+# /WX- # (do not) Treat Warnings as Errors
+# /W1  # Warning Level
+# /TP  # every file is a C++ file
+# /Zc:forScope  # Force Conformance in for Loop Scope
+# /Zc:inline    # Remove unreferenced COMDAT
+# /Zc:wchar_t   # wchar_t Is Native Type
 add_compile_definitions(
     _SCL_SECURE_NO_WARNINGS
     _CTR_SECURE_NO_WARNINGS
