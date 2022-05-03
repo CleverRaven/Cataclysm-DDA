d7cef684b315d68ced8de91d04b94f35c6d89c66
jumjummju <felkeraaron@gmail.com>
2022-05-03 18:10:01 -0400

    Update tests

--- tests/effective_dps_test.cpp
+++ tests/effective_dps_test.cpp
@@ -445,16 +445,16 @@ static void check_two_handed_swords( const std::function<Approx( const std::stri
         CHECK( calc_expected_dps( "nodachi" ) == 26.5 );
         CHECK( calc_expected_dps( "zweihander" ) == 27.0 );
         CHECK( calc_expected_dps( "estoc" ) == 27.0 );
-        CHECK( calc_expected_dps( "longsword" ) == 24.25 );
+        CHECK( calc_expected_dps( "longsword" ) == 25 );
         CHECK( calc_expected_dps( "katana" ) == 25.0 );
-        CHECK( calc_expected_dps( "longsword_inferior" ) == 18.5 );
+        CHECK( calc_expected_dps( "longsword_inferior" ) == 15.5 );
         CHECK( calc_expected_dps( "zweihander_inferior" ) == 15.5 );
         CHECK( calc_expected_dps( "katana_inferior" ) == 13.5 );
         CHECK( calc_expected_dps( "nodachi_inferior" ) == 16.25 );
         CHECK( calc_expected_dps( "estoc_inferior" ) == 15.5 );
         CHECK( calc_expected_dps( "estoc_fake" ) == 10.0 );
         CHECK( calc_expected_dps( "zweihander_fake" ) == 9.5 );
-        CHECK( calc_expected_dps( "longsword_fake" ) == 9.0 );
+        CHECK( calc_expected_dps( "longsword_fake" ) == 6.25 );
         CHECK( calc_expected_dps( "nodachi_fake" ) == 8.5 );
         CHECK( calc_expected_dps( "katana_fake" ) == 7.0 );
     }
