e1f0fca46943a37672070f0350a0347a704c1f56
ducanishah <33822852+ducanishah@users.noreply.github.com>
2022-05-04 11:02:36 -0700

    Power Tools do tool things (#57239)
    Co-authored-by: Anton Burmistrov <Night_Pryanik@mail.ru>

--- src/item_factory.cpp
+++ src/item_factory.cpp
@@ -231,6 +231,14 @@ void Item_factory::finalize_pre( itype &obj )
             }
         }
     }
+    for( const auto &q : obj.charged_qualities ) {
+        for( const auto &u : q.first.obj().usages ) {
+            if( q.second >= u.first ) {
+                emplace_usage( obj.use_methods, u.second );
+                // I do not know how to get the ammo scale, so hopefully it naturally comes with the item's scale?
+            }
+        }
+    }
 
     if( obj.mod ) {
         std::string func = obj.gunmod ? "GUNMOD_ATTACH" : "TOOLMOD_ATTACH";
