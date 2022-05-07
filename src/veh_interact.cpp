efd9866b8bcd4159b4a1ee86e6bcf734393df50f
Faalagorn <faalagorn@gmail.com>
2022-05-07 18:19:52 +0200

    Add quote to "Airframe and Powerplant Mechanic"

--- src/veh_interact.cpp
+++ src/veh_interact.cpp
@@ -1269,7 +1269,7 @@ void veh_interact::do_repair()
         bool would_prevent_flying = veh->would_repair_prevent_flyable( pt, player_character );
         if( would_prevent_flying &&
             !player_character.has_proficiency( proficiency_prof_aircraft_mechanic ) ) {
-            nmsg += _( "\n<color_yellow>You require the Airframe and Powerplant Mechanics proficiency to repair this part safely!</color>\n\n" );
+            nmsg += _( "\n<color_yellow>You require the \"Airframe and Powerplant Mechanic\" proficiency to repair this part safely!</color>\n\n" );
         }
 
         const nc_color desc_color = pt.is_broken() ? c_dark_gray : c_light_gray;
