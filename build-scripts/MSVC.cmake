27d334ca55922634c1fb3f4454baaa4dbcce9f32
alef <alef@users.noreply.github.com>
2022-05-08 20:46:23 +0200

    Moving user presets hack into the include_before script
    One should not use CMAKE_SOURCE_DIR because changes during compiler features
    testing and wastes time too.
    Renaming the script because it's not more just looking for VCPKG.

--- build-scripts/MSVC.cmake
+++ build-scripts/MSVC.cmake
@@ -4,68 +4,6 @@ if (NOT "$ENV{DevEnvDir}")
 endif()
 # Ninja is provided by Microsoft but not in the Path
 set(CMAKE_MAKE_PROGRAM $ENV{DevEnvDir}CommonExtensions\\Microsoft\\CMake\\Ninja\\ninja.exe CACHE PATH "")
-# Run VsDevCmd.bat and set all environment variables it changes
-set(VSDEVCMD_BAT "$ENV{DevEnvDir}\\..\\Tools\\VsDevCmd.bat")
-cmake_path(NATIVE_PATH VSDEVCMD_BAT VSDEVCMD_BAT)
-execute_process(COMMAND cmd /c ${VSDEVCMD_BAT} -no_logo -arch=amd64 && set
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    OUTPUT_VARIABLE _ENV)
-string(REGEX REPLACE ";" "\\\\;" _ENV "${_ENV}")
-string(REGEX MATCHALL "[^\n]+\n" _ENV "${_ENV}")
-foreach(_env IN LISTS _ENV)
-string(REGEX MATCH ^[^=]+    _key   "${_env}")
-    string(REGEX MATCH =[^\n]+\n _value "${_env}")
-    string(SUBSTRING "${_value}" 1 -1 _value) # Remove = at begin
-    string(STRIP "${_value}" _value) # Remove \r
-	# This list is essentially a revised result of :comm -1 -3 <(sort before.txt) <(sort after.txt) |egrep -o '^[^=]+='"
-	foreach(_replace 
-		ExtensionSdkDir=
-		Framework40Version=
-		FrameworkDIR64=
-		FrameworkDir=
-		FrameworkVersion64=
-		FrameworkVersion=
-		INCLUDE=
-		LIB=
-		LIBPATH=
-		NETFXSDKDir=
-		Path=
-		UCRTVersion=
-		UniversalCRTSdkDir=
-		VCIDEInstallDir=
-		VCINSTALLDIR=
-		VCToolsInstallDir=
-		VCToolsRedistDir=
-		VCToolsVersion=
-		VS170COMNTOOLS=
-		VSCMD_ARG_HOST_ARCH=
-		VSCMD_ARG_TGT_ARCH=
-		VSCMD_ARG_app_plat=
-		VSCMD_VER=
-		VSINSTALLDIR=
-		VisualStudioVersion=
-		WindowsLibPath=
-		WindowsSDKLibVersion=
-		WindowsSDKVersion=
-		WindowsSDK_ExecutablePath_x64=
-		WindowsSDK_ExecutablePath_x86=
-		WindowsSdkBinPath=
-		WindowsSdkDir=
-		WindowsSdkVerBinPath=
-	)
-        if("${_key}=" STREQUAL "${_replace}")
-            set(ENV{${_key}} "${_value}")
-            string(REPLACE \\ \\\\ _value "${_value}")
-            set(_json_entry "\"${_key}\": \"${_value}\"")
-            if("${_MSVC_DEVENV}" STREQUAL "")
-                string(APPEND _MSVC_DEVENV "${_json_entry}")
-            else()
-                string(APPEND _MSVC_DEVENV ",\n${_json_entry}")
-            endif()
-        endif()
-    endforeach()
-endforeach()
-configure_file(build-scripts/CMakeUserPresets.json.in ${CMAKE_SOURCE_DIR}/CMakeUserPresets.json @ONLY)
 
 # Path has changed, so this configure run will find cl.exe
 set(CMAKE_C_COMPILER   cl.exe)
