d0c9f947005458f2df20922856088d658df0b892
stubkan <subnormalx@hotmail.com>
2022-05-13 16:15:45 +0100

    Fixed condition to only increase cost if below 10c threshold

--- src/npctrade.cpp
+++ src/npctrade.cpp
@@ -202,7 +202,7 @@ int npc_trading::trading_price( Character const &buyer, Character const &seller,
                 return VisitResponse::SKIP;
             }
         }
-        if( price <= 10 ) {
+        if( price < 10 ) {
             // Make sure nothing is sold for free
             ret += 10;
         }
