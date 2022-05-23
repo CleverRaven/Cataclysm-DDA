aaa642bb9c22ae48c948525c40fcf772d41ed5df
Valiant <Night_Pryanik@mail.ru>
2022-05-23 15:39:03 +0400

    Update mapgen.cpp

--- src/mapgen.cpp
+++ src/mapgen.cpp
@@ -6388,20 +6388,20 @@ vehicle *map::add_vehicle( const vgroup_id &type, const tripoint &p, const units
                            const int veh_fuel, const int veh_status, const bool merge_wrecks, const std::string &faction,
                            bool locked )
 {
-    return add_vehicle( type.obj().pick(), p, dir, veh_fuel, veh_status, merge_wrecks, faction );
+    return add_vehicle( type.obj().pick(), p, dir, veh_fuel, veh_status, merge_wrecks, faction, locked );
 }
 
 vehicle *map::add_vehicle( const vgroup_id &type, const point &p, const units::angle &dir,
                            int veh_fuel, int veh_status, bool merge_wrecks, const std::string &faction, bool locked )
 {
-    return add_vehicle( type.obj().pick(), p, dir, veh_fuel, veh_status, merge_wrecks, faction );
+    return add_vehicle( type.obj().pick(), p, dir, veh_fuel, veh_status, merge_wrecks, faction, locked );
 }
 
 vehicle *map::add_vehicle( const vproto_id &type, const point &p, const units::angle &dir,
                            int veh_fuel, int veh_status, bool merge_wrecks, const std::string &faction, bool locked )
 {
     return add_vehicle( type, tripoint( p, abs_sub.z() ), dir, veh_fuel, veh_status, merge_wrecks,
-                        faction );
+                        faction, locked );
 }
 
 vehicle *map::add_vehicle( const vproto_id &type, const tripoint &p, const units::angle &dir,
