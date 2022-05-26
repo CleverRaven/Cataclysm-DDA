f732f005267634b0f09b1d1d62ce2283c259584a
andrei <68240139+andrei8l@users.noreply.github.com>
2022-05-26 03:54:29 +0300

    explosion: fixup for defered explosions (#57897)

--- src/explosion.cpp
+++ src/explosion.cpp
@@ -481,7 +481,7 @@ void explosion( const tripoint &p, float power, float factor, bool fire,
 
 void explosion( const tripoint &p, const explosion_data &ex )
 {
-    _explosions.emplace_back( p, ex );
+    _explosions.emplace_back( get_map().getglobal( p ), ex );
 }
 
 void _make_explosion( const tripoint &p, const explosion_data &ex )
@@ -868,7 +868,7 @@ void resonance_cascade( const tripoint &p )
 void process_explosions()
 {
     for( const queued_explosion &ex : _explosions ) {
-        _make_explosion( ex.first, ex.second );
+        _make_explosion( get_map().getlocal( ex.first ), ex.second );
     }
     _explosions.clear();
 }
