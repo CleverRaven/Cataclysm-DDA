1223d3feb5eaaa0cafe4e09ebad7b01558faf687
alef <alef@users.noreply.github.com>
2022-05-26 17:44:10 +0200

    Optmize replacing ENV vars loop with find()

--- build-scripts/x64-release.cmake
+++ build-scripts/x64-release.cmake
@@ -38,61 +38,61 @@ if("$ENV{VSCMD_VER}" STREQUAL "")
     execute_process(COMMAND cmd /c ${VSDEVCMD_BAT} -no_logo -arch=amd64 && set
         OUTPUT_STRIP_TRAILING_WHITESPACE
         OUTPUT_VARIABLE _ENV)
+    # This list is essentially a revised result of: comm -1 -3 <(sort before.txt) <(sort after.txt) |egrep -o '^[^=]+='"
+    set(_replace 
+        ExtensionSdkDir=
+        Framework40Version=
+        FrameworkDIR64=
+        FrameworkDir=
+        FrameworkVersion64=
+        FrameworkVersion=
+        INCLUDE=
+        LIB=
+        LIBPATH=
+        NETFXSDKDir=
+        Path=
+        UCRTVersion=
+        UniversalCRTSdkDir=
+        VCIDEInstallDir=
+        VCINSTALLDIR=
+        VCToolsInstallDir=
+        VCToolsRedistDir=
+        VCToolsVersion=
+        VS170COMNTOOLS=
+        VSCMD_ARG_HOST_ARCH=
+        VSCMD_ARG_TGT_ARCH=
+        VSCMD_ARG_app_plat=
+        VSCMD_VER=
+        VSINSTALLDIR=
+        VisualStudioVersion=
+        WindowsLibPath=
+        WindowsSDKLibVersion=
+        WindowsSDKVersion=
+        WindowsSDK_ExecutablePath_x64=
+        WindowsSDK_ExecutablePath_x86=
+        WindowsSdkBinPath=
+        WindowsSdkDir=
+        WindowsSdkVerBinPath=
+    )
     string(REGEX REPLACE ";" "\\\\;" _ENV "${_ENV}")
     string(REGEX MATCHALL "[^\n]+\n" _ENV "${_ENV}")
     foreach(_env IN LISTS _ENV)
         string(REGEX MATCH ^[^=]+    _key   "${_env}")
         string(REGEX MATCH =[^\n]+\n _value "${_env}")
-        message(XX${_value}XX)
         string(SUBSTRING "${_value}" 1 -1 _value) # Remove = at begin
         string(STRIP "${_value}" _value) # Remove \r
-        # This list is essentially a revised result of: comm -1 -3 <(sort before.txt) <(sort after.txt) |egrep -o '^[^=]+='"
-        foreach(_replace 
-            ExtensionSdkDir=
-            Framework40Version=
-            FrameworkDIR64=
-            FrameworkDir=
-            FrameworkVersion64=
-            FrameworkVersion=
-            INCLUDE=
-            LIB=
-            LIBPATH=
-            NETFXSDKDir=
-            Path=
-            UCRTVersion=
-            UniversalCRTSdkDir=
-            VCIDEInstallDir=
-            VCINSTALLDIR=
-            VCToolsInstallDir=
-            VCToolsRedistDir=
-            VCToolsVersion=
-            VS170COMNTOOLS=
-            VSCMD_ARG_HOST_ARCH=
-            VSCMD_ARG_TGT_ARCH=
-            VSCMD_ARG_app_plat=
-            VSCMD_VER=
-            VSINSTALLDIR=
-            VisualStudioVersion=
-            WindowsLibPath=
-            WindowsSDKLibVersion=
-            WindowsSDKVersion=
-            WindowsSDK_ExecutablePath_x64=
-            WindowsSDK_ExecutablePath_x86=
-            WindowsSdkBinPath=
-            WindowsSdkDir=
-            WindowsSdkVerBinPath=
-        )
-            if("${_key}=" STREQUAL "${_replace}")
-                set(ENV{${_key}} "${_value}")
-                string(REPLACE \\ \\\\ _value "${_value}")
-                set(_json_entry "\"${_key}\": \"${_value}\"")
-                if("${_MSVC_DEVENV}" STREQUAL "")
-                    string(APPEND _MSVC_DEVENV "${_json_entry}")
-                else()
-                    string(APPEND _MSVC_DEVENV ",\n${_json_entry}")
-                endif()
+        list(FIND _replace ${_key}= _idx)
+        if(NOT -1 EQUAL _idx)
+            list(REMOVE_AT _replace ${_idx})
+            set(ENV{${_key}} "${_value}")
+            string(REPLACE \\ \\\\ _value "${_value}")
+            set(_json_entry "\"${_key}\": \"${_value}\"")
+            if("${_MSVC_DEVENV}" STREQUAL "")
+                string(APPEND _MSVC_DEVENV "${_json_entry}")
+            else()
+                string(APPEND _MSVC_DEVENV ",\n${_json_entry}")
             endif()
-        endforeach() # replace
+        endif()
     endforeach() # _ENV
 endif() # VSCMD_VER
 
