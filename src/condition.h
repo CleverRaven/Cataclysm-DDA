6fbd53208bb75120e2eef030c6cb37532ca29ffb
Ramza13 <52087122+Ramza13@users.noreply.github.com>
2022-05-29 23:17:26 -0400

    Update condition.h

--- src/condition.h
+++ src/condition.h
@@ -223,7 +223,7 @@ var_info read_var_info( JsonObject jo, bool require_default );
 void write_var_value( var_type type, std::string name, talker *talk, std::string value );
 template<class T>
 std::string get_talk_varname( const JsonObject &jo, const std::string &member,
-                              bool check_value, int_or_var<dialogue> &default_val );
+                              bool check_value, int_or_var<T> &default_val );
 // the truly awful declaration for the conditional_t loading helper_function
 template<class T>
 void read_condition( const JsonObject &jo, const std::string &member_name,
