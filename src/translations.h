7385a790be86c760ac4966afd4c63b75666e39b8
John Bytheway <52664+jbytheway@users.noreply.github.com>
2022-06-05 16:27:55 -0400

    Avoid reserved identifiers
    There are three clang-tidy checks which are looking for use of reserved
    identifiers.  Enable all of them and fix or suppress the associated
    warnings.
    
    The reserved identifiers we had fall into three categories:
    - Names containing "__"
    - Names starting with "_" followed by a capital letter.
    - Names starting with "_" in the global namespace.
    
    Most of those could be automatically fixed by clang-tidy.
    
    One was also affected by the cata-static-string_id-constants check,
    which needed to be updated so they wouldn't conflict.
    
    Two cases were erroneous and needed suppression.
    
    The _ macro we use for translation is a reserved identifier, but we're
    going to use it anyway.  So that is also suppressed.

--- src/translations.h
+++ src/translations.h
@@ -48,6 +48,10 @@ inline const T &translation_argument_identity( const T &t )
 }
 
 // Note: in case of std::string argument, the result is copied, this is intended (for safety)
+// Note that _ triggers reserved identifier warnings, but we suppress all
+// three because it's a common use of _ and thus not likely to be a problem in
+// practice.
+// NOLINTNEXTLINE(bugprone-reserved-identifier,cert-dcl37-c,cert-dcl51-cpp)
 #define _( msg ) \
     ( ( []( const auto & arg ) { \
         static auto cache = detail::get_local_translation_cache( arg ); \
