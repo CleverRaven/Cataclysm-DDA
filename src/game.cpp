ceb72128e452fe957e96883e340edc4b18f64468
Willenbrink <sebastian.willenbrink@gmail.com>
2022-06-06 11:28:38 +0200

    Update src/game.cpp
    Co-authored-by: David Seguin <davidseguin@live.ca>

--- src/game.cpp
+++ src/game.cpp
@@ -4836,7 +4836,7 @@ bool game::is_sheltered( const tripoint &p )
 
     return !m.is_outside( p ) ||
            p.z < 0 ||
-           !is_inside;
+           is_inside;
 }
 
 bool game::revive_corpse( const tripoint &p, item &it )
