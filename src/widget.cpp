818e694f7209a14d8501c8382f51f00694368805
Inglonias <2125926+Inglonias@users.noreply.github.com>
2022-06-09 15:01:58 -0400

    Fixed failing test cases

--- src/widget.cpp
+++ src/widget.cpp
@@ -1397,9 +1397,9 @@ std::string widget::layout( const avatar &ava, const unsigned int max_width, int
             // Number of spaces between columns
             const int col_padding = 2;
             // Subtract column padding to get space available for widgets
-            int avail_width = max_width - col_padding * ( num_widgets - 1 );
+            const int avail_width = max_width - col_padding * ( num_widgets - 1 );
             // Divide available width equally among all widgets
-            int child_width = avail_width / num_widgets;
+            const int child_width = avail_width / num_widgets;
             // Keep remainder to distribute
             int remainder = avail_width % num_widgets;
             // Store the (potentially) multi-row text for each column
@@ -1413,16 +1413,14 @@ std::string widget::layout( const avatar &ava, const unsigned int max_width, int
                 if( cur_child._style == "layout" && cur_child._width > 1 ) {
                     cur_width = cur_child._width;
                 }
-                total_width += cur_width + col_padding;
-                avail_width = max_width - total_width;
-                child_width = avail_width / ( num_widgets - widgets_placed );
-                if( avail_width < 1 ) {
-                    debugmsg( "widget layout is wider than sidebar allows." );
-                }
                 // Spread remainder over the first few columns
                 if( remainder > 0 ) {
                     cur_width += 1;
                     remainder -= 1;
+                }
+                total_width += cur_width;
+                if (total_width > avail_width) {
+                    debugmsg("widget layout is wider than sidebar allows.");
                 }
                 // Layout child in this column
                 const std::string txt = cur_child.layout( ava, cur_width, label_width );
