3a7ea1243405b06d3de9b80af1d1bdc3708d7e29
Anton Burmistrov <Night_Pryanik@mail.ru>
2022-06-12 12:32:11 +0400

    Pain and attacked

--- src/character.cpp
+++ src/character.cpp
@@ -1214,7 +1214,7 @@ void Character::react_to_felt_pain( int intensity )
     if( intensity <= 0 ) {
         return;
     }
-    if( is_avatar() && intensity >= 2 ) {
+    if( get_option<bool>( "DISTRACTION_PAIN" ) && is_avatar() && intensity >= 2 ) {
         g->cancel_activity_or_ignore_query( distraction_type::pain, _( "Ouch, something hurts!" ) );
     }
     // Only a large pain burst will actually wake people while sleeping.
@@ -7520,7 +7520,7 @@ void Character::on_hurt( Creature *source, bool disturb /*= true*/ )
         if( has_effect( effect_sleep ) && !has_bionic( bio_sleep_shutdown ) ) {
             wake_up();
         }
-        if( !is_npc() && !has_effect( effect_narcosis ) ) {
+        if( get_option<bool>( "DISTRACTION_ATTACKED" ) && !is_npc() && !has_effect( effect_narcosis ) ) {
             if( source != nullptr ) {
                 if( sees( *source ) ) {
                     g->cancel_activity_or_ignore_query( distraction_type::attacked,
