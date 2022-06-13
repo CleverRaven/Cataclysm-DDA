93330c70677ae0bd9c48ad10b7ad2cec28c65fb4
Ofek <ofeklavie@gmail.com>
2022-06-13 17:05:16 +0300

    Fix stupid heap-o

--- src/mission_start.cpp
+++ src/mission_start.cpp
@@ -143,11 +143,11 @@ void mission_start::kill_nemesis( mission * )
 
     size_t attempt = 0;
     do {
-        int range = rng( 40, 80 ) * attempts_multipliers[attempt];
         if( ++attempt >= attempts_multipliers.size() ) {
             debugmsg( "Failed adding a nemesis mission" );
             return;
         }
+        int range = rng(40, 80) * attempts_multipliers[attempt - 1];
         site = overmap_buffer.find_random( center, "field", range, false );
     } while( site == overmap::invalid_tripoint );
     overmap_buffer.add_nemesis( site );
