3c58aee95d7ceb5097609d1a8c39eb1eb1722bab
tornikeo <tornikeonoprishvili@gmail.com>
2022-06-17 22:07:01 +0400

    Add info for brawler ranged use refusal

--- src/ranged.cpp
+++ src/ranged.cpp
@@ -3706,7 +3706,7 @@ bool gunmode_checks_common( avatar &you, const map &m, std::vector<std::string> 
 {
     bool result = true;
     if( you.has_trait( trait_BRAWLER ) ) {
-        messages.push_back( string_format( _( "You refuse to fire your %s." ),
+        messages.push_back( string_format( _( "Pfft. You are a brawler; Using %s is beneath you." ),
                                            gmode->tname() ) );
         result = false;
     }
