2661ab349320043aebe56c344220a7783961e337
Aaron Chancey <abchiptop@gmail.com>
2022-06-22 15:48:40 -0400

    Lint and astyle
    might as well

--- src/init.cpp
+++ src/init.cpp
@@ -264,7 +264,7 @@ void DynamicDataLoader::initialize()
     add( "bionic", &bionic_data::load_bionic );
     add( "profession", &profession::load_profession );
     add( "profession_item_substitutions", &profession::load_item_substitutions );
-    add( "loadout", &loadout::load_loadout);
+    add( "loadout", &loadout::load_loadout );
     add( "proficiency", &proficiency::load_proficiencies );
     add( "proficiency_category", &proficiency_category::load_proficiency_categories );
     add( "speed_description", &speed_description::load_speed_descriptions );
