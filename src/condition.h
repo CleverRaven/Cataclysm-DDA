b2852ac119f6ad25cb70f7f0b4d3cfc9bf9c55c7
Ramza13 <52087122+Ramza13@users.noreply.github.com>
2022-06-27 22:58:51 -0400

    Update condition.h

--- src/condition.h
+++ src/condition.h
@@ -202,7 +202,7 @@ duration_or_var_part<T> get_duration_or_var_part( const JsonValue &jv, std::stri
         bool required = true,
         time_duration default_val = 0_seconds );
 template<class T>
-const tripoint_abs_ms get_tripoint_from_var( cata::optional<var_info> var, const T &d );
+tripoint_abs_ms get_tripoint_from_var( cata::optional<var_info> var, const T &d );
 var_info read_var_info( const JsonObject &jo, bool require_default );
 void write_var_value( var_type type, std::string name, talker *talk, std::string value );
 template<class T>
