7854212ce4f1a8cfcd16e8b8b24a81207054af1f
Hirmuolio <hirmuolio@gmail.com>
2022-06-30 00:00:49 +0300

    unused converts

--- src/cata_utility.h
+++ src/cata_utility.h
@@ -199,20 +199,6 @@ double convert_velocity( int velocity, units_type vel_units );
 double temp_to_celsius( double fahrenheit );
 
 /**
- * Convert a temperature from degrees Fahrenheit to Kelvin.
- *
- * @return Temperature in degrees K.
- */
-double temp_to_kelvin( double fahrenheit );
-
-/**
- * Convert a temperature from degrees Celsius to Kelvin.
- *
- * @return Temperature in degrees K.
- */
-double celsius_to_kelvin( double celsius );
-
-/**
  * Convert a temperature from Kelvin to degrees Fahrenheit.
  *
  * @return Temperature in degrees C.
