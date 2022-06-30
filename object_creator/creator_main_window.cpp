bf669460c07917445413656cb575c164640cc40f
snipercup <50166150+snipercup@users.noreply.github.com>
2022-06-30 23:27:21 +0200

    Added mods selection box

--- object_creator/creator_main_window.cpp
+++ object_creator/creator_main_window.cpp
@@ -3,6 +3,8 @@
 #include "spell_window.h"
 #include "enum_conversions.h"
 #include "translations.h"
+#include "worldfactory.h"
+#include "mod_manager.h"
 
 #include <QtWidgets/qapplication.h>
 #include <QtWidgets/qmainwindow.h>
@@ -28,22 +30,79 @@ namespace io
 
 int creator::main_window::execute( QApplication &app )
 {
-    QMainWindow title_menu;
+    const int default_text_box_height = 20;
+    const int default_text_box_width = 100;
+    const QSize default_text_box_size(default_text_box_width, default_text_box_height);
 
+    int row = 0;
+    int col = 0;
+    int max_row = 0;
+    int max_col = 0;
+
+    QMainWindow title_menu;
     spell_window spell_editor( &title_menu );
 
-    QPushButton spell_button( _( "Spell Creator" ), &title_menu );
+    //Does nothing on it's own but once settings.setvalue() is called it will create
+    //an ini file in C:\Users\User\AppData\Roaming\CleverRaven or equivalent directory
+    QSettings settings(QSettings::IniFormat, QSettings::UserScope,
+        "CleverRaven", "Cataclysm - DDA");
 
+
+    // =========================================================================================
+    // first column of boxes
+
+    QLabel mods_label;
+    mods_label.setParent( &title_menu );
+    mods_label.setText( QString( "Select mods (restart required):" ) );
+    mods_label.resize( default_text_box_size*2 );
+    mods_label.move( QPoint( col * default_text_box_width, row++ * default_text_box_height ) );
+    mods_label.show();
+    row++;
+
+    QStringList all_mods;
+    for( const mod_id& e : world_generator->get_mod_manager().all_mods() ) {
+        if ( !e->obsolete && e->ident.str() != "dda" ) {
+            all_mods.append(e->ident.c_str());
+        }
+    }
+
+    dual_list_box mods_box;
+    mods_box.initialize( all_mods );
+    mods_box.resize( QSize( default_text_box_width * 4, default_text_box_height * 6 ) );
+    mods_box.setParent( &title_menu );
+    mods_box.move( QPoint( col * default_text_box_width, row * default_text_box_height ) );
+    mods_box.show();
+    QObject::connect( &mods_box, &dual_list_box::pressed, [&]() {
+            settings.setValue( "mods/include", mods_box.get_included() );
+        });
+
+    if( settings.contains("mods/include") ) {
+        QStringList modlist = settings.value( "mods/include" ).value<QStringList>();
+        mods_box.set_included( modlist );
+    }
+
+    row += 7;
+
+    QPushButton spell_button( _( "Spell Creator" ), &title_menu );
+    spell_button.move( QPoint( col * default_text_box_width, row * default_text_box_height ) );
     QObject::connect( &spell_button, &QPushButton::released,
-    [&]() {
-        title_menu.hide();
-        spell_editor.show();
-    } );
+        [&]() {
+            title_menu.hide();
+            spell_editor.show();
+        });
+
+
 
     title_menu.show();
     spell_button.show();
 
-    QSettings settings("CleverRaven", "Cataclysm - DDA");
+    row += 3;
+    col += 3;
+    max_row = std::max(max_row, row);
+    max_col = std::max(max_col, col);
+    title_menu.resize(QSize((max_col + 1) * default_text_box_width,
+        (max_row)*default_text_box_height));
+
 
     return app.exec();
 }
