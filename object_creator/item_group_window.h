8a4e0ccec143db23488cc84aed24eb31060db7c4
snipercup <50166150+snipercup@users.noreply.github.com>
2022-07-08 21:25:52 +0200

    Add entrieslist and distributioncollection classes
    They are used in new commits down the line

--- object_creator/item_group_window.h
+++ object_creator/item_group_window.h
@@ -1,26 +1,26 @@
 #ifndef CATA_OBJECT_CREATOR_ITEM_GROUP_WINDOW_H
 #define CATA_OBJECT_CREATOR_ITEM_GROUP_WINDOW_H
 
-#include "dual_list_box.h"
+#include "listwidget_drag.h"
 #include "item_group.h"
 
-#include <QtWidgets/qcheckbox.h>
-#include <QtWidgets/qcombobox.h>
-#include <QtWidgets/qgridlayout.h>
+#include "QtWidgets/qlistwidget.h"
 #include <QtWidgets/qlineedit.h>
-#include <QtWidgets/qlistwidget.h>
+#include "QtWidgets/qgridlayout.h"
 #include <QtWidgets/qmainwindow.h>
+#include "QtWidgets/qlabel.h"
 #include <QtWidgets/qplaintextedit.h>
-#include <QtWidgets/qspinbox.h>
 #include <QtWidgets/qtablewidget.h>
+#include "QtWidgets/qpushbutton.h"
 
 namespace creator
 {
-class item_group_window : public QMainWindow
-{
+    class item_group_window : public QMainWindow
+    {
 
     public:
-        item_group_window( QWidget *parent = nullptr, Qt::WindowFlags flags = Qt::WindowFlags() );
+        item_group_window( QWidget* parent = nullptr, Qt::WindowFlags flags = Qt::WindowFlags() );
+        void entries_box_content_changed();
 
         void show() {
             item_group_json.show();
@@ -37,30 +37,62 @@ class item_group_window : public QMainWindow
         QTextEdit item_group_json;
 
         void write_json();
-        void deleteEntriesLine();
         void items_search_return_pressed();
         void group_search_return_pressed();
         void group_list_populate_filtered( std::string searchQuery = "" );
         void item_list_populate_filtered( std::string searchQuery = "" );
-        void entries_add_item( QListWidgetItem* cur_widget, bool group );
-        void set_item_tooltip(QListWidgetItem* new_item, item tmpItem);
-        void selected_item_doubleclicked();
-        void selected_group_doubleclicked();
+        void set_item_tooltip( QListWidgetItem* new_item, item tmpItem );
 
         QLabel id_label;
         QLineEdit id_box;
 
         QLabel item_search_label;
         QLineEdit item_search_box;
-        QListWidget item_list_total_box;
+        ListWidget_Drag item_list_total_box;
 
         QLabel group_search_label;
         QLineEdit group_search_box;
-        QListWidget group_list_total_box;
+        ListWidget_Drag group_list_total_box;
 
-        QTableWidget entries_box;
+        //Top-level frame to hold the distributionCollections and/or entrieslist
+        QFrame* entries_box;
 
-};
+
+    protected:
+        bool event( QEvent* event ) override;
+    };
+
+    //Holds the most detailed data, a list of items and spawn properties
+    class entriesList : public QTableWidget
+    {
+    public:
+        explicit entriesList( QWidget* parent = nullptr );
+        void add_item( QString itemText, bool group );
+        void deleteEntriesLine();
+
+    protected:
+        void dragEnterEvent( QDragEnterEvent* event ) override;
+        void dragMoveEvent( QDragMoveEvent* event ) override;
+        void dropEvent( QDropEvent* event ) override;
+    };
+
+    //Holds the properties for either a collection or a distribution
+    //May contain entriesLists
+    class distributionCollection : public QFrame
+    {
+    public:
+        explicit distributionCollection( QWidget* parent = nullptr );
+    };
+
+    class item_group_changed : public QEvent
+    {
+    public:
+        item_group_changed() : QEvent( registeredType() ) { }
+        static QEvent::Type eventType;
+
+    private:
+        static QEvent::Type registeredType();
+    };
 }
 
 #endif
