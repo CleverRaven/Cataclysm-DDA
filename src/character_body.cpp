5afbe235a715577c87f11f0047303b94992e10e0
I-am-Erk <45136638+I-am-Erk@users.noreply.github.com>
2022-07-14 15:14:34 -0700

    Change cardio_accumulator to daily cardio counter in health checks

--- src/character_body.cpp
+++ src/character_body.cpp
@@ -270,30 +270,36 @@ void Character::update_body( const time_point &from, const time_point &to )
     // Cardio related health stuff
     if( calendar::once_every( 1_days ) ) {
         // reset timer on getting a bonus for reaching half stamina
-        half_thresh_not_reached = true;
+        set_value( "half_thresh_not_reached", "true" );
+        
+        const int cardio_fit = get_cardiofit();
+        const int last_24h_kcal = calorie_diary.front().spent;
 
-        int cardio_accumultor = get_cardio_acc();
-        if( cardio_accumultor > 0 ) {
+        int cardio_check = 0;
+        if( cardio_fit > last_24h_kcal ) {
+            cardio_check = -std::sqrt( cardio_fit - last_24h_kcal );
+        } else if( last_24h_kcal > cardio_fit ) {
+            cardio_check = std::sqrt( last_24h_kcal - cardio_fit );
+        }
+        
+        if( cardio_check > 0 ) {
             mod_daily_health( 1, 200 );
-            if( cardio_accumultor >= 10 ) {
+            if( cardio_check >= 10 ) {
                 mod_daily_health( 1, 200 );
             }
         }
-        if( cardio_accumultor < 0 ) {
+        if( cardio_check < 0 ) {
             mod_daily_health( -1, -200 );
             // not getting below 75% stamina even once in a whole day is not healthy
-            // however if your cardio accumulator is positive we assume you still were physically active enough
+            // however if your cardio effect is positive we assume you still were physically active
             if( get_value( "lowered_stam" ).empty() ) {
                 mod_daily_health( -1, -200 );
             } else {
-            remove_value( "lowered_stam" );
+                remove_value( "lowered_stam" );
             }
-            if( cardio_accumultor <= -10 ) {
+            if( cardio_check <= -10 ) {
                 mod_daily_health( -1, -200 );
             }
-        }
-        if( cardio_accumultor >= get_bmr() / 2 ) {
-            mod_daily_health( 2, 200 );
         }
     }
 
