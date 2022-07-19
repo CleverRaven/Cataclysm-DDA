8521d5103ce712030e19a1d38f5b55d626e235df
Inglonias <2125926+Inglonias@users.noreply.github.com>
2022-07-19 12:32:30 -0400

    Revert "More infrastructure"
    This reverts commit 98ad3576df47c1022d68127ec6ac1790ec8338af.

--- src/event.h
+++ src/event.h
@@ -436,9 +436,8 @@ struct event_spec<event_type::crosses_mycus_threshold> : event_spec_character {}
 
 template<>
 struct event_spec<event_type::custom_json_event> {
-    static constexpr std::array<std::pair<const char *, cata_variant_type>, 2> fields = {{
+    static constexpr std::array<std::pair<const char *, cata_variant_type>, 1> fields = {{
             { "id", cata_variant_type::string },
-            { "value", cata_variant_type::int_ }
         }
     };
 };
