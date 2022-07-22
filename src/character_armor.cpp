ace1e1a280971385a9f3854c86da173038398b59
Stadler76 <stadli@gmx.de>
2022-07-22 02:07:16 +0200

    Include SEESLEEP in FROG_EYES

--- src/character_armor.cpp
+++ src/character_armor.cpp
@@ -11,7 +11,7 @@
 
 static const bionic_id bio_ads( "bio_ads" );
 
-static const trait_id trait_SEESLEEP( "SEESLEEP" );
+static const json_character_flag json_flag_SEESLEEP( "SEESLEEP" );
 
 bool Character::can_interface_armor() const
 {
@@ -202,7 +202,7 @@ int Character::get_env_resist( bodypart_id bp ) const
         }
     }
 
-    if( bp == body_part_eyes && has_trait( trait_SEESLEEP ) ) {
+    if( bp == body_part_eyes && has_flag( json_flag_SEESLEEP ) ) {
         ret += 8;
     }
     return ret;
