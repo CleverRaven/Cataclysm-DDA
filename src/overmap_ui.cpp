b68dbc0e220678caeccccba00638c332f0b045f5
John Bytheway <52664+jbytheway@users.noreply.github.com>
2022-07-23 08:55:20 -0400

    Add mongroup debug info to overmap editor
    To help debug mongroup issues, add debug output on the overmap editor
    with various info about each mongroup on the OMT.

--- src/overmap_ui.cpp
+++ src/overmap_ui.cpp
@@ -1088,6 +1088,8 @@ static void draw_om_sidebar(
     }
 
     if( ( data.debug_editor && center_seen ) || data.debug_info ) {
+        mvwprintz( wbar, point( 1, ++lines ), c_white,
+                   "abs_omt: %s", center.to_string() );
         const oter_t &oter = overmap_buffer.ter( center ).obj();
         mvwprintz( wbar, point( 1, ++lines ), c_white, "oter: %s (rot %d)", oter.id.str(),
                    oter.get_rotation() );
@@ -1117,6 +1119,15 @@ static void draw_om_sidebar(
                 mvwprintz( wbar, point( 1, ++lines ), c_white, "join %s: %s",
                            io::enum_to_string( dir ), *join );
             }
+        }
+
+        for( const mongroup *mg : overmap_buffer.monsters_at( center ) ) {
+            mvwprintz( wbar, point( 1, ++lines ), c_red, "mongroup %s (%zu/%u), %s %s%s%s",
+                       mg->type.str(), mg->monsters.size(), mg->population,
+                       mg->horde_behaviour,
+                       mg->dying ? "x" : "", mg->horde ? "h" : "", mg->diffuse ? "d" : "" );
+            mvwprintz( wbar, point( 1, ++lines ), c_red, "target: %s (%d)",
+                       project_to<coords::omt>( mg->target ).to_string(), mg->interest );
         }
     }
 
