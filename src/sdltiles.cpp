6681c9d24341a76927cf54f5522ed8916578184c
needleye <needleye@gmx.com>
2022-07-28 13:57:45 -0400

    astyle

--- src/sdltiles.cpp
+++ src/sdltiles.cpp
@@ -1752,13 +1752,10 @@ static int sdl_keysym_to_curses( const SDL_Keysym &keysym )
 
     const std::string vert_mode = get_option<std::string>( "VERT_MOVE_WITH_MODIFIERS_MODE" );
 
-    if ( vert_mode == "on" )
-    {
+    if( vert_mode == "on" ) {
         // Shift + Up/Down Cursor Arrow (Ascend / Descend)
-        if ( keysym.mod & KMOD_SHIFT )
-        {
-            switch ( keysym.sym )
-            {
+        if( keysym.mod & KMOD_SHIFT ) {
+            switch( keysym.sym ) {
             case SDLK_UP:
                 return inp_mngr.get_first_char_for_action( "LEVEL_UP" );
             case SDLK_DOWN:
@@ -1769,13 +1766,10 @@ static int sdl_keysym_to_curses( const SDL_Keysym &keysym )
 
     const std::string cycle_mod_mode = get_option<std::string>( "CHANGE_MOVEMENT_WITH_MODIFIERS_MODE" );
 
-    if ( cycle_mod_mode == "on" )
-    {
+    if( cycle_mod_mode == "on" ) {
         // Ctrl + Up/Down Cursor Arrow (Change movement mode)
-        if ( keysym.mod & KMOD_CTRL )
-        {
-            switch ( keysym.sym )
-            {
+        if( keysym.mod & KMOD_CTRL ) {
+            switch( keysym.sym ) {
             case SDLK_UP:
                 return inp_mngr.get_first_char_for_action( "cycle_move" );
             case SDLK_DOWN:
