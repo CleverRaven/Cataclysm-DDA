94e162ea8ecf7312f071c20e7378f85d05ccc054
Son Phan Trung <phantrungson17@gmail.com>
2022-08-04 21:06:27 +0700

    REMOVE SKILL RUST FROM GAME OPTIONS

--- src/character.cpp
+++ src/character.cpp
@@ -3224,9 +3224,6 @@ void Character::apply_skill_boost()
 
 void Character::do_skill_rust()
 {
-    if( get_option<std::string>( "SKILL_RUST" ) == "off" ) {
-        return;
-    }
     for( std::pair<const skill_id, SkillLevel> &pair : *_skills ) {
         const Skill &aSkill = *pair.first;
         SkillLevel &skill_level_obj = pair.second;
