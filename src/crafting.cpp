c089839983d7918b5e39e63d6b8a84f896cec506
I-am-Erk <45136638+I-am-Erk@users.noreply.github.com>
2022-08-11 10:02:40 -0700

    Update src/crafting.cpp

--- src/crafting.cpp
+++ src/crafting.cpp
@@ -1092,7 +1092,6 @@ double Character::crafting_success_roll( const recipe &making ) const
     float summed_skills = player_weighted_skill_average + npc_helpers_weighted_average;
     if( summed_skills < 0 ) {
         negative = true;
-        summed_skills *= -1;
     }
     float weighted_skill_average = sqrt( summed_skills );
     if( negative ) {
