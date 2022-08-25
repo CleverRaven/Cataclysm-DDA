8ad78cccd45427828aacbabd9d45aa0a27efb115
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2022-08-25 14:44:58 -0700

    Update character.cpp

--- src/character.cpp
+++ src/character.cpp
@@ -3706,9 +3706,9 @@ void Character::calc_bmi_encumb( std::map<bodypart_id, encumbrance_data> &vals )
 {
     //if BMI > minimum BMI for the limb to receive penalty encumbrance, multiply that by the scalar value per point of BMI to get total penalty
     for( std::pair<const bodypart_id, encumbrance_data> &val : vals ) {
-        if( val.get_bmi_encumbrance_scalar() > 0 ) {
-            int penalty = std::floor( val.get_bmi_encumbrance_scalar() * ( std::max( 0,
-                                      ( get_bmi() - val.get_bmi_encumbrance_threshold() ) ) ) );
+        if( val.first.get_bmi_encumbrance_scalar() > 0 ) {
+            int penalty = std::floor( val.first.get_bmi_encumbrance_scalar() * ( std::max( 0,
+                                      ( get_bmi() - val.first.get_bmi_encumbrance_threshold() ) ) ) );
             val.second.encumbrance += penalty;
         }
     }
@@ -5864,11 +5864,10 @@ units::mass Character::bodyweight() const
 
 units::mass Character::bodyweight_fat() const
 {
-    return std::max( 0, ( ( get_bmi() * std::pow( height() / 100.0f,
-                            2 ) ) - ( 25 * std::pow( height() / 100.0f, 2 ) ) );
+    return std::max( 0, ( ( get_bmi() * std::pow( height() / 100.0f, 2 ) ) - ( 25 * std::pow( height() / 100.0f, 2 ) ) ) );
 }
 
-       units::mass Character::bionics_weight() const
+units::mass Character::bionics_weight() const
 {
     units::mass bio_weight = 0_gram;
     for( const bionic_id &bid : get_bionics() ) {
