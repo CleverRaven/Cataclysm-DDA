1e8290fb1ae0b5943011dd4528067c9383aac392
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2022-08-25 15:21:12 -0700

    Update character.cpp

--- src/character.cpp
+++ src/character.cpp
@@ -3706,9 +3706,9 @@ void Character::calc_bmi_encumb( std::map<bodypart_id, encumbrance_data> &vals )
 {
     //if BMI > minimum BMI for the limb to receive penalty encumbrance, multiply that by the scalar value per point of BMI to get total penalty
     for( std::pair<const bodypart_id, encumbrance_data> &val : vals ) {
-        if( val.first.get_bmi_encumbrance_scalar() > 0 ) {
-            int penalty = std::floor( val.first.get_bmi_encumbrance_scalar() * ( std::max( 0,
-                                      ( get_bmi() - val.first.get_bmi_encumbrance_threshold() ) ) ) );
+        if( val.second.get_bmi_encumbrance_scalar() > 0 ) {
+            int penalty = std::floor( val.second.get_bmi_encumbrance_scalar() * ( std::max( 0,
+                                      ( get_bmi() - val.second.get_bmi_encumbrance_threshold() ) ) ) );
             val.second.encumbrance += penalty;
         }
     }
