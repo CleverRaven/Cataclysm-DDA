13da744e071ef3d1b6c8a44bafaf8069cb5928e5
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2022-08-26 15:09:20 -0700

    Update character.cpp

--- src/character.cpp
+++ src/character.cpp
@@ -1269,8 +1269,9 @@ int Character::swim_speed() const
     float hand_bonus_mult = ( usable.test( body_part_hand_l ) ? 0.5f : 0.0f ) +
                             ( usable.test( body_part_hand_r ) ? 0.5f : 0.0f );
 
-    // base swim speed. don't include bodyweight_fat() in the equation since it is bouyant, and if your speed gets too low you drown
-    ret = ( 440 * mutation_value( "movecost_swim_modifier" ) ) + weight_carried() /
+    // base swim speed. weight carried usually includes bodyweight_fat() but cancel it out here since fat is bouyant to avoid drowning from it being too low
+    ret = ( 440 * mutation_value( "movecost_swim_modifier" ) ) + ( weight_carried() -
+            bodyweight_fat() ) /
           ( 60_gram / mutation_value( "movecost_swim_modifier" ) ) - 50 * get_skill_level( skill_swimming );
     /** @EFFECT_STR increases swim speed bonus from PAWS */
     if( has_trait( trait_PAWS ) ) {
@@ -2736,7 +2737,7 @@ units::mass Character::weight_carried() const
     if( cached_weight_carried ) {
         return *cached_weight_carried;
     }
-    cached_weight_carried = weight_carried_with_tweaks( item_tweaks() );
+    cached_weight_carried = bodyweight_fat() + weight_carried_with_tweaks( item_tweaks() );
     return *cached_weight_carried;
 }
 
@@ -6305,7 +6306,7 @@ void Character::mod_stamina( int mod )
 void Character::burn_move_stamina( int moves )
 {
     int overburden_percentage = 0;
-    units::mass current_weight = weight_carried() + bodyweight_fat();
+    units::mass current_weight = weight_carried();
     // Make it at least 1 gram to avoid divide-by-zero warning
     units::mass max_weight = std::max( weight_capacity(), 1_gram );
     if( current_weight > max_weight ) {
@@ -10653,8 +10654,8 @@ void Character::recalc_speed_bonus()
     if( weight_cap < 1_milligram ) {
         weight_cap = 1_milligram; //Prevent Clang warning about divide by zero
     }
-    if( weight_carried() + bodyweight_fat() > weight_cap ) {
-        carry_penalty = 25 * ( weight_carried() + bodyweight_fat() - weight_cap ) / weight_cap;
+    if( weight_carried() > weight_cap ) {
+        carry_penalty = 25 * ( weight_carried() - weight_cap ) / weight_cap;
     }
     mod_speed_bonus( -carry_penalty );
 
