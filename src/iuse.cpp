87a598f7df51d6ea740402da9e001cd94a0cead4
Hirmuolio <22011552+Hirmuolio@users.noreply.github.com>
2022-08-29 06:02:23 +0300

    fix cables (#60516)

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -8889,7 +8889,8 @@ cata::optional<int> iuse::cable_attach( Character *p, item *it, bool, const trip
                                       source_veh->name, target_veh->name );
             }
 
-            return 1; // Let the cable be destroyed.
+            p->i_rem( it );
+            return 0; // Let the cable be destroyed.
         }
     }
 
@@ -9020,8 +9021,8 @@ cata::optional<int> iuse::cord_attach( Character *p, item *it, bool, const tripo
                 p->add_msg_if_player( m_good, _( "You link up the electric systems of the %1$s and the %2$s." ),
                                       source_veh->name, target_veh->name );
             }
-
-            return 1; // Let the cable be destroyed.
+            p->i_rem( it );
+            return 0; // Let the cable be destroyed.
         }
     }
 
