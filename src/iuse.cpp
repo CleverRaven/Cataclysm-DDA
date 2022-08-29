4b15e3761289005becd1323784e0089e98d6cff6
Hirmuolio <22011552+Hirmuolio@users.noreply.github.com>
2022-08-29 23:30:52 +0300

    power_draw in energy (#60484)
    * power_draw in energy
    
    * mods
    
    * typo
    
    * no _
    
    * no _
    
    * anothersimulacrum

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -8408,8 +8408,8 @@ cata::optional<int> iuse::multicooker( Character *p, item *it, bool t, const tri
                     mealtime = meal->time_to_craft_moves( *p ) * 2;
                 }
 
-                const int all_charges = charges_to_start + ( ( mealtime / 100 ) * ( it->type->tool->power_draw /
-                                        1000 ) ) / 1000;
+                const int all_charges = charges_to_start + mealtime / 100 * units::to_joule(
+                                            it->type->tool->power_draw ) / 1000;
 
                 if( it->ammo_remaining( p ) < all_charges ) {
 
