cbb86de6c5a4af29a5344e66ab46c0101dadcf04
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2022-09-14 16:59:41 -0700

    Add canvas to salvageable whitelist (as well as epoxy and thermo_resin)

--- src/iuse_actor.h
+++ src/iuse_actor.h
@@ -472,6 +472,7 @@ class salvage_actor : public iuse_actor
             material_id( "acidchitin" ),
             material_id( "alien_resin" ),
             material_id( "bone" ),
+            material_id( "canvas" ),
             material_id( "chitin" ),
             material_id( "cotton" ),
             material_id( "faux_fur" ),
@@ -486,6 +487,7 @@ class salvage_actor : public iuse_actor
             material_id( "nylon" ),
             material_id( "plastic" ),
             material_id( "rubber" ),
+            material_id( "thermo_resin" ),
             material_id( "wood" ),
             material_id( "wool" )
         };
