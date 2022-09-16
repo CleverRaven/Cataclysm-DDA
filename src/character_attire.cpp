2157bc452073f5ba5a9fc9e4cadfa8900dae5f30
EI IKaO <109350502+EIIKaO@users.noreply.github.com>
2022-09-16 12:54:33 +0900

    Emphasize pocket priority and choose pockets in order when filling count_by_charges items (#60961)
    * Fix pocket autopickup for charges items
    
    When a count_by_charges item is picked up ( regardless of the number of items picked ), the priority is considered in selecting a pocket to store the item with the highest priority pockets storing the items first.
    
    * smallfix-1
    
    Added rigid() to the condition for pocket selection and sorting
    Fix calculation formulas
    Optimized for Clang-tidy
    
    * smallfix-2
    
    Adjust to unit test
    
    * smallfix-3
    
    Delete rigid() from the condition for pocket sorting
    Remove unused item_location
    Code diet
    
    * smallfix-a
    
    Fix wrong comment
    
    * Smallfix-b
    
    Sort condition : Use item_pocket::better_pocket instead of custom conditions
    Add deep nested pockets to the pockets list
    Remove redundant cast
    
    * smallfix-c
    
    Try to pass Clang 12, macOS 10.15, Tiles, Sound, UBSan

--- src/character_attire.cpp
+++ src/character_attire.cpp
@@ -2237,6 +2237,73 @@ void outfit::pickup_stash( const item &newit, int &remaining_charges, bool ignor
     }
 }
 
+void outfit::add_stash( Character &guy, const item &newit, int &remaining_charges,
+                        bool ignore_pkt_settings )
+{
+    if( ignore_pkt_settings ) {
+        // Crawl all pockets regardless of priority
+        // Crawl First : wielded item
+        item_location carried_item = guy.get_wielded_item();
+        if( carried_item && !carried_item->has_pocket_type( item_pocket::pocket_type::MAGAZINE ) &&
+            carried_item->can_contain_partial( newit ) ) {
+            int used_charges = carried_item->fill_with( newit, remaining_charges, false, false, false );
+            remaining_charges -= used_charges;
+        }
+        // Crawl Next : worn items
+        pickup_stash( newit, remaining_charges, ignore_pkt_settings );
+    } else {
+        item_pocket *found_pocket;
+        std::vector<item_pocket *> pockets;
+        item temp_it = item( newit );
+        temp_it.charges = 1;
+
+        // Collect all pockets
+        std::list<item *> items;
+        item_location carried_item = guy.get_wielded_item();
+        if( carried_item != item_location::nowhere ) {
+            items.emplace_back( &*carried_item );
+        }
+        for( item &i : worn ) {
+            items.emplace_back( &i );
+        }
+        for( item *i : items ) {
+            for( const item_pocket *pocket : i->get_all_contained_pockets() ) {
+                if( pocket->can_contain( temp_it ).success() ) {
+                    // Top-level( wielded, worn ) pockets may be stored
+                    found_pocket = const_cast<item_pocket *>( pocket );
+                    pockets.emplace_back( found_pocket );
+                }
+                for( const item *contained : pocket->all_items_ptr( item_pocket::pocket_type::CONTAINER ) ) {
+                    for( const item_pocket *pocket_nest : contained->get_all_contained_pockets() ) {
+                        if( pocket_nest->can_contain( temp_it ).success() && pocket_nest->rigid() ) {
+                            // Nested pocket with rigid() can only be stored
+                            found_pocket = const_cast<item_pocket *>( pocket_nest );
+                            pockets.emplace_back( found_pocket );
+                        }
+                    }
+                }
+            }
+        }
+
+        // Sort by item_pocket::better_pocket
+        std::sort( pockets.begin(), pockets.end(), [newit, temp_it]( item_pocket * lhs,
+        item_pocket * rhs ) {
+            return rhs->better_pocket( *lhs, newit, false );
+        } );
+
+        int amount = remaining_charges;
+        int num_contained = 0;
+        for( item_pocket *&pocket : pockets ) {
+            if( amount <= num_contained || remaining_charges <= 0 ) {
+                break;
+            }
+            const int filled_count = pocket->fill_with( newit, remaining_charges, false, false );
+            num_contained += filled_count;
+            remaining_charges -= filled_count;
+        }
+    }
+}
+
 void outfit::write_text_memorial( std::ostream &file, const std::string &indent,
                                   const char *eol ) const
 {
