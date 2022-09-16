fedffb29200f7309b2187a73f89348781613509e
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2022-09-16 10:02:44 -0700

    change max alts from 104 to 105

--- src/requirements.cpp
+++ src/requirements.cpp
@@ -1649,9 +1649,9 @@ deduped_requirement_data::deduped_requirement_data( const requirement_data &in,
 
         // Because this algorithm is super-exponential in the worst case, add a
         // sanity check to prevent things getting too far out of control.
-        // The worst case in the core game currently is chainmail_suit_faraday
-        // with 63 alternatives.
-        static constexpr size_t max_alternatives = 100;
+        // The worst case in the core game currently is boots_fur
+        // with 104 alternatives.
+        static constexpr size_t max_alternatives = 105;
         if( alternatives_.size() + pending.size() > max_alternatives ) {
             debugmsg( "Construction of deduped_requirement_data generated too many alternatives.  "
                       "The recipe %1s should be simplified.  See the Recipe section in "
