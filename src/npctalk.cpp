b5f8edfb9e66a7e602ab81ffe9c35a671ccee958
Ramza13 <52087122+Ramza13@users.noreply.github.com>
2022-09-27 17:44:49 -0400

    Update npctalk.cpp

--- src/npctalk.cpp
+++ src/npctalk.cpp
@@ -1554,7 +1554,7 @@ void parse_tags( std::string &phrase, const Character &u, const Character &me,
         const item_location u_weapon = u.get_wielded_item();
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wtautological-compare"
-        const item_location me_weapon = ( &me != NULL ) ? me.get_wielded_item() : item_location();
+        const item_location me_weapon = ( &me != 0 ) ? me.get_wielded_item() : item_location();
 #pragma clang diagnostic pop
         // Special, dynamic tags go here
         if( tag == "<yrwp>" ) {
