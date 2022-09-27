a82b28142072c90782d9c26a5acc6b5640a873f2
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2022-09-27 16:45:02 -0700

    Update relic.cpp

--- src/relic.cpp
+++ src/relic.cpp
@@ -773,9 +773,10 @@ relic relic_procgen_data::generate( const relic_procgen_data::generation_rules &
     }
 
     //add a mandatory enchantment of the value of ret's power (the artifact being created) - resonance is equal to its power (min zero)
+    const relic_procgen_data::enchantment_value_passive<int> *r = { "weight": 100, "min_value": 0.0, "max_value": 0.0, "type": "ARTIFACT_RESONANCE", "increment": 0.0, "power_per_increment": 0 };
     enchant_cache resonance;
     int value = std::max( 0, ret.power_level( id ) );
-    resonance.add_value_add( 1, value );
+    resonance.add_value_add( r->type, value );
     resonance.set_has( enchantment::has::HELD );
     ret.add_passive_effect( resonance );
 
