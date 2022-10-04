bd370e41def0ca6d8f8a6eac7e41961df2c9d980
Merikona <108444328+Merikona@users.noreply.github.com>
2022-10-04 03:18:34 -0400

    Adjust Time

--- src/handle_action.cpp
+++ src/handle_action.cpp
@@ -969,8 +969,8 @@ static void wait()
             as_m.addentry( 12, true, 'w', _( "Wait until you catch your breath" ) );
             durations.emplace( 12, 15_minutes ); // to hide it from showing
         }
-        add_menu_item( 1, '1', !has_watch ? _( "Wait 30 heartbeats" ) : "", 30_seconds );
-        add_menu_item( 2, '2', !has_watch ? _( "Wait 60 heartbeats" ) : "", 1_minute );
+        add_menu_item( 1, '1', !has_watch ? _( "Wait 10 heartbeats" ) : "", 10_seconds );
+        add_menu_item( 2, '2', !has_watch ? _( "Wait 60 heartbeats" ) : "", 60_seconds );
         add_menu_item( 3, '3', !has_watch ? _( "Wait 300 heartbeats" ) : "", 5_minutes );
         add_menu_item( 4, '4', !has_watch ? _( "Wait 1800 heartbeats" ) : "", 30_minutes );
 
