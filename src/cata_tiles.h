8072715eba7aa70aa2f71df060ddee27c355a4e3
mlange-42 <martin_lange_@gmx.net>
2022-10-08 23:46:17 +0200

    fix clang-tidy complains by using a point object for the offset

--- src/cata_tiles.h
+++ src/cata_tiles.h
@@ -124,8 +124,7 @@ class layer_variant
         std::string id;
         std::map<std::string, int> sprite;
         int layer;
-        int offset_x;
-        int offset_y;
+        point offset;
         int total_weight;
 };
 
@@ -446,15 +445,14 @@ class cata_tiles
         bool draw_from_id_string( const std::string &id, TILE_CATEGORY category,
                                   const std::string &subcategory, const tripoint &pos, int subtile, int rota,
                                   lit_level ll, bool apply_night_vision_goggles, int &height_3d, int intensity_level,
-                                  const std::string &variant, int offset_x, int offset_y );
+                                  const std::string &variant, const point &offset );
         bool draw_sprite_at(
             const tile_type &tile, const weighted_int_list<std::vector<int>> &svlist,
             const point &, unsigned int loc_rand, bool rota_fg, int rota, lit_level ll,
-            bool apply_night_vision_goggles, int retract, int &height_3d,
-            int offset_x, int offset_y );
+            bool apply_night_vision_goggles, int retract, int &height_3d, const point &offset );
         bool draw_tile_at( const tile_type &tile, const point &, unsigned int loc_rand, int rota,
                            lit_level ll, bool apply_night_vision_goggles, int retract, int &height_3d,
-                           int offset_x, int offset_y );
+                           const point &offset );
 
         /* Tile Picking */
         void get_tile_values( int t, const std::array<int, 4> &tn, int &subtile, int &rotation,
