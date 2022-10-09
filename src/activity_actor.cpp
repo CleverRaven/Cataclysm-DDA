6368f6d16bc47c7f4868f6e23100c569f40ce186
psuedolet <psuedolet@gmail.com>
2022-10-08 21:17:54 -0400

    Uneccesary debug message when picking a door that gets bashed. (#57166)
    * Picking ter_fern with no lockpicking_result logs a player message instead of a debug message
    
    * Add period to player message
    
    Co-authored-by: Anton Burmistrov <Night_Pryanik@mail.ru>
    
    * Add period to player message
    
    Co-authored-by: Anton Burmistrov <Night_Pryanik@mail.ru>
    
    Co-authored-by: Anton Burmistrov <Night_Pryanik@mail.ru>

--- src/activity_actor.cpp
+++ src/activity_actor.cpp
@@ -2225,7 +2225,7 @@ void lockpick_activity_actor::finish( player_activity &act, Character &who )
 
     if( here.has_furn( target ) ) {
         if( furn_type->lockpick_result.is_null() ) {
-            debugmsg( "%s lockpick_result is null", furn_type.id().str() );
+            who.add_msg_if_player( m_bad, _( "You can't open this lock." ) );
             return;
         }
 
@@ -2235,7 +2235,7 @@ void lockpick_activity_actor::finish( player_activity &act, Character &who )
         }
     } else {
         if( ter_type->lockpick_result.is_null() ) {
-            debugmsg( "%s lockpick_result is null", ter_type.id().str() );
+            who.add_msg_if_player( m_bad, _( "You can't open this lock." ) );
             return;
         }
 
