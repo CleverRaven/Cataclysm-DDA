fc8177fef0fa58df5639edeacc27cfd8ec99beae
KittyTac <52408044+KittyTac@users.noreply.github.com>
2022-10-14 17:01:36 +0700

    Update construction.cpp

--- src/construction.cpp
+++ src/construction.cpp
@@ -161,8 +161,7 @@ static void done_vehicle( const tripoint_bub_ms &, Character & );
 static void done_appliance( const tripoint_bub_ms &, Character & );
 static void done_wiring( const tripoint_bub_ms &, Character & );
 static void done_deconstruct( const tripoint_bub_ms &, Character & );
-static void done_digormine_stair( const tripoint_bub_ms &, bool, Character & );
-static void done_digormine_slope( const tripoint_bub_ms &, bool, Character & );
+static void done_digormine_stairorslope( const tripoint_bub_ms &, bool, bool, Character & );
 static void done_dig_grave( const tripoint_bub_ms &p, Character & );
 static void done_dig_grave_nospawn( const tripoint_bub_ms &p, Character & );
 static void done_dig_stair( const tripoint_bub_ms &, Character & );
@@ -1626,7 +1625,7 @@ static void unroll_digging( const int numer_of_2x4s )
     here.spawn_item( avatar_pos, itype_2x4, numer_of_2x4s );
 }
 
-void construct::done_digormine_stair( const tripoint_bub_ms &p, bool dig,
+void construct::done_digormine_stairorslope( const tripoint_bub_ms &p, bool dig, bool burrow,
                                       Character &player_character )
 {
     map &here = get_map();
@@ -1659,61 +1658,27 @@ void construct::done_digormine_stair( const tripoint_bub_ms &p, bool dig,
     }
 
     bool impassable = tmpmap.impassable( local_tmp );
-    if( !impassable ) {
-        add_msg( _( "You dig into a preexisting space, and improvise a ladder." ) );
-    } else if( dig ) {
-        add_msg( _( "You dig a stairway, adding sturdy timbers and a rope for safety." ) );
-    } else {
-        add_msg( _( "You drill out a passage, heading deeper underground." ) );
-    }
-    here.ter_set( p, t_stairs_down ); // There's the top half
-    // Again, need to use submap-local coordinates.
-    tmpmap.ter_set( local_tmp, impassable ? t_stairs_up : t_ladder_up ); // and there's the bottom half.
-    // And save to the center coordinate of the current active map.
-    tmpmap.save();
-}
-
-void construct::done_digormine_slope( const tripoint_bub_ms &p, bool dig,
-                                      Character &player_character )
-{
-    map &here = get_map();
-    const tripoint_abs_ms abs_pos = here.getglobal( p );
-    const tripoint_abs_sm pos_sm = project_to<coords::sm>( abs_pos );
-    tinymap tmpmap;
-    tmpmap.load( pos_sm + tripoint_below, false );
-    const tripoint local_tmp = tmpmap.getlocal( abs_pos );
-
-    bool dig_muts = player_character.has_trait( trait_PAINRESIST_TROGLO ) ||
-                    player_character.has_trait( trait_STOCKY_TROGLO );
-
-    int no_mut_penalty = dig_muts ? 10 : 0;
-    int mine_penalty = dig ? 0 : 10;
-    player_character.mod_stored_kcal( -43 - 9 * mine_penalty - 9 * no_mut_penalty );
-    player_character.mod_thirst( 5 + mine_penalty + no_mut_penalty );
-    player_character.mod_fatigue( 10 + mine_penalty + no_mut_penalty );
-
-    if( tmpmap.ter( local_tmp ) == t_lava ) {
-        if( !query_yn( _( "The rock feels much warmer than normal.  Proceed?" ) ) ) {
-            here.ter_set( p, t_pit ); // You dug down a bit before detecting the problem
-            unroll_digging( dig ? 8 : 12 );
-        } else {
-            add_msg( m_warning, _( "You just tunneled into lava!" ) );
-            get_event_bus().send<event_type::digs_into_lava>();
-            here.ter_set( p, t_hole );
-        }
-
-        return;
-    }
-
-    bool impassable = tmpmap.impassable( local_tmp );
-    if( !impassable ) {
-        add_msg( _( "You burrow into a preexisting space, and improvise a slope" ) );
-    } else {
-        add_msg( _( "You burrow a rough slope" ) );
-    }
-    here.ter_set( p, t_slope_down ); // There's the top half
-    // Again, need to use submap-local coordinates.
-    tmpmap.ter_set( local_tmp, t_slope_up ); // and there's the bottom half.
+	if ( !burrow ) {
+		if( !impassable ) {
+			add_msg( _( "You dig into a preexisting space, and improvise a ladder." ) );
+		} else if( dig ) {
+			add_msg( _( "You dig a stairway, adding sturdy timbers and a rope for safety." ) );
+		} else {
+			add_msg( _( "You drill out a passage, heading deeper underground." ) );
+		}
+		here.ter_set( p, t_stairs_down ); // There's the top half
+		// Again, need to use submap-local coordinates.
+		tmpmap.ter_set( local_tmp, impassable ? t_stairs_up : t_ladder_up ); // and there's the bottom half.
+	} else {
+		if( !impassable ) {
+			add_msg( _( "You burrow into a preexisting space, and improvise a slope" ) );
+		} else {
+			add_msg( _( "You burrow a rough slope" ) );
+		}
+		here.ter_set( p, t_slope_down ); // There's the top half
+		// Again, need to use submap-local coordinates.
+		tmpmap.ter_set( local_tmp, t_slope_up ); // and there's the bottom half.
+	}
     // And save to the center coordinate of the current active map.
     tmpmap.save();
 }
@@ -1757,17 +1722,17 @@ void construct::done_dig_grave_nospawn( const tripoint_bub_ms &p, Character &who
 
 void construct::done_dig_stair( const tripoint_bub_ms &p, Character &who )
 {
-    done_digormine_stair( p, true, who );
+    done_digormine_stairorslope( p, true, false, who );
 }
 
 void construct::done_mine_downstair( const tripoint_bub_ms &p, Character &who )
 {
-    done_digormine_stair( p, false, who );
+    done_digormine_stair( p, false, false, who );
 }
 
 void construct::done_dig_slope( const tripoint_bub_ms &p, Character &who )
 {
-    done_digormine_slope( p, true, who );
+    done_digormine_slope( p, true, true, who );
 }
 
 void construct::done_mine_upstair( const tripoint_bub_ms &p, Character &player_character )
