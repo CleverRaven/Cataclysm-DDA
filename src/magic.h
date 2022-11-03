8061c675348ea02d7fa2934abdfd520be5bf29c8
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2022-11-02 22:11:36 -0500

    Update magic.h

--- src/magic.h
+++ src/magic.h
@@ -337,6 +337,8 @@ class spell_type
 
         std::set<mtype_id> targeted_monster_ids;
 
+        std::set<species_id> targeted_species_ids;
+
         // list of bodyparts this spell applies its effect to
         body_part_set affected_bps;
 
@@ -552,6 +554,8 @@ class spell
         // returns the name string of all list of all targeted monster id
         //if targeted_monster_ids is empty, it returns an empty string
         std::string list_targeted_monster_names() const;
+        //if targeted_species_ids is empty, it returns an empty string
+        std::string list_targeted_species_names() const;
 
         std::string damage_string() const;
         std::string aoe_string() const;
