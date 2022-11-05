4b256504f8c8c7707a2d94fa569d53f65471f135
Venera3 <72006894+Venera3@users.noreply.github.com>
2022-11-05 19:41:05 +0100

    Clang??

--- src/creature.h
+++ src/creature.h
@@ -1245,7 +1245,7 @@ class Creature : public viewer
 
         virtual void on_stat_change( const std::string &, int ) {}
         virtual void on_effect_int_change( const efftype_id &, int, const bodypart_id & ) {}
-        virtual void on_damage_of_type( const effect_source &source, int, damage_type,
+        virtual void on_damage_of_type( const effect_source &, int, damage_type,
                                         const bodypart_id & ) {}
 
     public:
