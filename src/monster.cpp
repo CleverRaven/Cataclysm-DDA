4562bae80bcf94037756d5102aa61c8f0ecd5a06
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2022-11-12 15:00:08 -0600

    Portions of https://github.com/cataclysmbnteam/Cataclysm-BN/pull/2177
    Co-Authored-By: leoCottret <71428793+leoCottret@users.noreply.github.com>

--- src/monster.cpp
+++ src/monster.cpp
@@ -1392,7 +1392,7 @@ monster_attitude monster::attitude( const Character *u ) const
     }
 
     if( effective_anger <= 0 ) {
-        if( get_hp() != get_hp_max() ) {
+        if( get_hp() <= 0.6 * get_hp_max() ) {
             return MATT_FLEE;
         } else {
             return MATT_IGNORE;
@@ -3371,7 +3371,10 @@ void monster::hear_sound( const tripoint &source, const int vol, const int dist,
     if( wander_turns < wandf ) {
         return;
     }
-    process_trigger( mon_trigger::SOUND, volume );
+    // only trigger this if the monster is not friendly or the source isn't the player
+    if( friendly >= 0 || source != g->u.pos() ) {
+        process_trigger( mon_trigger::SOUND, volume );
+    }
     provocative_sound = tmp_provocative;
     if( morale >= 0 && anger >= 10 ) {
         // TODO: Add a proper check for fleeing attitude
