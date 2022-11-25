29ff7dd08c07b72a3a3fde145b611198bff9cd04
Jeremy Rose <nornagon@nornagon.net>
2022-11-24 20:27:18 -0800

    convert add_spawn to place_spawns in mx_house_wasp

--- src/map_extras.cpp
+++ src/map_extras.cpp
@@ -343,9 +343,8 @@ static bool mx_house_wasp( map &m, const tripoint &loc )
         m.place_spawns( GROUP_WASP_GUARD, 1, pod, pod, 1, true );
     }
     m.place_spawns( GROUP_WASP_QUEEN, 1, point_zero, point( SEEX, SEEY ), 1, true );
-    if( one_in( 5 ) ) {
-        m.add_spawn( mon_dermatik, rng( 1, 3 ), tripoint( point( SEEX * 2 - 1, SEEY * 2 - 1 ), loc.z ) );
-    }
+    m.place_spawns( GROUP_DERMATIK, 5, point( SEEX * 2 - 4, SEEY * 2 - 4 ), point( SEEX * 2 + 2,
+                    SEEY * 2 + 2 ), 0.1f );
 
     return true;
 }
