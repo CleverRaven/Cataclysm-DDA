c0476ad5dca128e1754d797daaef8372e01a7ee8
Catherine Halpern <cphalpern@gmail.com>
2022-11-28 19:53:32 -0500

    move place zombie mom to missionjson

--- src/mission_start.cpp
+++ src/mission_start.cpp
@@ -56,60 +56,6 @@ void mission_start::standard( mission * )
 {
 }
 
-void mission_start::place_zombie_mom( mission *miss )
-{
-    const tripoint_abs_omt house = mission_util::random_house_in_closest_city();
-
-    miss->target = house;
-    overmap_buffer.reveal( house, 6 );
-
-    tinymap zomhouse;
-    zomhouse.load( project_to<coords::sm>( house ), false );
-    zomhouse.add_spawn( mon_zombie, 1, { SEEX, SEEY, house.z() }, false, -1, miss->uid,
-                        Name::get( nameFlags::IsFemaleName | nameFlags::IsGivenName ) );
-    zomhouse.save();
-}
-
-void mission_start::kill_horde_master( mission *miss )
-{
-    npc *p = g->find_npc( miss->npc_id );
-    if( p == nullptr ) {
-        debugmsg( "could not find mission NPC %d", miss->npc_id.get_value() );
-        return;
-    }
-    // Npc joins you
-    p->set_attitude( NPCATT_FOLLOW );
-    // Pick one of the below locations for the horde to haunt
-    const tripoint_abs_omt center = p->global_omt_location();
-    tripoint_abs_omt site = overmap_buffer.find_closest( center, "office_tower_1", 0, false );
-    if( site == overmap::invalid_tripoint ) {
-        site = overmap_buffer.find_closest( center, "hotel_tower_1_8", 0, false );
-    }
-    if( site == overmap::invalid_tripoint ) {
-        site = overmap_buffer.find_closest( center, "school_5", 0, false );
-    }
-    if( site == overmap::invalid_tripoint ) {
-        site = overmap_buffer.find_closest( center, "forest_thick", 0, false );
-    }
-    miss->target = site;
-    overmap_buffer.reveal( site, 6 );
-    tinymap tile;
-    tile.load( project_to<coords::sm>( site ), false );
-    tile.add_spawn( mon_zombie_master, 1, { SEEX, SEEY, site.z() }, false, -1, miss->uid,
-                    _( "Demonic Soul" ) );
-    tile.add_spawn( mon_zombie_brute, 3, { SEEX, SEEY, site.z() } );
-    tile.add_spawn( mon_zombie_dog, 3, { SEEX, SEEY, site.z() } );
-
-    for( int x = SEEX - 1; x <= SEEX + 1; x++ ) {
-        for( int y = SEEY - 1; y <= SEEY + 1; y++ ) {
-            tile.add_spawn( mon_zombie, rng( 3, 10 ), { x, y, site.z() } );
-        }
-        tile.add_spawn( mon_zombie_dog, rng( 0, 2 ), { SEEX, SEEY, site.z() } );
-    }
-    tile.add_spawn( mon_zombie_necro, 2, { SEEX, SEEY, site.z() } );
-    tile.add_spawn( mon_zombie_hulk, 1, { SEEX, SEEY, site.z() } );
-    tile.save();
-}
 
 void mission_start::kill_nemesis( mission * )
 {
--- src/missiondef.cpp
+++ src/missiondef.cpp
@@ -193,8 +193,6 @@ enum legacy_mission_type_id {
 static const std::map<std::string, std::function<void( mission * )>> mission_function_map = {{
         // Starts
         { "standard", { } },
-        { "place_zombie_mom", mission_start::place_zombie_mom },
-        { "kill_horde_master", mission_start::kill_horde_master },
         { "kill_nemesis", mission_start::kill_nemesis },
         { "place_npc_software", mission_start::place_npc_software },
         { "place_priest_diary", mission_start::place_priest_diary },
