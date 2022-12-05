af158b17ace01e40704bcdabd146ddcee4ad3679
Termineitor244 <termineitor244@hotmail.com>
2022-12-04 20:49:48 -0600

    Update flag.h

--- src/flag.h
+++ src/flag.h
@@ -32,6 +32,7 @@ extern const flag_id flag_ALLERGEN_NUT;
 extern const flag_id flag_ALLERGEN_VEGGY;
 extern const flag_id flag_ALLERGEN_WHEAT;
 extern const flag_id flag_ALLERGEN_WOOL;
+extern const flag_id flag_ALLOWS_BODY_BLOCK;
 extern const flag_id flag_ALLOWS_NATURAL_ATTACKS;
 extern const flag_id flag_ALLOWS_REMOTE_USE;
 extern const flag_id flag_ALWAYS_TWOHAND;
