c58154ad5d406c762797eed0dcb59361f64bd81b
RenechCDDA <84619419+RenechCDDA@users.noreply.github.com>
2022-12-06 15:50:51 -0500

    Flag ACTIVE_GENERATOR and "active generator" furniture to replace most ASRGs (#62459)
    * implement flag ACTIVE_GENERATOR
    add active generator furniture
    
    * update generator parts
    
    * documentation
    
    * replace hardcoded ASRGs with new furniture active generators
    
    * update all the non-military/lab stuff to active generators
    update some obsolete ASRGs to the new standards (aftershock, microlab reactor)
    
    * remove ASRGs from vanilla irradiation plant

--- src/monattack.cpp
+++ src/monattack.cpp
@@ -136,8 +136,6 @@ static const efftype_id effect_targeted( "targeted" );
 static const efftype_id effect_tindrift( "tindrift" );
 static const efftype_id effect_under_operation( "under_operation" );
 
-static const furn_str_id furn_f_compact_ASRG_containment( "f_compact_ASRG_containment" );
-
 static const gun_mode_id gun_mode_AUTO( "AUTO" );
 
 static const itype_id itype_120mm_HEAT( "120mm_HEAT" );
@@ -3825,7 +3823,7 @@ bool mattack::searchlight( monster *z )
         for( int x = zpos.x - 24; x < zpos.x + 24; x++ ) {
             for( int y = zpos.y - 24; y < zpos.y + 24; y++ ) {
                 tripoint dest( x, y, z->posz() );
-                if( here.furn( dest ) == furn_f_compact_ASRG_containment ) {
+                if( here.has_flag( ter_furn_flag::TFLAG_ACTIVE_GENERATOR, dest ) ) {
                     generator_ok = true;
                 }
             }
