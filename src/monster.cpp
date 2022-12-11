bb575da71e39465403520303e58e148d6f12eead
Valiant <Night_Pryanik@mail.ru>
2022-12-11 17:36:06 +0400

    Removed adding bear trap on monster death to its inventory

--- src/monster.cpp
+++ src/monster.cpp
@@ -2664,9 +2664,6 @@ void monster::die( Creature *nkiller )
             add_item( item( "rope_6", calendar::turn_zero ) );
             add_item( item( "snare_trigger", calendar::turn_zero ) );
         }
-        if( has_effect( effect_beartrap ) ) {
-            add_item( item( "beartrap", calendar::turn_zero ) );
-        }
     }
 
     if( death_drops && !no_extra_death_drops ) {
