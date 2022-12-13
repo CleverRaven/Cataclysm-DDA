fbc139881f917ce8a9c92a3ae870cdb1663eb27a
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2022-12-13 15:56:02 -0800

    fix display

--- src/player_display.cpp
+++ src/player_display.cpp
@@ -1363,13 +1363,15 @@ void Character::disp_info( bool customize_character )
         }
 
         if( bmi < character_weight_category::underweight ) {
-            const float str_penalty = 1.0f - ( ( bmi - 13.0f ) / 3.0f );
-            starvation_text += std::string( _( "Strength" ) ) + " -" + string_format( "%2.0f%%\n",
-                               str_penalty * 100.0f );
-            starvation_text += std::string( _( "Dexterity" ) ) + " -" + string_format( "%2.0f%%\n",
-                               str_penalty * 50.0f );
-            starvation_text += std::string( _( "Intelligence" ) ) + " -" + string_format( "%2.0f%%",
-                               str_penalty * 50.0f );
+            const int str_penalty = std::floor( ( 1.0f - ( get_bmi_fat() /
+                                              character_weight_category::underweight ) ) * ( 1.0f - ( 2.0f / str_max ) ) * str_max );
+            const int dexint_penalty = std::floor( ( character_weight_category::underweight - bmi ) * 6.0f );
+            starvation_text += std::string( _( "Strength" ) ) + " -" + string_format( "%2.0f%\n",
+                               str_penalty );
+            starvation_text += std::string( _( "Dexterity" ) ) + " -" + string_format( "%2.0f%\n",
+                               dexint_penalty );
+            starvation_text += std::string( _( "Intelligence" ) ) + " -" + string_format( "%2.0f%",
+                               dexint_penalty );
         }
 
         effect_name_and_text.emplace_back( starvation_name, starvation_text );
