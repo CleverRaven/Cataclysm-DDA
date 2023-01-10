c5254ef7de71365d97e8f83f8ddf1d99025bce03
actually-a-cat <102094270+actually-a-cat@users.noreply.github.com>
2023-01-10 09:35:37 +0100

    Apply suggestion
    Co-authored-by: Anton Burmistrov <Night_Pryanik@mail.ru>

--- src/character.cpp
+++ src/character.cpp
@@ -1297,8 +1297,8 @@ int Character::swim_speed() const
     if( has_flag( json_flag_WEBBED_HANDS ) ) {
         ret -= hand_bonus_mult * webbing_factor * 0.5f;
     }
-    if( has_flag( json_flag_WEBBED_FEET ) ) {
-        ret -= is_barefoot() ? 1 : 0 * webbing_factor * 0.5f;
+    if( has_flag( json_flag_WEBBED_FEET ) && is_barefoot() ) {
+        ret -= webbing_factor * 0.5f;
     }
     /** @EFFECT_SWIMMING increases swim speed */
     ret *= get_modifier( character_modifier_swim_mod );
