33a453d6eb437d95469939beae5baf235e4dee38
Anton Burmistrov <Night_Pryanik@mail.ru>
2023-01-15 20:33:26 +0400

    Added a query on disassembling favorited item

--- src/crafting.cpp
+++ src/crafting.cpp
@@ -2368,6 +2368,11 @@ bool Character::disassemble( item_location target, bool interactive, bool disass
         return false;
     }
 
+    if( obj.is_favorite &&
+        !query_yn( _( "You're going to disassemble favorited item.\nAre you sure?" ) ) ) {
+        return false;
+    }
+
     avatar &player_character = get_avatar();
     const recipe &r = ( obj.typeId() == itype_disassembly ) ? obj.get_making() :
                       recipe_dictionary::get_uncraft( obj.typeId() );
