1399dcff621174bbfb22a691d57a6ecc3d89d5f0
CutenessOverloaded <oscarchow2005@gmail.com>
2023-01-25 11:18:31 +0800

    formatting . _ .

--- src/item_pocket.cpp
+++ src/item_pocket.cpp
@@ -1490,7 +1490,7 @@ bool item_pocket::can_reload_with( const item &ammo, const bool now ) const
         return allows_speedloader( ammo.typeId() ) &&
                is_compatible( ammo.loaded_ammo() ).success() &&
                ( remaining_ammo_capacity( ammo.loaded_ammo().ammo_type() ) + ammo_capacity(
-                     ammo.loaded_ammo().ammo_type() ) > ammo_capacity( ammo.loaded_ammo().ammo_type() ) );
+                 ammo.loaded_ammo().ammo_type() ) > ammo_capacity( ammo.loaded_ammo().ammo_type() ) );
     }
 
     if( !is_compatible( ammo ).success() ) {
