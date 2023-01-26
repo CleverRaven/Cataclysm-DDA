066e94864ec57f8186a8adfa32af219b3fc0b726
CutenessOverloaded <oscarchow2005@gmail.com>
2023-01-26 09:48:21 +0800

    Update item_pocket.cpp

--- src/item_pocket.cpp
+++ src/item_pocket.cpp
@@ -1480,7 +1480,8 @@ bool item_pocket::can_reload_with( const item &ammo, const bool now ) const
         // and the opcket needs to have enough space (except casings)
         return allows_speedloader( ammo.typeId() ) &&
                is_compatible( ammo.loaded_ammo() ).success() &&
-               ( remaining_ammo_capacity( ammo.loaded_ammo().ammo_type() ) >= ammo.ammo_remaining() );
+               ( remaining_ammo_capacity( ammo.loaded_ammo().ammo_type() ) == ammo_capacity(
+                     ammo.loaded_ammo().ammo_type() ) );
     }
 
     if( ammo.has_flag( flag_SPEEDLOADER_CLIP ) ) {
