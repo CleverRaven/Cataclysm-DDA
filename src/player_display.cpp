7d6da79204ac4e1352ec2ede98543c66413bee5e
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2023-02-15 14:51:54 -0800

    Update player_display.cpp

--- src/player_display.cpp
+++ src/player_display.cpp
@@ -970,7 +970,7 @@ static void draw_speed_tab( const catacurses::window &w_speed,
 
     const float temperature_speed_modifier = you.mutation_value( "temperature_speed_modifier" );
     if( temperature_speed_modifier != 0 ) {
-        const int climate_control = you->climate_control_strength().first;
+        const int climate_control = you.enchantment_cache->get_value_add( enchant_vals::mod::CLIMATE_CONTROL_HEAT );
         nc_color pen_color;
         std::string pen_sign;
         const units::temperature player_local_temp = get_weather().get_temperature( you.pos() );
