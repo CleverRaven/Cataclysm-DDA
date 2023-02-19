14d01d2d2220e70efccfa25a200606ac4f9bed43
Water_Moon <43123946+Water-Moon@users.noreply.github.com>
2023-02-19 03:08:00 -0500

    Added comments to pinyin.h
    Also fixed some coding style problem and removed a function that should not be there

--- src/pinyin.h
+++ src/pinyin.h
@@ -7,11 +7,23 @@
 namespace Pinyin
 {
 
-void build_data();
-
+/**
+ * Convert a string to possible pinyins
+ *
+ * @param str A u32string of (possibly Chinese) characters
+ * @param dest A vector to put the resulting pinyin combinations to
+ */
 void chinese_to_pinyin( const std::u32string &str, std::vector<std::u32string> &dest );
 
-bool pinyin_match( const std::u32string &str, const std::u32string &qtr );
+/**
+ * Match a search string with pinyin of a given string
+ *
+ * @param str A u32string of (possibly Chinese) characters, to be matched against
+ * @param qry A u32string of characters, the searching query
+ *
+ * @returns True if at least one possible pinyin combination of str contains qry
+ */
+bool pinyin_match( const std::u32string &str, const std::u32string &qry );
 
 }
 #endif //CATA_SRC_PINYIN_H
