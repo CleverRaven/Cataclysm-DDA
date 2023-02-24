75ebb699bdeefb95acc41610d387551b81e25801
actually-the-artificer <125104872+actually-the-artificer@users.noreply.github.com>
2023-02-24 21:17:35 +1300

    Update src/item.cpp TODO: check NUMB
    Co-authored-by: Jianxiang Wang (王健翔) <qrox@sina.com>

--- src/item.cpp
+++ src/item.cpp
@@ -2482,7 +2482,9 @@ void item::food_info( const item *food_item, std::vector<iteminfo> &info,
 
     if( food_item->has_flag( flag_CANNIBALISM ) &&
         parts->test( iteminfo_parts::FOOD_CANNIBALISM ) ) {
-         if( !player_character.has_flag( json_flag_CANNIBAL )  || !player_character.has_flag(json_flag_PSYCHOPATH) || !player_character.has_flag(json_flag_SAPIOVORE) || !player_character.has_flag(json_flag_NUMB)) {
+        if( !player_character.has_flag( json_flag_CANNIBAL ) &&
+            !player_character.has_flag( json_flag_PSYCHOPATH ) &&
+            !player_character.has_flag( json_flag_SAPIOVORE ) ) {
             info.emplace_back( "DESCRIPTION",
                                _( "* This food contains <bad>human flesh</bad>." ) );
         } else {
