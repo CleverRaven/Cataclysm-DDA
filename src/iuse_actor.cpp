cf224c73b05075e431d214d3ecae2df98359f7ae
Kamayana <pigbuster@gmail.com>
2023-03-01 16:39:38 -0600

    Reword existing trap message (#63681)
    * Reword existing trap message
    
    * Implement suggestion
    
    Co-authored-by: Jianxiang Wang (王健翔) <qrox@sina.com>
    
    * astyle
    
    ---------
    
    Co-authored-by: Jianxiang Wang (王健翔) <qrox@sina.com>

--- src/iuse_actor.cpp
+++ src/iuse_actor.cpp
@@ -3769,7 +3769,10 @@ bool place_trap_actor::is_allowed( Character &p, const tripoint &pos,
     const trap &existing_trap = here.tr_at( pos );
     if( !existing_trap.is_null() ) {
         if( existing_trap.can_see( pos, p ) ) {
-            p.add_msg_if_player( m_info, _( "You can't place a %s there.  It contains a trap already." ),
+            p.add_msg_if_player( m_info,
+                                 existing_trap.is_benign()
+                                 ? _( "You can't place a %s there.  It contains a deployed object already." )
+                                 : _( "You can't place a %s there.  It contains a trap already." ),
                                  name );
         } else {
             p.add_msg_if_player( m_bad, _( "You trigger a %s!" ), existing_trap.name() );
