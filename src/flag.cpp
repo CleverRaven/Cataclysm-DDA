4d5e2099767ad9ec732e91269dcd71fdbdadfa4b
cakepie <cake-pie@users.noreply.github.com>
2023-03-02 07:19:46 +0800

    Consolidate fishing implements logic to use only tool quality, eliminate redundant item flags (#62945)
    * eliminate FISH_GOOD and FISH_POOR flags
    
    * rename tool quality FISHING -> FISHING_ROD
    
    * move FISH_ROD from item use_action to tool quality usage

--- src/flag.cpp
+++ src/flag.cpp
@@ -121,8 +121,6 @@ const flag_id flag_FIRE_100( "FIRE_100" );
 const flag_id flag_FIRE_20( "FIRE_20" );
 const flag_id flag_FIRE_50( "FIRE_50" );
 const flag_id flag_FIRE_TWOHAND( "FIRE_TWOHAND" );
-const flag_id flag_FISH_GOOD( "FISH_GOOD" );
-const flag_id flag_FISH_POOR( "FISH_POOR" );
 const flag_id flag_FIT( "FIT" );
 const flag_id flag_FIX_FARSIGHT( "FIX_FARSIGHT" );
 const flag_id flag_FIX_NEARSIGHT( "FIX_NEARSIGHT" );
