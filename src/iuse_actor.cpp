eeb659e47400f4d729cccdf32056c2f4d144fbdc
Kamayana <pigbuster@gmail.com>
2023-03-10 11:48:32 -0600

    Make cable length more intuitive

--- src/iuse_actor.cpp
+++ src/iuse_actor.cpp
@@ -4399,11 +4399,12 @@ cata::optional<int> plug_in_actor::use( Character &p, item &it, bool t, const tr
         cable.set_var( "source_y", abspos.y );
         cable.set_var( "source_z", here.get_abs_sub().z() );
 
-        cable.set_var( "cable_length", cable_length );
+        // Add 1 to length so it's the max length it can stretch to, not the length that it breaks.
+        cable.set_var( "cable_length", cable_length + 1);
         cable.set_var( "efficiency", efficiency );
         // Convert wattage to how long it takes to charge 1 kW, the unit batteries use.
         cable.set_var( "charge_interval",
-                       std::max( 1, static_cast<int>( std::floor( 1000 / wattage + 0.5 ) ) ) );
+                       std::max( 1, static_cast<int>( std::floor( 1000.0 / wattage + 0.5 ) ) ) );
 
         cable.set_var( "state", "pay_out_cable" );
         cable.active = true;
