8ae40e3292f024763abe30bc6704e956a31a9fd4
bombasticSlacks <dillon.matchett@gmail.com>
2023-03-14 23:20:23 -0300

    linting

--- lang/string_extractor/parsers/enchant.py
+++ lang/string_extractor/parsers/enchant.py
@@ -5,7 +5,8 @@ from ..write_text import write_text
 def parse_enchant(json, origin):
     if "name" in json:
         name = get_singular_name(json["name"])
-        write_text(json["name"], origin, comment="Name of a enchantment", plural=True)
+        write_text(json["name"], origin, 
+                   comment="Name of a enchantment", plural=True)
 
     if "description" in json:
         write_text(json["description"], origin,
