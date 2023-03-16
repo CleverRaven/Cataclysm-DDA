4ddde238f5300d5ca82ca2229d95483e4d18f3f6
Kamayana <pigbuster@gmail.com>
2023-03-16 01:17:33 -0500

    Missing std namespace

--- src/map.cpp
+++ src/map.cpp
@@ -5513,7 +5513,7 @@ void map::process_linked_movement()
 void map::process_linked_removal( const vehicle *veh, const int removed_index )
 {
     auto bounds = links_to_process.equal_range( veh );
-    for_each( bounds.first, bounds.second, [this,removed_index]( std::pair<const vehicle *, item::cable_link *> l ) {
+    std::for_each( bounds.first, bounds.second, [this,removed_index]( std::pair<const vehicle *, item::cable_link *> l ) {
         if( l.second->vp_index == removed_index ) {
             add_msg_if_player_sees( getlocal( l.second->pos ), m_bad, _( "The cable has come loose!" ) );
             l.second->state = cable_state::needs_reeling;
@@ -5529,7 +5529,7 @@ units::power map::get_linked_power_draw( const vehicle *veh ) const
 {
     int milliwatts = 0;
     auto bounds = links_to_process.equal_range( veh );
-    for_each( bounds.first, bounds.second, [this,&milliwatts]( std::pair<const vehicle *, item::cable_link *> l ) {
+    std::for_each( bounds.first, bounds.second, [this,&milliwatts]( std::pair<const vehicle *, item::cable_link *> l ) {
         milliwatts += l.second->power_draw;
         } );
     /*units::power epower = 0_W;
