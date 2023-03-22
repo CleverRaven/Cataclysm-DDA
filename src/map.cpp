8e156585b822de48ff93ac061b0348ed16e1e1f6
cakepie <cake-pie@users.noreply.github.com>
2023-03-23 01:21:26 +0800

    fix friction drag causing direction change

--- src/map.cpp
+++ src/map.cpp
@@ -781,7 +781,8 @@ vehicle *map::move_vehicle( vehicle &veh, const tripoint &dp, const tileray &fac
     // If not enough wheels, mess up the ground a bit.
     if( !vertical && !veh.valid_wheel_config() && !( veh.is_watercraft() && veh.can_float() ) &&
         !veh.is_flying_in_air() && dp.z == 0 ) {
-        veh.velocity += veh.velocity < 0 ? 2000 : -2000;
+        veh.velocity -= veh.velocity < 0 ?
+                        std::max( veh.velocity, -2000 ) : std::min( veh.velocity, 2000 );
         for( const tripoint &p : veh.get_points() ) {
             const ter_id &pter = ter( p );
             if( pter == t_dirt || pter == t_grass ) {
