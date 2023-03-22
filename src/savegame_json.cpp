466b4be44bb0d23e49426212b6998f87fbe88b25
Kamejeir <99511880+Kamejeir@users.noreply.github.com>
2023-03-22 11:40:21 +0100

    New Radiophile mutation (#63910)
    * stuff for radiation sense mut
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Apply some suggestions
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update character.cpp
    
    Fix debugmsg popping up
    
    * Radsense -> Radiophile
    
    + new radiation detection message
    
    * might as well push this result of find&replace...
    
    am sorry, bandwidth for those whose tests might have been more worthy
    
    * commit
    
    I am terrified... or just full of nerves. Anyway this, with greater certainty than before, fixes the debugmsg.
    Also think this happifies the build test failure.
    
    * Update src/character.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * check satisfaction
    
    * Update morale.cpp
    
    When I asked Venera3 about balancing the morale part, I think this change constitutes the desired result.
    
    Co-Authored-By: Venera3 <72006894+Venera3@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Venera3 <72006894+Venera3@users.noreply.github.com>

--- src/savegame_json.cpp
+++ src/savegame_json.cpp
@@ -1041,6 +1041,7 @@ void Character::load( const JsonObject &data )
     on_stat_change( "sleep_deprivation", sleep_deprivation );
     on_stat_change( "pkill", pkill );
     on_stat_change( "perceived_pain", get_perceived_pain() );
+    on_stat_change( "radiation", get_rad() );
     recalc_sight_limits();
     calc_encumbrance();
 
