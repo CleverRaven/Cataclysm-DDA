76ec5df5695314abab584ef1b40234b1d489d6d3
Surflurer <22912139+Surflurer@users.noreply.github.com>
2023-03-28 12:41:33 +0800

    Manually revert some astyle

--- src/item.cpp
+++ src/item.cpp
@@ -1641,7 +1641,7 @@ tripoint item::get_var( const std::string &name, const tripoint &default_value )
     }
     std::vector<std::string> values = string_split( it->second, ',' );
     cata_assert( values.size() == 3 );
-    auto convert_or_error = []( const std::string &s ) {
+    auto convert_or_error = []( const std::string & s ) {
         ret_val<int> result = try_parse_integer<int>( s, false );
         if( result.success() ) {
             return result.value();
@@ -4029,7 +4029,7 @@ void item::armor_info( std::vector<iteminfo> &info, const iteminfo_query *parts,
     if( show_bodygraph ) {
         insert_separation_line( info );
 
-        auto bg_cb = [this]( const bodygraph_part * bgp, const std::string &sym ) {
+        auto bg_cb = [this]( const bodygraph_part * bgp, const std::string & sym ) {
             if( !bgp ) {
                 return colorize( sym, bodygraph_full_body_iteminfo->fill_color );
             }
@@ -5724,13 +5724,13 @@ void item::final_info( std::vector<iteminfo> &info, const iteminfo_query *parts,
 
         const auto print_parts = [&info, &player_character](
                                      const std::vector<vpart_info> &vparts,
-                                     const std::string &install_where,
+                                     const std::string & install_where,
                                      const std::function<bool( const vpart_info & )> &predicate
         ) {
             std::vector<std::string> result_parts;
             for( const vpart_info &vp : vparts ) {
                 const bool is_duplicate = std::any_of( result_parts.begin(), result_parts.end(),
-                [name = vp.name()]( const std::string &r ) {
+                [name = vp.name()]( const std::string & r ) {
                     return r == name;
                 } );
 
@@ -11465,7 +11465,7 @@ units::mass item::get_used_holster_weight() const
 int item::get_remaining_capacity_for_liquid( const item &liquid, bool allow_bucket,
         std::string *err ) const
 {
-    const auto error = [ &err ]( const std::string &message ) {
+    const auto error = [ &err ]( const std::string & message ) {
         if( err != nullptr ) {
             *err = message;
         }
