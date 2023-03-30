dc62510cecb248da0a6eb328399b8cad9670c6fb
Kevin Granade <kevin.granade@gmail.com>
2023-03-30 15:18:50 -0700

    remove skill rust option

--- src/skill.cpp
+++ src/skill.cpp
@@ -400,14 +400,9 @@ bool SkillLevel::rust( int rust_resist, float rust_multiplier )
 
     _rustAccumulator += rust_amount;
     _exercise -= rust_amount;
-    const std::string &rust_type = get_option<std::string>( "SKILL_RUST" );
     if( _exercise < 0 ) {
-        if( rust_type == "vanilla" || rust_type == "int" ) {
-            _exercise = ( 100 * 100 * pow( unadjustedLevel(), 2U ) ) - 1;
-            --_level;
-        } else {
-            _exercise = 0;
-        }
+        _exercise = ( 100 * 100 * pow( unadjustedLevel(), 2U ) ) - 1;
+        --_level;
     }
 
     return false;
