f6a770f591ef191ec5ccb491a13025e6e549b53d
Surflurer <22912139+Surflurer@users.noreply.github.com>
2023-04-06 15:43:01 +0800

    Rerun test?

--- src/activity_handlers.cpp
+++ src/activity_handlers.cpp
@@ -2179,8 +2179,7 @@ void repair_item_finish( player_activity *act, Character *you, bool no_menu )
     if( !act->targets.empty() ) {
         ploc = act->targets.data();
     }
-    item *main_tool = &( !w_hack.init( *act ) ?
-                         ploc ?
+    item *main_tool = &( !w_hack.init( *act ) ? ploc ?
                          **ploc : you->i_at( act->index ) : w_hack.get_item() );
     if( main_tool == nullptr ) {
         debugmsg( "Empty main tool for repair" );
