85216ace7fd913e5ad1870b5edfb0b69595ca08d
Rewryte <129854247+Rewryte@users.noreply.github.com>
2023-04-11 03:03:55 +0800

    revert bad auto format

--- src/item.cpp
+++ src/item.cpp
@@ -11250,10 +11250,11 @@ float item::simulate_burn( fire_data &frd ) const
 
 bool item::burn( fire_data &frd )
 {
-if( has_flag( flag_UNBREAKABLE ) )
-{
-    return false;
-}
+    if( has_flag( flag_UNBREAKABLE ) ) {
+        return false;
+    }
+    
+    float burn_added = simulate_burn( frd );
 
     if( burn_added <= 0 ) {
         return false;
