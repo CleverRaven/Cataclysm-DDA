b0fb57cf55867766b8d4a323d146baffd13179e0
mqrause <martin@qrause.de>
2023-04-10 23:50:09 +0200

    spawn items from spells with 0 damage

--- src/magic_spell_effect.cpp
+++ src/magic_spell_effect.cpp
@@ -1068,7 +1068,8 @@ void spell_effect::spawn_ethereal_item( const spell &sp, Creature &caster, const
 
     std::vector<item> granted;
 
-    for( int i = 0; i < sp.damage( caster ); i++ ) {
+    int count = std::min( 1, sp.damage( caster ) );
+    for( int i = 0; i < count; i++ ) {
         if( sp.has_flag( spell_flag::SPAWN_GROUP ) ) {
             std::vector<item> group_items = item_group::items_from( item_group_id( sp.effect_data() ),
                                             calendar::turn );
