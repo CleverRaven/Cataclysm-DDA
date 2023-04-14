c1fbe5cb233b3fdf12e78da94f32eaea890b841b
Rewryte <129854247+Rewryte@users.noreply.github.com>
2023-04-15 07:49:08 +0800

    Item summoning spells now respect keep hands free option (#65049)
    * added keep_hands_free check
    
    * auto format
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/magic_spell_effect.cpp
+++ src/magic_spell_effect.cpp
@@ -20,6 +20,7 @@
 #include "bodypart.h"
 #include "calendar.h"
 #include "character.h"
+#include "character_martial_arts.h"
 #include "colony.h"
 #include "color.h"
 #include "coordinates.h"
@@ -1095,6 +1096,7 @@ void spell_effect::spawn_ethereal_item( const spell &sp, Creature &caster, const
             it.set_flag( json_flag_FIT );
             player_character.wear_item( it, false );
         } else if( !player_character.has_wield_conflicts( it ) &&
+                   !player_character.martial_arts_data->keep_hands_free && //No wield if hands free
                    player_character.wield( it, 0 ) ) {
             // nothing to do
         } else {
