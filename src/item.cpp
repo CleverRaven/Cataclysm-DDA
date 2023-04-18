32b6e376a27d67f4ac0b41f51df1ebba5ce5db05
Inglonias <2125926+Inglonias@users.noreply.github.com>
2023-04-17 20:25:22 -0400

    Fix maps changing their location

--- src/item.cpp
+++ src/item.cpp
@@ -9291,7 +9291,11 @@ std::string item::get_book_skill() const
 
 bool item::is_map() const
 {
-    return get_category_shallow().get_id() == item_category_maps;
+    if((get_category_shallow().get_id() == item_category_maps) || (get_action() == "reveal_map")) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 bool item::seal()
