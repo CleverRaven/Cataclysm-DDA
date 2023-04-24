bfcd42d8ee78fd9cec2267393a1608c3b0a69fbf
Ramza13 <52087122+Ramza13@users.noreply.github.com>
2023-04-23 23:04:05 -0400

    Update mapgen.cpp

--- src/mapgen.cpp
+++ src/mapgen.cpp
@@ -3302,7 +3302,7 @@ class jmapgen_variable : public jmapgen_piece
 {
     public:
         std::string name;
-        jmapgen_variable( const JsonObject &jsi, const std::string &/*context*/ ) {
+        jmapgen_variable( const JsonObject &jsi, const std::string_view &/*context*/ ) {
             name = jsi.get_string( "name" );
         }
         mapgen_phase phase() const override {
