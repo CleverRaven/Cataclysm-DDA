42ea1f27f41e3730b3eda67f20326e863ed693e4
MarcusAseth <marcus.aseth@gmail.com>
2023-04-25 14:44:47 +0200

    added comment

--- src/game_inventory.cpp
+++ src/game_inventory.cpp
@@ -2077,6 +2077,7 @@ bool game_menus::inv::compare_items( const item &first, const item &second,
     bool first_execution = true;
     static int lang_version = detail::get_current_language_version();
     do {
+        //lang check here is needed to redraw the menu while using "Toggle language to English" option
         if( first_execution || lang_version != detail::get_current_language_version() ) {
             std::vector<iteminfo> v_item_first;
             std::vector<iteminfo> v_item_second;
