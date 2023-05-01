a1b0168812554aa9d2c6b14591385434ebd45bbb
Karol1223 <karlos.alex.c@gmail.com>
2023-05-01 18:29:18 +0200

    more astyle fun

--- src/mapdata.cpp
+++ src/mapdata.cpp
@@ -909,8 +909,8 @@ void set_furn_ids()
     f_kiln_metal_full = furn_id( "f_kiln_metal_full" );
     f_arcfurnace_empty = furn_id( "f_arcfurnace_empty" );
     f_arcfurnace_full = furn_id( "f_arcfurnace_full" );
-    f_digester_empty = furn_id( "f_digester_empty");
-    f_digester_full = furn_id( "f_digester_full");
+    f_digester_empty = furn_id( "f_digester_empty" );
+    f_digester_full = furn_id( "f_digester_full" );
     f_smoking_rack = furn_id( "f_smoking_rack" );
     f_smoking_rack_active = furn_id( "f_smoking_rack_active" );
     f_metal_smoking_rack = furn_id( "f_metal_smoking_rack" );
