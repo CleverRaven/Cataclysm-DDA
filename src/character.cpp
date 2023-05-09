5c25c6a3d7bc5c9ec2874c06c9b9dac7def1e7aa
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2023-05-09 16:42:07 -0700

    hopefully fix int related issues

--- src/character.cpp
+++ src/character.cpp
@@ -3130,7 +3130,7 @@ std::string Character::enumerate_unmet_requirements( const item &it, const item 
 int Character::read_speed() const
 {
     // Stat window shows stat effects on based on current stat
-    const float intel = 2 + get_int() / 8;
+    const float intel = 2.0f + get_int() / 8.0f;
     // 4 int = 72 seconds, 8 int = 60 seconds, 12 int = 51 seconds, 16 int = 45 seconds, 20 int = 40 seconds
     /** @EFFECT_INT affects reading speed by an decreasing amount the higher intelligence goes, intially about 9% per point at 4 int to lower than 4% at 20+ int */
     time_duration ret = 180_seconds / intel;
