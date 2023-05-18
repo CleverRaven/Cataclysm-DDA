7841d0ae4aad5917517b7e965f8a2a70bcf31228
Spaceboi <77634653+Spaceb0i@users.noreply.github.com>
2023-05-17 19:55:51 -0500

    Update src/item.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/item.cpp
+++ src/item.cpp
@@ -10338,7 +10338,7 @@ int item::ammo_capacity( const ammotype &ammo ) const
     }
     if( has_flag( flag_USE_UPS ) ) {
         int j = 0;
-        for ( const item* i : get_player_character().all_items_with_flag( flag_IS_UPS ) ) {
+        for( const item *i : get_player_character().all_items_with_flag( flag_IS_UPS ) ) {
             j += i->ammo_capacity( ammo );
         }
         return j;
