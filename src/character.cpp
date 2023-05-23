fac880485fe3a9dd0f69308ebdbcf3923a24867d
WhiteCloud0123 <112397151+WhiteCloud0123@users.noreply.github.com>
2023-05-23 16:25:16 +0800

    mounted horse ignores hallucination

--- src/character.cpp
+++ src/character.cpp
@@ -1467,6 +1467,9 @@ bool Character::check_mount_will_move( const tripoint &dest_loc )
     }
     if( mounted_creature && mounted_creature->type->has_fear_trigger( mon_trigger::HOSTILE_CLOSE ) ) {
         for( const monster &critter : g->all_monsters() ) {
+            if( critter.is_hallucination() ) {
+                continue;
+            }
             Attitude att = critter.attitude_to( *this );
             if( att == Attitude::HOSTILE && sees( critter ) && rl_dist( pos(), critter.pos() ) <= 15 &&
                 rl_dist( dest_loc, critter.pos() ) < rl_dist( pos(), critter.pos() ) ) {
