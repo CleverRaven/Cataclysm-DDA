e1d40f2ca8ab35e3c45453c4eba2b0a9c06a0a0f
Tread4 <84157408+Tread4@users.noreply.github.com>
2023-05-27 03:38:59 +0200

    Revert "2nd approach - Fix NPCs cancelling disassembly job while on other Z-lvl"
    This reverts commit 45e675f5114635ad688b9b3b2464e41179f8cfda.

--- do_turn.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef CATA_SRC_DO_TURN_H
-#define CATA_SRC_DO_TURN_H
-
-/** MAIN GAME LOOP. Returns true if game is over (death, saved, quit, etc.). */
-bool do_turn();
-void handle_key_blocking_activity();
-
-#endif // CATA_SRC_DO_TURN_H
