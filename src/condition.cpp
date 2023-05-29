76386812c8fe92bbc6ace4fdc92ccfbf782421f6
Surflurer <22912139+Surflurer@users.noreply.github.com>
2023-05-29 12:02:31 +0800

    Fix godco usage of xx_has_var and add a check for it

--- src/condition.cpp
+++ src/condition.cpp
@@ -767,6 +767,14 @@ void conditional_t::set_has_var( const JsonObject &jo, const std::string &member
     const std::string var_name = get_talk_varname( jo, member, false, empty );
     const std::string &value = jo.has_member( "value" ) ? jo.get_string( "value" ) : std::string();
     const bool time_check = jo.has_member( "time" ) && jo.get_bool( "time" );
+    if( !time_check && !jo.has_member( "value" ) ) {
+        jo.throw_error( "Missing field: \"value\" or \"time\"" );
+        condition = []( dialogue const & ) {
+            return false;
+        };
+        return;
+    }
+
     condition = [var_name, value, time_check, is_npc]( dialogue const & d ) {
         const talker *actor = d.actor( is_npc );
         if( time_check ) {
