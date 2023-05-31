5b5cbbd9f872ff6471bca1a01852fa885e97498b
Surflurer <22912139+Surflurer@users.noreply.github.com>
2023-05-31 15:43:44 +0800

    remove unused var

--- src/basecamp.cpp
+++ src/basecamp.cpp
@@ -661,8 +661,6 @@ std::list<item> basecamp::use_charges( const itype_id &fake_id, int &quantity )
 void basecamp::form_crafting_inventory( map &target_map )
 {
     _inv.clear();
-    const tripoint_abs_ms &dump_spot = get_dumping_spot();
-    const tripoint &origin = target_map.getlocal( dump_spot );
     zone_manager &mgr = zone_manager::get_manager();
     map &here = get_map();
     if( here.check_vehicle_zones( here.get_abs_sub().z() ) ) {
