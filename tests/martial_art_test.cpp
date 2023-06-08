e107ff56f9f365a76eead08377c97e1c038ac850
Venera3 <domper877@gmail.com>
2023-06-08 12:05:33 +0200

    Clangg

--- tests/martial_art_test.cpp
+++ tests/martial_art_test.cpp
@@ -18,9 +18,10 @@ static const itype_id itype_sword_wood( "sword_wood" );
 static const itype_id itype_test_weapon1( "test_weapon1" );
 static const itype_id itype_test_weapon2( "test_weapon2" );
 
+
+static const matec_id test_tech_condition_knockback( "test_tech_condition_knockback" );
 static const matec_id test_tech_condition_stun( "test_tech_condition_stun" );
 static const matec_id test_tech_condition_sweep( "test_tech_condition_sweep" );
-static const matec_id test_tech_condition_knockback( "test_tech_condition_knockback" );
 static const matec_id test_technique( "test_technique" );
 static const matype_id test_style_ma1( "test_style_ma1" );
 
