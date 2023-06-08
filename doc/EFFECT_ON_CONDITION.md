0b8a64ca2d56b0515c56e7f58020d59a79d5831f
bombasticSlacks <dillon.matchett@gmail.com>
2023-06-08 13:47:06 -0300

    Unit tests + docs

--- doc/EFFECT_ON_CONDITION.md
+++ doc/EFFECT_ON_CONDITION.md
@@ -67,6 +67,7 @@ broken_bone |  | { "character", `character_id` }, { "part", `body_part` }, |
 broken_bone_mends |  | { "character", `character_id` }, { "part", `body_part` }, |
 buries_corpse |  | { "character", `character_id` }, { "corpse_type", `mtype_id` }, { "corpse_name", `string` }, |
 causes_resonance_cascade |  | NONE |
+character_casts_spell | | { "character", `character_id` }, { "spell", `spell_id` }, { "difficulty", `int` }, { "cost", `int` }, { "cast_time", `int` }, { "damage", `int` }, |
 character_consumes_item |  | { "character", `character_id` }, { "itype", `itype_id` }, |
 character_eats_item |  | { "character", `character_id` }, { "itype", `itype_id` }, |
 character_forgets_spell |  | { "character", `character_id` }, { "spell", `spell_id` } |
--- tests/eoc_test.cpp
+++ tests/eoc_test.cpp
@@ -26,6 +26,8 @@ static const effect_on_condition_id
 effect_on_condition_EOC_math_diag_w_vars( "EOC_math_diag_w_vars" );
 static const effect_on_condition_id effect_on_condition_EOC_math_duration( "EOC_math_duration" );
 static const effect_on_condition_id
+effect_on_condition_EOC_math_spell_xp( "EOC_math_spell_xp" );
+static const effect_on_condition_id
 effect_on_condition_EOC_math_switch_math( "EOC_math_switch_math" );
 static const effect_on_condition_id
 effect_on_condition_EOC_math_test_context( "EOC_math_test_context" );
@@ -57,6 +59,8 @@ static const mtype_id mon_zombie( "mon_zombie" );
 static const recipe_id recipe_cattail_jelly( "cattail_jelly" );
 
 static const skill_id skill_survival( "survival" );
+
+static const spell_id spell_test_eoc_spell( "test_eoc_spell" );
 
 namespace
 {
@@ -465,6 +469,47 @@ TEST_CASE( "EOC_run_with_test_queue", "[eoc]" )
     CHECK( d.get_value( "npctalk_var_key" ).empty() );
     CHECK( d.get_value( "npctalk_var_key2" ).empty() );
     CHECK( d.get_value( "npctalk_var_key3" ).empty() );
+}
+
+TEST_CASE( "EOC_spell_event", "[eoc]" )
+{
+    clear_avatar();
+    clear_map();
+
+    dialogue d( get_talker_for( get_avatar() ), std::make_unique<talker>() );
+    global_variables &globvars = get_globals();
+    globvars.clear_global_values();
+
+    REQUIRE( globvars.get_global_value( "npctalk_var_key1" ).empty() );
+    REQUIRE( globvars.get_global_value( "npctalk_var_key2" ).empty() );
+    REQUIRE( globvars.get_global_value( "npctalk_var_key3" ).empty() );
+
+    spell temp_spell( spell_test_eoc_spell );
+    temp_spell.set_level( get_avatar(), 5 );
+    temp_spell.cast_all_effects( get_avatar(), tripoint() );
+
+    CHECK( globvars.get_global_value( "npctalk_var_key1" ) == "45" );
+    CHECK( globvars.get_global_value( "npctalk_var_key2" ) == "100" );
+    CHECK( globvars.get_global_value( "npctalk_var_key3" ) == "5" );
+}
+
+TEST_CASE( "EOC_spell_exp", "[eoc]" )
+{
+    clear_avatar();
+    clear_map();
+
+    dialogue d( get_talker_for( get_avatar() ), std::make_unique<talker>() );
+    global_variables &globvars = get_globals();
+    globvars.clear_global_values();
+
+    REQUIRE( globvars.get_global_value( "npctalk_var_key1" ).empty() );
+
+    get_avatar().magic->learn_spell( "test_eoc_spell", get_avatar(), true );
+
+    CHECK( effect_on_condition_EOC_math_spell_xp->activate( d ) );
+
+
+    CHECK( globvars.get_global_value( "npctalk_var_key1" ) == "1000" );
 }
 
 
