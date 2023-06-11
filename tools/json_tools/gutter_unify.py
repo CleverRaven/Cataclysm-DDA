da502e5a6ebabd64f096b6c384d4be7417ded6e2
Procyonae <45432782+Procyonae@users.noreply.github.com>
2023-06-11 15:51:36 +0100

    Update gutter_unify.py

--- tools/json_tools/gutter_unify.py
+++ tools/json_tools/gutter_unify.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
+"""
+
+Unifies "2", "3", "|" old gutters into "-" on maps using the roof_palette which don't define their own terrain definition for said characters.
+Prints any maps using other palettes with the roof_palette for manual correcting.
+
+"""
 import argparse
 import json
 import os
+import re
 
 args = argparse.ArgumentParser()
 args.add_argument("dir", action="store", help="specify json directory")
 args_dict = vars(args.parse_args())
 
+manuallychange = set()
 
 def gen_new(path):
     change = False
@@ -20,36 +28,46 @@ def gen_new(path):
                   " an array of objects!")
             return None
         for jo in json_data:
-            if jo["type"] == "mapgen" and "palettes" in jo["object"] and "rows" in jo["object"] and len(jo["object"]["palettes"]) == 1 and jo["object"]["palettes"][0] == "roof_palette":
-                terms = ["2", "3", "|"]
-                potentialterms = terms
-                if "terrain" in jo["object"]:
-                    for potentialterm in potentialterms:
-                        if potentialterm in jo["object"]["terrain"]:
-                            terms.remove(potentialterm)
-                if len(terms) != 0:
-                    rowsn = len(jo["object"]["rows"])
-                    i = 0
-                    while i < rowsn:
-                        for term in terms:
-                            jo["object"]["rows"][i] = jo["object"]["rows"][i].replace(term, "-")
-                            change = True
-                        i += 1
-
+            if jo["type"] == "mapgen" and "palettes" in jo["object"] and "rows" in jo["object"] and "roof_palette" in jo["object"]["palettes"]:
+                if len(jo["object"]["palettes"]) == 1:
+                    terms = ["2", "3", "|"]
+                    potentialterms = terms
+                    if "terrain" in jo["object"]:
+                        for potentialterm in potentialterms:
+                            if potentialterm in jo["object"]["terrain"]:
+                                terms.remove(potentialterm)
+                    if len(terms) != 0:
+                        rowsn = len(jo["object"]["rows"])
+                        i = 0
+                        while i < rowsn:
+                            for term in terms:
+                                newrow = jo["object"]["rows"][i].replace(term, "-")
+                                if(jo["object"]["rows"][i] != newrow)
+                                change = True
+                            i += 1
+                else:
+                    if isinstance(jo["om_terrain"], str):
+                        mapname = jo["om_terrain"]
+                    else:
+                        mapnames = []
+                        omrowsn = len(jo["om_terrain"])
+                        j = 0
+                        while j < omrowsn:
+                            mapnames.append(','.join(jo["om_terrain"][j]))
+                            j += 1
+                        mapname = ','.join(mapnames)
+                    manuallychange.add("Can't automatically correct map(s):\n" + mapname + "\nin file " + path + "\n due to multiple palettes being present.")
     return json_data if change else None
 
-all_filepaths = set()
 for root, directories, filenames in os.walk(args_dict["dir"]):
     for filename in filenames:
-        if not filename.endswith(".json"):
-            continue
-
         path = os.path.join(root, filename)
-        all_filepaths.add(path)
+        if path.endswith(".json"):
+            new = gen_new(path)
+            if new is not None:
+                with open(path, "w", encoding="utf-8") as jf:
+                    json.dump(new, jf, ensure_ascii=False)
+                os.system(f"tools\\format\\json_formatter.exe {path}")
 
-for path in all_filepaths:
-    new = gen_new(path)
-    if new is not None:
-        with open(path, "w", encoding="utf-8") as jf:
-            json.dump(new, jf, ensure_ascii=False)
-        os.system(f"./tools/format/json_formatter.cgi {path}")
+for statement in manuallychange:
+    print(statement)
