1cd99635629752a9928fc09ecd185153889209aa
Hirmuolio <hirmuolio@gmail.com>
2023-06-12 23:27:42 +0300

    astyle

--- src/activity_handlers.cpp
+++ src/activity_handlers.cpp
@@ -3732,7 +3732,7 @@ void activity_handlers::spellcasting_finish( player_activity *act, Character *yo
                 }
             }
             if( !act->targets.empty() ) {
-                item& it = *act->targets.front();
+                item &it = *act->targets.front();
                 if( !it.has_flag( flag_USE_PLAYER_ENERGY ) ) {
                     you->consume_charges( it, it.type->charges_to_use() );
                 }
