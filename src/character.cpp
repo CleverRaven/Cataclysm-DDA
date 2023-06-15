6419426ff188590dab26af1bc6470ea831b01ddb
WhiteCloud0123 <112397151+WhiteCloud0123@users.noreply.github.com>
2023-06-15 19:17:47 +0800

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/character.cpp
+++ src/character.cpp
@@ -1491,11 +1491,11 @@ void Character::mount_creature( monster &z )
         add_msg( m_warning, _( "You let go of the grabbed object." ) );
         player_avatar.grab( object_type::NONE );
     }
-    add_msg_if_player(m_good, _("You climb on the %s."), z.get_name());
-    if (z.has_flag(MF_RIDEABLE_MECH)) {
-        if (!z.type->mech_weapon.is_empty()) {
-            item mechwep = item(z.type->mech_weapon);
-            set_wielded_item(mechwep);
+    add_msg_if_player( m_good, _( "You climb on the %s." ), z.get_name() );
+    if( z.has_flag( MF_RIDEABLE_MECH ) ) {
+        if( !z.type->mech_weapon.is_empty() ) {
+            item mechwep = item( z.type->mech_weapon );
+            set_wielded_item( mechwep );
         }
         add_msg_if_player(m_good, _("You hear your %s whir to life."), z.get_name());
     }
