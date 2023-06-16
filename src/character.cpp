75f50e8ab2dc1f88f46c5d20ef7bcf442f99cb10
WhiteCloud0123 <112397151+WhiteCloud0123@users.noreply.github.com>
2023-06-16 14:52:08 +0800

    re run the fucking test

--- src/character.cpp
+++ src/character.cpp
@@ -1492,6 +1492,7 @@ void Character::mount_creature( monster &z )
         add_msg( m_warning, _( "You let go of the grabbed object." ) );
         player_avatar.grab( object_type::NONE );
     }
+    
     add_msg_if_player( m_good, _( "You climb on the %s." ), z.get_name() );
     if( z.has_flag( MF_RIDEABLE_MECH ) ) {
         if( !z.type->mech_weapon.is_empty() ) {
