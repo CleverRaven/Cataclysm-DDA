da77608b5664d9aa75389afb44079909c3aeaa1c
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2023-06-20 10:23:50 -0700

    redo the way fouling rusts

--- src/item.cpp
+++ src/item.cpp
@@ -13216,9 +13216,10 @@ bool item::process_tool( Character *carrier, const tripoint &pos )
 
 bool item::process_blackpowder_fouling( Character *carrier )
 {
-    //at low fouling about 24 hours to rust to XX. Down to 4 at very high fouling levels.
-    int dirt_factor = static_cast<int>( get_var( "dirt", 0 ) / 2000 );
-    if( damage() < max_damage() && ( dirt_factor + rng( 0, 21600 ) >= 21600 ) ) {
+    // rust is deterministic. 6 hours for first rust, then 12 (18 total), then 18 (36 total) and finally 24 (60 hours to go to XX)
+    // this speeds up by the amount the gun is dirty, 2-6x as fast depending on dirt level.
+    set_var( "rust_timer", get_var( "rust_timer", 0 ) + 1 + static_cast<int>( get_var( "dirt", 0 ) / 2000 ) );
+    if( damage() < max_damage() && get_var( "rust_timer", 0 ) > 21600 * ( damage() + 1 ) ) {
         inc_damage();
         if( carrier ) {
             carrier->add_msg_if_player( m_bad, _( "Your %s rusts due to blackpowder fouling." ), tname() );
