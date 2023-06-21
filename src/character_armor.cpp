c7cb9306bc1f4624b0cf7d57565db459a613d78c
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2023-06-21 15:08:11 -0700

    Update character_armor.cpp

--- src/character_armor.cpp
+++ src/character_armor.cpp
@@ -247,7 +247,7 @@ bool Character::armor_absorb( damage_unit &du, item &armor, const bodypart_id &b
     }
 
     if( armor.has_flag( flag_USE_POWER_WHEN_HIT ) ) {
-        armor.energy_consume( units::from_kilojoule( du.amount ) );
+        armor.energy_consume( units::from_kilojoule( du.amount ), pos(), this );
     }
 
     // reduce the damage
