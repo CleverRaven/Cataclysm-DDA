7fa704b74f8e79796486a4d728dc67a8beae1673
Numbuh474 <35499003+Numbuh474@users.noreply.github.com>
2023-06-28 10:56:22 -0400

    Lock vehicle doors (#61087)
    * Reproduce lock-vehicle-doors changes without the infinite files changed issue.
    
    * Fix typos, copy paste errors, and linting.
    
    * Astyle. Fix typos, copy paste errors, and linting.
    
    * Make sure the selected lockpick location is actually locked.
    
    * Astyle
    
    * Add missing avoid locks true_text and false_text.
    
    * Add check to prevent removal of lock (and door) while locked.
    
    * Astyle
    
    * Remove unused variable and replace instances of cata::optional.
    
    * Remove unused variable and replace instances of cata::optional. Fix logic on vehicle::damage.
    
    * Remove unused variable.
    
    * Fixes
    
    * Update vehicle_parts.json
    
    * Update doors.json
    
    * Update vehicle_parts.json
    
    * Update vans_busses.json
    
    ---------
    
    Co-authored-by: Maleclypse <54345792+Maleclypse@users.noreply.github.com>

--- src/character.cpp
+++ src/character.cpp
@@ -549,7 +549,7 @@ Character::Character() :
     name.clear();
     custom_profession.clear();
 
-    *path_settings = pathfinding_settings{ 0, 1000, 1000, 0, true, true, true, false, true };
+    *path_settings = pathfinding_settings{ 0, 1000, 1000, 0, true, true, true, true, false, true };
 
     move_mode = move_mode_walk;
     next_expected_position = std::nullopt;
