fe046d6cbe2224b43a363f171d73459816f4c4c6
irwiss <irwiss@users.noreply.github.com>
2023-06-30 10:31:06 +0300

    Autopilot tests

--- tests/vehicle_test.cpp
+++ tests/vehicle_test.cpp
@@ -31,6 +31,7 @@ static const itype_id itype_test_power_cord( "test_power_cord" );
 
 static const vpart_id vpart_ap_test_standing_lamp( "ap_test_standing_lamp" );
 static const vpart_id vpart_bike_rack( "bike_rack" );
+static const vpart_id vpart_programmable_autopilot( "programmable_autopilot" );
 
 static const vproto_id vehicle_prototype_bicycle( "bicycle" );
 static const vproto_id vehicle_prototype_car( "car" );
@@ -744,4 +745,56 @@ TEST_CASE( "Racking_and_unracking_tests", "[vehicle][bikerack]" )
     }
 
     clear_vehicles( &get_map() );
+}
+
+static int test_autopilot_moving( const vproto_id &veh_id, const vpart_id &extra_part )
+{
+    clear_avatar();
+    clear_map();
+    Character &player_character = get_player_character();
+    // Move player somewhere safe
+    REQUIRE_FALSE( player_character.in_vehicle );
+    player_character.setpos( tripoint_zero );
+
+    const tripoint map_starting_point( 60, 60, 0 );
+    map &here = get_map();
+    vehicle *veh_ptr = here.add_vehicle( veh_id, map_starting_point, -90_degrees, 100, 0, false );
+
+    REQUIRE( veh_ptr != nullptr );
+
+    vehicle &veh = *veh_ptr;
+    if( !extra_part.is_null() ) {
+        vehicle_part vp( extra_part, item( extra_part->base_item ) );
+        const int part_index = veh.install_part( point_zero, std::move( vp ) );
+        REQUIRE( part_index >= 0 );
+    }
+
+    veh.autopilot_on = true;
+    veh.is_following = true;
+    veh.is_patrolling = false;
+    veh.engine_on = true;
+    veh.refresh();
+
+    int turns_left = 10;
+    int tiles_travelled = 0;
+    const tripoint starting_point = veh.global_pos3();
+    while( veh.engine_on && turns_left > 0 ) {
+        turns_left--;
+        here.vehmove();
+        veh.idle( true );
+        // How much it moved
+        tiles_travelled += square_dist( starting_point, veh.global_pos3() );
+        // Bring it back to starting point to prevent it from leaving the map
+        const tripoint displacement = starting_point - veh.global_pos3();
+        here.displace_vehicle( veh, displacement );
+    }
+    return tiles_travelled;
+}
+
+TEST_CASE( "autopilot_tests", "[vehicle][autopilot]" )
+{
+    // spawns vehicle, installs the extra part if it's not null, activates follow and
+    // checks if it moves, most of the test is a cutout from vehicle_efficiency_test.cpp
+    CHECK( test_autopilot_moving( vehicle_prototype_car, vpart_id::NULL_ID() ) == 0 );
+    CHECK( test_autopilot_moving( vehicle_prototype_car, vpart_programmable_autopilot ) == 9 );
 }
