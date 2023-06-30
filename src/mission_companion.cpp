816c19252a5124d7eae003b3dba87147b118c31d
RenechCDDA <84619419+RenechCDDA@users.noreply.github.com>
2023-06-30 14:14:54 -0400

    Faction succession without death (#65732)
    * Faction succession without death (final C++ implementation)
    
    * horrifying cooldown implementation (but it works)
    
    * Apply suggestions from code review
    
    Co-authored-by: Brambor <tondadrd@seznam.cz>
    
    * move string declaration too
    
    * update EOC to not double-assign mission
    
    * fix take two: clear mission before assigning to prevent dupes
    
    * EOC fixes, take three
    
    * remove bizarre dead code for consumption in non-consumption tests
    
    * more test fixes
    
    * MORE test fixes.....
    
    * remove accidental space to preserve comment
    
    * Update json with now mandatory field
    
    * Remove previously added kludge for somtach contents test
    
    * silence clang tidy
    
    ---------
    
    Co-authored-by: Brambor <tondadrd@seznam.cz>

--- src/mission_companion.cpp
+++ src/mission_companion.cpp
@@ -150,6 +150,7 @@ std::string enum_to_string<mission_kind>( mission_kind data )
         case mission_kind::Forage_Job: return "Forage_Job";
         case mission_kind::Caravan_Commune_Center_Job: return "Caravan_Commune_Center_Job";
         case mission_kind::Camp_Distribute_Food: return "Camp_Distribute_Food";
+		case mission_kind::Camp_Determine_Leadership: return "Camp_Determine_Leadership";
         case mission_kind::Camp_Hide_Mission: return "Camp_Hide_Mission";
         case mission_kind::Camp_Reveal_Mission: return "Camp_Reveal_Mission";
         case mission_kind::Camp_Assign_Jobs: return "Camp_Assign_Jobs";
@@ -223,6 +224,10 @@ static const std::array < miss_data, Camp_Harvest + 1 > miss_info = { {
         //  Faction camp missions
         {
             "Camp_Distribute_Food",
+            no_translation( "" )
+        },
+        {
+            "Camp_Determine_Leadership",
             no_translation( "" )
         },
         {
@@ -1197,6 +1202,7 @@ bool talk_function::handle_outpost_mission( const mission_entry &cur_key, npc &p
             return false;
 
         case Camp_Distribute_Food:
+        case Camp_Determine_Leadership:
         case Camp_Hide_Mission:
         case Camp_Reveal_Mission:
         case Camp_Assign_Jobs:
