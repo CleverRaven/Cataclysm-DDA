e5ef70f2ee06c93c53239b9365d96f41f99718b0
Procyonae <45432782+Procyonae@users.noreply.github.com>
2023-07-17 00:58:00 +0100

    Overwrite/delete/extend behaviour for techniques

--- src/item_factory.h
+++ src/item_factory.h
@@ -366,6 +366,9 @@ class Item_factory
         void extend_qualities_from_json( const JsonObject &jo, std::string_view member, itype &def );
         void delete_qualities_from_json( const JsonObject &jo, std::string_view member, itype &def );
         void set_properties_from_json( const JsonObject &jo, std::string_view member, itype &def );
+        void set_techniques_from_json( const JsonObject &jo, const std::string &member, itype &def );
+        void extend_techniques_from_json( const JsonObject &jo, std::string_view member, itype &def );
+        void delete_techniques_from_json( const JsonObject &jo, std::string_view member, itype &def );
 
         // declared here to have friendship status with itype
         static void npc_implied_flags( itype &item_template );
