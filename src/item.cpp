e4bc6edd2a47d7e7f62e50a99379d61a30041771
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2023-07-15 21:43:48 -0700

    Update src/item.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/item.cpp
+++ src/item.cpp
@@ -5113,7 +5113,8 @@ void item::bionic_info( std::vector<iteminfo> &info, const iteminfo_query *parts
     insert_separation_line( info );
 
     if( bid->required_bionic ) {
-        info.emplace_back( "CBM", string_format( "* This CBM requires another CBM to also be installed: %s", bid->required_bionic->name ) );
+        info.emplace_back( "CBM", string_format( "* This CBM requires another CBM to also be installed: %s",
+                           bid->required_bionic->name ) );
     }
     insert_separation_line( info );
 
