a524239ce562d54e6c01b5d4c19bdd349a0263d7
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2023-07-18 11:25:10 -0700

    Update suffer.cpp

--- src/suffer.cpp
+++ src/suffer.cpp
@@ -1407,7 +1407,7 @@ void suffer::from_bad_bionics( Character &you )
     if( you.has_bionic( bio_synlungs ) && !you.has_active_bionic( bio_synlungs ) &&
         !you.has_effect( effect_narcosis ) ) {
         if( you.get_power_level() >= bio_synlungs->power_trigger ) {
-            you.activate_bionic( bio_synlungs );
+            you.activate_bionic( find_bionic_by_uid( bio_synlungs ) );
         } else {
             you.oxygen--;
             you.add_msg_if_player( m_bad, _( "Emergency! User's lungs are not powered!" ) );
