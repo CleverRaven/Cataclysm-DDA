3edbb19a0aed72e6e2337b0fbfbbe3e130843fe3
chichit1044 <139005418+chichit1044@users.noreply.github.com>
2023-07-22 23:55:24 -0700

    Update overmap_test.cpp

--- tests/overmap_test.cpp
+++ tests/overmap_test.cpp
@@ -337,11 +337,11 @@ TEST_CASE( "overmap_terrain_coverage", "[overmap][slow]" )
         oter_type_s_gas_g1.id(),
         oter_type_s_gas_g1_roof.id(),
         oter_type_s_restaurant_deserted_test.id(),// only in the desert test region
-        unvitrified_farm_0.id(),
-        unvitrified_farm_1.id(),
-        unvitrified_farm_2.id(),
-        unvitrified_farm__neg_1.id(),
-        unvitrified_orchard.id(),
+        oter_type_unvitrified_farm_0.id(),
+        oter_type_unvitrified_farm_1.id(),
+        oter_type_unvitrified_farm_2.id(),
+        oter_type_unvitrified_farm__neg_1.id(),
+        oter_type_unvitrified_orchard.id(),
     };
 
     std::unordered_set<oter_type_id> done;
