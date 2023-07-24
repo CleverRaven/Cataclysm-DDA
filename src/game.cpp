c8bf9d9789ee7f2a019f6e0ad8abb03a04dc1bee
SurFlurer <22912139+SurFlurer@users.noreply.github.com>
2023-07-24 16:26:00 +0800

    clang tidy

--- src/game.cpp
+++ src/game.cpp
@@ -524,7 +524,7 @@ bool game::check_mod_data( const std::vector<mod_id> &opts, loading_ui &ui )
             // Load any dependencies and de-duplicate them
             std::vector<mod_id> dep_vector = tree.get_dependencies_of_X_as_strings( mod.ident );
             std::set<mod_id> dep_set( dep_vector.begin(), dep_vector.end() );
-            for( auto &dep : dep_set ) {
+            for( const auto &dep : dep_set ) {
                 load_data_from_dir( dep->path, dep->ident.str(), ui );
             }
 
