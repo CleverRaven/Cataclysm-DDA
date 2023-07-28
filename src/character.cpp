a44d50a038cc2ecb7a440ca95b9880649acefa1a
anoobindisguise <56016372+anoobindisguise@users.noreply.github.com>
2023-07-28 11:31:49 -0700

    Update character.cpp

--- src/character.cpp
+++ src/character.cpp
@@ -1194,9 +1194,9 @@ float Character::tally_organic_size() const
 {
     float total_size = 0.0f;
     for( const bodypart_id &part : get_all_body_parts() ) {
-        if( !part->is_cybernetic ) {
+        //if( !part->is_cybernetic ) {
             total_size += part->hit_size;
-        }
+        //}
     }
     return total_size / 100.0f;
 }
