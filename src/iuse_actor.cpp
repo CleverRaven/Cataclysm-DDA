f9fbe0e88598a66ce28d41fbf3cb91998aff6eec
Kamayana <pigbuster@gmail.com>
2023-08-02 06:22:36 -0500

    Fix now-too-big extended devices not dropping immediately

--- src/iuse_actor.cpp
+++ src/iuse_actor.cpp
@@ -5146,6 +5146,8 @@ std::optional<int> link_up_actor::link_extend_cable( Character *p, item &it ) co
                           extended_ptr->type_name(), extension->type_name() );
     extension.remove_item();
     p->invalidate_inventory_validity_cache();
+    p->invalidate_weight_carried_cache();
+    p->drop_invalid_inventory();
     p->moves -= move_cost;
     return 0;
 }
