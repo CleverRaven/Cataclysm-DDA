f0c592db4e5e172917d7110d0cf59d1859f49c17
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2023-08-02 17:53:24 -0500

    Update src/iuse.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -4650,9 +4650,9 @@ std::optional<int> iuse::blood_draw( Character *p, item *it, bool, const tripoin
             p->add_msg_if_player( m_info, _( "…but acidic blood damages the %s!" ), it->tname() );
         }
     }
-        if( vampire ) {
-            p->vitamin_mod( vitamin_human_blood_vitamin, vitamin_human_blood_vitamin->min() - 500 );
-            }
+    if( vampire ) {
+        p->vitamin_mod( vitamin_human_blood_vitamin, vitamin_human_blood_vitamin->min() - 500 );
+    }
 
     if( !drew_blood ) {
         return std::nullopt;;
