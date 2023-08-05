467c40ec1857b112728a670604b48c399d08560d
Procyonae <45432782+Procyonae@users.noreply.github.com>
2023-08-05 18:46:23 +0100

    Making a mess of changing types as per usual

--- src/mapgendata.h
+++ src/mapgendata.h
@@ -236,6 +236,8 @@ class mapgendata
         bool has_predecessor() const;
         const oter_id &last_predecessor() const;
         void pop_last_predecessor();
+        void erase_linear_predecessor_duplicates();
+        void erase_linear_terrain_duplicates( const oter_type_id ter_type );
 
         template<typename Result>
         Result get_arg( const std::string &name ) const {
