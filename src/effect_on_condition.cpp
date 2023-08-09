3fe7b5e8e7ab2f60252e1c455c4515cb58fff7a4
Andrew Krieger <akrieger@users.noreply.github.com>
2023-08-09 09:52:07 -0700

    Eliminate single use of string_format for 4% total cpu wins, 10.5% of eoc processing cost

--- src/effect_on_condition.cpp
+++ src/effect_on_condition.cpp
@@ -289,7 +289,7 @@ void effect_on_conditions::process_reactivate()
 bool effect_on_condition::activate( dialogue &d ) const
 {
     bool retval = false;
-    d.amend_callstack( string_format( "EOC: %s", id.c_str() ) );
+    d.amend_callstack( "EOC: " + id.str() );
     // each version needs a copy of the dialogue to pass down
     dialogue d_eoc( d );
     if( !has_condition || condition( d_eoc ) ) {
