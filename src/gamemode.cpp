ec8a94cf0933b83c6249f44c5b0daf52799c31c8
MNG-cataclysm <matthew11alexander@gmail.com>
2023-08-11 12:30:53 -0400

    Remove old C++

--- src/gamemode.cpp
+++ src/gamemode.cpp
@@ -13,8 +13,6 @@ std::string special_game_name( special_game_type id )
             return "Bug (gamemode.cpp:special_game_name)";
         case special_game_type::TUTORIAL:
             return _( "Tutorial" );
-        case special_game_type::DEFENSE:
-            return _( "Defense" );
         default:
             return "Uncoded (gamemode.cpp:special_game_name)";
     }
@@ -29,9 +27,6 @@ std::unique_ptr<special_game> get_special_game( special_game_type id )
             break;
         case special_game_type::TUTORIAL:
             ret = std::make_unique<tutorial_game>();
-            break;
-        case special_game_type::DEFENSE:
-            ret = std::make_unique<defense_game>();
             break;
         default:
             debugmsg( "Missing something in gamemode.cpp:get_special_game()?" );
