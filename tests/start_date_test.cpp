1f0bd24a72a1dd2039b311ce25febb0b87bd05ea
Zhilkin Serg <ZhilkinSerg@users.noreply.github.com>
2023-08-20 02:46:10 +0300

    Reworked scenario calendar
    - removed `random` calendar entries from scenarion definitions
    - allowed free selection of cataclysm and game start dates in new character menu
    - updated UI in new character menu
    - fixed start date related tests
    - updated docs

--- tests/start_date_test.cpp
+++ tests/start_date_test.cpp
@@ -10,9 +10,6 @@ static const string_id<scenario> scenario_test_custom_both( "test_custom_both" )
 static const string_id<scenario> scenario_test_custom_cataclysm( "test_custom_cataclysm" );
 static const string_id<scenario> scenario_test_custom_game( "test_custom_game" );
 static const string_id<scenario> scenario_test_custom_game_invalid( "test_custom_game_invalid" );
-static const string_id<scenario> scenario_test_random_day( "test_random_day" );
-static const string_id<scenario> scenario_test_random_hour( "test_random_hour" );
-static const string_id<scenario> scenario_test_random_year( "test_random_year" );
 
 TEST_CASE( "Test_start_dates" )
 {
@@ -22,18 +19,17 @@ TEST_CASE( "Test_start_dates" )
     on_out_of_scope guard{ []()
     {
         set_scenario( scenario::generic() );
+        g->start_calendar();
     } };
 
     SECTION( "Scenario with custom game start date" ) {
         scenario scen = scenario_test_custom_game.obj();
-
         set_scenario( &scen );
-        scen.rerandomize();
         g->start_calendar();
 
         CHECK( calendar::start_of_game == calendar::turn_zero +
                1_hours * 18 +
-               1_days * 7 +
+               1_days * 6 +
                1_days * default_season_length * season_type::SUMMER +
                1_days * default_year_length * 3
              );
@@ -41,9 +37,7 @@ TEST_CASE( "Test_start_dates" )
 
     SECTION( "Scenario has game start date before cataclysm start date" ) {
         scenario scen = scenario_test_custom_game_invalid.obj();
-
         set_scenario( &scen );
-        scen.rerandomize();
         g->start_calendar();
 
         CHECK( calendar::start_of_game == calendar::start_of_cataclysm );
@@ -51,14 +45,12 @@ TEST_CASE( "Test_start_dates" )
 
     SECTION( "Scenario with custom cataclysm start date" ) {
         scenario scen = scenario_test_custom_cataclysm.obj();
-
         set_scenario( &scen );
-        scen.rerandomize();
         g->start_calendar();
 
         CHECK( calendar::start_of_cataclysm == calendar::turn_zero +
                1_hours * 1 +
-               1_days * 9 +
+               1_days * 8 +
                1_days * default_season_length * season_type::AUTUMN +
                1_days * default_year_length * 6
              );
@@ -66,137 +58,21 @@ TEST_CASE( "Test_start_dates" )
 
     SECTION( "Scenario with custom cataclysm start date and game start date" ) {
         scenario scen = scenario_test_custom_both.obj();
-
         set_scenario( &scen );
-        scen.rerandomize();
         g->start_calendar();
 
         CHECK( calendar::start_of_cataclysm == calendar::turn_zero +
                1_hours * 6 +
-               1_days * 4 +
+               1_days * 3 +
                1_days * default_season_length * season_type::WINTER +
                1_days * default_year_length * 5
              );
         CHECK( calendar::start_of_game == calendar::turn_zero +
                1_hours * 13 +
-               1_days * 19 +
+               1_days * 18 +
                1_days * default_season_length * season_type::AUTUMN +
                1_days * default_year_length * 8
              );
-    }
-
-    // Reset dates so other tests won't fail
-    calendar::start_of_game = calendar::turn_zero;
-    calendar::start_of_cataclysm = calendar::turn_zero;
-    calendar::turn = calendar::turn_zero;
-}
-
-TEST_CASE( "Random_scenario_dates" )
-{
-    // Days counted from start of year
-    time_duration first_day_of_summer = calendar::season_length();
-    time_duration last_day_of_summer = 2 * calendar::season_length() - 1_days;
-    time_duration default_start_hour = 1_hours * 8;
-
-    on_out_of_scope guard{ []()
-    {
-        set_scenario( scenario::generic() );
-    } };
-
-    SECTION( "Random hour" ) {
-        scenario scen = scenario_test_random_hour.obj();
-
-        set_scenario( &scen );
-        scen.rerandomize();
-        g->start_calendar();
-
-        // Random hour should result in something between day default_first_day_of_summer hour 0 - day first_day_of_summer hour 23
-        INFO( "Game started on turn " << to_turn<int>( calendar::start_of_game ) );
-        CHECK( calendar::start_of_game >= calendar::turn_zero + first_day_of_summer );
-        CHECK( calendar::start_of_game <= calendar::turn_zero + first_day_of_summer + 1_hours * 23 );
-
-        time_point start_of_game_1 = calendar::start_of_game;
-
-        // Generate up to 10 worlds to check that randomness makes different dates.
-        // If even one of them is different everything is fine.
-        bool all_same = true;
-        for( int i = 0; i < 10; i++ ) {
-            set_scenario( &scen );
-            scen.rerandomize();
-            g->start_calendar();
-            all_same = start_of_game_1 == calendar::start_of_game;
-            if( !all_same ) {
-                break;
-            }
-        }
-
-        // There is astronomically low chance for this to fail even if everything is working right.
-        CHECK_FALSE( all_same );
-    }
-
-    SECTION( "Random day" ) {
-        scenario scen = scenario_test_random_day.obj();
-
-        set_scenario( &scen );
-        scen.rerandomize();
-        g->start_calendar();
-
-        // Random day should result in something between day first_day_of_summer hour 8 - day last_day_of_summer hour 8
-        INFO( "Game started on turn " << to_turn<int>( calendar::start_of_game ) );
-        CHECK( calendar::start_of_game >= calendar::turn_zero + first_day_of_summer +
-               default_start_hour );
-        CHECK( calendar::start_of_game <= calendar::turn_zero + last_day_of_summer + default_start_hour );
-
-        time_point start_of_game_1 = calendar::start_of_game;
-
-        // Generate up to 10 worlds to check that randomness makes different dates.
-        // If even one of them is different everything is fine.
-        bool all_same = true;
-        for( int i = 0; i < 10; i++ ) {
-            set_scenario( &scen );
-            scen.rerandomize();
-            g->start_calendar();
-            all_same = start_of_game_1 == calendar::start_of_game;
-            if( !all_same ) {
-                break;
-            }
-        }
-
-        // There is astronomically low chance for this to fail even if everything is working right.
-        CHECK_FALSE( all_same );
-    }
-
-    SECTION( "Random year" ) {
-        scenario scen = scenario_test_random_year.obj();
-
-        set_scenario( &scen );
-        scen.rerandomize();
-        g->start_calendar();
-
-        // Random year should result in something between year 0 day first_day_of_summer hour 8 - year 11 day first_day_of_summer hour 8
-        INFO( "Game started on turn " << to_turn<int>( calendar::start_of_game ) );
-        CHECK( calendar::start_of_game >= calendar::turn_zero + first_day_of_summer +
-               default_start_hour );
-        CHECK( calendar::start_of_game <= calendar::turn_zero + first_day_of_summer +
-               calendar::year_length() * 10 + default_start_hour );
-
-        time_point start_of_game_1 = calendar::start_of_game;
-
-        // Generate up to 10 worlds to check that randomness makes different dates.
-        // If even one of them is different everything is fine.
-        bool all_same = true;
-        for( int i = 0; i < 10; i++ ) {
-            set_scenario( &scen );
-            scen.rerandomize();
-            g->start_calendar();
-            all_same = start_of_game_1 == calendar::start_of_game;
-            if( !all_same ) {
-                break;
-            }
-        }
-
-        // There is astronomically low chance for this to fail even if everything is working right.
-        CHECK_FALSE( all_same );
     }
 
     // Reset dates so other tests won't fail
