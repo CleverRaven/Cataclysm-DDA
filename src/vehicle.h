d59e160e3133b600ca99034fab6573c532f375f0
Dana Holdampf <DHoldampf@Gmail.com>
2023-08-23 21:13:32 -0400

    Driving, boating, and cycling proficiencies
    Adds proficiencies for land and water vehicles, and updated hobbies and professions accordingly.

--- src/vehicle.h
+++ src/vehicle.h
@@ -65,6 +65,9 @@ class vehicle_part_with_feature_range;
 
 void handbrake();
 
+void practice_cycling_proficiency( Character &p );
+void practice_pilot_proficiencies( Character &p, bool &boating );
+
 namespace catacurses
 {
 class window;
