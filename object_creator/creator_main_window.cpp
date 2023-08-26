ad11d66b5f2e4269ab768d1900972516c6f829a3
snipercup <snipercup@gmail.com>
2023-08-26 17:25:09 -0400

    Move mods selector to it's own window
    Remove the mods selector from the main page into it's own window

--- object_creator/creator_main_window.cpp
+++ object_creator/creator_main_window.cpp
@@ -54,39 +54,6 @@ int creator::main_window::execute( QApplication &app )
     spell_window spell_editor( &title_menu );
     item_group_window item_group_editor( &title_menu );
 
-    QLabel mods_label;
-    mods_label.setParent( &title_menu );
-    mods_label.setText( QString( "Select mods (restart required):" ) );
-    mods_label.resize( default_text_box_size * 2 );
-    mods_label.move( QPoint( col * default_text_box_width, row++ * default_text_box_height ) );
-    mods_label.show();
-    row++;
-
-    //We always load 'dda' so we exclude it from the mods list
-    QStringList all_mods;
-    for( const mod_id &e : world_generator->get_mod_manager().all_mods() ) {
-        if( !e->obsolete && e->ident.str() != "dda" ) {
-            all_mods.append( e->ident.c_str() );
-        }
-    }
-
-    dual_list_box mods_box;
-    mods_box.initialize( all_mods );
-    mods_box.resize( QSize( default_text_box_width * 8, default_text_box_height * 10 ) );
-    mods_box.setParent( &title_menu );
-    mods_box.move( QPoint( col * default_text_box_width, row * default_text_box_height ) );
-    mods_box.show();
-    //The user's mod selection gets saved to a file
-    QObject::connect( &mods_box, &dual_list_box::pressed, [&]() {
-        settings.setValue( "mods/include", mods_box.get_included() );
-    } );
-
-    //A previous selection of mods is loaded from disk and applied to the modlist widget
-    if( settings.contains( "mods/include" ) ) {
-        QStringList modlist = settings.value( "mods/include" ).value<QStringList>();
-        mods_box.set_included( modlist );
-    }
-
     row += 11;
 
     QPushButton spell_button( _( "Spell Creator" ), &title_menu );
---
+++ object_creator/mod_selection_window.h
@@ -0,0 +1,38 @@
+#ifndef CATA_OBJECT_CREATOR_MOD_SELECTION_WINDOW_H
+#define CATA_OBJECT_CREATOR_MOD_SELECTION_WINDOW_H
+
+//Include Qframe, QLabel and QSpinBox for the mod selection window
+#include <QtWidgets/qframe.h>
+#include <QtWidgets/qlabel.h>
+#include <QtWidgets/qspinbox.h>
+
+#include "dual_list_box.h"
+#include "mod_manager.h"
+#include "worldfactory.h"
+
+
+
+namespace creator
+{
+class mod_selection_window : public QFrame
+{
+    Q_OBJECT
+
+    public:
+        mod_selection_window( QWidget *parent = nullptr, Qt::WindowFlags flags = Qt::WindowFlags() );
+
+        void show() {
+            QWidget::show();
+        }
+
+        void hide() {
+            QWidget::hide();
+        }
+
+    private:
+        QLabel* mods_label;
+        QSpinBox* mods_box;
+};
+}
+
+#endif // CATA_OBJECT_CREATOR_MOD_SELECTION_WINDOW_H
