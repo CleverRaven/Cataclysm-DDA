1ba3ee5484178264dd3d225022db526e099ff709
snipercup <snipercup@gmail.com>
2023-08-26 17:30:14 -0400

    Add menubar to main window and clean up code
    The navigation menu allows you to switch between pages. We don't need
    the removed code anymore

--- object_creator/creator_main_window.cpp
+++ object_creator/creator_main_window.cpp
@@ -5,14 +5,13 @@
 #include "mod_selection_window.h"
 #include "enum_conversions.h"
 #include "translations.h"
-#include "worldfactory.h"
-#include "mod_manager.h"
 
 #include <QtWidgets/qapplication.h>
 #include <QtWidgets/qmainwindow.h>
 #include <QtWidgets/qpushbutton.h>
-#include <QtCore/QSettings>
 #include <QtWidgets/qstackedwidget.h>
+#include <QtWidgets/qmenubar.h>
+
 
 namespace io
 {
@@ -34,9 +33,7 @@ namespace io
 
 int creator::main_window::execute( QApplication &app )
 {
-    const int default_text_box_height = 20;
-    const int default_text_box_width = 100;
-    const QSize default_text_box_size( default_text_box_width, default_text_box_height );
+
     //Create the main window
     QMainWindow creator_main_window;
 
@@ -53,11 +50,33 @@ int creator::main_window::execute( QApplication &app )
 
     //Set the current widget to the mod selection window
     stackedWidget.setCurrentWidget( &mod_selection );
+
+
+    //Create a menu bar with one menu called navigation
+    //The navigation menu has three actions, one for each window
+    QMenuBar menu_bar( &creator_main_window );
+    QMenu* navigation_menu = menu_bar.addMenu( pgettext( "menu", "Navigation" ) );
+    QAction* spell_action = navigation_menu->addAction( pgettext( "menu", "Spell" ) );
+    QAction* item_group_action = navigation_menu->addAction( pgettext( "menu", "Item group" ) );
+    QAction* mod_selection_action = navigation_menu->addAction( pgettext( "menu", "Mod selection" ) );
+
+    // Connect the navigation menu items to slots that switch the current widget of the stacked widget
+    QObject::connect( spell_action, &QAction::triggered, [&]() {
+        stackedWidget.setCurrentWidget( &spell_editor );
+    } );
+    QObject::connect( item_group_action, &QAction::triggered, [&]() {
+        stackedWidget.setCurrentWidget( &item_group_editor );
+    } );
+    QObject::connect( mod_selection_action, &QAction::triggered, [&]() {
+        stackedWidget.setCurrentWidget( &mod_selection );
+    } );
+
+
+    // Set the menu bar as the main window's menu bar
+    creator_main_window.setMenuBar( &menu_bar );
     
-    int row = 0;
-    int col = 0;
-    int max_row = 0;
-    int max_col = 0;
+    //Make the main window fullscreen
+    creator_main_window.showFullScreen();
 
 
 
