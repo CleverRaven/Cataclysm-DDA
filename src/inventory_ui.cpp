1ee6190c2e255035e2e350a0c76e3cf93e1e80f0
Kamayana <pigbuster@gmail.com>
2023-09-16 16:04:52 -0500

    Show un-insertable items in insert menu, with denial reasons

--- src/inventory_ui.cpp
+++ src/inventory_ui.cpp
@@ -817,16 +817,6 @@ bool inventory_holster_preset::is_shown( const item_location &contained ) const
         // spilt liquid cannot be picked up
         return false;
     }
-    if( contained->made_of( phase_id::LIQUID ) && !holster->is_watertight_container() ) {
-        return false;
-    }
-    item item_copy( *contained );
-    item_copy.charges = 1;
-    item_location parent = contained.has_parent() ? contained.parent_item() : item_location();
-    if( !holster->can_contain( item_copy, false, false, true, parent ).success() ) {
-        return false;
-    }
-
     //only hide if it is in the toplevel of holster (to allow shuffling of items inside a bag)
     for( const item *it : holster->all_items_top() ) {
         if( it == contained.get_item() ) {
@@ -834,12 +824,6 @@ bool inventory_holster_preset::is_shown( const item_location &contained ) const
         }
     }
 
-    if( contained->is_bucket_nonempty() ) {
-        return false;
-    }
-    if( !holster.parents_can_contain_recursive( &item_copy ).success() ) {
-        return false;
-    }
     return true;
 }
 
@@ -851,6 +835,25 @@ std::string inventory_holster_preset::get_denial( const item_location &it ) cons
             return ret.str();
         }
     }
+
+    if( it->is_bucket_nonempty() ) {
+        return "item would spill";
+    }
+
+    item item_copy( *it );
+    item_copy.charges = 1;
+    item_location parent = it.has_parent() ? it.parent_item() : item_location();
+
+    ret_val<void> ret = holster->can_contain( item_copy, false, false, true, parent );
+    if( !ret.success() ) {
+        return !ret.str().empty() ? ret.str() : "item can't be stored there";
+    }
+
+    ret = holster.parents_can_contain_recursive( &item_copy );
+    if( !ret.success() ) {
+        return !ret.str().empty() ? ret.str() : "item can't be stored there";
+    }
+
     return {};
 }
 
