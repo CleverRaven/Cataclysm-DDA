94430ba28c0ddc4823b949087f8a7b100f81aa33
EvanBalster <evan@interactopia.com>
2023-09-21 22:23:08 -0700

    Fix python indentation

--- lang/string_extractor/parsers/climbing_aid.py
+++ lang/string_extractor/parsers/climbing_aid.py
@@ -16,16 +16,16 @@ def parse_climbing_aid(json, origin):
 
     if down and "menu_text" in down:
         write_text(down["menu_text"], origin,
-                    comment="Menu text for descent with \"{}\"".format(name))
+                   comment="Menu text for descent with \"{}\"".format(name))
     if down and "menu_cant" in down:
         write_text(down["menu_cant"], origin,
-                    comment="Menu disables descent with \"{}\"".format(name))
+                   comment="Menu disables descent with \"{}\"".format(name))
     if down and "confirm_text" in down:
         write_text(down["confirm_text"], origin,
-                    comment="Query: really descend with \"{}\"?".format(name))
+                   comment="Query: really descend with \"{}\"?".format(name))
     if down and "msg_before" in down:
         write_text(down["msg_before"], origin,
-                    comment="Message before descent with \"{}\"".format(name))
+                   comment="Message before descent with \"{}\"".format(name))
     if down and "msg_after" in down:
         write_text(down["msg_after"], origin,
-                    comment="Message after descent with \"{}\"".format(name))
+                   comment="Message after descent with \"{}\"".format(name))
