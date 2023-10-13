a3d15efc042f61b0faa07c5f0a9b104d47212df6
MorvarchPrincess <63691647+MorvarchPrincess@users.noreply.github.com>
2023-10-12 19:50:47 -0500

    Better Gas masks - Model concentration better, and make gas mask cartiges last longer  (#68570)
    * Some initial work on better modelling of gas mask usage, doesn't work as I can't get the intensity level
    
    * add absorbtion calculation
    
    * Modify calculation to make gas mask last 20 minutes from full charge in thick toxic gas
    
    * Add low gas charges warning
    
    * Change gas_absorption _factor to be a timescale
    
    * Fix loading of concentration
    
    * Add documentation
    
    * linting C++ code
    
    * Apply suggestions from code review
    
    Add linting suggestions
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    * Better documentation clarity
    
    * Update src/iuse.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Zhilkin Serg <ZhilkinSerg@users.noreply.github.com>

--- src/iuse.cpp
+++ src/iuse.cpp
@@ -120,6 +120,7 @@
 #include "vitamin.h"
 #include "vpart_position.h"
 #include "vpart_range.h"
+#include "units.h"
 #include "weather.h"
 #include "weather_gen.h"
 #include "weather_type.h"
@@ -4183,14 +4184,27 @@ std::optional<int> iuse::gasmask( Character *p, item *it, const tripoint &pos )
         const field &gasfield = get_map().field_at( pos );
         for( const auto &dfield : gasfield ) {
             const field_entry &entry = dfield.second;
-            const int gas_abs_factor = entry.get_field_type()->gas_absorption_factor;
-            if( gas_abs_factor > 0 ) {
-                it->set_var( "gas_absorbed", it->get_var( "gas_absorbed", 0 ) + gas_abs_factor );
+            int gas_abs_factor = to_turns<int>( entry.get_field_type()->gas_absorption_factor );
+            const field_intensity_level &int_level = entry.get_intensity_level();
+            // 6000 is the amount of "gas absorbed" charges in a full 100 capacity gas mask cartridge.
+            // factor/concentration gives an amount of seconds the cartidge is expected to last in current conditions.
+            /// 6000/that is the amount of "gas absorbed" charges to tick up every second in order to reach that number.
+            float gas_absorbed = 6000 / ( static_cast<float>( gas_abs_factor ) / static_cast<float>
+                                          ( int_level.concentration ) );
+            if( gas_absorbed > 0 ) {
+                it->set_var( "gas_absorbed", it->get_var( "gas_absorbed", 0 ) + gas_absorbed );
             }
         }
-        if( it->get_var( "gas_absorbed", 0 ) >= 100 ) {
+        if( it->get_var( "gas_absorbed", 0 ) >= 60 ) {
             it->ammo_consume( 1, pos, p );
             it->set_var( "gas_absorbed", 0 );
+            if( it->ammo_remaining() < 10 ) {
+                p->add_msg_player_or_npc(
+                    m_bad,
+                    _( "Your %s is getting hard to breathe in!" ),
+                    _( "<npcname>'s gas mask is getting hard to breathe in!" )
+                    , it->tname() );
+            }
         }
         if( it->ammo_remaining() == 0 ) {
             p->add_msg_player_or_npc(
