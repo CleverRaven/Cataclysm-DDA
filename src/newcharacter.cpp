be44ecdae4b1b068e8aa8b1aea9d5daaa5ce5689
Tim Nordenfur <tim@gurka.se>
2023-10-22 01:01:52 +0200

    Introduce martial arts priority, which is used only to decide which style is selected following character creation.

--- src/newcharacter.cpp
+++ src/newcharacter.cpp
@@ -933,15 +933,6 @@ void avatar::initialize( character_type type )
         }
     }
 
-    // Select a random known style, except for style_kicks.
-    std::vector<matype_id> all_styles = martial_arts_data->get_known_styles( false );
-    std::vector<matype_id>::iterator it_kicks = std::find( all_styles.begin(), all_styles.end(),
-            style_kicks );
-    if( it_kicks != all_styles.end() ) {
-        all_styles.erase( it_kicks );
-    }
-    martial_arts_data->set_style( random_entry( all_styles, style_none ) );
-
     for( const trait_id &t : get_base_traits() ) {
         std::vector<matype_id> styles;
         for( const matype_id &s : t->initial_ma_styles ) {
@@ -952,9 +943,23 @@ void avatar::initialize( character_type type )
         if( !styles.empty() ) {
             const matype_id ma_type = choose_ma_style( type, styles, *this );
             martial_arts_data->add_martialart( ma_type );
-            martial_arts_data->set_style( ma_type );
         }
     }
+
+    // Select a random known style
+    std::vector<matype_id> selectable_styles = martial_arts_data->get_known_styles( false );
+    if( !selectable_styles.empty() ) {
+        std::vector<matype_id>::iterator it_max_priority = std::max_element( selectable_styles.begin(),
+        selectable_styles.end(), []( const matype_id & a, const matype_id & b ) {
+            return a->priority < b->priority;
+        } );
+        int max_priority = ( *it_max_priority )->priority;
+        selectable_styles.erase( std::remove_if( selectable_styles.begin(),
+        selectable_styles.end(), [max_priority]( const matype_id & style ) {
+            return style->priority != max_priority;
+        } ), selectable_styles.end() );
+    }
+    martial_arts_data->set_style( random_entry( selectable_styles, style_none ) );
 
     for( const mtype_id &elem : prof->pets() ) {
         starting_pets.push_back( elem );
