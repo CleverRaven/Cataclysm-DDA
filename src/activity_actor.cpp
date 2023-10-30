c3fc2551f0bafa003aaa54a54c28868671a3ccf2
inogenous <123803852+inogenous@users.noreply.github.com>
2023-10-30 21:11:36 +0100

    Bugfix: Ensure all selected books are scanned to ereader
    * Fixes issue where not all selected books were scanned to ereader.
    * The problem before was that:
    	* `ebooksave_activity_actor` expected an exact number of calls to `do_turn()` until the current books was scanned.
    	* But player speed affects the speed of activities such as this one.
    	* In effect, `do_turn()` is called to few times compared to `to_turns<int>( duration )` when player speed > 100.
    * This change therefore makes sure that all selected books are fully scanned when the activity is finished, regardless of number of calls to `do_turn()`.
    * The number of calls to `do_turn()` does not really matter anyway, it's just used for supporting that some books are fully scanned while the activity is running.

--- src/activity_actor.cpp
+++ src/activity_actor.cpp
@@ -2711,7 +2711,6 @@ void ebooksave_activity_actor::start_scanning_next_book( player_activity &act )
     act.moves_left = to_moves<int>( required_time( books ) );
 }
 
-
 void ebooksave_activity_actor::start( player_activity &act, Character &/*who*/ )
 {
     const time_duration scanning_time = required_time( books );
@@ -2744,6 +2743,12 @@ void ebooksave_activity_actor::do_turn( player_activity &act, Character &who )
     }
 
     // We have now spent enough time to fully scan current book
+    completed_scanning_current_book( act, who );
+}
+
+void ebooksave_activity_actor::completed_scanning_current_book( player_activity &act,
+        Character &who )
+{
     item_location scanned_book = books.back();
     books.pop_back();
     if( scanned_book ) {
@@ -2764,6 +2769,10 @@ void ebooksave_activity_actor::do_turn( player_activity &act, Character &who )
 
 void ebooksave_activity_actor::finish( player_activity &act, Character &who )
 {
+    while( !books.empty() ) {
+        // We can end up here with books left if the player has speed>100
+        completed_scanning_current_book( act, who );
+    }
     if( who.is_avatar() ) {
         add_msg( m_info, _( "You scan %d %s into your device." ), handled_books,
                  n_gettext( "book", "books", handled_books ) );
