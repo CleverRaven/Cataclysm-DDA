32187006601543f4723fc9dee7f262f4f34e5331
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2023-11-13 07:12:29 -0800

    Update src/monexamine.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/monexamine.cpp
+++ src/monexamine.cpp
@@ -504,7 +504,7 @@ void milk_source( monster &source_mon )
         add_msg( _( "The %s has no more milk." ), source_mon.get_name() );
             if ( !source_mon.has_effect( effect_critter_well_fed ) ) {
             add_msg( _( "It might not be getting enough to eat." ) );
-            }
+        }
     }
 }
 
