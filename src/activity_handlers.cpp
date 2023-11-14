41b20d223c81a134da5ede8af798394cd9055f4d
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2023-11-13 19:23:20 -0800

    Update src/activity_handlers.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/activity_handlers.cpp
+++ src/activity_handlers.cpp
@@ -879,7 +879,8 @@ static bool butchery_drops_harvest( item *corpse_item, const mtype &mt, Characte
     }
     if( corpse_item->has_flag( flag_UNDERFED ) ) {
         monster_weight = std::round( 0.9 * monster_weight );
-        if( action != butcher_type::FIELD_DRESS || action != butcher_type::SKIN || action != butcher_type::DISSECT ) {
+        if( action != butcher_type::FIELD_DRESS || action != butcher_type::SKIN ||
+            action != butcher_type::DISSECT ) {
             you.add_msg_if_player( m_bad,
                                    _( "The corpse looks a little underweight..." ) );
         }
