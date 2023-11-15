28e4cdecf4199c485a5dd8b6226ca98450d38746
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2023-11-15 07:15:18 -0800

    Update src/character.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/character.cpp
+++ src/character.cpp
@@ -10727,7 +10727,7 @@ void Character::process_effects()
             checked_health += 50;
         }
         add_msg( _( "Checked health is %s!" ), checked_health );
-        int pinkeye_chance = round( 4 + 96 * log ( checked_health + 1 ) / log ( 401 ) ); 
+        int pinkeye_chance = round( 4 + 96 * log( checked_health + 1 ) / log( 401 ) );
         add_msg( _( "Pinkeye chance is 1 in %s!" ), pinkeye_chance );
         if( one_in( ( pinkeye_chance ) ) ) {
             if( one_in( 2 ) ) {
