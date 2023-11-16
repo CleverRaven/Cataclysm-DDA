2569cc84f8778f1cf12d70e56b06ef534d6ac728
I-am-Erk <45136638+I-am-Erk@users.noreply.github.com>
2023-11-16 12:21:28 -0800

    Update npc.h
    name change

--- src/npc.h
+++ src/npc.h
@@ -1364,7 +1364,7 @@ class npc : public Character
         npc_mission previous_mission = NPC_MISSION_NULL;
         npc_personality personality;
         npc_opinion op_of_u;
-        npc_logic_combat remember_combat;
+        npc_logic_combat mem_combat;
         dialogue_chatbin chatbin;
         int patience = 0; // Used when we expect the player to leave the area
         npc_follower_rules rules;
