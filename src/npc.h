102fab3124c5b97c8dfb732698cf6c49e3a85f2a
I-am-Erk <45136638+I-am-Erk@users.noreply.github.com>
2023-11-16 16:35:13 -0800

    Update npc.h

--- src/npc.h
+++ src/npc.h
@@ -261,13 +261,17 @@ struct npc_combat_logic {
     int hostile_count; // for tallying nearby threatening enemies
     int swarm_count; // for tallying swarming enemies if you're using a ranged weapon
     int friendly_count; // for counting how many allies are nearby
+    int failed_repositions;
     float old_danger_assessment; // keep track of recent danger levels to see if they change
+    bool repositioning;
     npc_combat_logic() {
         panic = 0;
         hostile_count = 0;
         swarm_count = 0;
         friendly_count = 1; // count yourself as a friendly
+        failed_repositions = true;
         old_danger_assessment = 0.0f;
+        repositioning = false;
     }
 };
 
