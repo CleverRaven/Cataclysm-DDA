4b52b541c84c1c54757a87cae707a66d443337b9
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2023-11-18 15:55:44 -0800

    Update character_body.cpp

--- src/character_body.cpp
+++ src/character_body.cpp
@@ -136,7 +136,7 @@ void Character::update_body_wetness( const w_point &weather )
             const float base_clothing_mult = worn.clothing_wetness_mult( bp );
             // always some evaporation even if completely covered
             // doesn't handle things that would be "air tight"
-            const float clothing_mult = std::max( clothing_mult, .1f );
+            const float clothing_mult = std::max( base_clothing_mult, .1f );
 
             const time_duration drying = bp->drying_increment * average_drying * trait_mult * weather_mult *
                                          temp_mult / clothing_mult;
