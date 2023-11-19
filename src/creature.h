7b1531b0121cec1bf367b317d9fd2a9ec2bbcb5c
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2023-11-19 08:43:08 -0800

    Update src/creature.h
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/creature.h
+++ src/creature.h
@@ -631,8 +631,8 @@ class Creature : public viewer
         /** Applies effects by spraying liquid on the creature. Returns false if the liquid was blocked
          * by waterproof gear. */
         bool add_liquid_effect( const efftype_id &eff_id, const bodypart_id &vector, int strength,
-                             const time_duration &dur, const bodypart_id &bp, bool permanent = false, int intensity = 1,
-                             bool force = false );
+                                const time_duration &dur, const bodypart_id &bp, bool permanent = false, int intensity = 1,
+                                bool force = false );
         bool add_liquid_effect( const efftype_id &eff_id, const bodypart_id &vector, int strength,
                              const time_duration &dur, bool permanent = false, int intensity = 1, bool force = false );
         /** Removes a listed effect. If the bodypart is not specified remove all effects of
