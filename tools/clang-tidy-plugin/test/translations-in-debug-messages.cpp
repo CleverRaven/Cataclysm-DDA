53addb9403682592a788192f9584d1116b5d64e6
prharvey <2677507+prharvey@users.noreply.github.com>
2023-11-20 10:08:33 -0800

    astyle the tools directory

--- tools/clang-tidy-plugin/test/translations-in-debug-messages.cpp
+++ tools/clang-tidy-plugin/test/translations-in-debug-messages.cpp
@@ -8,19 +8,19 @@
 static void f( const time_duration &duration, Creature &c )
 {
     char *skill_level;
-    add_msg_debug(debugmode::DF_ACT_BUTCHER, _("Skill: %s"), skill_level);
+    add_msg_debug( debugmode::DF_ACT_BUTCHER, _( "Skill: %s" ), skill_level );
     // CHECK-MESSAGES: [[@LINE-1]]:47: warning: string arguments to debug message functions should not be translated. [cata-translations-in-debug-messages]
 
-    add_msg_debug(debugmode::DF_ACT_BUTCHER, _("Skill"));
+    add_msg_debug( debugmode::DF_ACT_BUTCHER, _( "Skill" ) );
     // CHECK-MESSAGES: [[@LINE-1]]:47: warning: string arguments to debug message functions should not be translated. [cata-translations-in-debug-messages]
 
-    add_msg_debug(debugmode::DF_ACT_BUTCHER, pgettext("Skill", "foo"));
+    add_msg_debug( debugmode::DF_ACT_BUTCHER, pgettext( "Skill", "foo" ) );
     // CHECK-MESSAGES: [[@LINE-1]]:47: warning: string arguments to debug message functions should not be translated. [cata-translations-in-debug-messages]
 
-    add_msg_debug(debugmode::DF_ACT_BUTCHER, n_gettext("Skill", "Skills", 0));
+    add_msg_debug( debugmode::DF_ACT_BUTCHER, n_gettext( "Skill", "Skills", 0 ) );
     // CHECK-MESSAGES: [[@LINE-1]]:47: warning: string arguments to debug message functions should not be translated. [cata-translations-in-debug-messages]
 
-    add_msg_debug(debugmode::DF_ACT_BUTCHER, "%s", to_translation("Skill"));
+    add_msg_debug( debugmode::DF_ACT_BUTCHER, "%s", to_translation( "Skill" ) );
     // CHECK-MESSAGES: [[@LINE-1]]:53: warning: string arguments to debug message functions should not be translated. [cata-translations-in-debug-messages]
 
     add_msg_debug_if_player_sees( c, debugmode::DF_ACT_BUTCHER, _( "Skill: %s" ), skill_level );
@@ -35,6 +35,6 @@ static void f( const time_duration &duration, Creature &c )
     c.add_msg_debug_if_npc( debugmode::DF_ACT_BUTCHER, _( "Skill" ) );
     // CHECK-MESSAGES: [[@LINE-1]]:56: warning: string arguments to debug message functions should not be translated. [cata-translations-in-debug-messages]
 
-    add_msg_debug(debugmode::DF_ACT_BUTCHER, "Duration %s", to_string(duration));
+    add_msg_debug( debugmode::DF_ACT_BUTCHER, "Duration %s", to_string( duration ) );
     // CHECK-MESSAGES: [[@LINE-1]]:62: warning: string arguments to debug message functions should not be translated.  This call to to_string might involve a translation; consider using to_string_writable instead. [cata-translations-in-debug-messages]
 }
