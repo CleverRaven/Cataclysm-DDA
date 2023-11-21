08f8252a8342fe4efa6542ce9daed3a6e2813266
Fris0uman <sthapplz@gmail.com>
2023-11-21 19:16:48 +0100

    Forbid chosing non-highlighted things

--- src/action.cpp
+++ src/action.cpp
@@ -1189,5 +1189,12 @@ std::optional<tripoint> choose_adjacent_highlight( const tripoint &pos, const st
         g->add_draw_callback( hilite_cb );
     }
 
-    return choose_adjacent( pos, message, allow_vertical );
+
+    const std::optional<tripoint> dir = choose_direction( message, allow_vertical );
+    const std::optional<tripoint> chosen = dir ? *dir + pos : dir;
+
+    if( std::find( valid.begin(), valid.end(), chosen ) != valid.end() ) {
+        return chosen;
+    }
+    return std::nullopt;
 }
