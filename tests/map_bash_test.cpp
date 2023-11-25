9ce6a995e7d778f0f9f08ae6b8fe52d3295cb4b6
ehughsbaird <44244083+ehughsbaird@users.noreply.github.com>
2023-11-25 21:27:56 +0000

    Bashing furniture/terrain gradually gets easier
    Bashing is frustrating. It's largely a result of RNG, because it does
    not have persistent tracking of any data.
    
    To fix this, add some semi-persistent (ephemeral) data to submaps to
    track damage done to a tile. Change the formula for a successful bash to
    use this data.
    
    RNG is still a factor, but it will now take at most str_max - str_min
    bashes once str_min has been exceeded.

--- tests/map_bash_test.cpp
+++ tests/map_bash_test.cpp
@@ -6,6 +6,12 @@
 #include "player_helpers.h"
 #include "test_data.h"
 
+static const furn_str_id furn_test_f_bash_persist( "test_f_bash_persist" );
+static const furn_str_id furn_test_f_eoc( "test_f_eoc" );
+
+static const ter_str_id ter_test_t_bash_persist( "test_t_bash_persist" );
+static const ter_str_id ter_test_t_pit_shallow( "test_t_pit_shallow" );
+
 void bash_test_loadout::apply( Character &guy ) const
 {
     clear_character( guy );
@@ -51,10 +57,8 @@ static void test_bash_set( const bash_test_set &set )
             if( it == test.furn_tries.end() ) {
                 CHECK( tries == max_tries );
             } else {
-                // TODO: make bashing deterministic, this is just a "can bash" test right now
-                // CHECK( tries >= it->second.first );
-                // CHECK( tries <= it->second.second );
-                CHECK( tries != max_tries );
+                CHECK( tries >= it->second.first );
+                CHECK( tries <= it->second.second );
             }
         }
         for( const ter_id &ter : set.tested_ter ) {
@@ -70,10 +74,8 @@ static void test_bash_set( const bash_test_set &set )
             if( it == test.ter_tries.end() ) {
                 CHECK( tries == max_tries );
             } else {
-                // TODO: make bashing deterministic, this is just a "can bash" test right now
-                // CHECK( tries >= it->second.first );
-                // CHECK( tries <= it->second.second );
-                CHECK( tries != max_tries );
+                CHECK( tries >= it->second.first );
+                CHECK( tries <= it->second.second );
             }
         }
     }
@@ -85,5 +87,88 @@ TEST_CASE( "map_bash_chances", "[map][bash]" )
 
     for( const bash_test_set &set : test_data::bash_tests ) {
         test_bash_set( set );
+    }
+}
+
+TEST_CASE( "map_bash_ephemeral_persistence", "[map][bash]" )
+{
+    clear_map();
+    map &here = get_map();
+
+    const tripoint_bub_ms test_pt( 40, 40, 0 );
+
+    // Assumptions
+    REQUIRE( furn_test_f_bash_persist->bash.str_min == 4 );
+    REQUIRE( furn_test_f_bash_persist->bash.str_max == 100 );
+    REQUIRE( ter_test_t_bash_persist->bash.str_min == 4 );
+    REQUIRE( ter_test_t_bash_persist->bash.str_max == 100 );
+
+    SECTION( "bashing a furniture to completion leaves behind no map bash info" ) {
+        here.furn_set( test_pt, furn_test_f_bash_persist );
+
+        REQUIRE( here.furn( test_pt ) == furn_test_f_bash_persist );
+        REQUIRE( here.get_map_damage( test_pt ) == 0 );
+        // One above str_min, but well below str_max
+        here.bash( test_pt.raw(), 5 );
+        // Does not destroy it
+        CHECK( here.furn( test_pt ) == furn_test_f_bash_persist );
+        // There is any map damage
+        CHECK( here.get_map_damage( test_pt ) > 0 );
+        // Bash it again to destroy it
+        here.bash( test_pt.raw(), 999 );
+        CHECK( here.furn( test_pt ) != furn_test_f_bash_persist );
+        // Then, it is gone and there is no map damage
+        CHECK( here.furn( test_pt ) != furn_test_f_bash_persist );
+        CHECK( here.get_map_damage( test_pt ) == 0 );
+    }
+    SECTION( "bashing a terrain to completion leaves behind no map bash info" ) {
+        here.ter_set( test_pt, ter_test_t_bash_persist );
+
+        REQUIRE( here.ter( test_pt ) == ter_test_t_bash_persist );
+        REQUIRE( here.get_map_damage( test_pt ) == 0 );
+        // One above str_min, but well below str_max
+        here.bash( test_pt.raw(), 5 );
+        // Does not destroy the terrain
+        CHECK( here.ter( test_pt ) == ter_test_t_bash_persist );
+        // There is any map damage
+        CHECK( here.get_map_damage( test_pt ) > 0 );
+        // Bash it again to destroy it
+        here.bash( test_pt.raw(), 999 );
+        CHECK( here.ter( test_pt ) != ter_test_t_bash_persist );
+        // Then, it is gone and there is no map damage
+        CHECK( here.ter( test_pt ) != ter_test_t_bash_persist );
+        CHECK( here.get_map_damage( test_pt ) == 0 );
+    }
+    SECTION( "when a terrain changes, map damage is lost" ) {
+        here.ter_set( test_pt, ter_test_t_bash_persist );
+
+        REQUIRE( here.ter( test_pt ) == ter_test_t_bash_persist );
+        REQUIRE( here.get_map_damage( test_pt ) == 0 );
+        // One above str_min, but well below str_max
+        here.bash( test_pt.raw(), 5 );
+        // Does not destroy the terrain
+        CHECK( here.ter( test_pt ) == ter_test_t_bash_persist );
+        // There is any map damage
+        CHECK( here.get_map_damage( test_pt ) > 0 );
+        // Change the terrain
+        here.ter_set( test_pt, ter_test_t_pit_shallow );
+        CHECK( here.ter( test_pt ) == ter_test_t_pit_shallow );
+        CHECK( here.get_map_damage( test_pt ) == 0 );
+    }
+    SECTION( "when a furniture changes, map damage is lost" ) {
+        here.furn_set( test_pt, furn_test_f_bash_persist );
+
+        REQUIRE( here.furn( test_pt ) == furn_test_f_bash_persist );
+        REQUIRE( here.get_map_damage( test_pt ) == 0 );
+        // One above str_min, but well below str_max
+        here.bash( test_pt.raw(), 5 );
+        // Does not destroy the terrain
+        CHECK( here.furn( test_pt ) == furn_test_f_bash_persist );
+        // There is any map damage
+        CHECK( here.get_map_damage( test_pt ) > 0 );
+        // Change the terrain
+        here.furn_set( test_pt, furn_test_f_eoc );
+        CHECK( here.furn( test_pt ) == furn_test_f_eoc );
+        CHECK( here.get_map_damage( test_pt ) == 0 );
     }
 }
