c650050104e537d6adb27ae9efd1c6d7e414f281
inogenous <123803852+inogenous@users.noreply.github.com>
2023-11-27 19:33:11 +0100

    Bugfix: Clear AIM pane when filtering items
    * Clear the whole AIM pane before showing the text that explains how to filter
      items in AIM.
    * Prevents showing both the previous pane contents (list of items) behind the
      explanation text.

--- src/advanced_inv.cpp
+++ src/advanced_inv.cpp
@@ -1642,7 +1642,7 @@ void advanced_inventory::display()
             redraw_sidebar();
 
             if( filter_edit && spopup ) {
-                draw_item_filter_rules( panes[dest].window, 1, 11, item_filter_type::FILTER );
+                draw_item_filter_rules( panes[dest].window, 1, w_height - 2, item_filter_type::FILTER );
                 mvwprintz( panes[src].window, point( 2, getmaxy( panes[src].window ) - 1 ), c_cyan, "< " );
                 mvwprintz( panes[src].window, point( w_width / 2 - 4, getmaxy( panes[src].window ) - 1 ), c_cyan,
                            " >" );
