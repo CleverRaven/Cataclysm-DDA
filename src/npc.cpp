924079e778f03b0053bae24ede1f52acf0ebe02d
I-am-Erk <45136638+I-am-Erk@users.noreply.github.com>
2023-12-02 09:25:53 -0800

    Many small logic adjustments and bugfixes for NPC targeting / threat assessment (#69840)
    * Cleanup and small logic adjustments
    
    rename memory function to match other caches
    fix a small oversight with melee engagement
    Adjust swarming range to use weapon ranges instead of arbitrary numbers
    
    * missed a word
    
    * astyle
    
    * monster difficulty assessment adjustments
    
    added armour calculations
    added some special attack calculations
    
    * fix? npc yo-yo behaviour when repositioning
    
    * minor fix
    
    * allow NPCs to pursue hit-and-run enemies
    
    * astyle
    
    * Change several NPC default settings
    
    * don't pulp if far from player
    
    * assstyle
    
    * fix bugs, balance changes. The usual.
    
    * pulping corpses seeks within engagement distance
    
    * revert monster diff changes, for now
    
    We can return here later but I want this PR done.
    
    * update test
    
    * fix two clang errors, one test left
    
    * Update npc_talk_test.cpp
    
    set tests to use a flag that's less likely to change. It is unlikely we'll set "forbid engagement" to default to on.
    
    * Update npc_talk_test.cpp
    
    * Update npc_talk_test.cpp
    
    try disabling this line of the test because I have a headache
    
    * Update npc_talk_test.cpp
    
    * Update TALK_TEST.json
    
    * Update data/json/npcs/TALK_TEST.json
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/npc.cpp
+++ src/npc.cpp
@@ -118,6 +118,7 @@ static const mfaction_str_id monfaction_bee( "bee" );
 static const mfaction_str_id monfaction_human( "human" );
 static const mfaction_str_id monfaction_player( "player" );
 
+static const mon_flag_str_id mon_flag_HIT_AND_RUN( "HIT_AND_RUN" );
 static const mon_flag_str_id mon_flag_RIDEABLE_MECH( "RIDEABLE_MECH" );
 
 static const overmap_location_str_id overmap_location_source_of_ammo( "source_of_ammo" );
@@ -2629,7 +2630,11 @@ Creature::Attitude npc::attitude_to( const Creature &other ) const
         case MATT_FPASSIVE:
         case MATT_IGNORE:
         case MATT_FLEE:
-            return Attitude::NEUTRAL;
+            if( m.has_flag( mon_flag_HIT_AND_RUN ) ) {
+                return Attitude::HOSTILE;
+            } else {
+                return Attitude::NEUTRAL;
+            }
         case MATT_FRIEND:
             return Attitude::FRIENDLY;
         case MATT_ATTACK:
@@ -3781,8 +3786,8 @@ npc_follower_rules::npc_follower_rules()
     override_enable = ally_rule::DEFAULT;
 
     set_flag( ally_rule::use_guns );
-    set_flag( ally_rule::use_grenades );
-    clear_flag( ally_rule::use_silent );
+    clear_flag( ally_rule::use_grenades );
+    set_flag( ally_rule::use_silent );
     set_flag( ally_rule::avoid_friendly_fire );
 
     clear_flag( ally_rule::allow_pick_up );
@@ -3790,13 +3795,13 @@ npc_follower_rules::npc_follower_rules()
     clear_flag( ally_rule::allow_sleep );
     set_flag( ally_rule::allow_complain );
     set_flag( ally_rule::allow_pulp );
-    clear_flag( ally_rule::close_doors );
-    clear_flag( ally_rule::follow_close );
+    set_flag( ally_rule::close_doors );
+    set_flag( ally_rule::follow_close );
     clear_flag( ally_rule::avoid_doors );
     clear_flag( ally_rule::hold_the_line );
-    clear_flag( ally_rule::ignore_noise );
+    set_flag( ally_rule::ignore_noise );
     clear_flag( ally_rule::forbid_engage );
-    set_flag( ally_rule::follow_distance_2 );
+    clear_flag( ally_rule::follow_distance_2 );
 }
 
 bool npc_follower_rules::has_flag( ally_rule test, bool check_override ) const
