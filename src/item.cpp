2b7300d21364e0660531feeb7745b0e3acac7f89
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2023-12-12 01:32:35 -0800

    Update src/item.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/item.cpp
+++ src/item.cpp
@@ -13989,11 +13989,12 @@ std::string item::get_plant_name() const
     return type->seed->plant_name.translated();
 }
 
-std::optional<furn_str_id> item::get_plant_seedling_form() const {
-  if ( !type->seed ) {
-    return std::nullopt;
-  }
-  return type->seed->seedling_form;
+std::optional<furn_str_id> item::get_plant_seedling_form() const
+{
+    if( !type->seed ) {
+        return std::nullopt;
+    }
+    return type->seed->seedling_form;
 }
 
 std::optional<furn_str_id> item::get_plant_mature_form() const
