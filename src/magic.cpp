2ae6aff01ae77d2cce80f7f411a4522c65761889
Standing-Storm <120433252+Standing-Storm@users.noreply.github.com>
2023-12-16 14:08:03 -0600

    [MoM] Add Beast Taming telepathic power (#70198)
    * Initial commit
    
    * Initial commit
    
    * Add some more mutants
    
    * Update src/magic_spell_effect.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/magic_spell_effect.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/magic_spell_effect.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Fix errors
    
    * Update src/magic_spell_effect.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/magic_spell_effect.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/magic.cpp
+++ src/magic.cpp
@@ -159,6 +159,7 @@ std::string enum_to_string<spell_flag>( spell_flag data )
         case spell_flag::SPAWN_WITH_DEATH_DROPS: return "SPAWN_WITH_DEATH_DROPS";
         case spell_flag::NON_MAGICAL: return "NON_MAGICAL";
         case spell_flag::PSIONIC: return "PSIONIC";
+        case spell_flag::RECHARM: return "RECHARM";
         case spell_flag::LAST: break;
     }
     cata_fatal( "Invalid spell_flag" );
