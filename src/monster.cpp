1663c8e32d43a2a6d3bbc503567798b88dabd9b8
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2023-12-21 06:06:51 -0800

    Update src/monster.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/monster.cpp
+++ src/monster.cpp
@@ -3352,7 +3352,7 @@ void monster::process_effects()
         units::volume capacity = 0_ml;
         units::volume free_cargo = 0_ml;
         auto cargo_parts = veh.get_parts_at( z_pos, "CARGO", part_status_flag::any );
-        for( vehicle_part * &part : cargo_parts ) {
+        for( vehicle_part*&part : cargo_parts ) {
             vehicle_stack contents = veh.get_items( *part );
             const vpart_info &vpinfo = part->info();
             if( !vp.part_with_feature( "CARGO_PASSABLE", true ) ) {
