a65565248c49a4fe458d28e134082fefd09231b0
Physics Enthusiast <118960463+physics-enthusiast@users.noreply.github.com>
2023-12-24 16:32:21 +0800

    correct viewport corner naming

--- src/cata_tiles.cpp
+++ src/cata_tiles.cpp
@@ -1355,13 +1355,13 @@ void cata_tiles::draw( const point &dest, const tripoint &center, int width, int
     }
 
     //invalidate draw_points_cache if viewport dimensions have changed
-    point bottom_left( min_col, min_row );
-    point top_right( max_col, max_row );
-    if( bottom_left != here.prev_bottom_left || top_right != here.prev_top_right || o != here.prev_o ) {
+    point bottom_right( min_col, min_row );
+    point top_left( max_col, max_row );
+    if( bottom_right != here.prev_bottom_right || top_left != here.prev_top_left || o != here.prev_o ) {
         set_draw_cache_dirty();
     }
-    here.prev_bottom_left = bottom_left;
-    here.prev_top_right = top_right;
+    here.prev_bottom_right = bottom_right;
+    here.prev_top_left = top_left;
     here.prev_o = o;
 
     you.prepare_map_memory_region(
--- src/map.h
+++ src/map.h
@@ -2352,8 +2352,8 @@ class map
 #if defined(TILES)
         bool draw_points_cache_dirty = true;
         std::map<int, std::map<int, std::vector<tile_render_info>>> draw_points_cache;
-        point prev_bottom_left;
-        point prev_top_right;
+        point prev_bottom_right;
+        point prev_top_left;
         point prev_o;
         std::multimap<point, formatted_text> overlay_strings_cache;
         color_block_overlay_container color_blocks_cache;
