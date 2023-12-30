e771382d08751b7381d7092fd4e6046a35287cfd
Procyonae <45432782+Procyonae@users.noreply.github.com>
2023-12-30 19:06:30 +0000

    Revert unrelated changes

--- src/omdata.h
+++ src/omdata.h
@@ -176,7 +176,6 @@ enum class oter_flags : int {
     should_not_spawn,
     water,
     river_tile,
-    bridge,
     has_sidewalk,
     ignore_rotation_for_adjacency,
     line_drawing, // does this tile have 8 versions, including straights, bends, tees, and a fourway?
