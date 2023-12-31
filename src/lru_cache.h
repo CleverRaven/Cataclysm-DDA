7203e424369a517e2696f8c584522ed068360ee0
prharvey <2677507+prharvey@users.noreply.github.com>
2023-12-30 19:10:11 -0700

    Make the LRU cache actually an LRU cache.

--- src/lru_cache.h
+++ src/lru_cache.h
@@ -3,13 +3,8 @@
 #define CATA_SRC_LRU_CACHE_H
 
 #include <list>
-#include <memory>
-#include <string>
 #include <unordered_map>
 #include <utility>
-
-#include "enums.h" // IWYU pragma: keep
-#include "point.h"
 
 template<typename Key, typename Value>
 class lru_cache
@@ -17,16 +12,77 @@ class lru_cache
     public:
         using Pair = std::pair<Key, Value>;
 
-        void insert( int limit, const Key &, const Value & );
         Value get( const Key &, const Value &default_ ) const;
+        void insert( int limit, const Key &, const Value & );
         void remove( const Key & );
 
         void clear();
-        const std::list<Pair> &list() const;
-    private:
+    protected:
         void trim( int limit );
-        std::list<Pair> ordered_list;
+        void touch( typename std::list<Pair>::iterator iter ) const;
+        mutable std::list<Pair> ordered_list;
         std::unordered_map<Key, typename std::list<Pair>::iterator> map;
 };
+
+template<typename Key, typename Value>
+inline Value lru_cache<Key, Value>::get( const Key &pos, const Value &default_ ) const
+{
+    if( const auto found = this->map.find( pos ); found != this->map.end() ) {
+        const auto list_iter = found->second;
+        this->touch( list_iter );
+        return list_iter->second;
+    }
+    return default_;
+}
+
+template<typename Key, typename Value>
+inline void lru_cache<Key, Value>::remove( const Key &pos )
+{
+    if( const auto found = map.find( pos ); found != map.end() ) {
+        ordered_list.erase( found->second );
+        map.erase( found );
+    }
+}
+
+template<typename Key, typename Value>
+inline void lru_cache<Key, Value>::insert( int limit, const Key &pos, const Value &t )
+{
+    auto found = map.find( pos );
+    if( found == map.end() ) {
+        // Need new entry in map.  Make the new list entry and point to it.
+        ordered_list.emplace_back( pos, t );
+        map.emplace( pos, std::prev( ordered_list.end() ) );
+        trim( limit );
+    } else {
+        // Splice existing entry to the back.  Does not invalidate the
+        // iterator, so no need to change the map.
+        auto list_iterator = found->second;
+        touch( list_iterator );
+        // Update the moved item
+        list_iterator->second = t;
+    }
+}
+
+template<typename Key, typename Value>
+inline void lru_cache<Key, Value>::trim( int limit )
+{
+    while( map.size() > static_cast<size_t>( limit ) ) {
+        map.erase( ordered_list.front().first );
+        ordered_list.pop_front();
+    }
+}
+
+template<typename Key, typename Value>
+inline void lru_cache<Key, Value>::touch( typename std::list<Pair>::iterator iter ) const
+{
+    ordered_list.splice( ordered_list.end(), ordered_list, iter );
+}
+
+template<typename Key, typename Value>
+inline void lru_cache<Key, Value>::clear()
+{
+    map.clear();
+    ordered_list.clear();
+}
 
 #endif // CATA_SRC_LRU_CACHE_H
