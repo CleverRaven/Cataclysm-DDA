6f0b77bb72c4ae6d08c3a33e3a69135bf347e21f
George Karfakis <george.karfakis@gmail.com>
2024-01-02 13:56:20 +0200

    fixed dormant zombies reviving over time

--- src/item.cpp
+++ src/item.cpp
@@ -186,6 +186,10 @@ static const matec_id RAPID( "RAPID" );
 
 static const material_id material_wool( "wool" );
 
+static const mon_flag_str_id mon_flag_DORMANT( "DORMANT" );
+static const mon_flag_str_id mon_flag_POISON( "POISON" );
+static const mon_flag_str_id mon_flag_REVIVES( "REVIVES" );
+
 static const morale_type morale_null( "morale_null" );
 
 static const mtype_id debug_mon( "debug_mon" );
@@ -13029,6 +13033,9 @@ bool item::process_corpse( map &here, Character *carrier, const tripoint &pos )
 {
     // some corpses rez over time
     if( corpse == nullptr || damage() >= max_damage() ) {
+        return false;
+    }
+    if( corpse->has_flag( mon_flag_DORMANT ) ) {
         return false;
     }
 
