9d2b698b13e31bee30129b7ab5b586a901a535f4
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2024-01-04 04:24:01 -0800

    Update faction_camp.cpp

--- src/faction_camp.cpp
+++ src/faction_camp.cpp
@@ -4598,8 +4598,8 @@ int om_cutdown_trees( const tripoint_abs_omt &omt_tgt, int chance, bool estimate
             for( tripoint &elem : tree ) {
                 const tripoint &treetop = tripoint( elem.xy(), elem.z + 1 );
                 if( target_bay.ter( treetop ).obj().has_flag( ter_furn_flag::TFLAG_SINGLE_SUPPORT ) ) {
-                    target_bay.ter_set( treetop, t_open_air );
-                }
+                    target_bay.destroy( treetop );
+                    }
                 target_bay.destroy( elem );
                 target_bay.ter_set( elem, t_trunk );
             }
