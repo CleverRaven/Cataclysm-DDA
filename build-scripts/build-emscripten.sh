a14e5650d41cac29a1af39276019b4075398d12f
Jeremy Rose <nornagon@nornagon.net>
2024-01-05 15:05:10 -0800

    automatically detect parallelism

--- build-scripts/build-emscripten.sh
+++ build-scripts/build-emscripten.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
 emsdk install 3.1.51
 emsdk activate 3.1.51
 
-make -j24 NATIVE=emscripten BACKTRACE=0 TILES=1 TESTS=0 RUNTESTS=0 RELEASE=1 cataclysm-tiles.js
+make -j`nproc` NATIVE=emscripten BACKTRACE=0 TILES=1 TESTS=0 RUNTESTS=0 RELEASE=1 cataclysm-tiles.js
