fe2deb192bd3bb0a6263159abe8d3d8488f568e7
Jeremy Rose <nornagon@nornagon.net>
2024-01-05 14:57:19 -0800

    github action

--- build-scripts/prepare-web.sh
+++ build-scripts/prepare-web.sh
@@ -3,5 +3,6 @@ cp index.html build/
 cp cataclysm-tiles.data build/
 cp cataclysm-tiles.js build/
 cp cataclysm-tiles.wasm build/
+cp cataclysm-tiles.wasm.debug.wasm build/
 cp data/cataicon.ico build/favicon.ico
 cp data/font/Terminus.ttf build/Terminus.ttf
