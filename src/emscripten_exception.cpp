18cbdd7bcabb317cbe4cfbbb6e1ab8cd53b1c9ba
Jeremy Rose <nornagon@nornagon.net>
2024-01-05 17:07:54 -0800

    remove emscripten_exception.cpp

--- src/emscripten_exception.cpp
+++
@@ -1,9 +0,0 @@
-#include <emscripten/bind.h>
-
-std::string getExceptionMessage(intptr_t exceptionPtr) {
-  return std::string(reinterpret_cast<std::exception *>(exceptionPtr)->what());
-}
-
-EMSCRIPTEN_BINDINGS(Bindings) {
-  emscripten::function("getExceptionMessage", &getExceptionMessage);
-};
