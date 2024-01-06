6f928a2731df167d0c86c7f60842e07056fa770c
Jeremy Rose <nornagon@nornagon.net>
2024-01-05 16:50:46 -0800

    allow all tilesets, simplify obsolete mod removal

--- build-scripts/prepare-web-data.sh
+++ build-scripts/prepare-web-data.sh
@@ -3,42 +3,20 @@ set -exo pipefail
 
 rm -rf web_bundle
 
-DATA_DIR=web_bundle/data/
+BUNDLE_DIR=web_bundle
+DATA_DIR=$BUNDLE_DIR/data
 mkdir -p $DATA_DIR
-
-cp data/fontdata.json $DATA_DIR
-cp -R data/core $DATA_DIR
-cp -R data/font $DATA_DIR
-cp -R data/json $DATA_DIR
-cp -R data/mods $DATA_DIR
-cp -R data/names $DATA_DIR
-cp -R data/raw $DATA_DIR
-cp -R data/motd $DATA_DIR
-cp -R data/credits $DATA_DIR
-cp -R data/title $DATA_DIR
-cp -R data/help $DATA_DIR
-
-cp -R gfx web_bundle/
+cp -R data/{core,font,fontdata.json,json,mods,names,raw,motd,credits,title,help} $DATA_DIR/
+cp -R gfx $BUNDLE_DIR/
 
 # Remove .DS_Store files.
 find web_bundle -name ".DS_Store" -type f -exec rm {} \;
 
-# Remove many non-default tilesets.
-echo "Removing non-default tilesets..."
-rm -rf web_bundle/gfx/MshockXotto+
-rm -rf web_bundle/gfx/ChestHole16Tileset
-rm -rf web_bundle/gfx/ChibiUltica
-rm -rf web_bundle/gfx/Cuteclysm
-rm -rf web_bundle/gfx/BrownLikeBears
-rm -rf web_bundle/gfx/HoderTileset
-#rm -rf web_bundle/gfx/NeoDaysTileset
-rm -rf web_bundle/gfx/RetroDaysTileset
-
 # Remove obsolete mods.
 echo "Removing obsolete mods..."
-for MOD_DIR in web_bundle/data/mods/*/ ; do
-    if cat "$MOD_DIR/modinfo.json" | jq '.[] | select(.type == "MOD_INFO") | .obsolete == true' | grep -q 'true'; then
-        echo "$MOD_DIR is obsolete... Removing..."
+for MOD_DIR in $DATA_DIR/mods/*/ ; do
+    if jq -e '.[] | select(.type == "MOD_INFO") | .obsolete' "$MOD_DIR/modinfo.json" >/dev/null; then
+        echo "$MOD_DIR is obsolete, excluding from web_bundle..."
         rm -rf $MOD_DIR
     fi
 done
