99db8caf72273e0f3a2728a24353a014e72dab1f
EI IKaO <109350502+EIIKaO@users.noreply.github.com>
2024-01-08 08:26:45 +0900

    Add pocket_type::SOFTWARE
    Co-authored-by:Kamayana <Kamayana@users.noreply.github.com>

--- src/item_contents.cpp
+++ src/item_contents.cpp
@@ -675,6 +675,7 @@ void item_contents::combine( const item_contents &read_input, const bool convert
                     pocket.is_type( pocket_type::CORPSE ) ||
                     pocket.is_type( pocket_type::MAGAZINE ) ||
                     pocket.is_type( pocket_type::MAGAZINE_WELL ) ||
+                    pocket.is_type( pocket_type::SOFTWARE ) ||
                     pocket.is_type( pocket_type::EBOOK ) ) {
                     ++pocket_index;
                     for( const item *it : pocket.all_items_top() ) {
