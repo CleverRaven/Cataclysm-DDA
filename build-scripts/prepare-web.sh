99eb8b6ef4b272914a56e32dc854226dde51f1e7
Jeremy Rose <nornagon@nornagon.net>
2024-01-07 12:02:43 -0800

    we don't generate the debug.wasm any more

--- build-scripts/prepare-web.sh
+++ build-scripts/prepare-web.sh
@@ -4,7 +4,7 @@ set -exo pipefail
 mkdir -p build/
 cp \
   index.html \
-  cataclysm-tiles.{data,js,wasm,wasm.debug.wasm} \
+  cataclysm-tiles.{data,js,wasm} \
   data/font/Terminus.ttf \
   build
 cp data/cataicon.ico build/favicon.ico
