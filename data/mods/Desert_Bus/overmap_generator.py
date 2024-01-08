f843e0cd57b01978601cb61b01e007f7dc82b63f
Procyonae <45432782+Procyonae@users.noreply.github.com>
2024-01-08 18:28:57 +0000

    Half functional curve, half functional linear curve placement

--- data/mods/Desert_Bus/overmap_generator.py
+++ data/mods/Desert_Bus/overmap_generator.py
@@ -10,103 +10,120 @@ from the origin up to the specified range using the template,
 from inspect import getsourcefile
 from os.path import dirname
 from bitarray import bitarray
+import glob
 import os
 import gzip
 import random
 import math
 
 filler_type = 'field' # Designed to be field (which gets changed by desert_region)
-road_type = 'subway' # Designed to be a road or subway, has to be a linear OMT
+road_type = 'road' # Designed to be a road or subway, has to be a linear OMT
 vertical = True # Whether the road goes north-south or east-west
 windy = True # Whether the road gently winds back and forth, forces north-south
-overmaps = 11 # Number of overmaps to generate, must be odd
+overmaps = 3 # Number of overmaps to generate, must be odd
 
 dir_path = dirname(getsourcefile(lambda: 0))
 overmaps_path = os.path.join(dir_path + '/overmaps')
+if road_type == 'road':
+    overmaps_path = os.path.join(overmaps_path + '/road')
+    if windy:
+        overmaps_path = os.path.join(overmaps_path + '/windy')
+elif road_type == 'subway':
+    overmaps_path = os.path.join(overmaps_path + '/railroad')
+else:
+    overmaps_path = os.path.join(overmaps_path + '/custom')
+
+for filename in glob.glob(overmaps_path + '/*.gz'): # delete old maps in case overmaps has changed
+            os.remove(filename)
 
 def clamp(x, minimum, maximum):
     return max(minimum, min(x, maximum))
 
 all_oms_bits = []
-all_oms_bits_connected = []
-y = 0
+all_oms_bits_needs_populating = True
+most_negative_om_y = -1 * round((overmaps - 1) / 2)
+most_positive_om_y = round((overmaps - 1) / 2)
 
-for om_y in range(-1 * round((overmaps - 1) / 2), round((overmaps - 1) / 2) + 1):
+for om_y in range(most_negative_om_y, most_positive_om_y + 1):
     full_om = ''
     if windy:
-        if om_y == (-1 * round((overmaps - 1) / 2)): # figure out all the windiness in advance
-            empty_row_bits = 180 * bitarray('0')
-            min_amplitude = 10
-            max_amplitude = 30
-            max_amplitude_difference = 5 # max change between two winds from the last amplitude
+        if all_oms_bits_needs_populating: # figure out all the windiness in advance
+            y = 0
+            min_amplitude = 3
+            max_amplitude = 15
+            max_amplitude_difference = 3 # max change between two winds from the last amplitude
             amplitude = max(min_amplitude, max_amplitude * random.random())
-            min_period = 5
-            max_period = 20
-            max_period_difference = 3 # max change between two winds from the last amplitude
+            min_period = 20
+            max_period = 110
+            max_period_difference = 10 # max change between two winds from the last amplitude
             period = max(min_period, max_period * random.random())
+            #x_axis = 89
+            #max_x_axis_difference = 10
             direction = 1 # only do half a sine curve then reset origin and do half a negative sine curve with new period and amplitude
-            #tally = 0
-            #tally2 = 0
             for row in range((overmaps * 180) + 1):
                 x = 89 + round(direction * amplitude * math.sin( ( math.pi * y ) / period ))
                 #print("Row: " + str(row) + " y: " + str(y) + " x: " + str(x))
-                row_bits = empty_row_bits
+                row_bits = 180 * bitarray('0')
                 row_bits[x] = True
                 all_oms_bits.append(row_bits)
                 if (y != 0) and (( x >= 89 and direction == -1 ) or ( x <= 89 and direction == 1 )):
                     if direction == 1:
                         direction = -1
-                        #print("direction changed negative")
                     else:
                         direction = 1
-                        #print("direction changed positive")
-                    minmax = row - y + (y / 2) # math.floor(y / 2)?
-                    #print("row - y = " + str(row - y))
-                    #print("minmax = "+ str(minmax))
-                    #print("y = "+ str(y))
-                    #tally2 += 1
-                    for sub_row in range(row - y + 1, row): # Backfill the points with a "curve"
-                        #tally += 1
-                        if (sub_row != minmax):
+                    current_origin = row - y
+                    minima_maxima = current_origin + (y / 2)
+                    for sub_row in range(current_origin + 1, current_origin + y): # Backfill the points with a "curve"
+                        if (sub_row != minima_maxima):
                             x1 = all_oms_bits[sub_row].find(True)
-                            x2 = all_oms_bits[sub_row + 1 ].find(True)
-                            if ( sub_row == (row - y) ) and ( not ( sub_row == 0 ) ):
-                                print(len(all_oms_bits_connected))
-                                print(len(all_oms_bits))
-                                row_to_alter = all_oms_bits_connected[sub_row]
-                                row_to_alter[x1:x2] = True
-                                all_oms_bits_connected[sub_row] = row_to_alter
-                            elif sub_row < minmax:
-                                new_row = all_oms_bits[sub_row]
-                                new_row[x1:x2] = True
-                                all_oms_bits_connected.append(new_row)
-                            else:
-                                new_row = all_oms_bits[sub_row + 1]
-                                new_row[x1:x2] = True # do x1 and x2 need to be in order?
-                                all_oms_bits_connected.append(new_row)
-                        else:
-                            all_oms_bits_connected.append(all_oms_bits[sub_row])
+                            x2 = all_oms_bits[sub_row + 1].find(True)
+                            x1_alt = all_oms_bits[sub_row].find(True, right=True)
+                            x2_alt = all_oms_bits[sub_row + 1].find(True, right=True)
+                            if abs(x1_alt - x2_alt) < abs(x1 - x2):
+                                x1 = x1_alt
+                                x2 = x2_alt
+                            if x1 > x2:
+                                temp = x1
+                                x1 = x2
+                                x2 = temp
+                            if x1 != x2:
+                                #if x2 > 89:
+                                #    x2 += 1
+                                #if x1 < 89:
+                                #    x1 -= 1
+                                if ( sub_row == (current_origin + 1) ) and ( sub_row != 0 ):
+                                    all_oms_bits[sub_row][x1:x2] = True
+                                elif sub_row < minima_maxima:
+                                    all_oms_bits[sub_row][x1:x2] = True
+                                else:
+                                    all_oms_bits[sub_row + 1][x1:x2] = True
                     y = 0
                     period += max_period_difference * 2 * (random.random() - 0.5) # weight these so they go towards the mean of min max not get stuck at the edges
                     amplitude += max_amplitude_difference * 2 * (random.random() - 0.5)
                     period = clamp(round(period), min_period, max_period)
                     amplitude = clamp(round(amplitude), min_amplitude, max_amplitude)
+                    #min_x_axis = amplitude * 2
+                    #max_x_axis = 180 - amplitude * 2
+                    #x_axis += max_x_axis_difference * 2 * (random.random() - 0.5)
+                    #x_axis = clamp(round(x_axis), min_x_axis, max_x_axis)
                 else:
                     y += 1
-                    #print("y = "+ str(y))
-                #print("tally = " + str(tally))
-                #print("tally2 = " + str(tally))
+            all_oms_bits_needs_populating = False
+                # need partial curve at end handling
         #if not vertical:
             #rotate the mess above?
-        print("length of all_oms_bits_connected = " + str(len(all_oms_bits_connected)))
-        print("length of all_oms_bits = " + str(len(all_oms_bits)))
-        for om_row_n in range(om_y * 180,(om_y + 1) * 180):
-            om_row_bits = all_oms_bits_connected[om_row_n]
+        for om_row_n in range( (om_y - most_negative_om_y) * 180,(om_y - most_negative_om_y + 1) * 180): #needs translating
+            om_row_bits = all_oms_bits[om_row_n].copy()
             filler_start_amount = om_row_bits.find(True)
             road_amount = om_row_bits.count(True)
             filler_end_amount = 180 - filler_start_amount - road_amount
-            om_row = '["' + filler_type + '",' + str(filler_start_amount) + '],["' + road_type + '_ns",' + str(road_amount) + '],["' + filler_type + '",' + str(filler_end_amount) + ']' # need to make the road bend at the ends
-            if om_row_n != (((om_y + 1) * 180 - 1)):
+            if road_amount == 1:
+                om_row = '["' + filler_type + '",' + str(filler_start_amount) + '],["' + road_type + '_ns",1],["' + filler_type + '",' + str(filler_end_amount) + ']'
+            elif road_amount == 2:
+                om_row = '["' + filler_type + '",' + str(filler_start_amount) + '],["' + road_type + '_es",1],["'+ road_type + '_wn",1],["' + filler_type + '",' + str(filler_end_amount) + ']'
+            else:
+                om_row = '["' + filler_type + '",' + str(filler_start_amount) + '],["' + road_type + '_es",1],["'+ road_type + '_ew",' + str(road_amount-2) + '],["'+ road_type + '_wn",1],["' + filler_type + '",' + str(filler_end_amount) + ']'
+            if om_row_n != (((om_y - most_negative_om_y + 1) * 180) - 1):
                 full_om = full_om + om_row + ','
             else:
                 full_om = full_om + om_row
