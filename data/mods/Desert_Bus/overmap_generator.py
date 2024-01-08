4e6b03880fe92ec576a90e7a729c99896033eeb3
Procyonae <45432782+Procyonae@users.noreply.github.com>
2024-01-08 13:33:32 +0000

    Completely different windy approach

--- data/mods/Desert_Bus/overmap_generator.py
+++ data/mods/Desert_Bus/overmap_generator.py
@@ -9,83 +9,86 @@ from the origin up to the specified range using the template,
 """
 from inspect import getsourcefile
 from os.path import dirname
+from bitarray import bitarray
 import os
 import gzip
 import random
+import math
 
 filler_type = 'field' # Designed to be field (which gets changed by desert_region)
 road_type = 'subway' # Designed to be a road or subway, has to be a linear OMT
 vertical = True # Whether the road goes north-south or east-west
 windy = True # Whether the road gently winds back and forth, forces north-south
+overmaps = 21 # Number of overmaps to generate, must be odd
 
 dir_path = dirname(getsourcefile(lambda: 0))
 overmaps_path = os.path.join(dir_path + '/overmaps')
 
-for om_y in range(-20, 21):
+def clamp(x, minimum, maximum):
+    return max(minimum, min(x, maximum))
 
+full_om = ''
+all_oms_bits = []
+
+for om_y in range(-1 * round((overmaps - 1) / 2), round((overmaps - 1) / 2) + 1):
     if windy:
-        om_straighaway_row = '["' + filler_type + '",89],["' + road_type + '_ns",1],["' + filler_type + '",90]'
-        road_x = 89
-        chance_to_wind = 0.1 # chance to begin a wind
-        minimum_wind = 3 # minimum distance a wind can vary from the centre
-        maximum_wind = 10
-        winding_east = False # winding east of the centre
-        winding_west = False
-        winding_away = False # winding away from the centre
-        winding_back = False
-        road_width = 1
-        full_om = om_straighaway_row + ','
-        for omt_y in range(0, 178):
-            if ( 1 + omt_y + maximum_wind ) >= 180:
-                # prevent new winds and wind back
-            else:
-                winding = winding_east or winding_west
-                if road_x == 89:
-                    if winding_back:
-                        winding_east = False
-                        winding_west = False
-                        winding_back = False
-                        road_width = 1
-                    elif random.random() < 0.1:
-                        winding = True
-                        winding_away = True
-                        road_width = 2
-                if not winding:
-                    full_om = full_om + om_straighaway_row + ','
-                elif winding_away:
-                    om_row = '["' + filler_type + '",' + str(road_x - (road_width - 1) ) + '],["' + road_type + '_ns",' + str(road_width) + '],["' + filler_type + '",' + str(180 - road_x) + ']'
-                    full_om = full_om + om_row + ','
-                    road_x += road_width # check for passing centre
-                    offset = abs(road_x - 89)
-                    if offset > minimum_wind and ( offset >= maximum_wind or random.random < ( offset / maximum_wind ) ):
-                        winding_away = False
-                        road_width = 1
-                    elif random.random() < 0.5:
-                        road_width = 1
+        if om_y == (-1 * round((overmaps - 1) / 2)): # figure out all the windiness in advance
+            om_row_bits = 180 * bitarray('0')
+            for row in range((overmaps * 180) + 1):
+                all_oms_bits.append(om_row_bits)
+            y = 0
+            min_amplitude = 10
+            max_amplitude = 30
+            max_amplitude_difference = 5 # max change between two winds from the last amplitude
+            amplitude = min_amplitude + (max_amplitude * random.random())
+            min_period = 5
+            max_period = 20
+            max_period_difference = 3 # max change between two winds from the last amplitude
+            period = min_period + (max_period * random.random())
+            direction = 1 # only do half a sine curve then reset origin and do half a negative sine curve with new period and amplitude
+            for row in range((overmaps * 180) + 1):
+                x = 89 + round(direction * amplitude * math.sin( ( math.pi * y ) / period ))
+                all_oms_bits[row][x] = bitarray('1')
+                if x == 89: # probably need to check if it crossed too
+                    if direction == 1:
+                        direction = -1
                     else:
-                        road_width = 2
-                elif winding_back:
-                    om_row = '["' + filler_type + '",' + str(road_x) + '],["' + road_type + '_ns",' + str(road_width) + '],["' + filler_type + '",' + str(180 - road_x - (road_width - 1) ) + ']'
-                    full_om = full_om + om_row + ','
-                    road_x += road_width # check for passing centre
-                else:
-                    om_row = '["' + filler_type + '",' + str(road_x) + '],["' + road_type + '_ns",' + str(road_width) + '],["' + filler_type + '",' + str(180 - road_x) + ']'
-                    full_om = full_om + om_row + ','
-                    if random.random() < 0.4:
-                        winding_back = True
-
-        full_om = full_om + om_straighaway_row
+                        direction = 1
+                    minmax = round(y / 2)
+                    for sub_row in range(minmax): # Backfill the points with a "curve"
+                        x1 = all_oms_bits[row - y + subrow].find('1')
+                        x2 = all_oms_bits[row - y + subrow + 1 ].find('1')
+                        all_oms_bits[row - y + subrow][x1:x2] = 1
+                    for sub_row in range(minmax + 1, y + 1):
+                        x1 = all_oms_bits[row - y + minmax + subrow + 1].find('1')
+                        x2 = all_oms_bits[row - y + minmax + subrow + 2].find('1')
+                        all_oms_bits[row - y + minmax + subrow + 2][x1:x2] = 1 # do x1 and x2 need to be in order?
+                    y = 0
+                    period += max_period_difference * 2 * (random.random() - 0.5) # weight these so they go towards the mean of min max not get stuck at the edges
+                    amplitude += max_amplitude_difference * 2 * (random.random() - 0.5)
+                    period = clamp(round(period), min_period, max_period)
+                    amplitude = clamp(round(amplitude), min_amplitude, max_amplitude)
+                y += 1
+        #if not vertical:
+            #rotate the mess above?
+        for om_row_n in range(om_y * 180,(om_y + 1) * 180):
+            filler_start_amount = all_oms_bits[om_row_n].find('1')
+            road_amount = all_oms_bits[om_row_n].count('1')
+            filler_end_amount = 180 - filler_start - road
+            om_row = '["' + filler_type + '",' + str(filler_start_amount) + '],["' + road_type + '_ns",' + str(road_amount) + '],["' + filler_type + '",' + str(filler_end_amount) + ']' # need to make the road bend at the ends
+            if om_row_n != ((om_y + 1) * 180):
+                full_om = full_om + om_row + ','
+            else:
+                full_om = full_om + om_row
     else:
         if vertical:
             om_row = '["' + filler_type + '",89],["' + road_type + '_ns",1],["' + filler_type + '",90]'
-            full_om = om_row + ','
-            for omt_y in range(0, 178):
+            for omt_y in range(0, 179):
                 full_om = full_om + om_row + ','
             full_om = full_om + om_row
         else:
             om_row = '["' + filler_type + '",180]'
-            full_om = om_row + ','
-            for omt_y in range(0, 89):
+            for omt_y in range(0, 90):
                 full_om = full_om + om_row + ','
             full_om = full_om + '["' + road_type + '_ew",180],'
             for omt_y in range(0, 90):
