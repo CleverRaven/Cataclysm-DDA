cd9a535e1d580ac0aa7b14c1b365e1ef869e24ba
Procyonae <45432782+Procyonae@users.noreply.github.com>
2024-01-08 01:40:39 +0000

    Merge script and add start of winding logic

--- data/mods/Desert_Bus/overmap_generator.py
+++ data/mods/Desert_Bus/overmap_generator.py
@@ -11,44 +11,91 @@ from inspect import getsourcefile
 from os.path import dirname
 import os
 import gzip
+import random
 
-road_type = 2 # 0 for straight road, 1 for windy road, 2 for railroad
+filler_type = 'field' # Designed to be field (which gets changed by desert_region)
+road_type = 'subway' # Designed to be a road or subway, has to be a linear OMT
+vertical = True # Whether the road goes north-south or east-west
+windy = True # Whether the road gently winds back and forth, forces north-south
 
 dir_path = dirname(getsourcefile(lambda: 0))
-overmaps_path = os.path.join(dir_path + "/overmaps")
-template_file_name = ""
-if road_type == 0:
-    template_file_name = "/overmap_template_road.txt"
-elif road_type == 1:
-    template_file_name = "/overmap_template_road.txt"
-elif road_type == 2:
-    template_file_name = "/overmap_template_railroad.txt"
-template_path = os.path.join(dir_path + template_file_name)
-template_first_line = ""
-template_second_line = ""
-template_third_line = ""
-template_fourth_line = ""
-with open(template_path) as template_file:
-    template_first_line = template_file.readline()
-    template_second_line = template_file.readline()
-    template_third_line = template_file.readline()
-    template_fourth_line = template_file.readline()
+overmaps_path = os.path.join(dir_path + '/overmaps')
 
 for om_y in range(-20, 21):
 
-    if road_type == 0 or road_type == 2:
-        full_third_line = template_third_line + ","
+    if windy:
+        om_straighaway_row = '["' + filler_type + '",89],["' + road_type + '_ns",1],["' + filler_type + '",90]'
+        road_x = 89
+        chance_to_wind = 0.1 # chance to begin a wind
+        minimum_wind = 3 # minimum distance a wind can vary from the centre
+        maximum_wind = 10
+        winding_east = False # winding east of the centre
+        winding_west = False
+        winding_away = False # winding away from the centre
+        winding_back = False
+        road_width = 1
+        full_om = om_straighaway_row + ','
         for omt_y in range(0, 178):
-            full_third_line = full_third_line + template_third_line + ","
-        full_third_line = full_third_line + template_third_line
-    elif road_type == 1:
-        full_third_line = ""
-        #do some windy stuff
+            if ( 1 + omt_y + maximum_wind ) >= 180:
+                # prevent new winds and wind back
+            else:
+                winding = winding_east or winding_west
+                if road_x == 89:
+                    if winding_back:
+                        winding_east = False
+                        winding_west = False
+                        winding_back = False
+                        road_width = 1
+                    elif random.random() < 0.1:
+                        winding = True
+                        winding_away = True
+                        road_width = 2
+                if not winding:
+                    full_om = full_om + om_straighaway_row + ','
+                elif winding_away:
+                    om_row = '["' + filler_type + '",' + str(road_x - (road_width - 1) ) + '],["' + road_type + '_ns",' + str(road_width) + '],["' + filler_type + '",' + str(180 - road_x) + ']'
+                    full_om = full_om + om_row + ','
+                    road_x += road_width # check for passing centre
+                    offset = abs(road_x - 89)
+                    if offset > minimum_wind and ( offset >= maximum_wind or random.random < ( offset / maximum_wind ) ):
+                        winding_away = False
+                        road_width = 1
+                    elif random.random() < 0.5:
+                        road_width = 1
+                    else:
+                        road_width = 2
+                elif winding_back:
+                    om_row = '["' + filler_type + '",' + str(road_x) + '],["' + road_type + '_ns",' + str(road_width) + '],["' + filler_type + '",' + str(180 - road_x - (road_width - 1) ) + ']'
+                    full_om = full_om + om_row + ','
+                    road_x += road_width # check for passing centre
+                else:
+                    om_row = '["' + filler_type + '",' + str(road_x) + '],["' + road_type + '_ns",' + str(road_width) + '],["' + filler_type + '",' + str(180 - road_x) + ']'
+                    full_om = full_om + om_row + ','
+                    if random.random() < 0.4:
+                        winding_back = True
 
-    output_file_string = template_first_line + str(om_y) + template_second_line + full_third_line + template_fourth_line
+        full_om = full_om + om_straighaway_row
+    else:
+        if vertical:
+            om_row = '["' + filler_type + '",89],["' + road_type + '_ns",1],["' + filler_type + '",90]'
+            full_om = om_row + ','
+            for omt_y in range(0, 178):
+                full_om = full_om + om_row + ','
+            full_om = full_om + om_row
+        else:
+            om_row = '["' + filler_type + '",180]'
+            full_om = om_row + ','
+            for omt_y in range(0, 89):
+                full_om = full_om + om_row + ','
+            full_om = full_om + '["' + road_type + '_ew",180],'
+            for omt_y in range(0, 90):
+                full_om = full_om + om_row + ','
+            full_om = full_om + om_row
+
+    output_file_string = '[{"type":"overmap","om_pos":[0,' + str(om_y) + '],"layers":[' + full_om + ']}]'
     output_file_string = output_file_string.replace( '\n', '')
     output_file = str.encode(output_file_string)
-    output_filename = "overmap_0_" + str(om_y) + ".omap.gz"
+    output_filename = 'overmap_0_' + str(om_y) + '.omap.gz'
     output_path = os.path.join(overmaps_path, output_filename)
     f = gzip.open(output_path, 'wb')
     f.write(output_file)
