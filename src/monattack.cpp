10a6557dab07dd370167843a433d52c29d6e2131
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2024-02-04 07:37:45 -0800

    Altered Mindstates partially resist Flaming Eyes (#71460)
    * Update monattack.cpp
    
    * dave warns
    
    * fixes
    
    * Update src/monattack.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update src/monattack.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * lint
    
    * Update BEGGAR_2_Dino_Dave.json
    
    ellipsis
    
    * Update monattack.cpp
    
    drunks get stunned for 4 seconds
    
    * Update BEGGAR_2_Dino_Dave.json
    
    Dave mentions voices instead of pills
    
    * Update BEGGAR_2_Dino_Dave.json
    
    * Clarity
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/monattack.cpp
+++ src/monattack.cpp
@@ -116,6 +116,7 @@ static const efftype_id effect_deaf( "deaf" );
 static const efftype_id effect_dermatik( "dermatik" );
 static const efftype_id effect_downed( "downed" );
 static const efftype_id effect_dragging( "dragging" );
+static const efftype_id effect_drunk( "drunk" );
 static const efftype_id effect_eyebot_assisted( "eyebot_assisted" );
 static const efftype_id effect_eyebot_depleted( "eyebot_depleted" );
 static const efftype_id effect_fearparalyze( "fearparalyze" );
@@ -142,6 +143,7 @@ static const efftype_id effect_stunned( "stunned" );
 static const efftype_id effect_taint( "taint" );
 static const efftype_id effect_targeted( "targeted" );
 static const efftype_id effect_tindrift( "tindrift" );
+static const efftype_id effect_took_thorazine( "took thorazine" );
 
 static const gun_mode_id gun_mode_AUTO( "AUTO" );
 
@@ -233,6 +235,7 @@ static const trait_id trait_PROF_FED( "PROF_FED" );
 static const trait_id trait_PROF_PD_DET( "PROF_PD_DET" );
 static const trait_id trait_PROF_POLICE( "PROF_POLICE" );
 static const trait_id trait_PROF_SWAT( "PROF_SWAT" );
+static const trait_id trait_SCHIZOPHRENIC( "SCHIZOPHRENIC" );
 static const trait_id trait_TAIL_CATTLE( "TAIL_CATTLE" );
 static const trait_id trait_THRESH_MARLOSS( "THRESH_MARLOSS" );
 static const trait_id trait_THRESH_MYCUS( "THRESH_MYCUS" );
@@ -2886,6 +2889,23 @@ bool mattack::stare( monster *z )
         if( player_character.worn_with_flag( flag_DIMENSIONAL_ANCHOR ) ||
             player_character.has_flag( flag_DIMENSIONAL_ANCHOR ) ) {
             add_msg( m_warning, _( "You feel a strange reverberation across your body." ) );
+            return true;
+        }//Does the eye fear what it sees in the fractured mind, or is it simply unable to get a good look at it?
+        if( player_character.has_trait( trait_SCHIZOPHRENIC ) &&
+            !player_character.has_effect( effect_took_thorazine ) && x_in_y( 2.0, 3.0 ) ) {
+            if( player_character.sees( *z ) ) {
+                add_msg( m_warning, _( "The %s quickly looks away from you." ), z->name() );
+                return true;
+            } else {
+                add_msg( m_warning, _( "A chorus of whispers chatter raucously around you." ) );
+                return true;
+            }
+        }
+        //Being drunk can protect you from The Horrors
+        if( player_character.has_effect( effect_drunk ) &&
+            ( rng( 1, 6 ) < player_character.get_effect_int( effect_drunk ) ) ) {
+            add_msg( m_warning, _( "The world lurches drunkenly around you." ) );
+            player_character.add_effect( effect_stunned, 4_seconds );
             return true;
         }
         if( player_character.sees( *z ) ) {
