f218f66dac038e2dd92367a88a0241c68ef67834
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2024-02-08 00:45:38 -0800

    Update src/consumption.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/consumption.cpp
+++ src/consumption.cpp
@@ -1219,10 +1219,21 @@ ret_val<edible_rating> Character::will_eat( const item &food, bool interactive )
                                 }
                             }
 
-                            if( you.has_active_bionic( bio_taste_blocker ) && food.get_comestible_fun() < 0 &&
-                                you.get_power_level() > units::from_kilojoule( std::abs( food.get_comestible_fun() ) ) ) {
-                                you.mod_power_level( units::from_kilojoule( food.get_comestible_fun() ) );
-                            }
+                                    if( amorphous ) {
+                                        you.add_msg_player_or_npc( _( "You assimilate your %s." ), _( "<npcname> assimilates a %s." ),
+                                                                   food.tname() );
+                                    } else if( drinkable ) {
+                                        if( you.has_trait( trait_SCHIZOPHRENIC ) &&
+                                            !you.has_effect( effect_took_thorazine ) && one_in( 50 ) && !spoiled && food.goes_bad() &&
+                                            you.is_avatar() ) {
+
+                                            add_msg( m_bad, _( "Ick, this %s (rotten) doesn't taste so good…" ), food.tname() );
+                                            add_msg( _( "You eat your %s (rotten)." ), food.tname() );
+                                        } else {
+                                            you.add_msg_player_or_npc( _( "You eat your %s." ), _( "<npcname> eats a %s." ),
+                                                                       food.tname() );
+                                        }
+                                    }
 
                             if( you.has_active_bionic( bio_taste_blocker ) && food.get_comestible_fun() < 0 &&
                                 you.get_power_level() > units::from_kilojoule( std::abs( food.get_comestible_fun() ) ) ) {
