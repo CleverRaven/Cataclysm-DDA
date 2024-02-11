ff78f8be345ba0148ed8b89f976d068a87752a6b
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2024-02-10 22:26:40 -0800

    Update map.h

--- src/map.h
+++ src/map.h
@@ -1830,11 +1830,6 @@ class map
          * Used for infrared.
          */
         bool pl_line_of_sight( const tripoint &t, int max_range ) const;
-        /**
-         * Basically line_of_sight, but blocked by transparent objects
-         * and other sound baffling
-        */
-        bool pl_line_of_sound( const tripoint &t, int max_range ) const;
         std::set<vehicle *> dirty_vehicle_list;
 
         /** return @ref abs_sub */
