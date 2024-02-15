ed767a14c98327a25f74c7b68ecf81399d07825d
Qrox <qrox@sina.com>
2024-02-16 00:06:01 +0800

    Improve spell checker tokenizer

--- tools/spell_checker/__init__.py
+++ tools/spell_checker/__init__.py
@@ -1,17 +1,43 @@
 #!/usr/bin/env python3
 import os
-import re
+import regex
 from spellchecker import SpellChecker
 
 
 Speller = SpellChecker()
-Tokenizer = re.compile(r'\w+')
+Tokenizer = regex.compile(
+    r"(?<="
+        r"(?:" # a word can be after...
+            r"^" # ...the start of text...
+        r"|" # ...or...
+            r"""[\s\-'‘"“«\*&\(\[\{/>]""" # ...a valid starting delimiter
+        r")"
+        r"[…]*" # ignore possible punctuations before a word
+    r")"
+    # a single letter is always considered a valid word and therefore not
+    # tokenized
+    r"\pL" # a word always starts with an letter...
+    r"(?:\pL|['‘’])*" # ...can contain letters and apostrophes...
+    r"\pL" # ...and always ends with an letter
+    # exclude suffix `'s` and match the apostrophe as a delimiter later
+    r"(?<!'[sS])"
+    # note: staring and ending apostrophe cannot be distinguished from
+    # starting and ending single quote so the former is not considered.
+    r"(?="
+        r"[,\.\?!:;…]*" # ignore any punctuations after a word
+        r"(?:" # a word can be before...
+            r"$" # ...the end of text...
+        r"|" # ...or...
+            r"""[\s\-'’"”»\*\)\]\}/<]""" # ...a valid ending delimiter
+        r")"
+    r")"
+)
 KnownWords = set()
 
 
 def init_known_words():
     dictionary = os.path.join(os.path.dirname(__file__), 'dictionary.txt')
-    with open(dictionary) as fp:
+    with open(dictionary, 'r', encoding='utf-8') as fp:
         for line in fp:
             line = line.rstrip('\n').rstrip('\r')
             KnownWords.add(line.split(' ')[0])
--- tools/spell_checker/gen_dictionary.py
+++ tools/spell_checker/gen_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from . import Speller, Tokenizer
-from .spell_checker import is_english, sanitize_message
 import os
 import polib
 
@@ -11,11 +10,10 @@ def gen_dictionary():
     pofile = polib.pofile(po_path)
     dictionary = set()
     for entry in pofile:
-        words = filter(is_english,
-                       Tokenizer.findall(sanitize_message(entry.msgid)))
+        words = Tokenizer.findall(entry.msgid)
         for word in Speller.unknown(words):
             dictionary.add(word)
     dict_path = os.path.join(os.path.dirname(__file__), "dictionary.txt")
-    with open(dict_path, "w") as fp:
+    with open(dict_path, "w", encoding="utf-8") as fp:
         for word in sorted(dictionary):
             print(word, file=fp)
