7151f1a07cd24c502a225163d48973ef6453c7bd
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2024-02-16 17:39:26 -0800

    Remove social and fang stuff

--- src/martialarts.h
+++ src/martialarts.h
@@ -152,9 +152,6 @@ class ma_technique
         bool dummy = false;
         bool crit_tec = false;
         bool crit_ok = false;
-        // performs the listed technique if this attack procs a crit. tec_none skips this behavior.
-        // requires crit_ok to be true
-        matec_id crit_tec_id = tec_none;
         bool reach_tec = false; // only possible to use during a reach attack
         bool reach_ok = false; // possible to use during a reach attack
         bool attack_override = false; // The attack replaces the one it triggered off of
