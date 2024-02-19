71a196648ee8da746f3395328abacfd4e07c57d2
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2024-02-19 06:05:00 -0800

    Update melee.cpp

--- src/melee.cpp
+++ src/melee.cpp
@@ -795,7 +795,7 @@ bool Character::melee_attack_abstract( Creature &t, bool allow_special,
             }
             d.mult_damage( 0.3 );
         } else if( is_crouching() && ( ( !has_effect( effect_natural_stance ) &&
-                                           !unarmed_attack() ) ||
+                                         !unarmed_attack() ) ||
                                        !has_flag( json_flag_PSEUDOPOD_GRASP ) ) ) {
             d.mult_damage( 0.8 );
         }
