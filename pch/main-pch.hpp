0431adbda13078fff02461901590e9339c20c566
alef <alef@users.noreply.github.com>
2024-02-20 22:15:02 +0100

    Use `sdl_wrappers.h` as suggested by @Qrox

--- pch/main-pch.hpp
+++ pch/main-pch.hpp
@@ -92,21 +92,5 @@
 #endif
 
 #if defined(TILES)
-#   if defined(USE_VCPKG)
-#      include <SDL2/SDL.h>
-#      include <SDL2/SDL_image.h>
-#      include <SDL2/SDL_ttf.h>
-#      include <SDL2/SDL_version.h>
-#      ifdef SDL_SOUND
-#          include <SDL2/SDL_mixer.h>
-#      endif
-#   else
-#      include <SDL.h>
-#      include <SDL_image.h>
-#      include <SDL_ttf.h>
-#      include <SDL_version.h>
-#      ifdef SDL_SOUND
-#          include <SDL_mixer.h>
-#      endif
-#   endif
+#   include <sdl_wrappers.h>
 #endif
