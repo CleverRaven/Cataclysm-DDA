b579bc8918427f06aa093d3605fd42463616c01e
Pipeyume <1178795412@qq.com>
2024-02-20 15:11:13 +0800

    flake8 python linting

--- lang/string_extractor/parsers/effect.py
+++ lang/string_extractor/parsers/effect.py
@@ -105,8 +105,8 @@ def parse_effect(effects, origin, comment=""):
                 for eoc in eoc_list:
                     if type(eoc) is dict and "id" in eoc:
                         parse_effect_on_condition(eoc, origin,
-                                                  comment="nested EOC option in {}"
-                                                  .format(comment))
+                                            comment="nested EOC option in {}"
+                                            .format(comment))
                 for name in eff.get("names", []):
                     write_translation_or_var(name, origin,
                                              comment="EOC option name in {}"
