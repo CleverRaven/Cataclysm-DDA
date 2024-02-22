a2b91d477792f665fcc3e84c98cfda2842ae82ea
fairyarmadillo <94415528+fairyarmadillo@users.noreply.github.com>
2024-02-21 20:33:23 -0800

    astyle

--- src/melee.cpp
+++ src/melee.cpp
@@ -797,7 +797,7 @@ bool Character::melee_attack_abstract( Creature &t, bool allow_special,
             }
             d.mult_damage( 0.3 );
         } else if( is_crouching() && ( ( !has_effect( effect_natural_stance ) && !unarmed_attack() ) ) &&
-                                       !has_flag( json_flag_PSEUDOPOD_GRASP ) ) {
+                   !has_flag( json_flag_PSEUDOPOD_GRASP ) ) {
             d.mult_damage( 0.8 );
         }
 
