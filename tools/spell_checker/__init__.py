2199c2fdd8bf1094df1447ecc205a385f143ed7b
Qrox <qrox@sina.com>
2024-02-25 18:56:14 +0800

    Only consider Latin letters and diacritics in spell checker tokenization

--- tools/spell_checker/__init__.py
+++ tools/spell_checker/__init__.py
@@ -4,6 +4,7 @@ import regex
 from spellchecker import SpellChecker
 
 
+_latin_and_diacritic = r"\p{Script=Latin}|[\u0300-\u036F\u1AB0-\u1AC0\u1DC0-\u1DFF\u20D0-\u20F0\uFE20-\uFE2D]"
 Tokenizer = regex.compile(
     r"(?<="
         r"(?:" # a word can be after...
@@ -15,9 +16,15 @@ Tokenizer = regex.compile(
     r")"
     # a single letter is always considered a valid word and therefore not
     # tokenized
-    r"\pL" # a word always starts with an letter...
-    r"(?:\pL|['‘’])*" # ...can contain letters and apostrophes...
-    r"\pL" # ...and always ends with an letter
+    r"(?:" +
+        _latin_and_diacritic +
+    r")" # a word always starts with an letter...
+    r"(?:" +
+        _latin_and_diacritic +
+    r"|['‘’´])*" # ...can contain letters and apostrophes...
+    r"(?:" +
+        _latin_and_diacritic +
+    r")" # ...and always ends with an letter
     # exclude suffix `'s` and match the apostrophe as a delimiter later
     r"(?<!'[sS])"
     # note: staring and ending apostrophe cannot be distinguished from
@@ -66,12 +73,17 @@ if not KnownWords:
 def unit_test__tokenizer():
     text_and_results = [
         ("I'm a test", ["I'm", "test"]),
+        ("Bahá'í", ["Bahá'í"]), # containing composed character á
+        ("Bahá'í", ["Bahá'í"]), # containing decomposed sequence á
+        ("This is Megingjörð", ["This", "is", "Megingjörð"]),
+        ("Test 测试", ["Test"]),
         ("\"This'll do!\"", ["This'll", "do"]),
         ("Someone's test case", ["Someone", "test", "case"]),
         ("SOMEONE'S VERY IMPORTANT TEST", ["SOMEONE", "VERY", "IMPORTANT", "TEST"]),
         ("tic-tac-toe", ["tic", "tac", "toe"]),
         ("Ünicodé", ["Ünicodé"]),
         ("This apostrophe’s weird", ["This", "apostrophe’s", "weird"]),
+        ("This apostrophe´s weird", ["This", "apostrophe´s", "weird"]),
         ("Foo i18n l10n bar", ["Foo", "bar"]),
         ("Lorem 123 ipsum -- dolor ||| sit", ["Lorem", "ipsum", "dolor", "sit"]),
         ("Lorem", ["Lorem"]),
