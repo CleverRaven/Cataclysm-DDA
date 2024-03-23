47b9304bc6b54b8d027189209bcca99e6af20b2c
Kamayana <pigbuster@gmail.com>
2024-03-22 22:02:29 -0700

    Remove unneeded change
    I thought I'd deleted this change, but I guess it snuck back in at some point. It doesn't have any effect since an appliances pivot_point will be 0,0 at this point anyway

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -2469,7 +2469,7 @@ bool vehicle::split_vehicles( map &here,
         new_vehicle->precalc_mounts( 0, new_vehicle->turn_dir, point() );
         new_vehicle->precalc_mounts( 1, new_vehicle->skidding ?
                                      new_vehicle->turn_dir : new_vehicle->face.dir(),
-                                     is_appliance ? point() : new_vehicle->pivot_point() );
+                                     new_vehicle->pivot_point() );
         if( !passengers.empty() ) {
             new_vehicle->relocate_passengers( passengers );
         }
