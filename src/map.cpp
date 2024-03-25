cbeb88c18b2e97e38a9a0d16279aab109ff4ee93
wwkk222208 <373132166@qq.com>
2024-03-25 23:17:12 +0800

    Astyle

--- src/map.cpp
+++ src/map.cpp
@@ -125,7 +125,7 @@ static const efftype_id effect_pet( "pet" );
 static const field_type_str_id field_fd_clairvoyant( "fd_clairvoyant" );
 
 static const flag_id json_flag_AVATAR_ONLY( "AVATAR_ONLY" );
-static const flag_id json_flag_LEVITATION("LEVITATION");
+static const flag_id json_flag_LEVITATION( "LEVITATION" );
 static const flag_id json_flag_PRESERVE_SPAWN_OMT( "PRESERVE_SPAWN_OMT" );
 static const flag_id json_flag_PROXIMITY( "PROXIMITY" );
 static const flag_id json_flag_UNDODGEABLE( "UNDODGEABLE" );
@@ -9837,8 +9837,7 @@ void map::maybe_trigger_prox_trap( const tripoint &pos, Creature &c, const bool 
     if( tr.is_null() ) {
         return;
     }
-    if (tr == tr_ledge && c.has_effect_with_flag(json_flag_LEVITATION))
-    {
+    if( tr == tr_ledge && c.has_effect_with_flag( json_flag_LEVITATION ) ) {
         return;
     }
     //Don't trigger benign traps like cots and funnels
@@ -9872,8 +9871,7 @@ void map::maybe_trigger_trap( const tripoint &pos, Creature &c, const bool may_a
     if( tr.is_null() ) {
         return;
     }
-    if (tr == tr_ledge && c.has_effect_with_flag(json_flag_LEVITATION))
-    {
+    if( tr == tr_ledge && c.has_effect_with_flag( json_flag_LEVITATION ) ) {
         return;
     }
     //Don't trigger benign traps like cots and funnels
