95f36cbe940b02af6daff326783d32c6ad3e4c1b
Kamayana <pigbuster@gmail.com>
2024-03-26 03:45:17 -0500

    Fixes power grid teleportation, crashes, and other buggy interactions (#71889)
    * Rename OVP to something that's not reused later
    
    * Let can_unmount allow vehicle splits if needed
    
    * Use absolute coords when removing appliances
    
    * Rebuild caches after splitting appliances
    
    * Always make split appliances pivot at 0,0
    
    * Reset power grid pivot when adding new apps
    
    * Add vehicle::unlink_cables() to consolidate code
    
    * Allow removing cabled parts, which removes cables too
    
    * Clarification comment
    
    * Remove line that didn't do anything
    
    * Readjust anchor correction upon merge
    
    * Combine bounding boxes to get correct grid size
    
    Previously, if a grid was exactly at the max size, it would always fail, so you couldn't merge any more parts into it. This new method uses the min/max point of both grid's boxes to get the accurate width.
    
    * Update tow cable check to master branch
    
    * Remove unneeded change
    
    I thought I'd deleted this change, but I guess it snuck back in at some point. It doesn't have any effect since an appliances pivot_point will be 0,0 at this point anyway
    
    * Keep previous power unlink menu string if not an appliance

--- src/vehicle.h
+++ src/vehicle.h
@@ -1020,7 +1020,7 @@ class vehicle
         ret_val<void> can_mount( const point &dp, const vpart_info &vpi ) const;
 
         // @returns true if part \p vp_to_remove can be uninstalled
-        ret_val<void> can_unmount( const vehicle_part &vp_to_remove ) const;
+        ret_val<void> can_unmount( const vehicle_part &vp_to_remove, bool allow_splits = false ) const;
 
         // install a part of type \p type at mount \p dp
         // @return installed part index or -1 if can_mount(...) failed
@@ -1862,6 +1862,16 @@ class vehicle
          * @param dst Future vehicle position, used for calculating cable length when shed_cables == trinary::SOME.
          */
         void shed_loose_parts( trinary shed_cables = trinary::NONE, const tripoint_bub_ms *dst = nullptr );
+        /**
+         * Disconnect cables attached to the specified mount point.
+         * @param mount The mount point to detach cables from.
+         * @param remover The character disconnecting the cables.
+         * @param unlink_items If extension cord and device items should be unlinked.
+         * @param unlink_tow_cables If tow cables should be unlinked.
+         * @param unlink_power_cords If power grid cables (power_cord) should be unlinked.
+         */
+        void unlink_cables( const point &mount, Character &remover, bool unlink_items = false,
+                            bool unlink_tow_cables = false, bool unlink_power_cords = false );
 
         /**
          * @name Vehicle turrets
