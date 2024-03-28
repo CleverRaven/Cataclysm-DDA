1defa381a8127935a2632694bf5c4a7789fb1b68
Standing-Storm <120433252+Standing-Storm@users.noreply.github.com>
2024-03-28 10:31:55 -0500

    Update src/magic.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/magic.cpp
+++ src/magic.cpp
@@ -2507,7 +2507,6 @@ void spellcasting_callback::spell_info_text( const spell &sp, int width )
             cost_string = colorize( _( "Not Enough Energy" ), c_red );
             energy_cur.clear();
         }
-    
         info_txt.emplace_back(
             colorize( string_format( "%s: %s %s%s", cost_string, sp.energy_cost_string( pc ),
                                  sp.energy_string(), energy_cur ), c_light_gray ) );
