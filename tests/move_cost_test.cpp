958773203200fe8445af8f201b78038aed8fe686
Procyonae <45432782+Procyonae@users.noreply.github.com>
2024-04-02 14:58:59 +0100

    /tests

--- tests/move_cost_test.cpp
+++ tests/move_cost_test.cpp
@@ -34,6 +34,9 @@ static const move_mode_id move_mode_prone( "prone" );
 static const move_mode_id move_mode_run( "run" );
 static const move_mode_id move_mode_walk( "walk" );
 
+static const ter_str_id ter_t_grass( "t_grass" );
+static const ter_str_id ter_t_pavement( "t_pavement" );
+
 static const trait_id trait_HOOVES( "HOOVES" );
 static const trait_id trait_LEG_TENTACLES( "LEG_TENTACLES" );
 static const trait_id trait_PADDED_FEET( "PADDED_FEET" );
@@ -99,13 +102,13 @@ TEST_CASE( "footwear_may_affect_movement_cost", "[move_cost][shoes]" )
         REQUIRE( ava.get_modifier( character_modifier_limb_run_cost_mod ) == Approx( 1.11696 ) );
         WHEN( "on pavement and running" ) {
             ava.set_movement_mode( move_mode_run );
-            here.ter_set( ava.pos(), t_pavement );
+            here.ter_set( ava.pos(), ter_t_pavement );
             THEN( "much faster than sneakers" ) {
                 CHECK( ava.run_cost( 100 ) == 27 );
             }
         }
         WHEN( "on grass" ) {
-            here.ter_set( ava.pos(), t_grass );
+            here.ter_set( ava.pos(), ter_t_grass );
             THEN( "much slower than sneakers" ) {
                 CHECK( ava.run_cost( 100 ) == 167 );
             }
@@ -119,13 +122,13 @@ TEST_CASE( "footwear_may_affect_movement_cost", "[move_cost][shoes]" )
         REQUIRE( ava.get_modifier( character_modifier_limb_run_cost_mod ) == Approx( 1.11696 ) );
         WHEN( "on pavement and running" ) {
             ava.set_movement_mode( move_mode_run );
-            here.ter_set( ava.pos(), t_pavement );
+            here.ter_set( ava.pos(), ter_t_pavement );
             THEN( "faster than sneakers" ) {
                 CHECK( ava.run_cost( 100 ) == 39 );
             }
         }
         WHEN( "on grass" ) {
-            here.ter_set( ava.pos(), t_grass );
+            here.ter_set( ava.pos(), ter_t_grass );
             THEN( "slower than sneakers" ) {
                 CHECK( ava.run_cost( 100 ) == 145 );
             }
@@ -139,13 +142,13 @@ TEST_CASE( "footwear_may_affect_movement_cost", "[move_cost][shoes]" )
         REQUIRE( ava.get_modifier( character_modifier_limb_run_cost_mod ) == Approx( 1.0 ) );
         WHEN( "on pavement and running" ) {
             ava.set_movement_mode( move_mode_run );
-            here.ter_set( ava.pos(), t_pavement );
+            here.ter_set( ava.pos(), ter_t_pavement );
             THEN( "slightly faster than sneakers" ) {
                 CHECK( ava.run_cost( 100 ) == 42 );
             }
         }
         WHEN( "on grass" ) {
-            here.ter_set( ava.pos(), t_grass );
+            here.ter_set( ava.pos(), ter_t_grass );
             THEN( "slightly slower than sneakers" ) {
                 CHECK( ava.run_cost( 100 ) == 110 );
             }
