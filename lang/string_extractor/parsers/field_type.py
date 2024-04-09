7e7089f8e541c9e9358724deb39979005c7e228c
Uwuewsky <99945024+Uwuewsky@users.noreply.github.com>
2024-04-10 06:28:58 +0800

    Extract field bash and effect apply text (#72745)

--- lang/string_extractor/parsers/field_type.py
+++ lang/string_extractor/parsers/field_type.py
@@ -2,12 +2,15 @@ from ..write_text import write_text
 
 
 def parse_field_type(json, origin):
+    field_names = []
+
     for fd in json.get("intensity_levels", []):
         if "name" in fd:
             write_text(fd["name"], origin, comment="Field intensity level")
             id_or_name = fd["name"]
         else:
             id_or_name = json["id"]
+        field_names.append(id_or_name)
         for eff in fd.get("effects", []):
             if "message" in eff:
                 write_text(eff["message"], origin,
@@ -20,3 +23,9 @@ def parse_field_type(json, origin):
     if "npc_complain" in json:
         write_text(json["npc_complain"]["speech"], origin,
                    comment="Field NPC complaint")
+
+    if "bash" in json:
+        if "msg_success" in json["bash"]:
+            write_text(json["bash"]["msg_success"], origin,
+                       comment="Bashing message of fields {}"
+                       .format(", ".join(f"\"{n}\"" for n in field_names)))
