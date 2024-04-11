c1be0c57587c02530d363fd06f4ad70ff6a6c265
phi <phi@cloud7.local>
2024-04-10 20:55:22 -0600

    Fixed segfault when an NPC completes a pickup after a player has already taken it
    - Fixes https://github.com/CleverRaven/Cataclysm-DDA/issues/72340
    - Replaces naked pointer npc::wanted_item with: item_location npc::wanted_item
    - The safe reference will return null if the object gets deleted, which is what happens
      if someone else collects it before the NPC completes the pickup
    
    TODO: Sensibly, we should track the item coordinates using item_location
    since it's suppose to package a safe reference alongside location data.
    However, npc::wanted_item_pos is subject to shuffling via the game::update_map()
    and shift() mechanism, so rather than try to comprehend that and how it
    does or doesn't interact with item_location,  I'm instead leaving
    it alone since the coordinates appear to be calculated right at present.

--- src/npc.h
+++ src/npc.h
@@ -1358,7 +1358,12 @@ class npc : public Character
         // Player orders a friendly NPC to move to this position
         std::optional<tripoint_abs_ms> goto_to_this_pos;
         int last_seen_player_turn = 0; // Timeout to forgetting
-        const item *wanted_item = nullptr;
+
+        //Safe reference to an item at a specific location which can be checked for subsequent invalidation
+        //in case the object is destroyed or more than one creature goes for it.
+        //const item *wanted_item = nullptr;
+        item_location wanted_item = {};
+
         tripoint wanted_item_pos; // The square containing an item we want
         // These are the coordinates that a guard will return to inside of their goal tripoint
         std::optional<tripoint_abs_ms> guard_pos;
