bb1d87f234d4e5208d64163fbf2cc6f84c06dfe4
Anton Simakov <67688115+GuardianDll@users.noreply.github.com>
2024-04-21 18:39:46 +0200

    Do floating point math
    Co-authored-by: anothersimulacrum <anothersimulacrum@gmail.com>

--- src/math_parser_diag.cpp
+++ src/math_parser_diag.cpp
@@ -1338,7 +1338,7 @@ std::function<double( dialogue & )> calories_eval( char scope,
 
         if( format == "percent" ) {
             if( d.actor( beta )->get_character() ) {
-                int divisor = d.actor( beta )->get_healthy_kcal() / 100;
+                double divisor = d.actor( beta )->get_healthy_kcal() / 100.0;
                 //if no data, default to default height of 175cm
                 if( divisor == 0 ) {
                     divisor = 118169 / 100;
