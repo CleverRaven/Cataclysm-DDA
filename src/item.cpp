9e30ff7e0f7ee32a6d69516d9d8023f7127bb317
misterprimus <45959491+misterprimus@users.noreply.github.com>
2024-05-06 23:01:30 -0400

    fix cpp error

--- src/item.cpp
+++ src/item.cpp
@@ -13957,7 +13957,7 @@ bool item::process_blackpowder_fouling( Character *carrier )
 {
     // rust is deterministic. 12 hours for first rust, then 24 (36 total), then 36 (72 total) and finally 48 (120 hours to go to XX)
     // this speeds up by the amount the gun is dirty, 2-6x as fast depending on dirt level.
-    set_var( "rust_timer", get_var( "rust_timer", 0 ) + std::max( 0.3 + get_var( "dirt", 0 ) / 200, 1 ) );
+    set_var( "rust_timer", get_var( "rust_timer", 0 ) + std::max( 0.3 + get_var( "dirt", 0 ) / 200, 1.0 ) );
     double time_mult = 1.0 + ( 4.0 * static_cast<double>( damage() ) ) / static_cast<double>
                        ( max_damage() );
     if( damage() < max_damage() && get_var( "rust_timer", 0 ) > 43200.0 * time_mult ) {
