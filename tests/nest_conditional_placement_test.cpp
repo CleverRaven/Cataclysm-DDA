2dfbb3b387b4cedc38d0593b2595117ccabf989f
Procyonae <45432782+Procyonae@users.noreply.github.com>
2024-05-09 16:56:27 +0100

    Appease our clang overlords

--- tests/nest_conditional_placement_test.cpp
+++ tests/nest_conditional_placement_test.cpp
@@ -1,3 +1,4 @@
+#include "cata_catch.h"
 #include "city.h"
 #include "coordinates.h"
 #include "game_constants.h"
@@ -6,13 +7,14 @@
 #include "overmapbuffer.h"
 #include "test_data.h"
 
-static const oter_str_id field( "field" );
-static const oter_str_id ncpt_predecessors( "ncpt_predecessors" );
+static const oter_str_id oter_field( "field" );
+static const oter_str_id oter_ncpt_predecessors( "ncpt_predecessors" );
 
-static const overmap_special_id test_mutable( "nest_conditional_placement_test_mutable" );
+static const overmap_special_id
+overmap_special_nest_conditional_placement_test_mutable( "nest_conditional_placement_test_mutable" );
 
-static const ter_str_id placed( "t_linoleum_white" );
-static const ter_str_id unplaced( "t_linoleum_gray" );
+static const ter_str_id ter_t_linoleum_gray( "t_linoleum_gray" ); //Signifies the nest wasn't placed
+static const ter_str_id ter_t_linoleum_white( "t_linoleum_white" ); //Signifies the nest was placed
 
 //Check unconditional nests place and that each nest condition works as intended
 TEST_CASE( "nest_conditional_placement", "[map][nest]" )
@@ -24,7 +26,7 @@ TEST_CASE( "nest_conditional_placement", "[map][nest]" )
     overmap_buffer.create_custom_overmap( overmap_point, empty_special_batch );
     overmap *om = overmap_buffer.get_existing( overmap_point );
 
-    const overmap_special &special = *test_mutable;
+    const overmap_special &special = *overmap_special_nest_conditional_placement_test_mutable;
     const tripoint_om_omt test_omt_pos( OMAPX / 2, OMAPY / 2, 0 );
     const tripoint_rel_omt tinymap_point_offset( OMAPX / 2, OMAPY / 2, 0 );
     const tripoint_abs_omt tinymap_point = om->global_base_point() + tinymap_point_offset;
@@ -35,11 +37,11 @@ TEST_CASE( "nest_conditional_placement", "[map][nest]" )
     for( int i = -1; i < 2; i++ ) {
         for( int j = -1; j < 2; j++ ) {
             tripoint_rel_omt offset( i, j, 0 );
-            om->ter_set( test_omt_pos + offset, field.id() );
+            om->ter_set( test_omt_pos + offset, oter_field.id() );
         }
     }
     //Add the predecessor omt
-    om->ter_set( test_omt_pos, ncpt_predecessors.id() );
+    om->ter_set( test_omt_pos, oter_ncpt_predecessors.id() );
     //Place the mutable over the predecessor omt
     if( om->can_place_special( special, test_omt_pos, dir, false ) ) {
         std::vector<tripoint_om_omt> placed_points =
@@ -52,21 +54,21 @@ TEST_CASE( "nest_conditional_placement", "[map][nest]" )
     tinymap tm;
     tm.load( tinymap_point, false );
 
-    //The y0 row is always supposed to place while the y1 row should always fail
-    const bool unconditional_success = tm.ter( { 0, 0, 0 } ) == placed &&
-                                       tm.ter( { 0, 1, 0 } ) == unplaced;
-    const bool neighbors_success = tm.ter( { 1, 0, 0 } ) == placed &&
-                                   tm.ter( { 1, 1, 0 } ) == unplaced;
-    const bool joins_success = tm.ter( { 2, 0, 0 } ) == placed &&
-                               tm.ter( { 2, 1, 0 } ) == unplaced;
-    const bool flags_success = tm.ter( { 3, 0, 0 } ) == placed &&
-                               tm.ter( { 3, 1, 0 } ) == unplaced;
-    const bool flags_any_success = tm.ter( { 4, 0, 0 } ) == placed &&
-                                   tm.ter( { 4, 1, 0 } ) == unplaced;
-    const bool predecessors_success = tm.ter( { 5, 0, 0 } ) == placed &&
-                                      tm.ter( { 5, 1, 0 } ) == unplaced;
-    const bool z_check_success = tm.ter( { 6, 0, 0 } ) == placed &&
-                                 tm.ter( { 6, 1, 0 } ) == unplaced;
+    //The y0 row has conditions that should result in t_linoleum_white being placed while the y1 row has conditions that should always fail to place t_linoleum_white
+    const bool unconditional_success = tm.ter( { 0, 0, 0 } ) == ter_t_linoleum_white &&
+                                       tm.ter( { 0, 1, 0 } ) == ter_t_linoleum_gray;
+    const bool neighbors_success = tm.ter( { 1, 0, 0 } ) == ter_t_linoleum_white &&
+                                   tm.ter( { 1, 1, 0 } ) == ter_t_linoleum_gray;
+    const bool joins_success = tm.ter( { 2, 0, 0 } ) == ter_t_linoleum_white &&
+                               tm.ter( { 2, 1, 0 } ) == ter_t_linoleum_gray;
+    const bool flags_success = tm.ter( { 3, 0, 0 } ) == ter_t_linoleum_white &&
+                               tm.ter( { 3, 1, 0 } ) == ter_t_linoleum_gray;
+    const bool flags_any_success = tm.ter( { 4, 0, 0 } ) == ter_t_linoleum_white &&
+                                   tm.ter( { 4, 1, 0 } ) == ter_t_linoleum_gray;
+    const bool predecessors_success = tm.ter( { 5, 0, 0 } ) == ter_t_linoleum_white &&
+                                      tm.ter( { 5, 1, 0 } ) == ter_t_linoleum_gray;
+    const bool z_check_success = tm.ter( { 6, 0, 0 } ) == ter_t_linoleum_white &&
+                                 tm.ter( { 6, 1, 0 } ) == ter_t_linoleum_gray;
     CHECK( unconditional_success );
     CHECK( neighbors_success );
     CHECK( joins_success );
@@ -77,11 +79,11 @@ TEST_CASE( "nest_conditional_placement", "[map][nest]" )
     if( unconditional_success && neighbors_success && joins_success && flags_success &&
         flags_any_success && predecessors_success && z_check_success ) {
         //Check the neighbors condition works with om_terrain_match_type
-        const bool om_terrain_match_type_success = tm.ter( { 22, 0, 0 } ) == placed &&
-                tm.ter( { 22, 1, 0 } ) == unplaced;
+        const bool om_terrain_match_type_success = tm.ter( { 22, 0, 0 } ) == ter_t_linoleum_white &&
+                tm.ter( { 22, 1, 0 } ) == ter_t_linoleum_gray;
         //Check that all conditions are required when multiple are specified
-        const bool multiconditional_success = tm.ter( { 23, 0, 0 } ) == placed &&
-                                              tm.ter( { 23, 1, 0 } ) == unplaced;
+        const bool multiconditional_success = tm.ter( { 23, 0, 0 } ) == ter_t_linoleum_white &&
+                                              tm.ter( { 23, 1, 0 } ) == ter_t_linoleum_gray;
         CHECK( om_terrain_match_type_success );
         CHECK( multiconditional_success );
     }
