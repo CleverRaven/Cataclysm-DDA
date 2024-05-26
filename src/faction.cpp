41dd8b7f3598c966e89009c70ee95049193dd9c2
GalacticApple <87950676+GalacticApple@users.noreply.github.com>
2024-05-26 18:54:30 -0400

    Update src/faction.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/faction.cpp
+++ src/faction.cpp
@@ -146,7 +146,7 @@ faction_template::faction_template( const JsonObject &jsobj )
     for( const JsonObject jao : jsobj.get_array( "epilogues" ) ) {
         epilogue_data.emplace( jao.get_int( "power_min", std::numeric_limits<int>::min() ),
                                jao.get_int( "power_max", std::numeric_limits<int>::max() ),
-                               jao.get_string( "dynamic", 
+                               jao.get_string( "dynamic",
                                                "0000000" ), // old_guard, robofac, tacoma_commune, free_merchants, exodii, great_library, hells_raiders; 0 - anything; 1 - power >= 150; 2 - power < 150; 3,4,... - specific dynamic faction endings
                                snippet_id( jao.get_string( "id", "epilogue_faction_default" ) ) );
     }
