e6272838160063e6555585bb60e291255c9cf45b
GalacticApple <87950676+GalacticApple@users.noreply.github.com>
2024-05-31 17:41:39 -0400

    Update src/faction.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/faction.cpp
+++ src/faction.cpp
@@ -153,7 +153,7 @@ std::string faction::describe() const
     return ret;
 }
 
-void faction_power_spec::deserialize( const JsonObject& jo )
+void faction_power_spec::deserialize( const JsonObject &jo )
 {
     mandatory( jo, false, "faction", faction );
     optional( jo, false, "power_min", power_min );
