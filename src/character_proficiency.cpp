cb9bf5e5e38aacf134e25e84de2c87b62afb2bfa
osuphobia <osuphobia@gmail.com>
2024-06-08 20:54:52 +0800

    clang-tidy

--- src/character_proficiency.cpp
+++ src/character_proficiency.cpp
@@ -23,7 +23,7 @@ bool Character::has_proficiency( const proficiency_id &prof ) const
 
 void Character::set_prof_boost()
 {
-    for( const proficiency_category prof_cat : proficiency_category::get_all() ) {
+    for( const proficiency_category &prof_cat : proficiency_category::get_all() ) {
         float value = enchantment_cache->modify_value( prof_cat.id, 1.0 );
         if( value > 1.0 ) {
             prof_boosts.emplace( prof_cat.id, value );
