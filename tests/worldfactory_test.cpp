d85f0d3e21814c7fe7151844fc5ce2a96de012d8
Qrox <qrox@sina.com>
2024-06-10 00:39:11 +0800

    Add world timestamp creation test

---
+++ tests/worldfactory_test.cpp
@@ -0,0 +1,13 @@
+#include "cata_catch.h"
+#include "worldfactory.h"
+
+TEST_CASE( "world_create_timestamp" )
+{
+    std::unique_ptr<WORLD> world = std::make_unique<WORLD>();
+    REQUIRE( world->create_timestamp() );
+    CHECK( world->timestamp.size() + 1 == sizeof( "yyyymmddHHMMSS123456789" ) );
+    for( const char ch : world->timestamp ) {
+        CHECK( ch >= '0' );
+        CHECK( ch <= '9' );
+    }
+}
