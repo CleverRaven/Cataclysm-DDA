b0402d8008083ccb27ad936845c76a4fde0d29de
Kamayana <pigbuster@gmail.com>
2024-06-15 09:30:32 -0500

    Fix extension cords falling off when items are turned on (#74268)
    * Fix item conversion relying on a value that's only valid during loading
    
    * Update link traits when converting items
    
    ---------
    
    Co-authored-by: David Seguin <davidseguin@live.ca>

--- src/item.cpp
+++ src/item.cpp
@@ -669,6 +669,7 @@ item &item::convert( const itype_id &new_type, Character *carrier )
     }
 
     item_counter = 0;
+    update_link_traits();
     update_prefix_suffix_flags();
     return *this;
 }
