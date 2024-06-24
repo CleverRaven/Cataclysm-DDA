6381c3400e259f40e36ceed0aad7b138e51825b9
GuardianDll <antisim009@gmail.com>
2024-06-24 18:40:51 +0200

    Replace flag checks with vitamin checks
    Co-authored-by: RenechCDDA <84619419+RenechCDDA@users.noreply.github.com>

--- src/item.h
+++ src/item.h
@@ -1959,6 +1959,13 @@ class item : public visitable
                 return has_flag( flag );
             } );
         }
+        
+        template<typename Container, typename T = std::decay_t<decltype(*std::declval<const Container&>().begin())>>
+        bool has_any_vitamin(const Container& vitamins) const {
+            return std::any_of(vitamins.begin(), vitamins.end(), [&](const T& vitamin) {
+                return has_vitamin(vitamin);
+                });
+        }
 
         /**
          * Checks whether item itself has given flag (doesn't check item type or gunmods).
