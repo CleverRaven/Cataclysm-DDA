0b3b3d915ed6099664d637fb6756c61935373a29
Holli <hollierthanthouare@gmail.com>
2024-07-04 08:34:58 -0600

    remove acid bomb, adjust test

--- tests/throw_activation_test.cpp
+++ tests/throw_activation_test.cpp
@@ -3,15 +3,15 @@
 #include "item.h"
 #include "map.h"
 
-static const field_type_str_id field_fd_acid( "fd_acid" );
+static const field_type_str_id field_fd_fire( "fd_fire" );
 
 TEST_CASE( "throw_activation", "[item]" )
 {
-    item acid_bomb( "acidbomb" );
+    item molotov( "molotov_lit" );
 
-    SECTION( "acid_bomb_hits_ground" ) {
-        acid_bomb.activate_thrown( tripoint_zero );
-        get_map().get_field( tripoint_zero, field_fd_acid );
-        CHECK( get_map().get_field( tripoint_zero, field_fd_acid ) != nullptr );
+    SECTION( "molotov_hits_ground" ) {
+        molotov.activate_thrown( tripoint_zero );
+        get_map().get_field( tripoint_zero, field_fd_fire );
+        CHECK( get_map().get_field( tripoint_zero, field_fd_fire ) != nullptr );
     }
 }
