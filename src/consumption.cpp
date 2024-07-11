4cacaf2df3877d404784d51e6990e0b74c22d3df
Anton Simakov <67688115+GuardianDll@users.noreply.github.com>
2024-07-11 12:11:12 +0200

    Apply suggestions from code review

--- src/consumption.cpp
+++ src/consumption.cpp
@@ -245,8 +245,7 @@ static int compute_default_effective_kcal( const item &comest, const Character &
     }
 
     kcal = you.enchantment_cache->modify_value( enchant_vals::mod::KCAL, kcal );
-kcal = std:
-           max( 0, kcal );
+kcal = std::max( 0, kcal );
 
     return static_cast<int>( kcal );
 }
@@ -290,8 +289,7 @@ static std::map<vitamin_id, int> compute_default_effective_vitamins(
     for( std::pair<const vitamin_id, int> &vit : res ) {
         vit.second = you.enchantment_cache->modify_value( enchant_vals::mod::VITAMIN_ABSORB_MOD,
                      vit.second );
-vit.second = std:
-                     max( 0, vit.second );
+vit.second = std::max( 0, vit.second );
     }
     return res;
 }
