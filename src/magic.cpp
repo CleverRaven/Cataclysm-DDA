1113a47de67274947d54a3cf6afcbee2e9490a72
Maciek Kalisiak <mkalisiak@gmail.com>
2024-08-12 01:53:08 -0400

    Improve invlet management for Magiclysm spell list.
    First, impose sensible order on spell list. Specifically, we sort by 3
    dimensions:
    1. favorite spells before non-favorite
    2. by invlet
    3. by spell name
    
    Second, this change fixes some issues:
    - make auto-assigned invlets "permanent" (vs scrambling letters when
      spells are added or subtracted)
    - actually properly check if the desired invlet is actually in use (vs
      only checking against "reserved")
    
    Motivation:
    - addresses desire (and expected behavior in general), such as
      https://www.reddit.com/r/cataclysmdda/comments/oq2g3s/any_way_to_change_spell_list_order/
    - might actually address Issue 50465
    - might partially mitigate Issue 50018

--- src/magic.cpp
+++ src/magic.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cmath>
 #include <cstdlib>
+#include <cstring>
 #include <memory>
 #include <set>
 #include <utility>
@@ -2345,7 +2346,7 @@ class spellcasting_callback : public uilist_callback
         void display_spell_info( size_t index );
     public:
         // invlets reserved for special functions
-        const std::set<int> reserved_invlets{ 'I', '=', '*' };
+        static const std::set<int> reserved_invlets;
         bool casting_ignore;
 
         spellcasting_callback( std::vector<spell *> &spells,
@@ -2361,8 +2362,11 @@ class spellcasting_callback : public uilist_callback
                 int invlet = 0;
                 invlet = popup_getkey( _( "Choose a new hotkey for this spell." ) );
                 if( inv_chars.valid( invlet ) ) {
-                    const bool invlet_set =
-                        get_player_character().magic->set_invlet( known_spells[entnum]->id(), invlet, reserved_invlets );
+                    std::set<int> used_invlets{ spellcasting_callback::reserved_invlets };
+                    get_player_character().magic->update_used_invlets( used_invlets );
+                    const bool invlet_set = get_player_character().magic->set_invlet(
+                                                known_spells[entnum]->id(), invlet, used_invlets );
+                    // TODO: if key already in use, have spells swap invlets?
                     if( !invlet_set ) {
                         popup( _( "Hotkey already used." ) );
                     } else {
@@ -2407,6 +2411,8 @@ class spellcasting_callback : public uilist_callback
             ImGui::EndChild();
         }
 };
+
+const std::set<int> spellcasting_callback::reserved_invlets { 'I', '=', '*' };
 
 bool spell::casting_time_encumbered( const Character &guy ) const
 {
@@ -2695,12 +2701,21 @@ bool known_magic::set_invlet( const spell_id &sp, int invlet, const std::set<int
         return false;
     }
     invlets[sp] = invlet;
+    // TODO: we should really update used_invlets too, to avoid inconsistency
     return true;
 }
 
 void known_magic::rem_invlet( const spell_id &sp )
 {
+    // TODO: ... except that rem_invlet cannot update used_invlets (not passed in)
     invlets.erase( sp );
+}
+
+void known_magic::update_used_invlets( std::set<int> &used_invlets )
+{
+    for( const std::pair<const spell_id, int> &invlet_pair : invlets ) {
+        used_invlets.emplace( invlet_pair.second );
+    }
 }
 
 void known_magic::toggle_favorite( const spell_id &sp )
@@ -2723,25 +2738,15 @@ int known_magic::get_invlet( const spell_id &sp, std::set<int> &used_invlets )
     if( found != invlets.end() ) {
         return found->second;
     }
-    for( const std::pair<const spell_id, int> &invlet_pair : invlets ) {
-        used_invlets.emplace( invlet_pair.second );
-    }
-    for( int i = 'a'; i <= 'z'; i++ ) {
-        if( used_invlets.count( i ) == 0 ) {
-            used_invlets.emplace( i );
-            return i;
-        }
-    }
-    for( int i = 'A'; i <= 'Z'; i++ ) {
-        if( used_invlets.count( i ) == 0 ) {
-            used_invlets.emplace( i );
-            return i;
-        }
-    }
-    for( int i = '!'; i <= '-'; i++ ) {
-        if( used_invlets.count( i ) == 0 ) {
-            used_invlets.emplace( i );
-            return i;
+    update_used_invlets( used_invlets );
+    // For spells without an invlet, assign first available one.
+    // Assignment is "sticky" (permanent), to avoid invlets getting scrambled
+    // when spells are added or subtracted.
+    // TODO: respect "Auto inventory letters" option?
+    for( auto &ch : inv_chars.get_allowed_chars() ) {
+        if( set_invlet( sp, ch, used_invlets ) ) {
+            used_invlets.emplace( ch );
+            return ch;
         }
     }
     return 0;
@@ -2749,15 +2754,40 @@ int known_magic::get_invlet( const spell_id &sp, std::set<int> &used_invlets )
 
 int known_magic::select_spell( Character &guy )
 {
-    std::vector<spell *> known_spells = get_spells();
+    std::vector<spell *> known_spells_sorted = get_spells();
+
+    std::set<int> used_invlets{ spellcasting_callback::reserved_invlets };
+
+    // Sort the spell lists by 3 dimensions.
+    sort( known_spells_sorted.begin(), known_spells_sorted.end(),
+    [&guy, &used_invlets, this]( spell * left, spell * right ) -> int {
+        const bool l_fav = guy.magic->is_favorite( left->id() );
+        const bool r_fav = guy.magic->is_favorite( right->id() );
+        // 1. Favorite spells before non-favorite
+        if( l_fav != r_fav )
+        {
+            return l_fav > r_fav;
+        }
+        const int l_invlet = get_invlet( left->id(), used_invlets );
+        const int r_invlet = get_invlet( right->id(), used_invlets );
+        // 2. By invlet, if present (but in allowed_chars order; e.g.,
+        //    lower-case first)
+        if( l_invlet != r_invlet )
+        {
+            return inv_chars.ordinal( l_invlet ) < inv_chars.ordinal( r_invlet );
+        }
+        // 3. By spell name
+        return strcmp( left->name().c_str(), right->name().c_str() );
+    } );
 
     uilist spell_menu;
     spell_menu.desired_bounds = {
         -1.0,
             -1.0,
             std::max( 80, TERMX * 3 / 8 ) *ImGui::CalcTextSize( "X" ).x,
-            clamp( static_cast<int>( known_spells.size() ), 24, TERMY * 9 / 10 ) *ImGui::GetTextLineHeightWithSpacing(),
+            clamp( static_cast<int>( known_spells_sorted.size() ), 24, TERMY * 9 / 10 ) *ImGui::GetTextLineHeightWithSpacing(),
         };
+
     spell_menu.title = _( "Choose a Spell" );
     spell_menu.input_category = "SPELL_MENU";
     spell_menu.additional_actions.emplace_back( "CHOOSE_INVLET", translation() );
@@ -2766,13 +2796,13 @@ int known_magic::select_spell( Character &guy )
     spell_menu.additional_actions.emplace_back( "SCROLL_DOWN_SPELL_MENU", translation() );
     spell_menu.additional_actions.emplace_back( "SCROLL_FAVORITE", translation() );
     spell_menu.hilight_disabled = true;
-    spellcasting_callback cb( known_spells, casting_ignore );
+    spellcasting_callback cb( known_spells_sorted, casting_ignore );
     spell_menu.callback = &cb;
     spell_menu.add_category( "all", _( "All" ) );
     spell_menu.add_category( "favorites", _( "Favorites" ) );
 
     std::vector<std::pair<std::string, std::string>> categories;
-    for( const spell *s : known_spells ) {
+    for( const spell *s : known_spells_sorted ) {
         if( s->can_cast( guy ) && ( s->spell_class().is_valid() || s->spell_class() == trait_NONE ) ) {
             const std::string spell_class_name = s->spell_class() == trait_NONE ? _( "Classless" ) :
                                                  s->spell_class().obj().name();
@@ -2789,16 +2819,16 @@ int known_magic::select_spell( Character &guy )
         spell_menu.add_category( cat.first, cat.second );
     }
 
-    spell_menu.set_category_filter( [&guy, known_spells]( const uilist_entry & entry,
+    spell_menu.set_category_filter( [&guy, known_spells_sorted]( const uilist_entry & entry,
     const std::string & key )->bool {
         if( key == "all" )
         {
             return true;
         } else if( key == "favorites" )
         {
-            return guy.magic->is_favorite( known_spells[entry.retval]->id() );
+            return guy.magic->is_favorite( known_spells_sorted[entry.retval]->id() );
         }
-        return ( known_spells[entry.retval]->spell_class().is_valid() || known_spells[entry.retval]->spell_class() == trait_NONE ) && known_spells[entry.retval]->spell_class().str() == key;
+        return ( known_spells_sorted[entry.retval]->spell_class().is_valid() || known_spells_sorted[entry.retval]->spell_class() == trait_NONE ) && known_spells_sorted[entry.retval]->spell_class().str() == key;
     } );
     if( !favorites.empty() ) {
         spell_menu.set_category( "favorites" );
@@ -2806,13 +2836,11 @@ int known_magic::select_spell( Character &guy )
         spell_menu.set_category( "all" );
     }
 
-    std::set<int> used_invlets{ cb.reserved_invlets };
-
-    for( size_t i = 0; i < known_spells.size(); i++ ) {
-        spell_menu.addentry( static_cast<int>( i ), known_spells[i]->can_cast( guy ),
-                             get_invlet( known_spells[i]->id(), used_invlets ), known_spells[i]->name() );
+    for( size_t i = 0; i < known_spells_sorted.size(); i++ ) {
+        spell_menu.addentry( static_cast<int>( i ), known_spells_sorted[i]->can_cast( guy ),
+                             get_invlet( known_spells_sorted[i]->id(), used_invlets ), known_spells_sorted[i]->name() );
     }
-    reflesh_favorite( &spell_menu, known_spells );
+    reflesh_favorite( &spell_menu, known_spells_sorted );
 
     spell_menu.query( true, -1, true );
 
