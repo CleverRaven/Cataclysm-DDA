dd7b939cc57b80c0e639894f520ce755fc5d9fd3
HadeanLake <HadeanLake@i.ua>
2024-08-13 11:48:33 +0300

    maps can fully reveal partially revealed tiles now

--- src/iuse_actor.cpp
+++ src/iuse_actor.cpp
@@ -1222,7 +1222,7 @@ void reveal_map_actor::reveal_targets( const tripoint_abs_omt &center,
     const auto places = overmap_buffer.find_all( center, target.first, radius, false,
                         target.second );
     for( const tripoint_abs_omt &place : places ) {
-        if( overmap_buffer.seen( place ) == om_vision_level::unseen ) {
+        if( overmap_buffer.seen( place ) != om_vision_level::full ) {
             // Should be replaced with the character using the item passed as an argument if NPCs ever learn to use maps
             get_avatar().map_revealed_omts.emplace( place );
         }
