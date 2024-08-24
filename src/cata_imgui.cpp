0abbaed3907e8f727985df7d3caf336a9d2d471d
alef <alef@users.noreply.github.com>
2024-08-24 12:40:09 +0200

    Rename local variable

--- src/cata_imgui.cpp
+++ src/cata_imgui.cpp
@@ -262,14 +262,14 @@ void cataimgui::client::load_fonts( const Font_Ptr &cata_font,
     ImGuiIO &io = ImGui::GetIO();
     if( ImGui::GetIO().FontDefault == nullptr ) {
         std::vector<std::string> io_typefaces{ typefaces };
-        ensure_unifont_loaded( typefaces );
+        ensure_unifont_loaded( io_typefaces );
 
         for( size_t index = 0; index < color_loader<SDL_Color>::COLOR_NAMES_COUNT; index++ ) {
             SDL_Color sdlCol = windowsPalette[index];
             ImU32 rgb = sdlCol.b << 16 | sdlCol.g << 8 | sdlCol.r;
             sdlColorsToCata[rgb] = index;
         }
-        io.FontDefault = io.Fonts->AddFontFromFileTTF( typefaces[0].c_str(), fontheight, nullptr,
+        io.FontDefault = io.Fonts->AddFontFromFileTTF( io_typefaces[0].c_str(), fontheight, nullptr,
                          io.Fonts->GetGlyphRangesDefault() );
         io.Fonts->Fonts[0]->SetFallbackStrSizeCallback( GetFallbackStrWidth );
         io.Fonts->Fonts[0]->SetFallbackCharSizeCallback( GetFallbackCharWidth );
