b3dce0516e5363cdb186b8941e3ff412d292b5ed
mqrause <martin@qrause.de>
2024-08-26 12:55:16 +0200

    move NoScrollbar and NoScrollWithMouse flags from cataimgui::window to uilist_impl

--- src/cata_imgui.cpp
+++ src/cata_imgui.cpp
@@ -537,8 +537,7 @@ cataimgui::window::window( int window_flags )
 
     this->window_flags = window_flags | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize |
                          ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoNavFocus |
-                         ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoScrollbar |
-                         ImGuiWindowFlags_NoScrollWithMouse;
+                         ImGuiWindowFlags_NoBringToFrontOnFocus;
 }
 
 cataimgui::window::window( const std::string &id_, int window_flags ) : window( window_flags )
