e17d410f893c5661f77107cea0a9d68ed5ef9ced
Daniel Brooks <db48x@orod-na-thon.tailcd79e.ts.net>
2024-08-27 09:31:02 -0700

    write a loading screen for when we’re not using ImGui too
    Fixes #75678

--- src/loading_ui.cpp
+++ src/loading_ui.cpp
@@ -1,24 +1,33 @@
 #include "loading_ui.h"
 
-#define IMGUI_DEFINE_MATH_OPERATORS
-#include "imgui/imgui.h"
-#undef IMGUI_DEFINE_MATH_OPERATORS
+#include "cached_options.h"
 #include "input.h"
 #include "output.h"
-#include "path_info.h"
 #include "ui_manager.h"
 
 #if defined(TILES)
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include "imgui/imgui.h"
+#undef IMGUI_DEFINE_MATH_OPERATORS
+#include "path_info.h"
 #include "sdltiles.h"
 #include "sdl_wrappers.h"
+#else
+#include "cursesdef.h"
 #endif // TILES
 
 struct ui_state {
     ui_adaptor *ui;
     background_pane *bg;
+#ifdef TILES
     ImVec2 window_size;
     ImVec2 splash_size;
     SDL_Texture_Ptr splash;
+#else
+    size_t splash_width = 0;
+    std::vector<std::string> splash;
+    std::string blanks;
+#endif
     std::string context;
     std::string step;
 };
@@ -27,6 +36,7 @@ ui_state *gLUI = nullptr;
 
 void redraw()
 {
+#ifdef TILES
     ImVec2 pos = { 0.5f, 0.5f };
     ImGui::SetNextWindowPos( ImGui::GetMainViewport()->Size * pos, ImGuiCond_Always, { 0.5f, 0.5f } );
     ImGui::SetNextWindowSize( gLUI->window_size );
@@ -35,7 +45,7 @@ void redraw()
     if( ImGui::Begin( "Loading…", NULL,
                       ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
                       ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoSavedSettings ) ) {
-        ImGui::Image(static_cast<void*>(gLUI->splash.get()), gLUI->splash_size);
+        ImGui::Image( static_cast<void *>( gLUI->splash.get() ), gLUI->splash_size );
         ImGui::SetCursorPosX( ( gLUI->splash_size.x / 2.0f ) - 120.0f );
         ImGui::TextUnformatted( gLUI->context.c_str() );
         ImGui::SameLine();
@@ -44,6 +54,21 @@ void redraw()
     ImGui::End();
     ImGui::PopStyleColor();
     ImGui::PopStyleVar();
+#else
+    int x = ( TERMX - static_cast<int>( gLUI->splash_width ) ) / 2;
+    int y = 0;
+    nc_color white = c_white;
+    for( const std::string &line : gLUI->splash ) {
+        if( !line.empty() && line[0] == '#' ) {
+            continue;
+        }
+        print_colored_text( catacurses::stdscr, point( x, y++ ), white,
+                            white, line );
+    }
+    mvwprintz( catacurses::stdscr, point( 0, TERMY - 1 ), c_black, gLUI->blanks );
+    center_print( catacurses::stdscr, TERMY - 1, c_white, string_format( "%s %s",
+                  gLUI->context.c_str(), gLUI->step.c_str() ) );
+#endif
 }
 
 void resize()
@@ -64,11 +89,24 @@ void update_state( const std::string &context, const std::string &step )
             resize();
         } );
 
+#ifdef TILES
         cata_path path = PATH_INFO::gfxdir() / "cdda.avif";
         SDL_Surface_Ptr surf = load_image( path.get_unrelative_path().u8string().c_str() );
-        gLUI->splash_size = { static_cast<float>(surf->w), static_cast<float>(surf->h) };
+        gLUI->splash_size = { static_cast<float>( surf->w ), static_cast<float>( surf->h ) };
         gLUI->splash = CreateTextureFromSurface( get_sdl_renderer(), surf );
-        gLUI->window_size = gLUI->splash_size + ImVec2{ 0.0f, 2.0f*ImGui::GetTextLineHeightWithSpacing() };
+        gLUI->window_size = gLUI->splash_size + ImVec2{ 0.0f, 2.0f * ImGui::GetTextLineHeightWithSpacing() };
+#else
+        std::string splash = read_whole_file( PATH_INFO::title( get_holiday_from_time() ) ).value_or(
+                                 _( "Cataclysm: Dark Days Ahead" ) );
+        gLUI->splash = string_split( splash, '\n' );
+        gLUI->blanks = std::string( TERMX, ' ' );
+        for( const std::string &line : gLUI->splash ) {
+            if( !line.empty() && line[0] == '#' ) {
+                continue;
+            }
+            gLUI->splash_width = std::max( gLUI->splash_width, remove_color_tags( line ).length() );
+        }
+#endif
     }
     gLUI->context = std::string( context );
     gLUI->step = std::string( step );
@@ -76,6 +114,9 @@ void update_state( const std::string &context, const std::string &step )
 
 void loading_ui::show( const std::string &context, const std::string &step )
 {
+    if( test_mode ) {
+        return;
+    }
     update_state( context, step );
     ui_manager::redraw();
     refresh_display();
