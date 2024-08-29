ed4182859e31665dc7f38f3d591fc6e243d0420e
Anton Simakov <67688115+GuardianDll@users.noreply.github.com>
2024-08-29 08:51:07 +0200

    nullptr protection

--- src/character.cpp
+++ src/character.cpp
@@ -8061,8 +8061,12 @@ void Character::apply_damage( Creature *source, bodypart_id hurt, int dam,
     const int dam_to_bodypart = std::min( dam, get_part_hp_cur( part_to_damage ) );
 
     mod_part_hp_cur( part_to_damage, - dam_to_bodypart );
-    cata::event e = cata::event::make<event_type::character_takes_damage>( getID(), dam_to_bodypart );
-    get_event_bus().send_with_talker( this, source, e );
+    if( source ) {
+        cata::event e = cata::event::make<event_type::character_takes_damage>( getID(), dam_to_bodypart );
+        get_event_bus().send_with_talker( this, source, e );
+    } else {
+        get_event_bus().send<event_type::character_takes_damage>( getID(), dam_to_bodypart );
+    }
 
     if( !weapon.is_null() && !can_wield( weapon ).success() &&
         can_drop( weapon ).success() ) {
@@ -8277,8 +8281,13 @@ void Character::hurtall( int dam, Creature *source, bool disturb /*= true*/ )
         // Don't use apply_damage here or it will annoy the player with 6 queries
         const int dam_to_bodypart = std::min( dam, get_part_hp_cur( bp ) );
         mod_part_hp_cur( bp, - dam_to_bodypart );
-        cata::event e = cata::event::make<event_type::character_takes_damage>( getID(), dam_to_bodypart );
-        get_event_bus().send_with_talker( this, source, e );
+
+        if( source ) {
+            cata::event e = cata::event::make<event_type::character_takes_damage>( getID(), dam_to_bodypart );
+            get_event_bus().send_with_talker( this, source == nullptr ? nullptr : source, e );
+        } else {
+            get_event_bus().send<event_type::character_takes_damage>( getID(), dam_to_bodypart );
+        }
 
     }
 
