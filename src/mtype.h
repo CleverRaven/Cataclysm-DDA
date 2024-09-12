0d57eb0a632a94fa1ee27216e516d01beb058845
mqrause <martin@qrause.de>
2024-09-12 15:06:37 +0200

    initialize all reproducte_type members to null_id

--- src/mtype.h
+++ src/mtype.h
@@ -280,10 +280,10 @@ struct mount_item_data {
 };
 
 struct reproduction_type {
-    mtype_id baby_monster;
-    mongroup_id baby_monster_group;
-    itype_id baby_egg;
-    item_group_id baby_egg_group;
+    mtype_id baby_monster = mtype_id::NULL_ID();
+    mongroup_id baby_monster_group = mongroup_id::NULL_ID();
+    itype_id baby_egg = itype_id::NULL_ID();
+    item_group_id baby_egg_group = item_group_id::NULL_ID();
 };
 
 struct mtype {
