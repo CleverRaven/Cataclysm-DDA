6e631be2c6624299aa0429cbd74fc3263edce389
Vgoloshivskiy <56963648+Vgoloshivskiy@users.noreply.github.com>
2024-09-13 15:25:51 +0300

    more fixes

--- src/activity_actor.cpp
+++ src/activity_actor.cpp
@@ -218,18 +218,26 @@ static const item_group_id Item_spawn_data_forage_summer( "forage_summer" );
 static const item_group_id Item_spawn_data_forage_winter( "forage_winter" );
 static const item_group_id Item_spawn_data_trash_forest( "trash_forest" );
 
-static const itype_id itype_2x4( "2x4" );
-static const itype_id itype_detergent( "detergent" );
-static const itype_id itype_disassembly( "disassembly" );
-static const itype_id itype_liquid_soap( "liquid_soap" );
-static const itype_id itype_log( "log" );
-static const itype_id itype_paper( "paper" );
-static const itype_id itype_pseudo_bio_picklock( "pseudo_bio_picklock" );
-static const itype_id itype_soap( "soap" );
-static const itype_id itype_splinter( "splinter" );
-static const itype_id itype_stick_long( "stick_long" );
-static const itype_id itype_water( "water" );
-static const itype_id itype_water_clean( "water_clean" );
+static const itype_id itype_2x4("2x4");
+static const itype_id itype_detergent("detergent");
+static const itype_id itype_disassembly("disassembly");
+static const itype_id itype_liquid_soap("liquid_soap");
+static const itype_id itype_log("log");
+static const itype_id itype_paper("paper");
+static const itype_id itype_pseudo_bio_picklock("pseudo_bio_picklock");
+static const itype_id
+itype_robofac_yrax_delta_deactivation_manual("robofac_yrax_delta_deactivation_manual");
+static const itype_id
+itype_robofac_yrax_golden_monolith_deactivation_manual("robofac_yrax_golden_monolith_deactivation_manual");
+static const itype_id
+itype_robofac_yrax_triakis_deactivation_manual("robofac_yrax_triakis_deactivation_manual");
+static const itype_id
+itype_robofac_yrax_trifacet_deactivation_manual("robofac_yrax_trifacet_deactivation_manual");
+static const itype_id itype_soap("soap");
+static const itype_id itype_splinter("splinter");
+static const itype_id itype_stick_long("stick_long");
+static const itype_id itype_water("water");
+static const itype_id itype_water_clean("water_clean");
 
 static const json_character_flag json_flag_SAFECRACK_NO_TOOL( "SAFECRACK_NO_TOOL" );
 
@@ -246,15 +254,6 @@ static const mtype_id mon_golden_monolith("mon_golden_monolith");
 static const mtype_id mon_yrax_delta("mon_yrax_delta");
 static const mtype_id mon_yrax_triakis("mon_yrax_triakis");
 static const mtype_id mon_yrax_trifacet("mon_yrax_trifacet");
-
-static const itype_id
-itype_robofac_yrax_delta_deactivation_manual("robofac_yrax_delta_deactivation_manual");
-static const itype_id
-itype_robofac_yrax_golden_monolith_deactivation_manual("robofac_yrax_golden_monolith_deactivation_manual");
-static const itype_id
-itype_robofac_yrax_triakis_deactivation_manual("robofac_yrax_triakis_deactivation_manual");
-static const itype_id
-itype_robofac_yrax_trifacet_deactivation_manual("robofac_yrax_trifacet_deactivation_manual");
 
 
 static const proficiency_id proficiency_prof_lockpicking( "prof_lockpicking" );
