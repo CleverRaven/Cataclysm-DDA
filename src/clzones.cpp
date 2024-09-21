35bffa2e6378077a91421dbddbfad68527092d34
BathBall <43808510+peter4370@users.noreply.github.com>
2024-09-21 11:21:04 +0800

    apply astyle
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/clzones.cpp
+++ src/clzones.cpp
@@ -282,7 +282,7 @@ unload_options::query_unload_result unload_options::query_unload()
     mods = query_yn(
                _( "Detach mods from weapons?  (Be careful as you may not have the skills to reattach them)" ) );
     sparse_only = query_yn( string_format(
-                            _( "Avoid unloading items stacks (not charges) greater than a certain amount?  (Amount defined in next window)" ) ) );
+                                _( "Avoid unloading items stacks (not charges) greater than a certain amount?  (Amount defined in next window)" ) ) );
     if( sparse_only ) {
         int threshold;
         if( query_int( threshold,
