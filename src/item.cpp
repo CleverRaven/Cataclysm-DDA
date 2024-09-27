6ff7555b6598714479d6f655b0672694ff32cc01
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2024-09-26 22:20:27 -0500

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/item.cpp
+++ src/item.cpp
@@ -190,7 +190,7 @@ static const itype_id itype_water( "water" );
 static const itype_id itype_water_clean( "water_clean" );
 static const itype_id itype_waterproof_gunmod( "waterproof_gunmod" );
 
-static const flag_id flag_irreplaceable_consumable( "IRREPLACEABLE_CONSUMABLE" );
+static const flag_id json_flag_IRREPLACEABLE_CONSUMABLE( "IRREPLACEABLE_CONSUMABLE" );
 
 static const json_character_flag json_flag_CANNIBAL( "CANNIBAL" );
 static const json_character_flag json_flag_IMMUNE_SPOIL( "IMMUNE_SPOIL" );
@@ -7116,7 +7116,7 @@ int item::price_no_contents( bool practical, std::optional<int> price_override )
         price = std::max( price - PRICE_FILTHY_MALUS, 0 );
     }
 
-    if (flag_id_irreplaceable_consumeable()) {
+    if( flag_id_irreplaceable_consumeable() ) {
         // irreplaceable items are worth more by how many seasons from the cataclysm player is
         price *= 0.25 + (calendar::turn - calendar::start_of_cataclysm).days() / 90;
     }
