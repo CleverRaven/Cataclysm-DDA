1cf392d27034c6800be40d7a2af0b4067799a777
Blueflowers <77662682+Blueflowerss@users.noreply.github.com>
2024-10-03 20:35:41 +0000

    Update src/mapbuffer.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/mapbuffer.cpp
+++ src/mapbuffer.cpp
@@ -37,7 +37,8 @@ static cata_path find_quad_path( const cata_path &dirname, const tripoint_abs_om
     return dirname / string_format( "%d.%d.%d.map", om_addr.x(), om_addr.y(), om_addr.z() );
 }
 
-static cata_path find_dirname( const tripoint_abs_omt &om_addr, std::string world_prefix ){
+static cata_path find_dirname( const tripoint_abs_omt &om_addr, std::string world_prefix )
+{
     const tripoint_abs_seg segment_addr = project_to<coords::seg>( om_addr );
    std::string segment = string_format( "%d.%d.%d",
                                          segment_addr.x(),
