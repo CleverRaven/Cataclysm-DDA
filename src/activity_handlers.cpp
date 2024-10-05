36b642b724b617e94194d5f1e81c7ea7091ee7c5
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2024-10-04 21:58:15 -0500

    Update src/activity_handlers.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/activity_handlers.cpp
+++ src/activity_handlers.cpp
@@ -1870,8 +1870,8 @@ void activity_handlers::pickaxe_finish( player_activity *act, Character *you )
     here.destroy( pos, true );
     if( !act->targets.empty() ) {
         item &it = *act->targets.front();
-        if (it.is_null() || it.charges <= 0) {
-            debugmsg("pickaxe expired during mining" );
+        if( it.is_null() || it.charges <= 0 ) {
+            debugmsg( "pickaxe expired during mining" );
             return;
         }
         you->consume_charges( it, it.ammo_required() );
