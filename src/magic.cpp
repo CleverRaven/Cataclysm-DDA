Darzzz-123/Nica

rm parameter avatar from target_handler::mode_spell

src/magic.cpp
@@ -820,9 +820,7 @@ std::optional<tripoint> spell::select_target( Creature *source )
         if( source->is_avatar() ) {
             do {
                 avatar &source_avatar = *source->as_avatar();
-                std::vector<tripoint> trajectory = target_handler::mode_spell( source_avatar, *this,
-                                                   true,
-                                                   true );
+                std::vector<tripoint> trajectory = target_handler::mode_spell( *this, true, true );
                 if( !trajectory.empty() ) {
                     target = trajectory.back();
                     target_is_valid = is_valid_target( source_avatar, target );

src/ranged.cpp
@@ -541,18 +541,17 @@ target_handler::trajectory target_handler::mode_turrets( vehicle &veh,
     return ui.run();
 }
 
-target_handler::trajectory target_handler::mode_spell( avatar &you, spell &casting, bool no_fail,
-        bool no_mana )
+target_handler::trajectory target_handler::mode_spell( spell &casting, bool no_fail, bool no_mana )
 {
     target_ui ui = target_ui();
-    ui.you = &you;
+    ui.you = &get_avatar();
     ui.mode = target_ui::TargetMode::Spell;
     ui.casting = &casting;
-    ui.range = casting.range( you );
+    ui.range = casting.range( *ui.you );
     ui.no_fail = no_fail;
     ui.no_mana = no_mana;
 
-    restore_on_out_of_scope<tripoint> view_offset_prev( you.view_offset );
+    restore_on_out_of_scope<tripoint> view_offset_prev( ui.you->view_offset );
     return ui.run();
 }
 

CleverRaven/Cataclysm-DDA

src/magic.cpp
@@ -828,8 +828,7 @@ std::optional<tripoint_bub_ms> spell::select_target( Creature *source )
         if( source->is_avatar() ) {
             do {
                 avatar &source_avatar = *source->as_avatar();
-                std::vector<tripoint_bub_ms> trajectory = target_handler::mode_spell( source_avatar, *this, true,
-                        true );
+                std::vector<tripoint_bub_ms> trajectory = target_handler::mode_spell( *this, true, true );
                 if( !trajectory.empty() ) {
                     target = trajectory.back();
                     target_is_valid = is_valid_target( source_avatar, target );

src/ranged.cpp
@@ -542,18 +542,17 @@ target_handler::trajectory target_handler::mode_turrets( vehicle &veh,
     return ui.run();
 }
 
-target_handler::trajectory target_handler::mode_spell( avatar &you, spell &casting, bool no_fail,
-        bool no_mana )
+target_handler::trajectory target_handler::mode_spell( spell &casting, bool no_fail, bool no_mana )
 {
     target_ui ui = target_ui();
-    ui.you = &you;
+    ui.you = &get_avatar();
     ui.mode = target_ui::TargetMode::Spell;
     ui.casting = &casting;
-    ui.range = casting.range( you );
+    ui.range = casting.range( *ui.you );
     ui.no_fail = no_fail;
     ui.no_mana = no_mana;
 
-    restore_on_out_of_scope<tripoint_rel_ms> view_offset_prev( you.view_offset );
+    restore_on_out_of_scope<tripoint_rel_ms> view_offset_prev( ui.you->view_offset );
     return ui.run();
 }
 