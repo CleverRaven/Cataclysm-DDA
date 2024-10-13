8c9d8252bd386eb6883a77edecad009cc7912b3a
Naadn <97841551+Naadn@users.noreply.github.com>
2024-10-13 22:30:27 +0800

    Update src/mutation_data.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/mutation_data.cpp
+++ src/mutation_data.cpp
@@ -321,7 +321,7 @@ void mutation_branch::load( const JsonObject &jo, const std::string_view src )
     optional( jo, was_loaded, "kcal", hunger, false );
     optional( jo, was_loaded, "thirst", thirst, false );
     optional( jo, was_loaded, "sleepiness", sleepiness, false );
-    optional( jo, was_loaded, "mana", mana, false);
+    optional( jo, was_loaded, "mana", mana, false );
     optional( jo, was_loaded, "valid", valid, true );
     optional( jo, was_loaded, "purifiable", purifiable, true );
 
