585c32a0b02bd50b7ddc18f4af3f6f03d7babb19
b3brodie <b3brodie@gmail.com>
2024-10-12 19:18:00 -0700

    rerun tests pt.1

--- src/init.cpp
+++ src/init.cpp
@@ -561,7 +561,7 @@ void DynamicDataLoader::load_mod_data_from_path( const cata_path &path, const st
         }
     }
 }
- 
+
 void DynamicDataLoader::load_mod_interaction_files_from_path( const cata_path &path,
         const std::string &src )
 {
@@ -589,7 +589,6 @@ void DynamicDataLoader::load_mod_interaction_files_from_path( const cata_path &p
             }
         }
     }
-
     // iterate over each file
     for( const std::pair<const mod_id, cata_path> &file : files ) {
         try {
