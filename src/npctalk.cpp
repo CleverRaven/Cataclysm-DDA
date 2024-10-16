df670799181090d858f03be959c80a9d6a5b0714
b3brodie <b3brodie@gmail.com>
2024-10-15 19:42:22 -0700

    Switch to optional struct

--- src/npctalk.cpp
+++ src/npctalk.cpp
@@ -185,7 +185,7 @@ struct item_search_data {
     std::vector<str_or_var> material;
     std::vector<str_or_var> flags;
     std::vector<str_or_var> excluded_flags;
-    std::vector<bool> uses_energy;
+    std::optional<bool> uses_energy;
     bool worn_only;
     bool wielded_only;
     bool held_only;
@@ -255,7 +255,7 @@ struct item_search_data {
             has_condition = true;
         }
         if( jo.has_member( "uses_energy" ) ) {
-            uses_energy.emplace_back( jo.get_member( "uses_energy" ) );
+            uses_energy.emplace( jo.get_member( "uses_energy" ) );
         }
 
         worn_only = jo.get_bool( "worn_only", false );
@@ -369,7 +369,7 @@ struct item_search_data {
             }
         }
 
-        if( !uses_energy.empty() && loc->uses_energy() != uses_energy.front() ) {
+        if( !uses_energy.has_value() && loc->uses_energy() != uses_energy.value() ) {
             return false;
         }
 
