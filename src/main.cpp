e0e3ec485d9dea61f9ab0e3cd162894409d4ff48
Son Phan Trung <phantrungson17@gmail.com>
2024-10-20 19:40:24 +0700

    fix getpid on iOS

--- src/main.cpp
+++ src/main.cpp
@@ -60,7 +60,7 @@
 #include "type_id.h"
 #include "ui_manager.h"
 #include "cata_imgui.h"
-#if defined(MACOSX) || defined(__CYGWIN__)
+#if defined(MACOSX) || defined(__CYGWIN__) || defined(__IPHONEOS__)
 #   include <unistd.h> // getpid()
 #endif
 
