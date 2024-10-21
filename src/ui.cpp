81dcf9c8f3ceb4381b4c023509c3100176bd85d0
Daniel Brooks <db48x@db48x.net>
2024-10-20 21:30:05 -0700

    correct height when a uilist has desired_bounds not including height
    When the desired bounds doesn't include a height, use the screen
    height instead of -1 as the desired height.

--- src/ui.cpp
+++ src/ui.cpp
@@ -746,9 +746,10 @@ void uilist::calc_data()
     calculated_menu_size.x += calculated_hotkey_width + padding;
     calculated_menu_size.x += calculated_label_width + padding;
     calculated_menu_size.x += calculated_secondary_width + padding;
-    float max_avail_height = ImGui::GetMainViewport()->Size.y;
-    if( desired_bounds.has_value() ) {
-        max_avail_height = desired_bounds.value().h;
+    float max_avail_height = 0.9 * ImGui::GetMainViewport()->Size.y;
+    float desired_height;
+    if( desired_bounds.has_value() && ( desired_height = desired_bounds.value().h != -1 ) ) {
+        max_avail_height = std::min( max_avail_height, desired_height );
     }
     calculated_menu_size.y = std::min( max_avail_height - additional_height +
                                        ( s.FramePadding.y * 2.0 ),
