0425f0ecc2f3e428cc9d48114d86b4463b94025d
Zhan Avesna <97810032+AudBobb@users.noreply.github.com>
2024-10-26 18:37:00 -0500

    Add a new widget to display weight with the format "##.#/##.#[unit]" (#77212)
    * Add two new widgets to display weight with the format "##.#/##.# kg" and "##.#/##.# lb", and add the widgets to the legacy labels sidebar
    
    * Apply suggestions from code review
    
    I don't format goodly
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Remove imperial version of the feature, and modify the metric version to read users mass setting and adjust the display accordingly
    
    * Apply suggestions from code review
    
    Formatting
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Updated per IdleSol suggestion
    
    * I think I forgot to pull before pushing and I don't understand git so I think it ate this space in the merge :(
    
    * I'll get it right one day
    
    * Update to use functions I didn't know existed, removing a few lines of code.
    
    * Update src/display.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/widget.h
+++ src/widget.h
@@ -55,6 +55,7 @@ enum class widget_var : int {
     body_graph_wet,        // Body graph showing color-coded body part wetness
     bp_armor_outer_text, // Outermost armor on body part, with color/damage bars
     carry_weight_text,   // Weight carried, relative to capacity, in %
+    carry_weight_value, // Weight carried, formatted as "current/max"
     compass_text,   // Compass / visible threats by cardinal direction
     compass_legend_text, // Names of visible creatures that appear on the compass
     date_text,      // Current date, in terms of day within season
