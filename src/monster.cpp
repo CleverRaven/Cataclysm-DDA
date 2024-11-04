1c1d8cb816c5a6d7689a689f5c4ab1325b52b5f4
Standing-StormStanding-Storm git config --global user.name Standing-Storm git config --global user.name Standing-Storm <dorchadas@gmail.com>
2024-11-03 18:38:52 -0600

    Appease clang?

--- src/monster.cpp
+++ src/monster.cpp
@@ -3566,8 +3566,8 @@ bool monster::is_nether() const
 // The logic is If PSI_NULL, no -> If HAS_MIND, yes -> if ZOMBIE, no -> if HUMAN, yes -> else, no, and monsters temporarily immune to telepathy cannot be seen
 bool monster::has_mind() const
 {
-    return (( !in_species( species_PSI_NULL ) && has_flag( mon_flag_HAS_MIND ) ) ||
-           ( !in_species( species_PSI_NULL ) && !in_species( species_ZOMBIE ) && has_flag( mon_flag_HUMAN ) ) && 
+    return ( ( ( !in_species( species_PSI_NULL ) && has_flag( mon_flag_HAS_MIND ) ) ||
+           ( !in_species( species_PSI_NULL ) && !in_species( species_ZOMBIE ) && has_flag( mon_flag_HUMAN ) ) ) && 
            !has_effect( effect_eff_monster_immune_to_telepathy ) );
 }
 
