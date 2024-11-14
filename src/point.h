b6f11bbcf51ff75a456f4130ea4b44da3695f3de
CLIDragon <84266961+CLIDragon@users.noreply.github.com>
2024-11-14 20:47:00 +1000

    Update point.h

--- src/point.h
+++ src/point.h
@@ -68,7 +68,7 @@ struct point {
     // Dummy implementation of raw() to allow reasoning about
     // abstract generic points.
     constexpr point raw() const {
-        return point( x, y );
+        return this;
     }
 
     /**
@@ -212,7 +212,7 @@ struct tripoint {
     // Dummy implementation of raw() to allow reasoning about
     // abstract generic points.
     constexpr tripoint raw() const {
-        return tripoint( x, y, z );
+        return this;
     }
 
     /**
