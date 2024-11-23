5d0b24055a2298e73881b6799f5932344bba0ba3
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2024-11-22 23:40:58 -0600

    Update consumption.cpp

--- src/consumption.cpp
+++ src/consumption.cpp
@@ -541,12 +541,18 @@ std::pair<int, int> Character::fun_for( const item &comest, bool ignore_already_
     }
 
     if( ( comest.has_flag( flag_LUPINE ) && has_trait( trait_THRESH_LUPINE ) ) ||
-        ( comest.has_flag( flag_HERBIVORE ) && has_trait( trait_HERBIVORE ) ) ||
+        ( comest.has_flag( flag_CATTLE ) && has_trait( trait_THRESH_CATTLE ) ) ||
+        ( comest.has_flag( flag_RABBIT ) && has_trait( trait_THRESH_RABBIT ) ) ||
+        ( food.has_flag( flag_MOUSE ) && has_trait( trait_THRESH_MOUSE ) ) ||
+        ( food.has_flag( flag_RAT ) && has_trait( trait_THRESH_RAT ) ) ||
         ( comest.has_flag( flag_BIRD ) && has_trait( trait_THRESH_BIRD ) ) ||
         ( comest.has_flag( flag_FELINE ) && has_trait( trait_THRESH_FELINE ) ) ) {
         if( fun < 0 ) {
             fun = -fun;
             fun /= 2;
+        }
+        if( fun = 0 ) {
+            fun = 2
         }
     }
 
