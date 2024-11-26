d4a0ef571c39be89b9a8cdeaad2c93e89eac9100
Procyonae <45432782+Procyonae@users.noreply.github.com>
2024-11-26 16:50:43 +0000

    IMGUIfy Help Menu

--- lang/string_extractor/parsers/help.py
+++ lang/string_extractor/parsers/help.py
@@ -5,8 +5,19 @@ from ..write_text import write_text
 def parse_help(json, origin):
     name = get_singular_name(json)
 
-    write_text(json.get("name"), origin, comment="Help menu")
+    write_text(json.get("name"), origin, comment="Help menu category")
 
     for msg in json.get("messages", []):
-        write_text(msg, origin,
-                   comment=f"Help message in menu '{name}'")
+        if type(msg) is dict:
+            if msg.get("subtitle"):
+                write_text(msg["subtitle"]["str"]), origin,
+                       comment=f"Message in help menu category '{name}'")
+            elif msg.get("force_monospaced"):
+                write_text(msg["force_monospaced"]["str"]), origin,
+                       comment=f"Message in help menu category '{name}'")
+            elif msg.get("paragraph"):
+                write_text(msg["paragraph"]["str"]), origin,
+                       comment=f"Message in help menu category '{name}'")
+        else:
+            write_text(msg, origin,
+                   comment=f"Message in help menu category '{name}'")
--- src/help.h
+++ src/help.h
@@ -2,44 +2,109 @@
 #ifndef CATA_SRC_HELP_H
 #define CATA_SRC_HELP_H
 
+#include <imgui/imgui.h>
 #include <map>
+#include <optional>
 #include <string>
+#include <string_view>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
-#include "cuboid_rectangle.h"
-#include "point.h"
+#include "cata_imgui.h"
+#include "color.h"
+#include "input_context.h"
+#include "input_enums.h"
+#include "options.h"
 #include "translation.h"
 
 class JsonObject;
-struct input_event;
 
-namespace catacurses
-{
-class window;
-}  // namespace catacurses
+enum message_modifier {
+    MM_NORMAL = 0, // Normal message
+    MM_SUBTITLE, // Formatted subtitle
+    MM_MONOFONT, // Forced monofont for fixed space diagrams
+    MM_SEPARATOR // ImGui seperator, value is the color to use
+};
 
-class help
+struct help_category {
+    translation name;
+    nc_color color = c_light_blue;
+    std::vector<std::pair<translation, message_modifier>> paragraphs;
+};
+
+class help_data
 {
     public:
-        static void load( const JsonObject &jo, const std::string &src );
-        static void reset();
-        void display_help() const;
+        void load( const JsonObject &jo, const std::string &src );
+        void reset();
+        bool is_read( const int &option ) {
+            return _read_categories.find( option ) != _read_categories.end();
+        }
+        void mark_read( const int &option ) {
+            _read_categories.insert( option );
+        }
+        const help_category &get_help_category( const int &option ) {
+            return _help_categories[option];
+        }
+        const std::map<const int, const help_category> &get_help_categories() {
+            return _help_categories;
+        }
     private:
         void load_object( const JsonObject &jo, const std::string &src );
-        void reset_instance();
-        std::map<int, inclusive_rectangle<point>> draw_menu( const catacurses::window &win,
-                                               int selected, std::map<int, input_event> &hotkeys ) const;
-        static std::string get_note_colors();
-        static std::string get_dir_grid();
+        std::map<const int, const help_category> _help_categories;
+        // Only persists per load, intentionally resets on menu -> game, game -> menu
+        std::unordered_set<int> _read_categories;
         // Modifier for each mods order
         int current_order_start = 0;
         std::string current_src;
-        std::map<int, std::pair<translation, std::vector<translation>>> help_texts;
 };
 
-help &get_help();
+help_data &get_help();
 
-std::string get_hint();
+class help_window : public cataimgui::window
+{
+    public:
+        explicit help_window();
+        void show();
+    protected:
+        void draw_controls() override;
+        cataimgui::bounds get_bounds() override;
+    private:
+        help_data &data = get_help();
+
+        input_context ctxt;
+        std::map<int, input_event> hotkeys;
+
+        const bool screen_reader = get_option<bool>( "SCREEN_READER_MODE" );
+
+        // Width needed to display two of the longest category name side by side
+        bool is_space_for_two_cat();
+        void draw_category_selection();
+        void draw_category_option( const int &option, const help_category &category );
+        // Options aren't necessarily sequential and loop
+        int previous_option( int option );
+        int next_option( int option );
+        int selected_option;
+        bool has_selected_category = false;
+        int loaded_option;
+
+        // Save currently displayed parsed tag + translated paragraphs so they don't need to be recalced every frame
+        void swap_translated_paragraphs();
+        std::vector<std::pair<std::string, message_modifier>> translated_paragraphs;
+
+        void draw_category();
+        cataimgui::scroll s;
+
+        void format_title( std::optional<help_category> category = std::nullopt ) const;
+        void format_subtitle( const std::string_view &translated_category_name,
+                              const nc_color &category_color ) const;
+        void format_footer( const std::string &prev, const std::string &next,
+                            const nc_color &category_color );
+        const float one_em = ImGui::CalcTextSize( "M" ).x;
+
+        // Temporary to fix issue where IMGUI scrollbar width isn't accounted for for the first few frames
+        float get_wrap_width();
+};
 
 #endif // CATA_SRC_HELP_H
