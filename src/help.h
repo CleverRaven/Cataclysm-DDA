32002788393d6cc8763d26503bc6ae4d669bb48a
Procyonae <45432782+Procyonae@users.noreply.github.com>
2024-11-27 17:26:51 +0000

    Tidy up/Consistent naming

--- src/help.h
+++ src/help.h
@@ -10,6 +10,7 @@
 #include "cata_imgui.h"
 #include "input_context.h"
 #include "imgui/imgui.h"
+#include "options.h"
 #include "output.h"
 
 class JsonObject;
@@ -20,13 +21,18 @@ namespace catacurses
 class window;
 }  // namespace catacurses
 
+struct help_category {
+    translation name;
+    std::vector<translation> paragraphs;
+};
+
 class help
 {
     public:
         static void load( const JsonObject &jo, const std::string &src );
         static void reset();
         // TODO: Shouldn't be public
-        std::map<const int, std::pair<translation, std::vector<translation>>> help_texts;
+        std::map<const int, const help_category> help_categories;
     private:
         void load_object( const JsonObject &jo, const std::string &src );
         void reset_instance();
@@ -46,30 +52,38 @@ class help_window : public cataimgui::window
         void draw_controls() override;
         cataimgui::bounds get_bounds() override;
     private:
-        cataimgui::bounds bounds{ 0.0f, 0.0f, static_cast<float>( str_width_to_pixels( TERMX ) ), static_cast<float>( str_height_to_pixels( TERMY ) ) };
-        // 66 is optimal characters per line for reading?
+        // Fullscreen
         float window_width = static_cast<float>( str_width_to_pixels( TERMX ) );
-        //float window_height = static_cast<float>( str_width_to_pixels( TERMY ) );
-        float wrap_width = std::min( window_width * 0.95f,
-                                     static_cast<float>( str_width_to_pixels( 66 ) ) );
-        static std::string get_note_colors();
-        static std::string get_dir_grid();
-        std::string seperator( int length, char c );
-        input_context ctxt;
+        float window_height = static_cast<float>( str_width_to_pixels( TERMY ) );
+        float wrap_width = window_width * 0.95f;
+        cataimgui::bounds bounds{ 0.f, 0.f, static_cast<float>( str_width_to_pixels( TERMX ) ), static_cast<float>( str_height_to_pixels( TERMY ) ) };
+
+        // 66 is optimal characters per line for reading?
+        //float window_width = static_cast<float>( wrap_width ) * 2.025f;
+        //float window_height = static_cast<float>( str_width_to_pixels( TERMY ) ) * 0.8f;
+        //float wrap_width = std::min( window_width * 0.95f, static_cast<float>( str_width_to_pixels( 66 ) ) );
+        //cataimgui::bounds bounds{ 0.f, 0.f, window_width, window_height };
+
         help &data = get_help();
+        bool make_accessible = get_option<bool>( "SCREEN_READER_MODE" );
+        input_context ctxt;
         std::map<int, input_event> hotkeys;
+
+        void draw_category_selection();
+        void format_title( const std::string translated_category_name );
+        std::string seperator( int length, char c );
+
+        void draw_category_option( const int &option, const help_category &category );
         int mouse_selected_option;
         int keyboard_selected_option;
         int last_keyboard_selected_option;
         bool selected_category = false;
-        // TODO: Needs to be a pointer or iterator or something, currently assigning to it is overwriting the first help_text ;D
-        std::pair<translation, std::vector<translation>> &category = data.help_texts.begin()->second;
-        std::string parse_tags_help_window( std::string translated_line );
-        void draw_category_selection();
-        void draw_category( translation &category_name, std::vector<translation> &paragraphs );
-        void display_category_option( const
-                                      std::pair<const int, std::pair<translation, std::vector<translation>>> &help_text );
-        void format_title( const std::string translated_category_name );
+        help_category category;
+
+        void draw_category();
+        void parse_tags_help_window( std::string &translated_line );
+        static std::string get_note_colors();
+        static std::string get_dir_grid();
 };
 
 std::string get_hint();
