9ddff829b2752ee5d273bedb9a2c16f0abbb63b4
George Kightly <kightlygeorge@gmail.com>
2024-12-12 18:37:42 +0000

    Hopefully actually appease clang this time

--- src/advanced_inv.cpp
+++ src/advanced_inv.cpp
@@ -4,6 +4,7 @@
 #include <climits>
 #include <cmath>
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
 #include <initializer_list>
 #include <iterator>
@@ -667,9 +668,9 @@ struct advanced_inv_sorter {
             }
             case SORTBY_PRICEPERWEIGHT: {
                 const double price_density1 = static_cast<double>( d1.items.front()->price( true ) ) /
-                                              static_cast<double>( std::max( 1L, d1.items.front()->weight().value() ) );
+                                              static_cast<double>( std::max<std::int64_t>( 1, d1.items.front()->weight().value() ) );
                 const double price_density2 = static_cast<double>( d2.items.front()->price( true ) ) /
-                                              static_cast<double>( std::max( 1L, d2.items.front()->weight().value() ) );
+                                              static_cast<double>( std::max<std::int64_t>( 1, d2.items.front()->weight().value() ) );
                 if( price_density1 != price_density2 ) {
                     return price_density1 > price_density2;
                 }
