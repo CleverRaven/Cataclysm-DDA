e9fd1c4bd327d79b456d83ad15877e4b4ff179ef
Alex Mooney <AlexMMooney@gmail.com>
2024-12-12 20:10:49 -0800

    Tidy up the new string and add a comment

--- src/activity_actor.cpp
+++ src/activity_actor.cpp
@@ -5423,9 +5423,9 @@ void oxytorch_activity_actor::start( player_activity &act, Character &who )
     }
     if( tool->ammo_sufficient( &who, act.moves_total / 100 ) ||
         query_yn(
-            _( "Your %1$s doesn't have enough charges to complete the job. Continue anyway?" ), tool->tname()
+            _( "Your %1$s doesn't have enough charges to complete the job.  Continue anyway?" ), tool->tname()
         ) ||
-        test_mode
+        test_mode // In the tests, we want to check that the activity can be resumed.
       ) {
         add_msg_debug( debugmode::DF_ACTIVITY, "%s moves_total: %d", act.id().str(), act.moves_total );
         act.moves_left = act.moves_total;
