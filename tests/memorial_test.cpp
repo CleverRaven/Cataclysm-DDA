2a219fe7ea799c3e5c41b19c7b451ffc0def65e2
Standing-Storm <120433252+Standing-Storm@users.noreply.github.com>
2024-12-26 13:07:07 -0600

    [MoM] Add hitting Nether Attunement 5 and Nether Attunement 11 to the list of things that call for concentration checks (#78770)
    * Initial commit
    
    * Remove debug message
    
    * Add limit calculation EoC to base maintenance check
    
    * Update src/creature.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Fix tests?
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/memorial_test.cpp
+++ tests/memorial_test.cpp
@@ -125,7 +125,7 @@ TEST_CASE( "memorials", "[memorial]" )
         m, b, "Caused a resonance cascade." );
 
     check_memorial<event_type::character_gains_effect>(
-        m, b, "Caught on fire.", ch, bodypart_id( "arm_r" ), eff );
+        m, b, "Caught on fire.", ch, bodypart_id( "arm_r" ), eff, 1 );
 
     check_memorial<event_type::character_kills_character>(
         m, b, "Killed an innocent person, victim_name, in cold blood and felt terrible "
