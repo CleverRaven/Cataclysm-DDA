6fc18f9c0dbf685f422b0b6c59c8847654c52c30
moxian <moxian@users.noreply.github.com>
2024-12-26 22:49:05 -0800

    More targed clang-tidy

--- build-scripts/clang-tidy-wrapper.sh
+++ build-scripts/clang-tidy-wrapper.sh
@@ -7,7 +7,9 @@ plugin=build/tools/clang-tidy-plugin/libCataAnalyzerPlugin.so
 
 if [ -f "$plugin" ]
 then
+    set -x
     LD_PRELOAD=$plugin "$CATA_CLANG_TIDY" --enable-check-profile --store-check-profile=clang-tidy-trace "$@"
 else
+    set -x
     "$CATA_CLANG_TIDY" "$@"
 fi
