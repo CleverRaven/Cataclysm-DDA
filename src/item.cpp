f79608c56df33ae3b5807b24af9ad7716fb69c6a
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2024-12-30 00:58:04 -0600

    Update src/item.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/item.cpp
+++ src/item.cpp
@@ -7119,7 +7119,6 @@ int item::price_no_contents( bool practical, std::optional<int> price_override )
     if( flag_id_irreplaceable_consumeable() ) {
         // irreplaceable items are worth more by how many seasons from the cataclysm player is
         price *= 0.25 + ( calendar::turn - calendar::start_of_cataclysm ) / calendar::season_length();
-        
     }
 
     for( fault_id fault : faults ) {
