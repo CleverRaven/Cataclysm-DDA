f9882e6b08c499c5a03b0687c0276978318414de
Brambor <tondadrd@seznam.cz>
2025-01-02 19:10:21 +0100

    Recast spell

--- src/action.h
+++ src/action.h
@@ -188,6 +188,8 @@ enum action_id : int {
     ACTION_SELECT_DEFAULT_AMMO,
     /** Cast a spell (only if any spells are known) */
     ACTION_CAST_SPELL,
+    /** Recast last spell */
+    ACTION_RECAST_SPELL,
     /** Open the insert-item menu */
     ACTION_INSERT_ITEM,
     /** Unload container in a given direction */
--- src/handle_action.cpp
+++ src/handle_action.cpp
@@ -1831,7 +1831,7 @@ static void open_movement_mode_menu()
     }
 }
 
-static void cast_spell()
+static void cast_spell( bool recast_spell = false )
 {
     Character &player_character = get_player_character();
     player_character.magic->clear_opens_spellbook_data();
@@ -1873,11 +1873,19 @@ static void cast_spell()
         }
     }
 
-    spell &sp = player_character.magic->select_spell( player_character );
+    if( recast_spell && player_character.magic->last_spell.is_null() ) {
+        popup( _( "Cast a spell first" ) );
+        return;
+    }
+
+    spell &sp = recast_spell
+                ? player_character.magic->get_spell( player_character.magic->last_spell )
+                : player_character.magic->select_spell( player_character );
     // if no spell was selected
     if( sp.id().is_null() ) {
         return;
     }
+    player_character.magic->last_spell = sp.id();
     player_character.cast_spell( sp, false, std::nullopt );
 }
 
@@ -2636,6 +2644,10 @@ bool game::do_regular_action( action_id &act, avatar &player_character,
 
         case ACTION_CAST_SPELL:
             cast_spell();
+            break;
+
+        case ACTION_RECAST_SPELL:
+            cast_spell( true );
             break;
 
         case ACTION_FIRE_BURST: {
