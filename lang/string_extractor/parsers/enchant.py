ef2b448bc724cb531b68daa5d67455cbdb68d6a9
Procyonae <45432782+Procyonae@users.noreply.github.com>
2025-01-12 15:32:15 +0000

    Missed iterating an array

--- lang/string_extractor/parsers/enchant.py
+++ lang/string_extractor/parsers/enchant.py
@@ -16,10 +16,13 @@ def parse_enchant(json, origin):
     if "hit_me_effect" in json:
         parse_effect(json["hit_me_effect"], origin)
 
-    if "special_vision" in json and "descriptions" in json["special_vision"]:
-        for description in json["special_vision"]["descriptions"]:
-            if "text" in description:
-                special_vision_id = description["id"]
-                write_text(description["text"], origin,
-                        comment="Description of creature revealed by special"
-                                " vision \"{}\"".format(special_vision_id))
+    if "special_vision" in json:
+        for vision in json["special_vision"]:
+            if "descriptions" in vision:
+                for description in vision["descriptions"]:
+                    if "text" in description:
+                        special_vision_id = description["id"]
+                        write_text(description["text"], origin,
+                                comment="Description of creature revealed by "
+                                        "special vision \"{}\""
+                                                    .format(special_vision_id))
