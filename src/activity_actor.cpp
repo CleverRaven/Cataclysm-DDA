2cc23cc69be28968ea91805901c5f9abb4530760
marilynias <marilynias57+github@gmail.com>
2025-01-21 14:53:59 +0100

    rem debug var

--- src/activity_actor.cpp
+++ src/activity_actor.cpp
@@ -3871,7 +3871,6 @@ void craft_activity_actor::canceled( player_activity &/*act*/, Character &/*who*
         return;
     }
     const recipe item_recipe = craft->get_making();
-    auto cat = *item_recipe.category;
     // recipe items with no components and results can be safely removed
     if( item_recipe.result() == itype_id::NULL_ID() && craft->components.empty() ) {
         craft_item.remove_item();
