ed7abfa157ee8da4bce00bd7b90d0726c109ed00
PatrikLundell <j.patrik.r.lundell@gmail.com>
2025-02-08 15:27:10 +0100

    Update src/vehicle.h
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/vehicle.h
+++ src/vehicle.h
@@ -2490,7 +2490,7 @@ class DefaultRemovePartHandler : public RemovePartHandler
             get_map().set_floor_cache_dirty( z );
         }
         void removed( map *here, vehicle &veh, int part ) override;
-        void spawn_animal_from_part( item &base, map * /*here*/ , const tripoint_bub_ms& loc) override {
+        void spawn_animal_from_part( item &base, map * /*here*/, const tripoint_bub_ms &loc ) override {
             base.release_monster( loc, 1 );
         }
         map &get_map_ref() override {
