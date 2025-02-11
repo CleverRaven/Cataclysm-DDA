cdaaeed1c6fcefc99e1de86cd0a7569ddb06bfd4
PatrikLundell <j.patrik.r.lundell@gmail.com>
2025-02-11 15:44:09 +0100

    demanded changes

--- src/vehicle_use.cpp
+++ src/vehicle_use.cpp
@@ -2297,7 +2297,7 @@ void vehicle::build_interact_menu( veh_menu &menu, map *here, const tripoint_bub
                  fuel_left( *here, itype_battery ) >= itype_water_purifier->charges_to_use() )
         .hotkey( "PURIFY_WATER" )
         .on_submit( [this, &here] {
-            const auto sel = []( const map & here, const vehicle_part & pt )
+            const auto sel = []( const map &, const vehicle_part & pt )
             {
                 return pt.is_tank() && pt.ammo_current() == itype_water;
             };
