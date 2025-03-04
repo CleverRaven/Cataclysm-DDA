0d64364ad6dd8ef75b44c1d608e3597220422d2b
Anton Simakov <67688115+GuardianDll@users.noreply.github.com>
2025-03-04 10:52:17 +0100

    move calorie burn visibility to json flag, make smart watch show calorie burn

--- src/game_inventory.cpp
+++ src/game_inventory.cpp
@@ -77,6 +77,7 @@ static const activity_id ACT_EAT_MENU( "ACT_EAT_MENU" );
 static const bionic_id bio_fitnessband( "bio_fitnessband" );
 static const bionic_id bio_painkiller( "bio_painkiller" );
 
+static const flag_id json_flag_CALORIE_BURN( "CALORIE_BURN" );
 static const flag_id json_flag_CALORIES_INTAKE( "CALORIES_INTAKE" );
 
 static const itype_id itype_fitness_band( "fitness_band" );
@@ -937,7 +938,7 @@ static std::string get_consume_needs_hint( Character &you )
     int kcal_ingested_yesterday = you.as_avatar()->get_daily_ingested_kcal( true );
     int kcal_spent_today = you.as_avatar()->get_daily_spent_kcal( false );
     int kcal_spent_yesterday = you.as_avatar()->get_daily_spent_kcal( true );
-    bool has_fitness_band =  you.is_wearing( itype_fitness_band ) || you.has_bionic( bio_fitnessband );
+    bool has_fitness_band =  you.cache_has_item_with( json_flag_CALORIE_BURN );
     bool has_tracker = has_fitness_band || you.cache_has_item_with( json_flag_CALORIES_INTAKE );
 
     std::string kcal_estimated_intake;
