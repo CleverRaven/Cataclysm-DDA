c3e30cf7ba71b6e8236182adecf4102b1efb226b
Jack Dodge <51180872+jankyd@users.noreply.github.com>
2025-03-09 18:41:26 -0500

    Update src/character.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/character.cpp
+++ src/character.cpp
@@ -13046,8 +13046,8 @@ void Character::search_surroundings()
 
 bool Character::wield( item &it, std::optional<int> obtain_cost )
 {
-    if (it.is_null()) {
-        add_msg_if_player(m_info, _("ERROR: Null item. Shouldn't ever see this message."));
+    if( it.is_null() ) {
+        add_msg_if_player( m_info, _( "ERROR: Null item. Shouldn't ever see this message." ) );
         return false;
     }
     invalidate_inventory_validity_cache();
