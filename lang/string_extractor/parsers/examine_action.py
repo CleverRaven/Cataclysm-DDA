4109ae807418f9edae065853d7f19c6b9fc2776d
Anton Simakov <67688115+GuardianDll@users.noreply.github.com>
2025-03-11 14:04:06 +0100

    please flake8

--- lang/string_extractor/parsers/examine_action.py
+++ lang/string_extractor/parsers/examine_action.py
@@ -6,16 +6,16 @@ def parse_examine_action(json, origin, name):
     ename = "{}'s examine action".format(name)
     if "query_msg" in json:
         write_text(json["query_msg"], origin,
-                   comment = "Query message for {}".format(ename))
+                   comment="Query message for {}".format(ename))
     if "success_msg" in json:
         write_text(json["success_msg"], origin,
-                   comment = "Message when {} succeeds".format(ename))
+                   comment="Message when {} succeeds".format(ename))
     if "redundant_msg" in json:
         write_text(json["redundant_msg"], origin,
-                   comment = "Message when {} would be redundant".format(ename))
+                   comment="Message when {} would be redundant".format(ename))
     if "condition_fail_msg" in json:
         write_text(json["condition_fail_msg"], origin,
-                   comment = "Message when {} fails its condition".format(ename))
+                   comment="Message when {} fails its condition".format(ename))
     if "effect_on_conditions" in json:
         for eoc in json["effect_on_conditions"]:
             if type(eoc) is dict:
