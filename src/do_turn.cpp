79abc4e455bd608df81ced25f249eeae3c8bdd7a
Kray4000 <slavik.games@gmail.com>
2025-03-12 17:39:38 +0200

    Update src/do_turn.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/do_turn.cpp
+++ src/do_turn.cpp
@@ -328,7 +328,9 @@ void monmove()
             m.creature_in_field( critter );
         }
 
-        if( !critter.is_dead() && !critter.is_hallucination() && rl_dist( u.pos_abs(), critter.pos_abs() ) <= u.enchantment_cache->modify_value( enchant_vals::mod::MOTION_ALARM, 0 ) ) {
+        if( !critter.is_dead() && !critter.is_hallucination() &&
+            rl_dist( u.pos_abs(), critter.pos_abs() ) <= u.enchantment_cache->modify_value(
+                enchant_vals::mod::MOTION_ALARM, 0 ) ) {
             if( u.has_active_bionic( bio_alarm ) ) {
                 u.mod_power_level( -bio_alarm->power_trigger );
                 add_msg( m_warning, _( "Your motion alarm goes off!" ) );
