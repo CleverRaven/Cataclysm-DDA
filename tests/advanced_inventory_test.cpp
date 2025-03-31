e7eb767e877ae9794002a9c46f4e005a91051838
marilynias <marilynia57+github@gmail.com>
2025-03-31 12:10:32 +0200

    sort

--- tests/advanced_inventory_test.cpp
+++ tests/advanced_inventory_test.cpp
@@ -29,9 +29,8 @@
 static const itype_id itype_backpack( "backpack" );
 static const itype_id itype_debug_backpack( "debug_backpack" );
 static const itype_id itype_knife_combat( "knife_combat" );
-static const itype_id itype_test_heavy_debug_backpack( "test_heavy_debug_backpack" );
 static const itype_id itype_test_9mm_ammo( "test_9mm_ammo" );
-
+static const itype_id itype_test_heavy_debug_backpack( "test_heavy_debug_backpack" );
 
 /*
     --------- AIM testing ----------
