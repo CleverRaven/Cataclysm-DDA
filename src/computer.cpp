fceceeee7eabd833f7d3009fb07c2311e8ae481d
andrei <a12l+git@runbox.com>
2025-04-04 15:18:35 +0300

    eoc: unstringify furniture variables

--- src/computer.cpp
+++ src/computer.cpp
@@ -127,9 +127,9 @@ void computer::set_mission( const int id )
 }
 
 // Methods for setting/getting misc key/value pairs.
-void computer::set_value( const std::string &key, const std::string &value )
+void computer::set_value( const std::string &key, diag_value value )
 {
-    values[ key ] = value;
+    values[ key ] = std::move( value );
 }
 
 void computer::remove_value( const std::string &key )
@@ -137,10 +137,10 @@ void computer::remove_value( const std::string &key )
     values.erase( key );
 }
 
-std::optional<std::string> computer::maybe_get_value( const std::string &key ) const
+diag_value const *computer::maybe_get_value( const std::string &key ) const
 {
     auto it = values.find( key );
-    return it == values.end() ? std::nullopt : std::optional<std::string> { it->second };
+    return it == values.end() ? nullptr : &it->second;
 }
 
 static computer_action computer_action_from_legacy_enum( int val );
