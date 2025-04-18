4ee7afbd733e2814b46997b84b75c8219d085028
ShenMian <sms_school@outlook.com>
2025-04-18 05:17:56 +0100

    Replace `const std::string_view &` with `std::string_view`

--- src/condition.cpp
+++ src/condition.cpp
@@ -259,7 +259,7 @@ duration_or_var_part get_duration_or_var_part( const JsonValue &jv )
     return ret_val;
 }
 
-duration_or_var get_duration_or_var( const JsonObject &jo, const std::string_view &member,
+duration_or_var get_duration_or_var( const JsonObject &jo, std::string_view member,
                                      bool required,
                                      time_duration default_val )
 {
