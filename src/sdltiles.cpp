2c0585189ab077f9923d807cebba1dcaf674bed0
Son Phan Trung <phantrungson17@gmail.com>
2025-04-22 16:46:07 +0700

    fix

--- src/sdltiles.cpp
+++ src/sdltiles.cpp
@@ -3354,8 +3354,8 @@ static void CheckMessages()
                 dbg( D_INFO ) << "Fingermotion triggered.";
                 dbg( D_INFO ) << "ev.tfinger.fingerId: " << ev.tfinger.fingerId;
                 dbg( D_INFO ) << "ev.tfinger.touchId: " << ev.tfinger.touchId;
-                int numfingers = SDL_GetNumTouchFingers( ev.tfinger.touchId );
-                if( numfingers == 1 ) {
+                int numfingers_motion = SDL_GetNumTouchFingers( ev.tfinger.touchId );
+                if( numfingers_motion == 1 ) {
                     if( !is_quick_shortcut_touch ) {
                         dbg( D_INFO ) << "Not quick shortcut touch";
                         update_finger_repeat_delay();
@@ -3379,10 +3379,10 @@ static void CheckMessages()
                             finger_down_y += delta_y * delta_ratio;
                         }
                     }
-                } else if( numfingers == 2 ) {
+                } else if( numfingers_motion == 2 ) {
                     second_finger_curr_x = ev.tfinger.x * WindowWidth;
                     second_finger_curr_y = ev.tfinger.y * WindowHeight;
-                } else if( numfingers == 3 ) {
+                } else if( numfingers_motion == 3 ) {
                     third_finger_curr_x = ev.tfinger.x * WindowWidth;
                     third_finger_curr_y = ev.tfinger.y * WindowHeight;
                 }
@@ -3390,8 +3390,8 @@ static void CheckMessages()
             case SDL_FINGERDOWN:
                 dbg( D_INFO ) << "Fingerdown triggered.";
                 dbg( D_INFO ) << "ev.tfinger.fingerId: " << ev.tfinger.fingerId;
-                int numfingers = SDL_GetNumTouchFingers( ev.tfinger.touchId );
-                if( numfingers == 1 ) {
+                int numfingers_down = SDL_GetNumTouchFingers( ev.tfinger.touchId );
+                if( numfingers_down == 1 ) {
                     finger_down_x = finger_curr_x = ev.tfinger.x * WindowWidth;
                     finger_down_y = finger_curr_y = ev.tfinger.y * WindowHeight;
                     finger_down_time = ticks;
@@ -3405,13 +3405,13 @@ static void CheckMessages()
                     }
                     ui_manager::redraw_invalidated();
                     needupdate = true; // ensure virtual joystick and quick shortcuts redraw as we interact
-                } else if( numfingers == 2 && !is_quick_shortcut_touch ) {
+                } else if( numfingers_down == 2 && !is_quick_shortcut_touch ) {
                     second_finger_down_x = second_finger_curr_x = ev.tfinger.x * WindowWidth;
                     second_finger_down_y = second_finger_curr_y = ev.tfinger.y * WindowHeight;
                     DebugLog( D_INFO, DC_ALL ) << "second_finger_curr_x: " << finger_down_x;
                     DebugLog( D_INFO, DC_ALL ) << "second_finger_curr_y: " << second_finger_curr_y;
                     is_two_finger_touch = true;
-                } else if( numfingers == 3 && !is_quick_shortcut_touch ) {
+                } else if( numfingers_down == 3 && !is_quick_shortcut_touch ) {
                     third_finger_down_x = third_finger_curr_x = ev.tfinger.x * WindowWidth;
                     third_finger_down_y = third_finger_curr_y = ev.tfinger.y * WindowHeight;
                     is_three_finger_touch = true;
@@ -3421,8 +3421,8 @@ static void CheckMessages()
             case SDL_FINGERUP:
                 dbg( D_INFO ) << "Fingerup triggered.";
                 dbg( D_INFO ) << "ev.tfinger.fingerId: " << ev.tfinger.fingerId;
-                int numfingers = SDL_GetNumTouchFingers( ev.tfinger.touchId );
-                if( numfingers == 1 ) {
+                int numfingers_up = SDL_GetNumTouchFingers( ev.tfinger.touchId );
+                if( numfingers_up == 1 ) {
                     finger_curr_x = ev.tfinger.x * WindowWidth;
                     finger_curr_y = ev.tfinger.y * WindowHeight;
                     DebugLog( D_INFO, DC_ALL ) << "finger_curr_x: " << finger_curr_x;
