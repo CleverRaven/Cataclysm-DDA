3357ce2143ebfa4308f481b4aaf871650dbbc7c1
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2025-05-17 21:44:28 -0500

    Update monster.cpp

--- src/monster.cpp
+++ src/monster.cpp
@@ -1869,8 +1869,8 @@ monster_attitude monster::attitude( const Character *u ) const
     }
 
     bool target_has_anger_relation = false;
-        if( u != nullptr ) {
-        for( const trait_id &mut : u->get_functioning_mutations() ) {
+if( u != nullptr ) {
+    for( const trait_id &mut : u->get_functioning_mutations() ) {
         const mutation_branch &branch = *mut;
         for( const std::pair<const species_id, int> &elem : branch.anger_relations ) {
             if( type->in_species( elem.first ) ) {
@@ -1884,11 +1884,8 @@ monster_attitude monster::attitude( const Character *u ) const
     }
 }
 
-    if( u != nullptr && !aggro_character && !u->is_monster() ) {
-        return MATT_IGNORE;
-    }
-
-    return MATT_ATTACK;
+if( u != nullptr && !aggro_character && !u->is_monster() && !target_has_anger_relation ) {
+    return MATT_IGNORE;
 }
 
 int monster::hp_percentage() const
