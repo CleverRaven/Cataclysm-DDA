6829d827d01708e9c54a6a40cc0e8ef2634a1fd5
Son Phan Trung <phantrungson17@gmail.com>
2025-06-07 10:16:12 +0700

    I think I figure it out

--- src/sdltiles.cpp
+++ src/sdltiles.cpp
@@ -3377,7 +3377,7 @@ static void CheckMessages()
                 dbg( D_INFO ) << "ev.tfinger.touchId: " << ev.tfinger.touchId;
                 numfingers = SDL_GetNumTouchFingers( ev.tfinger.touchId );
                 dbg( D_INFO ) << "numfingers: " << numfingers;
-                if( numfingers == 1 && ev.tfinger.fingerId >= 0 ) {
+                if( numfingers == 1 && ev.tfinger.fingerId > 0 ) {
                     if( !is_quick_shortcut_touch ) {
                         dbg( D_INFO ) << "Not quick shortcut touch";
                         update_finger_repeat_delay();
@@ -3401,10 +3401,10 @@ static void CheckMessages()
                             finger_down_y += delta_y * delta_ratio;
                         }
                     }
-                } else if( numfingers == 2 && ev.tfinger.fingerId >= 0 ) {
+                } else if( numfingers == 2 && ev.tfinger.fingerId > 0 ) {
                     second_finger_curr_x = ev.tfinger.x * WindowWidth;
                     second_finger_curr_y = ev.tfinger.y * WindowHeight;
-                } else if( numfingers == 3 && ev.tfinger.fingerId >= 0 ) {
+                } else if( numfingers == 3 && ev.tfinger.fingerId > 0 ) {
                     third_finger_curr_x = ev.tfinger.x * WindowWidth;
                     third_finger_curr_y = ev.tfinger.y * WindowHeight;
                 }
@@ -3414,7 +3414,7 @@ static void CheckMessages()
                 dbg( D_INFO ) << "ev.tfinger.fingerId: " << ev.tfinger.fingerId;
                 numfingers = SDL_GetNumTouchFingers( ev.tfinger.touchId );
                 dbg( D_INFO ) << "numfingers: " << numfingers;
-                if( numfingers == 1 && ev.tfinger.fingerId >= 0 ) {
+                if( numfingers == 1 && ev.tfinger.fingerId > 0 ) {
                     finger_down_x = finger_curr_x = ev.tfinger.x * WindowWidth;
                     finger_down_y = finger_curr_y = ev.tfinger.y * WindowHeight;
                     finger_down_time = ticks;
@@ -3428,13 +3428,13 @@ static void CheckMessages()
                     }
                     ui_manager::redraw_invalidated();
                     needupdate = true; // ensure virtual joystick and quick shortcuts redraw as we interact
-                } else if( numfingers == 2 && ev.tfinger.fingerId >= 0 && !is_quick_shortcut_touch ) {
+                } else if( numfingers == 2 && ev.tfinger.fingerId > 0 && !is_quick_shortcut_touch ) {
                     second_finger_down_x = second_finger_curr_x = ev.tfinger.x * WindowWidth;
                     second_finger_down_y = second_finger_curr_y = ev.tfinger.y * WindowHeight;
                     DebugLog( D_INFO, DC_ALL ) << "second_finger_curr_x: " << finger_down_x;
                     DebugLog( D_INFO, DC_ALL ) << "second_finger_curr_y: " << second_finger_curr_y;
                     is_two_finger_touch = true;
-                } else if( numfingers == 3 && ev.tfinger.fingerId >= 0 && !is_quick_shortcut_touch ) {
+                } else if( numfingers == 3 && ev.tfinger.fingerId > 0 && !is_quick_shortcut_touch ) {
                     third_finger_down_x = third_finger_curr_x = ev.tfinger.x * WindowWidth;
                     third_finger_down_y = third_finger_curr_y = ev.tfinger.y * WindowHeight;
                     is_three_finger_touch = true;
@@ -3446,7 +3446,7 @@ static void CheckMessages()
                 dbg( D_INFO ) << "ev.tfinger.fingerId: " << ev.tfinger.fingerId;
                 numfingers = SDL_GetNumTouchFingers( ev.tfinger.touchId );
                 dbg( D_INFO ) << "numfingers: " << numfingers;
-                if( numfingers == 1 && ev.tfinger.fingerId >= 0 ) {
+                if( numfingers == 1 && ev.tfinger.fingerId > 0 ) {
                     finger_curr_x = ev.tfinger.x * WindowWidth;
                     finger_curr_y = ev.tfinger.y * WindowHeight;
                     DebugLog( D_INFO, DC_ALL ) << "finger_curr_x: " << finger_curr_x;
