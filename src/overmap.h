c814af8ebb9610586d199d65693c23afc0c3187a
Kevin Granade <kevin.granade@gmail.com>
2025-06-21 14:32:23 -0700

    Move omt map sumary to own file, add placeholder store

--- src/overmap.h
+++ src/overmap.h
@@ -5,12 +5,14 @@
 #include <stdint.h>
 #include <algorithm>
 #include <array>
+#include <bitset>
 #include <climits>
 #include <cstdlib>
 #include <functional>
 #include <iosfwd>
 #include <iterator>
 #include <map>
+#include <memory>
 #include <optional>
 #include <string>
 #include <string_view>
@@ -49,6 +51,7 @@ class cata_path;
 class character_id;
 class npc;
 class overmap_connection;
+struct map_data_summary;
 struct regional_settings;
 template <typename T> struct enum_traits;
 
@@ -125,16 +128,6 @@ enum class om_vision_level : int8_t {
 template<>
 struct enum_traits<om_vision_level> {
     static constexpr om_vision_level last = om_vision_level::last;
-};
-
-// Summarises one OMT worth of map data for use by agents operating at overmap scale.
-// E.g. hordes, fire spread, NPCs.
-// Dimensions are 24x24
-struct map_data_summary {
-    map_data_summary() = default;
-    map_data_summary( std::bitset<24 * 24> new_passable ): passable( new_passable ) {}
-    // TODO: constant?
-    std::bitset<24 * 24> passable;
 };
 
 struct map_layer {
