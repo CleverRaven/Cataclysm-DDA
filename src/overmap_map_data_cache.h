183925433a4770ef375cbe7dab80dfd15c08f357
Kevin Granade <kevin.granade@gmail.com>
2025-06-22 13:33:46 -0700

    Sins against memory management
    Also implements using both placeholders and dynamic map_data_summary objects in overmap.

--- src/overmap_map_data_cache.h
+++ src/overmap_map_data_cache.h
@@ -3,16 +3,19 @@
 #define CATA_SRC_OVERMAP_MAP_DATA_CACHE_H
 
 #include <bitset>
+#include <memory>
 #include <string>
 
-#include "json.h"
+#include "string_id.h"
 
+class JsonObject;
 struct map_data_summary;
 
 namespace map_data_placeholders
 {
 void load( const JsonObject &jo, const std::string &src );
 void reset();
+std::shared_ptr<const map_data_summary> get_ptr( string_id<map_data_summary> id );
 void finalize();
 } // namespace map_data_placeholders
 
@@ -20,6 +23,11 @@ void finalize();
 // E.g. hordes, fire spread, NPCs.
 // overmap already has to do a ton of coordinate stuff so just exposing the data
 // for the overmap to access instead of wrapping it in methods.
+// These come in two different flavors. "Placeholder" entries are loaded from json and live in
+// the placeholder_map_data generic_factory.  We pass "non-owning" references to these to
+// overmap::layer[].map_cache to use until the underlying data is edited such that we need precise
+// data instead, in which case we create an "actual" map_data_summary that is then kept in sync
+// with the underlying OMT data and serialized with the overmap.
 struct map_data_summary {
     map_data_summary() = default;
     map_data_summary( std::bitset<24 * 24> const &new_passable,
