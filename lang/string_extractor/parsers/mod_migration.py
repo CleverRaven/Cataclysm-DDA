3915bb9492e47bbeb67b1d4b19ba41accc1167de
Procyonae <45432782+Procyonae@users.noreply.github.com>
2025-06-29 19:18:55 +0100

    Whoops

--- lang/string_extractor/parsers/mod_migration.py
+++ lang/string_extractor/parsers/mod_migration.py
@@ -4,6 +4,6 @@ from ..write_text import write_text
 def parse_mod_migration(json, origin):
     id = json["id"]
 
-    if "removal_message" in json:
-        write_text(json["removal_message"], origin,
+    if "removal_reason" in json:
+        write_text(json["removal_reason"], origin,
                    comment="Explaination for removal of mod \"{}\"".format(id))
