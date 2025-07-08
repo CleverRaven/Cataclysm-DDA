68596dec9089c13681f93b7f5af8fd258997aff7
Erk <45136638+I-am-Erk@users.noreply.github.com>
2025-07-08 12:42:06 -0700

    Update mapdata.cpp

--- src/mapdata.cpp
+++ src/mapdata.cpp
@@ -281,6 +281,7 @@ std::string enum_to_string<ter_furn_flag>( ter_furn_flag data )
         case ter_furn_flag::TFLAG_HARVEST_REQ_CUT1: return "HARVEST_REQ_CUT1";
         case ter_furn_flag::TFLAG_NATURAL_UNDERGROUND: return "NATURAL_UNDERGROUND";
         case ter_furn_flag::TFLAG_WIRED_WALL: return "WIRED_WALL";
+        case ter_furn_flag::TFLAG_MON_AVOID_STRICT: return "MON_AVOID_STRICT";
 
         // *INDENT-ON*
         case ter_furn_flag::NUM_TFLAG_FLAGS:
