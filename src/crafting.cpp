c4839036a4cc6df3b8ca952831377ac8b98503a2
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2025-07-19 23:08:00 -0500

    Apply suggestions from code review

--- src/crafting.cpp
+++ src/crafting.cpp
@@ -34,7 +34,6 @@
 #include "effect_on_condition.h"
 #include "enum_traits.h"
 #include "enums.h"
-#include "explosion.h"
 #include "faction.h"
 #include "flag.h"
 #include "game.h"
@@ -119,7 +118,6 @@ static const trait_id trait_INT_ALPHA( "INT_ALPHA" );
 static const std::string flag_AFFECTED_BY_PAIN( "AFFECTED_BY_PAIN" );
 static const std::string flag_BLIND_EASY( "BLIND_EASY" );
 static const std::string flag_BLIND_HARD( "BLIND_HARD" );
-static const std::string flag_EXPLOSIVE( "EXPLOSIVE" );
 static const std::string flag_FULL_MAGAZINE( "FULL_MAGAZINE" );
 static const std::string flag_NO_BENCH( "NO_BENCH" );
 static const std::string flag_NO_ENCHANTMENT( "NO_ENCHANTMENT" );
@@ -1365,9 +1363,6 @@ static void destroy_random_component( item &craft, const Character &crafter )
     crafter.add_msg_player_or_npc( game_message_params( game_message_type::m_bad ),
                                    _( "You mess up and destroy the %s." ),
                                    _( "<npcname> messes up and destroys the %s" ), destroyed.tname() );
-
-    // Explosive failure chance reduced by proficiency ratio
-    if( craft.has_flag( flag_EXPLOSIVE ) ) {
         const recipe &rec = craft.get_making();
         const std::vector<recipe_proficiency> &profs = rec.proficiencies;
         int num_required = 0;
