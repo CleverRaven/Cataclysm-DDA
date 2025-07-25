5859177c37ff6262a0d1f2b74b2737a359f188d3
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2025-07-24 23:46:10 -0500

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/new_character_test.cpp
+++ tests/new_character_test.cpp
@@ -27,7 +27,7 @@ static const trait_id trait_ANTIFRUIT( "ANTIFRUIT" );
 static const trait_id trait_ANTIJUNK( "ANTIJUNK" );
 static const trait_id trait_ANTIWHEAT( "ANTIWHEAT" );
 static const trait_id trait_ASTHMA( "ASTHMA" );
-static const trait_id trait_CANNIBAL("CANNIBAL");
+static const trait_id trait_CANNIBAL( "CANNIBAL" );
 static const trait_id trait_LACTOSE( "LACTOSE" );
 static const trait_id trait_MEATARIAN( "MEATARIAN" );
 static const trait_id trait_TAIL_FLUFFY( "TAIL_FLUFFY" );
@@ -232,9 +232,8 @@ TEST_CASE( "Generated_character_with_category_mutations", "[mutation]" )
 
 TEST_CASE( "cannibal_not_randomly_selected", " [character] [traits] [random]" )
 {
-    // Try many times to ensure randomness is well-sampled
-    for (int i = 0; i < 1000; ++i) {
+    for( int i = 0; i < 1000; ++i ) {
         trait_id random_trait = get_avatar().random_good_trait();
-        REQUIRE(random_trait != trait_CANNIBAL);
+        REQUIRE( random_trait != trait_CANNIBAL );
     }
 }
