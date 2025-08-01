3e5a266094ebb6bfdccff584addb097eabda9ab1
Andrew Krieger <akrieger@users.noreply.github.com>
2025-08-01 14:08:34 -0700

    Fix min save archive generation for compressed saves.
    Overmap logic needed to handle .zzip extension but otherwise unchanged.
    Map memory needs to generate a minimized zzip stack which requires iterating
    the current stack and copying the in-range entries to a new zzip.

--- src/zzip.cpp
+++ src/zzip.cpp
@@ -469,6 +469,10 @@ bool zzip::add_file( std::filesystem::path const &zzip_relative_path, std::strin
 bool zzip::copy_files( std::vector<std::filesystem::path> const &zzip_relative_paths,
                        std::shared_ptr<zzip> const &from )
 {
+    if( zzip_relative_paths.empty() ) {
+        return true;
+    }
+
     zzip_footer other_footer{ from->footer_ };
     JsonObject original_footer = copy_footer();
     original_footer.allow_omitted_members();
