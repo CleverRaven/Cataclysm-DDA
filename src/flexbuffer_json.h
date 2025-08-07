f451689eeeff45483a98cf86a0429bfde16b4908
ehughsbaird <44244083+ehughsbaird@users.noreply.github.com>
2025-08-07 15:58:25 +0000

    get_int64 for JsonObject
    Just boilerplate

--- src/flexbuffer_json.h
+++ src/flexbuffer_json.h
@@ -617,6 +617,7 @@ class JsonObject : JsonWithPath
         // Vanilla accessors. Just return the named member and use it's conversion function.
         bool get_bool( std::string_view key ) const;
         int get_int( std::string_view key ) const;
+        int64_t get_int64( std::string_view key ) const;
         double get_float( std::string_view key ) const;
         JsonArray get_array( std::string_view key ) const;
         JsonObject get_object( std::string_view key ) const;
