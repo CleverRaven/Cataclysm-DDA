558cbcbc447576c4d6117651cf643b1d56262cf7
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2025-08-27 19:19:02 -0500

    Update mutation.cpp

--- src/mutation.cpp
+++ src/mutation.cpp
@@ -1363,8 +1363,6 @@ void Character::mutate( const int &true_random_chance, bool use_vitamins )
                 return;
             }
         }
-    } while( valid.empty() );
-}
 
 void Character::mutate( )
 {
