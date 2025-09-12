38de5421c4dca53a1fa58bd371a84b95a16d08d4
John Candlebury <johncandlebury@gmail.com>
2025-09-11 21:51:08 -0600

    Aftershock: Ruins of Orion I (#82770)
    * Ruins of orion
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update mapgen.cpp
    
    Revert "Update mapgen.cpp"
    
    This reverts commit 6ed38aa49b2e1ec0a8fbd9249dcf979259e36c23.
    
    Update mapgen.cpp
    
    * Update src/mapgen.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Fix json omissions
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/overmap.cpp
+++ src/overmap.cpp
@@ -756,6 +756,7 @@ std::string enum_to_string<oter_flags>( oter_flags data )
         case oter_flags::ravine: return "RAVINE";
         case oter_flags::ravine_edge: return "RAVINE_EDGE";
         case oter_flags::pp_generate_riot_damage: return "PP_GENERATE_RIOT_DAMAGE";
+        case oter_flags::pp_generate_ruined: return "PP_GENERATE_RUINED";
         case oter_flags::generic_loot: return "GENERIC_LOOT";
         case oter_flags::risk_extreme: return "RISK_EXTREME";
         case oter_flags::risk_high: return "RISK_HIGH";
