e026b9cc5d525cc1809a8e993a2a23db3a59e9f3
Sab Pyrope <99945024+Uwuewsky@users.noreply.github.com>
2025-10-22 19:45:22 +0800

    Update extractor part 2

--- lang/string_extractor/parsers/recipe_category.py
+++ lang/string_extractor/parsers/recipe_category.py
@@ -2,18 +2,22 @@ from ..write_text import write_text
 
 
 def parse_recipe_category(json, origin):
-    cid = json["id"]
-    if cid == 'CC_NONCRAFT':
+    ident = json["id"]
+    if ident == 'CC_NONCRAFT':
         return
-    cat_name = cid.split("_")[1]
-    write_text(cat_name, origin, comment="Crafting recipes category name")
+
+    cat_name = ident.split("_")[1]
+    write_text(cat_name, origin, comment="Crafting recipes category")
 
     for subcat in json.get("recipe_subcategories", []):
-        if subcat == 'CSC_ALL':
-            write_text("ALL", origin,
-                       comment="Crafting recipes subcategory \"all\"")
-        else:
-            subcat_name = subcat.split('_')[2]
-            write_text(subcat_name, origin,
-                       comment="Crafting recipes subcategory of \"{}\" cat."
-                       .format(cat_name))
+        subcat_name = subcat.split('_')[-1]
+        write_text(subcat_name, origin,
+                   comment=f"Crafting recipes subcategory of '{cat_name}'")
+        # if subcat == 'CSC_ALL':
+        #     write_text("ALL", origin,
+        #                comment="Crafting recipes subcategory 'all'")
+        # else:
+        #     subcat_name = subcat.split('_')[2]
+        #     write_text(subcat_name, origin,
+        #                comment="Crafting recipes subcategory "
+        #                f"of '{cat_name}'.")
