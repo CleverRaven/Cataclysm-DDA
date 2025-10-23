d0b2ebf3a46acd1cfbfe80d3e530022bfc20515e
Consoleable <107653330+Consoleable@users.noreply.github.com>
2025-10-22 19:42:09 -0700

    add accurate info to test

--- src/requirements.cpp
+++ src/requirements.cpp
@@ -1714,7 +1714,7 @@ deduped_requirement_data::deduped_requirement_data( const requirement_data &in,
         // Because this algorithm is super-exponential in the worst case, add a
         // sanity check to prevent things getting too far out of control.
         // The worst case in the core game currently is boots_fur
-        // with 104 alternatives.
+        // with 114 alternatives.
         static constexpr size_t max_alternatives = 115;
         if( alternatives_.size() + pending.size() > max_alternatives ) {
             debugmsg( "Construction of deduped_requirement_data generated too many alternatives.  "
