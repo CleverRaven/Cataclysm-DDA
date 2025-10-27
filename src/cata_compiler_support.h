5fd266801a689c3d6f5659fba8f692b621aa9fc6
a666 <19142162+a666@users.noreply.github.com>
2025-10-27 17:44:02 -0600

    Move another compiler macro to cata_compiler_support.h

--- src/cata_compiler_support.h
+++ src/cata_compiler_support.h
@@ -1,10 +1,20 @@
 #pragma once
 
 // Added due to clang 21.1 issue https://github.com/llvm/llvm-project/issues/154493
-#if defined(__clang__)
+#ifdef __clang__
 #   if __clang_major__ == 21 && __clang_minor__ <= 1
 #       define LAMBDA_NORETURN_CLANG21x1 __attribute__( ( noreturn ) )
 #   endif
 #else
 #   define LAMBDA_NORETURN_CLANG21x1
 #endif
+
+// Added for MSVC optimizations
+// ref: https://github.com/CleverRaven/Cataclysm-DDA/pull/75376
+#ifndef CATA_FORCEINLINE
+#   ifdef _MSC_VER
+#       define CATA_FORCEINLINE __forceinline
+#   else
+#       define CATA_FORCEINLINE inline __attribute__( ( always_inline ) )
+#   endif
+#endif // CATA_FORCEINLINE
