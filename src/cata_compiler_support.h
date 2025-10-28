83e12f008db2fb17f67bcb9a8c249472d65b1bf0
a666 <19142162+a666@users.noreply.github.com>
2025-10-28 09:43:07 -0600

    Fix conditional define for all compilers

--- src/cata_compiler_support.h
+++ src/cata_compiler_support.h
@@ -1,10 +1,8 @@
 #pragma once
 
 // Added due to clang 21.1 issue https://github.com/llvm/llvm-project/issues/154493
-#ifdef __clang__
-#   if __clang_major__ == 21 && __clang_minor__ <= 1
-#       define LAMBDA_NORETURN_CLANG21x1 __attribute__( ( noreturn ) )
-#   endif
+#if defined(__clang__) && __clang_major__ == 21 && __clang_minor__ <= 1
+#   define LAMBDA_NORETURN_CLANG21x1 __attribute__( ( noreturn ) )
 #else
 #   define LAMBDA_NORETURN_CLANG21x1
 #endif
