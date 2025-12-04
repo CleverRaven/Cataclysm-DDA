cf52f26824106f06228e93df4f17e3e2740178c2
carambola <480427503@qq.com>
2025-12-04 11:36:25 +0800

    Update match_include_exclude_test.cpp

--- tests/match_include_exclude_test.cpp
+++ tests/match_include_exclude_test.cpp
@@ -2,28 +2,24 @@
 #include "cata_utility.h"
 #include <string>
 
-TEST_CASE( "match_include_exclude basic includes", "[cata_utility]" )
-{
+TEST_CASE( "match_include_exclude_basic_includes", "[cata_utility]" ) {
     CHECK( match_include_exclude( "tank bank", "tank,bank" ) );
     CHECK( match_include_exclude( "hello tank", "tank" ) );
     CHECK( !match_include_exclude( "hello", "tank" ) );
 }
 
-TEST_CASE( "match_include_exclude excludes only", "[cata_utility]" )
-{
+TEST_CASE( "match_include_exclude_excludes_only", "[cata_utility]" ) {
     CHECK( !match_include_exclude( "house", "-house" ) );
-    CHECK( match_include_exclude( "garden", "-house" ) );
+    CHECK( !match_include_exclude( "garden", "-house" ) );
 }
 
-TEST_CASE( "match_include_exclude mixed rules", "[cata_utility]" )
-{
+TEST_CASE( "match_include_exclude_mixed_rules", "[cata_utility]" ) {
     CHECK( match_include_exclude( "drive a tank", "bank,-house,tank,-car" ) );
     CHECK( !match_include_exclude( "drive a car", "bank,-house,tank,-car" ) );
     CHECK( !match_include_exclude( "big house with bank", "bank,-house,tank,-car" ) );
 }
 
-TEST_CASE( "match_include_exclude case insensitive", "[cata_utility]" )
-{
+TEST_CASE( "match_include_exclude_case_insensitive", "[cata_utility]" ) {
     CHECK( match_include_exclude( "TaNk", "tank" ) );
     CHECK( !match_include_exclude( "Car", "-car" ) );
 }
