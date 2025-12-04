c1e5050fce925b47f7245ab37123062709ae08ca
carambola <480427503@qq.com>
2025-12-04 11:39:55 +0800

    Update tests/match_include_exclude_test.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/match_include_exclude_test.cpp
+++ tests/match_include_exclude_test.cpp
@@ -15,7 +15,8 @@ TEST_CASE( "match_include_exclude_excludes_only", "[cata_utility]" )
     CHECK( !match_include_exclude( "garden", "-house" ) );
 }
 
-TEST_CASE( "match_include_exclude_mixed_rules", "[cata_utility]" ) {
+TEST_CASE( "match_include_exclude_mixed_rules", "[cata_utility]" )
+{
     CHECK( match_include_exclude( "drive a tank", "bank,-house,tank,-car" ) );
     CHECK( !match_include_exclude( "drive a car", "bank,-house,tank,-car" ) );
     CHECK( !match_include_exclude( "big house with bank", "bank,-house,tank,-car" ) );
