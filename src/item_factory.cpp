02dc7e9088c6437bfb92c7aecd7aaf6ed1a33570
MilosRasic <milos.rasic@gmail.com>
2025-12-05 01:59:36 +0100

    fix mod item name inheritance

--- src/item_factory.cpp
+++ src/item_factory.cpp
@@ -4134,7 +4134,10 @@ void itype::load( const JsonObject &jo, std::string_view src )
         degrade_increments_ = std::isnan( adjusted_inc ) ? 0 : std::round( adjusted_inc );
     }
 
-    name = translation( translation::plural_tag() );
+    if( !was_loaded ) {
+        name = translation( translation::plural_tag() );
+    }
+
     mandatory( jo, was_loaded, "name", name );
     optional( jo, was_loaded, "conditional_names", conditional_names );
     optional( jo, was_loaded, "description", description );
