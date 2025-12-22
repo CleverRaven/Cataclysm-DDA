f31b8873d22b54f5076bcc692300f5ef84adbb8e
RenechCDDA <84619419+RenechCDDA@users.noreply.github.com>
2025-12-22 16:02:03 -0500

    Don't erase our coords_set, we're not *done*.

--- src/activity_actor.cpp
+++ src/activity_actor.cpp
@@ -10208,7 +10208,6 @@ bool zone_sort_activity_actor::stage_think( player_activity &act, Character &you
     // iterate over zone positions and look for items to move
     for( const tripoint_abs_ms &src : src_sorted ) {
         placement = src;
-        coord_set.erase( src );
 
         const tripoint_bub_ms src_bub = here.get_bub( src );
         if( !here.inbounds( src_bub ) ) {
