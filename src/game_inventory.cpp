99453dcb78d11258256b04d09976d6f9af7ba7ee
Fris0uman <fris0uman@users.noreply.github.com>
2026-01-04 12:01:26 +0100

    special case pet food

--- src/game_inventory.cpp
+++ src/game_inventory.cpp
@@ -1098,9 +1098,9 @@ class activatable_inventory_preset : public pickup_inventory_preset
         }
 
         bool is_shown( const item_location &loc ) const override {
-            return !loc.is_invisible_installed_gunmod() && ( loc->type->has_use() ||
-                    loc->has_relic_activation() ) && !loc->is_medical_tool() &&
-                   !loc->is_medication();
+            return !loc.is_invisible_installed_gunmod() &&  loc->is_craft() || ( ( loc->type->has_use() ||
+                    loc->has_relic_activation() ) &&
+                    ( !loc->is_comestible() || loc->type->can_use( "PETFOOD" ) ) );
         }
 
         std::string get_denial( const item_location &loc ) const override {
