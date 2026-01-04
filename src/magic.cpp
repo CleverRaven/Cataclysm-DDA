102291289ad92274b377a5a6ac4777f07cb5484f
Anton Simakov <67688115+GuardianDll@users.noreply.github.com>
2026-01-04 11:47:52 +0100

    update code and tests

--- src/magic.cpp
+++ src/magic.cpp
@@ -514,7 +514,8 @@ void spell_type::check_consistency()
                       sp_t.id.c_str() );
         }
 
-        if( !sp_t.spell_class.is_valid() ) {
+        // TODO: this may need to use trait_id::NULL_ID()?
+        if( !sp_t.spell_class.is_valid() && sp_t.spell_class.c_str() != "NONE" ) {
             debugmsg( R"(ERROR: %s has invalid spell class "%s"!)", sp_t.id.c_str(), sp_t.spell_class.c_str() );
         }
 
