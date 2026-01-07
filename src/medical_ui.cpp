a08d05fc222ec6e8929dbed0a3d73fae5cfe72de
Fris0uman <fris0uman@users.noreply.github.com>
2026-01-07 15:34:00 +0100

    medical tools are special

--- src/medical_ui.cpp
+++ src/medical_ui.cpp
@@ -25,10 +25,8 @@
 #include "flag.h"
 #include "game.h"
 #include "game_constants.h"
-#include "game_inventory.h"
 #include "input_context.h"
 #include "input_enums.h"
-#include "item_location.h"
 #include "output.h"
 #include "point.h"
 #include "string_formatter.h"
@@ -1023,9 +1021,8 @@ void Character::disp_medical()
         } else if( action == "APPLY" ) {
             avatar *a = this->as_avatar();
             if( a ) {
-                item_location loc = game_menus::inv::consume( "MED" );
-                avatar_action::eat( *a, loc );
-                break; // Close Medical Menu so that consume menu can work properly
+                avatar_action::use_item( *a );
+                break;
             } else {
                 popup( _( "Applying not implemented for NPCs." ) );
             }
