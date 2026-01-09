9656440ae8d64203a4c83cd2bd7c34bdbb111cea
gimy77357-netizen <230296971+gimy77357-netizen@users.noreply.github.com>
2026-01-09 18:07:32 +0900

    IWYU fix

--- src/lang_stats.cpp
+++ src/lang_stats.cpp
@@ -6,7 +6,7 @@
 using namespace std::literals::string_view_literals;
 
 static constexpr std::initializer_list<lang_stats> all_lang_stats = {
-#include "lang_stats.inc"
+#include "lang_stats.inc" // IWYU pragma: keep
 };
 
 const lang_stats *lang_stats_for( std::string_view lang )
