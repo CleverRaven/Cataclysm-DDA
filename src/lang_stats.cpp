6e028713f421c44690b7b2d146d99a093a514450
gimy77357-netizen <230296971+gimy77357-netizen@users.noreply.github.com>
2026-01-09 17:36:03 +0900

    Make the game show translation percentage

--- src/lang_stats.cpp
+++ src/lang_stats.cpp
@@ -6,6 +6,7 @@
 using namespace std::literals::string_view_literals;
 
 static constexpr std::initializer_list<lang_stats> all_lang_stats = {
+#include "lang_stats.inc"
 };
 
 const lang_stats *lang_stats_for( std::string_view lang )
