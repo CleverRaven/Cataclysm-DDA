e4003b660dd0597c6c359a4b786ee2db156d4506
timbuntu <tim.christen@gmx.de>
2026-01-17 16:40:52 +0100

    refactor(curses): Improve UTF-8 input parsing
    Only single byte input values between 127 and 245
    indicate UTF-8 input, for all other values we should
    not try to interpret them as UTF-8.

--- src/cata_imgui.cpp
+++ src/cata_imgui.cpp
@@ -203,7 +203,7 @@ void cataimgui::client::process_input( void *input )
         } else {
             int ch = curses_input->get_first_input();
             if( ch != UNKNOWN_UNICODE ) {
-                if( ch <= 0xff ) { // Values bigger than 1 byte represent function keys
+                if( ch > 127 && ch < 245 ) { // Values between 127 and 245 indicate UTF-8
                     ch = utf8_wrapper( curses_input->text ).at( 0 );
                 }
                 imtui_events.push_back( std::pair<int, ImTui::mouse_event>( ch, new_mouse_event ) );
