8639b2a644a980bbad5c0cf25d88830ba0c200f0
Tektolnes <43859252+Tektolnes@users.noreply.github.com>
2026-01-17 14:49:45 +0100

    proof of concept draft 1

--- src/mapdata.cpp
+++ src/mapdata.cpp
@@ -329,6 +329,9 @@ std::string enum_to_string<ter_furn_flag>( ter_furn_flag data )
         case ter_furn_flag::TFLAG_MON_AVOID_STRICT: return "MON_AVOID_STRICT";
         case ter_furn_flag::TFLAG_REGION_PSEUDO: return "REGION_PSEUDO";
         case ter_furn_flag::TFLAG_PHASE_BACK: return "PHASE_BACK";
+        case ter_furn_flag::TFLAG_ONE_DIMENSIONAL_X: return "ONE_DIMENSIONAL_X";
+        case ter_furn_flag::TFLAG_ONE_DIMENSIONAL_Y: return "ONE_DIMENSIONAL_Y";
+        case ter_furn_flag::TFLAG_ONE_DIMENSIONAL_Z: return "ONE_DIMENSIONAL_Z";
 
         // *INDENT-ON*
         case ter_furn_flag::NUM_TFLAG_FLAGS:
