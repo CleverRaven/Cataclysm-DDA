e9f79b8cd16a27ea36c1305f95d38b8b5807aa78
yuganxia <601299986@qq.com>
2026-01-22 14:58:58 +0800

    Update debug_menu.cpp

--- src/debug_menu.cpp
+++ src/debug_menu.cpp
@@ -8,6 +8,7 @@
 #include <exception>
 #include <filesystem>
 #include <functional>
+#include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <list>
@@ -832,7 +833,9 @@ static void monster_edit_menu()
         data << string_format( _( "Faction: %s" ), critter->get_monster_faction().id().str() ) << std::endl;
         data << string_format( _( "HP(max): %d (%d)" ), critter->get_hp(),
                                critter->get_hp_max() ) << std::endl;
-        data << string_format( _( "Volume: %sL (size %s)" ), units::to_liter( critter->get_volume() ),
+        std::ostringstream vol_ss;
+        vol_ss << std::fixed << std::setprecision( 2 ) << units::to_liter( critter->get_volume() );
+        data << string_format( _( "Volume: %sL (size %s)" ), vol_ss.str(),
                                size_string ) << std::endl;
         data << string_format( _( "Speed(base): %d (%d)" ), critter->get_speed(),
                                critter->get_speed_base() ) << std::endl;
