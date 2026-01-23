22fd2515c788e8d9fb38329e543d5403d99f18f3
Artem Manushenkov <saintgolubets@172.31.105.129-vpn.dhcp.yndx.net>
2026-01-23 22:33:38 +0300

    increate test tolerance a smidge

--- tests/coverage_test.cpp
+++ tests/coverage_test.cpp
@@ -226,7 +226,7 @@ TEST_CASE( "Ranged_coverage_vs_bullet", "[coverage] [ranged]" )
 {
     SECTION( "Full melee and ranged coverage vs. ranged attack" ) {
         const float dmg = get_avg_bullet_dmg( itype_test_hazmat_suit );
-        check_near( "Average damage", dmg, 16.4f, 0.2f );
+        check_near( "Average damage", dmg, 16.4f, 0.25f );
     }
 
     SECTION( "No ranged coverage vs. ranged attack" ) {
