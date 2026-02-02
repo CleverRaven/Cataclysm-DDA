37307c607b9c2ffd0382e6b49be68cafdf6904d2
thaelina <thaelina@gmail.com>
2026-02-01 19:38:16 -0600

    Support copy from in string extractor for overmap special

--- lang/string_extractor/parsers/overmap_special.py
+++ lang/string_extractor/parsers/overmap_special.py
@@ -3,10 +3,11 @@ from ..write_text import write_text
 
 def parse_overmap_special(json, origin):
 
-    overmaps = json["overmaps"]
+    overmaps = json.get("overmaps")
     if json.get("subtype") == "mutable":
-        overmaps = json["overmaps"].values()
+        overmaps = json.get("overmaps").values()
 
-    for om in overmaps:
-        write_text(om.get("camp_name"), origin,
-                   comment="Name of NPC faction camp")
+    if (overmaps):
+        for om in overmaps:
+            write_text(om.get("camp_name"), origin,
+                    comment="Name of NPC faction camp")
