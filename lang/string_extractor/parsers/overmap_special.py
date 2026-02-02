c80728857a79f11ff19d703e7a1d01f3570b004d
thaelina <thaelina@gmail.com>
2026-02-01 19:46:26 -0600

    Move check up to fail earlier if there is no overmaps

--- lang/string_extractor/parsers/overmap_special.py
+++ lang/string_extractor/parsers/overmap_special.py
@@ -4,10 +4,10 @@ from ..write_text import write_text
 def parse_overmap_special(json, origin):
 
     overmaps = json.get("overmaps")
-    if json.get("subtype") == "mutable":
-        overmaps = json.get("overmaps").values()
-
     if (overmaps):
+        if json.get("subtype") == "mutable":
+            overmaps = json.get("overmaps").values()
+
         for om in overmaps:
             write_text(om.get("camp_name"), origin,
                     comment="Name of NPC faction camp")
