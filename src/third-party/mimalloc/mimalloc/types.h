cc4dd06de3b0cc070c0a1e1e36ff680041a4bf5e
Andrew Krieger <akrieger@users.noreply.github.com>
2026-02-10 13:20:38 -0800

    Mangle mimalloc includes to avoid mangling build files.

--- src/third-party/mimalloc/mimalloc/types.h
+++ src/third-party/mimalloc/mimalloc/types.h
@@ -22,7 +22,7 @@ terms of the MIT license. A copy of the license can be found in the file
 // --------------------------------------------------------------------------
 
 
-#include <mimalloc-stats.h>
+#include <mimalloc/mimalloc-stats.h>
 #include <stddef.h>   // ptrdiff_t
 #include <stdint.h>   // uintptr_t, uint16_t, etc
 #include <stdbool.h>  // bool
