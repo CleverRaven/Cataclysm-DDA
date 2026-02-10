f29afa09d9bd5dce4fc3e6e6a40dfa2272604093
Standing-Storm <120433252+Standing-Storm@users.noreply.github.com>
2026-02-10 15:54:33 -0600

    Update src/mutation_ui.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/mutation_ui.cpp
+++ src/mutation_ui.cpp
@@ -454,8 +454,8 @@ void avatar::power_mutations()
                             } else if( ( !mut_data.hunger || get_kcal_percent() >= 0.8f ) &&
                                        ( !mut_data.thirst || get_thirst() <= 400 ) &&
                                        ( !mut_data.sleepiness || get_sleepiness() <= 400 ) &&
-                                       ( !mut_data.mana || magic->available_mana() >= mut_data.cost ) && 
-                                       ( !mut_data.stamina || get_stamina() >= mut_data.cost  ) ) {
+                                       ( !mut_data.mana || magic->available_mana() >= mut_data.cost ) &&
+                                       ( !mut_data.stamina || get_stamina() >= mut_data.cost ) ) {
                                 add_msg_if_player( m_neutral,
                                                    string_format( mut_data.activation_msg.translated(), mutation_name( mut_data.id ) ) );
                                 // Reset menu in advance
