6a75f4bd777314255fc6c0e6ee8842d4f746c561
SariusSkelrets <sariusskelrets@icloud.com>
2026-02-25 21:09:12 -0500

    Remove the weakness & rework morality removal.

--- src/consumption.cpp
+++ src/consumption.cpp
@@ -205,6 +205,7 @@ static const trait_id trait_UNDINE_SLEEP_WATER( "UNDINE_SLEEP_WATER" );
 static const trait_id trait_VEGAN( "VEGAN" );
 static const trait_id trait_VEGETARIAN( "VEGETARIAN" );
 static const trait_id trait_WATERSLEEP( "WATERSLEEP" );
+static const trait_id trait_BLOOD_DRINKER("BLOOD_DRINKER");
 
 static const vitamin_id vitamin_bread_allergen( "bread_allergen" );
 static const vitamin_id vitamin_egg_allergen( "egg_allergen" );
@@ -1428,7 +1429,11 @@ void Character::modify_morale( item &food, const int nutr )
             // Reduced penalties
             add_morale( morale_cannibal, moderate_morale_penalty, maximum_stacked_morale_penalty,
                         7_days, 4_days );
-        } else if( cannibal && psycho ) {
+        }
+        else if (has_trait(trait_BLOOD_DRINKER)) {
+            add_msg_if_player(m_good, _("You sate your thirst for blood.  You want more."));
+        }
+        else if( cannibal && psycho ) {
             add_msg_if_player( m_good,
                                _( "You worry that your hunger for human flesh is going to be a liability one of these days." ) );
             // Reduced penalties
