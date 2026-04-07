1cff20e46cfb415e477e94942f3ae1a0344cff4a
b3brodie <b3brodie@gmail.com>
2026-04-06 17:56:59 -0700

    Do that clang thingy

--- src/magic_enchantment.cpp
+++ src/magic_enchantment.cpp
@@ -27,7 +27,7 @@
 #include "units.h"
 #include "vehicle.h"
 
-static const bodypart_str_id all_bodypart_id( "all" );
+static const bodypart_str_id body_part_all( "all" );
 
 namespace io
 {
@@ -1164,7 +1164,7 @@ double enchant_cache::get_skill_value_add( const skill_id &value ) const
 int enchant_cache::get_encumbrance_add( const bodypart_str_id &value ) const
 {
     return get_value<bodypart_str_id>( value,
-                                       encumbrance_values_add ) + get_value<bodypart_str_id>( all_bodypart_id, encumbrance_values_add );
+                                       encumbrance_values_add ) + get_value<bodypart_str_id>( body_part_all, encumbrance_values_add );
 }
 
 int enchant_cache::get_damage_add( const damage_type_id &value ) const
@@ -1195,7 +1195,7 @@ double enchant_cache::get_skill_value_multiply( const skill_id &value ) const
 double enchant_cache::get_encumbrance_multiply( const bodypart_str_id &value ) const
 {
     return get_value<bodypart_str_id>( value,
-                                       encumbrance_values_multiply ) + get_value<bodypart_str_id>( all_bodypart_id,
+                                       encumbrance_values_multiply ) + get_value<bodypart_str_id>( body_part_all,
                                                encumbrance_values_multiply );
 }
 
