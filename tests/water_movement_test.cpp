936bbcbace6746bf05e7235aa7a8b67b715eb098
Standing-StormStanding-Storm git config --global user.name Standing-Storm git config --global user.name Standing-Storm <dorchadas@gmail.com>
2026-04-10 09:57:07 -0500

    Fixes

--- tests/water_movement_test.cpp
+++ tests/water_movement_test.cpp
@@ -914,7 +914,7 @@ static std::map<std::string, swim_result> expected_results = {
     {"move: walk, stats: minimum, skills: professional, gear: flotation vest, traits: paws", swim_result{400, 44}},
     {"move: walk, stats: minimum, skills: professional, gear: flotation vest, traits: webbed hands", swim_result{400, 44}},
     {"move: walk, stats: minimum, skills: professional, gear: flotation vest, traits: webbed hands and feet", swim_result{400, 44}},
-    {"move: walk, stats: minimum, skills: professional, gear: none, traits: large paws", swim_result{118, 204}},
+    {"move: walk, stats: minimum, skills: professional, gear: none, traits: large paws", swim_result{118, 205}},
     {"move: walk, stats: minimum, skills: professional, gear: none, traits: none", swim_result{150, 144}},
     {"move: walk, stats: minimum, skills: professional, gear: none, traits: paws", swim_result{125, 187}},
     {"move: walk, stats: minimum, skills: professional, gear: none, traits: webbed hands", swim_result{114, 215}},
