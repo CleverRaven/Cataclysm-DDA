d9a1342851b79c408a467e68775f406c42801548
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2026-05-10 22:59:07 -0500

    Update magic_spell_effect.cpp

--- src/magic_spell_effect.cpp
+++ src/magic_spell_effect.cpp
@@ -1609,6 +1609,7 @@ void spell_effect::charm_monster( const spell &sp, Creature &caster, const tripo
             continue;
         }
         sp.make_sound( potential_target, caster );
+        bool success = false;
         if( ( mon->friendly == 0 || ( mon->friendly != 0 && sp.has_flag( spell_flag::RECHARM ) ) ) &&
             mon->get_hp() <= sp.damage( caster ) ) {
             mon->unset_dest();
@@ -1617,6 +1618,12 @@ void spell_effect::charm_monster( const spell &sp, Creature &caster, const tripo
                 mon->friendly = -1;
                 mon->add_effect( effect_pet, 1_turns, true );
             }
+            success = true;
+        }
+        if( success ) {
+            add_msg_if_player_sees( potential_target, m_good, _( "You charm the %s!" ), mon->name() );
+        } else {
+            add_msg_if_player_sees( potential_target, m_bad, _( "The %s resists your spell." ), mon->name() );
         }
     }
 }
