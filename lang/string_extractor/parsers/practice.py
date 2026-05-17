830d6bd1c91a048b1f1f697fc29c5174ae9bbaa1
Wind Shodow <2552777415@qq.com>
2026-05-17 12:37:00 +0800

    extract practice result EOCs

--- lang/string_extractor/parsers/practice.py
+++ lang/string_extractor/parsers/practice.py
@@ -1,6 +1,6 @@
 from ..helper import get_singular_name
 from ..write_text import write_text
-
+from .effect import parse_effect_on_condition
 
 def parse_practice(json, origin):
     name = get_singular_name(json)
@@ -9,3 +9,6 @@ def parse_practice(json, origin):
                comment="Practice name")
     write_text(json.get("description"), origin,
                comment=f"Description of practice '{name}'")
+    for eoc in json.get("result_eocs", []):
+        parse_effect_on_condition(eoc, origin,
+                                  f"Result of practice '{name}'")
