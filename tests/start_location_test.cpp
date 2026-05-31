0591e9f073005572e34d75befdeeb5a4c3c14cf7
Fris0uman <fris0uman@users.noreply.github.com>
2026-05-31 17:52:26 +0200

    Add a test for origin_offset

---
+++ tests/start_location_test.cpp
@@ -0,0 +1,26 @@
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "cata_catch.h"
+#include "game.h"
+#include "scenario.h"
+#include "start_location.h"
+#include "string_id.h"
+
+static const string_id<scenario> scenario_beach_day( "beach_day" );
+static const start_location_id start_location_ocean_shore( "sloc_ocean_shore" );
+
+TEST_CASE( "Test_origin_offset" )
+{
+    const point_rel_om &offset = scenario_beach_day.obj().get_origin_offset();
+    const start_location &beach = start_location_ocean_shore.obj();
+    const point_abs_om origin = point_abs_om::zero + offset;
+
+    tripoint_abs_omt omtstart = tripoint_abs_omt::invalid;
+
+    auto ret = beach.find_player_initial_location( origin );
+    omtstart = ret.first;
+
+    CHECK( !omtstart.is_invalid() );
+}
