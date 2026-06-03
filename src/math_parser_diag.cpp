cc4a8a5da60dac13a46905d5ee66706e75e1e259
Anton Simakov <67688115+GuardianDll@users.noreply.github.com>
2026-06-03 22:26:58 +0200

    fix math effect to check light level (#87384)
    * fix math effect to check light level
    
    * Astyle
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/math_parser_diag.cpp
+++ src/math_parser_diag.cpp
@@ -257,11 +257,11 @@ double consumption_count_total_eval( const_dialogue const &d, char scope,
     return count;
 }
 
-double light_level_eval( const_dialogue const &/* d */, char /* scope */,
+double light_level_eval( const_dialogue const &d, char /* scope */,
                          std::vector<diag_value> const &params, diag_kwargs const &/* kwargs */ )
 {
     const map &here = get_map();
-    tripoint_abs_ms loc_val = params[0].tripoint();
+    tripoint_abs_ms loc_val = params[0].tripoint( d );
 
     return static_cast<double>( here.light_at( here.get_bub( loc_val ) ) );
 }
