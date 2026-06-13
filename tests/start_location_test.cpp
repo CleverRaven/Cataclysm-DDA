bd1489cb61cceb977ae25f8f6f100bf9eef1fd7d
Fris0uman <fris0uman@users.noreply.github.com>
2026-06-13 09:58:55 +0200

    clang tidy fix

--- tests/start_location_test.cpp
+++ tests/start_location_test.cpp
@@ -9,12 +9,12 @@
 #include "type_id.h"
 
 static const string_id<scenario> scenario_beach_day( "beach_day" );
-static const start_location_id start_location_ocean_shore( "sloc_ocean_shore" );
+static const start_location_id start_location_sloc_ocean_shore( "sloc_ocean_shore" );
 
 TEST_CASE( "Test_origin_offset" )
 {
     const point_rel_om &offset = scenario_beach_day.obj().get_origin_offset();
-    const start_location &beach = start_location_ocean_shore.obj();
+    const start_location &beach = start_location_sloc_ocean_shore.obj();
     const point_abs_om origin = point_abs_om::zero + offset;
 
     tripoint_abs_omt omtstart = tripoint_abs_omt::invalid;
