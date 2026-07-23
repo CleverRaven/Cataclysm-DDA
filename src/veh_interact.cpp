36b8b6ffb4f5794989dbace6fbf0cc16214d5888
Ryan <breuerryan@gmail.com>
2026-07-23 08:23:26 -0500

    Switch item for itype_id and use item_measure_prefix

--- src/veh_interact.cpp
+++ src/veh_interact.cpp
@@ -1809,20 +1809,21 @@ bool veh_interact::can_remove_part( map &here, int idx, const Character &you )
                     _( "<color_white>Removing the %1$s will yield:</color>\n> %2$s\n" ),
                     sel_vehicle_part->name(), result_of_removal.display_name() );
 
-        std::map<item, int> unique_salvageable_items;
+        std::map<itype_id, int> unique_salvageable_items;
         for( const item &it : sel_vehicle_part->get_salvageable() ) {
-            unique_salvageable_items[ it ] += 1;
+            unique_salvageable_items[ it.typeId() ] += 1;
         }
 
-        std::map<item, int>::iterator it;
+        std::map<itype_id, int>::iterator it;
         for( it = unique_salvageable_items.begin(); it != unique_salvageable_items.end(); it++ ) {
+            const itype *type = item::find_type( it->first );
             nmsg += "> ";
 
             if( it->second > 1 ) {
-                nmsg += std::to_string( it->second ) + " ";
+                nmsg += type->item_measure_prefix( it->second ) + " ";
             }
 
-            nmsg += it->first.display_name( it->second ) + "\n";
+            nmsg += type->nname( it->second ) + "\n";
         }
     }
 
