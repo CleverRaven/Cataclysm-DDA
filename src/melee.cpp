93afca7fec2f931f9f1e4a90470c6b184694472e
ahhwatson <alisonw4tson@gmail.com>
2026-07-26 04:44:50 -0700

    Update src/melee.cpp for astyle
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/melee.cpp
+++ src/melee.cpp
@@ -982,7 +982,8 @@ bool Character::melee_attack_abstract( Creature &t, bool allow_special,
                    total_stam + deft_bonus );
     // Weariness handling - 1 / the value, because it returns what % of the normal speed
     // ADRENALINE is defined in game_constants.h
-    const float weary_mult = std::max( ADRENALINE * exertion_adjusted_move_multiplier( EXTRA_EXERCISE ), 1.0f );
+    const float weary_mult = std::max( ADRENALINE * exertion_adjusted_move_multiplier( EXTRA_EXERCISE ),
+                                       1.0f );
     mod_moves( forced_movecost >= 0 ? -forced_movecost : -move_cost * ( 1 / weary_mult ) );
     // trigger martial arts on-attack effects
     martial_arts_data->ma_onattack_effects( *this );
