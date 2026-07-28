e21c191bd11d7f1c386d723bb6d2de0876d9ee0c
Alison W <alisonw4tson@gmail.com>
2026-07-27 21:44:56 -0700

    Fixed logic from max to min.

--- src/melee.cpp
+++ src/melee.cpp
@@ -981,8 +981,8 @@ bool Character::melee_attack_abstract( Creature &t, bool allow_special,
     add_msg_debug( debugmode::DF_MELEE, "Stamina burn base/total (capped at -50): %d/%d", base_stam,
                    total_stam + deft_bonus );
     // Weariness handling - 1 / the value, because it returns what % of the normal speed
-    // COMBAT_SPEED_MODIFIER is defined in game_constants.h
-    const float weary_mult = std::max( COMBAT_SPEED_MODIFIER * exertion_adjusted_move_multiplier(
+    // Sets the % move speed to the smaller of the modified speed or 100%, to avoid changing baseline attack speed
+    const float weary_mult = std::min( COMBAT_SPEED_MODIFIER * exertion_adjusted_move_multiplier(
                                            EXTRA_EXERCISE ),
                                        1.0f );
     mod_moves( forced_movecost >= 0 ? -forced_movecost : -move_cost * ( 1 / weary_mult ) );
