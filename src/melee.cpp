d7b4201feaf0a94deb972ea3babdca21770abf4f
Alison W <alisonw4tson@gmail.com>
2026-07-28 01:15:04 -0700

    Cache value to character so as to reduce overhead.

--- src/melee.cpp
+++ src/melee.cpp
@@ -215,6 +215,12 @@ bool Character::unarmed_attack() const
     return !weap;
 }
 
+int COMBAT_SPEED_MODIFIER() const
+{
+    const COMBAT_SPEED_MODIFIER_CACHE = get_option<float>( "COMBAT_SPEED_MODIFIER" );
+    return COMBAT_SPEED_MODIFIER_CACHE;
+}
+
 bool Character::handle_melee_wear( item_location shield, float wear_multiplier )
 {
     if( wear_multiplier <= 0.0f ) {
@@ -982,7 +988,7 @@ bool Character::melee_attack_abstract( Creature &t, bool allow_special,
                    total_stam + deft_bonus );
     // Weariness handling - 1 / the value, because it returns what % of the normal speed
     // Set the % move speed penalty to the smaller of the modified speed or 100%, to avoid changing baseline attack speed
-    const float weary_mult = 1 / std::min( get_option<float>( "COMBAT_SPEED_MODIFIER" ) *
+    const float weary_mult = 1 / std::min( COMBAT_SPEED_MODIFIER() *
                                            exertion_adjusted_move_multiplier(
                                                    EXTRA_EXERCISE ),
                                            1.0f );
@@ -1079,7 +1085,7 @@ void Character::reach_attack( const tripoint_bub_ms &p, int forced_movecost )
     // Weariness handling
     // 1 / mult because mult is the percent penalty, in the form 1.0 == 100%
     // mult cannot be less than 100% because then that would be a bonus.
-    const float weary_mult = 1 / std::min( get_option<float>( "COMBAT_SPEED_MODIFIER" ) *
+    const float weary_mult = 1 / std::min ( COMBAT_SPEED_MODIFIER() *
                                            exertion_adjusted_move_multiplier(
                                                    EXTRA_EXERCISE ),
                                            1.0f );
