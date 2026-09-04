ea1433eabd3392184cb5f5d5c364abea067ead0e
Matthew Richmond <matthew.richmond@wegmans.com>
2026-09-04 14:44:01 -0400

    Add unit test for form_from_map bulk grouping

---
+++ tests/inventory_test.cpp
@@ -0,0 +1,45 @@
+#include "cata_catch.h"
+#include "inventory.h"
+#include "item.h"
+#include "map.h"
+#include "map_helpers.h"
+#include "type_id.h"
+
+TEST_CASE( "inventory_form_from_map_bulk_batching", "[inventory][map]" )
+{
+    clear_map_without_vision();
+    map &m = get_map();
+
+    // Set up item positions
+    tripoint_bub_ms p1( 0, 0, 0 );
+    tripoint_bub_ms p2( 1, 0, 0 );
+    tripoint_bub_ms p3( 2, 0, 0 );
+
+    // Add items to the map
+    item rock( "rock", calendar::turn );
+    item stick( "stick", calendar::turn );
+    item string( "string_36", calendar::turn );
+
+    for( int i = 0; i < 5; ++i ) {
+        m.add_item_or_charges( p1, rock );
+        m.add_item_or_charges( p1, stick );
+        m.add_item_or_charges( p1, stick ); // Extra stick
+        m.add_item_or_charges( p2, stick );
+        m.add_item_or_charges( p2, string );
+        m.add_item_or_charges( p3, rock );
+    }
+
+    std::vector<tripoint_bub_ms> pts = { p1, p2, p3 };
+
+    inventory inv;
+    inv.form_from_map( m, pts, nullptr, false );
+
+    // Check counts
+    // Rocks: 5 at p1 + 5 at p3 = 10 rocks
+    // Sticks: 10 at p1 + 5 at p2 = 15 sticks
+    // Strings: 5 at p2 = 5 strings
+
+    CHECK( inv.count_item( itype_id( "rock" ) ) == 10 );
+    CHECK( inv.count_item( itype_id( "stick" ) ) == 15 );
+    CHECK( inv.count_item( itype_id( "string_36" ) ) == 5 );
+}
