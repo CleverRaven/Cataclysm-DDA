a290619e1e10a3e1c6872f922d06fcc41dc7e8e2
John Candlebury <johncandlebury@gmail.com>
2026-09-08 08:08:20 -0600

    Update src/projectile.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/projectile.cpp
+++ src/projectile.cpp
@@ -223,9 +223,9 @@ void apply_ammo_effects( Creature *source, const tripoint_bub_ms &p,
             for( const effect_on_condition_id &eoc : ae.eoc ) {
                 Creature *critter = get_creature_tracker().creature_at( p );
                 dialogue d( get_talker_for( *source ), critter == nullptr ? nullptr : get_talker_for( critter ) );
-                write_var_value( var_type::context, "proj_damage", &d, dealt_damage ); 
-                    const tripoint_abs_ms target_location = here.get_abs( p );
-                    write_var_value( var_type::context, "targeted_location", &d, target_location );
+                write_var_value( var_type::context, "proj_damage", &d, dealt_damage );
+                const tripoint_abs_ms target_location = here.get_abs( p );
+                write_var_value( var_type::context, "targeted_location", &d, target_location );
                 eoc->activate( d );
             }
 
