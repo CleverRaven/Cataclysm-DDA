de7ba16cba3fb514cdb72dd17e1c8079c53e23a8
Tektolnes <43859252+Tektolnes@users.noreply.github.com>
2026-09-09 10:59:10 -0400

    Weather can tint sunlight (#88591)
    * tinted-weather
    
    * Update WEATHER_TYPE.md
    
    * Apply batched suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/weather_type.cpp
+++ src/weather_type.cpp
@@ -112,6 +112,13 @@ void weather_type::load( const JsonObject &jo, std::string_view )
     mandatory( jo, was_loaded, "priority", priority );
     optional( jo, was_loaded, "light_multiplier", light_multiplier, 1.f );
     optional( jo, was_loaded, "sun_multiplier", sun_multiplier, 1.f );
+    if( jo.has_array( "tint_color" ) ) {
+        JsonArray jarr = jo.get_array( "tint_color" );
+        tint_color.r = jarr.get_int( 0 ) / 255.0f;
+        tint_color.g = jarr.get_int( 1 ) / 255.0f;
+        tint_color.b = jarr.get_int( 2 ) / 255.0f;
+    }
+    optional( jo, was_loaded, "tint_strength", tint_strength, 0.0f );
 
     mandatory( jo, was_loaded, "sound_attn", sound_attn );
     mandatory( jo, was_loaded, "dangerous", dangerous );
