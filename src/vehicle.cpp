4bc71bfb7567c45563947960c8887b962b2b9040
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2026-09-10 23:04:15 -0500

    Update src/vehicle.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -2994,8 +2994,8 @@ std::vector<std::string> optional_vpart_position::extended_description() const
 
     vehicle &v = value().vehicle();
     if( debug_mode ) {
-    ret.emplace_back( get_origin( v.type->src ) );
-    ret.emplace_back( "--" );
+        ret.emplace_back( get_origin( v.type->src ) );
+        ret.emplace_back( "--" );
     }
 
     ret.emplace_back( string_format( _( "%s (%s)" ), v.name, v.owner->get_name() ) );
