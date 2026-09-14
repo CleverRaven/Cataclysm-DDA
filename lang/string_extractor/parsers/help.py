af20495abc4d0407c76b77017fd8e1c86215b258
Procyonae <45432782+Procyonae@users.noreply.github.com>
2026-09-14 15:16:10 +0100

    Update translation parser

--- lang/string_extractor/parsers/help.py
+++ lang/string_extractor/parsers/help.py
@@ -10,14 +10,14 @@ def parse_help(json, origin):
     for msg in json.get("messages", []):
         if type(msg) is dict:
             if msg.get("subtitle"):
-                write_text(msg["subtitle"]["str"]), origin,
-                       comment=f"Message in help menu category '{name}'")
+                write_text(get_singular_name(msg["subtitle"]), origin,
+                           comment=f"Message in help menu category '{name}'")
             elif msg.get("force_monospaced"):
-                write_text(msg["force_monospaced"]["str"]), origin,
-                       comment=f"Message in help menu category '{name}'")
+                write_text(get_singular_name(msg["force_monospaced"]), origin,
+                           comment=f"Message in help menu category '{name}'")
             elif msg.get("paragraph"):
-                write_text(msg["paragraph"]["str"]), origin,
-                       comment=f"Message in help menu category '{name}'")
+                write_text(get_singular_name(msg["paragraph"]), origin,
+                           comment=f"Message in help menu category '{name}'")
         else:
             write_text(msg, origin,
-                   comment=f"Message in help menu category '{name}'")
+                       comment=f"Message in help menu category '{name}'")
