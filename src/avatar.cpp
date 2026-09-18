201a8474bbb682a4a1e18918d8866c9e59c5958c
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2026-09-17 22:02:45 -0500

    Update avatar.cpp

--- src/avatar.cpp
+++ src/avatar.cpp
@@ -104,6 +104,7 @@ static const itype_id itype_guidebook( "guidebook" );
 static const itype_id itype_mut_longpull( "mut_longpull" );
 
 static const json_character_flag json_flag_ALARMCLOCK( "ALARMCLOCK" );
+static const json_character_flag json_flag_CANNOT_READ_SPELLBOOKS( "CANNOT_READ_SPELLBOOKS" );
 static const json_character_flag json_flag_PAIN_IMMUNE( "PAIN_IMMUNE" );
 static const json_character_flag json_flag_WEBBED_HANDS( "WEBBED_HANDS" );
 
