d09515edd2b6804d27e252b0e9defdffcc3d8bab
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2026-09-18 10:09:15 -0500

    Update src/avatar.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/avatar.cpp
+++ src/avatar.cpp
@@ -505,8 +505,8 @@ bool avatar::read( item_location &book, item_location ereader )
     // src/iuse_actor.cpp -> learn_spell_actor::use
     if( book->get_use( "learn_spell" ) ) {
         if( has_flag( json_flag_CANNOT_READ_SPELLBOOKS ) ) {
-           return false;
-       }
+            return false;
+        }
 
         book->get_use( "learn_spell" )->call( this, *book, pos_bub() );
         return true;
