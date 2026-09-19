55ac9cb0485a41215f2cfd63367661bdaf81a5ac
Maleclypse <54345792+Maleclypse@users.noreply.github.com>
2026-09-19 06:46:49 -0500

    Sorcerers can't read magical script (#88445)
    * Update avatar.cpp
    
    * Apply suggestion from @github-actions[bot]
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update avatar.cpp
    
    * Update classes.json
    
    * Apply suggestion from @Maleclypse
    
    * Update iuse_actor.cpp
    
    * Update iuse_actor.cpp
    
    * Update iuse_actor.cpp
    
    * Apply suggestion from @Maleclypse
    
    * Apply suggestion from @Maleclypse
    
    * Revert "Apply suggestion from @Maleclypse"
    
    This reverts commit 95145f505e91ce516d57836b0ceb83bbcb170d80.
    
    * Update iuse_actor.cpp
    
    * Revert "Update iuse_actor.cpp"
    
    This reverts commit bdfa64eb27d3f2801e23005e6dfd52cd45b8b93b.
    
    * Update avatar.cpp
    
    * Update avatar.cpp
    
    * Apply suggestion from @Maleclypse
    
    * Update src/avatar.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/iuse_actor.cpp
+++ src/iuse_actor.cpp
@@ -174,6 +174,7 @@ static const skill_id skill_traps( "traps" );
 static const trait_id trait_DEBUG_BIONICS( "DEBUG_BIONICS" );
 static const trait_id trait_ILLITERATE( "ILLITERATE" );
 static const trait_id trait_LIGHTWEIGHT( "LIGHTWEIGHT" );
+static const trait_id trait_SORCERER( "SORCERER" );
 static const trait_id trait_TOLERANCE( "TOLERANCE" );
 
 static const trap_str_id tr_firewood_source( "tr_firewood_source" );
@@ -2731,6 +2732,10 @@ std::optional<int> learn_spell_actor::use( Character *p, item &, map *,
     }
     if( p->has_trait( trait_ILLITERATE ) ) {
         p->add_msg_if_player( m_bad, _( "You can't read." ) );
+        return std::nullopt;
+    }
+    if( p->has_trait( trait_SORCERER ) ) {
+        p->add_msg_if_player( m_bad, _( "Sorcerers cannot learn spells from books or scrolls." ) );
         return std::nullopt;
     }
     if( !p->has_morale_to_read() ) {
