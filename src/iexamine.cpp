9f7cd499637d13db62c424d1674b23aa2ab3fc1f
Curtis Merrill <curtis.r.merrill@gmail.com>
2026-09-24 21:09:46 -0400

    revert sap to original shape with item_location

--- src/iexamine.cpp
+++ src/iexamine.cpp
@@ -4518,24 +4518,25 @@ void iexamine::tree_maple_tapped( Character &you, const tripoint_bub_ms &examp )
     const std::string maple_sap_name = item::nname( itype_maple_sap );
 
     map &here = get_map();
-    map_cursor cur( examp );
-    cur.visit_items(
-    [&]( item_location node ) {
-        if( node->will_spill() || node->is_watertight_container() ) {
-            container = node.get_item();
-            if( node->typeId() == itype_maple_sap ) {
-                has_sap = true;
-                charges = node->charges;
-                return VisitResponse::ABORT;
-            }
-            return VisitResponse::NEXT;
+    const map_cursor cur( examp );
+    map_stack items = here.i_at( examp );
+    for( item &it : items ) {
+        if( it.will_spill() || it.is_watertight_container() ) {
+            container = &it;
+
+            item_location( cur, container ).visit_items( [&charges, &has_sap]( const item_location & it ) {
+                if( it->typeId() == itype_maple_sap ) {
+                    has_sap = true;
+                    charges = it->charges;
+                    return VisitResponse::ABORT;
+                }
+                return VisitResponse::NEXT;
+            } );
         }
-        if( node->get_quality_nonrecursive( qual_TREE_TAP ) > 0 ) {
-            spile = node.get_item();
+        if( it.get_quality_nonrecursive( qual_TREE_TAP ) > 0 ) {
+            spile = &it;
         }
-        return VisitResponse::NEXT;
     }
-    );
 
     if( !spile ) {
         return;
