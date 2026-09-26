da87473b45b846c988cd81ebaf71dcceb33718f6
Curtis Merrill <curtis.r.merrill@gmail.com>
2026-09-26 03:14:49 -0400

    fix trade issue

--- src/npctrade.cpp
+++ src/npctrade.cpp
@@ -69,7 +69,12 @@ std::list<item> npc_trading::transfer_items( trade_selector::select_t &stuff, Ch
                     trading_price( giver, receiver, { item_location{ giver, it }, 1 } );
                 if( !f_wants( item_location{ ip.first, it }, price ) ) {
                     giver.i_add_or_drop( *it, 1, ip.first.get_item() );
-                    ip.first.remove_item( *it );
+                    // need some lvalues to pass into this function
+                    int all = INT_MAX;
+                    std::list<item> res{};
+                    it->remove_internal( [it]( const item & other ) {
+                        return &other == it;
+                    }, all, res );
                 }
             }
         }
